108a468c06e62a867f2b9f4043adaa1e1e52b058
Tony Xu <tonyxu@microsoft.com>
2020-04-13 22:54:41 -0700

    Fix extracting nvidia device plugin

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -306,6 +306,18 @@ for NVIDIA_DEVICE_PLUGIN_VERSION in ${NVIDIA_DEVICE_PLUGIN_VERSIONS}; do
     echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
 done
 
+# GPU device plugin
+kubeletDevicePluginPath="/var/lib/kubelet/device-plugins"
+mkdir -p $kubeletDevicePluginPath
+echo "  - $kubeletDevicePluginPath" >> ${VHD_LOGS_FILEPATH}
+
+DEST="/usr/local/nvidia/bin"
+mkdir -p $DEST
+docker run --rm --entrypoint "" -v $DEST:$DEST "nvidia/k8s-device-plugin:1.11" /bin/bash -c "cp /usr/bin/nvidia-device-plugin $DEST" || exit 1
+chmod a+x $DEST/nvidia-device-plugin
+echo "  - extracted nvidia-device-plugin..." >> ${VHD_LOGS_FILEPATH}
+ls -ltr $DEST >> ${VHD_LOGS_FILEPATH}
+
 TUNNELFRONT_VERSIONS="v1.9.2-v3.0.11 v1.9.2-v4.0.11"
 for TUNNELFRONT_VERSION in ${TUNNELFRONT_VERSIONS}; do
     CONTAINER_IMAGE="docker.io/deis/hcp-tunnel-front:${TUNNELFRONT_VERSION}"
@@ -438,7 +450,7 @@ for PATCHED_KUBERNETES_VERSION in ${K8S_VERSIONS}; do
   # and put them to /usr/local/bin/kubelet-${KUBERNETES_VERSION}
   extractHyperkube "docker"
 done
-ls -ltr /usr/local/bin/* >> ${VHD_LOGS_FILEPATH}
+ls -ltr /usr/local/bin >> ${VHD_LOGS_FILEPATH}
 
 # pull patched hyperkube image for AKS
 # this is used by kube-proxy and need to cover previously supported version for VMAS scale up scenario
@@ -470,15 +482,6 @@ for KUBERNETES_VERSION in ${PATCHED_HYPERKUBE_IMAGES}; do
   pullContainerImage "docker" ${CONTAINER_IMAGE}
   echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
 done
-
-# GPU device plugin
-kubeletDevicePluginPath="/var/lib/kubelet/device-plugins"
-mkdir -p $kubeletDevicePluginPath
-echo "  - $kubeletDevicePluginPath" >> ${VHD_LOGS_FILEPATH}
-
-extractNvidiaDevicePlugin "docker" "nvidia/k8s-device-plugin:1.0.0-beta5" "/usr/local/nvidia/bin"
-echo "  - extracting nvidia-device-plugin..." >> ${VHD_LOGS_FILEPATH}
-ls -ltr /usr/local/nvidia/bin/* >> ${VHD_LOGS_FILEPATH}
 
 ADDON_IMAGES="
 mcr.microsoft.com/oss/open-policy-agent/gatekeeper:v2.0.1
