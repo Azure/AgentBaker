66aadd30bdb47df5a6015c915611dfd85172f9d8
Tony Xu <tonyxu@microsoft.com>
2020-04-30 19:36:43 -0700

    Fix creating managed disk with sig issue

--- vhdbuilder/packer/create-managed-disk-from-sig-gen2.sh
+++ vhdbuilder/packer/create-managed-disk-from-sig-gen2.sh
@@ -20,15 +20,13 @@ done
 sig_resource_id="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP_NAME}/providers/Microsoft.Compute/galleries/PackerSigGallery/images/1804Gen2/versions/1.0.${CREATE_TIME}"
 disk_resource_id="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP_NAME}/providers/Microsoft.Compute/disks/1.0.${CREATE_TIME}"
 
-curl -sL https://github.com/yangl900/armclient-go/releases/download/v0.2.3/armclient-go_linux_64-bit.tar.gz | tar xz
-
-./armclient put ${disk_resource_id}?api-version=2019-11-01 "{'location': '$LOCATION', \
-  'properties': { \
-    'osType': '$OS_TYPE', \
-    'creationData': { \
-      'createOption': 'FromImage', \
-      'galleryImageReference': { \
-        'id': '${sig_resource_id}' \
+az resource create --id $disk_resource_id  --is-full-object --location $LOCATION --properties "{\"location\": \"$LOCATION\", \
+  \"properties\": { \
+    \"osType\": \"$OS_TYPE\", \
+    \"creationData\": { \
+      \"createOption\": \"FromImage\", \
+      \"galleryImageReference\": { \
+        \"id\": \"${sig_resource_id}\" \
       } \
     } \
   } \
