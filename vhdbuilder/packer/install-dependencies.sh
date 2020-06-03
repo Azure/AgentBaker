9b613fe66717eaa68b1cb0449421ab3648fab576
Bo Wang <bowa@microsoft.com>
2020-06-02 19:06:45 -0700

    Increase space limit to 99% for now

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -490,8 +490,8 @@ df -h
 
 # warn at 75% space taken
 [ -s $(df -P | grep '/dev/sda1' | awk '0+$5 >= 75 {print}') ] || echo "WARNING: 75% of /dev/sda1 is used" >> ${VHD_LOGS_FILEPATH}
-# error at 96% space taken
-[ -s $(df -P | grep '/dev/sda1' | awk '0+$5 >= 96 {print}') ] || exit 1
+# error at 99% space taken
+[ -s $(df -P | grep '/dev/sda1' | awk '0+$5 >= 99 {print}') ] || exit 1
 
 echo "Using kernel:" >> ${VHD_LOGS_FILEPATH}
 tee -a ${VHD_LOGS_FILEPATH} < /proc/version
