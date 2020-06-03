7a5b136e65bd37c5685c7ccead8af004fb0bcbbb
Tony Xu <tonyxu@microsoft.com>
2020-06-02 20:54:07 -0700

    Revert gen2holder change

--- vhdbuilder/packer/create-managed-disk-from-sig-gen2.sh
+++ vhdbuilder/packer/create-managed-disk-from-sig-gen2.sh
@@ -17,7 +17,7 @@ do
     fi
 done
 
-sig_resource_id="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP_NAME}/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/1804Gen2/versions/1.0.${CREATE_TIME}"
+sig_resource_id="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP_NAME}/providers/Microsoft.Compute/galleries/PackerSigGallery/images/1804Gen2/versions/1.0.${CREATE_TIME}"
 disk_resource_id="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP_NAME}/providers/Microsoft.Compute/disks/1.0.${CREATE_TIME}"
 
 az resource create --id $disk_resource_id  --is-full-object --location $LOCATION --properties "{\"location\": \"$LOCATION\", \
