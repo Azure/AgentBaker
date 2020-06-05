96e3c78d44a04dc83084db8eac5e0c1aed941d9e
Bo Wang <61758179+bowang-666@users.noreply.github.com>
2020-06-05 11:43:15 -0700

    Revert "[CSE] Support GPU driver dedicated VHD"

--- pkg/agent/utils.go
+++ pkg/agent/utils.go
@@ -9,6 +9,8 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"github.com/Azure/agentbaker/pkg/templates"
+	"github.com/blang/semver"
 	"io/ioutil"
 	"log"
 	"net"
@@ -17,9 +19,6 @@ import (
 	"strconv"
 	"strings"
 	"text/template"
-
-	"github.com/Azure/agentbaker/pkg/templates"
-	"github.com/blang/semver"
 
 	"github.com/Azure/aks-engine/pkg/api"
 	"github.com/pkg/errors"
