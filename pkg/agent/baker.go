f57d2a11ff73c09256de6df6967a59be5f8d4191
Bo Wang <61758179+bowang-666@users.noreply.github.com>
2020-07-14 01:05:35 -0700

    Support dynamic kubelet (#139)
    * update to include config file
    
    * translate kubelet flags to config file
    
    * update authetication translation
    
    * fix feature gates
    
    * add systemReserved, kubeReserved and refactor
    
    * make generate
    
    * Add unit test
    
    * fix commands
    
    * Write file in cse and add unit tests
    
    * add dynamic config dir and some formatting
    
    * make generate and update tests
    
    * renaming
    
    * fix test
    
    * update tests
    
    * remove junit.xml
    
    * ignore junit in all directories

--- pkg/agent/baker.go
+++ pkg/agent/baker.go
@@ -19,11 +19,6 @@ import (
 	"github.com/Azure/aks-engine/pkg/i18n"
 )
 
-// Context represents the object that is passed to the package
-type Context struct {
-	Translator *i18n.Translator
-}
-
 // TemplateGenerator represents the object that performs the template generation.
 type TemplateGenerator struct {
 	Translator *i18n.Translator
@@ -232,11 +227,20 @@ func getContainerServiceFuncMap(cs *api.ContainerService, profile *api.AgentPool
 		"GetAgentKubernetesLabelsDeprecated": func(profile *api.AgentPoolProfile, rg string) string {
 			return profile.GetKubernetesLabels(rg, true)
 		},
+		"GetDynamicKubeletConfigFileContent": func() string {
+			if profile.KubernetesConfig == nil {
+				return ""
+			}
+			return getDynamicKubeletConfigFileContent(profile.KubernetesConfig.KubeletConfig)
+		},
+		"IsDynamicKubeletSupported": func() bool {
+			return IsDynamicKubeletSupported(cs)
+		},
 		"GetKubeletConfigKeyVals": func(kc *api.KubernetesConfig) string {
 			if kc == nil {
 				return ""
 			}
-			return kc.GetOrderedKubeletConfigString()
+			return GetOrderedKubeletConfigFlagString(kc, cs)
 		},
 		"GetKubeletConfigKeyValsPsh": func(kc *api.KubernetesConfig) string {
 			if kc == nil {
