41332408b3d916832971d0e230d93b42c8a90a78
Bo Wang <bowa@microsoft.com>
2020-07-14 17:49:19 -0700

    Fix an issue for generator

--- cmd/generate.go
+++ cmd/generate.go
@@ -234,7 +234,7 @@ func (gc *generateCmd) run() error {
 
 	customDataStr := templateGenerator.GetNodeBootstrappingPayload(gc.containerService, gc.containerService.Properties.AgentPoolProfiles[0])
 
-	cseCmdStr := templateGenerator.GetNodeBootstrappingCmd(gc.containerService, gc.containerService.Properties.AgentPoolProfiles[0], "<tenantid>", "<subid>", "rgname", "msiid")
+	cseCmdStr := templateGenerator.GetNodeBootstrappingCmd(gc.containerService, gc.containerService.Properties.AgentPoolProfiles[0], "<tenantid>", "<subid>", "rgname", "msiid", true, false)
 
 	writer := &engine.ArtifactWriter{
 		Translator: &i18n.Translator{
