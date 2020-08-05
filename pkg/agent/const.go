dc3d3201a90afa68cc062ae5dfa26650857c2db1
Bo Wang <61758179+bowang-666@users.noreply.github.com>
2020-08-05 14:57:16 -0700

    Ensure kubelet config flags value (#190)
    * Ensure kubelet config flags value
    
    * update toggle

--- pkg/agent/const.go
+++ pkg/agent/const.go
@@ -199,3 +199,9 @@ const (
 	// AppGwIngressAddonName appgw addon
 	AppGwIngressAddonName = "appgw-ingress"
 )
+
+// kubelet config
+const (
+	// DynamicKubeletConfigDir specifies the directory for dynamic kubelet to store configs
+	DynamicKubeletConfigDir = "/var/lib/kubelet"
+)
