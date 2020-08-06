25cc84d254d72859066196d82bfecade22e921c2
Bo Wang <61758179+bowang-666@users.noreply.github.com>
2020-08-06 12:17:22 -0700

    Fix baker test (#199)
    * Fix baker test
    
    * update pipelines

--- pkg/agent/testdata/AKSUbuntu1604+DynamicKubelet/line43.sh
+++ pkg/agent/testdata/AKSUbuntu1604+DynamicKubelet/line43.sh
@@ -31,7 +31,7 @@ ExecStart=/usr/local/bin/kubelet \
         --node-labels="${KUBELET_NODE_LABELS}" \
         --v=2  \
         --volume-plugin-dir=/etc/kubernetes/volumeplugins \
-        --config /etc/default/kubeletconfig.json --dynamic-config-dir /etc/default/dynamickubelet \
+        --config /etc/default/kubeletconfig.json \
         $KUBELET_FLAGS \
         $KUBELET_REGISTER_NODE $KUBELET_REGISTER_WITH_TAINTS
 
