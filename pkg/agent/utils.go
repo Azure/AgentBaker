8e26d9f0511b546bee08c6e63582531aadd0305f
Yi Zhang <yizhang5@microsoft.com>
2020-08-05 18:23:05 -0700

    Change the function signature to better fix the code merge issue.

--- pkg/agent/utils.go
+++ pkg/agent/utils.go
@@ -826,7 +826,7 @@ func GetOrderedKubeletConfigFlagString(k *api.KubernetesConfig, cs *datamodel.Co
 	if k.KubeletConfig == nil {
 		return ""
 	}
-	ensureKubeletConfigFlagsValue(k.KubeletConfig, datamodel.ToAksEngineContainerService(cs), dynamicKubeletToggleEnabled)
+	ensureKubeletConfigFlagsValue(k.KubeletConfig, cs, dynamicKubeletToggleEnabled)
 	keys := []string{}
 	dynamicKubeletEnabled := IsDynamicKubeletEnabled(cs, dynamicKubeletToggleEnabled)
 	for key := range k.KubeletConfig {
@@ -848,9 +848,9 @@ func IsDynamicKubeletEnabled(cs *datamodel.ContainerService, dynamicKubeletToggl
 	return dynamicKubeletToggleEnabled && cs.Properties.OrchestratorProfile.IsKubernetes() && IsKubernetesVersionGe(cs.Properties.OrchestratorProfile.OrchestratorVersion, "1.14.0")
 }
 
-func ensureKubeletConfigFlagsValue(kc map[string]string, cs *api.ContainerService, dynamicKubeletToggleEnabled bool) {
+func ensureKubeletConfigFlagsValue(kc map[string]string, cs *datamodel.ContainerService, dynamicKubeletToggleEnabled bool) {
 	// for now it's only dynamic kubelet, we could add more in future
-	if IsDynamicKubeletEnabled(datamodel.FromAksEngineContainerService(cs), dynamicKubeletToggleEnabled) && kc["--dynamic-config-dir"] == "" {
+	if IsDynamicKubeletEnabled(cs, dynamicKubeletToggleEnabled) && kc["--dynamic-config-dir"] == "" {
 		kc["--dynamic-config-dir"] = DynamicKubeletConfigDir
 	}
 }
