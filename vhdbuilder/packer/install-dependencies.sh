7dfd39f0f7a3e0e4e40e84fcc885144570b1c8f5
Andy Liu <andyliuliming@outlook.com>
2020-08-19 13:39:45 +0800

    fix

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -375,7 +375,7 @@ done
 # this is the patched images which AKS are using.
 AKS_IP_MASQ_AGENT_VERSIONS="
 2.5.0
-2.5.1
+2.5.0.1
 "
 for IP_MASQ_AGENT_VERSION in ${AKS_IP_MASQ_AGENT_VERSIONS}; do
     CONTAINER_IMAGE="mcr.microsoft.com/oss/kubernetes/ip-masq-agent:v${IP_MASQ_AGENT_VERSION}"
