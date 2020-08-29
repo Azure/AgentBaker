a94951a10e530dfdd1c81f6626f0ec67a2788670
Tony Xu <tonyxu@microsoft.com>
2020-08-28 21:17:43 -0700

    Remove unecessary files

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -410,6 +410,7 @@ done
 # v1.17.9-hotfix.20200824.1
 # v1.18.6-hotfix.20200723.1
 # v1.18.8
+# NOTE that we only keep the latest one per k8s patch version as kubelet/kubectl is decided by VHD version
 K8S_VERSIONS="
 1.14.7-hotfix.20200408.1
 1.14.8-hotfix.20200529.1
@@ -466,15 +467,12 @@ ls -ltr /usr/local/bin/* >> ${VHD_LOGS_FILEPATH}
 # v1.17.9-hotfix.20200824.1
 # v1.18.6-hotfix.20200723.1
 # v1.18.8
+# NOTE that we keep multiple files per k8s patch version as kubeproxy version is decided by CCP
 PATCHED_HYPERKUBE_IMAGES="
-1.15.10-hotfix.20200408.1
 1.15.11-hotfix.20200529.1
-1.15.11-hotfix.20200714.1
-1.15.11-hotfix.20200817.1
 1.15.11-hotfix.20200824.1
 1.15.12-hotfix.20200623.1
 1.15.12-hotfix.20200714.1
-1.15.12-hotfix.20200817.1
 1.15.12-hotfix.20200824.1
 1.16.9-hotfix.20200529.1
 1.16.10-hotfix.20200623.1
@@ -492,7 +490,6 @@ PATCHED_HYPERKUBE_IMAGES="
 1.17.9-hotfix.20200817.1
 1.17.9-hotfix.20200824.1
 1.18.4-hotfix.20200624
-1.18.4-hotfix.20200624.1
 1.18.4-hotfix.20200626.1
 1.18.6-hotfix.20200723.1
 1.18.6
