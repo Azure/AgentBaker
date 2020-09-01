76764ccc101dd2aa1d9e6006bc9ee350f9ed7b73
Yi Zhang <yizhang5@microsoft.com>
2020-09-01 11:55:14 -0700

    Remove aks-engine from "vendor/" and go.mod. (#277)
    * Remove aks-engine from "vendor/" and go.mod.
    I have previously removed all dependencies on aks-engine so we don't need it anymore.
    
    * Add modules depended upon by aks-engine to our go.mod.
    Previously I removed aks-engine from our go.mod file, now I got this build error:
    
    ../../../go/pkg/mod/k8s.io/client-go@v10.0.0+incompatible/rest/request.go:598:31: not enough arguments in call to watch.NewStreamWatcher
    	have (*versioned.Decoder)
    	want (watch.Decoder, watch.Reporter)
    
    It seems aks-engine depends on some older versions of some modules. After removing aks-engine from our go.mod, our code now would try to get newer, incompatible versions of the modules and thus the build break. Adding the depended upon module versions explicitly to the go.mod file fixes the problem. The versions are obtained from aks-engine's go.mod file (https://github.com/Azure/aks-engine/blob/v0.47.0-aks-gomod-a5/go.mod)

--- vendor/github.com/Azure/aks-engine/pkg/armhelpers/const.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-package armhelpers
-
-import "time"
-
-// DefaultARMOperationTimeout defines a default (permissive) ARM operation timeout
-const DefaultARMOperationTimeout = 150 * time.Minute
