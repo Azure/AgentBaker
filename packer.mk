60fca7433ea9c3fe6d3c675184a36f9da794f5bb
Qingchuan Hao <qinhao@microsoft.com>
2020-09-04 08:10:25 +0800

    VHD build: multiple windows version support (#269)
    * VHD: Add multiple windows image versions support
    
    * Add default and sig mode for windows vhd building
    
    * Add missing os-type environment
    
    * fix typo
    
    * Add windows version in publishing info
    
    * Use 'Windows' as offer name for windows SIG case

--- packer.mk
+++ packer.mk
@@ -11,7 +11,13 @@ else
 endif
 
 build-packer-windows:
+ifeq (${MODE},sigMode)
+	@echo "${MODE}: Building with Hyper-v generation 1 VM and save to Shared Image Gallery"
+	@packer build -var-file=vhdbuilder/packer/settings.json vhdbuilder/packer/windows-vhd-builder-sig.json
+else
+	@echo "${MODE}: Building with Hyper-v generation 1 VM and save to Classic Storage Account"
 	@packer build -var-file=vhdbuilder/packer/settings.json vhdbuilder/packer/windows-vhd-builder.json
+endif
 
 init-packer:
 	@./vhdbuilder/packer/init-variables.sh
