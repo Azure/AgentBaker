677a8f6c332224075b2ef632f5c89e5998bf7a4e
Erik Wu <erw@microsoft.com>
2020-09-11 15:49:38 -0700

    oops

--- pkg/agent/baker_test.go
+++ pkg/agent/baker_test.go
@@ -120,7 +120,7 @@ var _ = Describe("Assert generated customData and cseCmd", func() {
 		// customData
 		customData := baker.GetNodeBootstrappingPayload(config)
 		// Uncomment below line to generate test data in local if agentbaker is changed in generating customData
-		backfillCustomData(folder, customData)
+		// backfillCustomData(folder, customData)
 		expectedCustomData, err := ioutil.ReadFile(fmt.Sprintf("./testdata/%s/CustomData", folder))
 		if err != nil {
 			panic(err)
@@ -130,7 +130,7 @@ var _ = Describe("Assert generated customData and cseCmd", func() {
 		// CSE
 		cseCommand := baker.GetNodeBootstrappingCmd(config)
 		// Uncomment below line to generate test data in local if agentbaker is changed in generating customData
-		ioutil.WriteFile(fmt.Sprintf("./testdata/%s/CSECommand", folder), []byte(cseCommand), 0644)
+		// ioutil.WriteFile(fmt.Sprintf("./testdata/%s/CSECommand", folder), []byte(cseCommand), 0644)
 		expectedCSECommand, err := ioutil.ReadFile(fmt.Sprintf("./testdata/%s/CSECommand", folder))
 		if err != nil {
 			panic(err)
