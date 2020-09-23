2dc38f7cb646e010384ca64ea33cf4f51bdaaff2
Erik Wu <erw@microsoft.com>
2020-09-22 17:35:04 -0700

    update ignore settings

--- .pipelines/scripts/verify_shell.sh
+++ .pipelines/scripts/verify_shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-
+#inspired by https://github.com/Azure/aks-engine/blob/master/scripts/validate-shell.sh
 installed=$(which shellcheck 2>&1 >/dev/null)
 if [[ ${installed} -ne 0 ]]; then
     echo "shellcheck not installed...trying to install."
@@ -20,8 +20,25 @@ filesToCheck=$(find . -type f -name "*.sh" -not -path './parts/linux/cloud-init/
 echo "Running shellcheck..."
 
 IGNORED="
+SC1127
+SC1009
+SC1054
+SC1056
+SC1072
+SC1073
+SC1083
+SC1090
+SC1091
 SC2004
 SC2015
 SC2034
+SC2046
+SC2053
+SC2068
+SC2086
+SC2128
+SC2145
+SC2154
+SC2206
 "
 shellcheck $(printf -- "-e %s " $IGNORED) $filesToCheck
