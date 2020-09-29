643d198863cd396f593e038ac07a3fe39cb395d0
Harry He <zhedahht@hotmail.com>
2020-09-28 22:03:35 -0700

    Skip configuring azure0 on non Ubuntu 18.04 (#363)
    * configure azure0 bridge when starting kubelet
    
    configure azure0 bridge when starting kubelet
    
    * Run 'make generate'
    
    * Add generated changes in pkg/agent/testdata
    
    * skip for configuring azure0 for calico
    
    * rerun 'make generate'
    
    * Fix a typo
    
    * Fix error when running 'make generate'
    
    * more changes
    
    * fix parameter issue
    
    * fix empty content
    
    * resolve review comments
    
    * Skip configuring azure0 when it's not Ubuntu 18.04
    
    * exit when it's not Ubuntu 18.04
    
    * generated changes
    
    * Resolve review concerns
    
    Co-authored-by: Harry He <harryh@microsoft.com>

--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -344,11 +344,25 @@ func linuxCloudInitArtifactsCisSh() (*asset, error) {
 
 var _linuxCloudInitArtifactsConfigure_azure0Sh = []byte(`#!/usr/bin/env bash
 
+# It's only necessary to configure azure0 in Ubuntu 18.04
+lsb_release -i | awk -F':' '{print $2}'| awk '{$1=$1; print $1}' | grep -qi "^ubuntu$"
+if [ $? != 0 ]; then
+    echo 'It is not Ubuntu Skip configuring azure0'
+    exit 0
+fi
+
+lsb_release -r | awk -F':' '{print $2}'| awk '{$1=$1; print $1}' | grep -q "^18.04$"
+if [ $? != 0 ]; then
+    echo 'It is not Ubuntu 18.04. Skip configuring azure0'
+    exit 0
+fi
+
 # Check if the azure cni config is there... no need to run this script if not
 # Also don't want to run this when not using azure-cni
 [ ! -f /etc/cni/net.d/10-azure.conflist ] && exit 0
 
 # CNI team mentions that this is not needed for calico network policy to run this script
+echo "Network policy: ${NETWORK_POLICY}"
 if [[ "${NETWORK_POLICY}" == "calico" ]]; then
     exit 0
 fi
@@ -358,8 +372,8 @@ fi
 ip link show azure0 && exit 0
 
 run_plugin() {
-        export CNI_COMMAND=$1
-        cat /etc/cni/net.d/10-azure.conflist | jq '.name as $name | .cniVersion as $version | .plugins[]+= {name: $name, cniVersion: $version} | .plugins[0]' | /opt/cni/bin/azure-vnet
+    export CNI_COMMAND=$1
+    cat /etc/cni/net.d/10-azure.conflist | jq '.name as $name | .cniVersion as $version | .plugins[]+= {name: $name, cniVersion: $version} | .plugins[0]' | /opt/cni/bin/azure-vnet
 }
 
 export CNI_ARGS='K8S_POD_NAMESPACE=default;K8S_POD_NAME=configureAzureCNI'
@@ -372,8 +386,8 @@ ip netns add $(basename ${CNI_NETNS})
 run_plugin ADD
 
 if [ $? -gt 0 ]; then
-        ip netns del "$(basename ${CNI_NETNS})"
-        exit 1
+    ip netns del "$(basename ${CNI_NETNS})"
+    exit 1
 fi
 
 run_plugin DEL
