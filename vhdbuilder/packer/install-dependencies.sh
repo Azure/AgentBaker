92a1a0742fceb04cad012bba58183f3ded54cc52
Paul Miller <pmiller@microsoft.com>
2020-11-20 11:40:13 -0800

    take a another point release of calico as 3.8.9 stil has 3/7 vulnerabilities (#443)

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -345,8 +345,8 @@ done
 
 # calico images used by AKS
 CALICO_CNI_IMAGES="
-v3.8.0
 v3.8.9
+v3.8.9.1
 "
 for CALICO_CNI_IMAGE in ${CALICO_CNI_IMAGES}; do
     CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/cni:${CALICO_CNI_IMAGE}"
@@ -355,8 +355,8 @@ for CALICO_CNI_IMAGE in ${CALICO_CNI_IMAGES}; do
 done
 
 CALICO_NODE_IMAGES="
-v3.8.0
 v3.8.9
+v3.8.9.1
 "
 for CALICO_NODE_IMAGE in ${CALICO_NODE_IMAGES}; do
     CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/node:${CALICO_NODE_IMAGE}"
@@ -365,8 +365,8 @@ for CALICO_NODE_IMAGE in ${CALICO_NODE_IMAGES}; do
 done
 
 CALICO_TYPHA_IMAGES="
-v3.8.0
 v3.8.9
+v3.8.9.1
 "
 for CALICO_TYPHA_IMAGE in ${CALICO_TYPHA_IMAGES}; do
     CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/typha:${CALICO_TYPHA_IMAGE}"
@@ -376,7 +376,7 @@ done
 
 CALICO_POD2DAEMON_IMAGES="
 v3.8.0
-v3.8.9
+v3.8.9.1
 "
 for CALICO_POD2DAEMON_IMAGE in ${CALICO_POD2DAEMON_IMAGES}; do
     CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/pod2daemon-flexvol:${CALICO_POD2DAEMON_IMAGE}"
