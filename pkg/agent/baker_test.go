62f0d754df993509381c5dbeac08cba1d797eacf
Qingchuan Hao <qinhao@microsoft.com>
2020-12-11 22:12:38 +0800

    chore: support Windows CSE and Customdata (#396)
    * support replacing windows CSE
    
    * Add hyper-v and containerd support
    
    * add customcloud back and fix comments
    
    * Removed unused testdata
    
    * Add TODO comment for current less code change
    
    * More UTs and tetdata
    
    * Add more test cases and copy CSECommand from aks-engine
    
    * Update generated code
    
    * fix comments
    
    * Rebase onto the master
    
    * Update CustomVnet testdata
    
    * fix comments
    
    * nil check kubernetesconfig
    
    * Enable ip-masq-agent by default
    
    * Remove used variables
    
    * Split the function returning custom data
    
    * Split test for Linux and Windows
    
    * fix comments
    
    * Remove mastersubnet and modify name of container runtimes
    
    * Update testdata

--- pkg/agent/baker_test.go
+++ pkg/agent/baker_test.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/Azure/agentbaker/pkg/agent/datamodel"
 	"github.com/Azure/go-autorest/autorest/to"
@@ -285,12 +286,278 @@ var _ = Describe("Assert generated customData and cseCmd", func() {
 
 })
 
+var _ = Describe("Assert generated customData and cseCmd for Windows", func() {
+	DescribeTable("Generated customData and CSE", func(folder, k8sVersion string, configUpdator func(*datamodel.NodeBootstrappingConfiguration)) {
+		cs := &datamodel.ContainerService{
+			Location: "southcentralus",
+			Type:     "Microsoft.ContainerService/ManagedClusters",
+			Properties: &datamodel.Properties{
+				OrchestratorProfile: &datamodel.OrchestratorProfile{
+					OrchestratorType:    datamodel.Kubernetes,
+					OrchestratorVersion: k8sVersion,
+					KubernetesConfig: &datamodel.KubernetesConfig{
+						ContainerRuntime:     "docker",
+						KubernetesImageBase:  "mcr.microsoft.com/oss/kubernetes/",
+						WindowsContainerdURL: "https://k8swin.blob.core.windows.net/k8s-windows/containerd/containerplat-aks-test-0.0.8.zip",
+						LoadBalancerSku:      "Standard",
+						CustomHyperkubeImage: "mcr.microsoft.com/oss/kubernetes/hyperkube:v1.16.15-hotfix.20200903",
+						ClusterSubnet:        "10.240.0.0/16",
+						NetworkPlugin:        "azure",
+						DockerBridgeSubnet:   "172.17.0.1/16",
+						ServiceCIDR:          "10.0.0.0/16",
+						EnableRbac:           to.BoolPtr(true),
+						EnableSecureKubelet:  to.BoolPtr(true),
+						KubeletConfig: map[string]string{
+							"--feature-gates": "RotateKubeletServerCertificate=true,a=b, PodPriority=true, x=y",
+						},
+						UseInstanceMetadata: to.BoolPtr(true),
+						DNSServiceIP:        "10.0.0.10",
+					},
+				},
+				HostedMasterProfile: &datamodel.HostedMasterProfile{
+					DNSPrefix:   "uttestdom",
+					FQDN:        "uttestdom-dns-5d7c849e.hcp.southcentralus.azmk8s.io",
+					Subnet:      "10.240.0.0/16",
+					IPMasqAgent: true,
+				},
+				AgentPoolProfiles: []*datamodel.AgentPoolProfile{
+					{
+						Name:                "wpool2",
+						Count:               3,
+						VMSize:              "Standard_D2s_v3",
+						StorageProfile:      "ManagedDisks",
+						OSType:              datamodel.Windows,
+						VnetSubnetID:        "/subscriptions/359833f5/resourceGroups/MC_rg/providers/Microsoft.Network/virtualNetworks/aks-vnet-36873793/subnet/aks-subnet",
+						WindowsNameVersion:  "v2",
+						AvailabilityProfile: datamodel.VirtualMachineScaleSets,
+						CustomNodeLabels:    map[string]string{"kubernetes.azure.com/node-image-version": "AKSWindows-2019-17763.1577.201111"},
+						KubernetesConfig: &datamodel.KubernetesConfig{
+							KubeletConfig: map[string]string{
+								"--address":                           "0.0.0.0",
+								"--anonymous-auth":                    "false",
+								"--authentication-token-webhook":      "true",
+								"--authorization-mode":                "Webhook",
+								"--cloud-config":                      "c:\\k\\azure.json",
+								"--cgroups-per-qos":                   "false",
+								"--client-ca-file":                    "c:\\k\\ca.crt",
+								"--azure-container-registry-config":   "c:\\k\\azure.json",
+								"--cloud-provider":                    "azure",
+								"--cluster-dns":                       "10.0.0.10",
+								"--cluster-domain":                    "cluster.local",
+								"--enforce-node-allocatable":          "",
+								"--event-qps":                         "0",
+								"--eviction-hard":                     "",
+								"--feature-gates":                     "RotateKubeletServerCertificate=true",
+								"--hairpin-mode":                      "promiscuous-bridge",
+								"--image-gc-high-threshold":           "85",
+								"--image-gc-low-threshold":            "80",
+								"--image-pull-progress-deadline":      "20m",
+								"--keep-terminated-pod-volumes":       "false",
+								"--kube-reserved":                     "cpu=100m,memory=1843Mi",
+								"--kubeconfig":                        "c:\\k\\config",
+								"--max-pods":                          "30",
+								"--network-plugin":                    "cni",
+								"--node-status-update-frequency":      "10s",
+								"--non-masquerade-cidr":               "0.0.0.0/0",
+								"--pod-infra-container-image":         "kubletwin/pause",
+								"--pod-max-pids":                      "-1",
+								"--read-only-port":                    "0",
+								"--resolv-conf":                       `""`,
+								"--rotate-certificates":               "false",
+								"--streaming-connection-idle-timeout": "4h",
+								"--system-reserved":                   "memory=2Gi",
+								"--tls-cipher-suites":                 "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_RSA_WITH_AES_256_GCM_SHA384,TLS_RSA_WITH_AES_128_GCM_SHA256",
+							},
+						},
+						Distro: datamodel.Distro("aks-windows-2019"),
+					},
+				},
+				LinuxProfile: &datamodel.LinuxProfile{
+					AdminUsername: "azureuser",
+				},
+				WindowsProfile: &datamodel.WindowsProfile{
+					ProvisioningScriptsPackageURL: "https://acs-mirror.azureedge.net/aks-engine/windows/provisioning/signedscripts-v0.0.4.zip",
+					WindowsPauseImageURL:          "mcr.microsoft.com/oss/kubernetes/pause:1.4.0",
+					AdminUsername:                 "azureuser",
+					AdminPassword:                 "replacepassword1234",
+					WindowsPublisher:              "microsoft-aks",
+					WindowsOffer:                  "aks-windows",
+					ImageVersion:                  "17763.1577.201111",
+					WindowsSku:                    "aks-2019-datacenter-core-smalldisk-2011",
+				},
+				ServicePrincipalProfile: &datamodel.ServicePrincipalProfile{
+					ClientID: "ClientID",
+					Secret:   "Secret",
+				},
+			},
+		}
+		cs.Properties.LinuxProfile.SSH.PublicKeys = []datamodel.PublicKey{{
+			KeyData: string("testsshkey"),
+		}}
+
+		// AKS always pass in te customHyperKubeImage to aks-e, so we don't really rely on
+		// the default component version for "hyperkube", which is not set since 1.17
+		if IsKubernetesVersionGe(k8sVersion, "1.17.0") {
+			cs.Properties.OrchestratorProfile.KubernetesConfig.CustomHyperkubeImage = fmt.Sprintf("k8s.gcr.io/hyperkube-amd64:v%v", k8sVersion)
+		}
+
+		agentPool := cs.Properties.AgentPoolProfiles[0]
+		baker := InitializeTemplateGenerator()
+
+		fullK8sComponentsMap := K8sComponentsByVersionMap[cs.Properties.OrchestratorProfile.OrchestratorVersion]
+		pauseImage := cs.Properties.OrchestratorProfile.KubernetesConfig.MCRKubernetesImageBase + fullK8sComponentsMap["pause"]
+
+		hyperkubeImageBase := cs.Properties.OrchestratorProfile.KubernetesConfig.KubernetesImageBase
+		hyperkubeImage := hyperkubeImageBase + fullK8sComponentsMap["hyperkube"]
+		if cs.Properties.OrchestratorProfile.KubernetesConfig.CustomHyperkubeImage != "" {
+			hyperkubeImage = cs.Properties.OrchestratorProfile.KubernetesConfig.CustomHyperkubeImage
+		}
+
+		windowsPackage := datamodel.AzurePublicCloudSpecForTest.KubernetesSpecConfig.KubeBinariesSASURLBase + fullK8sComponentsMap["windowszip"]
+		k8sComponents := &datamodel.K8sComponents{
+			PodInfraContainerImageURL: pauseImage,
+			HyperkubeImageURL:         hyperkubeImage,
+			WindowsPackageURL:         windowsPackage,
+		}
+
+		config := &datamodel.NodeBootstrappingConfiguration{
+			ContainerService:              cs,
+			CloudSpecConfig:               datamodel.AzurePublicCloudSpecForTest,
+			K8sComponents:                 k8sComponents,
+			AgentPoolProfile:              agentPool,
+			TenantID:                      "tenantID",
+			SubscriptionID:                "subID",
+			ResourceGroupName:             "resourceGroupName",
+			UserAssignedIdentityClientID:  "userAssignedID",
+			ConfigGPUDriverIfNeeded:       true,
+			EnableGPUDevicePluginIfNeeded: false,
+			EnableKubeletConfigFile:       false,
+			EnableNvidia:                  false,
+		}
+
+		if configUpdator != nil {
+			configUpdator(config)
+		}
+
+		// customData
+		base64EncodedCustomData := baker.GetNodeBootstrappingPayload(config)
+		customDataBytes, err := base64.StdEncoding.DecodeString(base64EncodedCustomData)
+		customData := string(customDataBytes)
+		Expect(err).To(BeNil())
+
+		if generateTestData() {
+			backfillCustomData(folder, customData)
+		}
+
+		expectedCustomData, err := ioutil.ReadFile(fmt.Sprintf("./testdata/%s/CustomData", folder))
+		if err != nil {
+			panic(err)
+		}
+		Expect(customData).To(Equal(string(expectedCustomData)))
+
+		// CSE
+		cseCommand := baker.GetNodeBootstrappingCmd(config)
+		if generateTestData() {
+			ioutil.WriteFile(fmt.Sprintf("./testdata/%s/CSECommand", folder), []byte(cseCommand), 0644)
+		}
+		expectedCSECommand, err := ioutil.ReadFile(fmt.Sprintf("./testdata/%s/CSECommand", folder))
+		if err != nil {
+			panic(err)
+		}
+		Expect(cseCommand).To(Equal(string(expectedCSECommand)))
+
+	}, Entry("AKSWindows2019 with k8s version 1.16", "AKSWindows2019+K8S116", "1.16.15", func(config *datamodel.NodeBootstrappingConfiguration) {
+	}),
+		Entry("AKSWindows2019 with k8s version 1.17", "AKSWindows2019+K8S117", "1.17.7", func(config *datamodel.NodeBootstrappingConfiguration) {
+		}),
+		Entry("AKSWindows2019 with k8s version 1.18", "AKSWindows2019+K8S118", "1.18.2", func(config *datamodel.NodeBootstrappingConfiguration) {
+		}),
+		Entry("AKSWindows2019 with k8s version 1.19", "AKSWindows2019+K8S119", "1.19.0", func(config *datamodel.NodeBootstrappingConfiguration) {
+		}),
+		Entry("AKSWindows2019 with k8s version 1.19 + CSI", "AKSWindows2019+K8S119+CSI", "1.19.0", func(config *datamodel.NodeBootstrappingConfiguration) {
+			config.ContainerService.Properties.WindowsProfile.CSIProxyURL = "https://acs-mirror.azureedge.net/csi-proxy/v0.1.0/binaries/csi-proxy.tar.gz"
+			config.ContainerService.Properties.WindowsProfile.EnableCSIProxy = to.BoolPtr(true)
+		}),
+		Entry("AKSWindows2019 with CustomVnet", "AKSWindows2019+CustomVnet", "1.19.0", func(config *datamodel.NodeBootstrappingConfiguration) {
+			config.ContainerService.Properties.OrchestratorProfile.KubernetesConfig.ClusterSubnet = "172.17.0.0/24"
+			config.ContainerService.Properties.OrchestratorProfile.KubernetesConfig.ServiceCIDR = "172.17.255.0/24"
+			config.ContainerService.Properties.AgentPoolProfiles[0].VnetCidrs = []string{"172.17.0.0/16"}
+			config.ContainerService.Properties.AgentPoolProfiles[0].Subnet = "172.17.2.0/24"
+			config.ContainerService.Properties.AgentPoolProfiles[0].VnetSubnetID = "/subscriptions/359833f5/resourceGroups/MC_rg/providers/Microsoft.Network/virtualNetworks/aks-vnet-07752737/subnet/subnet2"
+			config.ContainerService.Properties.AgentPoolProfiles[0].KubernetesConfig.KubeletConfig["--cluster-dns"] = "172.17.255.10"
+		}),
+		Entry("AKSWindows2019 with Managed Identity", "AKSWindows2019+ManagedIdentity", "1.19.0", func(config *datamodel.NodeBootstrappingConfiguration) {
+			config.ContainerService.Properties.ServicePrincipalProfile = &datamodel.ServicePrincipalProfile{ClientID: "msi"}
+			config.ContainerService.Properties.OrchestratorProfile.KubernetesConfig.UseManagedIdentity = true
+			config.ContainerService.Properties.OrchestratorProfile.KubernetesConfig.UserAssignedID = "/subscriptions/359833f5/resourceGroups/MC_rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/k8s-agentpool"
+		}),
+		Entry("AKSWindows2019 with custom cloud", "AKSWindows2019+CustomCloud", "1.19.0", func(config *datamodel.NodeBootstrappingConfiguration) {
+			config.ContainerService.Properties.WindowsProfile.AlwaysPullWindowsPauseImage = to.BoolPtr(true)
+			config.ContainerService.Properties.CustomCloudEnv = &datamodel.CustomCloudEnv{
+				Name:                         "akscustom",
+				McrURL:                       "mcr.microsoft.fakecustomcloud",
+				RepoDepotEndpoint:            "https://repodepot.azure.microsoft.fakecustomcloud/ubuntu",
+				ManagementPortalURL:          "https://portal.azure.microsoft.fakecustomcloud/",
+				PublishSettingsURL:           "",
+				ServiceManagementEndpoint:    "https://management.core.microsoft.fakecustomcloud/",
+				ResourceManagerEndpoint:      "https://management.azure.microsoft.fakecustomcloud/",
+				ActiveDirectoryEndpoint:      "https://login.microsoftonline.microsoft.fakecustomcloud/",
+				GalleryEndpoint:              "",
+				KeyVaultEndpoint:             "https://vault.cloudapi.microsoft.fakecustomcloud/",
+				GraphEndpoint:                "https://graph.cloudapi.microsoft.fakecustomcloud/",
+				ServiceBusEndpoint:           "",
+				BatchManagementEndpoint:      "",
+				StorageEndpointSuffix:        "core.microsoft.fakecustomcloud",
+				SQLDatabaseDNSSuffix:         "database.cloudapi.microsoft.fakecustomcloud",
+				TrafficManagerDNSSuffix:      "",
+				KeyVaultDNSSuffix:            "vault.cloudapi.microsoft.fakecustomcloud",
+				ServiceBusEndpointSuffix:     "",
+				ServiceManagementVMDNSSuffix: "",
+				ResourceManagerVMDNSSuffix:   "cloudapp.azure.microsoft.fakecustomcloud/",
+				ContainerRegistryDNSSuffix:   ".azurecr.microsoft.fakecustomcloud",
+				CosmosDBDNSSuffix:            "documents.core.microsoft.fakecustomcloud/",
+				TokenAudience:                "https://management.core.microsoft.fakecustomcloud/",
+				ResourceIdentifiers: datamodel.ResourceIdentifiers{
+					Graph:               "",
+					KeyVault:            "",
+					Datalake:            "",
+					Batch:               "",
+					OperationalInsights: "",
+					Storage:             "",
+				},
+			}
+		}),
+		Entry("AKSWindows2019 EnablePrivateClusterHostsConfigAgent", "AKSWindows2019+EnablePrivateClusterHostsConfigAgent", "1.19.0", func(config *datamodel.NodeBootstrappingConfiguration) {
+			cs := config.ContainerService
+			if cs.Properties.OrchestratorProfile.KubernetesConfig.PrivateCluster == nil {
+				cs.Properties.OrchestratorProfile.KubernetesConfig.PrivateCluster = &datamodel.PrivateCluster{EnableHostsConfigAgent: to.BoolPtr(true)}
+			} else {
+				cs.Properties.OrchestratorProfile.KubernetesConfig.PrivateCluster.EnableHostsConfigAgent = to.BoolPtr(true)
+			}
+		}),
+		Entry("AKSWindows2004 with k8s version 1.19 and hyperv", "AKSWindows2004+K8S119+hyperv", "1.19.0", func(config *datamodel.NodeBootstrappingConfiguration) {
+			config.ContainerService.Properties.AgentPoolProfiles[0].Distro = datamodel.Distro("aks-windows-2019")
+			config.ContainerService.Properties.AgentPoolProfiles[0].CustomNodeLabels = map[string]string{"kubernetes.azure.com/node-image-version": "AKSWindows-2004-17763.1457.201019"}
+			config.ContainerService.Properties.AgentPoolProfiles[0].ImageRef = &datamodel.ImageReference{
+				Name:           "windows-2004",
+				ResourceGroup:  "akswinvhdbuilderrg",
+				SubscriptionID: "109a5e88-712a-48ae-9078-9ca8b3c81345",
+				Gallery:        "AKSWindows",
+				Version:        "17763.1457.201019",
+			}
+		}))
+
+})
+
 func backfillCustomData(folder, customData string) {
 	if _, err := os.Stat(fmt.Sprintf("./testdata/%s", folder)); os.IsNotExist(err) {
 		e := os.MkdirAll(fmt.Sprintf("./testdata/%s", folder), 0755)
 		Expect(e).To(BeNil())
 	}
 	ioutil.WriteFile(fmt.Sprintf("./testdata/%s/CustomData", folder), []byte(customData), 0644)
+	if strings.Contains(folder, "AKSWindows") {
+		return
+	}
 	err := exec.Command("/bin/sh", "-c", fmt.Sprintf("./testdata/convert.sh testdata/%s", folder)).Run()
 	Expect(err).To(BeNil())
 }
