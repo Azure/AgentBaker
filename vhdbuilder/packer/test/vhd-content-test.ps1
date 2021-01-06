9e05bcdde868ee93888fe4dc568a5af47da458e4
Chou Hu <abelch@microsoft.com>
2021-01-06 11:58:05 +0800

    chore: Use signed scripts v0.0.8 for Windows deployments (#511)

--- vhdbuilder/packer/test/vhd-content-test.ps1
+++ vhdbuilder/packer/test/vhd-content-test.ps1
@@ -49,9 +49,9 @@ function Test-FilesToCacheOnVHD
             "https://github.com/microsoft/SDN/raw/master/Kubernetes/windows/helper.psm1",
             "https://github.com/Microsoft/SDN/raw/master/Kubernetes/windows/hns.psm1",
             "https://globalcdn.nuget.org/packages/microsoft.applicationinsights.2.11.0.nupkg",
-            "https://acs-mirror.azureedge.net/aks-engine/windows/provisioning/signedscripts-v0.0.2.zip",
             "https://acs-mirror.azureedge.net/aks-engine/windows/provisioning/signedscripts-v0.0.3.zip",
-            "https://acs-mirror.azureedge.net/aks-engine/windows/provisioning/signedscripts-v0.0.4.zip"
+            "https://acs-mirror.azureedge.net/aks-engine/windows/provisioning/signedscripts-v0.0.4.zip",
+            "https://acs-mirror.azureedge.net/aks-engine/windows/provisioning/signedscripts-v0.0.8.zip"
         );
         "c:\akse-cache\containerd\" = @(
             $global:containerdPackageUrl
