42d940fb42124f1b01aea57e7e099fd385a7911b
eriksywu <erw@microsoft.com>
2021-01-26 17:11:30 -0800

    disable ipmasq for kubenet if using ipmasqagent (#537)

--- pkg/agent/baker_test.go
+++ pkg/agent/baker_test.go
@@ -263,6 +263,17 @@ var _ = Describe("Assert generated customData and cseCmd", func() {
 			config.ContainerService.Properties.OrchestratorProfile.KubernetesConfig.NetworkPlugin = NetworkPluginKubenet
 		}),
 
+		Entry("AKSUbuntu1804 with containerd and ipmasqagent enabled", "AKSUbuntu1804+Containerd+IPMasqAgent", "1.18.2", func(config *datamodel.NodeBootstrappingConfiguration) {
+			config.EnableACRTeleportPlugin = true
+			config.TeleportdPluginURL = "some url"
+			config.ContainerService.Properties.AgentPoolProfiles[0].KubernetesConfig = &datamodel.KubernetesConfig{
+				KubeletConfig:    map[string]string{},
+				ContainerRuntime: datamodel.Containerd,
+			}
+			config.ContainerService.Properties.OrchestratorProfile.KubernetesConfig.NetworkPlugin = NetworkPluginKubenet
+			config.ContainerService.Properties.HostedMasterProfile.IPMasqAgent = true
+		}),
+
 		Entry("AKSUbuntu1604 with custom kubeletConfig and osConfig", "AKSUbuntu1604+CustomKubeletConfig+CustomLinuxOSConfig", "1.16.13", func(config *datamodel.NodeBootstrappingConfiguration) {
 			config.EnableKubeletConfigFile = false
 			netIpv4TcpTwReuse := true
