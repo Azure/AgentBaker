87ee618374c326406fb98edccd6ec056c73939c4
Charlie McBride <Charlie.McBride@microsoft.com>
2021-01-29 11:32:57 -0800

    updating to get indead of writing each time

--- parts/linux/cloud-init/artifacts/labels.sh
+++ parts/linux/cloud-init/artifacts/labels.sh
@@ -8,9 +8,17 @@ NODE_NAME=$(echo $(hostname) | tr "[:upper:]" "[:lower:]")
 
 echo "updating labels for ${NODE_NAME}"
 
-for kubelet_label in $(echo $KUBELET_NODE_LABELS | sed "s/,/ /g")
-do
-  echo "updating label: ${kubelet_label}"
-  kubectl label --kubeconfig /var/lib/kubelet/kubeconfig --overwrite nodes $NODE_NAME $kubelet_label
-done
+CURRENT_NODE_LABELS=$(kubectl label --list=true $NODE_NAME)
+
+echo "current node labels: ${CURRENT_NODE_LABELS}"
+
+FORMATTED_NODE_LABELS_TO_ENSURE=$(echo $KUBELET_NODE_LABELS | sed "s/,/ /g")
+
+echo "formatted node labels to ensure: ${FORMATTED_NODE_LABELS_TO_ENSURE}"
+
+MISSING_LABELS=$(comm -32 <(printf $FORMATTED_KUBELET_NODE_LABELS) <(printf $CURRENT_NODE_LABELS))
+
+echo "missing labels: ${MISSING_LABELS}"
+
+kubectl label --kubeconfig /var/lib/kubelet/kubeconfig --overwrite nodes $NODE_NAME $MISSING_LABELS
 #EOF
