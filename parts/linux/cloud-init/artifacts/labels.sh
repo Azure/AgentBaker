24bfce3077fc9d107efeb26a9f8ef7388097787f
Charlie McBride <Charlie.McBride@microsoft.com>
2021-01-29 10:59:04 -0800

    updating to lowercase hostname

--- parts/linux/cloud-init/artifacts/labels.sh
+++ parts/linux/cloud-init/artifacts/labels.sh
@@ -4,11 +4,13 @@
 
 set -euo pipefail
 
-echo "updating labels for ${HOSTNAME}"
+NODE_NAME=$(echo $(hostname) | tr "[:upper:]" "[:lower:]")
+
+echo "updating labels for ${NODE_NAME}"
 
 for kubelet_label in $(echo $KUBELET_NODE_LABELS | sed "s/,/ /g")
 do
   echo "updating label: ${kubelet_label}"
-  kubectl label --kubeconfig /var/lib/kubelet/kubeconfig --overwrite nodes $HOSTNAME $kubelet_label
+  kubectl label --kubeconfig /var/lib/kubelet/kubeconfig --overwrite nodes $NODE_NAME $kubelet_label
 done
 #EOF
