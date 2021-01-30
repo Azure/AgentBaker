36b5ab87930d17a5f938e32c3b22eaf5be7740f2
Charlie McBride <Charlie.McBride@microsoft.com>
2021-01-29 18:28:42 -0800

    fixing issues after testing

--- parts/linux/cloud-init/artifacts/labels.sh
+++ parts/linux/cloud-init/artifacts/labels.sh
@@ -8,17 +8,19 @@ NODE_NAME=$(echo $(hostname) | tr "[:upper:]" "[:lower:]")
 
 echo "updating labels for ${NODE_NAME}"
 
-CURRENT_NODE_LABELS=$(kubectl label --list=true $NODE_NAME)
+FORMATTED_CURRENT_NODE_LABELS=$(kubectl label --kubeconfig /var/lib/kubelet/kubeconfig --list=true node $NODE_NAME | sort)
 
-echo "current node labels: ${CURRENT_NODE_LABELS}"
+echo "current node labels (sorted): ${FORMATTED_CURRENT_NODE_LABELS}"
 
-FORMATTED_NODE_LABELS_TO_ENSURE=$(echo $KUBELET_NODE_LABELS | sed "s/,/ /g")
+FORMATTED_NODE_LABELS_TO_ENSURE=$(echo $KUBELET_NODE_LABELS | tr ',' '\n' | sort)
 
-echo "formatted node labels to ensure: ${FORMATTED_NODE_LABELS_TO_ENSURE}"
+echo "node labels to ensure (formatted+sorted): ${FORMATTED_NODE_LABELS_TO_ENSURE}"
 
-MISSING_LABELS=$(comm -32 <(printf $FORMATTED_KUBELET_NODE_LABELS) <(printf $CURRENT_NODE_LABELS))
+MISSING_LABELS=$(comm -32 <(echo $FORMATTED_NODE_LABELS_TO_ENSURE | tr ' ' '\n') <(echo $FORMATTED_CURRENT_NODE_LABELS | tr ' ' '\n'))
 
 echo "missing labels: ${MISSING_LABELS}"
 
-kubectl label --kubeconfig /var/lib/kubelet/kubeconfig --overwrite nodes $NODE_NAME $MISSING_LABELS
+if [ ! -z "$MISSING_LABELS" ]; then
+  kubectl label --kubeconfig /var/lib/kubelet/kubeconfig --overwrite node $NODE_NAME $MISSING_LABELS
+fi
 #EOF
