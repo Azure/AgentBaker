faa3505e1d06df06e7c85c9dba3cd7cf91b3dfe4
Adil Adilli <adadilli@microsoft.com>
2021-02-04 22:32:32 -0800

    change error command

--- vhdbuilder/packer/test/run-test.sh
+++ vhdbuilder/packer/test/run-test.sh
@@ -78,8 +78,7 @@ if [ "$OS_TYPE" == "Linux" ]; then
       --name $VM_NAME \
       --resource-group $RESOURCE_GROUP_NAME \
       --scripts @$SCRIPT_PATH \
-      --parameters ${CONTAINER_RUNTIME})
-    [[ ! $ret =~ "has not reported status for VM agent or extensions" ]] && break
+      --parameters ${CONTAINER_RUNTIME}) && break
     echo "${i}: retrying az vm run-command"
   done
   # The error message for a Linux VM run-command is as follows:
