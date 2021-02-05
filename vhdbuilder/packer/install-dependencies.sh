0399d5ad41db693bb938433da893e0ffadc660a7
Paul Miller <pmiller@microsoft.com>
2021-02-05 11:08:24 -0800

    Bring in 3.8.9.2 and remove 3.8.9.1 for images we can't patch (#510)
    * remove 3.8.9.1 for images where they don
    t exit
    
    * Yet another cve

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -353,8 +353,8 @@ done
 
 # calico images used by AKS
 CALICO_CNI_IMAGES="
-v3.8.9
 v3.8.9.1
+v3.8.9.2
 "
 for CALICO_CNI_IMAGE in ${CALICO_CNI_IMAGES}; do
     CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/cni:${CALICO_CNI_IMAGE}"
@@ -363,8 +363,8 @@ for CALICO_CNI_IMAGE in ${CALICO_CNI_IMAGES}; do
 done
 
 CALICO_NODE_IMAGES="
-v3.8.9
 v3.8.9.1
+v3.8.9.2
 "
 for CALICO_NODE_IMAGE in ${CALICO_NODE_IMAGES}; do
     CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/node:${CALICO_NODE_IMAGE}"
@@ -372,9 +372,9 @@ for CALICO_NODE_IMAGE in ${CALICO_NODE_IMAGES}; do
     echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
 done
 
+# typha and pod2daemon can't be patched like cni and node can as they use scratch as a base
 CALICO_TYPHA_IMAGES="
 v3.8.9
-v3.8.9.1
 "
 for CALICO_TYPHA_IMAGE in ${CALICO_TYPHA_IMAGES}; do
     CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/typha:${CALICO_TYPHA_IMAGE}"
@@ -384,7 +384,6 @@ done
 
 CALICO_POD2DAEMON_IMAGES="
 v3.8.9
-v3.8.9.1
 "
 for CALICO_POD2DAEMON_IMAGE in ${CALICO_POD2DAEMON_IMAGES}; do
     CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/pod2daemon-flexvol:${CALICO_POD2DAEMON_IMAGE}"
