e2f155cd0c4eb9591045161cc779dd2b31f70f52
Charlie McBride <Charlie.McBride@microsoft.com>
2021-02-08 17:12:20 -0800

    Revert "make generate"
    This reverts commit 764a35efd9e8d21c9c8d474ddd495b820216bbbb.

--- pkg/agent/testdata/AKSUbuntu1604+OSKubeletDisk/line57.sh
+++
@@ -1,9 +0,0 @@
-[Unit]
-Description=Updates Labels for Kubernetes node
-After=kubelet.service
-[Service]
-Restart=on-failure
-RestartSec=30
-EnvironmentFile=/etc/default/kubelet
-ExecStart=/bin/bash /opt/azure/containers/update-node-labels.sh
-#EOF
