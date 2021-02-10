16ef9d6bcea6b7d580d2d461b3e124ff845170cc
Adil Adilli <adadilli@microsoft.com>
2021-02-10 10:11:49 -0800

    get id act on id

--- vhdbuilder/packer/cleanup.sh
+++ vhdbuilder/packer/cleanup.sh
@@ -18,21 +18,21 @@ for v in "${required_env_vars[@]}"; do
 done
 
 #clean up the temporary storage account
-id=$(az storage account show -n ${SA_NAME} -g ${AZURE_RESOURCE_GROUP_NAME})
+id=$(az storage account show -n ${SA_NAME} -g ${AZURE_RESOURCE_GROUP_NAME} | jq .id)
 if [ ! -z "$id" ]; then
   az storage account delete -n ${SA_NAME} -g ${AZURE_RESOURCE_GROUP_NAME} --yes
 fi
 
 #clean up managed image
 if [[ "$MODE" != 'default' ]]; then
-  id=$(az image show -n ${IMAGE_NAME} -g ${AZURE_RESOURCE_GROUP_NAME})
+  id=$(az image show -n ${IMAGE_NAME} -g ${AZURE_RESOURCE_GROUP_NAME} | jq .id)
   if [ ! -z "$id" ]; then
     az image delete -n ${IMAGE_NAME} -g ${AZURE_RESOURCE_GROUP_NAME}
   fi
 fi
 
 #clean up the packer generated resource group
-id=$(az group show --name ${PKR_RG_NAME})
+id=$(az group show --name ${PKR_RG_NAME} | jq .id)
 if [ ! -z "$id" ]; then
   echo "Deleting packer resource group ${PKR_RG_NAME}"
   az group delete --name ${PKR_RG_NAME} --yes
