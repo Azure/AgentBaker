4ca978a52792b0454f526c813089c53503b8f6bd
Adil Adilli <adilliadil14@gmail.com>
2021-02-22 13:41:41 -0800

    Test VHD content for Linux (#524)
    * add tests for image pulled and files saved
    
    * correct the variables
    
    * read images from file
    
    * add tenant id
    
    * add remove build prefixes
    
    * test script calling logic
    
    * test script calling logic
    
    * add debug
    
    * just run the test
    
    * without json
    
    * add debugging
    
    * restore to non test
    
    * adjust the parameters to linux path
    
    * add hardcoded os disk uri temporarily
    
    * print directory instead of a file
    
    * sleep for 2
    
    * sleep for 50mins
    
    * sleep for 5mins
    
    * run end to end
    
    * fix errors + add logging
    
    * add more logging, don't exit prematurely
    
    * add more logging, don't exit prematurely
    
    * decrease wait time
    
    * better logging, fix variable referencing
    
    * make the json as variable
    put the string_replace function inside test file
    
    * read json objects flatten them
    
    * read json objects flatten them
    
    * test sad case
    
    * check whether json is read
    
    * add bpftrace
    
    * change versioning
    
    * change error handling for linux
    
    * fix missing file
    
    * change error messaging
    
    * choose between docker and containerd
    
    * add more logging
    
    * list images correctly containerd
    
    * remove extra debug information
    
    * change location to test
    
    * add back old location
    
    * check for file sizes
    
    * print downlaod url
    
    * just test files downloaded
    
    * correct file_name
    
    * remove carriage return
    
    * remove extra logging
    
    * add cse execution time
    
    * remove cse execution time
    
    * add retry clause to az run command
    
    * change error command
    
    * change error command
    
    * merge
    
    * fix veriable naming
    
    * remove failing deps for now.
    
    Co-authored-by: Adil Adilli <adadilli@microsoft.com>

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -328,10 +328,10 @@ for TUNNELFRONT_VERSION in ${TUNNELFRONT_VERSIONS}; do
 done
 
 KONNECTIVITY_AGENT_VERSIONS="
-v0.0.13
+0.0.13
 "
 for KONNECTIVITY_AGENT_VERSION in ${KONNECTIVITY_AGENT_VERSIONS}; do
-    CONTAINER_IMAGE="mcr.microsoft.com/oss/kubernetes/apiserver-network-proxy/agent:${KONNECTIVITY_AGENT_VERSION}"
+    CONTAINER_IMAGE="mcr.microsoft.com/oss/kubernetes/apiserver-network-proxy/agent:v${KONNECTIVITY_AGENT_VERSION}"
     pullContainerImage ${cliTool} ${CONTAINER_IMAGE}
     echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
 done
@@ -367,60 +367,60 @@ done
 
 # calico images used by AKS
 CALICO_CNI_IMAGES="
-v3.8.9.1
-v3.8.9.2
+3.8.9.1
+3.8.9.2
 "
 for CALICO_CNI_IMAGE in ${CALICO_CNI_IMAGES}; do
-    CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/cni:${CALICO_CNI_IMAGE}"
+    CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/cni:v${CALICO_CNI_IMAGE}"
     pullContainerImage ${cliTool} ${CONTAINER_IMAGE}
     echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
 done
 
 CALICO_NODE_IMAGES="
-v3.17.2
-v3.8.9.1
-v3.8.9.2
+3.17.2
+3.8.9.1
+3.8.9.2
 "
 for CALICO_NODE_IMAGE in ${CALICO_NODE_IMAGES}; do
-    CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/node:${CALICO_NODE_IMAGE}"
+    CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/node:v${CALICO_NODE_IMAGE}"
     pullContainerImage ${cliTool} ${CONTAINER_IMAGE}
     echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
 done
 
 # typha and pod2daemon can't be patched like cni and node can as they use scratch as a base
 CALICO_TYPHA_IMAGES="
-v3.17.2
-v3.8.9
+3.17.2
+3.8.9
 "
 for CALICO_TYPHA_IMAGE in ${CALICO_TYPHA_IMAGES}; do
-    CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/typha:${CALICO_TYPHA_IMAGE}"
+    CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/typha:v${CALICO_TYPHA_IMAGE}"
     pullContainerImage ${cliTool} ${CONTAINER_IMAGE}
     echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
 done
 
 CALICO_KUBE_CONTROLLERS_IMAGES="
-v3.17.2
+3.17.2
 "
 for CALICO_KUBE_CONTROLLERS_IMAGE in ${CALICO_KUBE_CONTROLLERS_IMAGES}; do
-    CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/kube-controllers:${CALICO_KUBE_CONTROLLERS_IMAGE}"
+    CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/kube-controllers:v${CALICO_KUBE_CONTROLLERS_IMAGE}"
     pullContainerImage ${cliTool} ${CONTAINER_IMAGE}
     echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
 done
 
 TIGERA_OPERATOR_IMAGES="
-v1.13.5
+1.13.5
 "
 for TIGERA_OPERATOR_IMAGE in ${TIGERA_OPERATOR_IMAGES}; do
-    CONTAINER_IMAGE="mcr.microsoft.com/oss/tigera/kube-controllers:${TIGERA_OPERATOR_IMAGE}"
+    CONTAINER_IMAGE="mcr.microsoft.com/oss/tigera/kube-controllers:v${TIGERA_OPERATOR_IMAGE}"
     pullContainerImage ${cliTool} ${CONTAINER_IMAGE}
     echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
 done
 
 CALICO_POD2DAEMON_IMAGES="
-v3.8.9
+3.8.9
 "
 for CALICO_POD2DAEMON_IMAGE in ${CALICO_POD2DAEMON_IMAGES}; do
-    CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/pod2daemon-flexvol:${CALICO_POD2DAEMON_IMAGE}"
+    CONTAINER_IMAGE="mcr.microsoft.com/oss/calico/pod2daemon-flexvol:v${CALICO_POD2DAEMON_IMAGE}"
     pullContainerImage ${cliTool} ${CONTAINER_IMAGE}
     echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
 done
