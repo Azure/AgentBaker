bdb7dca6ece0d38a218cf57fe53a5ee9f4afcf9e
Adil Adilli <adilliadil14@gmail.com>
2021-03-05 22:54:06 -0800

    move chrony installation out of containerD block (#646)
    Co-authored-by: Adil Adilli <adadilli@microsoft.com>

--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -3587,27 +3587,6 @@ getMobyPkg() {
     apt_get_update || exit $ERR_APT_UPDATE_TIMEOUT
 }
 
-{{- if NeedsContainerd}}
-# CSE+VHD can dictate the containerd version, users don't care as long as it works
-installStandaloneContainerd() {
-    # azure-built runtimes have a "+azure" suffix in their version strings (i.e 1.4.1+azure). remove that here.
-    CURRENT_VERSION=$(containerd -version | cut -d " " -f 3 | sed 's|v||' | cut -d "+" -f 1)
-    # v1.4.1 is our lowest supported version of containerd
-    local CONTAINERD_VERSION="1.4.3"
-    if semverCompare ${CURRENT_VERSION:-"0.0.0"} ${CONTAINERD_VERSION}; then
-        echo "currently installed containerd version ${CURRENT_VERSION} is greater than (or equal to) target base version ${CONTAINERD_VERSION}. skipping installStandaloneContainerd."
-    else
-        echo "installing containerd version ${CONTAINERD_VERSION}"
-        removeMoby
-        removeContainerd
-        downloadContainerd ${CONTAINERD_VERSION}
-        wait_for_apt_locks
-        retrycmd_if_failure 10 5 600 apt-get -y -f install ${CONTAINERD_DEB_FILE} || exit $ERR_CONTAINERD_INSTALL_TIMEOUT
-        rm -Rf $CONTAINERD_DOWNLOADS_DIR &
-    fi
-}
-
-
 disableNtpAndTimesyncdInstallChrony() {
     # Disable systemd-timesyncd
     sudo systemctl stop systemd-timesyncd
@@ -3668,6 +3647,26 @@ makestep 1.0 -1
 EOF
 
     systemctl restart chrony
+}
+
+{{- if NeedsContainerd}}
+# CSE+VHD can dictate the containerd version, users don't care as long as it works
+installStandaloneContainerd() {
+    # azure-built runtimes have a "+azure" suffix in their version strings (i.e 1.4.1+azure). remove that here.
+    CURRENT_VERSION=$(containerd -version | cut -d " " -f 3 | sed 's|v||' | cut -d "+" -f 1)
+    # v1.4.1 is our lowest supported version of containerd
+    local CONTAINERD_VERSION="1.4.3"
+    if semverCompare ${CURRENT_VERSION:-"0.0.0"} ${CONTAINERD_VERSION}; then
+        echo "currently installed containerd version ${CURRENT_VERSION} is greater than (or equal to) target base version ${CONTAINERD_VERSION}. skipping installStandaloneContainerd."
+    else
+        echo "installing containerd version ${CONTAINERD_VERSION}"
+        removeMoby
+        removeContainerd
+        downloadContainerd ${CONTAINERD_VERSION}
+        wait_for_apt_locks
+        retrycmd_if_failure 10 5 600 apt-get -y -f install ${CONTAINERD_DEB_FILE} || exit $ERR_CONTAINERD_INSTALL_TIMEOUT
+        rm -Rf $CONTAINERD_DOWNLOADS_DIR &
+    fi
 }
 
 downloadContainerd() {
