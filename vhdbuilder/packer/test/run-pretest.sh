875959fdeb0e8be1be53305b460e4736cf6ef457
Adil Adilli <adadilli@microsoft.com>
2021-03-08 12:11:53 -0800

    changed components.json URL format

--- vhdbuilder/packer/test/run-pretest.sh
+++ vhdbuilder/packer/test/run-pretest.sh
@@ -1,7 +1,7 @@
 #!/bin/bah
 set -euo pipefail
 
-components=$(cat components.json | jq .ContainerImages[] --monochrome-output --compact-output)
+components=$(cat ../components.json | jq .ContainerImages[] --monochrome-output --compact-output)
 for component in ${components[*]}; do
 	downloadURL=$(echo ${component} | jq .downloadURL)
 	downloadURL=$(echo ${downloadURL//\*/} | jq 'sub(".com/" ; ".com/v2/") | sub(":" ; "/tags/list")' -r)
