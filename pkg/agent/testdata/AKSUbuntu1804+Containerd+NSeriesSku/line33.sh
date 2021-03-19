accc871d1ca15c8dc5729e381755fc1bfba3fac8
Adil Adilli <adadilli@microsoft.com>
2021-03-19 13:17:06 -0700

    make generate

--- pkg/agent/testdata/AKSUbuntu1804+Containerd+NSeriesSku/line33.sh
+++ pkg/agent/testdata/AKSUbuntu1804+Containerd+NSeriesSku/line33.sh
@@ -42,11 +42,6 @@ source /opt/azure/containers/provision_configs.sh
 
 disable1804SystemdResolved
 
-set +x
-ETCD_PEER_CERT=$(echo ${ETCD_PEER_CERTIFICATES} | cut -d'[' -f 2 | cut -d']' -f 1 | cut -d',' -f $((${NODE_INDEX}+1)))
-ETCD_PEER_KEY=$(echo ${ETCD_PEER_PRIVATE_KEYS} | cut -d'[' -f 2 | cut -d']' -f 1 | cut -d',' -f $((${NODE_INDEX}+1)))
-set -x
-
 if [[ $OS == $COREOS_OS_NAME ]]; then
     echo "Changing default kubectl bin location"
     KUBECTL=/opt/kubectl
