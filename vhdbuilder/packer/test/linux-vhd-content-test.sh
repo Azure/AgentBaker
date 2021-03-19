dc0e3ab97d5653bb57c410b27590750964084965
Adil Adilli <adilliadil14@gmail.com>
2021-03-18 18:58:57 -0700

    Add pretests for VHD (#640)
    * create a file with the images to be pulled
    
    * remove deps from test file
    
    * provide file from outside VM
    
    * test script parameters
    
    * save file and reference
    
    * cleaning
    
    * remove debug logs
    
    * add also download files
    
    * mkdir
    
    * correct json read
    
    * Added script to verify image versions
    
    * changed components.json URL format
    
    * update typo
    
    * update json directory
    
    * Added debug logging
    
    * update script
    
    * resolved the Useless cat command
    
    * renamed to clear variable names, changed log message
    
    * changed job name for image-version test, handled redundant variable in post-test
    
    * corrected piping issue
    
    * minor changes, suppressed curl log
    
    * fix some diverged images
    
    Co-authored-by: Adil Adilli <adadilli@microsoft.com>
    Co-authored-by: anujmaheshwari1 <anujmaheshwari600@gmail.com>
    Co-authored-by: Bo Wang <61758179+bowang-666@users.noreply.github.com>

--- vhdbuilder/packer/test/linux-vhd-content-test.sh
+++ vhdbuilder/packer/test/linux-vhd-content-test.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+COMPONENTS_FILEPATH=/opt/azure/components.json
 
 testFilesDownloaded() {
   test="testFilesDownloaded"
   echo "$test:Start"
-  filesToDownload=$1
-
-  filesToDownload=$(echo $filesToDownload | jq -r ".[]" | jq . --monochrome-output --compact-output)
+  filesToDownload=$(jq .DownloadFiles[] --monochrome-output --compact-output < $COMPONENTS_FILEPATH)
 
   for fileToDownload in ${filesToDownload[*]}; do
     fileName=$(echo "${fileToDownload}" | jq .fileName -r)
@@ -45,7 +44,7 @@ testImagesPulled() {
   test="testImagesPulled"
   echo "$test:Start"
   containerRuntime=$1
-  imagesToBePulled=$2
+
   if [ $containerRuntime == 'containerd' ]; then
     pulledImages=$(ctr -n k8s.io image ls)
   elif [ $containerRuntime == 'docker' ]; then
@@ -55,13 +54,11 @@ testImagesPulled() {
     return
   fi
 
-  imagesNotPulled=()
+  imagesToBePulled=$(jq .ContainerImages[] --monochrome-output --compact-output < $COMPONENTS_FILEPATH)
 
-  imagesToBePulled=$(echo $imagesToBePulled | jq -r ".[]" | jq . --monochrome-output --compact-output)
   for imageToBePulled in ${imagesToBePulled[*]}; do
     downloadURL=$(echo "${imageToBePulled}" | jq .downloadURL -r)
     versions=$(echo "${imageToBePulled}" | jq .versions -r | jq -r ".[]")
-
     for version in ${versions}; do
       download_URL=$(string_replace $downloadURL $version)
 
@@ -69,7 +66,6 @@ testImagesPulled() {
         echo "Image ${download_URL} has been pulled Successfully"
       else
         err $test "Image ${download_URL} has NOT been pulled"
-        imagesNotPulled+=("$download_URL")
       fi
     done
 
@@ -122,214 +118,8 @@ string_replace() {
   echo ${1//\*/$2}
 }
 
-filesToDownload='
-[
-{
-  "fileName":"cni-plugins-amd64-v*.tgz",
-  "downloadLocation":"/opt/cni/downloads",
-  "downloadURL":"https://acs-mirror.azureedge.net/cni",
-  "versions": ["0.7.6","0.7.5","0.7.1"]
-},
-{
-  "fileName":"cni-plugins-linux-amd64-v*.tgz",
-  "downloadLocation":"/opt/cni/downloads",
-  "downloadURL":"https://acs-mirror.azureedge.net/cni-plugins/v*/binaries",
-  "versions": ["0.8.6"]
-},
-{
-  "fileName":"azure-vnet-cni-linux-amd64-v*.tgz",
-  "downloadLocation":"/opt/cni/downloads",
-  "downloadURL":"https://acs-mirror.azureedge.net/azure-cni/v*/binaries",
-  "versions":["1.2.7", "1.2.6", "1.2.0_hotfix"]
-},
-{
-  "fileName":"azure-vnet-cni-swift-linux-amd64-v*.tgz",
-  "downloadLocation":"/opt/cni/downloads",
-  "downloadURL":"https://acs-mirror.azureedge.net/azure-cni/v*/binaries",
-  "versions":["1.2.7", "1.2.6"]
-},
-{
-  "fileName":"v*/bpftrace-tools.tar",
-  "downloadLocation":"/opt/bpftrace/downloads",
-  "downloadURL":"https://upstreamartifacts.azureedge.net/bpftrace",
-  "versions": ["0.9.4"]
-}
-]
-'
-imagesToBePulled='
-[
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes/kubernetes-dashboard:v*",
-    "versions": ["1.10.1"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes/dashboard:v*",
-    "versions": ["2.0.0-beta8","2.0.0-rc3","2.0.0-rc7","2.0.1"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes/metrics-scraper:v*",
-    "versions": ["1.0.2","1.0.3","1.0.4"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes/exechealthz:*",
-    "versions": ["1.2"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes/autoscaler/addon-resizer:*",
-    "versions": ["1.8.5","1.8.4","1.8.1","1.7"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes/metrics-server:v*",
-    "versions": ["0.3.6","0.3.5"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/k8s/core/pause:*",
-    "versions": ["1.2.0"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes/pause:*",
-    "versions": ["1.2.0","1.3.1","1.4.0"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes/coredns:*",
-    "versions": ["1.6.6","1.6.5","1.5.0","1.3.1","1.2.6"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/containernetworking/networkmonitor:v*",
-    "versions": ["1.1.8","0.0.7","0.0.6"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/containernetworking/azure-npm:v*",
-    "versions": ["1.2.7","1.2.2_hotfix","1.2.1","1.1.8"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/containernetworking/azure-vnet-telemetry:v*",
-    "versions": ["1.0.30"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/aks/hcp/hcp-tunnel-front:v*",
-    "versions": ["1.9.2-v3.0.18","1.9.2-v3.0.19","1.9.2-v3.0.20"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes/apiserver-network-proxy/agent:v*",
-    "versions": ["0.0.13"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes-csi/secrets-store/driver:v*",
-    "versions": ["0.0.19"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/azure/secrets-store/provider-azure:*",
-    "versions": ["0.0.12"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/aks/hcp/kube-svc-redirect:v*",
-    "versions": ["1.0.7"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/azuremonitor/containerinsights/ciprod:*",
-    "versions": ["ciprod11092020","ciprod01112021","ciprod02232021"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/calico/cni:v*",
-    "versions": ["3.8.9.1","3.8.9.2"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/calico/node:v*",
-    "versions": ["3.17.2","3.8.9.1","3.8.9.2"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/calico/typha:v*",
-    "versions": ["3.17.2","3.8.9"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/calico/pod2daemon-flexvol:v*",
-    "versions": ["3.8.9"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/calico/kube-controllers:v*",
-    "versions": ["3.17.2"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/tigera/operator:v*",
-    "versions": ["1.13.5"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v*",
-    "versions": ["0.5.1","0.6.0","0.7.0"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes/autoscaler/cluster-proportional-autoscaler:*",
-    "versions": ["1.3.0_v0.0.5","1.7.1","1.7.1-hotfix.20200403"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/k8s/flexvolume/blobfuse-flexvolume:*",
-    "versions": ["1.0.15"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes/ip-masq-agent:v*",
-    "versions": ["2.5.0.2","2.5.0.3"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/k8s/kms/keyvault:v*",
-    "versions": ["0.0.9"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes-csi/azuredisk-csi:v*",
-    "versions": ["0.9.0","1.0.0","1.1.0"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes-csi/azurefile-csi:v*",
-    "versions": ["0.9.0","1.0.0"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes-csi/livenessprobe:v*",
-    "versions": ["1.1.0","2.2.0"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes-csi/csi-node-driver-registrar:v*",
-    "versions": ["1.2.0","2.0.1"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/open-policy-agent/gatekeeper:v*",
-    "versions": ["3.1.3","3.2.3"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes/external-dns:v*",
-    "versions": ["0.6.0-hotfix-20200228"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes/defaultbackend:*",
-    "versions": ["1.4"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/kubernetes/ingress/nginx-ingress-controller:*",
-    "versions": ["0.19.0"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/virtual-kubelet/virtual-kubelet:*",
-    "versions": ["1.2.1.1"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/azure-policy/policy-kubernetes-addon-prod:*",
-    "versions": ["prod_20201015.1","prod_20210216.1"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/azure-policy/policy-kubernetes-webhook:*",
-    "versions": ["prod_20200505.3","prod_20210209.1"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/azure-application-gateway/kubernetes-ingress:*",
-    "versions": ["1.0.1-rc3","1.2.0","1.3.0"]
-  },
-  {
-    "downloadURL": "mcr.microsoft.com/oss/azure/aad-pod-identity/nmi:v*",
-    "versions": ["1.7.0","1.7.4"]
-  }
-]
-'
+testFilesDownloaded
+testImagesPulled $1
 
-testFilesDownloaded "$filesToDownload"
-testImagesPulled $1 "$imagesToBePulled"
 testAuditDNotPresent
 testFips $2 $3
