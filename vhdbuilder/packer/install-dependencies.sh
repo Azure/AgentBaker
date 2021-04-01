ffb6ddc6e1443bfd0d9f1999ae2046b3b10b6761
Andy Liu <andyliuliming@outlook.com>
2021-04-01 14:41:26 +0800

    fix

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -275,7 +275,7 @@ KUBE_PROXY_IMAGE_VERSIONS="
 1.20.5-hotfix.20210322
 "
 for KUBE_PROXY_IMAGE_VERSION in ${KUBE_PROXY_IMAGE_VERSIONS}; do
-  if (($(echo ${KUBE_PROXY_IMAGE_VERSIONS} | cut -d"." -f2) < 19)) && [[ ${CONTAINER_RUNTIME} == "containerd" ]]; then
+  if [[ ${CONTAINER_RUNTIME} == "containerd" ]] && (($(echo ${KUBE_PROXY_IMAGE_VERSIONS} | cut -d"." -f2) < 19)) ; then
     echo "Only need to store k8s components >= 1.19 for containerd VHDs"
     continue
   fi
