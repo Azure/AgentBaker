59c040e04d5b5aa2df8f3d55b78a9a31a9a630fe
Tony Xu <tonyxu@microsoft.com>
2021-04-03 11:35:48 -0700

    add time logging

--- pkg/agent/testdata/AKSUbuntu1804+Containerd+Kubenet+FIPSEnabled/line33.sh
+++ pkg/agent/testdata/AKSUbuntu1804+Containerd+Kubenet+FIPSEnabled/line33.sh
@@ -144,7 +144,7 @@ else
     if [[ $API_SERVER_NAME == *.privatelink.* ]]; then
         API_SERVER_CONN_RETRIES=100
     fi
-    retrycmd_if_failure ${API_SERVER_CONN_RETRIES} 1 10 nc -vz ${API_SERVER_NAME} 443 || VALIDATION_ERR=$ERR_K8S_API_SERVER_CONN_FAIL
+    retrycmd_if_failure ${API_SERVER_CONN_RETRIES} 1 10 nc -vz ${API_SERVER_NAME} 443 || (VALIDATION_ERR=$ERR_K8S_API_SERVER_CONN_FAIL && time nc -vz ${API_SERVER_NAME} 443)
 fi
 
 if $REBOOTREQUIRED; then
