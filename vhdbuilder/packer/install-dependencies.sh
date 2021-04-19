57c610d820daf50cd989110481fff78b1f9f3929
pragyaaneja <69482679+pragyaaneja@users.noreply.github.com>
2021-04-19 00:18:05 -0500

    update version to 0.9 for webhook (#756)
    * update version to 0.9 for webhook
    
    * adding both old and new versions of images

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -228,14 +228,20 @@ if [[ ${installSGX} == "True" ]]; then
         echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
     done
 
-    SGX_PLUGIN_VERSIONS="0.2"
+    SGX_PLUGIN_VERSIONS="
+    0.2
+    0.4
+    "
     for SGX_PLUGIN_VERSION in ${SGX_PLUGIN_VERSIONS}; do
         CONTAINER_IMAGE="mcr.microsoft.com/aks/acc/sgx-plugin:${SGX_PLUGIN_VERSION}"
         pullContainerImage ${cliTool} ${CONTAINER_IMAGE}
         echo "  - ${CONTAINER_IMAGE}" >> ${VHD_LOGS_FILEPATH}
     done
 
-    SGX_WEBHOOK_VERSIONS="0.6"
+    SGX_WEBHOOK_VERSIONS="
+    0.6
+    0.9
+    "
     for SGX_WEBHOOK_VERSION in ${SGX_WEBHOOK_VERSIONS}; do
         CONTAINER_IMAGE="mcr.microsoft.com/aks/acc/sgx-webhook:${SGX_WEBHOOK_VERSION}"
         pullContainerImage ${cliTool} ${CONTAINER_IMAGE}
