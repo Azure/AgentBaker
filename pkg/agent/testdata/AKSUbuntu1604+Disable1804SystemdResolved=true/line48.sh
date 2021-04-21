8ce8b492149779de50e8225a937b01e1c47a3c0b
Erik Wu <erw@microsoft.com>
2021-04-20 17:57:31 -0700

    Revert "install specific version of runc for all SKUs (#754)"
    This reverts commit 7cf71d8c655c6e96e220ced4cf94a1910c3b92e1.

--- pkg/agent/testdata/AKSUbuntu1604+Disable1804SystemdResolved=true/line48.sh
+++ pkg/agent/testdata/AKSUbuntu1604+Disable1804SystemdResolved=true/line48.sh
@@ -163,18 +163,6 @@ installMoby() {
         fi
         apt_get_install 20 30 120 moby-engine=${MOBY_VERSION}* moby-cli=${MOBY_CLI}* --allow-downgrades || exit $ERR_MOBY_INSTALL_TIMEOUT
     fi
-    ensureRunc
-}
-
-ensureRunc() {
-    CURRENT_VERSION=$(runc --version | head -n1 | sed 's/runc version //' | sed 's/-/~/')
-    local TARGET_VERSION="1.0.0~rc92"
-    # runc rc93 has a regression that causes pods to be stuck in containercreation
-    # https://github.com/opencontainers/runc/issues/2865
-    # not using semverCompare b/c we need to downgrade
-    if [ "${CURRENT_VERSION}" != "${TARGET_VERSION}" ]; then
-        apt_get_install 20 30 120 moby-runc=${TARGET_VERSION}* --allow-downgrades || exit $ERR_RUNC_INSTALL_TIMEOUT
-    fi
 }
 
 cleanUpGPUDrivers() {
