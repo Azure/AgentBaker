7898a2ff07dbf2297d518adefb31477f7888420b
Erik Wu <erw@microsoft.com>
2021-04-22 13:06:29 -0700

    update

--- pkg/agent/testdata/AKSUbuntu1804+Containerd+Teleport/line40.sh
+++ pkg/agent/testdata/AKSUbuntu1804+Containerd+Teleport/line40.sh
@@ -216,7 +216,7 @@ cleanUpImages() {
     function cleanupImagesRun() {
         
         if [[ "${CLI_TOOL}" == "crictl" ]]; then
-            images_to_delete=$(crictl images | grep -vE "${KUBERNETES_VERSION}$|${KUBERNETES_VERSION}.[0-9]+$|${KUBERNETES_VERSION}-|${KUBERNETES_VERSION}_" | grep ${targetImage} | awk '{print $1}' | tr ' ' '\n')
+            images_to_delete=$(crictl images | grep -vE "${KUBERNETES_VERSION}$|${KUBERNETES_VERSION}.[0-9]+$|${KUBERNETES_VERSION}-|${KUBERNETES_VERSION}_" | grep ${targetImage} | awk '{print $1":"$2}' | tr ' ' '\n')
         else
             images_to_delete=$(ctr --namespace k8s.io images list | grep -vE "${KUBERNETES_VERSION}$|${KUBERNETES_VERSION}.[0-9]+$|${KUBERNETES_VERSION}-|${KUBERNETES_VERSION}_" | grep ${targetImage} | awk '{print $1}' | tr ' ' '\n')
         fi
