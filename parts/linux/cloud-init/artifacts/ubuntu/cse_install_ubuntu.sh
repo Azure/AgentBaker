fee8d16f478b0058a6d136bec73d5fee061c9329
eriksywu <erw@microsoft.com>
2021-04-26 10:59:25 -0700

    Revert "update containerd version to 1.5.0-beta (#691)" (#776)
    * Revert "update containerd version to 1.5.0-beta (#691)"
    
    This reverts commit c6cb2e7c6cc40faebfbc9aa736428ca411cc76f4.
    
    * also need to update install_dependencies

--- parts/linux/cloud-init/artifacts/ubuntu/cse_install_ubuntu.sh
+++ parts/linux/cloud-init/artifacts/ubuntu/cse_install_ubuntu.sh
@@ -166,7 +166,7 @@ installStandaloneContainerd() {
     # v1.4.1 is our lowest supported version of containerd
     #if there is no containerd_version input from RP, use hardcoded version
     if [[ -z ${CONTAINERD_VERSION} ]]; then
-        CONTAINERD_VERSION="1.5.0-beta.git31a0f92df"
+        CONTAINERD_VERSION="1.4.4"
     fi
 
     if semverCompare ${CURRENT_VERSION:-"0.0.0"} ${CONTAINERD_VERSION}; then
@@ -176,11 +176,7 @@ installStandaloneContainerd() {
         removeMoby
         removeContainerd
         updateAptWithMicrosoftPkg
-        # TODO: first try downloading from microsoft apt repo then fallback to our storage ep
-        downloadContainerd ${CONTAINERD_VERSION}
-        wait_for_apt_locks
-        retrycmd_if_failure 10 5 600 apt-get -y -f install ${CONTAINERD_DEB_FILE} || exit $ERR_CONTAINERD_INSTALL_TIMEOUT
-        rm -Rf $CONTAINERD_DOWNLOADS_DIR &
+        apt_get_install 20 30 120 moby-containerd=${CONTAINERD_VERSION}* --allow-downgrades || exit $ERR_CONTAINERD_INSTALL_TIMEOUT
         # runc rc93 has a regression that causes pods to be stuck in containercreation
         # https://github.com/opencontainers/runc/issues/2865
         apt_get_install 20 30 120 moby-runc=1.0.0~rc92* --allow-downgrades || exit $ERR_RUNC_INSTALL_TIMEOUT
@@ -190,7 +186,7 @@ installStandaloneContainerd() {
 downloadContainerd() {
     CONTAINERD_VERSION=$1
     # currently upstream maintains the package on a storage endpoint rather than an actual apt repo
-    CONTAINERD_DOWNLOAD_URL="https://mobyartifacts.azureedge.net/moby/moby-containerd/${CONTAINERD_VERSION}+azure/bionic/linux_amd64/moby-containerd_${CONTAINERD_VERSION/-/\~}+azure-1_amd64.deb"
+    CONTAINERD_DOWNLOAD_URL="https://mobyartifacts.azureedge.net/moby/moby-containerd/${CONTAINERD_VERSION}+azure/bionic/linux_amd64/moby-containerd_${CONTAINERD_VERSION}+azure-1_amd64.deb"
     mkdir -p $CONTAINERD_DOWNLOADS_DIR
     CONTAINERD_DEB_TMP=${CONTAINERD_DOWNLOAD_URL##*/}
     retrycmd_curl_file 120 5 60 "$CONTAINERD_DOWNLOADS_DIR/${CONTAINERD_DEB_TMP}" ${CONTAINERD_DOWNLOAD_URL} || exit $ERR_CONTAINERD_DOWNLOAD_TIMEOUT
