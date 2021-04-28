e085aa4ce7b5952234fc9e1ee18872b36872f9c1
Abel Hu <abelch@microsoft.com>
2021-04-28 16:36:23 +0800

    chore: add Windows pause image to 3.4.1

--- vhdbuilder/packer/test/windows-vhd-content-test.ps1
+++ vhdbuilder/packer/test/windows-vhd-content-test.ps1
@@ -173,6 +173,7 @@ function Test-ImagesPulled {
                 "mcr.microsoft.com/windows/servercore:ltsc2019",
                 "mcr.microsoft.com/windows/nanoserver:1809",
                 "mcr.microsoft.com/oss/kubernetes/pause:1.4.1",
+                "mcr.microsoft.com/oss/kubernetes/pause:3.4.1",
                 "mcr.microsoft.com/oss/kubernetes-csi/livenessprobe:v2.0.1-alpha.1-windows-1809-amd64",
                 "mcr.microsoft.com/oss/kubernetes-csi/livenessprobe:v2.2.0",
                 "mcr.microsoft.com/oss/kubernetes-csi/csi-node-driver-registrar:v1.2.1-alpha.1-windows-1809-amd64",
