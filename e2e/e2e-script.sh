0f4717ed04124d8af25b58f187d93719bfc92ef2
Anuj Maheshwari <amaheshwari@microsoft.com>
2021-05-26 11:54:21 -0700

    get the client keys and server certificates, add them to the json file

--- e2e/e2e-script.sh
+++ e2e/e2e-script.sh
@@ -4,6 +4,7 @@ SUBSCRIPTION_ID="8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8" #Azure Container Service 
 RESOURCE_GROUP_NAME="agentbaker-e2e-tests"
 LOCATION="eastus"
 CLUSTER_NAME="agentbaker-e2e-test-cluster"
+declare -a files=("apiserver.crt" "ca.crt" "client.key" "client.crt")
 
 if [ $(az group exists -n $RESOURCE_GROUP_NAME --subscription $SUBSCRIPTION_ID) == "false" ]; then
     echo "Creating resource group"
@@ -28,3 +29,16 @@ az vmss run-command invoke \
             --instance-id 0 \
             --scripts "cat /etc/kubernetes/azure.json" | jq -r '.value[].message' | awk '/{/{flag=1}/}/{print;flag=0}flag' \
             > fields.json
+
+for file in "${files[@]}"; do
+    content=$(az vmss run-command invoke \
+                -n $VMSS_NAME \
+                -g $MC_RESOURCE_GROUP_NAME \
+                --command-id RunShellScript \
+                --instance-id 0 \
+                --scripts "cat /etc/kubernetes/certs/$file | base64 -w0" | \
+                jq -r '.value[].message' | \
+                awk '/stdout/{flag=1;next}/stderr/{flag=0}flag' \
+            )
+    jq -r --arg key $file --arg value $content '. + { ($key) : $value }' < fields.json > dummy.json && mv dummy.json fields.json
+done
