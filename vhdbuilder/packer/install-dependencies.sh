25ac2258dd72260c19b607071b90428a8a1bd5f8
Julius Liu <juliusl@microsoft.com>
2021-06-02 14:21:53 -0700

    Updating teleportd to v0.8.0

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -108,7 +108,7 @@ if [[ ${CONTAINER_RUNTIME:-""} == "containerd" ]]; then
   cliTool="ctr"
 
   # also pre-download Teleportd plugin for containerd
-  downloadTeleportdPlugin ${TELEPORTD_PLUGIN_DOWNLOAD_URL} "0.7.0"
+  downloadTeleportdPlugin ${TELEPORTD_PLUGIN_DOWNLOAD_URL} "0.8.0"
 else
   CONTAINER_RUNTIME="docker"
   MOBY_VERSION="19.03.14"
