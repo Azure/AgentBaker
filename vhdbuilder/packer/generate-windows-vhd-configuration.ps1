f8a64b8f8582c0b9d8ba65776aabb52438ee4380
Qi Ni <pomelonicky@gmail.com>
2021-06-21 15:11:14 +0800

    chore: bump cnm image version to v1.0.1

--- vhdbuilder/packer/generate-windows-vhd-configuration.ps1
+++ vhdbuilder/packer/generate-windows-vhd-configuration.ps1
@@ -50,7 +50,7 @@ switch ($windowsSKU) {
             "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v0.5.1", # for k8s 1.18.x
             "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v0.6.0", # for k8s 1.19.x
             "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v0.7.4", # for k8s 1.20.x
-            "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.0.0", # for k8s 1.21.x
+            "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.0.1", # for k8s 1.21.x
             "mcr.microsoft.com/azuremonitor/containerinsights/ciprod:win-ciprod04222021",
             "mcr.microsoft.com/azuremonitor/containerinsights/ciprod:win-ciprod06112021")
     }
@@ -70,7 +70,7 @@ switch ($windowsSKU) {
             "mcr.microsoft.com/oss/kubernetes-csi/secrets-store/driver:v0.0.21",
             "mcr.microsoft.com/oss/azure/secrets-store/provider-azure:0.0.14",
             "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v0.7.4", # for k8s 1.20.x
-            "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.0.0", # for k8s 1.21.x
+            "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.0.1", # for k8s 1.21.x
             "mcr.microsoft.com/azuremonitor/containerinsights/ciprod:win-ciprod04222021",
             "mcr.microsoft.com/azuremonitor/containerinsights/ciprod:win-ciprod06112021")
     }
