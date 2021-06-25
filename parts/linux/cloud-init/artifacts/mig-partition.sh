e6199ff89c46e60d478fee4882ca1021e228ac78
qinchen352 <t-qinchen@microsoft.com>
2021-06-24 23:54:10 -0700

    mig config

--- parts/linux/cloud-init/artifacts/mig-partition.sh
+++ parts/linux/cloud-init/artifacts/mig-partition.sh
@@ -4,7 +4,7 @@
 #nvidia-smi -mig 1
 MIG_PROFILE=${1}
 echo "mig profile is ${MIG_PROFILE}"
-if [${MIG_PROFILE} = "all-1g.5gb"] then
+if [${MIG_PROFILE} = "all1g5gb"] then
     nvidia-smi mig -cgi 19,19,19,19,19,19,19
     nvidia-smi mig -cci 
 fi
