fa9e3ca4c938a6f271ba981cc64cf8b5ce3dc9b7
Qingchuan Hao <qinhao@microsoft.com>
2021-07-29 09:05:44 +0000

    wait until managed image ready

--- vhdbuilder/packer/raw-image-to-sig.sh
+++ vhdbuilder/packer/raw-image-to-sig.sh
@@ -8,9 +8,14 @@ set -x
 [[ -z "${OS_TYPE}" ]] && echo "OS_TYPE is not set" && exit 1
 
 CREATE_TIME="$(date +%s)"
-
 IMPORTED_IMAGE_NAME="imported-$CREATE_TIME-$RANDOM"
 
 echo "Creating new image for a custom VHD ${RAW_IMAGE_URL}"
-
+# use eval as a workaround to https://github.com/Azure/azure-cli/issues/16109
 eval "az image create --resource-group ${AZURE_RESOURCE_GROUP_NAME} --name ${IMPORTED_IMAGE_NAME} --os-type ${OS_TYPE} --hyper-v-generation ${hyperv_generation} --hyper-v-generation ${HYPERV_GENERATION} --source ${RAW_IMAGE_URL}"
+until [ ! -z "$managed_image_uri" ]; do
+    echo "${log_prefix}: sleeping for 1m before getting managed image ${AZURE_RESOURCE_GROUP_NAME}/${IMPORTED_IMAGE_NAME}..."
+    sleep 1m
+    managed_image_uri=$(az image show --resource-group ${AZURE_RESOURCE_GROUP_NAME} --name ${IMPORTED_IMAGE_NAME} -o json | jq -r ".id")
+done
+echo "managed image ${managed_image_uri} found"
