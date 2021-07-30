1ef7ee46035377d8340835050fa63d1fc67636c3
Alexander Eldeib <aleldeib@microsoft.com>
2021-07-30 14:16:25 -0700

    regenerate for vhd diff

--- pkg/agent/testdata/AKSUbuntu1804+Containerd+Kubenet/line40.sh
+++ pkg/agent/testdata/AKSUbuntu1804+Containerd+Kubenet/line40.sh
@@ -38,6 +38,22 @@ downloadCNI() {
     retrycmd_get_tarball 120 5 "$CNI_DOWNLOADS_DIR/${CNI_TGZ_TMP}" ${CNI_PLUGINS_URL} || exit $ERR_CNI_DOWNLOAD_TIMEOUT
 }
 
+downloadKrustlet() {
+    local krustlet_version="v0.7.0"
+    local krustlet_url="https://acs-mirror.azureedge.net/krustlet/$krustlet_version/linux/amd64/krustlet-wasi"
+    local krustlet_filepath="/usr/local/bin/krustlet-wasi"
+    if [[ -f "$krustlet_filepath" ]]; then
+        installed_version="$("$krustlet_filepath" --version | cut -d' ' -f2)"
+        if [[ "$krustlet_version" == "$installed_version" ]]; then
+            echo "desired krustlet version exists on disk, skipping download."
+            return
+        fi
+        rm -rf "$krustlet_filepath"
+    fi
+    retrycmd_if_failure 30 5 60 curl -fSL -o "$krustlet_filepath" "$krustlet_url" || exit $ERR_KRUSTLET_DOWNLOAD_TIMEOUT
+    chmod 755 "$krustlet_filepath"
+}
+
 downloadAzureCNI() {
     mkdir -p $CNI_DOWNLOADS_DIR
     CNI_TGZ_TMP=${VNET_CNI_PLUGINS_URL##*/} # Use bash builtin ## to remove all chars ("*") up to the final "/"
--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -1121,6 +1121,8 @@ ERR_HTTP_PROXY_CA_UPDATE=161 {{/* Error updating ca certs to include http proxy 
 ERR_DISBALE_IPTABLES=170 {{/* Error disabling iptables service */}}
 
 ERR_MIG_PARTITION_FAILURE=180 {{/* Error creating MIG instances on MIG node */}}
+ERR_KRUSTLET_DOWNLOAD_TIMEOUT=171 {{/* Timeout waiting for krustlet downloads */}}
+
 
 OS=$(sort -r /etc/*-release | gawk 'match($0, /^(ID_LIKE=(coreos)|ID=(.*))$/, a) { print toupper(a[2] a[3]); exit }')
 UBUNTU_OS_NAME="UBUNTU"
@@ -1350,6 +1352,22 @@ downloadCNI() {
     mkdir -p $CNI_DOWNLOADS_DIR
     CNI_TGZ_TMP=${CNI_PLUGINS_URL##*/} # Use bash builtin ## to remove all chars ("*") up to the final "/"
     retrycmd_get_tarball 120 5 "$CNI_DOWNLOADS_DIR/${CNI_TGZ_TMP}" ${CNI_PLUGINS_URL} || exit $ERR_CNI_DOWNLOAD_TIMEOUT
+}
+
+downloadKrustlet() {
+    local krustlet_version="v0.7.0"
+    local krustlet_url="https://acs-mirror.azureedge.net/krustlet/$krustlet_version/linux/amd64/krustlet-wasi"
+    local krustlet_filepath="/usr/local/bin/krustlet-wasi"
+    if [[ -f "$krustlet_filepath" ]]; then
+        installed_version="$("$krustlet_filepath" --version | cut -d' ' -f2)"
+        if [[ "$krustlet_version" == "$installed_version" ]]; then
+            echo "desired krustlet version exists on disk, skipping download."
+            return
+        fi
+        rm -rf "$krustlet_filepath"
+    fi
+    retrycmd_if_failure 30 5 60 curl -fSL -o "$krustlet_filepath" "$krustlet_url" || exit $ERR_KRUSTLET_DOWNLOAD_TIMEOUT
+    chmod 755 "$krustlet_filepath"
 }
 
 downloadAzureCNI() {
