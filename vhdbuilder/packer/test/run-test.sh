4be55d60a3fa2f5c6db405d99c47f5cf5391d480
Ace Eldeib <alexeldeib@gmail.com>
2021-08-20 23:12:44 +0000

    remove extra debug

--- vhdbuilder/packer/test/run-test.sh
+++ vhdbuilder/packer/test/run-test.sh
@@ -76,8 +76,6 @@ else
   echo "VHD test VM username: $TEST_VM_ADMIN_USERNAME, password: $TEST_VM_ADMIN_PASSWORD"
 fi
 
-az vm get-instance-view -g $RESOURCE_GROUP_NAME -n $VM_NAME
-
 # wait for guest agent to be ready or else run commands may time out, even though the VM is ready.
 timeout 10m time az vm wait -g $RESOURCE_GROUP_NAME -n $VM_NAME --custom 'instanceView.vmAgent.statuses[?code=="ProvisioningState/succeeded"]'
 
