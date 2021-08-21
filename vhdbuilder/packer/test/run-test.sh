8d1f4494f93f1f025877d6c937c185e832155ba3
Ace Eldeib <alexeldeib@gmail.com>
2021-08-21 20:52:52 +0000

    multiple short timeouts

--- vhdbuilder/packer/test/run-test.sh
+++ vhdbuilder/packer/test/run-test.sh
@@ -80,7 +80,9 @@ fi
 az vm get-instance-view -g $RESOURCE_GROUP_NAME -n $VM_NAME
 SECONDS=0
 set +e
-DURATION="60m"
+DURATION="5m"
+time timeout "$DURATION" az vm wait -g $RESOURCE_GROUP_NAME -n $VM_NAME --custom 'instanceView.vmAgent.statuses[?code=="ProvisioningState/succeeded"]'
+time timeout "$DURATION" az vm wait -g $RESOURCE_GROUP_NAME -n $VM_NAME --custom 'instanceView.vmAgent.statuses[?code=="ProvisioningState/succeeded"]'
 time timeout "$DURATION" az vm wait -g $RESOURCE_GROUP_NAME -n $VM_NAME --custom 'instanceView.vmAgent.statuses[?code=="ProvisioningState/succeeded"]'
 WAIT_CODE="$?"
 set -e
