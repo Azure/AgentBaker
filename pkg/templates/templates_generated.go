35543feac99e7f47841fd8641f8134bdebe8b5c2
Alexander Eldeib <aleldeib@microsoft.com>
2021-08-28 21:53:02 +0000

    fix json quoting, image registry, pre-pull image, cloud-init stage name

--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -4246,23 +4246,24 @@ if ! systemctl is-active containerd; then
 fi
 
 echo "regenerating payload"
-ctr run --mount type=bind,src=/var/lib/cloud,dst=/var/lib/cloud,options=rbind --mount type=bind,src=/opt/azure/containers,dst=/opt/azure/containers,options=rbind --rm {{ GetParameter "bakerRegisry" }}/baker:{{ GetParameter "bakerVersion" }} baker /usr/local/bin/baker "{{ToJSON .}}"
+IMAGE={{ GetParameter "bakerRegistry" }}/baker:{{ GetParameter "bakerVersion" }}
+ctr image pull $IMAGE
+ctr run --mount type=bind,src=/var/lib/cloud,dst=/var/lib/cloud,options=rbind --mount type=bind,src=/opt/azure/containers,dst=/opt/azure/containers,options=rbind --rm $IMAGE baker /usr/local/bin/baker {{ToJSON .}}
 echo "removing semaphores"
-rm /var/lib/cloud/instance/sem/config_cc_write_files || true
+rm /var/lib/cloud/instance/sem/config_write_files || true
 rm /var/lib/cloud/instance/sem/config_runcmd || true
 rm /var/lib/cloud/instance/sem/config_scripts_user || true
 echo "rerunning cc_write_files"
 base64 -d /var/lib/cloud/instance/user-data.txt > tmp
 mv tmp /var/lib/cloud/instance/user-data.txt
 mv /var/lib/cloud/instance/scripts/part-001 /tmp/old-001
-cloud-init single -n cc_write_files
+cloud-init single -n write_files
 echo "rerunning runcmd"
 cloud-init single -n runcmd
 echo "rerunning cc_write_files"
 cloud-init single -n scripts_user
 echo "executing regenerated CSE file"
 bash /opt/azure/containers/cse_regen.sh
-
 `)
 
 func linuxCloudInitNodecustomdataVersionedShBytes() ([]byte, error) {
