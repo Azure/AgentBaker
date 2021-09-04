26aa1d8b62f7b3a10958d46bb202ce8a65d912ce
Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>
2021-09-03 18:20:26 -0700

    make generate after previous change

--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -4097,7 +4097,12 @@ installStandaloneContainerd() {
     
     #if there is no containerd_version input from RP, use hardcoded version
     if [[ -z ${CONTAINERD_VERSION} ]]; then
-        CONTAINERD_VERSION="1.5.5"
+        if [[${KUBERNETES_VERSION} ==  "1.22.1"]]; 
+            then
+                CONTAINERD_VERSION="1.5.5"
+            else
+                CONTAINERD_VERSION="1.4.8"
+        fi
         echo "Containerd Version not specified, using default version: ${CONTAINERD_VERSION}"
     else
         echo "Using specified Containerd Version: ${CONTAINERD_VERSION}"
