67e47fbdc33ca743d539e2aaeaad210913c30433
Ganeshkumar Ashokavardhanan <35557827+ganeshkumar5699@users.noreply.github.com>
2021-09-03 17:00:50 -0700

    Add k8s version 1.22.1 to Linux VHD (#1119)
    * Add 1k8s version 1.22 to Linux VHD
    
    * Fix version
    
    * fix version
    
    * fix version
    
    Co-authored-by: Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -322,6 +322,7 @@ done
 # v1.20.9
 # v1.21.1
 # v1.21.2
+# v1.22.1 (preview)
 # NOTE that we keep multiple files per k8s patch version as kubeproxy version is decided by CCP.
 
 if [[ ${CONTAINER_RUNTIME} == "containerd" ]]; then
@@ -366,6 +367,7 @@ KUBE_BINARY_VERSIONS="
 1.20.9-hotfix.20210830
 1.21.1-hotfix.20210827
 1.21.2-hotfix.20210830
+1.22.1
 "
 for PATCHED_KUBE_BINARY_VERSION in ${KUBE_BINARY_VERSIONS}; do
   if (($(echo ${PATCHED_KUBE_BINARY_VERSION} | cut -d"." -f2) < 19)) && [[ ${CONTAINER_RUNTIME} == "containerd" ]]; then
