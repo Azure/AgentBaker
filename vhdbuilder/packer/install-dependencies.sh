181e629dedafd0425b130a9db0973aab01add58b
Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>
2021-09-03 18:01:40 -0700

    Add containerd 1.5.5 only for 1.22.1

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -93,7 +93,12 @@ echo "  - krustlet ${KRUSTLET_VERSION}" >> ${VHD_LOGS_FILEPATH}
 
 if [[ ${CONTAINER_RUNTIME:-""} == "containerd" ]]; then
   echo "VHD will be built with containerd as the container runtime"
-  containerd_version="1.5.5"
+  if [ ${KUBERNETES_VERSION} ==  "1.22.1" ]; 
+    then
+      containerd_version="1.5.5"
+    else
+      containerd_version="1.4.8"
+  fi
   installStandaloneContainerd ${containerd_version}
   echo "  - [installed] containerd v${containerd_version}" >> ${VHD_LOGS_FILEPATH}
   if [[ $OS == $UBUNTU_OS_NAME ]]; then
