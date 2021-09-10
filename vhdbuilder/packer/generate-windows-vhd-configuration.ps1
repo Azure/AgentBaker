7eccfa58a3acf99c0981761dec0b510dd0adb45d
Abel Hu <abelch@microsoft.com>
2021-09-10 14:36:54 +0800

    chore: Cache Windows signedscripts-v0.0.15.zip

--- vhdbuilder/packer/generate-windows-vhd-configuration.ps1
+++ vhdbuilder/packer/generate-windows-vhd-configuration.ps1
@@ -83,6 +83,7 @@ $global:map = @{
         "https://github.com/Microsoft/SDN/raw/master/Kubernetes/windows/hns.psm1",
         "https://globalcdn.nuget.org/packages/microsoft.applicationinsights.2.11.0.nupkg",
         "https://acs-mirror.azureedge.net/aks-engine/windows/provisioning/signedscripts-v0.0.14.zip",
+        "https://acs-mirror.azureedge.net/aks-engine/windows/provisioning/signedscripts-v0.0.15.zip",
         "https://acs-mirror.azureedge.net/ccgakvplugin/v1.1.1/binaries/windows-gmsa-ccgakvplugin-v1.1.1.zip"
     );
     # Different from other packages which are downloaded/cached and used later only during CSE, windows containerd is installed
