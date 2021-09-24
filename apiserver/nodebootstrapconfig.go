ebec432df64f0f52ccbbf979d7d0cc63ad21bb3b
Mirza Sikander <mirsik@microsoft.com>
2021-09-23 23:03:31 -0700

    Restructured and added Cobra and mux

---
+++ apiserver/nodebootstrapconfig.go
@@ -0,0 +1,52 @@
+package apiserver
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	agent "github.com/Azure/agentbaker/pkg/agent"
+	"github.com/Azure/agentbaker/pkg/agent/datamodel"
+)
+
+const (
+	// RoutePathNodeBootstrapping the route path to get node bootstrapping data.
+	RoutePathNodeBootstrapConfig string = "/nodebootstrapconfig"
+)
+
+func handleError(err error, w http.ResponseWriter) {
+	log.Println(err.Error())
+	http.Error(w, err.Error(), http.StatusBadRequest)
+}
+
+// GetNodeBootstrapConfig endpoint for getting node bootstrapping data.
+func (api *APIServer) GetNodeBootstrappingConfig(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	var config datamodel.NodeBootstrappingConfiguration
+
+	err := json.NewDecoder(r.Body).Decode(&config)
+	if err != nil {
+		handleError(err, w)
+		return
+	}
+
+	agentBaker, err := agent.NewAgentBaker()
+	if err != nil {
+		handleError(err, w)
+		return
+	}
+	nodeBootStrapping, err := agentBaker.GetNodeBootstrapping(ctx, &config)
+	if err != nil {
+		handleError(err, w)
+		return
+	}
+	result, err := json.Marshal(nodeBootStrapping)
+	if err != nil {
+		handleError(err, w)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, string(result))
+}
---
+++ apiserver/routers.go
@@ -0,0 +1,33 @@
+package apiserver
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Route the route specifics
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+// Routes list of routes to be added to the server
+type Routes []Route
+
+// NewRouter returns a new router with defaults
+func (api *APIServer) NewRouter(ctx context.Context) *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
+
+	// healthz should not require authentication
+	router.
+		Methods("POST").
+		Path(RoutePathNodeBootstrapConfig).
+		Name("GetNodeBootstrappingConfig").
+		HandlerFunc(api.GetNodeBootstrappingConfig)
+
+	return router
+}
