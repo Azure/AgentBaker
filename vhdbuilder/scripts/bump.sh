159eee1bf5315544eba311f9b4923c1e0427ddac
Anuj Maheshwari <amaheshwari@microsoft.com>
2021-09-27 22:08:08 -0700

    add test script

---
+++ vhdbuilder/scripts/bump.sh
@@ -0,0 +1,25 @@
+echo "Starting script"
+filepath=pkg/agent/datamodel/osimageconfig.go
+flag=0
+image_version=""
+new_version="2021.09.25"
+while read p; do
+    if [[ $p == *":"* ]]; then
+        image_variable=$(echo $p | awk -F: '{print $1}')
+        image_value=$(echo $p | awk -F'\"' '{print $2}')
+        if [[ $flag == 0 ]]; then
+            if [[ $image_value == "aks" ]]; then
+                flag=1
+            fi
+        fi
+
+        if [[ $flag == 1 ]] && [[ $image_variable == "ImageVersion" ]]; then
+            image_version=$image_value
+            flag=0
+            break
+        fi
+    fi
+done < $filepath
+
+echo $image_version
+git status
