60783fb59b97549435c8caa855f318c314fd8b52
Alexander Eldeib <aleldeib@microsoft.com>
2021-10-01 16:11:06 +0000

    bump image for bad ctr version

--- pkg/agent/datamodel/osimageconfig.go
+++ pkg/agent/datamodel/osimageconfig.go
@@ -22,7 +22,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-ubuntu-1804-gen2-2021-q3",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2021.09.27",
+		ImageVersion:   "2021.09.29",
 	}
 
 	RHELOSImageConfig = AzureOSImageConfig{
@@ -36,14 +36,14 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aks-ubuntu-1604-2021-q3",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2021.09.27",
+		ImageVersion:   "2021.09.29",
 	}
 
 	AKSUbuntu1804OSImageConfig = AzureOSImageConfig{
 		ImageOffer:     "aks",
 		ImageSku:       "aks-ubuntu-1804-2021-q3",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "2021.09.27",
+		ImageVersion:   "2021.09.29",
 	}
 
 	AKSWindowsServer2019OSImageConfig = AzureOSImageConfig{
