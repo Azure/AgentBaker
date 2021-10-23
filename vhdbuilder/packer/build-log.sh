b68f6017390075d9881637552c9b4a005464d2af
Alexander Eldeib <aleldeib@microsoft.com>
2021-10-23 03:16:09 +0000

    fix: reboot machine to log accurate kernel

---
+++ vhdbuilder/packer/build-log.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+VHD_LOGS_FILEPATH=/opt/azure/vhd-install.complete
+
+echo "Using kernel:" >> ${VHD_LOGS_FILEPATH}
+tee -a ${VHD_LOGS_FILEPATH} < /proc/version
+{
+  echo "Install completed successfully on " $(date)
+  echo "VSTS Build NUMBER: ${BUILD_NUMBER}"
+  echo "VSTS Build ID: ${BUILD_ID}"
+  echo "Commit: ${COMMIT}"
+  echo "Ubuntu version: ${UBUNTU_RELEASE}"
+  echo "Hyperv generation: ${HYPERV_GENERATION}"
+  echo "Feature flags: ${FEATURE_FLAGS}"
+  echo "Container runtime: ${CONTAINER_RUNTIME}"
+  echo "FIPS enabled: ${ENABLE_FIPS}"
+} >> ${VHD_LOGS_FILEPATH}
--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -432,20 +432,6 @@ df -h >> ${VHD_LOGS_FILEPATH}
 # error at 99% space taken
 [ -s $(df -P | grep '/dev/sda1' | awk '0+$5 >= 99 {print}') ] || exit 1
 
-echo "Using kernel:" >> ${VHD_LOGS_FILEPATH}
-tee -a ${VHD_LOGS_FILEPATH} < /proc/version
-{
-  echo "Install completed successfully on " $(date)
-  echo "VSTS Build NUMBER: ${BUILD_NUMBER}"
-  echo "VSTS Build ID: ${BUILD_ID}"
-  echo "Commit: ${COMMIT}"
-  echo "Ubuntu version: ${UBUNTU_RELEASE}"
-  echo "Hyperv generation: ${HYPERV_GENERATION}"
-  echo "Feature flags: ${FEATURE_FLAGS}"
-  echo "Container runtime: ${CONTAINER_RUNTIME}"
-  echo "FIPS enabled: ${ENABLE_FIPS}"
-} >> ${VHD_LOGS_FILEPATH}
-
 if [[ ${CPU_ARCH,,} != "arm64" ]]; then
   # no asc-baseline-1.0.0-35.arm64.deb
   installAscBaseline
@@ -487,3 +473,6 @@ if [[ $OS == $UBUNTU_OS_NAME ]]; then
     retagContainerImage ${cliTool} ${mcrImage} ${retagMCRImage}
   done
 fi
+
+nohup shutdown -r now < /dev/null > /dev/null 2>&1 &
+exit 0
