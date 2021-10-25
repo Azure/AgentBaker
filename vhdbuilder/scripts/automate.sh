2c4f2500b0307f3a0ba5c7478e6447bb56ddc612
Anuj Maheshwari <amaheshwari@microsoft.com>
2021-10-25 12:26:51 -0700

    find current image_version

--- vhdbuilder/scripts/automate.sh
+++ vhdbuilder/scripts/automate.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
-set -x
+#set -x
+
+current_image_version=""
+find_current_image_version() {
+    filepath=$1
+    flag=0
+    while read p; do
+        if [[ $p == *":"* ]]; then
+            image_variable=$(echo $p | awk -F: '{print $1}')
+            image_value=$(echo $p | awk -F'\"' '{print $2}')
+            if [[ $flag == 0 ]]; then
+                if [[ $image_value == "aks" ]]; then
+                    flag=1
+                fi
+            fi
+
+            if [[ $flag == 1 ]] && [[ $image_variable == "ImageVersion" ]]; then
+                current_image_version=$image_value
+                flag=0
+                break
+            fi
+        fi
+    done < $filepath
+}
 
 echo "Starting script"
-git branch
-git status
-git log --pretty=oneline
+# git branch
+# git status
+# git log --pretty=oneline
+find_current_image_version "pkg/agent/datamodel/osimageconfig.go"
+echo $current_image_version
+
+
