9c5bc3b3bfa752fefeb4fe4b4bedba3c5189895c
Anuj Maheshwari <amaheshwari@microsoft.com>
2021-10-26 11:33:29 -0700

    update image version for all files, add stage for release notes

--- vhdbuilder/scripts/automate.sh
+++ vhdbuilder/scripts/automate.sh
@@ -2,6 +2,7 @@
 #set -x
 
 current_image_version=""
+new_image_version="2021.10.23"
 find_current_image_version() {
     filepath=$1
     flag=0
@@ -24,11 +25,20 @@ find_current_image_version() {
     done < $filepath
 }
 
+update_image_version() {
+    sed -i '' "s/${current_image_version}/${new_image_version}/g" pkg/agent/datamodel/osimageconfig.go
+    sed -i '' "s/${current_image_version}/${new_image_version}/g" pkg/agent/bakerapi_test.go
+    sed -i '' "s/${current_image_version}/${new_image_version}/g" pkg/agent/datamodel/sig_config.go
+    sed -i '' "s/${current_image_version}/${new_image_version}/g" pkg/agent/datamodel/sig_config_test.go
+}
+
 echo "Starting script"
 # git branch
 # git status
 # git log --pretty=oneline
 find_current_image_version "pkg/agent/datamodel/osimageconfig.go"
 echo $current_image_version
+update_image_version
+
 
 
