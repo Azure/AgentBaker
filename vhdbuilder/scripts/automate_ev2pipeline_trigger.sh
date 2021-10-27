bbdee700d21437c297a009b4c0061488a06a5f22
Anuj Maheshwari <amaheshwari@microsoft.com>
2021-10-27 13:25:49 -0700

    add stage for ev2 pipeline trigger

---
+++ vhdbuilder/scripts/automate_ev2pipeline_trigger.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+
+echo "Triggering ev2 artifact pipeline with build id $1"
+build_id=$1
+system_access_token=$2
+
+configure_az_devops() {
+    az extension add -n azure-devops
+    echo ${system_access_token} | az devops login
+    az devops configure --defaults organization=https://dev.azure.com/msazure project=CloudNativeCompute
+}
+
+trigger_pipeline() {
+    az pipelines variable update --name VHD_PIPELINE_RUN_ID --pipeline-id 165939 --value $build_id
+    az pipelines run --id 165939
+}
+
+configure_az_devops
+trigger_pipeline
