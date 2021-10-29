a29ddd24f08e6075806c043427c45547aded41f0
anujmaheshwari1 <amaheshwari@microsoft.com>
2021-10-29 12:30:17 -0700

    log space seperated build id

--- vhdbuilder/scripts/automate_release_notes.sh
+++ vhdbuilder/scripts/automate_release_notes.sh
@@ -6,7 +6,7 @@ source vhdbuilder/scripts/automate_helpers.sh
 echo "Build is for release notes is $1"
 
 image_version=$1
-build_id=$2
+build_ids=$2
 
 set +x
 github_access_token=$3
@@ -17,13 +17,17 @@ branch_name=releaseNotes/$1
 pr_title="ReleaseNotes"
 
 generate_release_notes() {
-    go run vhdbuilder/release-notes/autonotes/main.go --build $build_id --date $image_version
+    echo $build_ids
+    for build_id in $build_ids; do
+        echo $build_id
+    done
+    #go run vhdbuilder/release-notes/autonotes/main.go --build $build_id --date $image_version
 }
 
 configure_az_devops $system_access_token
 set_git_config
-create_branch $branch_name
+#create_branch $branch_name
 generate_release_notes
 
 set +x
-create_pull_request $image_version $github_access_token $branch_name $pr_title
+#create_pull_request $image_version $github_access_token $branch_name $pr_title
