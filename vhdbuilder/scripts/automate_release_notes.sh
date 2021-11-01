0190492ab0f8212906b06be990046a5bc54c8d69
anujmaheshwari1 <amaheshwari@microsoft.com>
2021-11-01 12:56:26 -0700

    reorder , and sku name, add comments

--- vhdbuilder/scripts/automate_release_notes.sh
+++ vhdbuilder/scripts/automate_release_notes.sh
@@ -17,21 +17,19 @@ branch_name=releaseNotes/$1
 pr_title="ReleaseNotes"
 
 generate_release_notes() {
-    echo $build_ids
     for build_id in $build_ids; do
         echo $build_id
         included_skus=""
-        artifacts=($(az pipelines runs artifact list --run-id $build_id | jq -r '.[].name'))
+        artifacts=($(az pipelines runs artifact list --run-id $build_id | jq -r '.[].name')) # Retrieve what artifacts were published
         for artifact in "${artifacts[@]}"; do
             if [[ $artifact == *"vhd-release-notes"* ]]; then
-                sku=$(echo $artifact | cut -d "-" -f4-)
-                included_skus+=",$sku"
+                sku=$(echo $artifact | cut -d "-" -f4-) # Format of artifact is vhd-release-notes-<name of sku>
+                included_skus+="$sku,"
             fi
         done
-        echo "skus for release notes are $included_skus"
+        echo "SKUs for release notes are $included_skus"
         go run vhdbuilder/release-notes/autonotes/main.go --build $build_id --date $image_version --include ${included_skus%?}
     done
-    #go run vhdbuilder/release-notes/autonotes/main.go --build $build_id --date $image_version
 }
 
 configure_az_devops $system_access_token
@@ -40,4 +38,4 @@ create_branch $branch_name
 generate_release_notes
 
 set +x
-#create_pull_request $image_version $github_access_token $branch_name $pr_title
+create_pull_request $image_version $github_access_token $branch_name $pr_title
