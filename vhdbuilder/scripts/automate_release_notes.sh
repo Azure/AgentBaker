4088dcc7aa99b3021883f77535a45bafc83e3dcb
anujmaheshwari1 <anujmaheshwari600@gmail.com>
2021-11-01 18:24:58 -0700

    Multiple builds for release notes in Automation Pipeline (#1290)
    * log space seperated build id
    
    * remove dummy release notes file
    
    * pass array arg for build ids
    
    * logic to append included skus to autonotes
    
    * add confition for release notes, strip last ,
    
    * reorder , and sku name, add comments
    
    * remove redundant use of bash array
    
    Co-authored-by: anujmaheshwari1 <amaheshwari@microsoft.com>

--- vhdbuilder/scripts/automate_release_notes.sh
+++ vhdbuilder/scripts/automate_release_notes.sh
@@ -6,7 +6,7 @@ source vhdbuilder/scripts/automate_helpers.sh
 echo "Build is for release notes is $1"
 
 image_version=$1
-build_id=$2
+build_ids=$2
 
 set +x
 github_access_token=$3
@@ -17,7 +17,18 @@ branch_name=releaseNotes/$1
 pr_title="ReleaseNotes"
 
 generate_release_notes() {
-    go run vhdbuilder/release-notes/autonotes/main.go --build $build_id --date $image_version
+    for build_id in $build_ids; do
+        echo $build_id
+        included_skus=""
+        for artifact in $(az pipelines runs artifact list --run-id $build_id | jq -r '.[].name'); do    # Retrieve what artifacts were published
+            if [[ $artifact == *"vhd-release-notes"* ]]; then
+                sku=$(echo $artifact | cut -d "-" -f4-) # Format of artifact is vhd-release-notes-<name of sku>
+                included_skus+="$sku,"
+            fi
+        done
+        echo "SKUs for release notes are $included_skus"
+        go run vhdbuilder/release-notes/autonotes/main.go --build $build_id --date $image_version --include ${included_skus%?}
+    done
 }
 
 configure_az_devops $system_access_token
