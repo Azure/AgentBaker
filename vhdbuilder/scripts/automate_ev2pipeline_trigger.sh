320d414ee9280eb5de57f51bd133a73716ea1b98
anujmaheshwari1 <amaheshwari@microsoft.com>
2021-11-04 11:52:20 -0700

    add logic to wait till ev2 artifacts succeeds

--- vhdbuilder/scripts/automate_ev2pipeline_trigger.sh
+++ vhdbuilder/scripts/automate_ev2pipeline_trigger.sh
@@ -5,17 +5,27 @@ source vhdbuilder/scripts/automate_helpers.sh
 
 echo "Triggering ev2 artifact pipeline with Build ID $1"
 
-build_id=$1
-
-set +x
-system_access_token=$2
+build_ids=$1
 
 trigger_pipeline() {
-    az pipelines variable update --name VHD_PIPELINE_RUN_ID --pipeline-id 165939 --value $build_id  # Update the VHD_PIPELINE_RUN_ID with the build ID
-    az pipelines run --id 165939  # 165939 is the pipeline ID for ev2 artifacts pipeline
-}
+    pipeline_id="165939"   # 165939 is the pipeline ID for ev2 artifacts pipeline
+    for build_id in $build_ids; do
+        echo "Build ID for the release is $build_id"
+        az pipelines variable update --name VHD_PIPELINE_RUN_ID --pipeline-id $pipeline_id --value $build_id  # Update the VHD_PIPELINE_RUN_ID with the build ID
+        if [[ $build_id != $(az pipelines variable list --pipline-id $pipeline_id | jq -r '.VHD_PIPELINE_RUN_ID.value') ]]; then
+            echo "Build ID failed to update, cancel operation"
+            exit 1
+        else
+            echo "Build ID successfully updated"
+        fi
 
-#configure_az_devops $system_access_token
-set -x
+        run_id=$(az pipelines run --id $pipeline_id | jq -r '.id')
+        while ! az pipelines runs show --id $run_id | grep -q '"result": "succeeded"'; do
+            echo "ev2 artifacts still running for build $build_id"
+            sleep 100
+        done
+        echo "ev2 artifacts successfully built for build $build_id"
+    done
+}
 
 trigger_pipeline
