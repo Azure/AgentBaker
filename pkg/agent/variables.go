7a70856728472315026a17a3bd2f507624ef0996
anujmaheshwari1 <amaheshwari@microsoft.com>
2021-11-12 12:09:33 -0800

    change outboundcmd to use curl instead of netcat

--- pkg/agent/variables.go
+++ pkg/agent/variables.go
@@ -169,15 +169,15 @@ func getOutBoundCmd(cs *datamodel.ContainerService, cloudSpecConfig *datamodel.A
 	}
 	registry := ""
 	if cloudSpecConfig.CloudName == datamodel.AzureChinaCloud {
-		registry = `gcr.azk8s.cn 443`
+		registry = `gcr.azk8s.cn`
 	} else if cs.IsAKSCustomCloud() {
-		registry = cs.Properties.CustomCloudEnv.McrURL + " 443"
+		registry = cs.Properties.CustomCloudEnv.McrURL
 	} else {
-		registry = `mcr.microsoft.com 443`
+		registry = `mcr.microsoft.com`
 	}
 
 	if registry == "" {
 		return ""
 	}
-	return `retrycmd_if_failure() { r=$1; w=$2; t=$3; shift && shift && shift; for i in $(seq 1 $r); do timeout $t ${@}; [ $? -eq 0  ] && break || if [ $i -eq $r ]; then return 1; else sleep $w; fi; done }; ERR_OUTBOUND_CONN_FAIL=50; retrycmd_if_failure 100 1 10 nc -vz ` + registry + ` >> /var/log/azure/cluster-provision-cse-output.log 2>&1 || time nc -vz ` + registry + ` || exit $ERR_OUTBOUND_CONN_FAIL;`
+	return `retrycmd_if_failure() { r=$1; w=$2; t=$3; shift && shift && shift; for i in $(seq 1 $r); do timeout $t ${@}; [ $? -eq 0  ] && break || if [ $i -eq $r ]; then return 1; else sleep $w; fi; done }; ERR_OUTBOUND_CONN_FAIL=50; retrycmd_if_failure 100 1 10 curl -v --insecure --proxy-insecure https://` + registry + `/v2/ >> /var/log/azure/cluster-provision-cse-output.log 2>&1 || time curl -v --insecure --proxy-insecure https://` + registry + `/v2/ || exit $ERR_OUTBOUND_CONN_FAIL;`
 }
