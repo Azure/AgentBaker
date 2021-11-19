1b2fd6091b737a6061431fda6059cc85b39c07a1
Ganeshkumar Ashokavardhanan <35557827+ganeshkumar5699@users.noreply.github.com>
2021-11-19 13:56:47 -0800

    Add k8s patch versions: 1.20.13, 1.21.7, 1.22.4 (#1336)
    Co-authored-by: Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -380,10 +380,13 @@ KUBE_BINARY_VERSIONS="
 1.19.13-hotfix.20210830
 1.20.7-hotfix.20210816
 1.20.9-hotfix.20210830
+1.20.13
 1.21.1-hotfix.20210827
 1.21.2-hotfix.20210830
+1.21.7
 1.22.1
 1.22.2
+1.22.4
 "
 for PATCHED_KUBE_BINARY_VERSION in ${KUBE_BINARY_VERSIONS}; do
   if (($(echo ${PATCHED_KUBE_BINARY_VERSION} | cut -d"." -f2) < 19)) && [[ ${CONTAINER_RUNTIME} == "containerd" ]]; then
--- vhdbuilder/packer/test/linux-vhd-content-test.sh
+++ vhdbuilder/packer/test/linux-vhd-content-test.sh
@@ -196,10 +196,13 @@ testKubeBinariesPresent() {
   1.19.13-hotfix.20210830
   1.20.7-hotfix.20210816
   1.20.9-hotfix.20210830
+  1.20.13
   1.21.1-hotfix.20210827
   1.21.2-hotfix.20210830
+  1.21.7
   1.22.1
   1.22.2
+  1.22.4
   "
   for patchedK8sVersion in ${k8sVersions}; do
     # Only need to store k8s components >= 1.19 for containerd VHDs
