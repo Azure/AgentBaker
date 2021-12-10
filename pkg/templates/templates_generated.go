e8924e0138151e338dca02db4b36f24b99512bdd
Patrick W. Healy <patrick.healy@microsoft.com>
2021-12-10 02:30:18 +0000

    add k8s version check to containerd registry config

--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -4646,8 +4646,10 @@ write_files:
         conf_dir = "/etc/cni/net.d"
         conf_template = "/etc/containerd/kubenet_template.conf"
       {{- end}}
+      {{if IsKubernetesVersionGe "1.22.0"}}
       [plugins."io.containerd.grpc.v1.cri".registry]
         config_path = "/etc/containerd/certs.d"
+      {{end}}
       [plugins."io.containerd.grpc.v1.cri".registry.headers]
         X-Meta-Source-Client = ["azure/aks"]
     [metrics]
@@ -4695,8 +4697,10 @@ write_files:
         conf_dir = "/etc/cni/net.d"
         conf_template = "/etc/containerd/kubenet_template.conf"
       {{ end}}
+      {{if IsKubernetesVersionGe "1.22.0"}}
       [plugins."io.containerd.grpc.v1.cri".registry]
         config_path = "/etc/containerd/certs.d"
+      {{end}}
       [plugins."io.containerd.grpc.v1.cri".registry.headers]
         X-Meta-Source-Client = ["azure/aks"]
     [metrics]
