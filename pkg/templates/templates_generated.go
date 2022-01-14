20bc03f9f17b1ab797033cbfd1df93db64d23d45
Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>
2022-01-13 21:52:44 -0800

    upgrade (only) containerd to 1.5.9 and make generate

--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -1433,8 +1433,8 @@ installContainerRuntime() {
     {{if NeedsContainerd}}
         echo "in installContainerRuntime - KUBERNETES_VERSION = ${KUBERNETES_VERSION}"
         if semverCompare ${KUBERNETES_VERSION} "1.22.0"; then
-            CONTAINERD_VERSION="1.5.5"
-            CONTAINERD_PATCH_VERSION="3"
+            CONTAINERD_VERSION="1.5.9"
+            CONTAINERD_PATCH_VERSION="2"
             installStandaloneContainerd ${CONTAINERD_VERSION} "${CONTAINERD_PATCH_VERSION}"
             echo "in installContainerRuntime - CONTAINERD_VERION = ${CONTAINERD_VERSION}"
         else
