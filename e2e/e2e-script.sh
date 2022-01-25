349114a8894ae8b8286271daa8ab1eeb83b90b51
Ace Eldeib <aleldeib@microsoft.com>
2022-01-25 07:28:16 +0000

    fix kubeconfig

--- e2e/e2e-script.sh
+++ e2e/e2e-script.sh
@@ -43,6 +43,7 @@ if [ -z "$out" ]; then
 fi
 
 az aks get-credentials -g $RESOURCE_GROUP_NAME -n $CLUSTER_NAME --file kubeconfig --overwrite-existing
+KUBECONFIG=$(pwd)/kubeconfig
 kubectl cluster-info 
 kubectl get pod -A
 
