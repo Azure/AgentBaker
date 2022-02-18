f228ca4b9c229a8fadb5e33dd79aef470fc6fd45
Chou Hu <abelch@microsoft.com>
2022-02-18 14:23:12 +0800

    chore: Cache windows-gmsa-ccgakvplugin-v1.1.4.zip (#1544)

--- vhdbuilder/packer/generate-windows-vhd-configuration.ps1
+++ vhdbuilder/packer/generate-windows-vhd-configuration.ps1
@@ -108,6 +108,7 @@ $global:map = @{
         "https://acs-mirror.azureedge.net/aks-engine/windows/provisioning/signedscripts-v0.0.15.zip",
         "https://acs-mirror.azureedge.net/aks-engine/windows/provisioning/signedscripts-v0.0.16.zip",
         "https://acs-mirror.azureedge.net/ccgakvplugin/v1.1.3/binaries/windows-gmsa-ccgakvplugin-v1.1.3.zip",
+        "https://acs-mirror.azureedge.net/ccgakvplugin/v1.1.4/binaries/windows-gmsa-ccgakvplugin-v1.1.4.zip",
         "https://acs-mirror.azureedge.net/aks/windows/cse/aks-windows-cse-scripts-v0.0.4.zip"
     );
     # Different from other packages which are downloaded/cached and used later only during CSE, windows containerd is installed
