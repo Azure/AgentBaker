eaa871fbdd4ab7755117d4ef23864ae3d6074352
Mirza Sikander <mirsik@microsoft.com>
2022-03-11 11:58:02 -0800

    revert image bump since we will not be releasing a version this week

--- pkg/agent/datamodel/sig_config.go
+++ pkg/agent/datamodel/sig_config.go
@@ -204,8 +204,9 @@ const (
 )
 
 const (
-	LinuxSIGImageVersion   string = "2022.03.07"
+	LinuxSIGImageVersion   string = "2022.03.01"
 	WindowsSIGImageVersion string = "17763.2686.220309"
+
 	// will not do weekly vhd release as amd64 when ARM64 Compute/AKS is still under development
 	Arm64LinuxSIGImageVersion string = "2022.03.05"
 	MarinerSIGImageVersion    string = "2022.02.19"
