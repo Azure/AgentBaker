02766987c2eff6eb074af8f145a4944a7c701fc2
anujmaheshwari1 <amaheshwari@microsoft.com>
2022-03-11 01:10:35 +0000

    Bumping image version to 2022.03.05

--- pkg/agent/datamodel/sig_config.go
+++ pkg/agent/datamodel/sig_config.go
@@ -204,7 +204,7 @@ const (
 )
 
 const (
-	LinuxSIGImageVersion   string = "2022.03.07"
+	LinuxSIGImageVersion   string = "2022.03.05"
 	WindowsSIGImageVersion string = "17763.2686.220309"
 	// will not do weekly vhd release as amd64 when ARM64 Compute/AKS is still under development
 	Arm64LinuxSIGImageVersion string = "2022.03.05"
