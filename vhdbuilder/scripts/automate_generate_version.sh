a6b5f48b22baaf652ee7816b3f93f098bae38eba
anujmaheshwari1 <amaheshwari@microsoft.com>
2022-03-17 00:32:22 -0700

    echo only version from child process

--- vhdbuilder/scripts/automate_generate_version.sh
+++ vhdbuilder/scripts/automate_generate_version.sh
@@ -8,14 +8,14 @@ for build_id in $build_ids; do
         if [[ $artifact == *"publishing-info"* ]]; then
             az pipelines runs artifact download --artifact-name $artifact --path $(pwd) --run-id $build_id
             current_image_version=$(cat vhd-publishing-info.json | jq -r .image_version)
-            echo "current_image_version is $current_image_version"
+            #echo "current_image_version is $current_image_version"
             if [[ -z $global_image_version ]]; then
                 global_image_version=$current_image_version
             elif [[ $global_image_version != $current_image_version ]]; then
-                echo "Image versions mismatch for $build_id and $artifact"
+                #echo "Image versions mismatch for $build_id and $artifact"
                 exit 1
             else
-                echo "Image versions match, check next"
+                #echo "Image versions match, check next"
             fi
         fi
     done
