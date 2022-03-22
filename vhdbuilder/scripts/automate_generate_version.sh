1f3e24ad932551233939bfa20be334a77e2739a3
anujmaheshwari1 <amaheshwari@microsoft.com>
2022-03-22 10:12:58 -0700

    fix shell check cat issue

--- vhdbuilder/scripts/automate_generate_version.sh
+++ vhdbuilder/scripts/automate_generate_version.sh
@@ -7,7 +7,7 @@ for build_id in $build_ids; do
     for artifact in $(az pipelines runs artifact list --run-id $build_id | jq -r '.[].name'); do    # Retrieve what artifacts were published
         if [[ $artifact == *"publishing-info"* ]]; then
             az pipelines runs artifact download --artifact-name $artifact --path $(pwd) --run-id $build_id
-            current_image_version=$(cat vhd-publishing-info.json | jq -r .image_version)
+            current_image_version=$(jq -r .image_version < vhd-publishing-info.json)
             if [[ $global_image_version != $current_image_version ]]; then
                 if [[ -z $global_image_version ]]; then
                     global_image_version=$current_image_version
