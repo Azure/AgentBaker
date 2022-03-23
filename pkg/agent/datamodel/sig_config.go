ee1ccb09065bc33c812927fb7fa9e5cdfe1ca8dd
Ace Eldeib <aleldeib@microsoft.com>
2022-03-23 13:52:52 -0400

    bump: linux vhd to 2022.03.20 (#1641)

--- pkg/agent/datamodel/sig_config.go
+++ pkg/agent/datamodel/sig_config.go
@@ -205,7 +205,7 @@ const (
 )
 
 const (
-	LinuxSIGImageVersion string = "2022.03.15"
+	LinuxSIGImageVersion string = "2022.03.20"
 
 	Windows2019SIGImageVersion string = "17763.2686.220322"
 	Windows2022SIGImageVersion string = "20348.587.220322"
