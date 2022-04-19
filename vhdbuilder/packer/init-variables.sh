30a96cd6617520f3a22a9ab239a7e014db158235
Chou Hu <abelch@microsoft.com>
2022-04-19 14:50:40 +0800

    build: create nsg for Windows build VM (#1729)

--- vhdbuilder/packer/init-variables.sh
+++ vhdbuilder/packer/init-variables.sh
@@ -8,7 +8,6 @@ SUBSCRIPTION_ID="${SUBSCRIPTION_ID:-$(az account show -o json --query="id" | tr 
 CREATE_TIME="$(date +%s)"
 STORAGE_ACCOUNT_NAME="aksimages${CREATE_TIME}$RANDOM"
 
-
 echo "Subscription ID: ${SUBSCRIPTION_ID}"
 echo "Service Principal Path: ${SP_JSON}"
 
@@ -27,6 +26,34 @@ rg_id=$(az group show --name $AZURE_RESOURCE_GROUP_NAME) || rg_id=""
 if [ -z "$rg_id" ]; then
 	echo "Creating resource group $AZURE_RESOURCE_GROUP_NAME, location ${AZURE_LOCATION}"
 	az group create --name $AZURE_RESOURCE_GROUP_NAME --location ${AZURE_LOCATION}
+fi
+
+if [ -n "${VNET_RESOURCE_GROUP_NAME}" ]; then
+	VIRTUAL_NETWORK_NAME="vnet"
+	VIRTUAL_NETWORK_SUBNET_NAME="subnet"
+	NETWORK_SECURITY_GROUP_NAME="nsg"
+
+	echo "creating resource group ${VNET_RESOURCE_GROUP_NAME}, location ${AZURE_LOCATION} for VNET"
+	az group create --name ${VNET_RESOURCE_GROUP_NAME} --location ${AZURE_LOCATION} \
+		--tags 'os=Windows' 'createdBy=aks-vhd-pipeline' 'SkipASMAzSecPack=True'
+
+	echo "creating new network security group ${NETWORK_SECURITY_GROUP_NAME}"
+	az network nsg create --name $NETWORK_SECURITY_GROUP_NAME --resource-group ${VNET_RESOURCE_GROUP_NAME} --location ${AZURE_LOCATION}
+	echo "creating nsg rule to allow WinRM with ssl"
+	az network nsg rule create --resource-group ${VNET_RESOURCE_GROUP_NAME} --nsg-name $NETWORK_SECURITY_GROUP_NAME -n AllowWinRM --priority 100 \
+		--source-address-prefixes '*' --source-port-ranges '*' \
+		--destination-address-prefixes '*' --destination-port-ranges 5986 --access Allow \
+		--protocol Tcp --description "Allow all inbound to WinRM with SSL 5986."
+	echo "creating default nsg rule to deny all internet inbound"
+	az network nsg rule create --resource-group ${VNET_RESOURCE_GROUP_NAME} --nsg-name $NETWORK_SECURITY_GROUP_NAME -n DenyAll --priority 4096 \
+		--source-address-prefixes '*' --source-port-ranges '*' \
+		--destination-address-prefixes '*' --destination-port-ranges '*' --access Deny \
+		--protocol '*' --description "Deny all inbound by default"
+
+	echo "creating new vnet ${VIRTUAL_NETWORK_NAME}, subnet ${VIRTUAL_NETWORK_SUBNET_NAME}"
+	az network vnet create --resource-group ${VNET_RESOURCE_GROUP_NAME} --name $VIRTUAL_NETWORK_NAME --address-prefix 10.0.0.0/16 \
+		--subnet-name $VIRTUAL_NETWORK_SUBNET_NAME --subnet-prefix 10.0.0.0/24 --network-security-group $NETWORK_SECURITY_GROUP_NAME \
+		--tags 'os=Windows' 'createdBy=aks-vhd-pipeline' 'SkipNRMSNSG=295551581' 'SkipASMAzSecPack=True'
 fi
 
 avail=$(az storage account check-name -n ${STORAGE_ACCOUNT_NAME} -o json | jq -r .nameAvailable)
