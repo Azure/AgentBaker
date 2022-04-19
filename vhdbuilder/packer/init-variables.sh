8dbb180066f222e320eb2dde8d82a97b8848c2ca
Charlie Li <charlili@microsoft.com>
2022-04-18 18:17:18 -0700

    fix build error because of different locations

--- vhdbuilder/packer/init-variables.sh
+++ vhdbuilder/packer/init-variables.sh
@@ -35,7 +35,7 @@ if $avail ; then
 	az storage account create -n $STORAGE_ACCOUNT_NAME -g $AZURE_RESOURCE_GROUP_NAME --sku "Standard_RAGRS" --tags "now=${CREATE_TIME}" --location ${AZURE_LOCATION}
 	echo "creating new container system"
 	key=$(az storage account keys list -n $STORAGE_ACCOUNT_NAME -g $AZURE_RESOURCE_GROUP_NAME | jq -r '.[0].value')
-	az storage container create --name system --account-key=$key --account-name=$STORAGE_ACCOUNT_NAME --location ${AZURE_LOCATION}
+	az storage container create --name system --account-key=$key --account-name=$STORAGE_ACCOUNT_NAME
 else
 	echo "storage account ${STORAGE_ACCOUNT_NAME} already exists."
 fi
