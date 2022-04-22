d47bfae87165ca92916d2ed6b532314b5a55817d
Ace Eldeib <aleldeib@microsoft.com>
2022-04-22 17:15:00 +0000

    tweak for 18.04+ only

--- vhdbuilder/scripts/linux/ubuntu/tool_installs_ubuntu.sh
+++ vhdbuilder/scripts/linux/ubuntu/tool_installs_ubuntu.sh
@@ -38,10 +38,7 @@ installBcc() {
     wait_for_apt_locks
     apt_get_update || exit $ERR_APT_UPDATE_TIMEOUT
     VERSION=$(grep DISTRIB_RELEASE /etc/*-release| cut -f 2 -d "=")
-    apt_get_install 120 5 300 build-essential git bison cmake flex  libedit-dev libllvm8.0 llvm-8.0-dev libclang-8.0-dev python zlib1g-dev libelf-dev || exit $ERR_BCC_INSTALL_TIMEOUT
-    if [ "$VERSION" = "18.04" ]; then
-        apt_get_install 120 5 300 python3-distutils libfl-dev
-    fi
+    apt_get_install 120 5 300 build-essential git bison cmake flex  libedit-dev libllvm6.0 llvm-6.0-dev libclang-6.0-dev python zlib1g-dev libelf-dev python3-distutils libfl-dev || exit $ERR_BCC_INSTALL_TIMEOUT
     mkdir -p /tmp/bcc
     pushd /tmp/bcc
     git clone https://github.com/iovisor/bcc.git
@@ -59,10 +56,7 @@ installBcc() {
     # we explicitly do not remove build-essential or git
     # these are standard packages we want to keep, they should usually be in the final build anyway.
     # only ensuring they are installed above.
-    apt_get_purge 120 5 300 bison cmake flex libedit-dev libllvm8.0 llvm-8.0-dev libclang-8.0-dev zlib1g-dev libelf-dev || exit $ERR_BCC_INSTALL_TIMEOUT
-    if [ "$VERSION" = "18.04" ]; then
-        apt_get_purge 120 5 25 python3-distutils libfl-dev
-    fi
+    apt_get_purge 120 5 300 bison cmake flex libedit-dev libllvm6.0 llvm-6.0-dev libclang-6.0-dev zlib1g-dev libelf-dev python3-distutils libfl-dev || exit $ERR_BCC_INSTALL_TIMEOUT
 }
 
 configGPUDrivers() {
