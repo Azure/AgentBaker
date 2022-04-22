ee33e250f2573dbf721c0019f6c2c447e6b7c2e2
Ace Eldeib <aleldeib@microsoft.com>
2022-04-22 03:01:03 +0000

    8?

--- vhdbuilder/scripts/linux/ubuntu/tool_installs_ubuntu.sh
+++ vhdbuilder/scripts/linux/ubuntu/tool_installs_ubuntu.sh
@@ -38,7 +38,7 @@ installBcc() {
     wait_for_apt_locks
     apt_get_update || exit $ERR_APT_UPDATE_TIMEOUT
     VERSION=$(grep DISTRIB_RELEASE /etc/*-release| cut -f 2 -d "=")
-    apt_get_install 120 5 300 build-essential git bison cmake flex  libedit-dev libllvm9.0 llvm-9.0-dev libclang-9.0-dev python zlib1g-dev libelf-dev || exit $ERR_BCC_INSTALL_TIMEOUT
+    apt_get_install 120 5 300 build-essential git bison cmake flex  libedit-dev libllvm8.0 llvm-8.0-dev libclang-8.0-dev python zlib1g-dev libelf-dev || exit $ERR_BCC_INSTALL_TIMEOUT
     if [ "$VERSION" = "18.04" ]; then
         apt_get_install 120 5 300 python3-distutils libfl-dev
     fi
@@ -58,7 +58,7 @@ installBcc() {
     # we explicitly do not remove build-essential or git
     # these are standard packages we want to keep, they should usually be in the final build anyway.
     # only ensuring they are installed above.
-    apt_get_purge 120 5 300 bison cmake flex libedit-dev libllvm9.0 llvm-9.0-dev libclang-9.0-dev zlib1g-dev libelf-dev || exit $ERR_BCC_INSTALL_TIMEOUT
+    apt_get_purge 120 5 300 bison cmake flex libedit-dev libllvm8.0 llvm-8.0-dev libclang-8.0-dev zlib1g-dev libelf-dev || exit $ERR_BCC_INSTALL_TIMEOUT
     if [ "$VERSION" = "18.04" ]; then
         apt_get_purge 120 5 25 python3-distutils libfl-dev
     fi
@@ -195,7 +195,7 @@ installFIPS() {
     rm -f /etc/apt/sources.list.d/ubuntu-esm-apps.list
     rm -f /etc/apt/sources.list.d/ubuntu-esm-infra.list
     rm -f /etc/apt/sources.list.d/ubuntu-fips.list
-    rm -f /etc/apt/auth.conf.d/90ubuntu-advantage
+    rm -f /etc/apt/auth.conf.d/80ubuntu-advantage
     apt_get_update || exit $ERR_APT_UPDATE_TIMEOUT
 }
 
