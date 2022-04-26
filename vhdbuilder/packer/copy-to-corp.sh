49093a8819d5d6d00d245f442980f3deb60af684
Mirza Sikander <mirsik@microsoft.com>
2022-04-26 03:24:37 -0700

    use blob copy

--- vhdbuilder/packer/copy-to-corp.sh
+++ vhdbuilder/packer/copy-to-corp.sh
@@ -18,11 +18,16 @@ done
 
 STORAGE_ACCOUNT_NAME="vhdbuildercorpwestus2"
 CONTAINER_NAME="vhds"
-endtime=`date -u -d "60 minutes" '+%Y-%m-%dT%H:%MZ'`
-sas_token=`az storage container generate-sas --account-name $STORAGE_ACCOUNT_NAME --name $CONTAINER_NAME --permissions dlrw --expiry $endtime --auth-mode login --as-user -o tsv`
-
 BLOB_NAME="$OFFER_NAME.$SKU_NAME.$HYPERV_GEN.$IMAGE_VERSION"
-target_uri="https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net/${CONTAINER_NAME}/${BLOB_NAME}.vhd?$sas_token"
 
-echo $SOURCE_VHD_URL $target_uri
-azcopy-preview copy "${SOURCE_VHD_URL}" "${target_uri}"
+az storage blob copy start --account-name $STORAGE_ACCOUNT_NAME --destination-container $CONTAINER_NAME --destination-blob $BLOB_NAME --source-uri $SOURCE_VHD_URL
+sleep 5m
+
+while : ; do
+  status=$(az storage blob show --account-name $STORAGE_ACCOUNT_NAME --container-name vhds --name osdisk2.vhd | jq -r '.properties.copy.status')
+  if [[ $status != "pending" ]]; then
+    break
+  fi
+  echo "Copy pending. Waiting 2 mins."
+  sleep 2m
+done
