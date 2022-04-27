e73eedb0c6ebb95ea549760a50a9c688b1b4a5a2
anujmaheshwari1 <amaheshwari@microsoft.com>
2022-04-27 20:22:35 +0000

    Update image version in official branch

--- pkg/agent/datamodel/sig_config.go
+++ pkg/agent/datamodel/sig_config.go
@@ -214,7 +214,7 @@ const (
 )
 
 const (
-	LinuxSIGImageVersion string = "2022.04.19"
+	LinuxSIGImageVersion string = "2022.04.27"
 
 	Windows2019SIGImageVersion string = "17763.2803.220413"
 	Windows2022SIGImageVersion string = "20348.681.220426"
