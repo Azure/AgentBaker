f9968da025759a419c2674797b65d7f4845062d4
Ace Eldeib <aleldeib@microsoft.com>
2022-05-11 18:45:28 +0000

    exit on err

--- vhdbuilder/packer/list-images.sh
+++ vhdbuilder/packer/list-images.sh
@@ -9,4 +9,5 @@ elif [[ ${CONTAINER_RUNTIME} == "docker" ]]; then
     docker inspect $(docker images -aq) -f '{"id":"{{.ID}}","repoTags":{{json .RepoTags}},"repoDigests":{{json .RepoDigests}}}' | uniq | jq --slurp > /opt/azure/containers/image-bom.json
 else
     echo "Unknown container runtime: ${CONTAINER_RUNTIME}"
+    exit 1
 fi
