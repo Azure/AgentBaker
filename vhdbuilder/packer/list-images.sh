70ba3336186a8231f489c98b2679695ae085e29a
Ace Eldeib <aleldeib@microsoft.com>
2022-05-11 20:09:21 +0000

    tweak format and script path

--- vhdbuilder/packer/list-images.sh
+++ vhdbuilder/packer/list-images.sh
@@ -4,7 +4,7 @@ set -euxo
 CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-docker}"
 
 if [[ ${CONTAINER_RUNTIME} == "containerd" ]]; then
-    crictl images -o json > /opt/azure/containers/image-bom.json
+    crictl images -o json | jq '[.images | .[] | {id:.id, repoTags:.repoTags, repoDigests:.repoDigests}]' > /opt/azure/containers/image-bom.json
 elif [[ ${CONTAINER_RUNTIME} == "docker" ]]; then
     docker inspect $(docker images -aq) -f '{"id":"{{.ID}}","repoTags":{{json .RepoTags}},"repoDigests":{{json .RepoDigests}}}' | uniq | jq --slurp > /opt/azure/containers/image-bom.json
 else
