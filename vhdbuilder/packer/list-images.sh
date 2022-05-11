7282b828cac5501978e44d5a3cc325df5a3e94a7
Ace Eldeib <aleldeib@microsoft.com>
2022-05-11 18:45:18 +0000

    same format

--- vhdbuilder/packer/list-images.sh
+++ vhdbuilder/packer/list-images.sh
@@ -4,9 +4,9 @@ set -euxo
 CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-docker}"
 
 if [[ ${CONTAINER_RUNTIME} == "containerd" ]]; then
-    crictl images -o json | jq '.runtime = "containerd"' > /opt/azure/containers/image-bom.json
+    crictl images -o json > /opt/azure/containers/image-bom.json
 elif [[ ${CONTAINER_RUNTIME} == "docker" ]]; then
-    docker images --all --digests --format "{{json .}}" | jq --slurp | jq '{"images": ., "runtime": "docker"}' > /opt/azure/containers/image-bom.json
+    docker inspect $(docker images -aq) -f '{"id":"{{.ID}}","repoTags":{{json .RepoTags}},"repoDigests":{{json .RepoDigests}}}' | uniq | jq --slurp > /opt/azure/containers/image-bom.json
 else
     echo "Unknown container runtime: ${CONTAINER_RUNTIME}"
 fi
