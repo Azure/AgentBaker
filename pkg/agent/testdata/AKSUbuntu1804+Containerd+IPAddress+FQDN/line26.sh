f7e681bd7ac5f12984d02220bf3532e015918a9d
Ace Eldeib <aleldeib@microsoft.com>
2022-05-19 22:07:38 +0000

    use PS4

--- pkg/agent/testdata/AKSUbuntu1804+Containerd+IPAddress+FQDN/line26.sh
+++ pkg/agent/testdata/AKSUbuntu1804+Containerd+IPAddress+FQDN/line26.sh
@@ -1,5 +1,5 @@
 CSE_STARTTIME=$(date)
-/bin/bash /opt/azure/containers/provision.sh | ts '[%Y-%m-%d %H:%M:%S]' >> /var/log/azure/cluster-provision.log 2>&1
+PS4="+\t " /bin/bash /opt/azure/containers/provision.sh >> /var/log/azure/cluster-provision.log 2>&1
 EXIT_CODE=$?
 systemctl --no-pager -l status kubelet >> /var/log/azure/cluster-provision-cse-output.log 2>&1
 OUTPUT=$(tail -c 3000 "/var/log/azure/cluster-provision.log")
