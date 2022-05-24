c653cb8e32104062b6bae5e9fe36d8e8363d3d5d
Alexander Eldeib <aleldeib@microsoft.com>
2022-05-24 02:56:55 +0200

    one more...

--- vhdbuilder/scripts/linux/ubuntu/tool_installs_ubuntu.sh
+++ vhdbuilder/scripts/linux/ubuntu/tool_installs_ubuntu.sh
@@ -95,6 +95,33 @@ setupGpuRunfileInstall() {
     umount -l /usr/lib/x86_64-linux-gnu
 }
 
+installGPUDriversRun() {
+    {{- /* there is no file under the module folder, the installation failed, so clean up the dirty directory
+    when you upgrade the GPU driver version, please help check whether the retry installation issue is gone,
+    if yes please help remove the clean up logic here too */}}
+    set -x
+    MODULE_NAME="nvidia"
+    NVIDIA_DKMS_DIR="/var/lib/dkms/${MODULE_NAME}/${GPU_DV}"
+    KERNEL_NAME=$(uname -r)
+    if [ -d "${NVIDIA_DKMS_DIR}" ]; then
+        if [ -x "$(command -v dkms)" ]; then
+          dkms remove -m ${MODULE_NAME} -v ${GPU_DV} -k ${KERNEL_NAME}
+        else
+          rm -rf "${NVIDIA_DKMS_DIR}"
+        fi
+    fi
+    {{- /* we need to append the date to the end of the file because the retry will override the log file */}}
+    local log_file_name="/var/log/nvidia-installer-$(date +%s).log"
+    if [ ! -f "${GPU_DEST}/nvidia-drivers-${GPU_DV}" ]; then
+        downloadGPUDrivers
+    fi
+    sh $GPU_DEST/nvidia-drivers-$GPU_DV -s \
+        -k=$KERNEL_NAME \
+        --log-file-name=${log_file_name} \
+        -a --no-drm --dkms --utility-prefix="${GPU_DEST}" --opengl-prefix="${GPU_DEST}"
+    exit $?
+}
+
 disableNtpAndTimesyncdInstallChrony() {
     # Disable systemd-timesyncd
     systemctl_stop 20 30 120 systemd-timesyncd || exit $ERR_STOP_OR_DISABLE_SYSTEMD_TIMESYNCD_TIMEOUT
