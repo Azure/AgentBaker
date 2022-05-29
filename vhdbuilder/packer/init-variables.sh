9f62e474e7ed668f995dfe212bd1b916e554d4ca
Charlie Li <charlili@microsoft.com>
2022-05-28 22:01:51 -0700

    randomize resource names to reduce conflicts

--- vhdbuilder/packer/init-variables.sh
+++ vhdbuilder/packer/init-variables.sh
@@ -5,8 +5,11 @@ CDIR=$(dirname "${BASH_SOURCE}")
 SETTINGS_JSON="${SETTINGS_JSON:-./packer/settings.json}"
 SP_JSON="${SP_JSON:-./packer/sp.json}"
 SUBSCRIPTION_ID="${SUBSCRIPTION_ID:-$(az account show -o json --query="id" | tr -d '"')}"
-CREATE_TIME="$(($(date +%s)+($RANDOM%300)))"
+CREATE_TIME="$(date +%s)"
 STORAGE_ACCOUNT_NAME="aksimages${CREATE_TIME}$RANDOM"
+# Before this PR, packer captured Gen2 SIG version and converted VHD name use pattern "1.0.${CREATE_TIME}", CREATE_TIME is in second
+# multiple Gen2 builds in a pipleline can conflict each other, use pattern 1.${CREATE_TIME}.$RANDOM to reduce conflicts probability
+GEN2_CAPTURED_SIG_VERSION="1.${CREATE_TIME}.$RANDOM"
 
 echo "Subscription ID: ${SUBSCRIPTION_ID}"
 echo "Service Principal Path: ${SP_JSON}"
@@ -102,7 +105,7 @@ if [[ "$MODE" == "gen2Mode" ]]; then
 fi
 
 if [[ ${ARCHITECTURE,,} == "arm64" ]]; then
-  ARM64_OS_DISK_SNAPSHOT_NAME="arm64_os_disk_snapshot_${CREATE_TIME}"
+  ARM64_OS_DISK_SNAPSHOT_NAME="arm64_osdisk_snapshot_${CREATE_TIME}_$RANDOM"
   SIG_IMAGE_NAME=${SIG_IMAGE_NAME//./}Arm64
   # Only az published after April 06 2022 supports --architecture for command 'az sig image-definition create...'
   azversion=$(az version | jq '."azure-cli"' | tr -d '"')
@@ -291,6 +294,7 @@ cat <<EOF > vhdbuilder/packer/settings.json
   "imported_image_name": "${IMPORTED_IMAGE_NAME}",
   "sig_image_name":  "${SIG_IMAGE_NAME}",
   "arm64_os_disk_snapshot_name": "${ARM64_OS_DISK_SNAPSHOT_NAME}",
+  "gen2_captured_sig_version": "${GEN2_CAPTURED_SIG_VERSION}",
   "os_disk_size_gb": "${os_disk_size_gb}"
 }
 EOF
