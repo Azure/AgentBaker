aefbabc21edea526cda68cbf032a06d7e8f96b41
anujmaheshwari1 <anujmaheshwari600@gmail.com>
2022-06-14 10:33:59 -0700

    change osstate to generalized

--- vhdbuilder/packer/init-variables.sh
+++ vhdbuilder/packer/init-variables.sh
@@ -159,7 +159,6 @@ if [[ "$MODE" == "sigMode" || "$MODE" == "gen2Mode" ]]; then
 				--offer ${SIG_GALLERY_NAME} \
 				--sku ${SIG_IMAGE_NAME} \
 				--os-type ${OS_TYPE} \
-				--os-state Specialized \
 				--features SecurityType=ConfidentialVMSupported \
 				--hyper-v-generation ${HYPERV_GENERATION} \
 				--location ${AZURE_LOCATION}
