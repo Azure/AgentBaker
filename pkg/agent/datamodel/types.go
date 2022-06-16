2e1859ebfd5b27532d9ddd92e3cc49170ef0585f
anujmaheshwari1 <amaheshwari@microsoft.com>
2022-06-16 09:28:32 -0700

    add sig image def for 2004 cvm

--- pkg/agent/datamodel/types.go
+++ pkg/agent/datamodel/types.go
@@ -143,6 +143,7 @@ const (
 	AKSUbuntuArm64Containerd1804Gen2   Distro = "aks-ubuntu-arm64-containerd-18.04-gen2"
 	AKSUbuntuContainerd2004            Distro = "aks-ubuntu-containerd-20.04"
 	AKSUbuntuContainerd2004Gen2        Distro = "aks-ubuntu-containerd-20.04-gen2"
+	AKSUbuntuContainerd2004CVMGen2	   Distro = "aks-ubuntu-containerd-20.04-cvm-gen2"
 	RHEL                               Distro = "rhel"
 	CoreOS                             Distro = "coreos"
 	AKS1604Deprecated                  Distro = "aks"      // deprecated AKS 16.04 distro. Equivalent to aks-ubuntu-16.04.
@@ -184,6 +185,7 @@ var AKSDistrosAvailableOnVHD []Distro = []Distro{
 	AKSUbuntuArm64Containerd1804Gen2,
 	AKSUbuntuContainerd2004,
 	AKSUbuntuContainerd2004Gen2,
+	AKSUbuntuContainerd2004CVMGen2,
 }
 
 type CustomConfigurationComponent string
