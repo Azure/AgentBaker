544ef3aa33086c3f62d1409628d60e664b5a3998
Alexander Eldeib <aleldeib@microsoft.com>
2022-06-17 22:59:30 +0200

    fix: unbound var in containerd install

--- pkg/agent/testdata/AKSUbuntu1804+Containerd+MIG/line48.sh
+++ pkg/agent/testdata/AKSUbuntu1804+Containerd+MIG/line48.sh
@@ -160,6 +160,7 @@ installStandaloneContainerd() {
         removeContainerd
         # if containerd version has been overriden then there should exist a local .deb file for it on aks VHDs (best-effort)
         # if no files found then try fetching from packages.microsoft repo
+        CONTAINERD_DEB_FILE="$(ls ${CONTAINERD_DOWNLOADS_DIR}/moby-containerd*)"
         if [[ -f "${CONTAINERD_DEB_FILE}" ]]; then
             installDebPackageFromFile ${CONTAINERD_DEB_FILE} || exit $ERR_CONTAINERD_INSTALL_TIMEOUT
             return 0
