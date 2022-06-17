a766b728690b66b04b5e9bdffe55935a17e4bd39
Alexander Eldeib <aleldeib@microsoft.com>
2022-06-17 17:48:52 +0200

    try to fix deb file issue?

--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -4459,13 +4459,16 @@ installStandaloneContainerd() {
         # if containerd version has been overriden then there should exist a local .deb file for it on aks VHDs (best-effort)
         # if no files found then try fetching from packages.microsoft repo
         CONTAINERD_DEB_TMP="moby-containerd_${CONTAINERD_VERSION/-/\~}+azure-ubuntu18.04u${CONTAINERD_PATCH_VERSION}_${CPU_ARCH}.deb"
-        CONTAINERD_DEB_FILE="$CONTAINERD_DOWNLOADS_DIR/${CONTAINERD_DEB_TMP}"
+        CONTAINERD_DEB_FILE="${CONTAINERD_DEB_TMP}"
         if [[ -f "${CONTAINERD_DEB_FILE}" ]]; then
             installDebPackageFromFile ${CONTAINERD_DEB_FILE} || exit $ERR_CONTAINERD_INSTALL_TIMEOUT
             return 0
-        fi 
+        fi
+        ls $CONTAINERD_DOWNLOADS_DIR
         downloadContainerdFromVersion ${CONTAINERD_VERSION} ${CONTAINERD_PATCH_VERSION}
+        pushd $CONTAINERD_DOWNLOADS_DIR || exit $ERR_CONTAINERD_INSTALL_TIMEOUT
         installDebPackageFromFile ${CONTAINERD_DEB_FILE} || exit $ERR_CONTAINERD_INSTALL_TIMEOUT
+        popd || exit $ERR_CONTAINERD_INSTALL_TIMEOUT
         return 0
     fi
 }
