bc6d7b4ca4cbbde0ebf24d4dd2c539f66cbb175a
wanqingfu <wanqingfu@microsoft.com>
2022-06-20 13:51:20 +0800

    ci: update json set dry run to false managed img to sig

--- vhdbuilder/packer/convert-sig-to-classic-storage-account-blob.sh
+++ vhdbuilder/packer/convert-sig-to-classic-storage-account-blob.sh
@@ -35,12 +35,19 @@ az resource create --id $disk_resource_id  --is-full-object --location $LOCATION
 # shellcheck disable=SC2102
 sas=$(az disk grant-access --ids $disk_resource_id --duration-in-seconds 3600 --query [accessSas] -o tsv)
 
+echo "Before coping"
+az image show -n 2019-containerd -g aksvhdbuilderrg
+az image show -n 2022-containerd -g aksvhdbuilderrg
 azcopy-preview copy "${sas}" "${CLASSIC_BLOB}/1.0.${CREATE_TIME}.vhd${CLASSIC_SAS_TOKEN}" --recursive=true
 
 echo "Converted $sig_resource_id to ${CLASSIC_BLOB}/1.0.${CREATE_TIME}.vhd"
-
+echo "After converting and before revoking access"
+az image show -n 2019-containerd -g aksvhdbuilderrg
+az image show -n 2022-containerd -g aksvhdbuilderrg
 az disk revoke-access --ids $disk_resource_id 
 
 az resource delete --ids $disk_resource_id
-
+echo "After deleting disk resource"
+az image show -n 2019-containerd -g aksvhdbuilderrg
+az image show -n 2022-containerd -g aksvhdbuilderrg
 echo "Deleted $disk_resource_id"
