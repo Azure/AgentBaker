398e0805135ef5dd0a7068edf0bcffe828cdcadc
Patrick W. Healy <patrick.healy@microsoft.com>
2022-06-26 07:55:45 -0500

    remove typo

--- parts/linux/cloud-init/artifacts/cse_main.sh
+++ parts/linux/cloud-init/artifacts/cse_main.sh
@@ -84,7 +84,7 @@ if [ -f $VHD_LOGS_FILEPATH ]; then
     cleanUpContainerImages
     FULL_INSTALL_REQUIRED=false
 else
-    if [[ "${IS_VHD}" = true ]]; tmhen
+    if [[ "${IS_VHD}" = true ]]; then
         echo "Using VHD distro but file $VHD_LOGS_FILEPATH not found"
         exit $ERR_VHD_FILE_NOT_FOUND
     fi
