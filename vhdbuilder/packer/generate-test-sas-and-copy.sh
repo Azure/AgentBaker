77e18eff068e2e53eef7f77eb3904467a5168237
wanqingfu <wanqingfu@microsoft.com>
2022-06-27 12:29:41 +0800

    ci: test azcopy

--- vhdbuilder/packer/generate-test-sas-and-copy.sh
+++ vhdbuilder/packer/generate-test-sas-and-copy.sh
@@ -4,4 +4,8 @@ TEST_CREATE_TIME="$(date +%s)"
 TEST_STORAGE_ACCOUNT_NAME="aksimages${TEST_CREATE_TIME}$RANDOM"
 TEST_EXPIRY_DATE=$(date -u -d "180 minutes" '+%Y-%m-%dT%H:%MZ')
 TEST_SAS_TOKEN=$(az storage account generate-sas --account-name $TEST_STORAGE_ACCOUNT_NAME --permissions r --resource-types o --services b --expiry ${TEST_EXPIRY_DATE} | tr -d '"')
-azcopy-preview copy "${OS_DISK_SAS}" "${CLASSIC_BLOB}${TEST_SAS_TOKEN}" --recursive=true
+
+TEST_IMAGE_NAME="windows-${TEST_CREATE_TIME}-${RANDOM}"
+
+TEST_IMAGE_URL="https://${TEST_STORAGE_ACCOUNT_NAME}.blob.core.windows.net/system/${TEST_IMAGE_NAME}.vhd"
+azcopy-preview copy "${OS_DISK_SAS}" "${TEST_IMAGE_URL}${TEST_SAS_TOKEN}" --recursive=true
