5f391805fab65a98196d6716bf567eac7d3b8844
Cameron Meissner <cameissner@microsoft.com>
2022-06-29 15:35:43 -0700

    bumping timeout for cse execution to 15m

--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -2361,7 +2361,7 @@ func linuxCloudInitArtifactsCse_send_logsPy() (*asset, error) {
 }
 
 var _linuxCloudInitArtifactsCse_startSh = []byte(`CSE_STARTTIME=$(date)
-timeout -k5s 14m /bin/bash /opt/azure/containers/provision.sh >> /var/log/azure/cluster-provision.log 2>&1
+timeout -k5s 15m /bin/bash /opt/azure/containers/provision.sh >> /var/log/azure/cluster-provision.log 2>&1
 EXIT_CODE=$?
 systemctl --no-pager -l status kubelet >> /var/log/azure/cluster-provision-cse-output.log 2>&1
 OUTPUT=$(tail -c 3000 "/var/log/azure/cluster-provision.log")
