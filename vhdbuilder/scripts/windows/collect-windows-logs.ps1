8cfecd99e6580ee2812403609780282cc44e7da0
Abel Hu <abelch@microsoft.com>
2022-07-04 16:04:01 +0800

    feat: Collect disk usage in the Windows node

--- vhdbuilder/scripts/windows/collect-windows-logs.ps1
+++ vhdbuilder/scripts/windows/collect-windows-logs.ps1
@@ -178,6 +178,11 @@ else {
   Write-Host "Calico logs not available"
 }
 
+Write-Host "Collecting disk usage"
+$tempDiskUsageFile = Join-Path ([System.IO.Path]::GetTempPath()) ("disk-usage.txt")
+Get-CimInstance -Class CIM_LogicalDisk | Select-Object @{Name="Size(GB)";Expression={$_.size/1gb}}, @{Name="Free Space(GB)";Expression={$_.freespace/1gb}}, @{Name="Free (%)";Expression={"{0,6:P0}" -f(($_.freespace/1gb) / ($_.size/1gb))}}, DeviceID, DriveType | Where-Object DriveType -EQ '3' > $tempDiskUsageFile
+$paths += $tempDiskUsageFile
+
 Write-Host "Compressing all logs to $zipName"
 $paths | Format-Table FullName, Length -AutoSize
 Compress-Archive -LiteralPath $paths -DestinationPath $zipName
