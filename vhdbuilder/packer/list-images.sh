ca0fa686180bb8fee774504acc98805d12b78032
Alexander Eldeib <aleldeib@microsoft.com>
2022-07-25 23:48:26 +0200

    fix docker list

--- vhdbuilder/packer/list-images.sh
+++ vhdbuilder/packer/list-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eux
+set -euxo pipefail
 
 CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-docker}"
 
@@ -7,21 +7,7 @@ if [[ ${CONTAINER_RUNTIME} == "containerd" ]]; then
     crictl images -o json
     crictl images -o json | jq -c '[.images | .[] | {id:.id, repoTags:.repoTags, repoDigests:.repoDigests}]' > /opt/azure/containers/image-bom.json
 elif [[ ${CONTAINER_RUNTIME} == "docker" ]]; then
-    set +x
-    docker images -a
-    sleep 5
-    echo "output without filter"
-    docker inspect $(docker images -aq) -f '{{json .}}'
-    echo $?
-    docker inspect $(docker images -aq) -f '{{json .}}' | jq .
-    echo $?
-    sleep 5
-    echo "output with filter"
-    docker inspect $(docker images -aq) -f '{"id":"{{.ID}}","repoTags":{{json .RepoTags}},"repoDigests":{{json .RepoDigests}}}' 
-    sleep 5
-    echo "final output"
     docker inspect $(docker images -aq) -f '{"id":"{{.ID}}","repoTags":{{json .RepoTags}},"repoDigests":{{json .RepoDigests}}}' | jq --slurp . > /opt/azure/containers/image-bom.json
-
 else
     echo "Unknown container runtime: ${CONTAINER_RUNTIME}"
     exit 1
