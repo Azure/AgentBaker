fe7c0be5278aacb02140de89678f6806699daf0b
Alexander Eldeib <aleldeib@microsoft.com>
2022-07-25 22:08:09 +0200

    debug

--- vhdbuilder/packer/list-images.sh
+++ vhdbuilder/packer/list-images.sh
@@ -7,9 +7,16 @@ if [[ ${CONTAINER_RUNTIME} == "containerd" ]]; then
     crictl images -o json
     crictl images -o json | jq -c '[.images | .[] | {id:.id, repoTags:.repoTags, repoDigests:.repoDigests}]' > /opt/azure/containers/image-bom.json
 elif [[ ${CONTAINER_RUNTIME} == "docker" ]]; then
+    set +x
     docker images -a
+    sleep 5
+    echo "output without filter"
+    docker inspect $(docker images -aq) -f '{{json .}}'
     docker inspect $(docker images -aq) -f '{{json .}}' | jq
+    sleep 5
+    echo "output with filter"
     docker inspect $(docker images -aq) -f '{"id":"{{.ID}}","repoTags":{{json .RepoTags}},"repoDigests":{{json .RepoDigests}}}' | jq --slurp > /opt/azure/containers/image-bom.json
+
 else
     echo "Unknown container runtime: ${CONTAINER_RUNTIME}"
     exit 1
