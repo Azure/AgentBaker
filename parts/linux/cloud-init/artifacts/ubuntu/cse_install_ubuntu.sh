fe0347bed7efe94cc89fc79bdf743d0cfa5c3465
yirali <yirali@microsoft.com>
2022-08-01 11:25:11 -0700

    fix indentation

--- parts/linux/cloud-init/artifacts/ubuntu/cse_install_ubuntu.sh
+++ parts/linux/cloud-init/artifacts/ubuntu/cse_install_ubuntu.sh
@@ -102,7 +102,7 @@ updateAptWithMicrosoftPkg() {
     if [[ $(isARM64) == 1 ]]; then
         if [ "${UBUNTU_RELEASE}" == "22.04" ]; then
                 retrycmd_if_failure_no_stats 120 5 25 curl https://packages.microsoft.com/config/ubuntu/${UBUNTU_RELEASE}/prod.list > /tmp/microsoft-prod.list || exit $ERR_MOBY_APT_LIST_TIMEOUT
-            else
+        else
                 retrycmd_if_failure_no_stats 120 5 25 curl https://packages.microsoft.com/config/ubuntu/${UBUNTU_RELEASE}/multiarch/prod.list > /tmp/microsoft-prod.list || exit $ERR_MOBY_APT_LIST_TIMEOUT
         fi
     else
