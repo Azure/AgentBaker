f84c4f53734c277e77601e64931f4ceabc6e5818
Alexander Eldeib <aleldeib@microsoft.com>
2022-08-01 21:42:56 +0200

    clean up more?

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -511,6 +511,9 @@ ls -ltr /dev/* | grep sgx >>  ${VHD_LOGS_FILEPATH}
 
 echo -e "=== Installed Packages Begin\n$(listInstalledPackages)\n=== Installed Packages End" >> ${VHD_LOGS_FILEPATH}
 
+ls -ltr /tmp
+rm -rf /tmp/*
+
 echo "Disk usage:" >> ${VHD_LOGS_FILEPATH}
 df -h >> ${VHD_LOGS_FILEPATH}
 # warn at 75% space taken
