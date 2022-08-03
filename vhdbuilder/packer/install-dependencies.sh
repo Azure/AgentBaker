fe4f0544838a3d14795356fb8f15fe06f7824f31
Alexander Eldeib <aleldeib@microsoft.com>
2022-08-03 03:56:31 +0200

    fix: pull first

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -200,8 +200,10 @@ echo "  - bpftrace" >> ${VHD_LOGS_FILEPATH}
 if [[ $OS == $UBUNTU_OS_NAME && $(isARM64) != 1 ]]; then  # no ARM64 SKU with GPU now
   if [[ "${CONTAINER_RUNTIME}" == "containerd" ]]; then
     if grep -q "fullgpu" <<< "$FEATURE_FLAGS"; then
+      ctr image pull docker.io/alexeldeib/aks-gpu:latest
       ctr run --privileged --net-host --with-ns pid:/proc/1/ns/pid --mount type=bind,src=/opt/gpu,dst=/mnt/gpu,options=rbind --mount type=bind,src=/opt/actions,dst=/mnt/actions,options=rbind -t docker.io/alexeldeib/aks-gpu:latest /entrypoint.sh install.sh
     else
+      ctr image pull docker.io/alexeldeib/aks-gpu:latest
       ctr run --privileged --net-host --with-ns pid:/proc/1/ns/pid --mount type=bind,src=/opt/gpu,dst=/mnt/gpu,options=rbind --mount type=bind,src=/opt/actions,dst=/mnt/actions,options=rbind -t docker.io/alexeldeib/aks-gpu:latest /entrypoint.sh copy
     fi
   else
