5d099a480ef22e82c4ee54e8491601721fb381a2
Alexander Eldeib <aleldeib@microsoft.com>
2022-08-09 17:40:30 +0200

    debug disk space

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -524,10 +524,11 @@ echo -e "=== Installed Packages Begin\n$(listInstalledPackages)\n=== Installed P
 
 dpkg-query -W --showformat='${Installed-Size} ${Package}\n' | sort -nr | numfmt --to=iec | head -n 25 >> ${VHD_LOGS_FILEPATH}
 
-apt-get install localepurge
-apt-get autoclean
-apt-get autoremove
-apt-get clean
+df -h
+
+apt-get autoclean -y
+apt-get autoremove -y
+apt-get clean -y
 
 df -h
 
