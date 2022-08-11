f20f045630fd6e547d3da13dbab5d0b932b81959
Alexander Eldeib <aleldeib@microsoft.com>
2022-08-12 01:07:31 +0200

    use azure cni to avoid route table stuff

--- e2e/e2e-starter.sh
+++ e2e/e2e-starter.sh
@@ -37,7 +37,7 @@ fi
 if [ "$create_cluster" == "true" ]; then
     log "Creating cluster"
     clusterCreateStartTime=$(date +%s)
-    az aks create -g $RESOURCE_GROUP_NAME -n $CLUSTER_NAME --node-count 1 --generate-ssh-keys -ojson
+    az aks create -g $RESOURCE_GROUP_NAME -n $CLUSTER_NAME --node-count 1 --generate-ssh-keys --network-plugin azure -ojson
     clusterCreateEndTime=$(date +%s)
     log "Created cluster in $((clusterCreateEndTime-clusterCreateStartTime)) seconds"
 fi
