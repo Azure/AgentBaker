a4061e169de35b7cd3716c8af628e067df3b59f3
Alexander Eldeib <aleldeib@microsoft.com>
2022-08-11 18:44:22 +0200

    add nvidia-docker, switch back to mcr

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -198,10 +198,10 @@ if [[ $OS == $UBUNTU_OS_NAME && $(isARM64) != 1 ]]; then  # no ARM64 SKU with GP
   gpu_action="copy"
   if grep -q "fullgpu" <<< "$FEATURE_FLAGS"; then
     # force older driver on dedicated vhd to avoid build failures.
-    export NVIDIA_DRIVER_IMAGE_TAG="470.57.02"
+    export NVIDIA_DRIVER_IMAGE_TAG="470.57.02-v0.0.2-rev17-sha-ff708e"
     gpu_action="install"
   else
-    export NVIDIA_DRIVER_IMAGE_TAG="510.47.03"
+    export NVIDIA_DRIVER_IMAGE_TAG="510.47.03-v0.0.2-rev17-sha-ff708e"
   fi
 
   mkdir -p /opt/{actions,gpu}
