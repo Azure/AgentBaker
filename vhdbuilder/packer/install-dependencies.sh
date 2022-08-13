d77dafc123d566a99e28f55c5cf8cbb30d5c76c1
Alexander Eldeib <aleldeib@microsoft.com>
2022-08-13 02:54:07 +0200

    try bumping both to 510 one more time

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -196,13 +196,11 @@ fi
 
 if [[ $OS == $UBUNTU_OS_NAME && $(isARM64) != 1 ]]; then  # no ARM64 SKU with GPU now
   gpu_action="copy"
+  export NVIDIA_DRIVER_IMAGE_TAG="510.47.03-v0.0.2-rev17-sha-ff708e"
   if grep -q "fullgpu" <<< "$FEATURE_FLAGS"; then
     # force older driver on dedicated vhd to avoid build failures.
-    export NVIDIA_DRIVER_IMAGE_TAG="470.57.02-v0.0.2-rev17-sha-ff708e"
     gpu_action="install"
   else
-    export NVIDIA_DRIVER_IMAGE_TAG="510.47.03-v0.0.2-rev17-sha-ff708e"
-  fi
 
   mkdir -p /opt/{actions,gpu}
   if [[ "${CONTAINER_RUNTIME}" == "containerd" ]]; then
