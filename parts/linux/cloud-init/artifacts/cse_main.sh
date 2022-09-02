4155a7a83f92f16869391c54dddbf41ec0ccbd1e
Charlie Li <charlili@microsoft.com>
2022-09-02 09:52:19 -0700

    Revert "additional logging for cse duration (#2116)"
    This reverts commit be17af3d23cc36afefbd2fc24c61bfdaf2d3632b.

--- parts/linux/cloud-init/artifacts/cse_main.sh
+++ parts/linux/cloud-init/artifacts/cse_main.sh
@@ -69,7 +69,7 @@ source /etc/os-release
 # Mandb is not currently available on MarinerV1
 if [[ ${ID} != "mariner" ]]; then
     echo "Removing man-db auto-update flag file..."
-    logs_to_events "AKS.CSE.removeManDbAutoUpdateFlagFile" removeManDbAutoUpdateFlagFile
+    removeManDbAutoUpdateFlagFile
 fi
 
 {{- if not NeedsContainerd}}
@@ -77,24 +77,24 @@ cleanUpContainerd
 {{- end}}
 
 if [[ "${GPU_NODE}" != "true" ]]; then
-    logs_to_events "AKS.CSE.cleanUpGPUDrivers" cleanUpGPUDrivers
+    cleanUpGPUDrivers
 fi
 
-logs_to_events "AKS.CSE.disableSystemdResolved" disableSystemdResolved
+disableSystemdResolved
 
-logs_to_events "AKS.CSE.configureAdminUser" configureAdminUser
+configureAdminUser
 
 {{- if NeedsContainerd}}
 # If crictl gets installed then use it as the cri cli instead of ctr
 # crictl is not a critical component so continue with boostrapping if the install fails
 # CLI_TOOL is by default set to "ctr"
-logs_to_events "AKS.CSE.installCrictl" 'installCrictl && CLI_TOOL="crictl"'
+installCrictl && CLI_TOOL="crictl"
 {{- end}}
 
 VHD_LOGS_FILEPATH=/opt/azure/vhd-install.complete
 if [ -f $VHD_LOGS_FILEPATH ]; then
     echo "detected golden image pre-install"
-    logs_to_events "AKS.CSE.cleanUpContainerImages" cleanUpContainerImages
+    cleanUpContainerImages
     FULL_INSTALL_REQUIRED=false
 else
     if [[ "${IS_VHD}" = true ]]; then
@@ -105,42 +105,42 @@ else
 fi
 
 if [[ $OS == $UBUNTU_OS_NAME ]] && [ "$FULL_INSTALL_REQUIRED" = "true" ]; then
-    logs_to_events "AKS.CSE.installDeps" installDeps
+    installDeps
 else
     echo "Golden image; skipping dependencies installation"
 fi
 
-logs_to_events "AKS.CSE.installContainerRuntime" installContainerRuntime
+installContainerRuntime
 {{- if and NeedsContainerd TeleportEnabled}}
-logs_to_events "AKS.CSE.installTeleportdPlugin" installTeleportdPlugin
+installTeleportdPlugin
 {{- end}}
 
 setupCNIDirs
 
-logs_to_events "AKS.CSE.installNetworkPlugin" installNetworkPlugin
+installNetworkPlugin
 
 {{- if IsKrustlet }}
-    logs_to_events "AKS.CSE.downloadKrustlet" downloadKrustlet
+    downloadKrustlet
 {{- end }}
 
 {{- if IsNSeriesSKU}}
 echo $(date),$(hostname), "Start configuring GPU drivers"
 if [[ "${GPU_NODE}" = true ]]; then
-    logs_to_events "AKS.CSE.ensureGPUDrivers" ensureGPUDrivers
+    ensureGPUDrivers
     if [[ "${ENABLE_GPU_DEVICE_PLUGIN_IF_NEEDED}" = true ]]; then
         if [[ "${MIG_NODE}" == "true" ]] && [[ -f "/etc/systemd/system/nvidia-device-plugin.service" ]]; then
-            logs_to_events "AKS.CSE.mig_strategy" "wait_for_file 3600 1 /etc/systemd/system/nvidia-device-plugin.service.d/10-mig_strategy.conf" || exit $ERR_FILE_WATCH_TIMEOUT
+            wait_for_file 3600 1 /etc/systemd/system/nvidia-device-plugin.service.d/10-mig_strategy.conf || exit $ERR_FILE_WATCH_TIMEOUT
         fi
-        logs_to_events "AKS.CSE.start.nvidia-device-plugin" "systemctlEnableAndStart nvidia-device-plugin" || exit $ERR_GPU_DEVICE_PLUGIN_START_FAIL
+        systemctlEnableAndStart nvidia-device-plugin || exit $ERR_GPU_DEVICE_PLUGIN_START_FAIL
     else
-        logs_to_events "AKS.CSE.stop.nvidia-device-plugin" "systemctlDisableAndStop nvidia-device-plugin"
+        systemctlDisableAndStop nvidia-device-plugin
     fi
 fi
 # If it is a MIG Node, enable mig-partition systemd service to create MIG instances
 if [[ "${MIG_NODE}" == "true" ]]; then
     REBOOTREQUIRED=true
-    logs_to_events "AKS.CSE.nvidia-fabricmanager" "systemctlEnableAndStart nvidia-fabricmanager" || exit $ERR_GPU_DRIVERS_START_FAIL
-    logs_to_events "AKS.CSE.ensureMigPartition" ensureMigPartition
+    systemctlEnableAndStart nvidia-fabricmanager || exit $ERR_GPU_DRIVERS_START_FAIL
+    ensureMigPartition
 fi
 
 echo $(date),$(hostname), "End configuring GPU drivers"
@@ -148,46 +148,46 @@ echo $(date),$(hostname), "End configuring GPU drivers"
 
 {{- if and IsDockerContainerRuntime HasPrivateAzureRegistryServer}}
 set +x
-logs_to_events "AKS.CSE.GetPrivateAzureRegistryServer" "docker login -u $SERVICE_PRINCIPAL_CLIENT_ID -p $SERVICE_PRINCIPAL_CLIENT_SECRET {{GetPrivateAzureRegistryServer}}"
+docker login -u $SERVICE_PRINCIPAL_CLIENT_ID -p $SERVICE_PRINCIPAL_CLIENT_SECRET {{GetPrivateAzureRegistryServer}}
 set -x
 {{end}}
 
-logs_to_events "AKS.CSE.installKubeletKubectlAndKubeProxy" installKubeletKubectlAndKubeProxy
+installKubeletKubectlAndKubeProxy
 
-logs_to_events "AKS.CSE.ensureRPC" ensureRPC
+ensureRPC
 
 createKubeManifestDir
 
 {{- if HasDCSeriesSKU}}
 if [[ ${SGX_NODE} == true && ! -e "/dev/sgx" ]]; then
-    logs_to_events "AKS.CSE.installSGXDrivers" installSGXDrivers
+    installSGXDrivers
 fi
 {{end}}
 
 {{- if HasCustomSearchDomain}}
-logs_to_events "AKS.CSE.GetCustomSearchDomainsCSEScriptFilepath" "wait_for_file 3600 1 {{GetCustomSearchDomainsCSEScriptFilepath}}" || exit $ERR_FILE_WATCH_TIMEOUT
+wait_for_file 3600 1 {{GetCustomSearchDomainsCSEScriptFilepath}} || exit $ERR_FILE_WATCH_TIMEOUT
 {{GetCustomSearchDomainsCSEScriptFilepath}} > /opt/azure/containers/setup-custom-search-domain.log 2>&1 || exit $ERR_CUSTOM_SEARCH_DOMAINS_FAIL
 {{end}}
 
-logs_to_events "AKS.CSE.configureK8s" configureK8s
+configureK8s
 
-logs_to_events "AKS.CSE.configureCNI" configureCNI
+configureCNI
 
 {{/* configure and enable dhcpv6 for dual stack feature */}}
 {{- if IsIPv6DualStackFeatureEnabled}}
-logs_to_events "AKS.CSE.ensureDHCPv6" ensureDHCPv6
+ensureDHCPv6
 {{- end}}
 
 {{- if NeedsContainerd}}
-logs_to_events "AKS.CSE.ensureContainerd" ensureContainerd {{/* containerd should not be configured until cni has been configured first */}}
+ensureContainerd {{/* containerd should not be configured until cni has been configured first */}}
 {{- else}}
-logs_to_events "AKS.CSE.ensureDocker" ensureDocker
+ensureDocker
 {{- end}}
 
 # Start the service to synchronize tunnel logs so WALinuxAgent can pick them up
-logs_to_events "AKS.CSE.sync-tunnel-logs" "systemctlEnableAndStart sync-tunnel-logs"
- 
-logs_to_events "AKS.CSE.ensureMonitorService" ensureMonitorService
+systemctlEnableAndStart sync-tunnel-logs
+
+ensureMonitorService
 # must run before kubelet starts to avoid race in container status using wrong image
 # https://github.com/kubernetes/kubernetes/issues/51017
 # can remove when fixed
@@ -196,26 +196,25 @@ if [[ "{{GetTargetEnvironment}}" == "AzureChinaCloud" ]]; then
 fi
 
 {{- if EnableHostsConfigAgent}}
-logs_to_events "AKS.CSE.configPrivateClusterHosts" configPrivateClusterHosts
+configPrivateClusterHosts
 {{- end}}
 
 {{- if ShouldConfigTransparentHugePage}}
-logs_to_events "AKS.CSE.configureTransparentHugePage" configureTransparentHugePage
+configureTransparentHugePage
 {{- end}}
 
 {{- if ShouldConfigSwapFile}}
-logs_to_events "AKS.CSE.configureSwapFile" configureSwapFile
+configureSwapFile
 {{- end}}
 
-logs_to_events "AKS.CSE.ensureSysctl" ensureSysctl
-logs_to_events "AKS.CSE.ensureJournal" ensureJournal
+ensureSysctl
+ensureJournal
 {{- if IsKrustlet}}
-logs_to_events "AKS.CSE.krustlet" "systemctlEnableAndStart krustlet"
+systemctlEnableAndStart krustlet
 {{- else}}
-
-logs_to_events "AKS.CSE.ensureKubelet" ensureKubelet
+ensureKubelet
 {{- if NeedsContainerd}} {{- if and IsKubenet (not HasCalicoNetworkPolicy)}}
-logs_to_events "AKS.CSE.ensureNoDupOnPromiscuBridge" ensureNoDupOnPromiscuBridge
+ensureNoDupOnPromiscuBridge
 {{- end}} {{- end}}
 {{- end}}
 
@@ -231,7 +230,6 @@ fi
 rm -f /etc/apt/apt.conf.d/99periodic
 
 if [[ $OS == $UBUNTU_OS_NAME ]]; then
-    # logs_to_events should not be run on & commands
     apt_get_purge 20 30 120 apache2-utils &
 fi
 
@@ -264,10 +262,10 @@ if ! [[ ${API_SERVER_NAME} =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
             VALIDATION_ERR=$ERR_K8S_API_SERVER_DNS_LOOKUP_FAIL
         fi
     else
-        logs_to_events "AKS.CSE.apiserverNC" "retrycmd_if_failure ${API_SERVER_CONN_RETRIES} 1 10 nc -vz ${API_SERVER_NAME} 443 || time nc -vz ${API_SERVER_NAME} 443" || VALIDATION_ERR=$ERR_K8S_API_SERVER_CONN_FAIL
+        retrycmd_if_failure ${API_SERVER_CONN_RETRIES} 1 10 nc -vz ${API_SERVER_NAME} 443 || time nc -vz ${API_SERVER_NAME} 443 || VALIDATION_ERR=$ERR_K8S_API_SERVER_CONN_FAIL
     fi
 else
-    logs_to_events "AKS.CSE.apiserverNC" "retrycmd_if_failure ${API_SERVER_CONN_RETRIES} 1 10 nc -vz ${API_SERVER_NAME} 443 || time nc -vz ${API_SERVER_NAME} 443" || VALIDATION_ERR=$ERR_K8S_API_SERVER_CONN_FAIL
+    retrycmd_if_failure ${API_SERVER_CONN_RETRIES} 1 10 nc -vz ${API_SERVER_NAME} 443 || time nc -vz ${API_SERVER_NAME} 443 || VALIDATION_ERR=$ERR_K8S_API_SERVER_CONN_FAIL
 fi
 
 if [[ ${ID} != "mariner" ]]; then
@@ -282,12 +280,10 @@ if $REBOOTREQUIRED; then
     echo 'reboot required, rebooting node in 1 minute'
     /bin/bash -c "shutdown -r 1 &"
     if [[ $OS == $UBUNTU_OS_NAME ]]; then
-        # logs_to_events should not be run on & commands
         aptmarkWALinuxAgent unhold &
     fi
 else
     if [[ $OS == $UBUNTU_OS_NAME ]]; then
-        # logs_to_events should not be run on & commands
         /usr/lib/apt/apt.systemd.daily &
         aptmarkWALinuxAgent unhold &
     fi
