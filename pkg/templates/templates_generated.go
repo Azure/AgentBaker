5ae7e1f8d02a30a34167001294501858da91063f
Cameron Meissner <cameissner@microsoft.com>
2022-09-05 11:19:01 -0700

    fixed runc install version regex

--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -4712,10 +4712,10 @@ installStandaloneContainerd() {
 downloadContainerdFromVersion() {
     CONTAINERD_VERSION=$1
     mkdir -p $CONTAINERD_DOWNLOADS_DIR
-    # # Adding updateAptWithMicrosoftPkg since AB e2e uses an older image version with uncached containerd 1.6 so it needs to download from testing repo.
-    # # And RP no image pull e2e has apt update restrictions that prevent calls to packages.microsoft.com in CSE
-    # # This won't be called for new VHDs as they have containerd 1.6 cached
-    # updateAptWithMicrosoftPkg 
+    # Adding updateAptWithMicrosoftPkg since AB e2e uses an older image version with uncached containerd 1.6 so it needs to download from testing repo.
+    # And RP no image pull e2e has apt update restrictions that prevent calls to packages.microsoft.com in CSE
+    # This won't be called for new VHDs as they have containerd 1.6 cached
+    updateAptWithMicrosoftPkg 
     apt_get_download 20 30 moby-containerd=${CONTAINERD_VERSION}* || exit $ERR_CONTAINERD_INSTALL_TIMEOUT
     cp -al ${APT_CACHE_DIR}moby-containerd_${CONTAINERD_VERSION}* $CONTAINERD_DOWNLOADS_DIR/ || exit $ERR_CONTAINERD_INSTALL_TIMEOUT
 }
@@ -4728,16 +4728,11 @@ downloadContainerdFromURL() {
     CONTAINERD_DEB_FILE="$CONTAINERD_DOWNLOADS_DIR/${CONTAINERD_DEB_TMP}"
 }
 
-downloadRuncFromVersionAndCPUArch() {
+downloadRuncFromVersion() {
     local RUNC_VERSION=$1
-    local CPU_ARCH=$2
     mkdir -p $RUNC_DOWNLOADS_DIR
-    # Adding updateAptWithMicrosoftPkg since AB e2e uses an older image version with uncached containerd 1.6 so it needs to download from testing repo.
-    # And RP no image pull e2e has apt update restrictions that prevent calls to packages.microsoft.com in CSE
-    # This also fixes an issue where the required moby-runc package cannot be found on Ubuntu 16.04.
-    updateAptWithMicrosoftPkg 
-    apt_get_download 20 30 moby-runc=${RUNC_VERSION} || exit $ERR_RUNC_DOWNLOAD_TIMEOUT
-    cp -al ${APT_CACHE_DIR}moby-runc_${RUNC_VERSION}+azure-*_${CPU_ARCH}.deb $RUNC_DOWNLOADS_DIR || exit $ERR_RUNC_DOWNLOAD_TIMEOUT
+    apt_get_download 20 30 moby-runc=${RUNC_VERSION}* || exit $ERR_RUNC_DOWNLOAD_TIMEOUT
+    cp -al ${APT_CACHE_DIR}moby-runc_${RUNC_VERSION}* $RUNC_DOWNLOADS_DIR || exit $ERR_RUNC_DOWNLOAD_TIMEOUT
 }
 
 downloadAndInstallMobyDockerPackagesFromVersion() {
@@ -4804,7 +4799,7 @@ ensureRunc() {
     RUNC_DEB_PATTERN="moby-runc_${TARGET_VERSION/-/\~}+azure-*_${CPU_ARCH}.deb"
     RUNC_DEB_FILE=$(find ${RUNC_DOWNLOADS_DIR} -type f -iname "${RUNC_DEB_PATTERN}" | sort -V | tail -n1)
     if [[ -z "${RUNC_DEB_FILE}" ]]; then
-        downloadRuncFromVersionAndCPUArch ${TARGET_VERSION/-/\~}* $CPU_ARCH
+        downloadRuncFromVersion ${TARGET_VERSION/-/\~}
         RUNC_DEB_FILE=$(find ${RUNC_DOWNLOADS_DIR} -type f -iname "${RUNC_DEB_PATTERN}" | sort -V | tail -n1)
         if [[ -z "${RUNC_DEB_FILE}" ]]; then
             echo "Failed to locate cached moby-runc deb"
