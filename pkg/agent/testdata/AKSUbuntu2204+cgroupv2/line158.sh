5c206f3918ed7a7059aa41231811311f20bc9bbe
anujmaheshwari1 <amaheshwari@microsoft.com>
2022-09-13 11:53:29 -0700

    rebase and regenerate

--- pkg/agent/testdata/AKSUbuntu2204+cgroupv2/line158.sh
+++ pkg/agent/testdata/AKSUbuntu2204+cgroupv2/line158.sh
@@ -1,2 +1,2 @@
 [Service]
-Environment="KUBELET_CONTAINERD_FLAGS=--container-runtime=remote --runtime-request-timeout=15m --container-runtime-endpoint=unix:///run/containerd/containerd.sock --runtime-cgroups=/system.slice/containerd.service"
+Environment="KUBELET_CGROUP_FLAGS=--cgroup-driver=systemd"
