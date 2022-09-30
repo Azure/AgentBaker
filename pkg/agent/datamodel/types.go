2296799afeaefcb2e65874996a869bd2ee86695e
xinywa <xinywa@microsoft.com>
2022-09-30 11:46:10 -0700

    remove labels in agentbaker

--- pkg/agent/datamodel/types.go
+++ pkg/agent/datamodel/types.go
@@ -1026,34 +1026,8 @@ func (a *AgentPoolProfile) IsAvailabilitySets() bool {
 // GetKubernetesLabels returns a k8s API-compliant labels string for nodes in this profile
 func (a *AgentPoolProfile) GetKubernetesLabels(rg string, deprecated bool, nvidiaEnabled bool, fipsEnabled bool, osSku string) string {
 	var buf bytes.Buffer
-	buf.WriteString("kubernetes.azure.com/role=agent")
-	if deprecated {
-		buf.WriteString(",node-role.kubernetes.io/agent=")
-		buf.WriteString(",kubernetes.io/role=agent")
-	}
-	// label key agentpool will be depreated soon
-	buf.WriteString(fmt.Sprintf(",agentpool=%s", a.Name))
-	buf.WriteString(fmt.Sprintf(",kubernetes.azure.com/agentpool=%s", a.Name))
+	buf.WriteString(fmt.Sprintf("kubernetes.azure.com/agentpool=%s", a.Name))
 
-	if strings.EqualFold(a.StorageProfile, ManagedDisks) {
-		storagetier, _ := GetStorageAccountType(a.VMSize)
-		// label key storageprofile and storagetier will be depreated soon
-		buf.WriteString(fmt.Sprintf(",storageprofile=managed,storagetier=%s", storagetier))
-		buf.WriteString(fmt.Sprintf(",kubernetes.azure.com/storageprofile=managed,kubernetes.azure.com/storagetier=%s", storagetier))
-	}
-	if nvidiaEnabled {
-		accelerator := "nvidia"
-		// label key accelerator will be depreated soon
-		buf.WriteString(fmt.Sprintf(",accelerator=%s", accelerator))
-		buf.WriteString(fmt.Sprintf(",kubernetes.azure.com/accelerator=%s", accelerator))
-	}
-	if fipsEnabled {
-		buf.WriteString(",kubernetes.azure.com/fips_enabled=true")
-	}
-	if osSku != "" {
-		buf.WriteString(fmt.Sprintf(",kubernetes.azure.com/os-sku=%s", osSku))
-	}
-	buf.WriteString(fmt.Sprintf(",kubernetes.azure.com/cluster=%s", rg))
 	keys := []string{}
 	for key := range a.CustomNodeLabels {
 		keys = append(keys, key)
