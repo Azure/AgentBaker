4b2476ff14a6cb0588bcf1d3d5daff67c9769f9d
anujmaheshwari1 <amaheshwari@microsoft.com>
2022-10-04 16:54:34 -0700

    add security type for TL

--- vhdbuilder/packer/convert-sig-to-classic-storage-account-blob.sh
+++ vhdbuilder/packer/convert-sig-to-classic-storage-account-blob.sh
@@ -37,6 +37,7 @@ disk_resource_id="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GR
 az resource create --id $disk_resource_id  --is-full-object --location $LOCATION --properties "{\"location\": \"$LOCATION\", \
   \"properties\": { \
     \"osType\": \"$OS_TYPE\", \
+    \"securityType\": \"TrustedLaunch\", \
     \"creationData\": { \
       \"createOption\": \"FromImage\", \
       \"galleryImageReference\": { \
