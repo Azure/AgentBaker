e103fa167004a9c733ec1a3ebb04d40a5e55d8bc
anujmaheshwari1 <amaheshwari@microsoft.com>
2022-10-13 22:20:11 -0700

    fix operator

--- vhdbuilder/packer/test/run-test.sh
+++ vhdbuilder/packer/test/run-test.sh
@@ -75,7 +75,7 @@ else
   TARGET_COMMAND_STRING=""
   if [[ "${ARCHITECTURE,,}" == "arm64" ]]; then
     TARGET_COMMAND_STRING+="--size Standard_D2pds_v5"
-  elif [[ "$OS_TYPE" == "Linux" && ${ENABLE_TRUSTED_LAUNCH}" == "True" ]]; then
+  elif [[ "${OS_TYPE}" == "Linux" && "${ENABLE_TRUSTED_LAUNCH}" == "True" ]]; then
     TARGET_COMMAND_STRING+="--security-type TrustedLaunch --enable-secure-boot true --enable-vtpm true"
   fi
 
