05850bb4aa0473c55bc215d5751826dd8bd7971a
anujmaheshwari1 <anujmaheshwari600@gmail.com>
2022-10-14 11:25:10 -0700

    add TL sig image configs (#2325)
    * add TL sig image configs
    
    * fix UT
    
    Co-authored-by: anujmaheshwari1 <amaheshwari@microsoft.com>

--- pkg/agent/datamodel/types.go
+++ pkg/agent/datamodel/types.go
@@ -154,6 +154,7 @@ const (
 	AKSUbuntuContainerd2004CVMGen2     Distro = "aks-ubuntu-containerd-20.04-cvm-gen2"
 	AKSUbuntuArm64Containerd2204Gen2   Distro = "aks-ubuntu-arm64-containerd-22.04-gen2"
 	AKSCBLMarinerV2Arm64Gen2           Distro = "aks-cblmariner-v2-arm64-gen2"
+	AKSUbuntuContainerd2204TLGen2      Distro = "aks-ubuntu-containerd-22.04-tl-gen2"
 	RHEL                               Distro = "rhel"
 	CoreOS                             Distro = "coreos"
 	AKS1604Deprecated                  Distro = "aks"      // deprecated AKS 16.04 distro. Equivalent to aks-ubuntu-16.04.
@@ -202,6 +203,7 @@ var AKSDistrosAvailableOnVHD []Distro = []Distro{
 	AKSUbuntuContainerd2004CVMGen2,
 	AKSUbuntuArm64Containerd2204Gen2,
 	AKSCBLMarinerV2Arm64Gen2,
+	AKSUbuntuContainerd2204TLGen2,
 }
 
 type CustomConfigurationComponent string
