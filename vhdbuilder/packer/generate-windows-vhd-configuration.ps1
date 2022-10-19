31622645ff0abc5bdd0e79ef9d512fa21ce018f4
wanqingfu <wanqingfu@microsoft.com>
2022-10-19 09:11:00 +0800

    fix: update path of networkhealth.ps1

--- vhdbuilder/packer/generate-windows-vhd-configuration.ps1
+++ vhdbuilder/packer/generate-windows-vhd-configuration.ps1
@@ -128,7 +128,7 @@ $global:map = @{
         "https://github.com/microsoft/SDN/raw/master/Kubernetes/windows/debug/starthnstrace.ps1",
         "https://github.com/microsoft/SDN/raw/master/Kubernetes/windows/debug/startpacketcapture.ps1",
         "https://github.com/Microsoft/SDN/raw/master/Kubernetes/windows/debug/VFP.psm1",
-        "https://github.com/Microsoft/SDN/raw/master/Kubernetes/windows/debug/networkhealth.ps1",
+        "https://github.com/microsoft/SDN/blob/master/Kubernetes/windows/debug/networkmonitor/networkhealth.ps1",
         "https://github.com/microsoft/SDN/raw/master/Kubernetes/windows/helper.psm1",
         "https://github.com/Microsoft/SDN/raw/master/Kubernetes/windows/hns.psm1",
         "https://github.com/Microsoft/SDN/raw/master/Kubernetes/windows/hns.v2.psm1",
