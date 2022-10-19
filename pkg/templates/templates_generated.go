c401a718f002fe51ecde8a652c66d2db7899105d
Mikolaj Umanski <mumanski@microsoft.com>
2022-10-19 14:58:52 -0700

    Add some logs, silence update certs output

--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -5053,22 +5053,26 @@ certDestination=/usr/local/share/ca-certificates/certs
 cp -a "$certSource"/. "$certDestination"
 
 if [[ -z $(ls -A "$certSource") ]]; then
+  echo "Source dir "$certSource" was empty, attempting to remove cert files"
   ls "$certDestination" | grep -E '^[0-9]{14}' | while read -r line; do
+    echo "removing "$line" in "$certDestination""
     rm $certDestination/"$line"
   done
 else
+  echo "found cert files in "$certSource""
   certsToCopy=(${certSource}/*)
   currIterationCertFile=${certsToCopy[0]##*/}
   currIterationTag=${currIterationCertFile:0:14}
   for file in "$certDestination"/*.crt; do
       currFile=${file##*/}
      if [[ "${currFile:0:14}" != "${currIterationTag}" && -f "${file}" ]]; then
+          echo "removing "$file" in "$certDestination""
           rm "${file}"
      fi
   done
 fi
 
-update-ca-certificates -f`)
+update-ca-certificates -f 1> /dev/null`)
 
 func linuxCloudInitArtifactsUpdate_certsShBytes() ([]byte, error) {
 	return _linuxCloudInitArtifactsUpdate_certsSh, nil
