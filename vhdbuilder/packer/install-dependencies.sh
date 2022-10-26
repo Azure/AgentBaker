7d3bf51b138170cf95269997a5afe4df0f6ab0a5
Cameron Meissner <cameissner@microsoft.com>
2022-10-26 09:44:17 -0700

    typo in pasted comment

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -577,7 +577,7 @@ dd if=/dev/zero of=/EMPTY bs=1M  || echo "dd exit code $? is suppressed"
 rm -f /EMPTY
 
 # Make sure we wait until all the data is written to disk, otherwise
-# Packer might quite too early before the large files are deleted
+# Packer might quit too early before the large files are deleted
 sync
 
 echo "Disk usage after final cleanup:" >> ${VHD_LOGS_FILEPATH}
