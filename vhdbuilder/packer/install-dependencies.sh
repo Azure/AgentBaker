78154dfeeadb3de0e378e262fdc6900ed80efe89
Cameron Meissner <cameissner@microsoft.com>
2022-11-01 15:41:45 -0700

    resolve dangling merge conflict

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -41,7 +41,7 @@ systemctlEnableAndStart disk_queue || exit 1
 mkdir /opt/certs
 chmod 666 /opt/certs
 systemctlEnableAndStart update_certs.path || exit 1
-systemctlEnableAndStart update_certs.timer || exit 1
+systemctlEnableAndStart update_certs.timer || exit
 
 systemctlEnableAndStart ci-syslog-watcher.path || exit 1
 systemctlEnableAndStart ci-syslog-watcher.service || exit 1
@@ -559,7 +559,3 @@ if [[ ${UBUNTU_RELEASE} == "18.04" || ${UBUNTU_RELEASE} == "22.04" ]]; then
 fi
 
 
-<<<<<<< HEAD
-fi
-=======
->>>>>>> c297f716528a8f617b259bdfad9a56fb54ed4bb9
