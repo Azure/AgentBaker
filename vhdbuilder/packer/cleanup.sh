f36a4206367895f6212aa2659cccda594bf5fa79
Cameron Meissner <cameissner@microsoft.com>
2022-11-03 15:20:54 -0700

    expand list of image to delete, delete 15 SIG versions per definition

--- vhdbuilder/packer/cleanup.sh
+++ vhdbuilder/packer/cleanup.sh
@@ -145,7 +145,7 @@ if [[ -n "${AZURE_RESOURCE_GROUP_NAME}" && "${DRY_RUN,,}" == "false" ]]; then
   sig_version_ids=""
   # we limit deletions to 25 of each resource type to make sure the build doesn't take too long and can finish successfully
   for image in $(az image list -g ${AZURE_RESOURCE_GROUP_NAME} | jq --arg dl $deadline '.[] | select(.tags.now < $dl).name' | head -n 25 | tr -d '\"' || ""); do
-    if [[ $image = Ubuntu* ]] || [[ $image = CBLMariner* ]] || [[ $image = 1804* ]] || [[ $image = 2004* ]] || [[ $image = 2204* ]]; then
+    if [[ $image = Ubuntu* ]] || [[ $image = CBLMariner* ]] || [[ image = V1* ]] || [[ image = V2* ]] || [[ $image = 1804* ]] || [[ $image = 2004* ]] || [[ $image = 2204* ]]; then
       managed_image_ids="${managed_image_ids} /subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${AZURE_RESOURCE_GROUP_NAME}/providers/Microsoft.Compute/images/${image}"
       sig_version_ids="${sig_version_ids} /subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${AZURE_RESOURCE_GROUP_NAME}/providers/Microsoft.Compute/galleries/${SIG_GALLERY_NAME}/images/${image%-*}/versions/${image#*-}"
       echo "Will delete managed image ${image} and associated SIG version from resource group ${AZURE_RESOURCE_GROUP_NAME}"
@@ -167,8 +167,8 @@ if [[ -n "${AZURE_RESOURCE_GROUP_NAME}" && "${DRY_RUN,,}" == "false" ]]; then
   fi
 
   old_sig_version_ids=""
-  for image_definition in $(az sig image-definition list -g ${AZURE_RESOURCE_GROUP_NAME} -r ${SIG_GALLERY_NAME} | jq '.[] | select(.name | test("Ubuntu*|CBLMariner*|1804*|2004*|2204*")).name' | tr -d '\"' || ""); do
-    for image_version in $(az sig image-version list -g ${AZURE_RESOURCE_GROUP_NAME} -r ${SIG_GALLERY_NAME} -i ${image_definition} | jq --arg dl $deadline '.[] | select(.tags.now < $dl).name' | head -n 25 | tr -d '\"' || ""); do
+  for image_definition in $(az sig image-definition list -g ${AZURE_RESOURCE_GROUP_NAME} -r ${SIG_GALLERY_NAME} | jq '.[] | select(.name | test("Ubuntu*|CBLMariner*|V1*|V2*|1804*|2004*|2204*")).name' | tr -d '\"' || ""); do
+    for image_version in $(az sig image-version list -g ${AZURE_RESOURCE_GROUP_NAME} -r ${SIG_GALLERY_NAME} -i ${image_definition} | jq --arg dl $deadline '.[] | select(.tags.now < $dl).name' | head -n 15 | tr -d '\"' || ""); do
       old_sig_version_ids="${old_sig_version_ids} /subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${AZURE_RESOURCE_GROUP_NAME}/providers/Microsoft.Compute/galleries/${SIG_GALLERY_NAME}/images/${image_definition}/versions/${image_version}"
     done
   done
