7ebecfafb29658db570da83ad74b22c1c3d3b759
Alexander Eldeib <aleldeib@microsoft.com>
2022-11-13 18:59:32 +0000

    add pivot

--- pkg/agent/baker.go
+++ pkg/agent/baker.go
@@ -357,6 +357,9 @@ func getContainerServiceFuncMap(config *datamodel.NodeBootstrappingConfiguration
 		"GetTLSBootstrapTokenForKubeConfig": func() string {
 			return GetTLSBootstrapTokenForKubeConfig(config.KubeletClientTLSBootstrapToken)
 		},
+		"EnableSecureTLSBootstrapping": func() bool {
+			return config.EnableSecureTLSBootstrapping
+		},
 		"GetKubeletConfigKeyVals": func() string {
 			return GetOrderedKubeletConfigFlagString(config.KubeletConfig, cs, profile, config.EnableKubeletConfigFile)
 		},
