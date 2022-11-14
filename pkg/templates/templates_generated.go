4ee654e9ec6637ff95e55b83c28452e03b28b2ac
Chou Hu <abelch@microsoft.com>
2022-11-14 17:01:07 +0800

    feat: Support collecting AKS Windows logs in GuestVMLogs (#2435)
    * feat: Support collecting AKS Windows logs in GuestVMLogs
    
    * fix: Remove wrong code

--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -6705,6 +6705,9 @@ $fipsEnabled = [System.Convert]::ToBoolean("{{ FIPSEnabled }}")
 # HNS remediator
 $global:HNSRemediatorIntervalInMinutes = [System.Convert]::ToUInt32("{{GetHnsRemediatorIntervalInMinutes}}");
 
+# Log generator
+$global:LogGeneratorIntervalInMinutes = [System.Convert]::ToUInt32("{{GetLogGeneratorIntervalInMinutes}}");
+
 $global:EnableIncreaseDynamicPortRange = $false
 
 if ($useContainerD) {
@@ -6994,6 +6997,8 @@ try
         Remove-Item $kubeConfigFile
     }
 
+    Enable-GuestVMLogs -IntervalInMinutes $global:LogGeneratorIntervalInMinutes
+
     if ($global:IsNotRebootWindowsNode) {
         Write-Log "Setup Complete, starting NodeResetScriptTask to register Winodws node without reboot"
         Start-ScheduledTask -TaskName "k8s-restart-job"
@@ -7036,6 +7041,8 @@ finally
 
     # Flush stdout to C:\AzureData\CustomDataSetupScript.log
     [Console]::Out.Flush()
+
+    Upload-GuestVMLogs -ExitCode $global:ExitCode
 }
 `)
 
