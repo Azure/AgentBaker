7ea3f5d836063808a0264bd68431631023a8d393
Cameron Meissner <cameissner@microsoft.com>
2022-11-21 12:16:38 -0800

    reboot vm after fips kernel install step and before kernel/packeges cleanup step

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 OS=$(sort -r /etc/*-release | gawk 'match($0, /^(ID_LIKE=(coreos)|ID=(.*))$/, a) { print toupper(a[2] a[3]); exit }')
-OS_VERSION=$(sort -r /etc/*-release | gawk 'match($0, /^(VERSION_ID=(.*))$/, a) { print toupper(a[2] a[3]); exit }' | tr -d '"')
 UBUNTU_OS_NAME="UBUNTU"
 MARINER_OS_NAME="MARINER"
 THIS_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)"
-
-#the following sed removes all comments of the format {{/* */}}
-sed -i 's/{{\/\*[^*]*\*\/}}//g' /home/packer/provision_source.sh
-sed -i 's/{{\/\*[^*]*\*\/}}//g' /home/packer/tool_installs_distro.sh
 
 source /home/packer/provision_installs.sh
 source /home/packer/provision_installs_distro.sh
@@ -21,41 +16,6 @@ source /home/packer/packer_source.sh
 CPU_ARCH=$(getCPUArch)  #amd64 or arm64
 VHD_LOGS_FILEPATH=/opt/azure/vhd-install.complete
 COMPONENTS_FILEPATH=/opt/azure/components.json
-MANIFEST_FILEPATH=/opt/azure/manifest.json
-KUBE_PROXY_IMAGES_FILEPATH=/opt/azure/kube-proxy-images.json
-#this is used by post build test to check whether the compoenents do indeed exist
-cat components.json > ${COMPONENTS_FILEPATH}
-cat manifest.json > ${MANIFEST_FILEPATH}
-cat ${THIS_DIR}/kube-proxy-images.json > ${KUBE_PROXY_IMAGES_FILEPATH}
-echo "Starting build on " $(date) > ${VHD_LOGS_FILEPATH}
-
-if [[ $OS == $MARINER_OS_NAME ]]; then
-  chmod 755 /opt
-  chmod 755 /opt/azure
-  chmod 644 ${VHD_LOGS_FILEPATH}
-fi
-
-copyPackerFiles
-systemctlEnableAndStart disk_queue || exit 1
-
-mkdir /opt/certs
-chmod 666 /opt/certs
-systemctlEnableAndStart update_certs.path || exit 1
-systemctlEnableAndStart update_certs.timer || exit 1
-
-systemctlEnableAndStart ci-syslog-watcher.path || exit 1
-systemctlEnableAndStart ci-syslog-watcher.service || exit 1
-
-# enable the modified logrotate service and remove the auto-generated default logrotate cron job if present
-systemctlEnableAndStart logrotate.timer || exit 1
-rm -f /etc/cron.daily/logrotate
-
-if [[ ${UBUNTU_RELEASE} == "18.04" && ${ENABLE_FIPS,,} == "true" ]]; then
-  installFIPS
-elif [[ ${ENABLE_FIPS,,} == "true" ]]; then
-  echo "AKS enables FIPS on Ubuntu 18.04 only, exiting..."
-  exit 1
-fi
 
 echo ""
 echo "Components downloaded in this VHD build (some of the below components might get deleted during cluster provisioning if they are not needed):" >> ${VHD_LOGS_FILEPATH}
@@ -487,20 +447,6 @@ for KUBE_PROXY_IMAGE_VERSION in ${KUBE_PROXY_IMAGE_VERSIONS}; do
   echo "  - ${CONTAINER_IMAGE}" >>${VHD_LOGS_FILEPATH}
 done
 
-if [[ $OS == $UBUNTU_OS_NAME ]]; then
-  # remove snapd, which is not used by container stack
-  apt_get_purge 20 30 120 snapd || exit 1
-  apt_get_purge 20 30 120 apache2-utils || exit 1
-
-  apt-get -y autoclean || exit 1
-  apt-get -y autoremove --purge || exit 1
-  apt-get -y clean || exit 1
-  # update message-of-the-day to start after multi-user.target
-  # multi-user.target usually start at the end of the boot sequence
-  sed -i 's/After=network-online.target/After=multi-user.target/g' /lib/systemd/system/motd-news.service
-fi
-
-
 # kubelet and kubectl
 # need to cover previously supported version for VMAS scale up scenario
 # So keeping as many versions as we can - those unsupported version can be removed when we don't have enough space
@@ -517,53 +463,3 @@ for PATCHED_KUBE_BINARY_VERSION in ${KUBE_BINARY_VERSIONS}; do
   KUBERNETES_VERSION=$(echo ${PATCHED_KUBE_BINARY_VERSION} | cut -d"_" -f1 | cut -d"-" -f1 | cut -d"." -f1,2,3)
   extractKubeBinaries $KUBERNETES_VERSION "https://acs-mirror.azureedge.net/kubernetes/v${PATCHED_KUBE_BINARY_VERSION}/binaries/kubernetes-node-linux-${CPU_ARCH}.tar.gz"
 done
-
-if [[ $OS == $UBUNTU_OS_NAME ]]; then
-  # remove apport
-  apt-get purge --auto-remove apport open-vm-tools -y
-fi
-
-# shellcheck disable=SC2129
-echo "kubelet/kubectl downloaded:" >> ${VHD_LOGS_FILEPATH}
-ls -ltr /usr/local/bin/* >> ${VHD_LOGS_FILEPATH}
-
-# shellcheck disable=SC2010
-ls -ltr /dev/* | grep sgx >>  ${VHD_LOGS_FILEPATH} 
-
-echo -e "=== Installed Packages Begin\n$(listInstalledPackages)\n=== Installed Packages End" >> ${VHD_LOGS_FILEPATH}
-
-echo "Disk usage:" >> ${VHD_LOGS_FILEPATH}
-df -h >> ${VHD_LOGS_FILEPATH}
-
-
-# warn at 75% space taken
-[ -s $(df -P | grep '/dev/sda1' | awk '0+$5 >= 75 {print}') ] || echo "WARNING: 75% of /dev/sda1 is used" >> ${VHD_LOGS_FILEPATH}
-# error at 99% space taken
-[ -s $(df -P | grep '/dev/sda1' | awk '0+$5 >= 99 {print}') ] || exit 1
-
-echo "Using kernel:" >> ${VHD_LOGS_FILEPATH}
-tee -a ${VHD_LOGS_FILEPATH} < /proc/version
-{
-  echo "Install completed successfully on " $(date)
-  echo "VSTS Build NUMBER: ${BUILD_NUMBER}"
-  echo "VSTS Build ID: ${BUILD_ID}"
-  echo "Commit: ${COMMIT}"
-  echo "Ubuntu version: ${UBUNTU_RELEASE}"
-  echo "Hyperv generation: ${HYPERV_GENERATION}"
-  echo "Feature flags: ${FEATURE_FLAGS}"
-  echo "Container runtime: ${CONTAINER_RUNTIME}"
-  echo "FIPS enabled: ${ENABLE_FIPS}"
-} >> ${VHD_LOGS_FILEPATH}
-
-if [[ $(isARM64) != 1 ]]; then
-  # no asc-baseline-1.1.0-268.arm64.deb
-  installAscBaseline
-fi
-
-if [[ ${UBUNTU_RELEASE} == "18.04" || ${UBUNTU_RELEASE} == "22.04" ]]; then
-  if [[ ${ENABLE_FIPS,,} == "true" || ${CPU_ARCH} == "arm64" ]]; then
-    relinkResolvConf
-  fi
-fi
-
-
