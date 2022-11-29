04b13e5f6a058cd40047fda3352ef25479055147
anujmaheshwari1 <amaheshwari@microsoft.com>
2022-11-29 12:11:18 -0800

    update mariner sig count

--- pkg/agent/datamodel/sig_config_test.go
+++ pkg/agent/datamodel/sig_config_test.go
@@ -47,7 +47,7 @@ var _ = Describe("GetSIGAzureCloudSpecConfig", func() {
 		Expect(aksUbuntuGPU1804Gen2.Definition).To(Equal("1804gen2gpu"))
 		Expect(aksUbuntuGPU1804Gen2.Version).To(Equal("2022.08.29"))
 
-		Expect(len(sigConfig.SigCBLMarinerImageConfig)).To(Equal(4))
+		Expect(len(sigConfig.SigCBLMarinerImageConfig)).To(Equal(5))
 
 		mariner := sigConfig.SigCBLMarinerImageConfig[AKSCBLMarinerV1]
 		Expect(mariner.ResourceGroup).To(Equal("resourcegroup"))
