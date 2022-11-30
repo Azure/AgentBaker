6d586e76723c8631ee9c9b5c394670e94d5cd643
Junjie Zhang <junjiezhang@microsoft.com>
2022-11-30 11:16:55 +0000

    test

--- e2e/e2e-create-windows-nodepool.sh
+++ e2e/e2e-create-windows-nodepool.sh
@@ -9,16 +9,16 @@ log "Starting to create windows nodepool"
 windowsNodepool="win19"
 export windowsNodepool
 
-az aks nodepool add --resource-group $RESOURCE_GROUP_NAME --cluster-name $CLUSTER_NAME --name $windowsNodepool --os-type Windows --node-count 1
+# az aks nodepool add --resource-group $RESOURCE_GROUP_NAME --cluster-name $CLUSTER_NAME --name $windowsNodepool --os-type Windows --node-count 1
 
 log "Created windows nodepool"
-# out=$(az aks nodepool list --cluster-name $CLUSTER_NAME -g $RESOURCE_GROUP_NAME | grep "$windowsNodepool")
+out=$(az aks nodepool list --cluster-name $CLUSTER_NAME -g $RESOURCE_GROUP_NAME | grep "$windowsNodepool")
 
-# if [ -z "$out"]; then
-#     log "Creating windows nodepool"
-#     az aks nodepool add --resource-group $RESOURCE_GROUP_NAME --cluster-name $CLUSTER_NAME --name $windowsNodepool --os-type Windows --node-count 1
-#     log "Created windows nodepool"
-# else
-#     log "Already create windows nodepool"
-# fi
+if [ "$out" == "" ]; then
+    log "Creating windows nodepool"
+    az aks nodepool add --resource-group $RESOURCE_GROUP_NAME --cluster-name $CLUSTER_NAME --name $windowsNodepool --os-type Windows --node-count 1
+    log "Created windows nodepool"
+else
+    log "Already create windows nodepool"
+fi
 
