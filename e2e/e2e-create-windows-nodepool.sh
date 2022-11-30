e031d01c46f5c8304b3674c1733ad4f432115355
Junjie Zhang <junjiezhang@microsoft.com>
2022-11-30 11:28:53 +0000

    test

--- e2e/e2e-create-windows-nodepool.sh
+++ e2e/e2e-create-windows-nodepool.sh
@@ -14,7 +14,7 @@ export windowsNodepool
 log "Created windows nodepool"
 out=$(az aks nodepool list --cluster-name $CLUSTER_NAME -g $RESOURCE_GROUP_NAME | jq '.[1].name')
 
-if [ "$out" == "" ]; then
+if [ "$out" == null ]; then
     log "Creating windows nodepool"
     az aks nodepool add --resource-group $RESOURCE_GROUP_NAME --cluster-name $CLUSTER_NAME --name $windowsNodepool --os-type Windows --node-count 1
     log "Created windows nodepool"
