deb4da724fe53dc7fdee2a9e47594d6091e2c1c4
Cameron Meissner <cameissner@microsoft.com>
2022-11-29 16:33:46 -0800

    use msi identity to test check-in pipelines

--- packer.mk
+++ packer.mk
@@ -80,7 +80,7 @@ init-packer:
 	@./vhdbuilder/packer/init-variables.sh
 
 az-login:
-	az login --service-principal -u ${CLIENT_ID} -p ${CLIENT_SECRET} --tenant ${TENANT_ID}
+	# az login --service-principal -u ${CLIENT_ID} -p ${CLIENT_SECRET} --tenant ${TENANT_ID}
 	az account set -s ${SUBSCRIPTION_ID}
 
 run-packer: az-login
