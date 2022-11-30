13a76ee8580f8e95c7cc6f8b6c7959b6ffa0d862
Junjie Zhang <junjiezhang@microsoft.com>
2022-11-30 10:59:30 +0000

    test

--- e2e/e2e-create-windows-nodepool.sh
+++ e2e/e2e-create-windows-nodepool.sh
@@ -4,13 +4,14 @@ set -euxo pipefail
 
 source e2e-helper.sh
 
-log "Starting to check windows nodepool"
+log "Starting to create windows nodepool"
 
 windowsNodepool="win19"
 export windowsNodepool
 
-az aks nodepool list --cluster-name $CLUSTER_NAME -g $RESOURCE_GROUP_NAME | grep "$windowsNodepool"
+az aks nodepool add --resource-group $RESOURCE_GROUP_NAME --cluster-name $CLUSTER_NAME --name $windowsNodepool --os-type Windows --node-count 1
 
+log "Created windows nodepool"
 # out=$(az aks nodepool list --cluster-name $CLUSTER_NAME -g $RESOURCE_GROUP_NAME | grep "$windowsNodepool")
 
 # if [ -z "$out"]; then
