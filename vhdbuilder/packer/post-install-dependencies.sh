502383821c5c9f8cbe06dd93a1b7aae29402acf3
Cameron <cameronmeissner@gmail.com>
2022-12-08 09:15:48 -0800

    reboot packer VM multiple times during VHD builds (#2452)
    * bring in 1es changes
    
    * rename vnet
    
    * rename hostedpool
    
    * added vnet/subnet config to packer builder
    
    * add vnet resource group name, add vnet variables to top of template
    
    * typo
    
    * debug mode for packer build
    
    * vanilla 1es
    
    * bring back vnet/subnet config
    
    * add networking configs to marinerv1 template
    
    * increase packer ssh timeout to 3h for debugging
    
    * nodesigtest -> nodetest for debugging
    
    * nodesigtest -> nodetest for debugging
    
    * nodetest -> nodesigtest in release.yaml
    
    * reverting...
    
    * remove vnet resource group name from mariner case
    
    * old prefix for mariner
    
    * updating packer templates based on bicep template changes
    
    * update vnet name
    
    * trying to pivot hosted pool based off System.DefinitionId
    
    * trying different method
    
    * Update .vsts-vhd-builder-release.yaml for Azure Pipelines
    
    * Update .vsts-vhd-builder-release.yaml for Azure Pipelines
    
    * Update .vsts-vhd-builder-release.yaml for Azure Pipelines
    
    * Update .vsts-vhd-builder-release.yaml for Azure Pipelines
    
    * Update .vsts-vhd-builder-release.yaml for Azure Pipelines
    
    * Update .vsts-vhd-builder-release.yaml for Azure Pipelines
    
    * using dynamic poolName
    
    * packer.mk: remove debug flag, add explicit logging for template file being used, using msi identity
    
    * use POOL_NAME variable
    
    * use POOL_NAME variable
    
    * reboot vm after fips kernel install step and before kernel/packeges cleanup step
    
    * bring back purge before kube binary installs
    
    * moved some source/import calls around
    
    * bring back provision installs
    
    * fixing imports
    
    * working for fips images? added logging on reboot shell
    
    * remove cse imports
    
    * added logging
    
    * add sleep 60 after reboot
    
    * expect_disconnect on deps installation steps
    
    * print out network info after reboot
    
    * add ifconfig at top of each step's script
    
    * ifconfig looks good, adding ssh timeouts/pauses
    
    * remove ifconfigs
    
    * previous commit seems to have everything working, updating remaining templates accordingly
    
    * upload new scripts to other vhd builds
    
    * OS_VERSION in install-deps
    
    * cleanup
    
    * strip old kernels/packages in post-install-dependencies step
    
    * increase pause_after time for kata
    
    * purge/clean if ubuntu, increase pause_after on kata again
    
    * typo
    
    * refactor
    
    * move enableMarinerKata call after reboots
    
    * move enableMarinerKata call after reboots
    
    * revert az-login
    
    * add strip kernel step to post-install-dependencies
    
    * shellcheck 2021
    
    * shellcheck disable 2021
    
    * move old kernel stripping step to ubuntu case
    
    * addressing comments
    
    * bring back enableMarinerKata before reboots, use nested-virt-enabled VM size for kata builds for now
    
    Co-authored-by: Cameron Meissner <cameissner@microsoft.com>

---
+++ vhdbuilder/packer/post-install-dependencies.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+OS=$(sort -r /etc/*-release | gawk 'match($0, /^(ID_LIKE=(coreos)|ID=(.*))$/, a) { print toupper(a[2] a[3]); exit }')
+UBUNTU_OS_NAME="UBUNTU"
+
+source /home/packer/provision_installs.sh
+source /home/packer/provision_installs_distro.sh
+source /home/packer/provision_source.sh
+source /home/packer/provision_source_distro.sh
+source /home/packer/tool_installs.sh
+source /home/packer/tool_installs_distro.sh
+
+CPU_ARCH=$(getCPUArch)  #amd64 or arm64
+VHD_LOGS_FILEPATH=/opt/azure/vhd-install.complete
+
+if [[ $OS == $UBUNTU_OS_NAME ]]; then
+  # shellcheck disable=SC2021
+  current_kernel="$(uname -r | cut -d- -f-2)"
+  dpkg --get-selections | grep -e "linux-\(headers\|modules\|image\)" | grep -v "$current_kernel" | tr -s '[[:space:]]' | tr '\t' ' ' | cut -d' ' -f1 | xargs -I{} apt-get remove -yq {}
+
+  # remove apport
+  apt-get purge --auto-remove apport open-vm-tools -y
+
+  # strip old kernels/packages
+  apt-get -y autoclean || exit 1
+  apt-get -y autoremove --purge || exit 1
+  apt-get -y clean || exit 1
+fi
+
+# shellcheck disable=SC2129
+echo "kubelet/kubectl downloaded:" >> ${VHD_LOGS_FILEPATH}
+ls -ltr /usr/local/bin/* >> ${VHD_LOGS_FILEPATH}
+
+# shellcheck disable=SC2010
+ls -ltr /dev/* | grep sgx >>  ${VHD_LOGS_FILEPATH} 
+
+echo -e "=== Installed Packages Begin\n$(listInstalledPackages)\n=== Installed Packages End" >> ${VHD_LOGS_FILEPATH}
+
+echo "Disk usage:" >> ${VHD_LOGS_FILEPATH}
+df -h >> ${VHD_LOGS_FILEPATH}
+
+
+# warn at 75% space taken
+[ -s $(df -P | grep '/dev/sda1' | awk '0+$5 >= 75 {print}') ] || echo "WARNING: 75% of /dev/sda1 is used" >> ${VHD_LOGS_FILEPATH}
+# error at 99% space taken
+[ -s $(df -P | grep '/dev/sda1' | awk '0+$5 >= 99 {print}') ] || exit 1
+
+echo "Using kernel:" >> ${VHD_LOGS_FILEPATH}
+tee -a ${VHD_LOGS_FILEPATH} < /proc/version
+{
+  echo "Install completed successfully on " $(date)
+  echo "VSTS Build NUMBER: ${BUILD_NUMBER}"
+  echo "VSTS Build ID: ${BUILD_ID}"
+  echo "Commit: ${COMMIT}"
+  echo "Ubuntu version: ${UBUNTU_RELEASE}"
+  echo "Hyperv generation: ${HYPERV_GENERATION}"
+  echo "Feature flags: ${FEATURE_FLAGS}"
+  echo "Container runtime: ${CONTAINER_RUNTIME}"
+  echo "FIPS enabled: ${ENABLE_FIPS}"
+} >> ${VHD_LOGS_FILEPATH}
+
+if [[ $(isARM64) != 1 ]]; then
+  # no asc-baseline-1.1.0-268.arm64.deb
+  installAscBaseline
+fi
+
+if [[ ${UBUNTU_RELEASE} == "18.04" || ${UBUNTU_RELEASE} == "22.04" ]]; then
+  if [[ ${ENABLE_FIPS,,} == "true" || ${CPU_ARCH} == "arm64" ]]; then
+    relinkResolvConf
+  fi
+fi
+
+echo "post-install-dependencies step completed successfully"
