727fc9edada0682bb389dffe2b41480c4c75638e
Ace Eldeib <aleldeib@microsoft.com>
2022-12-09 09:42:58 -0500

    fix: add timeout and panic recovery handler for all absvc routes (#2496) (#2497)

--- apiserver/getlatestsigimageconfig.go
+++ apiserver/getlatestsigimageconfig.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 
@@ -22,47 +23,36 @@ func (api *APIServer) GetLatestSigImageConfig(w http.ResponseWriter, r *http.Req
 	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
 	defer cancel()
 
-	processResult := make(chan Result)
-	go func() {
-		var config datamodel.GetLatestSigImageConfigRequest
+	var config datamodel.GetLatestSigImageConfigRequest
 
-		err := json.NewDecoder(r.Body).Decode(&config)
-		if err != nil {
-			handleError(err)
-			return
-		}
-
-		agentBaker, err := agent.NewAgentBaker()
-		if err != nil {
-			processResult <- handleError(err)
-			return
-		}
-
-		latestSigConfig, err := agentBaker.GetLatestSigImageConfig(config.SIGConfig, config.Region, config.Distro)
-		if err != nil {
-			processResult <- handleError(err)
-			return
-		}
-
-		result, err := json.Marshal(latestSigConfig)
-		if err != nil {
-			processResult <- handleError(err)
-			return
-		}
-
-		processResult <- Result{string(result), nil}
-	}()
-
-	select {
-	case <-ctx.Done():
-		http.Error(w, "process timeout", http.StatusInternalServerError)
-	case result := <-processResult:
-		if result.Error != nil {
-			http.Error(w, result.Error.Error(), http.StatusBadRequest)
-		} else {
-			w.WriteHeader(http.StatusOK)
-			fmt.Fprint(w, result.Message)
-		}
-
+	err := json.NewDecoder(r.Body).Decode(&config)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
+
+	agentBaker, err := agent.NewAgentBaker()
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	latestSigConfig, err := agentBaker.GetLatestSigImageConfig(config.SIGConfig, config.Region, config.Distro)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	result, err := json.Marshal(latestSigConfig)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, string(result))
 }
--- apiserver/routers.go
+++ apiserver/routers.go
@@ -3,7 +3,9 @@ package apiserver
 import (
 	"context"
 	"net/http"
+	"time"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -22,7 +24,6 @@ type Routes []Route
 func (api *APIServer) NewRouter(ctx context.Context) *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 
-	// healthz should not require authentication
 	router.
 		Methods("POST").
 		Path(RoutePathNodeBootstrapData).
@@ -41,5 +42,29 @@ func (api *APIServer) NewRouter(ctx context.Context) *mux.Router {
 		Name("GetDistroSigImageConfig").
 		HandlerFunc(api.GetDistroSigImageConfig)
 
+	router.Methods("GET").Path("/healthz").Name("healthz").HandlerFunc(healthz)
+
+	// global timeout and panic handlers.
+	router.Use(timeoutHandler(), recoveryHandler())
+
 	return router
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	handleOK(w, r)
+}
+
+func handleOK(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+}
+
+func recoveryHandler() mux.MiddlewareFunc {
+	return handlers.RecoveryHandler(handlers.PrintRecoveryStack(true))
+}
+
+func timeoutHandler() mux.MiddlewareFunc {
+	return func(h http.Handler) http.Handler {
+		return http.TimeoutHandler(h, time.Second*30, "")
+	}
 }
