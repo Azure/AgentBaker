752be347d3d6108d27a5f1a34434a5b56f5202e4
Pablo Trivino <pablotrivino@microsoft.com>
2022-12-12 13:15:04 -0800

    Removed extra file

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -230,7 +230,6 @@ fi
 if [[ $OS == $UBUNTU_OS_NAME && $(isARM64) != 1 ]]; then  # no ARM64 SKU with GPU now
   gpu_action="install"
   export NVIDIA_DRIVER_IMAGE_TAG="cuda-510.47.03-${NVIDIA_DRIVER_IMAGE_SHA}"
-  echo "$NVIDIA_DRIVER_IMAGE:$NVIDIA_DRIVER_IMAGE_TAG" > driver-ver.txt
 
   mkdir -p /opt/{actions,gpu}
   if [[ "${CONTAINER_RUNTIME}" == "containerd" ]]; then
