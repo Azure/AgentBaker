876341936ea4720ef33a47b048cc4bda7ff1f3d3
Junjie Zhang <junjiezhang@microsoft.com>
2022-12-13 08:47:41 +0000

    create windows e2e pipeline

---
+++ e2e/e2e-create-windows-nodepool.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+source e2e-helper.sh
+
+log "Starting to create windows nodepool"
+
+# windowsNodepool="win19"
+# export windowsNodepool
+
+# az aks nodepool add --resource-group $RESOURCE_GROUP_NAME --cluster-name $CLUSTER_NAME --name $windowsNodepool --os-type Windows --node-count 1
+
+log "Created windows nodepool"
+out=$(az aks nodepool list --cluster-name $CLUSTER_NAME -g $RESOURCE_GROUP_NAME | jq '.[1].name')
+
+if [ "$out" == null ]; then
+    log "Creating windows nodepool"
+    az aks nodepool add --resource-group $RESOURCE_GROUP_NAME --cluster-name $CLUSTER_NAME --name $WINDOWS_NODEPOOL --os-type Windows --node-count 1
+    log "Created windows nodepool"
+else
+    log "Already create windows nodepool"
+fi
