059cdc2f809c1bbfaa84899b72df9c26aca18a68
Junjie Zhang <junjiezhang@microsoft.com>
2022-12-19 04:02:28 +0000

    fix bug

--- staging/cse/windows/cse.tests.ps1
+++ staging/cse/windows/cse.tests.ps1
@@ -1,3 +1,3 @@
 Invoke-Pester -Output Detailed $PSScriptRoot\azurecnifunc.tests.ps1
 Invoke-Pester -Output Detailed $PSScriptRoot\configfunc.tests.ps1
-Invoke-Pester -Output Detailed $PSScriptRoot\..\..\..\parts\windows\windowscsehelper.test.ps1
+Invoke-Pester -Output Detailed $PSScriptRoot\..\..\..\parts\windows\windowscsehelper.tests.ps1
