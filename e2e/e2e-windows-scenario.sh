962d226f46f8c9ed3dea0d8f6543ca532017e472
Junjie Zhang <junjiezhang@microsoft.com>
2022-12-28 13:28:37 +0000

    test

--- e2e/e2e-windows-scenario.sh
+++ e2e/e2e-windows-scenario.sh
@@ -102,7 +102,9 @@ else
         --resource-group $MC_RESOURCE_GROUP_NAME \
         --name $DEPLOYMENT_VMSS_NAME \
         --instance-id $VMSS_INSTANCE_ID \
-        --scripts 'Invoke-WebRequest -UseBasicParsing https://aka.ms/downloadazcopy-v10-windows -OutFile azcopy.zip;expand-archive azcopy.zip;cd .\azcopy\*;.\azcopy.exe copy "C:\azuredata\CustomDataSetupScript.log" "https://abe2ecselog.blob.core.windows.net/cselogs/" + ${DEPLOYMENT_VMSS_NAME} + "-cse.log?" + $token'
+        --scripts 'param([string]$arg1,[string]$arg2)' \
+        'Invoke-WebRequest -UseBasicParsing https://aka.ms/downloadazcopy-v10-windows -OutFile azcopy.zip;expand-archive azcopy.zip;cd .\azcopy\*;.\azcopy.exe copy "C:\azuredata\CustomDataSetupScript.log" "https://abe2ecselog.blob.core.windows.net/cselogs/$arg1-cse.log?$arg2"' \
+        --parameters arg1=$DEPLOYMENT_VMSS_NAME arg2=$token
 
     # set -x
     echo "debug done"
