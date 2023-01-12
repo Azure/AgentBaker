441e933a467ffe2d27f8fdbd79ae80f460b7c782
Pablo Trivino <pablotrivino@microsoft.com>
2023-01-12 11:25:14 -0800

    added logs for testing and corrected image ver

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -195,7 +195,8 @@ fi
 
 if [[ $OS == $UBUNTU_OS_NAME && $(isARM64) != 1 ]]; then  # no ARM64 SKU with GPU now
   gpu_action="install"
-  export NVIDIA_DRIVER_IMAGE_TAG="cuda-510.47.03-${NVIDIA_DRIVER_IMAGE_SHA}"
+  #export NVIDIA_DRIVER_IMAGE_TAG="cuda-510.47.03-${NVIDIA_DRIVER_IMAGE_SHA}"
+  export NVIDIA_DRIVER_IMAGE_TAG="cuda-510.47.03"
 
   mkdir -p /opt/{actions,gpu}
   if [[ "${CONTAINER_RUNTIME}" == "containerd" ]]; then
@@ -237,6 +238,7 @@ cat << EOF >> ${VHD_LOGS_FILEPATH}
 EOF
 
 echo "${CONTAINER_RUNTIME} images pre-pulled:" >> ${VHD_LOGS_FILEPATH}
+grep -i "nvidia-gpu-driver-version" ${VHD_LOGS_FILEPATH}
 
 string_replace() {
   echo ${1//\*/$2}
