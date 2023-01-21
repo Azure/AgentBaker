b187ea81b412f25e1a1013f51bdebafc64adadcf
Cameron Meissner <cameissner@microsoft.com>
2023-01-20 16:13:41 -0800

    use az account show to determine whether using msi

--- vhdbuilder/packer/generate-vhd-publishing-info.sh
+++ vhdbuilder/packer/generate-vhd-publishing-info.sh
@@ -32,22 +32,20 @@ else
     IMAGE_ARCH="x64"
 fi
 
-echo "az account show..."
-account_info=$(az account show)
-echo "${account_info}"
-
-if [[ "${OS_NAME,,}" == "windows" ]]; then
-    # we still need to use the original connection string for windows until windows builds run on the 1ES pool agents with an assigned identity
-    start_date=$(date +"%Y-%m-%dT00:00Z" -d "-1 day")
-    expiry_date=$(date +"%Y-%m-%dT00:00Z" -d "+1 year")
-    sas_token=$(az storage container generate-sas --name vhd --permissions lr --connection-string ${CLASSIC_SA_CONNECTION_STRING} --start ${start_date} --expiry ${expiry_date} | tr -d '"')
-else
+identity_name=$(az account show | jq -r '.user.name')
+if [[ "${identity_name,,}" == "systemassignedidentity" ]]; then
     [ -z "${OUTPUT_STORAGE_ACCOUNT_NAME}" ] && echo "OUTPUT_STORAGE_ACCOUNT_NAME should be set when generating Linux VHD publishing info..." && exit 1
     [ -z "${OUTPUT_STORAGE_CONTAINER_NAME}" ] && echo "OUTPUT_STORAGE_CONTAINER_NAME should be set when generating Linux VHD publishing info..." && exit 1
+    echo "using systemAssignedIdentity to generate user-delegation SAS token..."
     # max of 7 day expiration time when using user delegation SAS
     expiry_date=$(date +"%Y-%m-%dT00:00Z" -d "+7 day")
     sas_token=$(az storage container generate-sas --account-name ${OUTPUT_STORAGE_ACCOUNT_NAME} --name ${OUTPUT_STORAGE_CONTAINER_NAME} --permissions lr --expiry ${expiry_date} --auth-mode login --as-user | tr -d '"')
-    echo "generated SAS token using output storage account name '${OUTPUT_STORAGE_ACCOUNT_NAME}' and output storage container name '${OUTPUT_STORAGE_CONTAINER_NAME}'"
+else
+    echo "using connection-string to generate SAS token..."
+    # we still need to use the original connection string when not using a system-assigned identity on 1ES pools
+    start_date=$(date +"%Y-%m-%dT00:00Z" -d "-1 day")
+    expiry_date=$(date +"%Y-%m-%dT00:00Z" -d "+1 year")
+    sas_token=$(az storage container generate-sas --name vhd --permissions lr --connection-string ${CLASSIC_SA_CONNECTION_STRING} --start ${start_date} --expiry ${expiry_date} | tr -d '"')
 fi
 
 if [ "$sas_token" == "" ]; then
