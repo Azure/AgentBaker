a699da73d2121d9561c77a75d57fe0810bdf090d
Alexander Eldeib <aleldeib@microsoft.com>
2023-01-26 14:19:11 +0000

    refactor(fix): teleport conditional with two sets of brackets

--- pkg/agent/testdata/AKSUbuntu1804+HTTPProxy/line33.sh
+++ pkg/agent/testdata/AKSUbuntu1804+HTTPProxy/line33.sh
@@ -109,7 +109,7 @@ else
 fi
 
 logs_to_events "AKS.CSE.installContainerRuntime" installContainerRuntime
-if [ "${NEEDS_CONTAINERD}" == "true" && "${TELEPORT_ENABLED}" == "true" ]; then 
+if [ "${NEEDS_CONTAINERD}" == "true" ] && [ "${TELEPORT_ENABLED}" == "true" ]; then 
     logs_to_events "AKS.CSE.installTeleportdPlugin" installTeleportdPlugin
 fi
 
