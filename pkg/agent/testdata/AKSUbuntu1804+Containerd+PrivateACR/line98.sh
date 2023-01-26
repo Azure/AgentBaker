8b176fcbd4afdcb94b8d2a6088133bcfc57d4075
Alexander Eldeib <aleldeib@microsoft.com>
2023-01-26 22:57:16 +0000

    test: remove all cloud init scripts

--- pkg/agent/testdata/AKSUbuntu1804+Containerd+PrivateACR/line98.sh
+++
@@ -1,8 +0,0 @@
-[Unit]
-Description=Reconcile /etc/hosts file for private cluster
-[Service]
-Type=simple
-Restart=on-failure
-ExecStart=/bin/bash /opt/azure/containers/reconcilePrivateHosts.sh
-[Install]
-WantedBy=multi-user.target
