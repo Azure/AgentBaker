21a9d900a4702f78f3c0027328f511a67a3a423b
Ace Eldeib <alexeldeib@gmail.com>
2023-02-01 11:37:32 -0500

    bump: vhd to 2023.01.26 for fips fix (#2716)

--- pkg/agent/datamodel/sig_config.go
+++ pkg/agent/datamodel/sig_config.go
@@ -245,7 +245,7 @@ const (
 )
 
 const (
-	LinuxSIGImageVersion string = "2023.01.25"
+	LinuxSIGImageVersion string = "2023.01.26"
 
 	// DO NOT MODIFY: used for freezing linux images with docker
 	FrozenLinuxSIGImageVersionForDocker string = "2022.08.29"
