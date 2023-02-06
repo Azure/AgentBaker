d7a7dc2f41ce4899c0304d7b6f680ab39819c246
anujmaheshwari1 <amaheshwari@microsoft.com>
2023-02-06 10:26:32 -0800

    remove redundant check

--- vhdbuilder/scripts/automate_generate_version.sh
+++ vhdbuilder/scripts/automate_generate_version.sh
@@ -13,16 +13,14 @@ for build_id in $build_ids; do
         # So in the past, it has happened that you trigger a build at say 3/4pm PST, 
         # some SKUs will have todays date some will have tomorrows based on when they are triggered because of UTC conversion
         # TODO(amaheshwari): Change VHD script to use a common var for image version that is plumbed down to all SKUs
-        if [[ $artifact == *"publishing-info"* ]]; then
-            az pipelines runs artifact download --artifact-name $artifact --path $(pwd) --run-id $build_id
-            current_image_version=$(jq -r .image_version < vhd-publishing-info.json)
-            if [[ $global_image_version != $current_image_version ]]; then
-                if [[ -z $global_image_version ]]; then
-                    global_image_version=$current_image_version
-                else 
-                    echo "mismatched image, exiting"
-                    exit 1
-                fi
+        az pipelines runs artifact download --artifact-name $artifact --path $(pwd) --run-id $build_id
+        current_image_version=$(jq -r .image_version < vhd-publishing-info.json)
+        if [[ $global_image_version != $current_image_version ]]; then
+            if [[ -z $global_image_version ]]; then
+                global_image_version=$current_image_version
+            else 
+                echo "mismatched image, exiting"
+                exit 1
             fi
         fi
     done
