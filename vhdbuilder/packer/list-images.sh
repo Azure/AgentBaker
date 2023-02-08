fc0953bb60442d1eac30623023cf25e95f74aed3
Cameron Meissner <cameissner@microsoft.com>
2023-02-08 09:46:42 -0800

    simplify

--- vhdbuilder/packer/list-images.sh
+++ vhdbuilder/packer/list-images.sh
@@ -8,7 +8,7 @@ function generate_image_bom_for_containerd() {
     IFS=$'\n'
     ctr_list=$(ctr --namespace k8s.io image list | sed 1d | awk '{print $1, $3}')
     crictl_list=$(crictl images --no-trunc | sed 1d | awk '{printf "%s:%s %s\n", $1,$2,$3;}')
-    
+
     touch /opt/azure/containers/images.json
     for image in $ctr_list; do
         tag=$(echo $image | awk '{print $1}')
@@ -26,14 +26,10 @@ function generate_image_bom_for_containerd() {
     jq < /opt/azure/containers/image-bom.json
 }
 
-function generate_image_bom_for_docker() {
-    docker inspect $(docker images -aq) -f '{"id":"{{.ID}}","repoTags":{{json .RepoTags}},"repoDigests":{{json .RepoDigests}}}' | jq --slurp . > /opt/azure/containers/image-bom.json
-}
-
 if [[ ${CONTAINER_RUNTIME} == "containerd" ]]; then
     generate_image_bom_for_containerd
 elif [[ ${CONTAINER_RUNTIME} == "docker" ]]; then
-    generate_image_bom_for_docker
+    docker inspect $(docker images -aq) -f '{"id":"{{.ID}}","repoTags":{{json .RepoTags}},"repoDigests":{{json .RepoDigests}}}' | jq --slurp . > /opt/azure/containers/image-bom.json
 else
     echo "Unknown container runtime: ${CONTAINER_RUNTIME}"
     exit 1
