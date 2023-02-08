64ea209114e5555c46ea9cc3e16d730c03407fac
Cameron Meissner <cameissner@microsoft.com>
2023-02-08 09:53:34 -0800

    typos

--- vhdbuilder/packer/list-images.sh
+++ vhdbuilder/packer/list-images.sh
@@ -4,12 +4,12 @@ set -euxo pipefail
 CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-docker}"
 
 function generate_image_bom_for_containerd() {
+    temp_image_bom=/opt/azure/containers/temp-image-bom.json
     IFS_backup=$IFS
     IFS=$'\n'
     ctr_list=$(ctr --namespace k8s.io image list | sed 1d | awk '{print $1, $3}')
     crictl_list=$(crictl images --no-trunc | sed 1d | awk '{printf "%s:%s %s\n", $1,$2,$3;}')
 
-    touch /opt/azure/containers/images.json
     for image in $ctr_list; do
         tag=$(echo $image | awk '{print $1}')
         # skip image tags that consist of a sha256 hash
@@ -17,12 +17,12 @@ function generate_image_bom_for_containerd() {
         digest=$(echo $image | awk '{print $2}')
         id=$(echo "$crictl_list" | grep -e "$tag" | awk '{print $2}')
 
-        jq --arg repoTag "$tag" --arg repoDigest "$digest" --arg id "$id" -n '{id:$id, repoTags:[$repoTag], repoDigests:[$repoDigest]}' >> /opt/azure/containers/images.json
+        jq --arg repoTag "$tag" --arg repoDigest "$digest" --arg id "$id" -n '{id:$id, repoTags:[$repoTag], repoDigests:[$repoDigest]}' >> $temp_image_bom
     done
 
     IFS=$IFS_backup
-    jq --slurpfile images images.json -n '$images | group_by(.id) | map({id:.[0].id, repoTags:map(.repoTags | add) | unique, repoDigests:map(.repoDigests | add) | unique})' > /opt/azure/containers/image-bom.json
-    rm -f /opt/azure/containers/images.json
+    jq --slurpfile images $temp_image_bom -n '$images | group_by(.id) | map({id:.[0].id, repoTags:map(.repoTags | add) | unique, repoDigests:map(.repoDigests | add) | unique})' > /opt/azure/containers/image-bom.json
+    rm -f $temp_image_bom
     jq < /opt/azure/containers/image-bom.json
 }
 
