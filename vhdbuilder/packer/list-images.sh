7e50d80d00b69b9b6b210153c56dbff678e14e8c
Cameron Meissner <cameissner@microsoft.com>
2023-02-09 09:03:56 -0800

    fix formatting

--- vhdbuilder/packer/list-images.sh
+++ vhdbuilder/packer/list-images.sh
@@ -8,18 +8,18 @@ function generate_image_bom_for_containerd() {
     IFS_backup=$IFS
     IFS=$'\n'
     ctr_list=$(ctr -n k8s.io image list | sed 1d | awk '{print $1, $3}')
-    digests=$(echo "$ctr_list" | awk '{print $2}')
+    digests=$(echo "$ctr_list" | awk '{print $2}' | xargs -n1 | sort -u)
 
     for digest in $digests; do
         digest_entries=$(echo "$ctr_list" | grep -e "$digest")
         tags=$(echo "$digest_entries" | awk '{print $1}' | grep -v "sha256")
         id=$(echo "$digest_entries" | awk '{print $1}' | grep "sha256")
 
-        jq --arg tags "$tags" --arg digest "$digest" --arg id "$id" -n '{id:$id, repoTags:[$tags | split("\n")], repoDigests:[$digest]}' >> $temp_image_bom
+        jq --arg tags "$tags" --arg digest "$digest" --arg id "$id" -n '{id:$id, repoTags:$tags | split("\n"), repoDigests:[$digest]}' >> $temp_image_bom
     done
 
     IFS=$IFS_backup
-    jq --slurpfile images $temp_image_bom -n '$images | group_by(.id) | map({id:.[0].id, repoTags:map(.repoTags | add) | unique, repoDigests:map(.repoDigests | add) | unique})' > /opt/azure/containers/image-bom.json
+    jq --slurpfile images $temp_image_bom -n '$images | group_by(.id) | map({id:.[0].id, repoTags:.[0].repoTags | unique, repoDigests:map(.repoDigests | add) | unique})' > /opt/azure/containers/image-bom.json
     rm -f $temp_image_bom
 }
 
