3446d0147d8d45396809c2f7121c0c94f990a600
Cameron <cameronmeissner@gmail.com>
2023-02-09 16:56:21 -0800

    restore repoDigests within image-bom.json for VHD builds (#2742)
    * cleanup ab e2e custom SIG version ID scenario impl
    
    * add digests to image-bom.json
    
    * simplify
    
    * typos
    
    * errors...
    
    * just cat it out
    
    * fix id grep
    
    * remove cat
    
    * comments
    
    * simplify, can only use ctr
    
    * fix formatting
    
    * cleanup
    
    * make docker and containerd image-bom's consistent
    
    * refactor with functions and constants
    
    * finally fixed the aggregation...
    
    ---------
    
    Co-authored-by: Cameron Meissner <cameissner@microsoft.com>

--- vhdbuilder/packer/list-images.sh
+++ vhdbuilder/packer/list-images.sh
@@ -3,14 +3,38 @@ set -euxo pipefail
 
 CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-docker}"
 
+TEMP_IMAGE_BOM_PATH=/opt/azure/containers/temp-image-bom.json
+IMAGE_BOM_PATH=/opt/azure/containers/image-bom.json
+
+function generate_image_bom_for_containerd() {
+    IFS_backup=$IFS; IFS=$'\n'
+    ctr_list=$(ctr -n k8s.io image list | sed 1d | awk '{print $1, $3}')
+    digests=$(echo "$ctr_list" | awk '{print $2}' | xargs -n1 | sort -u)
+
+    for digest in $digests; do
+        digest_entries=$(echo "$ctr_list" | grep -e "$digest")
+        tags=$(echo "$digest_entries" | awk '{print $1}' | grep -v "sha256")
+        id=$(echo "$digest_entries" | awk '{print $1}' | grep -e "sha256")
+
+        jq --arg tags "$tags" --arg digest "$digest" --arg id "$id" -n '{id:$id, repoTags:$tags | split("\n"), repoDigests:[$digest]}' >> $TEMP_IMAGE_BOM_PATH
+    done
+
+    IFS=$IFS_backup
+    jq --slurpfile images $TEMP_IMAGE_BOM_PATH -n '$images | group_by(.id) | map({id:.[0].id, repoTags:[.[].repoTags] | add | unique, repoDigests:[.[].repoDigests] | add | unique})' > $IMAGE_BOM_PATH
+    rm -f $TEMP_IMAGE_BOM_PATH
+}
+
+function generate_image_bom_for_docker() {
+    docker inspect $(docker images -aq) -f '{"id":"{{.ID}}","repoTags":{{json .RepoTags}},"repoDigests":{{json .RepoDigests}}}' | jq --slurp . | jq  'map({id:.id, repoTags:.repoTags, repoDigests:.repoDigests | map(split("@")[1])})' > $IMAGE_BOM_PATH
+}
+
 if [[ ${CONTAINER_RUNTIME} == "containerd" ]]; then
-    crictl images -o json
-    crictl images -o json | jq '[.images | .[] | {id:.id, repoTags:.repoTags, repoDigests:.repoDigests}]' > /opt/azure/containers/image-bom.json
+    generate_image_bom_for_containerd
 elif [[ ${CONTAINER_RUNTIME} == "docker" ]]; then
-    docker inspect $(docker images -aq) -f '{"id":"{{.ID}}","repoTags":{{json .RepoTags}},"repoDigests":{{json .RepoDigests}}}' | jq --slurp . > /opt/azure/containers/image-bom.json
+    generate_image_bom_for_docker
 else
     echo "Unknown container runtime: ${CONTAINER_RUNTIME}"
     exit 1
 fi
 
-chmod a+r /opt/azure/containers/image-bom.json
+chmod a+r $IMAGE_BOM_PATH
