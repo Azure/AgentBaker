0e4f3dfa4ceb0d626c11981b140df2fe478b92f0
Jason Whitehouse <jawhiteh@microsoft.com>
2023-03-01 17:23:02 +0000

    forcing a different vm for gpu

--- vhdbuilder/packer/init-variables.sh
+++ vhdbuilder/packer/init-variables.sh
@@ -373,7 +373,7 @@ cat <<EOF > vhdbuilder/packer/settings.json
   "resource_group_name": "${AZURE_RESOURCE_GROUP_NAME}",
   "location": "${AZURE_LOCATION}",
   "storage_account_name": "${STORAGE_ACCOUNT_NAME}",
-  "vm_size": "Standard_ND6s",
+  "vm_size": "Standard_NV12s_v3",
   "create_time": "${CREATE_TIME}",
   "windows_image_publisher": "${WINDOWS_IMAGE_PUBLISHER}",
   "windows_image_offer": "${WINDOWS_IMAGE_OFFER}",
