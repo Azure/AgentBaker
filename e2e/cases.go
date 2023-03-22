73227d17730b8a210e4571596facae4b106acb14
Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>
2023-03-22 10:36:10 -0700

    Test with A100 GPU

--- e2e/cases.go
+++ e2e/cases.go
@@ -14,14 +14,14 @@ var cases = map[string]scenarioConfig{
 		bootstrapConfigMutator: func(t *testing.T, nbc *datamodel.NodeBootstrappingConfiguration) {
 			nbc.ContainerService.Properties.AgentPoolProfiles[0].VMSize = "Standard_NC6"
 			nbc.ContainerService.Properties.AgentPoolProfiles[0].Distro = "aks-ubuntu-containerd-18.04-gen2"
-			nbc.AgentPoolProfile.VMSize = "Standard_NC6"
+			nbc.AgentPoolProfile.VMSize = "Standard_ND96asr"
 			nbc.AgentPoolProfile.Distro = "aks-ubuntu-containerd-18.04-gen2"
 			nbc.ConfigGPUDriverIfNeeded = true
 			nbc.EnableGPUDevicePluginIfNeeded = false
 			nbc.EnableNvidia = true
 		},
 		vmConfigMutator: func(vmss *armcompute.VirtualMachineScaleSet) {
-			vmss.SKU.Name = to.Ptr("Standard_NC6s_v3")
+			vmss.SKU.Name = to.Ptr("Standard_ND96asr_v4")
 		},
 	},
 }
