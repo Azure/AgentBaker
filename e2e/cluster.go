acfa1758e93d951c1ce659b21444236fd2f5bb7c
Cameron <cameronmeissner@gmail.com>
2023-03-27 09:12:51 -0700

    fix abe2ego cluster recreation logic, add retries, wait on pod readiness, increase timeouts (#2919)
    * fix cluster recreation logic
    
    * use variable for rg location
    
    * add retries to cluster creation fix (#2921)
    
    * committing retry changes
    
    * wait until nginx pod is running before passing
    
    * get debug pod from aks nodepool for ssh and param extraction
    
    ---------
    
    Co-authored-by: Cameron Meissner <cameissner@microsoft.com>
    
    * refactored for comments, bumped go test timeout
    
    * bump aks agentpool count to 2
    
    * cleanup creation logic
    
    ---------
    
    Co-authored-by: Cameron Meissner <cameissner@microsoft.com>

--- e2e/cluster.go
+++ e2e/cluster.go
@@ -6,30 +6,63 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
 )
 
 func setupCluster(ctx context.Context, cloud *azureClient, location, resourceGroupName, clusterName string) error {
-	aksCluster, err := cloud.aksClient.Get(ctx, resourceGroupName, clusterName, nil)
+	var needNewCluster bool
+
+	testRgExistence, err := cloud.resourceGroupClient.CheckExistence(ctx, resourceGroupName, nil)
 	if err != nil {
-		return fmt.Errorf("failed to get aks cluster: %q", err)
+		return fmt.Errorf("failed to get AB E2E RG %q: %q", resourceGroupName, err)
 	}
 
-	rgExistence, err := cloud.resourceGroupClient.CheckExistence(ctx, agentbakerTestResourceGroupName, nil)
-	if err != nil {
-		return fmt.Errorf("failed to get MC RG: %q", err)
+	if testRgExistence.Success {
+		aksCluster, err := cloud.aksClient.Get(ctx, resourceGroupName, clusterName, nil)
+		if err != nil {
+			if isResourceNotFoundError(err) {
+				needNewCluster = true
+			} else {
+				return fmt.Errorf("failed to get aks cluster %q: %q", clusterName, err)
+			}
+		} else {
+			// We only need to check the MC resource group + cluster properties if the cluster resource itself exists
+			mcRgExistence, err := cloud.resourceGroupClient.CheckExistence(ctx, agentbakerTestClusterMCResourceGroupName, nil)
+			if err != nil {
+				return fmt.Errorf("failed to get test cluster MC RG %q: %q", agentbakerTestClusterMCResourceGroupName, err)
+			}
+
+			if !mcRgExistence.Success || aksCluster.Properties == nil || aksCluster.Properties.ProvisioningState == nil || *aksCluster.Properties.ProvisioningState == "Failed" {
+				needNewCluster = true
+				poller, err := cloud.aksClient.BeginDelete(ctx, resourceGroupName, clusterName, nil)
+				if err != nil {
+					return fmt.Errorf("failed to start aks cluster %q deletion: %q", clusterName, err)
+				}
+
+				_, err = poller.PollUntilDone(ctx, nil)
+				if err != nil {
+					return fmt.Errorf("failed to wait for aks cluster %q deletion: %q", clusterName, err)
+				}
+			}
+		}
+	} else {
+		needNewCluster = true
+		_, err := cloud.resourceGroupClient.CreateOrUpdate(
+			ctx,
+			resourceGroupName,
+			armresources.ResourceGroup{
+				Location: to.Ptr(e2eTestLocation),
+				Name:     to.Ptr(resourceGroupName),
+			},
+			nil)
+
+		if err != nil {
+			return fmt.Errorf("failed to create AB E2E RG %q: %q", resourceGroupName, err)
+		}
 	}
 
-	if !rgExistence.Success || aksCluster.Properties == nil || aksCluster.Properties.ProvisioningState == nil || *aksCluster.Properties.ProvisioningState == "Failed" {
-		poller, err := cloud.aksClient.BeginDelete(ctx, resourceGroupName, clusterName, nil)
-		if err != nil {
-			return fmt.Errorf("failed to start aks cluster deletion: %q", err)
-		}
-
-		_, err = poller.PollUntilDone(ctx, nil)
-		if err != nil {
-			return fmt.Errorf("failed to wait for aks cluster deletion: %q", err)
-		}
-
+	// A new cluster is created if the test RG does not exist, the cluster itself does not exist, or if the cluster is in an unusable state
+	if needNewCluster {
 		pollerResp, err := cloud.aksClient.BeginCreateOrUpdate(
 			ctx,
 			resourceGroupName,
@@ -41,11 +74,9 @@ func setupCluster(ctx context.Context, cloud *azureClient, location, resourceGro
 					AgentPoolProfiles: []*armcontainerservice.ManagedClusterAgentPoolProfile{
 						{
 							Name:         to.Ptr("nodepool1"),
-							Count:        to.Ptr[int32](1),
+							Count:        to.Ptr[int32](2),
 							VMSize:       to.Ptr("Standard_DS2_v2"),
 							MaxPods:      to.Ptr[int32](110),
-							MinCount:     to.Ptr[int32](1),
-							MaxCount:     to.Ptr[int32](100),
 							OSType:       to.Ptr(armcontainerservice.OSTypeLinux),
 							Type:         to.Ptr(armcontainerservice.AgentPoolTypeVirtualMachineScaleSets),
 							Mode:         to.Ptr(armcontainerservice.AgentPoolModeSystem),
@@ -55,6 +86,9 @@ func setupCluster(ctx context.Context, cloud *azureClient, location, resourceGro
 					NetworkProfile: &armcontainerservice.NetworkProfile{
 						NetworkPlugin: to.Ptr(armcontainerservice.NetworkPluginKubenet),
 					},
+				},
+				Identity: &armcontainerservice.ManagedClusterIdentity{
+					Type: to.Ptr(armcontainerservice.ResourceIdentityTypeSystemAssigned),
 				},
 			},
 			nil,
@@ -74,7 +108,7 @@ func setupCluster(ctx context.Context, cloud *azureClient, location, resourceGro
 }
 
 func getClusterSubnetID(ctx context.Context, cloud *azureClient, location, resourceGroupName, clusterName string) (string, error) {
-	pager := cloud.vnetClient.NewListPager(agentbakerTestResourceGroupName, nil)
+	pager := cloud.vnetClient.NewListPager(agentbakerTestClusterMCResourceGroupName, nil)
 
 	for pager.More() {
 		nextResult, err := pager.NextPage(ctx)
