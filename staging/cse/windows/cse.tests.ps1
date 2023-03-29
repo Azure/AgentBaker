536ac8be5dd4cec0b6ad9957e1041df05be1bee3
Cameron Meissner <cameissner@microsoft.com>
2023-03-29 16:30:30 -0700

    feat: add fips and kata abe2e scenarios
    feat: add fips and kata abe2e scenarios
    
    add fips and kata
    
    chore: bump cloud-node-manager images
    
    feat: create a unit test pipeline for windows powershell unit test scripts
    
    feat: increase Windows VHD size to 35 GB for distros WS2022 and WS2022-GEN2
    
    updated scenarios, added cluster creation logging
    
    chore: bump cloud-node-manager images
    
    feat: create a unit test pipeline for windows powershell unit test scripts
    
    feat: increase Windows VHD size to 35 GB for distros WS2022 and WS2022-GEN2
    
    updated scenarios, added cluster creation logging

--- staging/cse/windows/cse.tests.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-Pester -Output Detailed $PSScriptRoot\azurecnifunc.tests.ps1
-Invoke-Pester -Output Detailed $PSScriptRoot\configfunc.tests.ps1
-Invoke-Pester -Output Detailed $PSScriptRoot\..\..\..\parts\windows\windowscsehelper.tests.ps1
