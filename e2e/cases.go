9b11ec85fd5b07d52f3d004bce7c280fe7ff6581
Cameron Meissner <cameissner@microsoft.com>
2023-03-30 10:57:21 -0700

    feat: add fips and kata abe2e scenarios

--- e2e/cases.go
+++ e2e/cases.go
@@ -9,13 +9,18 @@ import (
 )
 
 // These SIG image versions are stored in the ACS test subscription, guarded by resource deletion locks
-var defaultImageVersionIDs = map[string]string{
-	"ubuntu1804":       "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/1804Gen2/versions/1.1677169694.31375",
-	"ubuntu2204":       "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/2204Gen2/versions/1.1679939578.12283",
-	"marinerv1":        "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/CBLMarinerV1/versions/1.1679939595.17588",
-	"marinerv2":        "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/CBLMarinerV2Gen2/versions/1.1679939582.10768",
-	"ubuntu2204-arm64": "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/2204Gen2Arm64/versions/1.1679939579.29526",
-	"marinerv2-arm64":  "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/CBLMarinerV2Gen2Arm64/versions/1.1679939588.23459",
+var defaultUbuntuImageVersionIDs = map[string]string{
+	"1804gen2":      "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/1804Gen2/versions/1.1677169694.31375",
+	"2004gen2fips":  "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/2004Gen2/versions/1.1679939587.10044",
+	"2204gen2":      "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/2204Gen2/versions/1.1679939578.12283",
+	"2204gen2arm64": "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/2204Gen2Arm64/versions/1.1679939579.29526",
+}
+
+var defaultMarinerImageVersionIDs = map[string]string{
+	"v1":          "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/CBLMarinerV1/versions/1.1679939595.17588",
+	"v2gen2":      "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/CBLMarinerV2Gen2/versions/1.1679939582.10768",
+	"v2gen2arm64": "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/CBLMarinerV2Gen2Arm64/versions/1.1679939588.23459",
+	"v2gen2kata":  "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/CBLMarinerV2kataGen2/versions/1.1679939567.7755",
 }
 
 var cases = map[string]scenarioConfig{
@@ -27,7 +32,7 @@ var cases = map[string]scenarioConfig{
 		},
 		vmConfigMutator: func(vmss *armcompute.VirtualMachineScaleSet) {
 			vmss.Properties.VirtualMachineProfile.StorageProfile.ImageReference = &armcompute.ImageReference{
-				ID: to.Ptr(defaultImageVersionIDs["ubuntu2204"]),
+				ID: to.Ptr(defaultUbuntuImageVersionIDs["2204gen2"]),
 			}
 		},
 	},
@@ -38,7 +43,7 @@ var cases = map[string]scenarioConfig{
 		},
 		vmConfigMutator: func(vmss *armcompute.VirtualMachineScaleSet) {
 			vmss.Properties.VirtualMachineProfile.StorageProfile.ImageReference = &armcompute.ImageReference{
-				ID: to.Ptr(defaultImageVersionIDs["marinerv1"]),
+				ID: to.Ptr(defaultMarinerImageVersionIDs["v1"]),
 			}
 		},
 	},
@@ -49,7 +54,7 @@ var cases = map[string]scenarioConfig{
 		},
 		vmConfigMutator: func(vmss *armcompute.VirtualMachineScaleSet) {
 			vmss.Properties.VirtualMachineProfile.StorageProfile.ImageReference = &armcompute.ImageReference{
-				ID: to.Ptr(defaultImageVersionIDs["marinerv2"]),
+				ID: to.Ptr(defaultMarinerImageVersionIDs["v2gen2"]),
 			}
 		},
 	},
@@ -66,7 +71,7 @@ var cases = map[string]scenarioConfig{
 		},
 		vmConfigMutator: func(vmss *armcompute.VirtualMachineScaleSet) {
 			vmss.Properties.VirtualMachineProfile.StorageProfile.ImageReference = &armcompute.ImageReference{
-				ID: to.Ptr(defaultImageVersionIDs["ubuntu2204-arm64"]),
+				ID: to.Ptr(defaultUbuntuImageVersionIDs["2204gen2arm64"]),
 			}
 			vmss.SKU.Name = to.Ptr("Standard_D2pds_V5")
 		},
@@ -82,9 +87,38 @@ var cases = map[string]scenarioConfig{
 		},
 		vmConfigMutator: func(vmss *armcompute.VirtualMachineScaleSet) {
 			vmss.Properties.VirtualMachineProfile.StorageProfile.ImageReference = &armcompute.ImageReference{
-				ID: to.Ptr(defaultImageVersionIDs["marinerv2-arm64"]),
+				ID: to.Ptr(defaultMarinerImageVersionIDs["v2gen2arm64"]),
 			}
 			vmss.SKU.Name = to.Ptr("Standard_D2pds_V5")
+		},
+	},
+	"marinerv2-kata": {
+		bootstrapConfigMutator: func(t *testing.T, nbc *datamodel.NodeBootstrappingConfiguration) {
+			nbc.ContainerService.Properties.AgentPoolProfiles[0].VMSize = "Standard_D4ads_v5"
+			nbc.ContainerService.Properties.AgentPoolProfiles[0].Distro = "aks-cblmariner-v2-gen2-kata"
+			nbc.AgentPoolProfile.VMSize = "Standard_D4ads_v5"
+			nbc.AgentPoolProfile.Distro = "aks-cblmariner-v2-gen2-kata"
+		},
+		vmConfigMutator: func(vmss *armcompute.VirtualMachineScaleSet) {
+			vmss.Properties.VirtualMachineProfile.StorageProfile.ImageReference = &armcompute.ImageReference{
+				ID: to.Ptr(defaultMarinerImageVersionIDs["v2gen2kata"]),
+			}
+			vmss.SKU.Name = to.Ptr("Standard_D4ads_v5")
+		},
+	},
+	"ubuntu2004-fips": {
+		bootstrapConfigMutator: func(t *testing.T, nbc *datamodel.NodeBootstrappingConfiguration) {
+			nbc.ContainerService.Properties.AgentPoolProfiles[0].VMSize = "Standard_DS2_v2"
+			nbc.ContainerService.Properties.AgentPoolProfiles[0].Distro = "aks-ubuntu-fips-containerd-20.04-gen2"
+			nbc.AgentPoolProfile.VMSize = "Standard_DS2_v2"
+			nbc.AgentPoolProfile.Distro = "aks-ubuntu-fips-containerd-20.04-gen2"
+			nbc.FIPSEnabled = true
+		},
+		vmConfigMutator: func(vmss *armcompute.VirtualMachineScaleSet) {
+			vmss.Properties.VirtualMachineProfile.StorageProfile.ImageReference = &armcompute.ImageReference{
+				ID: to.Ptr(defaultUbuntuImageVersionIDs["2004gen2fips"]),
+			}
+			vmss.SKU.Name = to.Ptr("Standard_DS2_v2")
 		},
 	},
 	"gpu": {
