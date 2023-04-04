28fbb0e4dba99940c602e638727850bdb20e6fc2
mingheren <mingheren@microsoft.com>
2023-04-04 15:37:59 -0700

    fix typo

--- pkg/agent/datamodel/sig_config_test.go
+++ pkg/agent/datamodel/sig_config_test.go
@@ -153,13 +153,13 @@ var _ = Describe("GetSIGAzureCloudSpecConfig", func() {
 		Expect(marinerV2KataGen2TL.Definition).To(Equal("V2katagen2TL"))
 		Expect(marinerV2KataGen2TL.Version).To(Equal(CBLMarinerV2KataGen2TLSIGImageVersion))
 
-		marinerV2FIPSGen1 := sigConfig.SigCBLMarinerImageConfig[AKSCBLMarinerV2FIPSGen1]
+		marinerV2FIPSGen1 := sigConfig.SigCBLMarinerImageConfig[AKSCBLMarinerV2FIPS]
 		Expect(marinerV2Arm64.ResourceGroup).To(Equal("resourcegroup"))
 		Expect(marinerV2Arm64.Gallery).To(Equal("akscblmariner"))
 		Expect(marinerV2Arm64.Definition).To(Equal("V2fips"))
 		Expect(marinerV2Arm64.Version).To(Equal(LinuxSIGImageVersion))
 
-		marinerV2FIPSGen2 := sigConfig.SigCBLMarinerImageConfig[AKSCBLMarinerV2FIPSGen1]
+		marinerV2FIPSGen2 := sigConfig.SigCBLMarinerImageConfig[AKSCBLMarinerV2FIPSGen2]
 		Expect(marinerV2Arm64.ResourceGroup).To(Equal("resourcegroup"))
 		Expect(marinerV2Arm64.Gallery).To(Equal("akscblmariner"))
 		Expect(marinerV2Arm64.Definition).To(Equal("V2fipsgen2"))
