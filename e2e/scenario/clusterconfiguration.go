3fd89f1076a6b822136dcd8a7b022bd8c370869d
Cameron Meissner <cameissner@microsoft.com>
2023-04-05 09:09:11 -0700

    feat: add cluster config to abe2e

---
+++ e2e/scenario/clusterconfiguration.go
@@ -0,0 +1,23 @@
+package scenario
+
+import (
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice"
+)
+
+// Selectors
+
+func NetworkPluginKubenetSelector(cluster *armcontainerservice.ManagedCluster) bool {
+	if cluster != nil && cluster.Properties != nil && cluster.Properties.NetworkProfile != nil {
+		return *cluster.Properties.NetworkProfile.NetworkPlugin == armcontainerservice.NetworkPluginKubenet
+	}
+	return false
+}
+
+// Mutators
+
+func NetworkPluginKubenetMutator(cluster *armcontainerservice.ManagedCluster) {
+	if cluster != nil && cluster.Properties != nil && cluster.Properties.NetworkProfile != nil {
+		cluster.Properties.NetworkProfile.NetworkPlugin = to.Ptr(armcontainerservice.NetworkPluginKubenet)
+	}
+}
