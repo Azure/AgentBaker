e18d1d64b272e6692999a738e753909f56dce8d0
mingheren <mingheren@microsoft.com>
2023-04-05 19:14:35 -0700

    fix typo

--- pkg/agent/datamodel/sig_config_test.go
+++ pkg/agent/datamodel/sig_config_test.go
@@ -154,15 +154,15 @@ var _ = Describe("GetSIGAzureCloudSpecConfig", func() {
 		Expect(marinerV2KataGen2TL.Version).To(Equal(CBLMarinerV2KataGen2TLSIGImageVersion))
 
 		marinerV2FIPS := sigConfig.SigCBLMarinerImageConfig[AKSCBLMarinerV2FIPS]
-		Expect(marinerV2Arm64.ResourceGroup).To(Equal("resourcegroup"))
-		Expect(marinerV2Arm64.Gallery).To(Equal("akscblmariner"))
-		Expect(marinerV2Arm64.Definition).To(Equal("V2fips"))
-		Expect(marinerV2Arm64.Version).To(Equal(LinuxSIGImageVersion))
+		Expect(marinerV2FIPS.ResourceGroup).To(Equal("resourcegroup"))
+		Expect(marinerV2FIPS.Gallery).To(Equal("akscblmariner"))
+		Expect(marinerV2FIPS.Definition).To(Equal("V2fips"))
+		Expect(marinerV2FIPS.Version).To(Equal(LinuxSIGImageVersion))
 
 		marinerV2FIPSGen2 := sigConfig.SigCBLMarinerImageConfig[AKSCBLMarinerV2FIPSGen2]
-		Expect(marinerV2Arm64.ResourceGroup).To(Equal("resourcegroup"))
-		Expect(marinerV2Arm64.Gallery).To(Equal("akscblmariner"))
-		Expect(marinerV2Arm64.Definition).To(Equal("V2fipsgen2"))
-		Expect(marinerV2Arm64.Version).To(Equal(LinuxSIGImageVersion))
+		Expect(marinerV2FIPSGen2.ResourceGroup).To(Equal("resourcegroup"))
+		Expect(marinerV2FIPSGen2.Gallery).To(Equal("akscblmariner"))
+		Expect(marinerV2FIPSGen2.Definition).To(Equal("V2fipsgen2"))
+		Expect(marinerV2FIPSGen2.Version).To(Equal(LinuxSIGImageVersion))
 	})
 })
