822e243a45660fd2bf3825b0e6b486965a979d84
Mikolaj Umanski <mumanski@microsoft.com>
2023-04-06 04:13:01 -0700

    test binary tests

--- e2e/windows/upload-cse-logs.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    [string]$arg1,
-    [string]$arg2,
-    [string]$arg3,
-    [string]$arg4
-)
-
-Invoke-WebRequest -UseBasicParsing https://aka.ms/downloadazcopy-v10-windows -OutFile azcopy.zip;expand-archive azcopy.zip;cd .\azcopy\*;.\azcopy.exe copy "C:\azuredata\CustomDataSetupScript.log" "https://$arg1.blob.core.windows.net/$arg2/$arg3-cse.log?$arg4"
