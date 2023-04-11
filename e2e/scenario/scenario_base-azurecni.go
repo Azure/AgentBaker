276981d5d26f8772f0f6b265b286f6cdbe8f4ddd
Cameron Meissner <cameissner@microsoft.com>
2023-04-10 20:02:58 -0700

    add bootstrap config mutator for azure cni scenario

--- e2e/scenario/scenario_base-azurecni.go
+++ e2e/scenario/scenario_base-azurecni.go
@@ -1,5 +1,15 @@
 package scenario
 
+import (
+	"testing"
+
+	"github.com/Azure/agentbaker/pkg/agent/datamodel"
+)
+
+const (
+	networkPluginAzure = "azure"
+)
+
 func base_azurecni() *Scenario {
 	return &Scenario{
 		Name:        "base-azurecni",
@@ -7,6 +17,10 @@ func base_azurecni() *Scenario {
 		Config: Config{
 			ClusterSelector: NetworkPluginAzureSelector,
 			ClusterMutator:  NetworkPluginAzureMutator,
+			BootstrapConfigMutator: func(t *testing.T, nbc *datamodel.NodeBootstrappingConfiguration) {
+				nbc.ContainerService.Properties.OrchestratorProfile.KubernetesConfig.NetworkPlugin = networkPluginAzure
+				nbc.AgentPoolProfile.KubernetesConfig.NetworkPlugin = networkPluginAzure
+			},
 		},
 	}
 }
