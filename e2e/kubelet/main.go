5f7cd7c7af747be6f9b386bbeae6bdc7017c2f45
anujmaheshwari1 <amaheshwari@microsoft.com>
2023-04-14 12:18:36 -0700

    feat: automate generating kubelet flags

---
+++ e2e/kubelet/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"regexp"
+
+	"github.com/sanity-io/litter"
+)
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	k8sVersion := os.Getenv("KUBE_BINARY_VERSION")
+	binaryPath := fmt.Sprintf("/usr/local/bin/kubelet-%s", k8sVersion)
+	fmt.Println("k8s version is:", k8sVersion)
+	
+	r, w := io.Pipe()
+
+	c1 := exec.Command("sudo", "timeout", "-k", "3", "--preserve-status", "1", binaryPath, "-v", "1", "--container-runtime-endpoint", "unix:///var/run/containerd/containerd.sock")
+	fmt.Println(c1)
+	c1.Stdout = w
+	c1.Stderr = w
+
+	c2 := exec.Command("grep", "FLAG")
+	c2.Stdin = r
+
+	var grepOut bytes.Buffer
+
+	c2.Stdout = &grepOut
+	c2.Stderr = &grepOut
+
+	if err := c2.Start(); err != nil {
+		return fmt.Errorf("failed to start grep pipeline: %q", err)
+	}
+
+	if err := c1.Run(); err != nil {
+		return fmt.Errorf("failed to run kubelet: %q", err)
+	}
+
+	w.Close()
+
+	if err := c2.Wait(); err != nil {
+		fmt.Println(fmt.Errorf("failed to wait for grep to exit: %q", err))
+	}
+
+	flags, err := extractKeyValuePairs(grepOut.Bytes())
+	if err != nil {
+		return fmt.Errorf("failed to extract key value pairs: %q", err)
+	}
+
+	litter.Dump(flags)
+
+	return nil
+}
+
+// this regex looks for groups of the following forms, returning KEY and VALUE as submatches
+// - KEY=VALUE
+// - KEY="VALUE"
+// - KEY=
+// - KEY="VALUE WITH WHITESPACE"
+const regexString = `FLAG: ([^=\s]+)=(\"[^\"]*\"|[^\s]*)`
+
+func extractKeyValuePairs(data []byte) (map[string]string, error) {
+	r, err := regexp.Compile(regexString)
+	if err != nil {
+		return nil, fmt.Errorf("failed to compile regex: %s", err)
+	}
+
+	matches := r.FindAllStringSubmatch(string(data), -1)
+
+	litter.Dump(matches)
+
+	var resultKeyValuePairs = make(map[string]string)
+
+	for _, submatchGroup := range matches {
+		if len(submatchGroup) < 3 {
+			return nil, fmt.Errorf("expected 3 results (match, key, value) from regex, found %d, result %q", len(submatchGroup), submatchGroup)
+		}
+
+		key := submatchGroup[1]
+		val := submatchGroup[2]
+
+		resultKeyValuePairs[key] = val
+	}
+
+	return resultKeyValuePairs, nil
+}
