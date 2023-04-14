f2d5863ff263f0ac78af843c9bb5c7050493349c
Cameron <cameronmeissner@gmail.com>
2023-04-14 09:29:40 -0700

    feat: add azure cni scenarios to abe2e (#3035)
    Co-authored-by: Cameron Meissner <cameissner@microsoft.com>

---
+++ e2e/scenario/scenario_base-azurecni.go
@@ -0,0 +1,21 @@
+package scenario
+
+import (
+	"github.com/Azure/agentbaker/pkg/agent/datamodel"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice"
+)
+
+func base_azurecni() *Scenario {
+	return &Scenario{
+		Name:        "base-azurecni",
+		Description: "base scenario on cluster configured with Azure CNI",
+		Config: Config{
+			ClusterSelector: NetworkPluginAzureSelector,
+			ClusterMutator:  NetworkPluginAzureMutator,
+			BootstrapConfigMutator: func(nbc *datamodel.NodeBootstrappingConfiguration) {
+				nbc.ContainerService.Properties.OrchestratorProfile.KubernetesConfig.NetworkPlugin = string(armcontainerservice.NetworkPluginAzure)
+				nbc.AgentPoolProfile.KubernetesConfig.NetworkPlugin = string(armcontainerservice.NetworkPluginAzure)
+			},
+		},
+	}
+}
