93d87978e28cc81a3b3eff7df41e364ecfc04063
Tyler Lloyd <tylloyd@microsoft.com>
2023-04-17 08:58:53 -0400

    test: add test for >= 1.15 < 1.25

--- pkg/agent/datamodel/types_test.go
+++ pkg/agent/datamodel/types_test.go
@@ -1664,6 +1664,18 @@ func TestGetKubeProxyFeatureGatesWindowsArguments(t *testing.T) {
 			expectedFeatureGates: "",
 		},
 		{
+			name: "IPV6 enabled and version has feature gate (>= 1.15 < 1.25)",
+			properties: &Properties{
+				FeatureFlags: &FeatureFlags{
+					EnableIPv6DualStack: true,
+				},
+				OrchestratorProfile: &OrchestratorProfile{
+					OrchestratorVersion: "1.24.12",
+				},
+			},
+			expectedFeatureGates: "\"IPv6DualStack=true\"",
+		},
+		{
 			name: "IPV6 enabled but version does not have feature gate",
 			properties: &Properties{
 				FeatureFlags: &FeatureFlags{
