a6afd703d3545c43de08211650173bee9aa8d3af
Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>
2023-04-19 17:52:58 -0700

    feat: generate trivy table

--- vhdbuilder/packer/trivy-scan.sh
+++ vhdbuilder/packer/trivy-scan.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-TRIVY_REPORT_PATH=/opt/azure/containers/trivy-report.json
+TRIVY_REPORT_JSON_PATH=/opt/azure/containers/trivy-report.json
+TRIVY_REPORT_TABLE_PATH=/opt/azure/containers/trivy-table.txt
 TRIVY_VERSION="0.40.0"
 TRIVY_ARCH=""
 
@@ -15,15 +16,19 @@ else
     exit 1
 fi
 
-mkdir -p "$(dirname "${TRIVY_REPORT_PATH}")"
+mkdir -p "$(dirname "${TRIVY_REPORT_JSON_PATH}")"
+mkdir -p "$(dirname "${TRIVY_REPORT_TABLE_PATH}")"
 
 wget "https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_${TRIVY_ARCH}.tar.gz"
 tar -xvzf "trivy_${TRIVY_VERSION}_${TRIVY_ARCH}.tar.gz"
 rm "trivy_${TRIVY_VERSION}_${TRIVY_ARCH}.tar.gz"
 chmod a+x trivy 
 
-./trivy --security-checks vuln rootfs -f json --ignore-unfixed --severity HIGH,CRITICAL -o "${TRIVY_REPORT_PATH}" /
+./trivy --security-checks vuln rootfs -f json --ignore-unfixed --severity HIGH,CRITICAL -o "${TRIVY_REPORT_JSON_PATH}" /
+./trivy --security-checks vuln rootfs -f table --ignore-unfixed --severity HIGH,CRITICAL -o "${TRIVY_REPORT_TABLE_PATH}" /
+
 
 rm ./trivy 
 
-chmod a+r "${TRIVY_REPORT_PATH}"
+chmod a+r "${TRIVY_REPORT_JSON_PATH}"
+chmod a+r "${TRIVY_REPORT_TABLE_PATH}"
