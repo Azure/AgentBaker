9fdaecb8b725d1d83be8ef5c75ce2812ffdd9e0f
Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>
2023-04-27 11:00:25 -0700

    fix: change file name and add context to start

--- vhdbuilder/packer/trivy-scan.sh
+++ vhdbuilder/packer/trivy-scan.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 TRIVY_REPORT_JSON_PATH=/opt/azure/containers/trivy-report.json
-TRIVY_REPORT_TABLE_PATH=/opt/azure/containers/trivy-table.txt
+TRIVY_REPORT_TABLE_PATH=/opt/azure/containers/trivy-images-table.txt
 TRIVY_VERSION="0.40.0"
 TRIVY_ARCH=""
 
@@ -27,6 +27,10 @@ chmod a+x trivy
 ./trivy --scanners vuln rootfs -f json --skip-dirs /var/lib/containerd --ignore-unfixed --severity HIGH,CRITICAL -o "${TRIVY_REPORT_JSON_PATH}" /
 
 IMAGE_LIST=$(ctr -n k8s.io image list -q | grep -v sha256)
+
+echo "This contains the list of images with high and critical level CVEs (if present), that are present in the node. 
+Note: images without CVEs are also listed" >> "${TRIVY_REPORT_TABLE_PATH}"
+
 for image in $IMAGE_LIST; do
     ./trivy --scanners vuln image --ignore-unfixed --severity HIGH,CRITICAL -f table $image >> ${TRIVY_REPORT_TABLE_PATH} || true
 done
