02bd4b42d347b57a91b8fa2a05c743b61d5cb3e5
Mitch Zhu <zhumc11@gmail.com>
2023-05-23 16:22:55 -0700

    Add kublet part back

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -11,7 +11,7 @@ source /home/packer/provision_source.sh
 source /home/packer/provision_source_distro.sh
 source /home/packer/tool_installs.sh
 source /home/packer/tool_installs_distro.sh
-#source /home/packer/packer_source.sh
+source /home/packer/packer_source.sh
 
 CPU_ARCH=$(getCPUArch)  #amd64 or arm64
 VHD_LOGS_FILEPATH=/opt/azure/vhd-install.complete
@@ -456,36 +456,36 @@ echo "install-dependencies step completed successfully"
 #fi
 
 # shellcheck disable=SC2129
-#echo "kubelet/kubectl downloaded:" >> ${VHD_LOGS_FILEPATH}
-#ls -ltr /usr/local/bin/* >> ${VHD_LOGS_FILEPATH}
+echo "kubelet/kubectl downloaded:" >> ${VHD_LOGS_FILEPATH}
+ls -ltr /usr/local/bin/* >> ${VHD_LOGS_FILEPATH}
 
 # shellcheck disable=SC2010
-#ls -ltr /dev/* | grep sgx >>  ${VHD_LOGS_FILEPATH}
+ls -ltr /dev/* | grep sgx >>  ${VHD_LOGS_FILEPATH}
 
-#echo -e "=== Installed Packages Begin\n$(listInstalledPackages)\n=== Installed Packages End" >> ${VHD_LOGS_FILEPATH}
+echo -e "=== Installed Packages Begin\n$(listInstalledPackages)\n=== Installed Packages End" >> ${VHD_LOGS_FILEPATH}
 
-#echo "Disk usage:" >> ${VHD_LOGS_FILEPATH}
-#df -h >> ${VHD_LOGS_FILEPATH}
+echo "Disk usage:" >> ${VHD_LOGS_FILEPATH}
+df -h >> ${VHD_LOGS_FILEPATH}
 
 
 # warn at 75% space taken
-#[ -s $(df -P | grep '/dev/sda1' | awk '0+$5 >= 75 {print}') ] || echo "WARNING: 75% of /dev/sda1 is used" >> ${VHD_LOGS_FILEPATH}
+[ -s $(df -P | grep '/dev/sda1' | awk '0+$5 >= 75 {print}') ] || echo "WARNING: 75% of /dev/sda1 is used" >> ${VHD_LOGS_FILEPATH}
 # error at 99% space taken
-#[ -s $(df -P | grep '/dev/sda1' | awk '0+$5 >= 99 {print}') ] || exit 1
+[ -s $(df -P | grep '/dev/sda1' | awk '0+$5 >= 99 {print}') ] || exit 1
 
-#echo "Using kernel:" >> ${VHD_LOGS_FILEPATH}
-#tee -a ${VHD_LOGS_FILEPATH} < /proc/version
-#{
-#  echo "Install completed successfully on " $(date)
-#  echo "VSTS Build NUMBER: ${BUILD_NUMBER}"
-#  echo "VSTS Build ID: ${BUILD_ID}"
-#  echo "Commit: ${COMMIT}"
-#  echo "Ubuntu version: ${UBUNTU_RELEASE}"
-#  echo "Hyperv generation: ${HYPERV_GENERATION}"
-#  echo "Feature flags: ${FEATURE_FLAGS}"
-#  echo "Container runtime: ${CONTAINER_RUNTIME}"
-#  echo "FIPS enabled: ${ENABLE_FIPS}"
-#} >> ${VHD_LOGS_FILEPATH}
+echo "Using kernel:" >> ${VHD_LOGS_FILEPATH}
+tee -a ${VHD_LOGS_FILEPATH} < /proc/version
+{
+  echo "Install completed successfully on " $(date)
+  echo "VSTS Build NUMBER: ${BUILD_NUMBER}"
+  echo "VSTS Build ID: ${BUILD_ID}"
+  echo "Commit: ${COMMIT}"
+  echo "Ubuntu version: ${UBUNTU_RELEASE}"
+  echo "Hyperv generation: ${HYPERV_GENERATION}"
+  echo "Feature flags: ${FEATURE_FLAGS}"
+  echo "Container runtime: ${CONTAINER_RUNTIME}"
+  echo "FIPS enabled: ${ENABLE_FIPS}"
+} >> ${VHD_LOGS_FILEPATH}
 
 #if [[ $(isARM64) != 1 ]]; then
 #  # no asc-baseline-1.1.0-268.arm64.deb
