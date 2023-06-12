1f19080980130c201960d743046c4b5e4428a3e8
Mitch Zhu <zhumc11@gmail.com>
2023-06-12 15:18:37 -0700

    Remove commented blocks

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -11,44 +11,10 @@ source /home/packer/provision_source.sh
 source /home/packer/provision_source_distro.sh
 source /home/packer/tool_installs.sh
 source /home/packer/tool_installs_distro.sh
-source /home/packer/packer_source.sh
 
 CPU_ARCH=$(getCPUArch)  #amd64 or arm64
 VHD_LOGS_FILEPATH=/opt/azure/vhd-install.complete
 COMPONENTS_FILEPATH=/opt/azure/components.json
-#MANIFEST_FILEPATH=/opt/azure/manifest.json
-#KUBE_PROXY_IMAGES_FILEPATH=/opt/azure/kube-proxy-images.json
-##this is used by post build test to check whether the compoenents do indeed exist
-#cat components.json > ${COMPONENTS_FILEPATH}
-#cat manifest.json > ${MANIFEST_FILEPATH}
-#cat ${THIS_DIR}/kube-proxy-images.json > ${KUBE_PROXY_IMAGES_FILEPATH}
-#echo "Starting build on " $(date) > ${VHD_LOGS_FILEPATH}
-
-#if [[ $OS == $MARINER_OS_NAME ]]; then
-#  chmod 755 /opt
-#  chmod 755 /opt/azure
-#  chmod 644 ${VHD_LOGS_FILEPATH}
-#fi
-
-#copyPackerFiles
-#systemctlEnableAndStart disk_queue || exit 1
-
-#mkdir /opt/certs
-#chmod 666 /opt/certs
-
-#systemctlEnableAndStart ci-syslog-watcher.path || exit 1
-#systemctlEnableAndStart ci-syslog-watcher.service || exit 1
-
-## enable the modified logrotate service and remove the auto-generated default logrotate cron job if present
-#systemctlEnableAndStart logrotate.timer || exit 1
-#rm -f /etc/cron.daily/logrotate
-
-#if [[ ${UBUNTU_RELEASE} == "18.04" && ${ENABLE_FIPS,,} == "true" ]]; then
-#  installFIPS
-#elif [[ ${ENABLE_FIPS,,} == "true" ]]; then
-#  echo "AKS enables FIPS on Ubuntu 18.04 only, exiting..."
-#  exit 1
-#fi
 
 echo ""
 echo "Components downloaded in this VHD build (some of the below components might get deleted during cluster provisioning if they are not needed):" >> ${VHD_LOGS_FILEPATH}
@@ -415,50 +381,3 @@ for PATCHED_KUBE_BINARY_VERSION in ${KUBE_BINARY_VERSIONS}; do
 done
 
 echo "install-dependencies step completed successfully"
-#if [[ $OS == $UBUNTU_OS_NAME ]]; then
-#  # remove apport
-#  apt-get purge --auto-remove apport open-vm-tools -y
-#fi
-
-## shellcheck disable=SC2129
-#echo "kubelet/kubectl downloaded:" >> ${VHD_LOGS_FILEPATH}
-#ls -ltr /usr/local/bin/* >> ${VHD_LOGS_FILEPATH}
-
-## shellcheck disable=SC2010
-#ls -ltr /dev/* | grep sgx >>  ${VHD_LOGS_FILEPATH}
-
-#echo -e "=== Installed Packages Begin\n$(listInstalledPackages)\n=== Installed Packages End" >> ${VHD_LOGS_FILEPATH}
-
-#echo "Disk usage:" >> ${VHD_LOGS_FILEPATH}
-#df -h >> ${VHD_LOGS_FILEPATH}
-
-
-## warn at 75% space taken
-#[ -s $(df -P | grep '/dev/sda1' | awk '0+$5 >= 75 {print}') ] || echo "WARNING: 75% of /dev/sda1 is used" >> ${VHD_LOGS_FILEPATH}
-## error at 99% space taken
-#[ -s $(df -P | grep '/dev/sda1' | awk '0+$5 >= 99 {print}') ] || exit 1
-
-#echo "Using kernel:" >> ${VHD_LOGS_FILEPATH}
-#tee -a ${VHD_LOGS_FILEPATH} < /proc/version
-#{
-#  echo "Install completed successfully on " $(date)
-#  echo "VSTS Build NUMBER: ${BUILD_NUMBER}"
-#  echo "VSTS Build ID: ${BUILD_ID}"
-#  echo "Commit: ${COMMIT}"
-#  echo "Ubuntu version: ${UBUNTU_RELEASE}"
-#  echo "Hyperv generation: ${HYPERV_GENERATION}"
-#  echo "Feature flags: ${FEATURE_FLAGS}"
-#  echo "Container runtime: ${CONTAINER_RUNTIME}"
-#  echo "FIPS enabled: ${ENABLE_FIPS}"
-#} >> ${VHD_LOGS_FILEPATH}
-
-#if [[ $(isARM64) != 1 ]]; then
-#  # no asc-baseline-1.1.0-268.arm64.deb
-#  installAscBaseline
-#fi
-
-#if [[ ${UBUNTU_RELEASE} == "18.04" || ${UBUNTU_RELEASE} == "22.04" ]]; then
-#  if [[ ${ENABLE_FIPS,,} == "true" || ${CPU_ARCH} == "arm64" ]]; then
-#    relinkResolvConf
-#  fi
-#fi
