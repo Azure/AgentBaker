353d5f5fbf2cce56db8871253d1e7399dff5cf8b
anujmaheshwari1 <amaheshwari@microsoft.com>
2023-06-26 12:48:39 -0700

    fix: change bool value in e2e template

--- e2e/template.go
+++ e2e/template.go
@@ -437,7 +437,7 @@ func baseTemplate() *datamodel.NodeBootstrappingConfiguration {
 		CustomCATrustConfig:       nil,
 		DisableUnattendedUpgrades: true,
 		SSHStatus:                 0,
-		DisableCustomData:         true,
+		DisableCustomData:         false,
 	}
 }
 
