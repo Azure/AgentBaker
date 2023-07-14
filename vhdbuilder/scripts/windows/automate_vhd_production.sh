342e757251670b56da95b6b3d84397062c215128
wanqingfu <wanqingfu@microsoft.com>
2023-07-14 15:36:30 +0800

    fix: add azlogin and trigger checkin for testing purpose

--- vhdbuilder/scripts/windows/automate_vhd_production.sh
+++ vhdbuilder/scripts/windows/automate_vhd_production.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -euxo pipefail
 
+az login --identity
+
 trigger_pipeline() {
-    az pipelines run --id 188674 # --name "AKS Windows VHD Build -ContainerD&Docker"    
+    #az pipelines run --id 188674 # --name "AKS Windows VHD Build -ContainerD&Docker"    
+    az pipelines run --id 182855 # use the AKS Windows VHD Build - PR check-in gate for trial and error
 }
 trigger_pipeline
