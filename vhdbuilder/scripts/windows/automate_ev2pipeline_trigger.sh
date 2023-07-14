17da390bed98435179fa8554d862894e04aed9aa
wanqingfu <wanqingfu@microsoft.com>
2023-07-14 17:27:57 +0800

    test

--- vhdbuilder/scripts/windows/automate_ev2pipeline_trigger.sh
+++ vhdbuilder/scripts/windows/automate_ev2pipeline_trigger.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -euxo pipefail
 
-source vhdbuilder/scripts/windows/automate_helpers.sh
-
 echo "Triggering windows ev2 artifact pipeline with Build ID 76289801"
 
 build_id="76289801"
+az login --identity
 
-trigger_pipeline() {
+trigger_win_ev2_pipeline() {
     pipeline_id="322622" # [OneBranch][Official] AKS Windows VHD Build EV2 Artifacts
     echo "Build ID for the release is $build_id"
     az pipelines variable update --name VHD_PIPELINE_RUN_ID --pipeline-id $pipeline_id --value $build_id  # Update the VHD_PIPELINE_RUN_ID with the build ID
@@ -15,4 +14,4 @@ trigger_pipeline() {
     # In case auth fails/other issue, we do not want the pipeline to run if the build ID was not correctly updated
     
 }
-trigger_pipeline
+trigger_win_ev2_pipeline
--- vhdbuilder/scripts/windows/automate_vhd_production.sh
+++ vhdbuilder/scripts/windows/automate_vhd_production.sh
@@ -3,9 +3,9 @@ set -euxo pipefail
 
 az login --identity
 
-trigger_pipeline() {
+trigger_win_vhd_prod_pipeline() {
     #az pipelines run --id 188674 # --name "AKS Windows VHD Build -ContainerD&Docker"    
     #az pipelines run --id 182855 # use the AKS Windows VHD Build - PR check-in gate for trial and error
 }
 
-trigger_pipeline
+trigger_win_vhd_prod_pipeline
