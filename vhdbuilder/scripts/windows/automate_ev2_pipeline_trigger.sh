edb5edbb690be4849c29dbe1f5c480acfb7bb377
wanqingfu <wanqingfu@microsoft.com>
2023-07-17 13:57:05 +0800

    ci: ev2 triggering step

--- vhdbuilder/scripts/windows/automate_ev2_pipeline_trigger.sh
+++ vhdbuilder/scripts/windows/automate_ev2_pipeline_trigger.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-# set -euxo pipefail
+set -euxo pipefail
+
+az login --identity
 build_id="76289801"
 
 something_to_do() {
-    echo "Build ID for the release is $build_id"   
+    build_id="76289801"
+    echo "Build ID for the release is $build_id"
+    pipeline_id="322622" # [OneBranch][Official] AKS Windows VHD Build EV2 Artifacts
+    az pipelines variable update --name VHD_PIPELINE_RUN_ID --pipeline-id $pipeline_id --value $build_id  # Update the VHD_PIPELINE_RUN_ID with the build ID
 }
 
 something_to_do
--- vhdbuilder/scripts/windows/automate_vhd_production.sh
+++ vhdbuilder/scripts/windows/automate_vhd_production.sh
@@ -8,4 +8,12 @@ trigger_win_vhd_prod_pipeline() {
     az pipelines run --id 182855 # use the AKS Windows VHD Build - PR check-in gate for trial and error
 }
 
+queue_ev2_pipeline() {
+    build_id="76289801"
+    echo "Build ID for the release is $build_id"
+    pipeline_id="322622" # [OneBranch][Official] AKS Windows VHD Build EV2 Artifacts
+    az pipelines variable update --name VHD_PIPELINE_RUN_ID --pipeline-id $pipeline_id --value $build_id  # Update the VHD_PIPELINE_RUN_ID with the build ID
+}
+
 trigger_win_vhd_prod_pipeline
+# queue_ev2_pipeline
