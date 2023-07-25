fd49989d7d9bd88967697604b5d5b3e303a0f320
Tobias Brick <39196763+tobiasb-ms@users.noreply.github.com>
2023-07-25 10:03:48 -0700

    fix: set umask in a way that cis audits recognize (#3453)

--- vhdbuilder/packer/test/linux-vhd-content-test.sh
+++ vhdbuilder/packer/test/linux-vhd-content-test.sh
@@ -476,6 +476,28 @@ testNetworkSettings() {
   echo "$test:End"
 }
 
+# Ensures that the content /etc/profile.d/umask.sh is correct, per code in
+# <repo-root>/parts/linux/cloud-init/artifacts/cis.sh
+testUmaskSettings() {
+  local test="testUmaskSettings"
+  local settings_file=/etc/profile.d/umask.sh
+  local expected_settings_file_content='umask 027'
+  echo "$test:Start"
+
+  # If the settings file exists, it must just be a single line that sets umask properly.
+  if [[ -f "${settings_file}" && -s "${settings_file}" ]]; then
+    echo "${test}: Checking that ${settings_file} contains '${expected_settings_file_content}'"
+
+    if ! diff "${settings_file}" <(echo "${expected_settings_file_content}") ; then
+      err $test "The content of the file '${settings_file}' must exactly match '${expected_settings_file_content}'. See above for differences"
+    fi
+  else
+    echo "${test}: Settings file '${settings_file}' does not exist or is empty, so not testing contents."
+  fi
+
+  echo "$test:End"
+}
+
 # Tests that the modes on the cron-related files and directories in /etc are set correctly, per the
 # function assignFilePermissions in <repo-root>/parts/linux/cloud-init/artifacts/cis.sh.
 testCronPermissions() {
@@ -832,3 +854,4 @@ testCoreDumpSettings
 testNfsServerService
 testPamDSettings $OS_SKU $OS_VERSION
 testPam $OS_SKU $OS_VERSION
+testUmaskSettings
