386eef0cfec948905c6e9cb1d0824fe0d7c4cb71
anujmaheshwari1 <amaheshwari@microsoft.com>
2023-07-27 01:33:11 -0700

    fix: remove loop for imds curl

--- e2e/scenario/images.go
+++ e2e/scenario/images.go
@@ -2,7 +2,7 @@ package scenario
 
 // These SIG image versions are stored in the ACS test subscription, guarded by resource deletion locks
 var DefaultImageVersionIDs = map[string]string{
-	"ubuntu1804":       "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/1804Gen2/versions/1.1690393198.28720",
+	"ubuntu1804":       "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/1804Gen2/versions/1.1690441692.21456",
 	"ubuntu2204":       "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/2204Gen2/versions/1.1687293262.1409",
 	"marinerv1":        "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/CBLMarinerV1Gen2/versions/1.1687293295.23327",
 	"marinerv2":        "/subscriptions/8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/CBLMarinerV2Gen2/versions/1.1687293291.4523",
--- parts/linux/cloud-init/artifacts/userdata.sh
+++ parts/linux/cloud-init/artifacts/userdata.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -x
-res=$(curl -o /dev/null -w "%{http_code}" -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")
-start="$(date -Isec)"
-echo "started at $start"
-until test "$res" == "200"; do
-   echo "the curl command failed with: $res"
-   res=$(curl -o /dev/null -w "%{http_code}" -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")
-   sleep 1
-done
-echo "started at $start"
+# res=$(curl -o /dev/null -w "%{http_code}" -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")
+# start="$(date -Isec)"
+# echo "started at $start"
+# until test "$res" == "200"; do
+#    echo "the curl command failed with: $res"
+#    res=$(curl -o /dev/null -w "%{http_code}" -fsSL -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")
+#    sleep 1
+# done
+# echo "started at $start"
 
 curl -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance/compute/userData?api-version=2021-01-01&format=text" | base64 -d > csecmd.sh
 chmod +x csecmd.sh
-cat csecmd.sh
+#cat csecmd.sh
 ./csecmd.sh
