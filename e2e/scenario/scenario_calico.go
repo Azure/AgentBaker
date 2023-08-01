d23273056523b03b8ef3bff08a2bb2806d4f8dfd
Will Daly <widaly@microsoft.com>
2023-08-01 15:33:31 -0700

    scenario calico

---
+++ e2e/scenario/scenario_calico.go
@@ -0,0 +1,30 @@
+package scenario
+
+import (
+	"github.com/Azure/agentbaker/pkg/agent/datamodel"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute"
+)
+
+func calico() *Scenario {
+	return &Scenario{
+		Name:        "calico",
+		Description: "Test an Ubuntu 22.04 node configured for Calico NPM",
+		// This is the same as ubuntu2204, except use a larger VM SKU and set netpol Calico.
+		Config: Config{
+			ClusterSelector: NetworkPluginKubenetSelector,
+			ClusterMutator:  NetworkPluginKubenetMutator,
+			BootstrapConfigMutator: func(nbc *datamodel.NodeBootstrappingConfiguration) {
+				nbc.ContainerService.Properties.AgentPoolProfiles[0].Distro = "aks-ubuntu-containerd-22.04-gen2"
+				nbc.AgentPoolProfile.Distro = "aks-ubuntu-containerd-22.04-gen2"
+				nbc.AgentPoolProfile.KubernetesConfig.NetworkPolicy = "calico"
+			},
+			VMConfigMutator: func(vmss *armcompute.VirtualMachineScaleSet) {
+				vmss.Properties.VirtualMachineProfile.StorageProfile.ImageReference = &armcompute.ImageReference{
+					ID: to.Ptr(DefaultImageVersionIDs["ubuntu2204"]),
+				}
+				vmss.SKU.Name = to.Ptr("Standard_D4_v3")
+			},
+		},
+	}
+}
