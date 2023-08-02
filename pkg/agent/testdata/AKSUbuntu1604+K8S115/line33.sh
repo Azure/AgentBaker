613d11b9099e88de682eb11d9d974abfb43c5034
Will Daly <widaly@microsoft.com>
2023-08-02 08:29:48 -0700

    report bio req count

--- pkg/agent/testdata/AKSUbuntu1604+K8S115/line33.sh
+++ pkg/agent/testdata/AKSUbuntu1604+K8S115/line33.sh
@@ -32,7 +32,7 @@ cat <<EOF > /usr/local/bin/iotrace.sh
 
 set -xe
 
-bpftrace -o /var/log/azure/iotrace.log -e 'tracepoint:block:block_rq_issue { @biorqbytes = sum(args->bytes); }  tracepoint:syscalls:sys_exit_read { @reads[comm,pid] = count(); } tracepoint:syscalls:sys_exit_write { @writes[comm,pid] = count(); } interval:s:10 { time("%H:%M:%S\n"); print(@biorqbytes); clear(@biorqbytes); print(@reads); clear(@reads); print(@writes); clear(@writes); }  interval:s:600 { exit(); }'
+bpftrace -o /var/log/azure/iotrace.log -e 'tracepoint:block:block_rq_issue { @biorqcount = count(); @biorqbytes = sum(args->bytes); }  tracepoint:syscalls:sys_exit_read { @reads[comm,pid] = count(); } tracepoint:syscalls:sys_exit_write { @writes[comm,pid] = count(); } interval:s:10 { time("%H:%M:%S\n"); print(@biorqcount); zero(@biorqcount); print(@biorqbytes); zero(@biorqbytes); print(@reads); clear(@reads); print(@writes); clear(@writes); }  interval:s:600 { exit(); }'
 
 EOF
 chmod +x /usr/local/bin/iotrace.sh
