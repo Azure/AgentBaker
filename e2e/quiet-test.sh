64dbb3e8ffa342efffa7311fcd56c308b4147c02
Will Daly <widaly@microsoft.com>
2023-08-02 09:43:32 -0700

    wip

--- e2e/quiet-test.sh
+++ e2e/quiet-test.sh
@@ -8,7 +8,8 @@ shopt -s failglob
 NODE=$1
 
 echo "Removing Calico CNI binaries and conflist"
-kubectl run cleanup --rm --image=alpine -it --privileged=true --overrides="{\"spec\": {\"nodeSelector\": {\"kubernetes.io/hostname\": \"$NODE\"}, \"hostNetwork\":true, \"hostPID\":true}}" -- nsenter --target "1" --mount --uts --ipc --net --pid -- bash -c "rm -f /opt/cni/bin/{calico-ipam,calico,flannel,install} /etc/cni/net.d/{10-calico.conflist,calico-kubeconfig}"
+kubectl delete pods cleanup || true
+cat cleanup.yaml | sed s/NODE/$NODE/g | kubectl apply -f -
 
 CALICOPOD=$(kubectl get pods -n calico-system -l k8s-app=calico-node -owide | grep $NODE | cut -d ' ' -f 1)
 echo "Deleting $CALICOPOD"
