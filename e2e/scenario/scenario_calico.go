851f82fdd0db2707985b252346a1efec0232daed
Will Daly <widaly@microsoft.com>
2023-08-18 15:01:08 -0700

    actually set netpol calico

--- e2e/scenario/scenario_calico.go
+++ e2e/scenario/scenario_calico.go
@@ -17,6 +17,7 @@ func calico() *Scenario {
 			BootstrapConfigMutator: func(nbc *datamodel.NodeBootstrappingConfiguration) {
 				nbc.ContainerService.Properties.AgentPoolProfiles[0].Distro = "aks-ubuntu-containerd-22.04-gen2"
 				nbc.AgentPoolProfile.Distro = "aks-ubuntu-containerd-22.04-gen2"
+				nbc.ContainerService.Properties.OrchestratorProfile.KubernetesConfig.NetworkPolicy = "calico"
 				nbc.AgentPoolProfile.KubernetesConfig.NetworkPolicy = "calico"
 			},
 			VMConfigMutator: func(vmss *armcompute.VirtualMachineScaleSet) {
