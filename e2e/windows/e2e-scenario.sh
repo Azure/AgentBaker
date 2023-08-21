b2e483998b86c41c6703a6808a110952d0f5e20e
Junjie Zhang <junjiezhang@microsoft.com>
2023-08-21 07:40:54 +0000

    fix: remove comments

--- e2e/windows/e2e-scenario.sh
+++ e2e/windows/e2e-scenario.sh
@@ -140,9 +140,6 @@ MC_WIN_VMSS_NAME=$(az vmss list -g $MC_RESOURCE_GROUP_NAME --query "[?contains(n
 VMSS_RESOURCE_Id=$(az resource show --resource-group $MC_RESOURCE_GROUP_NAME --name $MC_WIN_VMSS_NAME --resource-type Microsoft.Compute/virtualMachineScaleSets --query id --output tsv)
 
 az group export --resource-group $MC_RESOURCE_GROUP_NAME --resource-ids $VMSS_RESOURCE_Id --include-parameter-default-value > test.json
-# TODO: use prod image
-# IMAGE_REFERENCE=$(jq -c '.resources[0].properties.virtualMachineProfile.storageProfile.imageReference.id' test.json)
-# IMAGE_REFERENCE="[concat(parameters('galleries_AKSWindows_externalid'), '/images/windows-e2e-test-$WINDOWS_E2E_IMAGE/versions/2023.02.07')]"
 IMAGE_REFERENCE=$(az sig image-version list --gallery-image-definition "windows-e2e-test-$WINDOWS_E2E_IMAGE" --gallery-name $IMAGE_GALLERY_NAME --resource-group $IMAGE_RESOURCE_GROUP --subscription $IMAGE_SUBSCRIPTION_ID  | jq -r '.[-1].id')
 IMAGE_EXTERNALID="/subscriptions/$IMAGE_SUBSCRIPTION_ID/resourceGroups/akswinvhdbuilderrg/providers/Microsoft.Compute/galleries/AKSWindows"
 WINDOWS_VNET=$(jq -c '.parameters | with_entries( select(.key|contains("vnet")))' test.json)
