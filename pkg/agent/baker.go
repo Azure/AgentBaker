71a809a69cd731cc8c2c751c07971720a2046ae8
bsoghigian <bsoghigian@microsoft.com>
2023-09-11 17:33:46 -0700

    chore: exposing gpuDriverVersion as a public method to be consumed by anyone

--- pkg/agent/baker.go
+++ pkg/agent/baker.go
@@ -901,7 +901,7 @@ func getContainerServiceFuncMap(config *datamodel.NodeBootstrappingConfiguration
 			return gpuNeedsFabricManager(profile.VMSize)
 		},
 		"GPUDriverVersion": func() string {
-			return getGPUDriverVersion(profile.VMSize)
+			return GetGPUDriverVersion(profile.VMSize)
 		},
 		"GetHnsRemediatorIntervalInMinutes": func() uint32 {
 			// Only need to enable HNSRemediator for Windows 2019
@@ -964,7 +964,7 @@ func getPortRangeEndValue(portRange string) int {
 // they typically use GRID, not CUDA drivers, and will fail to install CUDA drivers.
 // NVv1 seems to run with CUDA, NVv5 requires GRID.
 // NVv3 is untested on AKS, NVv4 is AMD so n/a, and NVv2 no longer seems to exist (?).
-func getGPUDriverVersion(size string) string {
+func GetGPUDriverVersion(size string) string {
 	if useGridDrivers(size) {
 		return datamodel.Nvidia510GridDriverVersion
 	}
