9228a67e0ceb79683bd287716c70495d87d50a07
Junjie Zhang <109257558+junjiezhang1997@users.noreply.github.com>
2023-09-13 17:58:34 +0800

    chore: add function to get latest windows defender platform update (#3616)

--- vhdbuilder/packer/generate-windows-vhd-configuration.ps1
+++ vhdbuilder/packer/generate-windows-vhd-configuration.ps1
@@ -20,6 +20,11 @@ $global:defaultContainerdPackageUrl = "https://acs-mirror.azureedge.net/containe
 # You must install the April 22, 2021 servicing stack update (SSU) (KB5001407) before installing the latest cumulative update (LCU).
 # SSUs improve the reliability of the update process to mitigate potential issues while installing the LCU.
 
+# defenderUpdateUrl refers to the latest windows defender platform update
+$global:defenderUpdateUrl = "https://go.microsoft.com/fwlink/?linkid=870379&arch=x64"
+# defenderUpdateInfoUrl refers to the info of latest windows defender platform update
+$global:defenderUpdateInfoUrl = "https://go.microsoft.com/fwlink/?linkid=870379&arch=x64&action=info"
+
 switch -Regex ($windowsSku) {
     "2019-containerd" {
         $global:patchUrls = @()
