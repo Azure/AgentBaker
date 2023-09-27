2af416de119e637a9be117540c5f722acd7f9fc9
SeanDougherty <sdougherty@microsoft.com>
2023-09-27 08:23:34 -0700

    feat: allow for specifying managed identity when more than one is pre… (#3635)

--- packer.mk
+++ packer.mk
@@ -59,7 +59,13 @@ endif
 
 az-login:
 	@echo "Logging into Azure with agent VM MSI..."
+ifeq ($(origin MANAGED_IDENTITY_ID), undefined)
+	@echo "Logging in with Hosted Pool's Default Managed Identity"
 	@az login --identity
+else
+	@echo "Logging in with Hosted Pool's Managed Identity: ${MANAGED_IDENTITY_ID}"
+	@az login --identity --username ${MANAGED_IDENTITY_ID}
+endif
 	@az account set -s ${SUBSCRIPTION_ID}
 
 init-packer:
