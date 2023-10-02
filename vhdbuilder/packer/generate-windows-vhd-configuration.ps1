c9cd56fd5e2d94bb6078b04ffa808f9419419ef8
Jaeryn <13284103+jaer-tsun@users.noreply.github.com>
2023-10-02 11:38:19 -0700

    chore: bump windows cni version to v1.4.39.1 for podsubnet and overlay (#3652)
    Co-authored-by: Jaeryn <tsch@microsoft.com>

--- vhdbuilder/packer/generate-windows-vhd-configuration.ps1
+++ vhdbuilder/packer/generate-windows-vhd-configuration.ps1
@@ -156,15 +156,13 @@ $global:map = @{
     );
     "c:\akse-cache\win-vnet-cni\" = @(
         # Azure CNI v1 (legacy)
-        "https://acs-mirror.azureedge.net/azure-cni/v1.4.35/binaries/azure-vnet-cni-singletenancy-windows-amd64-v1.4.35.zip",
-        "https://acs-mirror.azureedge.net/azure-cni/v1.5.6/binaries/azure-vnet-cni-singletenancy-windows-amd64-v1.5.6.zip",
         "https://acs-mirror.azureedge.net/azure-cni/v1.5.6.1/binaries/azure-vnet-cni-singletenancy-windows-amd64-v1.5.6.1.zip",
-        # Azure CNI v2 (pod subnet) upgrading from v1.4.35 to v1.5.5
+        # Azure CNI v2 (pod subnet) upgrading from v1.4.35 to v1.4.39.1
         "https://acs-mirror.azureedge.net/azure-cni/v1.4.35/binaries/azure-vnet-cni-singletenancy-swift-windows-amd64-v1.4.35.zip",
-        "https://acs-mirror.azureedge.net/azure-cni/v1.5.5/binaries/azure-vnet-cni-singletenancy-swift-windows-amd64-v1.5.5.zip",
-        # Azure CNI for Overlay upgrading from v1.4.35_Win2019OverlayFix to v1.5.5
+        "https://acs-mirror.azureedge.net/azure-cni/v1.4.39.1/binaries/azure-vnet-cni-singletenancy-swift-windows-amd64-v1.4.39.1.zip",
+        # Azure CNI for Overlay upgrading from v1.4.35_Win2019OverlayFix to v1.4.39.1
         "https://acs-mirror.azureedge.net/azure-cni/v1.4.35_Win2019OverlayFix/binaries/azure-vnet-cni-singletenancy-overlay-windows-amd64-v1.4.35_Win2019OverlayFix.zip",
-        "https://acs-mirror.azureedge.net/azure-cni/v1.5.5/binaries/azure-vnet-cni-singletenancy-overlay-windows-amd64-v1.5.5.zip"
+        "https://acs-mirror.azureedge.net/azure-cni/v1.4.39.1/binaries/azure-vnet-cni-singletenancy-overlay-windows-amd64-v1.4.39.1.zip"
     );
     "c:\akse-cache\calico\" = @(
         "https://acs-mirror.azureedge.net/calico-node/v3.21.6/binaries/calico-windows-v3.21.6.zip",
