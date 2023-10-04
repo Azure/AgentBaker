f5a015107703fc27255aa7aa7bf184facc9f7368
anujmaheshwari1 <amaheshwari@microsoft.com>
2023-10-04 12:18:53 -0700

    fix: change filter for dpkg

--- vhdbuilder/packer/pre-install-dependencies.sh
+++ vhdbuilder/packer/pre-install-dependencies.sh
@@ -91,15 +91,15 @@ fi
 
 if [[ "${UBUNTU_RELEASE}" == "22.04" ]]; then
   echo "Logging the currently running kernel: $(uname -r)"
-  echo "Before purging kernel, here is a list of kernels/headers installed:"; dpkg --list | grep 'linux-image\|linux-headers'
+  echo "Before purging kernel, here is a list of kernels/headers installed:"; dpkg -l 'linux-*azure*'
 
   # Purge all current kernels and dependencies
   DEBIAN_FRONTEND=noninteractive apt-get remove --purge -y $(dpkg-query -W 'linux-*azure*' | awk '$2 != "" { print $1 }' | paste -s)
-  echo "After purging kernel, dpkg list should be empty"; dpkg --list | grep 'linux-image\|linux-headers'
+  echo "After purging kernel, dpkg list should be empty"; dpkg -l 'linux-*azure*'
 
   # Install lts-22.04 kernel
   DEBIAN_FRONTEND=noninteractive apt-get install -y linux-image-azure-lts-22.04 linux-cloud-tools-azure-lts-22.04 linux-headers-azure-lts-22.04 linux-modules-extra-azure-lts-22.04 linux-tools-azure-lts-22.04
-  echo "After installing new kernel, here is a list of kernels/headers installed"; dpkg --list | grep 'linux-image\|linux-headers'
+  echo "After installing new kernel, here is a list of kernels/headers installed"; dpkg -l 'linux-*azure*'
   
   update-grub
 fi
