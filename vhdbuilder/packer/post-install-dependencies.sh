df91d56f5e35600385c3d9606932bfbe9148db9f
Henry Beberman <henry.beberman@microsoft.com>
2023-10-06 11:33:49 -0700

    refactor: move os-release earlier in vhd build log

--- vhdbuilder/packer/post-install-dependencies.sh
+++ vhdbuilder/packer/post-install-dependencies.sh
@@ -60,6 +60,10 @@ usage=${usage%.*}
 [ ${usage} -ge 99 ] && echo "ERROR: root partition on OS device (${os_device}) already passed 99% of the 30GB cap!" && exit 1
 [ ${usage} -ge 75 ] && echo "WARNING: root partition on OS device (${os_device}) already passed 75% of the 30GB cap!"
 
+echo -e "=== os-release Begin" >> ${VHD_LOGS_FILEPATH}
+cat /etc/os-release >> ${VHD_LOGS_FILEPATH}
+echo -e "=== os-release End" >> ${VHD_LOGS_FILEPATH}
+
 echo "Using kernel:" >> ${VHD_LOGS_FILEPATH}
 tee -a ${VHD_LOGS_FILEPATH} < /proc/version
 {
@@ -73,10 +77,6 @@ tee -a ${VHD_LOGS_FILEPATH} < /proc/version
   echo "Container runtime: ${CONTAINER_RUNTIME}"
   echo "FIPS enabled: ${ENABLE_FIPS}"
 } >> ${VHD_LOGS_FILEPATH}
-
-echo -e "=== os-release Begin" >> ${VHD_LOGS_FILEPATH}
-cat /etc/os-release >> ${VHD_LOGS_FILEPATH}
-echo -e "=== os-release End" >> ${VHD_LOGS_FILEPATH}
 
 if [[ $(isARM64) != 1 ]]; then
   # no asc-baseline-1.1.0-268.arm64.deb
