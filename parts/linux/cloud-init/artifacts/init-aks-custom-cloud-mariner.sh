eece6de75039fbe11a6a590a7ce86869972365cc
Mikołaj Umański <mik.umanski@gmail.com>
2023-10-16 15:45:20 -0700

    chore: add repo depot for mariner in AGC (#3702)
    Co-authored-by: Mikolaj Umanski <mumanski@microsoft.com>

--- parts/linux/cloud-init/artifacts/init-aks-custom-cloud-mariner.sh
+++ parts/linux/cloud-init/artifacts/init-aks-custom-cloud-mariner.sh
@@ -16,7 +16,16 @@ cp /root/AzureCACertificates/*.crt /etc/pki/ca-trust/source/anchors/
 
 cloud-init status --wait
 
-# TODO - Set the repoDepotEndpoint in a .repo file if package update becomes necessary
+marinerRepoDepotEndpoint="$(echo "${REPO_DEPOT_ENDPOINT}" | sed 's/\/ubuntu//')"
+if [[ "$marinerRepoDepotEndpoint" == "" ]]; then
+  >&2 echo "repo depot endpoint empty while running custom-cloud init script"
+else
+  for f in /etc/yum.repos.d/*.repo
+  do
+      sed -i -e "s|https://packages.microsoft.com|${marinerRepoDepotEndpoint}/mariner/packages.microsoft.com|" $f
+      echo "## REPO - $f - MODIFIED"
+  done
+fi
 
 # Set the chrony config to use the PHC /dev/ptp0 clock
 cat > /etc/chrony.conf <<EOF
