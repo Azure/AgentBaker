bedcfcbcb18e8226844599f4764569fa019d4be1
Cameron Meissner <cameissner@microsoft.com>
2023-10-17 16:05:37 -0700

    refactor: remove secure TLS bootstrap AAD server ID from NBC

--- pkg/agent/baker.go
+++ pkg/agent/baker.go
@@ -387,9 +387,6 @@ func getContainerServiceFuncMap(config *datamodel.NodeBootstrappingConfiguration
 		"GetTLSBootstrapTokenForKubeConfig": func() string {
 			return GetTLSBootstrapTokenForKubeConfig(config.KubeletClientTLSBootstrapToken)
 		},
-		"GetSecureTLSBootstrapAADServerApplicationID": func() string {
-			return config.SecureTLSBootstrapAADServerApplicationID
-		},
 		"GetKubeletConfigKeyVals": func() string {
 			return GetOrderedKubeletConfigFlagString(config.KubeletConfig, cs, profile, config.EnableKubeletConfigFile)
 		},
