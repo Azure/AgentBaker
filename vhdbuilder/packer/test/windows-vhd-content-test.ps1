6b791a63c04645707e7894b246b53eb7c395cf0d
Junjie Zhang <junjiezhang@microsoft.com>
2023-10-23 09:28:51 +0000

    fix: use ConvertTo-Json

--- vhdbuilder/packer/test/windows-vhd-content-test.ps1
+++ vhdbuilder/packer/test/windows-vhd-content-test.ps1
@@ -289,11 +289,11 @@ function Test-ValidateSinglePackageSignature {
         Remove-Item -Path $installDir -Force -Recurse
     }
 
-    $AllNotSignedFiles = (echo $AllNotSignedFiles | Format-Wide -AutoSize | Out-String)
+    $AllNotSignedFiles = (echo $AllNotSignedFiles | ConvertTo-Json -Compress)
     Write-Output "Under folder $dir, all not signed file in cached packages are: $AllNotSignedFiles"
 
     if ($NotSignedResult.Count -ne 0) {
-        $NotSignedResult = (echo $NotSignedResult | Format-Wide -AutoSize | Out-String)
+        $NotSignedResult = (echo $NotSignedResult | ConvertTo-Json -Compress)
         Write-ErrorWithTimestamp "Under folder $dir, binaries in $NotSignedResult are not signed"
         exit 1
     }
