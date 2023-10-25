1b46f4a75a748ef869ed31f1d5f020ac277ccd23
Hongbin Lu <hongbin034@gmail.com>
2023-10-25 13:08:33 +0800

    Add systemd unit for ubuntu live patching (#3643)

--- pkg/agent/const.go
+++ pkg/agent/const.go
@@ -71,6 +71,9 @@ const (
 	reconcilePrivateHostsService              = "linux/cloud-init/artifacts/reconcile-private-hosts.service"
 	bindMountScript                           = "linux/cloud-init/artifacts/bind-mount.sh"
 	bindMountSystemdService                   = "linux/cloud-init/artifacts/bind-mount.service"
+	snapshotUpdateScript                      = "linux/cloud-init/artifacts/ubuntu/ubuntu-snapshot-update.sh"
+	snapshotUpdateSystemdService              = "linux/cloud-init/artifacts/ubuntu/snapshot-update.service"
+	snapshotUpdateSystemdTimer                = "linux/cloud-init/artifacts/ubuntu/snapshot-update.timer"
 	migPartitionScript                        = "linux/cloud-init/artifacts/mig-partition.sh"
 	migPartitionSystemdService                = "linux/cloud-init/artifacts/mig-partition.service"
 
