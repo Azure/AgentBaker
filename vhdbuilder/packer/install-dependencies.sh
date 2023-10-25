e0b9cebe31eabd59b3ffbd4f2080b081d92e4ee0
anujmaheshwari1 <amaheshwari@microsoft.com>
2023-10-25 15:39:06 -0700

    chore: fix cni versions in install deps

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -291,9 +291,8 @@ unpackAzureCNI() {
 
 #must be both amd64/arm64 images
 VNET_CNI_VERSIONS="
-1.5.5
+1.5.11
 1.4.43.1
-1.4.43
 "
 
 
@@ -307,9 +306,8 @@ done
 #UNITE swift and overlay versions?
 #Please add new version (>=1.4.13) in this section in order that it can be pulled by both AMD64/ARM64 vhd
 SWIFT_CNI_VERSIONS="
-1.5.5
+1.5.11
 1.4.43.1
-1.4.43
 "
 
 for SWIFT_CNI_VERSION in $SWIFT_CNI_VERSIONS; do
