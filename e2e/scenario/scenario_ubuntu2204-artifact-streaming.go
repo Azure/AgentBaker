4d78ed949f8007b44c4e4e9c3b4f3d91d84ff822
Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>
2023-11-04 19:12:48 -0700

    test: artifact streaming e2e

--- e2e/scenario/scenario_ubuntu2204-artifact-streaming.go
+++ e2e/scenario/scenario_ubuntu2204-artifact-streaming.go
@@ -9,24 +9,12 @@ import (
 func ubuntu2204ArtifactStreaming() *Scenario {
 	return &Scenario{
 		Name:        "ubuntu2204-artifact-streaming",
-		Description: "tests that an ubuntu 2204 VHD can be properly bootstrapped when artifact streaming is enabled",
+		Description: "tests that a new ubuntu 2204 node using artifact streaming can be properly bootstrapepd",
 		Config: Config{
 			ClusterSelector: NetworkPluginKubenetSelector,
 			ClusterMutator:  NetworkPluginKubenetMutator,
 			BootstrapConfigMutator: func(nbc *datamodel.NodeBootstrappingConfiguration) {
-				customLinuxConfig := &datamodel.CustomLinuxOSConfig{
-					Sysctls: &datamodel.SysctlConfig{
-						NetNetfilterNfConntrackMax:     to.Ptr(stringToInt32(customSysctls["net.netfilter.nf_conntrack_max"])),
-						NetNetfilterNfConntrackBuckets: to.Ptr(stringToInt32(customSysctls["net.netfilter.nf_conntrack_buckets"])),
-						NetIpv4IpLocalPortRange:        customSysctls["net.ipv4.ip_local_port_range"],
-						NetIpv4TcpkeepaliveIntvl:       to.Ptr(stringToInt32(customSysctls["net.ipv4.tcp_keepalive_intvl"])),
-					},
-					UlimitConfig: &datamodel.UlimitConfig{
-						MaxLockedMemory: "75000",
-						NoFile:          "1048",
-					},
-				}
-				nbc.AgentPoolProfile.EnableArtifactStreaming = true
+				nbc.EnableArtifactStreaming = true
 				nbc.ContainerService.Properties.AgentPoolProfiles[0].Distro = "aks-ubuntu-containerd-22.04-gen2"
 				nbc.AgentPoolProfile.Distro = "aks-ubuntu-containerd-22.04-gen2"
 			},
@@ -34,6 +22,9 @@ func ubuntu2204ArtifactStreaming() *Scenario {
 				vmss.Properties.VirtualMachineProfile.StorageProfile.ImageReference = &armcompute.ImageReference{
 					ID: to.Ptr(DefaultImageVersionIDs["ubuntu2204"]),
 				}
+			},
+			LiveVMValidators: []*LiveVMValidator{
+				NonEmptyDirectoryValidator("/etc/overlaybd"),
 			},
 		},
 	}
