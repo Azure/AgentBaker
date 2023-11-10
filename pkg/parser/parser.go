9a164df5b927332ceee46c2d1ff826c322253759
Cameron Meissner <cameissner@microsoft.com>
2023-11-10 13:16:16 -0800

    chore: parse example

---
+++ pkg/parser/parser.go
@@ -0,0 +1,9 @@
+package parser
+
+import (
+	nbcontractv1 "github.com/Azure/agentbaker/pkg/proto/nbcontract/v1"
+)
+
+func parse() *nbcontractv1.Configuration {
+	return &nbcontractv1.Configuration{}
+}
---
+++ pkg/parser/parser_test.go
@@ -0,0 +1,12 @@
+package parser
+
+import (
+	"testing"
+)
+
+func TestPar(t *testing.T) {
+	config := parse()
+	if config == nil {
+		t.Fatalf("it's nil :(")
+	}
+}
