7b2a7338d5f2ebd140e0e06d5a33c929b01d43eb
Devin Wong <wongsiosun@outlook.com>
2023-11-14 23:54:34 +0000

    fix lints

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -27,18 +27,21 @@ func executeBootstrapTemplate(inputContract *nbcontractv1.Configuration) (string
 // this function will eventually take a pointer to the bootstrap contract struct.
 // it will then template out the variables into the final bootstrap trigger script.
 func Parse() {
-	inputJson, err := json.Marshal(getBaseTemplate())
+	inputJSON, err := json.Marshal(getBaseTemplate())
 	if err != nil {
-		log.Printf("Failed to Marshal the nbcontractv1 to json: %v", err)
+		log.Printf("Failed to marshal the nbcontractv1 to json: %v", err)
 	}
 	log.Println("Input Json: ")
-	log.Println(string(inputJson))
+	log.Println(string(inputJSON))
 
-	//inputJson above will be provided by bootstrappers. We are using getBaseTemplate() for dev/test purpose for now.
-	//We can further move it to other file for unit tests later.
+	// inputJson above will be provided by bootstrappers. We are using getBaseTemplate() for dev/test purpose for now.
+	// We can further move it to other file for unit tests later.
 
 	inputContract := nbcontractv1.Configuration{}
-	json.Unmarshal(inputJson, &inputContract)
+	json.Unmarshal(inputJSON, &inputContract)
+	if err != nil {
+		log.Printf("Failed to unmarshal the json to nbcontractv1: %v", err)
+	}
 	triggerBootstrapScript, err := executeBootstrapTemplate(&inputContract)
 	if err != nil {
 		log.Printf("Failed to execute the template: %v", err)
@@ -60,5 +63,4 @@ func getBaseTemplate() *nbcontractv1.Configuration {
 		CustomKubeBinaryUrl: "test custom kube binary Url",
 		KubeproxyUrl:        "test kube proxy Url",
 	}
-
 }
