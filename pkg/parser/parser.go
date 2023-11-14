4589b58c6cf8917562c642043561d85c9be78a8b
anujmaheshwari1 <amaheshwari@microsoft.com>
2023-11-13 16:06:13 -0800

    fix: end comments with godot

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -21,8 +21,8 @@ func executeBootstrapTemplate() (string, error) {
 	return buffer.String(), nil
 }
 
-// this function will eventually take a pointer to the bootstrap contract struct
-// it will then emplate out the variables into the final bootstrap trigger script
+// this function will eventually take a pointer to the bootstrap contract struct.
+// it will then emplate out the variables into the final bootstrap trigger script.
 func Parse() {
 	triggerBootstrapScript, err := executeBootstrapTemplate()
 	if err != nil {
