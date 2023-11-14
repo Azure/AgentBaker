fabf80d51c377e4dfb2ebfad5046df0b5b65c21d
anujmaheshwari1 <amaheshwari@microsoft.com>
2023-11-13 16:01:24 -0800

    chore: add parser skeleton

---
+++ pkg/parser/cse_cmd.sh.gtpl
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "hello"
+#This is a simple placeholder script, but will eventually resemble what cse_cmd.sh looks today
---
+++ pkg/parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"text/template"
+)
+
+var (
+	//go:embed cse_cmd.sh.gtpl
+	bootstrapTrigger         string
+	bootstrapTriggerTemplate = template.Must(template.New("triggerBootstrapScript").Parse(bootstrapTrigger))
+)
+
+func executeBootstrapTemplate() (string, error) {
+	var buffer bytes.Buffer
+	if err := bootstrapTriggerTemplate.Execute(&buffer, nil); err != nil {
+		return "", fmt.Errorf("error executing bootstrap trigger template: %w", err)
+	}
+	return buffer.String(), nil
+}
+
+// this function will eventually take a pointer to the bootstrap contract struct
+// it will then emplate out the variables into the final bootstrap trigger script
+func Parse() {
+	triggerBootstrapScript, err := executeBootstrapTemplate()
+	if err != nil {
+		fmt.Errorf("failed to execute the template")
+	}
+
+	fmt.Println(triggerBootstrapScript)
+}
