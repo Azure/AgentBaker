72d89371f4c90f45888068539cd15aadc4f52a8b
anujmaheshwari1 <anujmaheshwari600@gmail.com>
2023-11-14 09:11:11 -0800

    chore: add basic skeleton for contract parser (#3826)
    Co-authored-by: anujmaheshwari1 <amaheshwari@microsoft.com>

---
+++ pkg/parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"bytes"
+	_ "embed"
+	"log"
+	"text/template"
+)
+
+var (
+	//go:embed cse_cmd.sh.gtpl
+	bootstrapTrigger         string
+	bootstrapTriggerTemplate = template.Must(template.New("triggerBootstrapScript").Parse(bootstrapTrigger)) //nolint:gochecknoglobals
+)
+
+func executeBootstrapTemplate() (string, error) {
+	var buffer bytes.Buffer
+	if err := bootstrapTriggerTemplate.Execute(&buffer, nil); err != nil {
+		return "", err
+	}
+	return buffer.String(), nil
+}
+
+// this function will eventually take a pointer to the bootstrap contract struct.
+// it will then template out the variables into the final bootstrap trigger script.
+func Parse() {
+	triggerBootstrapScript, err := executeBootstrapTemplate()
+	if err != nil {
+		log.Printf("Failed to execute the template: %v", err)
+	}
+
+	log.Println(triggerBootstrapScript)
+}
