965efb7d2a42f51408282fd7db865362efff876e
Junjie Zhang <junjiezhang@microsoft.com>
2023-11-28 02:23:18 +0000

    chore: update registry key for 23H2

--- vhdbuilder/packer/configure-windows-vhd.ps1
+++ vhdbuilder/packer/configure-windows-vhd.ps1
@@ -492,7 +492,7 @@ function Update-Registry {
         Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Policies\Microsoft\FeatureManagement\Overrides" -Name 1102009996 -Value 1 -Type DWORD
     }
 
-    if ($env:WindowsSKU -Like '2022*') {
+    if (($env:WindowsSKU -Like '2022*') -or ($env:WindowsSKU -Like '23H2*')) {
         Write-Log "Enable a WCIFS fix in 2022-10B"
         $regPath=(Get-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Policies\Microsoft" -ErrorAction Ignore)
         if (!$regPath) {
