d9647de43e153f44c893d824ebc9bd1df9f132ee
anujmaheshwari1 <anujmaheshwari600@gmail.com>
2023-12-18 16:17:07 -0800

    feat: add custom linux os config to contract (#3929)
    Co-authored-by: anujmaheshwari1 <amaheshwari@microsoft.com>
    Co-authored-by: Mikołaj Umański <mik.umanski@gmail.com>

--- pkg/proto/nbcontract/v1/gpuconfig.pb.go
+++ pkg/proto/nbcontract/v1/gpuconfig.pb.go
@@ -20,23 +20,23 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
-type GpuConfig struct {
+type GPUConfig struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
 	// nvidia_state represents the Nvidia GPU state
 	NvidiaState *FeatureState `protobuf:"varint,1,opt,name=nvidia_state,json=nvidiaState,proto3,enum=nbcontract.v1.FeatureState,oneof" json:"nvidia_state,omitempty"`
-	// config_gpu_driver represents if bootstrap process should install and configure the GPU driver when necessary. Configuration includes appropriate set up of components like the fabric manager where applicable.
+	// config_gpu_driver specifies whether bootstrap process should install and configure the GPU driver when necessary. Configuration includes appropriate set up of components like the fabric manager where applicable.
 	ConfigGpuDriver *FeatureState `protobuf:"varint,2,opt,name=config_gpu_driver,json=configGpuDriver,proto3,enum=nbcontract.v1.FeatureState,oneof" json:"config_gpu_driver,omitempty"`
-	// gpu_device_plugin represents if special config is needed for MIG GPUs that use GPU dedicated VHDs and enable the device plugin (for all GPU dedicated VHDs)
+	// gpu_device_plugin specifies whether special config is needed for MIG GPUs that use GPU dedicated VHDs and enable the device plugin (for all GPU dedicated VHDs)
 	GpuDevicePlugin *FeatureState `protobuf:"varint,3,opt,name=gpu_device_plugin,json=gpuDevicePlugin,proto3,enum=nbcontract.v1.FeatureState,oneof" json:"gpu_device_plugin,omitempty"`
 	// gpu_instance_profile represents the GPU instance profile.
 	GpuInstanceProfile *string `protobuf:"bytes,4,opt,name=gpu_instance_profile,json=gpuInstanceProfile,proto3,oneof" json:"gpu_instance_profile,omitempty"`
 }
 
-func (x *GpuConfig) Reset() {
-	*x = GpuConfig{}
+func (x *GPUConfig) Reset() {
+	*x = GPUConfig{}
 	if protoimpl.UnsafeEnabled {
 		mi := &file_pkg_proto_nbcontract_v1_gpuconfig_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -44,13 +44,13 @@ func (x *GpuConfig) Reset() {
 	}
 }
 
-func (x *GpuConfig) String() string {
+func (x *GPUConfig) String() string {
 	return protoimpl.X.MessageStringOf(x)
 }
 
-func (*GpuConfig) ProtoMessage() {}
+func (*GPUConfig) ProtoMessage() {}
 
-func (x *GpuConfig) ProtoReflect() protoreflect.Message {
+func (x *GPUConfig) ProtoReflect() protoreflect.Message {
 	mi := &file_pkg_proto_nbcontract_v1_gpuconfig_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -62,33 +62,33 @@ func (x *GpuConfig) ProtoReflect() protoreflect.Message {
 	return mi.MessageOf(x)
 }
 
-// Deprecated: Use GpuConfig.ProtoReflect.Descriptor instead.
-func (*GpuConfig) Descriptor() ([]byte, []int) {
+// Deprecated: Use GPUConfig.ProtoReflect.Descriptor instead.
+func (*GPUConfig) Descriptor() ([]byte, []int) {
 	return file_pkg_proto_nbcontract_v1_gpuconfig_proto_rawDescGZIP(), []int{0}
 }
 
-func (x *GpuConfig) GetNvidiaState() FeatureState {
+func (x *GPUConfig) GetNvidiaState() FeatureState {
 	if x != nil && x.NvidiaState != nil {
 		return *x.NvidiaState
 	}
 	return FeatureState_FEATURE_STATE_UNSPECIFIED
 }
 
-func (x *GpuConfig) GetConfigGpuDriver() FeatureState {
+func (x *GPUConfig) GetConfigGpuDriver() FeatureState {
 	if x != nil && x.ConfigGpuDriver != nil {
 		return *x.ConfigGpuDriver
 	}
 	return FeatureState_FEATURE_STATE_UNSPECIFIED
 }
 
-func (x *GpuConfig) GetGpuDevicePlugin() FeatureState {
+func (x *GPUConfig) GetGpuDevicePlugin() FeatureState {
 	if x != nil && x.GpuDevicePlugin != nil {
 		return *x.GpuDevicePlugin
 	}
 	return FeatureState_FEATURE_STATE_UNSPECIFIED
 }
 
-func (x *GpuConfig) GetGpuInstanceProfile() string {
+func (x *GPUConfig) GetGpuInstanceProfile() string {
 	if x != nil && x.GpuInstanceProfile != nil {
 		return *x.GpuInstanceProfile
 	}
@@ -104,7 +104,7 @@ var file_pkg_proto_nbcontract_v1_gpuconfig_proto_rawDesc = []byte{
 	0x74, 0x72, 0x61, 0x63, 0x74, 0x2e, 0x76, 0x31, 0x1a, 0x22, 0x70, 0x6b, 0x67, 0x2f, 0x70, 0x72,
 	0x6f, 0x74, 0x6f, 0x2f, 0x6e, 0x62, 0x63, 0x6f, 0x6e, 0x74, 0x72, 0x61, 0x63, 0x74, 0x2f, 0x76,
 	0x31, 0x2f, 0x65, 0x6e, 0x75, 0x6d, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0xf9, 0x02, 0x0a,
-	0x09, 0x47, 0x70, 0x75, 0x43, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x12, 0x43, 0x0a, 0x0c, 0x6e, 0x76,
+	0x09, 0x47, 0x50, 0x55, 0x43, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x12, 0x43, 0x0a, 0x0c, 0x6e, 0x76,
 	0x69, 0x64, 0x69, 0x61, 0x5f, 0x73, 0x74, 0x61, 0x74, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0e,
 	0x32, 0x1b, 0x2e, 0x6e, 0x62, 0x63, 0x6f, 0x6e, 0x74, 0x72, 0x61, 0x63, 0x74, 0x2e, 0x76, 0x31,
 	0x2e, 0x46, 0x65, 0x61, 0x74, 0x75, 0x72, 0x65, 0x53, 0x74, 0x61, 0x74, 0x65, 0x48, 0x00, 0x52,
@@ -156,13 +156,13 @@ func file_pkg_proto_nbcontract_v1_gpuconfig_proto_rawDescGZIP() []byte {
 
 var file_pkg_proto_nbcontract_v1_gpuconfig_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
 var file_pkg_proto_nbcontract_v1_gpuconfig_proto_goTypes = []interface{}{
-	(*GpuConfig)(nil), // 0: nbcontract.v1.GpuConfig
+	(*GPUConfig)(nil), // 0: nbcontract.v1.GPUConfig
 	(FeatureState)(0), // 1: nbcontract.v1.FeatureState
 }
 var file_pkg_proto_nbcontract_v1_gpuconfig_proto_depIdxs = []int32{
-	1, // 0: nbcontract.v1.GpuConfig.nvidia_state:type_name -> nbcontract.v1.FeatureState
-	1, // 1: nbcontract.v1.GpuConfig.config_gpu_driver:type_name -> nbcontract.v1.FeatureState
-	1, // 2: nbcontract.v1.GpuConfig.gpu_device_plugin:type_name -> nbcontract.v1.FeatureState
+	1, // 0: nbcontract.v1.GPUConfig.nvidia_state:type_name -> nbcontract.v1.FeatureState
+	1, // 1: nbcontract.v1.GPUConfig.config_gpu_driver:type_name -> nbcontract.v1.FeatureState
+	1, // 2: nbcontract.v1.GPUConfig.gpu_device_plugin:type_name -> nbcontract.v1.FeatureState
 	3, // [3:3] is the sub-list for method output_type
 	3, // [3:3] is the sub-list for method input_type
 	3, // [3:3] is the sub-list for extension type_name
@@ -178,7 +178,7 @@ func file_pkg_proto_nbcontract_v1_gpuconfig_proto_init() {
 	file_pkg_proto_nbcontract_v1_enum_proto_init()
 	if !protoimpl.UnsafeEnabled {
 		file_pkg_proto_nbcontract_v1_gpuconfig_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*GpuConfig); i {
+			switch v := v.(*GPUConfig); i {
 			case 0:
 				return &v.state
 			case 1:
