4418b4e58d5acbde717b899c7e0bc75780e9acab
Alison Burgess <alburgess@microsoft.com>
2024-01-16 11:59:18 -0800

    feat: fix nits

--- vhdbuilder/scripts/cni-prefetch-driver.sh
+++ vhdbuilder/scripts/cni-prefetch-driver.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 CNI_PREFETCH_SCRIPT_PATH="/opt/azure/containers/cni-prefetch.sh"
-if [[ ! -f "$CNI_PREFETCH_SCRIPT_PATH" ]]; then
-    echo "CNI Prefetch file does not exist at: $CNI_PREFETCH_SCRIPT_PATH"
+if [ ! -f "$CNI_PREFETCH_SCRIPT_PATH" ]; then
+    echo "CNI prefetch driver script does not exist at: $CNI_PREFETCH_SCRIPT_PATH"
     exit 1
 fi
 
 chmod +x $CNI_PREFETCH_SCRIPT_PATH
-echo "running cni-prefetch.sh ..."
+echo "running CNI prefetch driver script at $CNI_PREFETCH_SCRIPT_PATH..."
 $CNI_PREFETCH_SCRIPT_PATH
