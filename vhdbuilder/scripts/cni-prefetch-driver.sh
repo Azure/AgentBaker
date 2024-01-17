49119a7e49bacf9e706ef4e842a3c64b96cd9a83
Alison Burgess <alburgess@microsoft.com>
2024-01-17 13:27:22 -0800

    feat: make the variables more generic

--- vhdbuilder/scripts/cni-prefetch-driver.sh
+++ vhdbuilder/scripts/cni-prefetch-driver.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-CNI_PREFETCH_SCRIPT_PATH="/opt/azure/containers/prefetch.sh"
+CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH="/opt/azure/containers/prefetch.sh"
 
-if [ ! -f "$CNI_PREFETCH_SCRIPT_PATH" ]; then
-    echo "CNI prefetch script does not exist at $CNI_PREFETCH_SCRIPT_PATH"
-    return 1
+if [ ! -f "$CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH" ]; then
+    echo "container image prefetch script path does not exist at $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH"
+    exit 1
 fi
 
-chmod +x $CNI_PREFETCH_SCRIPT_PATH
-echo "running CNI prefetch driver script at $CNI_PREFETCH_SCRIPT_PATH..."
-sudo /bin/bash $CNI_PREFETCH_SCRIPT_PATH
-echo "CNI prefetch driver script completed successfully"
+chmod +x $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH
+echo "running container image prefetch script at $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH..."
+sudo /bin/bash $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH
+echo "container image prefetch script completed successfully"
 
-echo "deleting CNI prefetch driver script at $CNI_PREFETCH_SCRIPT_PATH..."
-rm -- "$0"
-echo "CNI prefetch driver script deleted"
+echo "deleting container image prefetch script at $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH..."
+rm -f $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH
+echo "container image prefetch script deleted"
