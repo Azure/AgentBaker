7e0cdf058ad9d51bdbe65decd050ab8267b3b6bb
Alison Burgess <alburgess@microsoft.com>
2024-01-16 16:55:00 -0800

    feat: fix echo

--- vhdbuilder/scripts/cni-prefetch-driver.sh
+++ vhdbuilder/scripts/cni-prefetch-driver.sh
@@ -2,7 +2,7 @@
 CNI_PREFETCH_SCRIPT_PATH="/opt/azure/containers/prefetch.sh"
 
 if [ ! -f "$CNI_PREFETCH_SCRIPT_PATH" ]; then
-    err "$test: CNI prefetch script does not exist at $CNI_PREFETCH_SCRIPT_PATH"
+    echo "CNI prefetch script does not exist at $CNI_PREFETCH_SCRIPT_PATH"
     return 1
 fi
 
