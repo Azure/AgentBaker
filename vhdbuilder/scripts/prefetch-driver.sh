cfc72239f1639910ab80b25588b97cc293adc27e
Cameron Meissner <cameronmeissner@gmail.com>
2024-01-24 12:18:21 -0800

    refactor: no-op in prefetch driver when prefetch script does not exist (#3995)
    Co-authored-by: Cameron Meissner <cameissner@microsoft.com>

--- vhdbuilder/scripts/prefetch-driver.sh
+++ vhdbuilder/scripts/prefetch-driver.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+set -eux
+
 CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH="/opt/azure/containers/prefetch.sh"
 
 if [ ! -f "$CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH" ]; then
-    echo "container image prefetch script path does not exist at $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH"
-    exit 1
+    echo "container image prefetch script path does not exist at $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH, exiting early..."
+    exit 0
 fi
 
-chmod +x $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH
 echo "running container image prefetch script at $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH..."
-sudo /bin/bash $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH
+sudo chmod +x $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH || exit $?
+sudo /bin/bash $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH || exit $?
 echo "container image prefetch script completed successfully"
 
-echo "deleting container image prefetch script at $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH..."
-rm -f $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH
-echo "container image prefetch script deleted"
+echo "removing container image prefetch script at $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH..."
+sudo rm -f $CONTAINER_IMAGE_PREFETCH_SCRIPT_PATH || exit $?
+echo "removed container image prefetch script"
