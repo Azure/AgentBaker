c35a822631d21edf3913aa8646284a8ec0d8b34a
Patrick W. Healy <patrick.healy@microsoft.com>
2024-02-08 07:07:43 -0600

    move aks-local-dns later in the install sequence so that jq is there

--- vhdbuilder/packer/pre-install-dependencies.sh
+++ vhdbuilder/packer/pre-install-dependencies.sh
@@ -46,14 +46,6 @@ systemctlEnableAndStart ci-syslog-watcher.service || exit 1
 echo -e "\n# Disable WALA log collection because AKS Log Collector is installed.\nLogs.Collect=n" >> /etc/waagent.conf || exit 1
 systemctlEnableAndStart aks-log-collector.timer || exit 1
 
-# enable AKS local DNS
-# TODO move this into CSE start and control it with an API field
-# Also fix the local DNS field
-cd /opt/azure/aks-local-dns || exit 1
-curl -Lo - https://github.com/coredns/coredns/releases/download/v1.11.1/coredns_1.11.1_linux_amd64.tgz | tar zxv || exit 1
-systemctl restart systemd-resolved || exit 1
-systemctlEnableAndStart aks-local-dns.service || exit 1
-
 # enable the modified logrotate service and remove the auto-generated default logrotate cron job if present
 systemctlEnableAndStart logrotate.timer || exit 1
 rm -f /etc/cron.daily/logrotate
