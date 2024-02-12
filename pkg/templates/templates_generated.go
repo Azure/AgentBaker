b3724a80179f8f3382631f3b5aa05d9cc0afcf51
Alison Burgess <alburgess@microsoft.com>
2024-02-12 14:42:10 -0800

    fix: run make generate

--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -4993,9 +4993,13 @@ sudo sed -i "s,http://.[^ ]*,$repoDepotEndpoint,g" /etc/apt/sources.list
 systemctl stop systemd-timesyncd
 systemctl disable systemd-timesyncd
 
-apt-get update
-apt-get install chrony -y
-cat > /etc/chrony/chrony.conf <<EOF
+chrony_conf="/etc/chrony/chrony.conf"
+if [ ! -e "$chrony_conf" ]; then
+    apt-get update
+    apt-get install chrony -y
+fi
+
+cat > $chrony_conf <<EOF
 # Welcome to the chrony configuration file. See chrony.conf(5) for more
 # information about usuable directives.
 
