8d36b22ec85c327c54e14114068c05d141b2b4c8
Zachary <t-zbailey@microsoft.com>
2024-02-15 08:43:02 -0800

    install-dependencies.sh benchmark revision, add stopwatch

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -1,18 +1,45 @@
 #!/bin/bash
 
-#Timestamps is an associative array that links the logical steps of the install script with timestamps
+#time_stamps is an associative array that links the logical steps of the install script with timestamps
 #Logicalorder is a regular array that remembers the actual order of the steps (associative arrays are not indexed)
-declare -A Timestamps=()   
-declare -a LogicalOrder=() 
+declare -A time_stamps=()   
+declare -a logical_order=() 
 
-#The RecordBenchmark function adds the current step into LogicalOrder and then associates that step with a start and end time in the timestamps array
-RecordBenchmark () { dtStamp=$(date +%H:%M:%S); LogicalOrder+=( "${1}" ); Timestamps["${1}"]=$dtStamp; }
+#The record_benchmark function adds the current step into logical_order and then associates that step with a start and end time in the timestamps array
+record_benchmark () { dt_stamp=$(date +%H:%M:%S); logical_order+=( "${1}" ); time_stamps["${1}"]=$dt_stamp; }
 
-#The PrintBenchmarks function uses the LogicalOrder array as a control to chronologically iterate over the step/timestamp pairs in the timestamps array
-PrintBenchMarkResults () { echo; echo; echo "Benchmarking Results:"; echo; for i in "${LogicalOrder[@]}"; do echo "   $i: ${Timestamps[$i]}"; done; echo; echo; }
+#The PrintBenchmarks function uses the logical_order array as a control to chronologically iterate over the step/timestamp pairs in the timestamps array
+print_benchmark_results () { echo; echo; echo "Benchmarking Results:"; echo; for i in "${logical_order[@]}"; do echo "   $i: ${time_stamps[$i]}"; done; echo; echo; }
+
+#Used as a reset for the stopwatch functions
+start_time=$(date +%s)
+
+#This function resets the the stopwatch start time
+start_watch () {
+  start_time=$(date +%s)
+}
+
+#This function captures the current time and subtracts the start time of the stopwatch, resulting in the total elapsed time
+stop_watch () {
+  local current_time=$(date +%s)
+  local difference_in_seconds=$((current_time - start_time))
+
+  local elapsed_hours=$(($difference_in_seconds / 3600))
+  local remaining_seconds_to_minutes=$(($difference_in_seconds % 3600))
+
+  local elapsed_minutes=$(($remaining_seconds_to_minutes / 60))
+  local remaining_seconds_to_seconds=$(($remaining_seconds_to_minutes % 60))
+
+  local elapsed_seconds=$(($remaining_seconds_to_seconds % 60))
+
+  echo
+  printf "'${1}' - Total Time Elapsed: %02d:%02d:%02d" $elapsed_hours $elapsed_minutes $elapsed_seconds
+  echo
+}
 
 #Benchmark 1 Start
-RecordBenchmark 'Declare Variables / Configure Environment (Lines 17 - 32) Start'
+record_benchmark 'Declare Variables / Configure Environment (Lines 44-62) Start'
+start_watch
 
 OS=$(sort -r /etc/*-release | gawk 'match($0, /^(ID_LIKE=(coreos)|ID=(.*))$/, a) { print toupper(a[2] a[3]); exit }')
 OS_VERSION=$(sort -r /etc/*-release | gawk 'match($0, /^(VERSION_ID=(.*))$/, a) { print toupper(a[2] a[3]); exit }' | tr -d '"')
@@ -31,13 +58,15 @@ CPU_ARCH=$(getCPUArch)  #amd64 or arm64
 VHD_LOGS_FILEPATH=/opt/azure/vhd-install.complete
 COMPONENTS_FILEPATH=/opt/azure/components.json
 
-RecordBenchmark 'Declare Variables / Configure Environment (Lines 17 - 32) End'
-#Benchmark 1 End
-#BenchMark 2 Start
-RecordBenchmark 'Purge and Reinstall Ubuntu (Lines 39 - 64) Start'
-
 echo ""
 echo "Components downloaded in this VHD build (some of the below components might get deleted during cluster provisioning if they are not needed):" >> ${VHD_LOGS_FILEPATH}
+
+record_benchmark 'Declare Variables / Configure Environment (Lines 44-62) End'
+stop_watch 'Declare Variables / Configure Environment'
+#Benchmark 1 End
+#BenchMark 2 Start
+record_benchmark 'Purge and Reinstall Ubuntu (Lines 71 - 93) Start'
+start_watch
 
 echo "Logging the kernel after purge and reinstall + reboot: $(uname -r)"
 # fix grub issue with cvm by reinstalling before other deps
@@ -63,10 +92,12 @@ APT::Periodic::Unattended-Upgrade "0";
 EOF
 fi
 
-RecordBenchmark 'Purge and Reinstall  Ubuntu (Lines 39 - 64) End'
+record_benchmark 'Purge and Reinstall  Ubuntu (Lines 71-93) End'
+stop_watch 'Purge and Reinstall  Ubuntu'
 #Benchmark 2 End
 #BenchMark 3 Start
-RecordBenchmark 'Install Dependencies (Lines 71 - 93) Start'
+record_benchmark 'Install Dependencies (Lines 102 - 124) Start'
+start_watch
 
 # If the IMG_SKU does not contain "minimal", installDeps normally
 if [[ "$IMG_SKU" != *"minimal"* ]]; then
@@ -92,10 +123,12 @@ RuntimeMaxUse=1G
 ForwardToSyslog=yes
 EOF
 
-RecordBenchmark 'Install Dependencies (Lines 71 - 93) End'
+record_benchmark 'Install Dependencies (Lines 102-124) End'
+stop_watch 'Install Dependencies'
 #Benchmark 3 End
 #Benchmark 4 Start
-RecordBenchmark 'Check Container Runtime / Network Configurations (Lines 100 - 124) Start'
+record_benchmark 'Check Container Runtime / Network Configurations (Lines 133 - 157) Start'
+start_watch
 
 if [[ ${CONTAINER_RUNTIME:-""} != "containerd" ]]; then
   echo "Unsupported container runtime. Only containerd is supported for new VHD builds."
@@ -123,10 +156,12 @@ if [[ "${UBUNTU_RELEASE}" == "18.04" || "${UBUNTU_RELEASE}" == "20.04" || "${UBU
   disableNtpAndTimesyncdInstallChrony || exit 1
 fi
 
-RecordBenchmark 'Check Container Runtime / Network Configurations (Lines 100 - 124) End'
+record_benchmark 'Check Container Runtime / Network Configurations (Lines 133 - 157) End'
+stop_watch 'Check Container Runtime / Network Configurations'
 #Benchmark 4 End
 #Benchmark 5 Start
-RecordBenchmark 'Create containerd service directory, download shims, configure runtime and network (Lines 131 - 184) Start'
+record_benchmark 'Create containerd service directory, download shims, configure runtime and network (Lines 166 - 219) Start'
+start_watch
 
 CONTAINERD_SERVICE_DIR="/etc/systemd/system/containerd.service.d"
 mkdir -p "${CONTAINERD_SERVICE_DIR}"
@@ -183,10 +218,12 @@ containerd_patch_version="$(echo "$installed_version" | cut -d- -f2)"
 installStandaloneContainerd ${containerd_version} ${containerd_patch_version}
 echo "  - [installed] containerd v${containerd_version}-${containerd_patch_version}" >> ${VHD_LOGS_FILEPATH}
 
-RecordBenchmark 'Create containerd service directory, download shims, configure runtime and network (Lines 131 - 184) End'
+record_benchmark 'Create containerd service directory, download shims, configure runtime and network (Lines 166 - 219) End'
+stop_watch 'Create containerd service directory, download shims, configure runtime and network'
 #Benchmark 5 End
 #Benchmark 6 Start
-RecordBenchmark 'Download components, determine / download crictl version (Lines 191 - 211) Start'
+record_benchmark 'Download components, determine / download crictl version (Lines 228 - 246) Start'
+start_watch
 
 DOWNLOAD_FILES=$(jq ".DownloadFiles" $COMPONENTS_FILEPATH | jq .[] --monochrome-output --compact-output)
 for componentToDownload in ${DOWNLOAD_FILES[*]}; do
@@ -208,10 +245,12 @@ for CRICTL_VERSION in ${CRICTL_VERSIONS}; do
   echo "  - crictl version ${CRICTL_VERSION}" >> ${VHD_LOGS_FILEPATH}
 done
 
-RecordBenchmark 'Download components, determine / download crictl version (Lines 191 - 211) End'
+record_benchmark 'Download components, determine / download crictl version (Lines 228 - 246) End'
+stop_watch 'Download components, determine / download crictl version'
 #Benchmark 6 End
 #Benchmark 7 Start
-RecordBenchmark 'Artifact streaming, download containerd plugins / NVIDIA driver images and CLI (Lines 216 - 285) Start'
+record_benchmark 'Artifact streaming, download containerd plugins (Lines 255 - 290) Start'
+start_watch
 
 installAndConfigureArtifactStreaming() {
   # arguments: package name, package extension
@@ -250,6 +289,13 @@ downloadTeleportdPlugin ${TELEPORTD_PLUGIN_DOWNLOAD_URL} "0.8.0"
 INSTALLED_RUNC_VERSION=$(runc --version | head -n1 | sed 's/runc version //')
 echo "  - runc version ${INSTALLED_RUNC_VERSION}" >> ${VHD_LOGS_FILEPATH}
 
+record_benchmark 'Artifact streaming, download containerd plugins (Lines 255 - 290) End'
+stop_watch 'Artifact streaming, download containerd plugins'
+#Benchmark 7 End
+#Benchmark 8 Start
+record_benchmark 'Pull NVIDIA driver images (Lines 299 - 331) Start'
+start_watch
+
 if [[ $OS == $UBUNTU_OS_NAME && $(isARM64) != 1 ]]; then  # no ARM64 SKU with GPU now
   gpu_action="copy"
   NVIDIA_DRIVER_IMAGE_SHA="sha-ff213d"
@@ -284,10 +330,12 @@ EOF
 
 echo "${CONTAINER_RUNTIME} images pre-pulled:" >> ${VHD_LOGS_FILEPATH}
 
-RecordBenchmark 'Artifact streaming, download containerd plugins / NVIDIA driver images and CLI (Lines 213 - 285) End'
-#Benchmark 6 End
-#Benchmark 7 Start
-RecordBenchmark 'Pull and tag container images (Lines 292 - 336) Start'
+record_benchmark 'Pull NVIDIA driver images (Lines 299 - 331) End'
+stop_watch 'Pull NVIDIA driver images'
+#Benchmark 8 End
+#Benchmark 9 Start
+record_benchmark 'Pull and tag container images (Lines 340 - 384) Start'
+start_watch
 
 string_replace() {
   echo ${1//\*/$2}
@@ -335,10 +383,12 @@ watcherStaticImg=${watcherBaseImg//\*/static}
 # can't use cliTool because crictl doesn't support retagging.
 retagContainerImage "ctr" ${watcherFullImg} ${watcherStaticImg}
 
-RecordBenchmark 'Pull and tag container images (Lines 289 - 336) End'
-#Benchmark 7 End
-#Benchmark 8 Start
-RecordBenchmark 'Configure container networking and interface (Lines 343 - 398) Start'
+record_benchmark 'Pull and tag container images (Lines 340 - 384) End'
+stop_watch 'Pull and tag container images'
+#Benchmark 9 End
+#Benchmark 10 Start
+record_benchmark 'Configure container networking and interface (Lines 393 - 448) Start'
+start_watch
 
 # doing this at vhd allows CSE to be faster with just mv
 unpackAzureCNI() {
@@ -397,10 +447,12 @@ if [[ $OS == $UBUNTU_OS_NAME || ( $OS == $MARINER_OS_NAME && $OS_VERSION == "2.0
   systemctlEnableAndStart ipv6_nftables || exit 1
 fi
 
-RecordBenchmark 'Configure container networking and interface (Lines 340 - 398) End'
-#Benchmark 8 End
-#Benchmark 9 Start
-RecordBenchmark 'GPU Device plugin (Lines 405 - 430) Start'
+record_benchmark 'Configure container networking and interface (Lines 393 - 448) End'
+stop_watch 'Configure container networking and interface'
+#Benchmark 10 End
+#Benchmark 11 Start
+record_benchmark 'GPU Device plugin (Lines 457 - 482) Start'
+start_watch
 
 if [[ $OS == $UBUNTU_OS_NAME && $(isARM64) != 1 ]]; then  # no ARM64 SKU with GPU now
 NVIDIA_DEVICE_PLUGIN_VERSIONS="
@@ -429,10 +481,12 @@ if grep -q "fullgpu" <<< "$FEATURE_FLAGS" && grep -q "gpudaemon" <<< "$FEATURE_F
 fi
 fi
 
-RecordBenchmark 'GPU Device plugin (Lines 405 - 430) End'
-#Benchmark 9 End
-#Benchmark 10 Start
-RecordBenchmark 'Configure telemetry, create logging directory, kube-proxy (Lines 437 - 469) Start'
+record_benchmark 'GPU Device plugin (Lines 457 - 482) End'
+stop_watch 'GPU Device plugin'
+#Benchmark 11 End
+#Benchmark 12 Start
+record_benchmark 'Configure telemetry, create logging directory, kube-proxy (Lines 491 - 523) Start'
+start_watch
 
 mkdir -p /var/log/azure/Microsoft.Azure.Extensions.CustomScript/events
 
@@ -468,10 +522,12 @@ for KUBE_PROXY_IMAGE_VERSION in ${KUBE_PROXY_IMAGE_VERSIONS}; do
   echo "  - ${CONTAINER_IMAGE}" >>${VHD_LOGS_FILEPATH}
 done
 
-RecordBenchmark 'Configure telemetry, create logging directory, kube-proxy (Lines 437 - 469) End'
-#Benchmark 10 End
-#Benchmark 11 Start
-RecordBenchmark 'Download Kubernetes package, process package, extract binaries (Lines 476 - 538) Start'
+record_benchmark 'Configure telemetry, create logging directory, kube-proxy (Lines 491 - 523) End'
+stop_watch 'Configure telemetry, create logging directory, kube-proxy'
+#Benchmark 12 End
+#Benchmark 13 Start
+record_benchmark 'Download Kubernetes package, process package, extract binaries (Lines 532 - 594) Start'
+start_watch
 
 # download kubernetes package from the given URL using MSI for auth for azcopy
 # if it is a kube-proxy package, extract image from the downloaded package
@@ -537,11 +593,12 @@ done
 
 rm -f ./azcopy # cleanup immediately after usage will return in two downloads
 
-RecordBenchmark 'Download Kubernetes package, process package, extract binaries (Lines 476 - 538) End'
-#Benchmark 11 End
+record_benchmark 'Download Kubernetes package, process package, extract binaries (Lines 532 - 594) End'
+stop_watch 'Download Kubernetes package, process package, extract binaries'
+#Benchmark 13 End
 #End of benchmarks
 echo
-PrintBenchMarkResults
+print_benchmark_results
 echo
 
 echo "install-dependencies step completed successfully"
