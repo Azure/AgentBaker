36a1d712b4c54355f8850592935f818a73aba353
Cameron Meissner <cameissner@microsoft.com>
2024-02-26 15:34:05 -0800

    fix: typo

--- pkg/agent/overrides/service.go
+++ pkg/agent/overrides/service.go
@@ -2,7 +2,8 @@ package overrides
 
 // All AgentBaker service overrides go below
 
-// GetLinuxNodeImageVersionOverrides returns the Linux node image version overrides.
+// GetLinuxNodeImageVersion returns the Linux node image version overrides.
+// The returned value is a map from distro to image version.
 func (o *Overrides) GetLinuxNodeImageVersion(entity *Entity) map[string]string {
 	return o.getMap("linux-node-image-version", entity)
 }
