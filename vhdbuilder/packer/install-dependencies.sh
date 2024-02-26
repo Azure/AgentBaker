5f7064ad9105d4b1972a1e00eef968f9cc0abdbf
Zachary Bailey <t-zbailey@microsoft.com>
2024-02-26 13:20:16 -0800

    benchmark install-dependencies.sh

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+start_time=$(date +%s)
+echo "Declare Variables / Configure Environment Start - $start_time"
+declare -A time_stamps=()   
+declare -a logical_order=()
+
 OS=$(sort -r /etc/*-release | gawk 'match($0, /^(ID_LIKE=(coreos)|ID=(.*))$/, a) { print toupper(a[2] a[3]); exit }')
 OS_VERSION=$(sort -r /etc/*-release | gawk 'match($0, /^(VERSION_ID=(.*))$/, a) { print toupper(a[2] a[3]); exit }' | tr -d '"')
 UBUNTU_OS_NAME="UBUNTU"
@@ -18,6 +24,11 @@ COMPONENTS_FILEPATH=/opt/azure/components.json
 
 echo ""
 echo "Components downloaded in this VHD build (some of the below components might get deleted during cluster provisioning if they are not needed):" >> ${VHD_LOGS_FILEPATH}
+
+record_benchmark 'Declare Variables / Configure Environment End'
+stop_watch 'Declare Variables / Configure Environment'
+record_benchmark 'Purge and Reinstall Ubuntu Start'
+start_watch
 
 echo "Logging the kernel after purge and reinstall + reboot: $(uname -r)"
 # fix grub issue with cvm by reinstalling before other deps
@@ -43,6 +54,11 @@ APT::Periodic::Unattended-Upgrade "0";
 EOF
 fi
 
+record_benchmark 'Purge and Reinstall Ubuntu End'
+stop_watch 'Purge and Reinstall  Ubuntu'
+record_benchmark 'Install Dependencies Start'
+start_watch
+
 # If the IMG_SKU does not contain "minimal", installDeps normally
 if [[ "$IMG_SKU" != *"minimal"* ]]; then
   installDeps
@@ -66,6 +82,11 @@ SystemMaxUse=1G
 RuntimeMaxUse=1G
 ForwardToSyslog=yes
 EOF
+
+record_benchmark 'Install Dependencies End'
+stop_watch 'Install Dependencies'
+record_benchmark 'Check Container Runtime / Network Configurations Start'
+start_watch
 
 if [[ ${CONTAINER_RUNTIME:-""} != "containerd" ]]; then
   echo "Unsupported container runtime. Only containerd is supported for new VHD builds."
@@ -92,6 +113,10 @@ if [[ "${UBUNTU_RELEASE}" == "18.04" || "${UBUNTU_RELEASE}" == "20.04" || "${UBU
   overrideNetworkConfig || exit 1
   disableNtpAndTimesyncdInstallChrony || exit 1
 fi
+
+record_benchmark 'Check Container Runtime / Network Configurations End'
+stop_watch 'Check Container Runtime / Network Configurations'
+record_benchmark 'Create containerd service directory, download shims, configure runtime and network Start'
 
 CONTAINERD_SERVICE_DIR="/etc/systemd/system/containerd.service.d"
 mkdir -p "${CONTAINERD_SERVICE_DIR}"
@@ -131,8 +156,10 @@ if [[ $OS == $MARINER_OS_NAME ]]; then
     activateNfConntrack
 fi
 
+start_watch
 downloadContainerdWasmShims
 echo "  - containerd-wasm-shims ${CONTAINERD_WASM_VERSIONS}" >> ${VHD_LOGS_FILEPATH}
+stop_watch 'downloadContainerdWasmShimsFunction'
 
 echo "VHD will be built with containerd as the container runtime"
 updateAptWithMicrosoftPkg
@@ -147,6 +174,10 @@ containerd_version="$(echo "$installed_version" | cut -d- -f1)"
 containerd_patch_version="$(echo "$installed_version" | cut -d- -f2)"
 installStandaloneContainerd ${containerd_version} ${containerd_patch_version}
 echo "  - [installed] containerd v${containerd_version}-${containerd_patch_version}" >> ${VHD_LOGS_FILEPATH}
+
+record_benchmark 'Create containerd service directory, download shims, configure runtime and network End'
+record_benchmark 'Download components, determine / download crictl version Start'
+start_watch
 
 DOWNLOAD_FILES=$(jq ".DownloadFiles" $COMPONENTS_FILEPATH | jq .[] --monochrome-output --compact-output)
 for componentToDownload in ${DOWNLOAD_FILES[*]}; do
@@ -167,6 +198,11 @@ for CRICTL_VERSION in ${CRICTL_VERSIONS}; do
   downloadCrictl ${CRICTL_VERSION}
   echo "  - crictl version ${CRICTL_VERSION}" >> ${VHD_LOGS_FILEPATH}
 done
+
+record_benchmark 'Download components, determine / download crictl version End'
+stop_watch 'Download components, determine / download crictl version'
+record_benchmark 'Artifact streaming, download containerd plugins Start'
+start_watch
 
 installAndConfigureArtifactStreaming() {
   # arguments: package name, package extension
@@ -205,6 +241,11 @@ downloadTeleportdPlugin ${TELEPORTD_PLUGIN_DOWNLOAD_URL} "0.8.0"
 INSTALLED_RUNC_VERSION=$(runc --version | head -n1 | sed 's/runc version //')
 echo "  - runc version ${INSTALLED_RUNC_VERSION}" >> ${VHD_LOGS_FILEPATH}
 
+record_benchmark 'Artifact streaming, download containerd plugins End'
+stop_watch 'Artifact streaming, download containerd plugins'
+record_benchmark 'Pull NVIDIA driver images Start'
+start_watch
+
 if [[ $OS == $UBUNTU_OS_NAME && $(isARM64) != 1 ]]; then  # no ARM64 SKU with GPU now
   gpu_action="copy"
   NVIDIA_DRIVER_IMAGE_SHA="sha-ff213d"
@@ -228,6 +269,12 @@ fi
 
 ls -ltr /opt/gpu/* >> ${VHD_LOGS_FILEPATH}
 
+echo "${CONTAINER_RUNTIME} images pre-pulled:" >> ${VHD_LOGS_FILEPATH}
+
+string_replace() {
+  echo ${1//\*/$2}
+}
+
 installBpftrace
 echo "  - $(bpftrace --version)" >> ${VHD_LOGS_FILEPATH}
 
@@ -243,13 +290,10 @@ PARENT_DIR=$(pwd)
 
 BCC_PID=$!
 
-echo "${CONTAINER_RUNTIME} images pre-pulled:" >> ${VHD_LOGS_FILEPATH}
-
-
-
-string_replace() {
-  echo ${1//\*/$2}
-}
+record_benchmark 'Pull NVIDIA driver images End'
+stop_watch 'Pull NVIDIA driver images'
+record_benchmark 'Pull and tag container images Start'
+start_watch
 
 ContainerImages=$(jq ".ContainerImages" $COMPONENTS_FILEPATH | jq .[] --monochrome-output --compact-output)
 for imageToBePulled in ${ContainerImages[*]}; do
@@ -293,6 +337,11 @@ watcherStaticImg=${watcherBaseImg//\*/static}
 # can't use cliTool because crictl doesn't support retagging.
 retagContainerImage "ctr" ${watcherFullImg} ${watcherStaticImg}
 
+record_benchmark 'Pull and tag container images End'
+stop_watch 'Pull and tag container images'
+record_benchmark 'Configure container networking and interface Start'
+start_watch
+
 # doing this at vhd allows CSE to be faster with just mv
 unpackAzureCNI() {
   local URL=$1
@@ -309,7 +358,6 @@ VNET_CNI_VERSIONS="
 1.5.11
 1.4.43.1
 "
-
 
 for VNET_CNI_VERSION in $VNET_CNI_VERSIONS; do
     VNET_CNI_PLUGINS_URL="https://acs-mirror.azureedge.net/azure-cni/v${VNET_CNI_VERSION}/binaries/azure-vnet-cni-linux-${CPU_ARCH}-v${VNET_CNI_VERSION}.tgz"
@@ -350,6 +398,11 @@ if [[ $OS == $UBUNTU_OS_NAME || ( $OS == $MARINER_OS_NAME && $OS_VERSION == "2.0
   systemctlEnableAndStart ipv6_nftables || exit 1
 fi
 
+record_benchmark 'Configure container networking and interface End'
+stop_watch 'Configure container networking and interface'
+record_benchmark 'GPU Device plugin Start'
+start_watch
+
 if [[ $OS == $UBUNTU_OS_NAME && $(isARM64) != 1 ]]; then  # no ARM64 SKU with GPU now
 NVIDIA_DEVICE_PLUGIN_VERSIONS="
 v0.13.0.7
@@ -377,6 +430,12 @@ if grep -q "fullgpu" <<< "$FEATURE_FLAGS" && grep -q "gpudaemon" <<< "$FEATURE_F
 fi
 fi
 
+
+record_benchmark 'GPU Device plugin End'
+stop_watch 'GPU Device plugin'
+record_benchmark 'Configure telemetry, create logging directory, kube-proxy, wait for installBcc to complete Start'
+
+start_watch
 echo "Waiting for BCC Install to complete..."
 wait $BCC_PID
 BCC_EXIT_STATUS=$?
@@ -402,6 +461,8 @@ cat << EOF >> ${VHD_LOGS_FILEPATH}
   - libbcc-examples
 EOF
 echo "BCC Install complete..."
+stop_watch 'installBcc additional time required'
+start_watch
 
 mkdir -p /var/log/azure/Microsoft.Azure.Extensions.CustomScript/events
 
@@ -438,6 +499,11 @@ for KUBE_PROXY_IMAGE_VERSION in ${KUBE_PROXY_IMAGE_VERSIONS}; do
   echo "  - ${CONTAINER_IMAGE}" >>${VHD_LOGS_FILEPATH}
 done
 wait ${PIDS1[@]} # Wait for all background processes to finish
+
+record_benchmark 'Configure telemetry, create logging directory, kube-proxy, wait for installBcc to complete End'
+stop_watch 'Configure telemetry, create logging directory, kube-proxy, after waiting for installBcc to complete'
+record_benchmark 'Download Kubernetes package, process package, extract binaries Start'
+start_watch
 
 # download kubernetes package from the given URL using MSI for auth for azcopy
 # if it is a kube-proxy package, extract image from the downloaded package
@@ -503,4 +569,11 @@ done
 
 rm -f ./azcopy # cleanup immediately after usage will return in two downloads
 
+record_benchmark 'Download Kubernetes package, process package, extract binaries End'
+stop_watch 'Download Kubernetes package, process package, extract binaries'
+echo
+echo
+print_benchmark_results
+echo
+echo
 echo "install-dependencies step completed successfully"
