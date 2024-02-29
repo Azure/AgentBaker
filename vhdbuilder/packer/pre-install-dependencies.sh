2de97c05e0e60a8bbd5ce94e1ecb815f7b4c4780
anujmaheshwari1 <amaheshwari@microsoft.com>
2024-02-29 11:30:59 -0800

    test: test for 2204

--- vhdbuilder/packer/pre-install-dependencies.sh
+++ vhdbuilder/packer/pre-install-dependencies.sh
@@ -78,11 +78,11 @@ else
     sed -i 's#http://azure.archive.ubuntu.com/ubuntu/#https://snapshot.ubuntu.com/ubuntu/20240226T090022Z#g' /etc/apt/sources.list
     echo "echoing sources.list HERE in pre-install"
     cat /etc/apt/sources.list
-    echo "echoing esm infra sources.list HERE in pre-install"
-    cat /etc/apt/sources.list.d/ubuntu-esm-infra.list
-    sed -i 's#https://esm.ubuntu.com/infra/ubuntu#https://snapshot.ubuntu.com/ubuntu/20240226T090022Z#g' /etc/apt/sources.list.d/ubuntu-esm-infra.list
-    echo "echoing esm infra sources.list AFTER CHANGE in pre-install"
-    cat /etc/apt/sources.list.d/ubuntu-esm-infra.list
+    # echo "echoing esm infra sources.list HERE in pre-install"
+    # cat /etc/apt/sources.list.d/ubuntu-esm-infra.list
+    # sed -i 's#https://esm.ubuntu.com/infra/ubuntu#https://snapshot.ubuntu.com/ubuntu/20240226T090022Z#g' /etc/apt/sources.list.d/ubuntu-esm-infra.list
+    # echo "echoing esm infra sources.list AFTER CHANGE in pre-install"
+    # cat /etc/apt/sources.list.d/ubuntu-esm-infra.list
     apt_get_update || exit $ERR_APT_UPDATE_TIMEOUT
     apt_get_dist_upgrade || exit $ERR_APT_DIST_UPGRADE_TIMEOUT  
     echo -e "=== Pre Install Dependencies Packages Begin\n$(listInstalledPackages)\n=== Installed Packages End"  
