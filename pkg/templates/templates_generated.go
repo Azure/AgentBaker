5ce1dc4b4f9f2607929127c07af3bc8ef039ff0b
Abel Hu <abelch@microsoft.com>
2024-03-13 23:50:28 +0000

    chore: use the same file name for Windows cse provision complete

--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -8194,7 +8194,7 @@ $arguments = '
 -AADClientSecret ''{{ GetParameter "encodedServicePrincipalClientSecret" }}''
 -NetworkAPIVersion 2018-08-01
 -LogFile %SYSTEMDRIVE%\AzureData\CustomDataSetupScript.log
--CSEResultFilePath %SYSTEMDRIVE%\AzureData\CSEResult.log';
+-CSEResultFilePath %SYSTEMDRIVE%\AzureData\provision.complete';
 $inputFile = '%SYSTEMDRIVE%\AzureData\CustomData.bin';
 $outputFile = '%SYSTEMDRIVE%\AzureData\CustomDataSetupScript.ps1';
 if (!(Test-Path $inputFile)) { throw 'ExitCode: |49|, Output: |WINDOWS_CSE_ERROR_NO_CUSTOM_DATA_BIN|, Error: |C:\AzureData\CustomData.bin does not exist.|' };
