996bb76a77a2f1e645aa1730994e51f43612cc9d
Zachary Bailey <t-zbailey@microsoft.com>
2024-03-15 07:24:49 -0700

    perf: use shallow replication

--- vhdbuilder/packer/generate-vhd-publishing-info.sh
+++ vhdbuilder/packer/generate-vhd-publishing-info.sh
@@ -11,6 +11,7 @@ required_env_vars=(
     "IMAGE_VERSION"
 )
 
+Replication_Mode="Shallow"
 # Higher the replication_inverse, lower is the usage and number of replicas
 set -x
 REPLICATION_INVERSE=1
@@ -123,6 +124,7 @@ if [ "${OS_NAME,,}" == "linux" ]; then
     "hyperv_generation": "${HYPERV_GENERATION}",
     "image_architecture": "${IMAGE_ARCH}",
     "image_version": "${IMAGE_VERSION}",
+    "replication_mode": "${Replication_Mode}"
     "replication_inverse": "${REPLICATION_INVERSE}"
 }
 EOF
