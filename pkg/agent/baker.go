cd0b202789dc619b5d796937f8aa90aee796017d
Zachary Bailey <t-zbailey@microsoft.com>
2024-03-18 08:40:31 -0700

    fix: wasmshims versions

--- pkg/agent/baker.go
+++ pkg/agent/baker.go
@@ -1216,6 +1216,18 @@ root = "{{GetDataDir}}"{{- end}}
       BinaryName = "/usr/bin/runc"
     {{- end}}
     {{- if IsKrustlet }}
+    [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.spin]
+      runtime_type = "io.containerd.spin-v0-3-0.v1"
+    [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.slight]
+      runtime_type = "io.containerd.slight-v0-3-0.v1"
+    [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.spin-v0-3-0]
+      runtime_type = "io.containerd.spin-v0-3-0.v1"
+    [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.slight-v0-3-0]
+      runtime_type = "io.containerd.slight-v0-3-0.v1"
+    [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.spin-v0-5-1]
+      runtime_type = "io.containerd.spin-v0-5-1.v1"
+    [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.slight-v0-5-1]
+      runtime_type = "io.containerd.slight-v0-5-1.v1"
     [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.spin-v0-8-0]
       runtime_type = "io.containerd.spin-v0-8-0.v1"
     [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.slight-v0-8-0]
