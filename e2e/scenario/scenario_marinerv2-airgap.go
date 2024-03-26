334ec22820082209f7a8619561d9cfa35e3acd7f
Alison Burgess <alburgess@microsoft.com>
2024-03-26 11:12:21 -0700

    fix scenario

--- e2e/scenario/scenario_marinerv2-airgap.go
+++ e2e/scenario/scenario_marinerv2-airgap.go
@@ -6,7 +6,7 @@ import (
 
 func (t *Template) marinerv2AirGap() *Scenario {
 	return &Scenario{
-		Name:        "marinerv2-airgap",
+		Name:        "marinerv2",
 		Description: "Tests that a node using a MarinerV2 VHD can be properly bootstrapped",
 		Config: Config{
 			ClusterSelector: NetworkPluginKubenetSelector,
@@ -15,9 +15,6 @@ func (t *Template) marinerv2AirGap() *Scenario {
 			BootstrapConfigMutator: func(nbc *datamodel.NodeBootstrappingConfiguration) {
 				nbc.ContainerService.Properties.AgentPoolProfiles[0].Distro = "aks-cblmariner-v2-gen2"
 				nbc.AgentPoolProfile.Distro = "aks-cblmariner-v2-gen2"
-			},
-			LiveVMValidators: []*LiveVMValidator{
-				KubenetEnsureNoDupEbtablesValidator(),
 			},
 			Airgap: true,
 		},
