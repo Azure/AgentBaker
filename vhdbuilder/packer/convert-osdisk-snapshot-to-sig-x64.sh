c2f74688f36771698a6dac445f680f20067472fb
Zachary Bailey <t-zbailey@microsoft.com>
2024-04-01 08:11:59 -0700

    perf: add executable for shallow replication

---
+++ vhdbuilder/packer/convert-osdisk-snapshot-to-sig-x64.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+required_env_vars=(
+    "SUBSCRIPTION_ID"
+    "AZURE_RESOURCE_GROUP_NAME"
+    "AZURE_LOCATION"
+)
+
+for v in "${required_env_vars[@]}"
+do
+    if [ -z "${!v}" ]; then
+        echo "$v was not set!"
+        exit 1
+    fi
+done
+
+OS_DISK_SNAPSHOT_NAME="$(grep "os_disk_snapshot_name" ./vhdbuilder/packer/settings.json | awk -F':' '{print $2}' | awk -F'"' '{print $2}')"
+CAPTURED_SIG_VERSION="$(grep "captured_sig_version" ./vhdbuilder/packer/settings.json | awk -F':' '{print $2}' | awk -F'"' '{print $2}')"
+SIG_IMAGE_NAME="$(grep "sig_image_name" ./vhdbuilder/packer/settings.json | awk -F':' '{print $2}' | awk -F'"' '{print $2}')" && \
+echo "ManagedImageSharedImageGalleryId: /subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${AZURE_RESOURCE_GROUP_NAME}/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/${SIG_IMAGE_NAME}/versions/${CAPTURED_SIG_VERSION}"
+
+REPLICATION_MODE_STRING=""
+if [[ ${IMG_SKU} == "20_04-lts-cvm" ]]; then
+    REPLICATION_MODE_STRING+="--replication-mode Full"
+else
+    REPLICATION_MODE_STRING+="--replication-mode Shallow"
+fi
+echo "Replication Mode: ${REPLICATION_MODE_STRING}"
+
+time_now=$(echo ${CAPTURED_SIG_VERSION} | cut -d '.' -f2)
+disk_snapshot_id="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${AZURE_RESOURCE_GROUP_NAME}/providers/Microsoft.Compute/snapshots/${OS_DISK_SNAPSHOT_NAME}"
+az snapshot update --resource-group ${AZURE_RESOURCE_GROUP_NAME} -n ${OS_DISK_SNAPSHOT_NAME}
+az sig image-version create --location $AZURE_LOCATION --resource-group ${AZURE_RESOURCE_GROUP_NAME} --gallery-name PackerSigGalleryEastUS \
+     --gallery-image-definition ${SIG_IMAGE_NAME} --gallery-image-version ${CAPTURED_SIG_VERSION} \
+     --os-snapshot ${disk_snapshot_id} --tags now=${time_now} ${REPLICATION_MODE_STRING}
