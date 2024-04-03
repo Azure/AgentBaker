1dfe8e09ca776d645bc12e3450b472a4da3ea706
Devin Wong <devinwon@microsoft.com>
2024-04-03 15:19:56 -0700

    removed unncessary functions

--- pkg/parser/utilities.go
+++ pkg/parser/utilities.go
@@ -1,8 +1,6 @@
 package parser
 
 import (
-	"text/template"
-
 	nbcontractv1 "github.com/Azure/agentbaker/pkg/proto/nbcontract/v1"
 )
 
@@ -18,28 +16,5 @@ func getBaseTemplate() *nbcontractv1.Configuration {
 		},
 		KubeproxyUrl: "",
 		EnableSsh:    true,
-	}
-}
-
-func getBoolStr(state nbcontractv1.FeatureState) string {
-	if state == nbcontractv1.FeatureState_FEATURE_STATE_ENABLED {
-		return "true"
-	}
-
-	return "false"
-}
-
-func getInverseBoolStr(state nbcontractv1.FeatureState) string {
-	if state == nbcontractv1.FeatureState_FEATURE_STATE_ENABLED {
-		return "false"
-	}
-
-	return "true"
-}
-
-func getFuncMap() template.FuncMap {
-	return template.FuncMap{
-		"getBoolStr":        getBoolStr,
-		"getInverseBoolStr": getInverseBoolStr,
 	}
 }
