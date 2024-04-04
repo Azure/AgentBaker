3decc8d61f23547494a949e60dfae08decbfac1c
Artun Duman <artunduman@microsoft.com>
2024-04-04 13:36:23 -0700

    uncomment kernel purding

--- vhdbuilder/packer/pre-install-dependencies.sh
+++ vhdbuilder/packer/pre-install-dependencies.sh
@@ -120,20 +120,20 @@ if [[ ${OS} == ${MARINER_OS_NAME} ]] && [[ "${ENABLE_CGROUPV2,,}" == "true" ]]; 
   enableCgroupV2forAzureLinux
 fi
 
-# if [[ "${UBUNTU_RELEASE}" == "22.04" ]]; then
-#   echo "Logging the currently running kernel: $(uname -r)"
-#   echo "Before purging kernel, here is a list of kernels/headers installed:"; dpkg -l 'linux-*azure*'
+if [[ "${UBUNTU_RELEASE}" == "22.04" ]]; then
+  echo "Logging the currently running kernel: $(uname -r)"
+  echo "Before purging kernel, here is a list of kernels/headers installed:"; dpkg -l 'linux-*azure*'
 
-#   # Purge all current kernels and dependencies
-#   DEBIAN_FRONTEND=noninteractive apt-get remove --purge -y $(dpkg-query -W 'linux-*azure*' | awk '$2 != "" { print $1 }' | paste -s)
-#   echo "After purging kernel, dpkg list should be empty"; dpkg -l 'linux-*azure*'
+  # Purge all current kernels and dependencies
+  DEBIAN_FRONTEND=noninteractive apt-get remove --purge -y $(dpkg-query -W 'linux-*azure*' | awk '$2 != "" { print $1 }' | paste -s)
+  echo "After purging kernel, dpkg list should be empty"; dpkg -l 'linux-*azure*'
 
-#   # Install lts-22.04 kernel
-#   DEBIAN_FRONTEND=noninteractive apt-get install -y linux-image-azure-lts-22.04 linux-cloud-tools-azure-lts-22.04 linux-headers-azure-lts-22.04 linux-modules-extra-azure-lts-22.04 linux-tools-azure-lts-22.04
-#   echo "After installing new kernel, here is a list of kernels/headers installed"; dpkg -l 'linux-*azure*'
+  # Install lts-22.04 kernel
+  DEBIAN_FRONTEND=noninteractive apt-get install -y linux-image-azure-lts-22.04 linux-cloud-tools-azure-lts-22.04 linux-headers-azure-lts-22.04 linux-modules-extra-azure-lts-22.04 linux-tools-azure-lts-22.04
+  echo "After installing new kernel, here is a list of kernels/headers installed"; dpkg -l 'linux-*azure*'
   
-#   update-grub
-# fi
+  update-grub
+fi
 
 echo "Logging the currently running kernel: $(uname -r)"
 echo "Here's a list of kernels/headers installed at the end of pre-install-dependencies:"; dpkg -l 'linux-*azure*'
