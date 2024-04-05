d9fe35fe11b139671e4b51aa464e059916a6ec2b
Cameron Meissner <cameissner@microsoft.com>
2024-04-05 16:18:28 -0700

    bump: containerd 1.7.15 for CVE and bug fixes

--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -5357,7 +5357,7 @@ var _linuxCloudInitArtifactsManifestJson = []byte(`{
         "pinned": {
             "1804": "1.7.1-1"
         },
-        "edge": "1.7.14-1"
+        "edge": "1.7.15-1"
     },
     "runc": {
         "fileName": "moby-runc_${RUNC_VERSION}+azure-ubuntu${RUNC_PATCH_VERSION}_${CPU_ARCH}.deb",
@@ -7320,7 +7320,7 @@ installStandaloneContainerd() {
     #if there is no containerd_version input from RP, use hardcoded version
     if [[ -z ${CONTAINERD_VERSION} ]]; then
         # pin 18.04 to 1.7.1
-        CONTAINERD_VERSION="1.7.14"
+        CONTAINERD_VERSION="1.7.15"
         if [ "${UBUNTU_RELEASE}" == "18.04" ]; then
             CONTAINERD_VERSION="1.7.1"
         fi
