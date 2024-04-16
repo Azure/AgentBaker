6ff557886c96f9a2f3959cc14ee36446debe411a
Devin Wong <devinwon@microsoft.com>
2024-04-16 11:48:25 -0700

    Refactor NBContractConfig initialization and configuration handling

--- pkg/nbcontracthelper/helper.go
+++ pkg/nbcontracthelper/helper.go
@@ -4,6 +4,11 @@ import (
 	nbcontractv1 "github.com/Azure/agentbaker/pkg/proto/nbcontract/v1"
 )
 
+type NBContractConfig struct {
+	// NBContractConfiguration is the configuration object for the NBContract (Node Bootstrap Contract)
+	nBContractConfiguration *nbcontractv1.Configuration
+}
+
 // Check and initialize each field if it is nil
 func initializeIfNil[T any](field **T) {
 	if *field == nil {
@@ -11,33 +16,59 @@ func initializeIfNil[T any](field **T) {
 	}
 }
 
-// EnsureConfigsNonNil checks if the config is nil and initializes it if it is.
-func ensureConfigsNonNil(v *nbcontractv1.Configuration) *nbcontractv1.Configuration {
-	if v == nil {
-		v = &nbcontractv1.Configuration{}
-	}
-
-	initializeIfNil(&v.KubeBinaryConfig)
-	initializeIfNil(&v.ApiServerConfig)
-	initializeIfNil(&v.AuthConfig)
-	initializeIfNil(&v.ClusterConfig)
-	initializeIfNil(&v.NetworkConfig)
-	initializeIfNil(&v.GpuConfig)
-	initializeIfNil(&v.TlsBootstrappingConfig)
-	initializeIfNil(&v.KubeletConfig)
-	initializeIfNil(&v.RuncConfig)
-	initializeIfNil(&v.ContainerdConfig)
-	initializeIfNil(&v.TeleportConfig)
-	initializeIfNil(&v.CustomLinuxOsConfig)
-	initializeIfNil(&v.HttpProxyConfig)
-	initializeIfNil(&v.CustomCloudConfig)
-	initializeIfNil(&v.CustomSearchDomainConfig)
-
-	return v
+// Ensure all objects are non-nil. Please add new objects here.
+func (nbcc *NBContractConfig) ensureConfigsNonNil() {
+	initializeIfNil(&nbcc.nBContractConfiguration.KubeBinaryConfig)
+	initializeIfNil(&nbcc.nBContractConfiguration.ApiServerConfig)
+	initializeIfNil(&nbcc.nBContractConfiguration.AuthConfig)
+	initializeIfNil(&nbcc.nBContractConfiguration.ClusterConfig)
+	initializeIfNil(&nbcc.nBContractConfiguration.NetworkConfig)
+	initializeIfNil(&nbcc.nBContractConfiguration.GpuConfig)
+	initializeIfNil(&nbcc.nBContractConfiguration.TlsBootstrappingConfig)
+	initializeIfNil(&nbcc.nBContractConfiguration.KubeletConfig)
+	initializeIfNil(&nbcc.nBContractConfiguration.RuncConfig)
+	initializeIfNil(&nbcc.nBContractConfiguration.ContainerdConfig)
+	initializeIfNil(&nbcc.nBContractConfiguration.TeleportConfig)
+	initializeIfNil(&nbcc.nBContractConfiguration.CustomLinuxOsConfig)
+	initializeIfNil(&nbcc.nBContractConfiguration.HttpProxyConfig)
+	initializeIfNil(&nbcc.nBContractConfiguration.CustomCloudConfig)
+	initializeIfNil(&nbcc.nBContractConfiguration.CustomSearchDomainConfig)
 }
 
-// Creates a new instance of nbcontractv1.Configuration and ensures that
-// all fields are non-nil unless it's set as proto3 explicit presence (with label optional).
-func NewNBContractConfiguration(v *nbcontractv1.Configuration) *nbcontractv1.Configuration {
-	return ensureConfigsNonNil(v)
+// Creates a new instance of NBContractConfig and ensures all objects are non-nil
+func NewNBContractConfiguration() *NBContractConfig {
+	nbcc := &NBContractConfig{
+		nBContractConfiguration: &nbcontractv1.Configuration{
+			KubeBinaryConfig:         &nbcontractv1.KubeBinaryConfig{},
+			ApiServerConfig:          &nbcontractv1.ApiServerConfig{},
+			AuthConfig:               &nbcontractv1.AuthConfig{},
+			ClusterConfig:            &nbcontractv1.ClusterConfig{},
+			NetworkConfig:            &nbcontractv1.NetworkConfig{},
+			GpuConfig:                &nbcontractv1.GPUConfig{},
+			TlsBootstrappingConfig:   &nbcontractv1.TLSBootstrappingConfig{},
+			KubeletConfig:            &nbcontractv1.KubeletConfig{},
+			RuncConfig:               &nbcontractv1.RuncConfig{},
+			ContainerdConfig:         &nbcontractv1.ContainerdConfig{},
+			TeleportConfig:           &nbcontractv1.TeleportConfig{},
+			CustomLinuxOsConfig:      &nbcontractv1.CustomLinuxOSConfig{},
+			HttpProxyConfig:          &nbcontractv1.HTTPProxyConfig{},
+			CustomCloudConfig:        &nbcontractv1.CustomCloudConfig{},
+			CustomSearchDomainConfig: &nbcontractv1.CustomSearchDomainConfig{},
+		},
+	}
+	return nbcc
+}
+
+// Apply the configuration to the NBContractConfig nbContractConfiguration object
+func (nbcc *NBContractConfig) ApplyConfiguration(config *nbcontractv1.Configuration) {
+	if config == nil {
+		return
+	}
+	nbcc.nBContractConfiguration = config
+	nbcc.ensureConfigsNonNil()
+}
+
+// Get the NBContractConfiguration object
+func (nbcc *NBContractConfig) GetNBContractConfiguration() *nbcontractv1.Configuration {
+	return nbcc.nBContractConfiguration
 }
--- pkg/nbcontracthelper/helper_test.go
+++ pkg/nbcontracthelper/helper_test.go
@@ -8,9 +8,6 @@ import (
 )
 
 func TestNewNBContractConfiguration(t *testing.T) {
-	type args struct {
-		v *nbcontractv1.Configuration
-	}
 	wantedResult := &nbcontractv1.Configuration{
 		KubeBinaryConfig:         &nbcontractv1.KubeBinaryConfig{},
 		ApiServerConfig:          &nbcontractv1.ApiServerConfig{},
@@ -30,60 +27,88 @@ func TestNewNBContractConfiguration(t *testing.T) {
 	}
 	tests := []struct {
 		name string
-		args args
 		want *nbcontractv1.Configuration
 	}{
 		{
 			name: "Test with nil configuration",
-			args: args{
-				v: nil,
-			},
-			want: wantedResult,
-		},
-		{
-			name: "Test with non-nil configuration and all nil Configs",
-			args: args{
-				v: &nbcontractv1.Configuration{},
-			},
-			want: wantedResult,
-		},
-		{
-			name: "Test with non-nil configuration and partially nil Configs",
-			args: args{
-				v: &nbcontractv1.Configuration{
-					KubeBinaryConfig: &nbcontractv1.KubeBinaryConfig{},
-				},
-			},
-			want: wantedResult,
-		},
-		{
-			name: "Test with non-nil configuration and all non-nil Configs",
-			args: args{
-				v: &nbcontractv1.Configuration{
-					KubeBinaryConfig:         &nbcontractv1.KubeBinaryConfig{},
-					ApiServerConfig:          &nbcontractv1.ApiServerConfig{},
-					AuthConfig:               &nbcontractv1.AuthConfig{},
-					ClusterConfig:            &nbcontractv1.ClusterConfig{},
-					NetworkConfig:            &nbcontractv1.NetworkConfig{},
-					GpuConfig:                &nbcontractv1.GPUConfig{},
-					TlsBootstrappingConfig:   &nbcontractv1.TLSBootstrappingConfig{},
-					KubeletConfig:            &nbcontractv1.KubeletConfig{},
-					RuncConfig:               &nbcontractv1.RuncConfig{},
-					ContainerdConfig:         &nbcontractv1.ContainerdConfig{},
-					TeleportConfig:           &nbcontractv1.TeleportConfig{},
-					CustomLinuxOsConfig:      &nbcontractv1.CustomLinuxOSConfig{},
-					HttpProxyConfig:          &nbcontractv1.HTTPProxyConfig{},
-					CustomCloudConfig:        &nbcontractv1.CustomCloudConfig{},
-					CustomSearchDomainConfig: &nbcontractv1.CustomSearchDomainConfig{},
-				},
-			},
 			want: wantedResult,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := NewNBContractConfiguration(tt.args.v); !reflect.DeepEqual(got, tt.want) {
+			if got := NewNBContractConfiguration().nBContractConfiguration; !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewNBContractConfiguration() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNBContractConfig_ApplyConfiguration(t *testing.T) {
+	type fields struct {
+		nBContractConfiguration *nbcontractv1.Configuration
+	}
+	wantedResult := &nbcontractv1.Configuration{
+		KubeBinaryConfig:         &nbcontractv1.KubeBinaryConfig{},
+		ApiServerConfig:          &nbcontractv1.ApiServerConfig{},
+		AuthConfig:               &nbcontractv1.AuthConfig{},
+		ClusterConfig:            &nbcontractv1.ClusterConfig{},
+		NetworkConfig:            &nbcontractv1.NetworkConfig{},
+		GpuConfig:                &nbcontractv1.GPUConfig{},
+		TlsBootstrappingConfig:   &nbcontractv1.TLSBootstrappingConfig{},
+		KubeletConfig:            &nbcontractv1.KubeletConfig{},
+		RuncConfig:               &nbcontractv1.RuncConfig{},
+		ContainerdConfig:         &nbcontractv1.ContainerdConfig{},
+		TeleportConfig:           &nbcontractv1.TeleportConfig{},
+		CustomLinuxOsConfig:      &nbcontractv1.CustomLinuxOSConfig{},
+		HttpProxyConfig:          &nbcontractv1.HTTPProxyConfig{},
+		CustomCloudConfig:        &nbcontractv1.CustomCloudConfig{},
+		CustomSearchDomainConfig: &nbcontractv1.CustomSearchDomainConfig{},
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   *nbcontractv1.Configuration
+	}{
+		{
+			name: "Test with nil configuration",
+			fields: fields{
+				nBContractConfiguration: &nbcontractv1.Configuration{},
+			},
+			want: wantedResult,
+		},
+		{
+			name: "Apply nil AuthConfig configuration and expect AuthConfig in nBContractConfiguration to be non-nil",
+			fields: fields{
+				nBContractConfiguration: &nbcontractv1.Configuration{
+					AuthConfig: nil,
+				},
+			},
+			want: wantedResult,
+		},
+		{
+			name: "Apply some configurations and expect them to be applied",
+			fields: fields{
+				nBContractConfiguration: &nbcontractv1.Configuration{
+					AuthConfig: &nbcontractv1.AuthConfig{
+						TargetCloud: "some-cloud",
+					},
+					LinuxAdminUsername: "testuser",
+				},
+			},
+			want: func() *nbcontractv1.Configuration {
+				tmpResult := NewNBContractConfiguration().nBContractConfiguration
+				tmpResult.AuthConfig.TargetCloud = "some-cloud"
+				tmpResult.LinuxAdminUsername = "testuser"
+				return tmpResult
+			}(),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			nbcc := NewNBContractConfiguration()
+			nbcc.ApplyConfiguration(tt.fields.nBContractConfiguration)
+			if got := nbcc.nBContractConfiguration; !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ApplyConfiguration() = %v, want %v", got, tt.want)
 			}
 		})
 	}
