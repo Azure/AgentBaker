ef16f541506b6cf14694565c460ba46fc91587f5
Zachary Bailey <t-zbailey@microsoft.com>
2024-04-17 11:11:39 -0700

    chore: modify install-dependencies.sh to use json benchmarks

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 script_start_timestamp=$(date +%H:%M:%S)
-start_timestamp=$(date +%H:%M:%S)
+section_start_timestamp=$(date +%H:%M:%S)
 
-capture_script_start=$(date +%s)
-capture_time=$(date +%s)
+script_start_stopwatch=$(date +%s)
+section_start_stopwatch=$(date +%s)
 
 declare -a benchmarks=()
+declare -a jsonBenchmarks=()
 
 OS=$(sort -r /etc/*-release | gawk 'match($0, /^(ID_LIKE=(coreos)|ID=(.*))$/, a) { print toupper(a[2] a[3]); exit }')
 OS_VERSION=$(sort -r /etc/*-release | gawk 'match($0, /^(VERSION_ID=(.*))$/, a) { print toupper(a[2] a[3]); exit }' | tr -d '"')
@@ -27,7 +28,7 @@ COMPONENTS_FILEPATH=/opt/azure/components.json
 
 echo ""
 echo "Components downloaded in this VHD build (some of the below components might get deleted during cluster provisioning if they are not needed):" >> ${VHD_LOGS_FILEPATH}
-stop_watch $capture_time "Declare Variables / Configure Environment" false
+capture_benchmarks false "declare_variables_and_configure_environment"
 start_watch
 
 echo "Logging the kernel after purge and reinstall + reboot: $(uname -r)"
@@ -53,7 +54,7 @@ APT::Periodic::AutocleanInterval "0";
 APT::Periodic::Unattended-Upgrade "0";
 EOF
 fi
-stop_watch $capture_time "Purge and Re-install Ubuntu" false
+capture_benchmarks false "purge_and_reinstall_ubuntu"
 start_watch
 
 # If the IMG_SKU does not contain "minimal", installDeps normally
@@ -83,7 +84,7 @@ SystemMaxUse=1G
 RuntimeMaxUse=1G
 ForwardToSyslog=yes
 EOF
-stop_watch $capture_time "Install Dependencies" false
+capture_benchmarks false "install_dependencies"
 start_watch
 
 if [[ ${CONTAINER_RUNTIME:-""} != "containerd" ]]; then
@@ -111,7 +112,7 @@ if [[ "${UBUNTU_RELEASE}" == "18.04" || "${UBUNTU_RELEASE}" == "20.04" || "${UBU
   overrideNetworkConfig || exit 1
   disableNtpAndTimesyncdInstallChrony || exit 1
 fi
-stop_watch $capture_time "Check Container Runtime / Network Configurations" false
+capture_benchmarks false "check_container_runtime_and_network_configurations"
 start_watch
 
 CONTAINERD_SERVICE_DIR="/etc/systemd/system/containerd.service.d"
@@ -168,7 +169,7 @@ containerd_version="$(echo "$installed_version" | cut -d- -f1)"
 containerd_patch_version="$(echo "$installed_version" | cut -d- -f2)"
 installStandaloneContainerd ${containerd_version} ${containerd_patch_version}
 echo "  - [installed] containerd v${containerd_version}-${containerd_patch_version}" >> ${VHD_LOGS_FILEPATH}
-stop_watch $capture_time "Create Containerd Service Directory, Download Shims, Configure Runtime and Network" false
+capture_benchmarks false "create_containerd_service_directory_download_shims_configure_runtime_and_network"
 start_watch
 
 DOWNLOAD_FILES=$(jq ".DownloadFiles" $COMPONENTS_FILEPATH | jq .[] --monochrome-output --compact-output)
@@ -190,7 +191,7 @@ for CRICTL_VERSION in ${CRICTL_VERSIONS}; do
   downloadCrictl ${CRICTL_VERSION}
   echo "  - crictl version ${CRICTL_VERSION}" >> ${VHD_LOGS_FILEPATH}
 done
-stop_watch $capture_time "Download Components, Determine / Download crictl Version" false
+capture_benchmarks false "download_crictl"
 start_watch
 
 installAndConfigureArtifactStreaming() {
@@ -229,7 +230,7 @@ downloadTeleportdPlugin ${TELEPORTD_PLUGIN_DOWNLOAD_URL} "0.8.0"
 
 INSTALLED_RUNC_VERSION=$(runc --version | head -n1 | sed 's/runc version //')
 echo "  - runc version ${INSTALLED_RUNC_VERSION}" >> ${VHD_LOGS_FILEPATH}
-stop_watch $capture_time "Artifact Streaming, Download Containerd Plugins" false
+capture_benchmarks false "artifact_streaming_and_teleportd_download"
 start_watch
 
 if [[ $OS == $UBUNTU_OS_NAME && $(isARM64) != 1 ]]; then  # no ARM64 SKU with GPU now
@@ -265,7 +266,7 @@ cat << EOF >> ${VHD_LOGS_FILEPATH}
 EOF
 
 echo "${CONTAINER_RUNTIME} images pre-pulled:" >> ${VHD_LOGS_FILEPATH}
-stop_watch $capture_time "Pull NVIDIA driver image (mcr), Start installBcc subshell" false
+capture_benchmarks false "pull_nvidia_driver_image(mcr)_and_run_installBcc_in_subshell"
 start_watch
 
 string_replace() {
@@ -313,7 +314,7 @@ watcherStaticImg=${watcherBaseImg//\*/static}
 
 # can't use cliTool because crictl doesn't support retagging.
 retagContainerImage "ctr" ${watcherFullImg} ${watcherStaticImg}
-stop_watch $capture_time "Pull and Re-tag Container Images" false
+capture_benchmarks false "pull_and_retag_container_images"
 start_watch
 
 # doing this at vhd allows CSE to be faster with just mv
@@ -376,7 +377,7 @@ done
 if [[ $OS == $UBUNTU_OS_NAME || ( $OS == $MARINER_OS_NAME && $OS_VERSION == "2.0" ) ]]; then
   systemctlEnableAndStart ipv6_nftables || exit 1
 fi
-stop_watch $capture_time "Configure Networking and Interface" false
+capture_benchmarks false "configure_networking_and_interface"
 start_watch
 
 if [[ $OS == $UBUNTU_OS_NAME && $(isARM64) != 1 ]]; then  # no ARM64 SKU with GPU now
@@ -405,7 +406,7 @@ if grep -q "fullgpu" <<< "$FEATURE_FLAGS" && grep -q "gpudaemon" <<< "$FEATURE_F
   systemctlEnableAndStart nvidia-device-plugin || exit 1
 fi
 fi
-stop_watch $capture_time "GPU Device plugin" false
+capture_benchmarks false "gpu_device_plugin"
 start_watch
 
 # Kubelet credential provider plugins
@@ -451,7 +452,7 @@ for KUBE_PROXY_IMAGE_VERSION in ${KUBE_PROXY_IMAGE_VERSIONS}; do
   # shellcheck disable=SC2181
   echo "  - ${CONTAINER_IMAGE}" >>${VHD_LOGS_FILEPATH}
 done
-stop_watch $capture_time "Configure Telemetry, Create Logging Directory, Kube-proxy" false
+capture_benchmarks false "configure_telemetry_create_logging_directory_and_download_kube-proxy_images"
 start_watch
 
 # download kubernetes package from the given URL using MSI for auth for azcopy
@@ -517,8 +518,7 @@ for PATCHED_KUBE_BINARY_VERSION in ${KUBE_BINARY_VERSIONS}; do
 done
 
 rm -f ./azcopy # cleanup immediately after usage will return in two downloads
-stop_watch $capture_time "Download and Process Kubernetes Packages / Extract Binaries" false
+capture_benchmarks false "download_kubernetes_binaries"
 
 echo "install-dependencies step completed successfully"
-stop_watch $capture_script_start "install-dependencies.sh" true
-show_benchmarks
+capture_benchmarks true "install-dependencies.sh"
