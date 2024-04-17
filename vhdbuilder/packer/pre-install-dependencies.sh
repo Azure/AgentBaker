1958e60ea83606ad80a36d97e71b213b38d788f3
Zachary Bailey <t-zbailey@microsoft.com>
2024-04-17 12:37:56 -0700

    add jq

--- vhdbuilder/packer/pre-install-dependencies.sh
+++ vhdbuilder/packer/pre-install-dependencies.sh
@@ -25,7 +25,7 @@ source /home/packer/tool_installs.sh
 source /home/packer/tool_installs_distro.sh
 source /home/packer/packer_source.sh
 
-snap install jq
+sudo snap install jq
 export PATH=$PATH:/snap/bin
 jq --version
 capture_benchmarks false "declare_variables_remove_comments_and_execute_packer_files"
