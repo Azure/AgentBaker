936b74e52e23cfba0aa62b3fe6cf5a83544a2a46
Zachary Bailey <t-zbailey@microsoft.com>
2024-04-17 18:31:12 -0700

    fix typo

--- vhdbuilder/packer/pre-install-dependencies.sh
+++ vhdbuilder/packer/pre-install-dependencies.sh
@@ -25,7 +25,7 @@ source /home/packer/tool_installs.sh
 source /home/packer/tool_installs_distro.sh
 source /home/packer/packer_source.sh
 
-installSnapd
+installJq
 capture_benchmarks false "declare_variables_remove_comments_and_execute_packer_files"
 start_watch
 
