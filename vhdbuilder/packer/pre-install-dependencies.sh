9bf38794dd5cc4a4e6f7e82fce5b8e502bbc7729
Zachary Bailey <t-zbailey@microsoft.com>
2024-04-18 07:42:30 -0700

    get os version

--- vhdbuilder/packer/pre-install-dependencies.sh
+++ vhdbuilder/packer/pre-install-dependencies.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 script_start_timestamp=$(date +%H:%M:%S)
-start_timestamp=$(date +%H:%M:%S)
+section_start_timestamp=$(date +%H:%M:%S)
 
-capture_script_start=$(date +%s)
-capture_time=$(date +%s)
+script_start_stopwatch=$(date +%s)
+section_start_stopwatch=$(date +%s)
 
 declare -a benchmarks=()
+declare -a jsonBenchmarks=()
 
 OS=$(sort -r /etc/*-release | gawk 'match($0, /^(ID_LIKE=(coreos)|ID=(.*))$/, a) { print toupper(a[2] a[3]); exit }')
 OS_VERSION=$(sort -r /etc/*-release | gawk 'match($0, /^(VERSION_ID=(.*))$/, a) { print toupper(a[2] a[3]); exit }' | tr -d '"')
 THIS_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)"
 
+echo $OS
+echo "OS VERSION"
 #the following sed removes all comments of the format {{/* */}}
 sed -i 's/{{\/\*[^*]*\*\/}}//g' /home/packer/provision_source.sh
 sed -i 's/{{\/\*[^*]*\*\/}}//g' /home/packer/tool_installs_distro.sh
@@ -23,39 +26,39 @@ source /home/packer/provision_source_distro.sh
 source /home/packer/tool_installs.sh
 source /home/packer/tool_installs_distro.sh
 source /home/packer/packer_source.sh
-stop_watch $capture_time "Declare Variables / Remove Comments / Execute home/packer files" false
-start_watch
 
 CPU_ARCH=$(getCPUArch)  #amd64 or arm64
 VHD_LOGS_FILEPATH=/opt/azure/vhd-install.complete
 COMPONENTS_FILEPATH=/opt/azure/components.json
 MANIFEST_FILEPATH=/opt/azure/manifest.json
 KUBE_PROXY_IMAGES_FILEPATH=/opt/azure/kube-proxy-images.json
+sudo mkdir -p /opt/azure/perf
 #this is used by post build test to check whether the compoenents do indeed exist
 cat components.json > ${COMPONENTS_FILEPATH}
 cat manifest.json > ${MANIFEST_FILEPATH}
 cat ${THIS_DIR}/kube-proxy-images.json > ${KUBE_PROXY_IMAGES_FILEPATH}
 echo "Starting build on " $(date) > ${VHD_LOGS_FILEPATH}
-stop_watch $capture_time "Create Post-build Test" false
-start_watch
 
 if [[ $OS == $MARINER_OS_NAME ]]; then
   chmod 755 /opt
   chmod 755 /opt/azure
+  chmod 755 /opt/azure/perf
   chmod 644 ${VHD_LOGS_FILEPATH}
 fi
-stop_watch $capture_time "Set Permissions if Mariner" false
+
+installJq
+capture_benchmarks false "set_mariner_permissions_declare_variables_and_execute_packer_files"
 start_watch
 
 copyPackerFiles
 systemctlEnableAndStart disk_queue || exit 1
-stop_watch $capture_time "Copy Packer Files" false
+capture_benchmarks false "copy_packer_files"
 start_watch
 
 mkdir /opt/certs
 chmod 1666 /opt/certs
 systemctlEnableAndStart update_certs.path || exit 1
-stop_watch $capture_time "Make Certs Directory / Set Permissions / Update Certs" false
+capture_benchmarks false "update_certs_make_directory_and_set_permissions"
 start_watch
 
 systemctlEnableAndStart ci-syslog-watcher.path || exit 1
@@ -64,17 +67,17 @@ systemctlEnableAndStart ci-syslog-watcher.service || exit 1
 # enable AKS log collector
 echo -e "\n# Disable WALA log collection because AKS Log Collector is installed.\nLogs.Collect=n" >> /etc/waagent.conf || exit 1
 systemctlEnableAndStart aks-log-collector.timer || exit 1
-stop_watch $capture_time "Start System Logs / AKS Log Collector" false
+capture_benchmarks false "start_sys_logs_and_aks_log_collector"
 start_watch
 
 # enable the modified logrotate service and remove the auto-generated default logrotate cron job if present
 systemctlEnableAndStart logrotate.timer || exit 1
 rm -f /etc/cron.daily/logrotate
-stop_watch $capture_time "Start Modified Log-rotate Service / Remove Auto-generated Service" false
+capture_benchmarks false "enable_modified_log_rotate_service"
 start_watch
 
 systemctlEnableAndStart sync-container-logs.service || exit 1
-stop_watch $capture_time "Sync Container Logs" false
+capture_benchmarks false "sync_container_logs"
 start_watch
 
 # First handle Mariner + FIPS
@@ -112,7 +115,7 @@ else
     installFIPS
   fi
 fi
-stop_watch $capture_time "Handle Mariner / FIPS Configurations" false
+capture_benchmarks false "handle_mariner_and_fips_configurations"
 start_watch
 
 # Handle Azure Linux + CgroupV2
@@ -134,8 +137,6 @@ if [[ "${UBUNTU_RELEASE}" == "22.04" && "${ENABLE_FIPS,,}" != "true" ]]; then
   
   update-grub
 fi
-stop_watch $capture_time "Handle Azure Linux / CgroupV2" false
-
+capture_benchmarks false "handle_azureLinux_and_cgroupV2"
 echo "pre-install-dependencies step finished successfully"
-stop_watch $capture_script_start "pre-install-dependencies.sh" true
-show_benchmarks
+capture_benchmarks true "pre_install_dependencies.sh"
