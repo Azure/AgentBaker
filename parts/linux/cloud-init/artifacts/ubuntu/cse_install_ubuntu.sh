98c7a91db80d878c216f01c3dd0cc7eeb7fb50c0
anujmaheshwari1 <amaheshwari@microsoft.com>
2024-04-18 11:48:45 -0700

    fix: skip blobfuse install on 24.04

--- parts/linux/cloud-init/artifacts/ubuntu/cse_install_ubuntu.sh
+++ parts/linux/cloud-init/artifacts/ubuntu/cse_install_ubuntu.sh
@@ -45,7 +45,7 @@ installDeps() {
     fi
 
     pkg_list+=(blobfuse2=${BLOBFUSE2_VERSION})
-    if [[ $(isARM64) != 1 ]]; then
+    if [[ $(isARM64) != 1 && "${OSVERSION}" != "24.04" ]]; then
         # blobfuse2 is installed for all ubuntu versions, it is included in pkg_list
         # for 22.04, fuse3 is installed. for all others, fuse is installed
         # for 16.04, installed blobfuse1.3.7, for all others except 22.04, installed blobfuse1.4.5
