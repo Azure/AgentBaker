46fc40e405c8b1b543aad3119c454581d1b28208
Zachary Bailey <t-zbailey@microsoft.com>
2024-05-01 12:46:43 -0700

    perf: modify convert-osdisk-snapshot-to-sig-x64.sh to enable shallow replication

--- vhdbuilder/packer/convert-osdisk-snapshot-to-sig.sh
+++ vhdbuilder/packer/convert-osdisk-snapshot-to-sig.sh
@@ -19,9 +19,16 @@ CAPTURED_SIG_VERSION="$(grep "captured_sig_version" ./vhdbuilder/packer/settings
 SIG_IMAGE_NAME="$(grep "sig_image_name" ./vhdbuilder/packer/settings.json | awk -F':' '{print $2}' | awk -F'"' '{print $2}')" && \
 echo "ManagedImageSharedImageGalleryId: /subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${AZURE_RESOURCE_GROUP_NAME}/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/${SIG_IMAGE_NAME}/versions/${CAPTURED_SIG_VERSION}"
 
+ARCH_STRING=""
+if [[ "${ARCHITECTURE,,}" == "arm64" ]]; then
+ARCH_STRING+="--architecture Arm64"
+elif 
+ARCH_STRING=""
+fi
+
 time_now=$(echo ${CAPTURED_SIG_VERSION} | cut -d '.' -f2)
 disk_snapshot_id="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${AZURE_RESOURCE_GROUP_NAME}/providers/Microsoft.Compute/snapshots/${ARM64_OS_DISK_SNAPSHOT_NAME}"
-az snapshot update --resource-group ${AZURE_RESOURCE_GROUP_NAME} -n ${ARM64_OS_DISK_SNAPSHOT_NAME} --architecture Arm64
+az snapshot update --resource-group ${AZURE_RESOURCE_GROUP_NAME} -n ${ARM64_OS_DISK_SNAPSHOT_NAME} ${ARCH_STRING}
 az sig image-version create --location $AZURE_LOCATION --resource-group ${AZURE_RESOURCE_GROUP_NAME} --gallery-name PackerSigGalleryEastUS \
      --gallery-image-definition ${SIG_IMAGE_NAME} --gallery-image-version ${CAPTURED_SIG_VERSION} \
-     --os-snapshot ${disk_snapshot_id} --tags now=${time_now}
+     --os-snapshot ${disk_snapshot_id} --tags now=${time_now} --replication-mode Shallow
