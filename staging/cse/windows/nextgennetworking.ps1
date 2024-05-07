13bab48ef7ef741346635ce1e4e882acaaeaba78
Will Daly <widaly@microsoft.com>
2024-05-07 13:21:29 -0700

    feat: placeholder for windows next-gen networking func

---
+++ staging/cse/windows/nextgennetworking.ps1
@@ -0,0 +1,10 @@
+function Start-InstallWindowsNextGenNetworking {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$NextGenNetworkingURL
+    )
+
+    Logs-To-Event -TaskName "AKS.WindowsCSE.InstallWindowsNextGenNetworking" -TaskMessage "Start to install Windows next-gen networking eBPF using URL $NextGenNetworkingURL"
+
+    # TODO: this is a placeholder for the installation script.
+}
