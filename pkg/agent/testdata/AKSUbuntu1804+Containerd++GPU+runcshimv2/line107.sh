e9d75ceabf16b4886f2d37ebc5e9189bc09394bd
Cameron Meissner <cameissner@microsoft.com>
2024-05-08 17:39:31 -0700

    chore: update kubelet service file

--- pkg/agent/testdata/AKSUbuntu1804+Containerd++GPU+runcshimv2/line107.sh
+++ pkg/agent/testdata/AKSUbuntu1804+Containerd++GPU+runcshimv2/line107.sh
@@ -11,17 +11,7 @@ TimeoutStartSec=270
 EnvironmentFile=/etc/default/kubelet
 SuccessExitStatus=143
 
-ExecStart=/usr/local/bin/kubelet \
-        --enable-server \
-        --node-labels="${KUBELET_NODE_LABELS}" \
-        --v=2 \
-        --volume-plugin-dir=/etc/kubernetes/volumeplugins \
-        $KUBELET_TLS_BOOTSTRAP_FLAGS \
-        $KUBELET_CONFIG_FILE_FLAGS \
-        $KUBELET_CONTAINERD_FLAGS \
-        $KUBELET_CONTAINER_RUNTIME_FLAG \
-        $KUBELET_CGROUP_FLAGS \
-        $KUBELET_FLAGS
+ExecStart=/opt/azure/start-kubelet.sh
 
 [Install]
 WantedBy=multi-user.target
