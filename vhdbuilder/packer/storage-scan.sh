1303f38db064988abe133229be0306934769f7a2
Alison Burgess <alburgess@microsoft.com>
2024-05-09 16:54:38 -0700

    adjust script

--- vhdbuilder/packer/storage-scan.sh
+++ vhdbuilder/packer/storage-scan.sh
@@ -5,8 +5,10 @@ STORAGE_REPORT_PATH=/opt/azure/containers/storage-report.txt
 
 mkdir -p "$(dirname "${STORAGE_REPORT_PATH}")"
 
-sudo find / -type f -size +1M -exec du -h {} + | sort -h >> $STORAGE_REPORT_PATH
 echo "----" >> $STORAGE_REPORT_PATH
-df -h >> $STORAGE_REPORT_PATH
+df -h 2>&1 >> $STORAGE_REPORT_PATH
+echo "----" >> $STORAGE_REPORT_PATH
+find / -type f -size +1M -exec du -h {} + 2>/dev/null | sort -rh >> $STORAGE_REPORT_PATH
+
 
 chmod a+r "${STORAGE_REPORT_PATH}"
