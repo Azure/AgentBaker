4f7df101baca9f63be6c5fa3a63fda7df5a8ea55
Abel Hu <abelch@microsoft.com>
2024-05-10 05:47:17 +0000

    fix: remove the test

--- e2e/windows/upload-cse-logs.ps1
+++ e2e/windows/upload-cse-logs.ps1
@@ -15,7 +15,3 @@ $env:AZCOPY_MSI_RESOURCE_STRING=$arg4
 C:\k\debug\collect-windows-logs.ps1
 $CollectedLogs=(Get-ChildItem . -Filter "*$arg3*" -File)[0].Name
 .\azcopy.exe copy $CollectedLogs "https://$arg1.blob.core.windows.net/$arg2/$arg3-collected-node-logs.zip"
-rm $CollectedLogs
-C:\k\debug\collect-windows-logs.ps1 -enableAll
-$CollectedLogs=(Get-ChildItem . -Filter "*$arg3*" -File)[0].Name
-.\azcopy.exe copy $CollectedLogs "https://$arg1.blob.core.windows.net/$arg2/$arg3-collected-node-full-logs.zip"
