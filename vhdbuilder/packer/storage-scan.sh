152bb7dc3df0f4e88eccd1fbe6c9633dddb28601
Alison Burgess <alburgess@microsoft.com>
2024-05-09 19:08:11 -0700

    add some sudos?

--- vhdbuilder/packer/storage-scan.sh
+++ vhdbuilder/packer/storage-scan.sh
@@ -6,9 +6,9 @@ STORAGE_REPORT_PATH=/opt/azure/containers/storage-report.txt
 mkdir -p "$(dirname "${STORAGE_REPORT_PATH}")"
 
 echo "----" >> $STORAGE_REPORT_PATH
-df -h 2>&1 >> $STORAGE_REPORT_PATH
+sudo df -h 2>&1 >> $STORAGE_REPORT_PATH
 echo "----" >> $STORAGE_REPORT_PATH
-find / -type f -size +1M -exec du -h {} + 2>/dev/null | sort -rh >> $STORAGE_REPORT_PATH
+sudo find / -type f -size +1M -exec du -h {} + 2>/dev/null | sort -rh >> $STORAGE_REPORT_PATH
 
 
 chmod a+r "${STORAGE_REPORT_PATH}"
