03bbfa95f7cdc97b0c6290f0657edd573ba48956
Alison Burgess <alburgess@microsoft.com>
2024-05-10 01:26:35 -0700

    run some error handling

--- vhdbuilder/packer/storage-scan.sh
+++ vhdbuilder/packer/storage-scan.sh
@@ -6,9 +6,12 @@ STORAGE_REPORT_PATH=/opt/azure/containers/storage-report.txt
 mkdir -p "$(dirname "${STORAGE_REPORT_PATH}")"
 
 echo "----" >> $STORAGE_REPORT_PATH
-sudo df -h 2>&1 >> $STORAGE_REPORT_PATH
+df -h 2>&1 >> $STORAGE_REPORT_PATH
 echo "----" >> $STORAGE_REPORT_PATH
-sudo find / -type f -size +1M -exec du -h {} + 2>/dev/null | sort -rh >> $STORAGE_REPORT_PATH
+if ! sudo find / -type f -size +1M -exec du -h {} + 2>/dev/null | sort -rh >> "$STORAGE_REPORT_PATH"; then
+    error_message=$(sudo find / -type f -size +1M -exec du -h {} + 2>&1 >/dev/null | sort -rh)
+    echo "Error: $error_message" >&2
+fi
 
 
 chmod a+r "${STORAGE_REPORT_PATH}"
