1270670c95a42bfbf16ef5384bab4720c6c6b31c
Junjie Zhang <junjiezhang@microsoft.com>
2024-05-10 05:17:26 +0000

    chore: add enableAll to collect logs in AgentBaker Windows E2E

--- e2e/windows/upload-cse-logs.ps1
+++ e2e/windows/upload-cse-logs.ps1
@@ -12,6 +12,6 @@ $env:AZCOPY_AUTO_LOGIN_TYPE="MSI"
 $env:AZCOPY_MSI_RESOURCE_STRING=$arg4
 .\azcopy.exe copy "C:\azuredata\CustomDataSetupScript.log" "https://$arg1.blob.core.windows.net/$arg2/$arg3-cse.log"
 .\azcopy.exe copy "C:\AzureData\provision.complete" "https://$arg1.blob.core.windows.net/$arg2/$arg3-provision.complete"
-C:\k\debug\collect-windows-logs.ps1
+C:\k\debug\collect-windows-logs.ps1 -enableAll
 $CollectedLogs=(Get-ChildItem . -Filter "*$arg3*" -File)[0].Name
 .\azcopy.exe copy $CollectedLogs "https://$arg1.blob.core.windows.net/$arg2/$arg3-collected-node-logs.zip"
