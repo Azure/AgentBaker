bd7e801e20dfa31c80abbd9ca8757d5f47e9f8a3
Alison <32000834+AlisonB319@users.noreply.github.com>
2024-05-10 09:30:22 -0700

    Mount check space unmount

--- vhdbuilder/packer/storage-scan.sh
+++ vhdbuilder/packer/storage-scan.sh
@@ -8,10 +8,21 @@ mkdir -p "$(dirname "${STORAGE_REPORT_PATH}")"
 echo "----" >> $STORAGE_REPORT_PATH
 df -h 2>&1 >> $STORAGE_REPORT_PATH
 echo "----" >> $STORAGE_REPORT_PATH
+
+CUR_DIR=$(pwd)
+
+mkdir -p /mnt/sdb1
+mount /dev/sdb1 /mnt/sdb1
+cd /mnt/sdb1
+
 if ! find / -type f -size +1M -maxdepth 0 -exec du -h {} + 2>/dev/null | sort -rh >> "$STORAGE_REPORT_PATH"; then
-    error_message=$(find / -type f -size +1M -maxdepth 0 -exec du -h {} + 2>&1 >/dev/null | sort -rh)
+    error_message=$(find / -type f -size +1M -exec du -h {} + 2>&1 >/dev/null | sort -rh)
     echo "Error: $error_message" >&2
 fi
+
+cd "$CUR_DIR"
+umount /mnt/sdb1
+rmdir /mnt/sdb1
 
 
 chmod a+r "${STORAGE_REPORT_PATH}"
