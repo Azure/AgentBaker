57f5725ac6cfd98236b93bb15b3f8252c251b5f9
anujmaheshwari1 <amaheshwari@microsoft.com>
2024-05-13 12:16:37 -0700

    fix: use exact package versions instead of lts ones

--- vhdbuilder/packer/pre-install-dependencies.sh
+++ vhdbuilder/packer/pre-install-dependencies.sh
@@ -129,7 +129,7 @@ if [[ "${UBUNTU_RELEASE}" == "22.04" && "${ENABLE_FIPS,,}" != "true" ]]; then
   echo "After purging kernel, dpkg list should be empty"; dpkg -l 'linux-*azure*'
 
   # Install lts-22.04 kernel, ping it to 1061
-  DEBIAN_FRONTEND=noninteractive apt-get install -y linux-image-azure-lts-22.04 linux-cloud-tools-azure-lts-22.04 linux-headers-azure-lts-22.04 linux-modules-extra-azure-lts-22.04 linux-tools-azure-lts-22.04
+  DEBIAN_FRONTEND=noninteractive apt-get install -y linux-image-5.15.0-1061-azure linux-cloud-tools-5.15.0-1061-azure linux-headers-5.15.0-1061-azure linux-modules-extra-5.15.0-1061-azure linux-tools-5.15.0-1061-azure
   echo "After installing new kernel, here is a list of kernels/headers installed"; dpkg -l 'linux-*azure*'
   
   update-grub
