f97a0519f77fe91ca9bd13654cf8562a68d16bbb
anujmaheshwari1 <amaheshwari@microsoft.com>
2024-05-13 10:49:28 -0700

    chore: pin kernel for 22.04 to 1061

--- vhdbuilder/packer/pre-install-dependencies.sh
+++ vhdbuilder/packer/pre-install-dependencies.sh
@@ -128,8 +128,12 @@ if [[ "${UBUNTU_RELEASE}" == "22.04" && "${ENABLE_FIPS,,}" != "true" ]]; then
   DEBIAN_FRONTEND=noninteractive apt-get remove --purge -y $(dpkg-query -W 'linux-*azure*' | awk '$2 != "" { print $1 }' | paste -s)
   echo "After purging kernel, dpkg list should be empty"; dpkg -l 'linux-*azure*'
 
-  # Install lts-22.04 kernel
-  DEBIAN_FRONTEND=noninteractive apt-get install -y linux-image-azure-lts-22.04 linux-cloud-tools-azure-lts-22.04 linux-headers-azure-lts-22.04 linux-modules-extra-azure-lts-22.04 linux-tools-azure-lts-22.04
+  # Install lts-22.04 kernel, ping it to 1061
+  pkg_list=(linux-image-azure-lts-22.04 linux-cloud-tools-azure-lts-22.04 linux-headers-azure-lts-22.04 linux-modules-extra-azure-lts-22.04 linux-tools-azure-lts-22.04)
+  for linux_package in ${pkg_list[*]}; do
+    kernel_version=$(apt-cache madison ${linux_package} | awk 'NR==1{print $3}')
+    DEBIAN_FRONTEND=noninteractive apt-get install -y ${linux_package}=${kernel_version}
+  done
   echo "After installing new kernel, here is a list of kernels/headers installed"; dpkg -l 'linux-*azure*'
   
   update-grub
