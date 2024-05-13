22a58ee3ffce469dcbc5b53cb338bf6eb243a5ea
anujmaheshwari1 <amaheshwari@microsoft.com>
2024-05-13 11:53:29 -0700

    fix: revert to original code

--- vhdbuilder/packer/pre-install-dependencies.sh
+++ vhdbuilder/packer/pre-install-dependencies.sh
@@ -129,12 +129,7 @@ if [[ "${UBUNTU_RELEASE}" == "22.04" && "${ENABLE_FIPS,,}" != "true" ]]; then
   echo "After purging kernel, dpkg list should be empty"; dpkg -l 'linux-*azure*'
 
   # Install lts-22.04 kernel, ping it to 1061
-  pkg_list=(linux-image-azure-lts-22.04 linux-cloud-tools-azure-lts-22.04 linux-headers-azure-lts-22.04 linux-modules-extra-azure-lts-22.04 linux-tools-azure-lts-22.04)
-  for linux_package in ${pkg_list[*]}; do
-    # Ideal way to do this is to use apt-cache madison, however, after we purge kernel and do apt-get update, madison gets 1063
-    # Therefore hardcoding it now
-    DEBIAN_FRONTEND=noninteractive apt-get install -y ${linux_package}=5.15.0.1061.70
-  done
+  DEBIAN_FRONTEND=noninteractive apt-get install -y linux-image-azure-lts-22.04=5.15.0.1061.70 linux-cloud-tools-azure-lts-22.04=5.15.0.1061.70 linux-headers-azure-lts-22.04=5.15.0.1061.70 linux-modules-extra-azure-lts-22.04=5.15.0.1061.70 linux-tools-azure-lts-22.04=5.15.0.1061.70
   echo "After installing new kernel, here is a list of kernels/headers installed"; dpkg -l 'linux-*azure*'
   
   update-grub
