5ca9c27ddd9af3fc4b185e7a6f4016d88ba7721e
Christian Muirhead <christian.muirhead@microsoft.com>
2024-05-23 16:21:03 +1200

    Remove SAS token from VHD url in publishing info
    The identity downloading the VHDs in the release step should have
    access to the blobs without needing a token.

--- vhdbuilder/packer/generate-vhd-publishing-info.sh
+++ vhdbuilder/packer/generate-vhd-publishing-info.sh
@@ -81,35 +81,10 @@ else
     IMAGE_ARCH="x64"
 fi
 
-echo "generating traditional SAS token with CLASSIC_SA_CONNECTION_STRING..."
-start_date=$(date +"%Y-%m-%dT00:00Z" -d "-1 day")
-expiry_date=$(date +"%Y-%m-%dT00:00Z" -d "+1 year")
-if [[ "${OS_NAME,,}" != "windows" ]]; then
-    [ -z "${OUTPUT_STORAGE_CONTAINER_NAME}" ] && echo "OUTPUT_STORAGE_CONTAINER_NAME should be set..." && exit 1
-    echo "storage container name: ${OUTPUT_STORAGE_CONTAINER_NAME}"
-    # max of 7 day expiration time when using user delegation SAS
-    sas_token=$(az storage container generate-sas --name ${OUTPUT_STORAGE_CONTAINER_NAME} --permissions lr --connection-string ${CLASSIC_SA_CONNECTION_STRING} --start ${start_date} --expiry ${expiry_date} | tr -d '"')
-else
-    # we still need to use the original connection string when not using a system-assigned identity on 1ES pools
-    sas_token=$(az storage container generate-sas --name vhds --permissions lr --connection-string ${CLASSIC_SA_CONNECTION_STRING} --start ${start_date} --expiry ${expiry_date} | tr -d '"')
-fi
-
-if [ "$sas_token" == "" ]; then
-    echo "sas_token is empty"
-    exit 1
-fi
-vhd_url="${STORAGE_ACCT_BLOB_URL}/${VHD_NAME}?$sas_token"
-
-echo "Testing whether the generated sas token works"
-vhd_size=$(curl -sI $vhd_url | grep -i Content-Length | awk '{print $2}')
-if [ "$vhd_size" == "" ]; then
-    echo "The genrated sas token does not work"
-    exit 1
-fi
-echo "The generated sas token works"
+vhd_url="${STORAGE_ACCT_BLOB_URL}/${VHD_NAME}"
 
 # Do not log sas token
-echo "COPY ME ---> ${STORAGE_ACCT_BLOB_URL}/${VHD_NAME}?***"
+echo "COPY ME ---> ${vhd_url}"
 
 # Note: The offer_name is the value from OS_SKU (eg. Ubuntu)
 if [ "${OS_NAME,,}" == "linux" ]; then
