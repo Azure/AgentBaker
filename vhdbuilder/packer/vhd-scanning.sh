f6472f47a0b894a5a03ab531c2930d6c1b966c20
Alison Burgess <alburgess@microsoft.com>
2024-05-22 19:55:18 -0700

    updated method of scanning

--- vhdbuilder/packer/vhd-scanning.sh
+++ vhdbuilder/packer/vhd-scanning.sh
@@ -58,76 +58,12 @@ az vm run-command invoke \
     --resource-group $RESOURCE_GROUP_NAME \
     --scripts @$SCRIPT_PATH
 
-if [[ "$OS_SKU" == "Ubuntu" ]] && [[ "$OS_VERSION" == "20.04" ]]; then
-    az vm run-command invoke \
-        --command-id RunShellScript \
-        --name $VM_NAME \
-        --resource-group $RESOURCE_GROUP_NAME \
-        --scripts "sudo apt-get install -y azure-cli"
-
-elif [[ "$OS_SKU" == "Ubuntu" ]] && [[ "$OS_VERSION" == "22.04" ]]; then
-    az vm run-command invoke \
-        --command-id RunShellScript \
-        --name $VM_NAME \
-        --resource-group $RESOURCE_GROUP_NAME \
-        --scripts "sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg &&
-            curl -sL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add - &&
-            echo \"deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ \$(lsb_release -cs) main\" | sudo tee /etc/apt/sources.list.d/azure-cli.list"
-    
-    az vm run-command invoke \
-        --command-id RunShellScript \
-        --name $VM_NAME \
-        --resource-group $RESOURCE_GROUP_NAME \
-        --scripts "sudo apt-get update -y && sudo apt-get upgrade -y"
-
-    az vm run-command invoke \
-        --command-id RunShellScript \
-        --name $VM_NAME \
-        --resource-group $RESOURCE_GROUP_NAME \
-        --scripts "sudo apt-get install -y azure-cli"
-
-elif [ "$OS_SKU" == "CBLMariner" ] || [ "$OS_SKU" == "AzureLinux" ]; then
-    az vm run-command invoke \
-        --command-id RunShellScript \
-        --name $VM_NAME \
-        --resource-group $RESOURCE_GROUP_NAME \
-        --scripts "sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
-            sudo sh -c 'echo -e \"[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\" > /etc/yum.repos.d/azure-cli.repo' &&
-            sudo dnf install -y azure-cli"
-else
-    echo "I don't know what you are, exiting shell script: $OS_SKU $SIG_IMAGE_NAME"
-    exit 1
-fi
-
 az vm run-command invoke \
     --command-id RunShellScript \
     --name $VM_NAME \
     --resource-group $RESOURCE_GROUP_NAME \
-    --scripts "az login --identity"
-
-TIMESTAMP=$(date +%s%3N)
-TRIVY_REPORT_NAME="trivy-report-${BUILD_ID}-${TIMESTAMP}.json"
-az vm run-command invoke \
-    --command-id RunShellScript \
-    --name $VM_NAME \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --scripts "az storage blob upload --file /opt/azure/containers/trivy-report.json \
-    --container-name ${SIG_CONTAINER_NAME} \
-    --name ${TRIVY_REPORT_NAME} \
-    --account-name ${STORAGE_ACCOUNT_NAME} \
-    --auth-mode login"
-
-TRIVY_TABLE_NAME="trivy-table-${BUILD_ID}-${TIMESTAMP}.txt"
-az vm run-command invoke \
-    --command-id RunShellScript \
-    --name $VM_NAME \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --scripts "az storage blob upload --file /opt/azure/containers/trivy-images-table.txt \
-    --container-name ${SIG_CONTAINER_NAME} \
-    --name ${TRIVY_TABLE_NAME} \
-    --account-name ${STORAGE_ACCOUNT_NAME} \
-    --auth-mode login"
-
+    --scripts @$SCRIPT_PATH \
+    --parameters "OS_SKU=${OS_SKU}" "OS_VERSION=${OS_VERSION}" "TEST_VM_ADMIN_USERNAME=${TEST_VM_ADMIN_USERNAME}" "ARCHITECTURE=${ARCHITECTURE}"
 
 az storage blob download --container-name ${SIG_CONTAINER_NAME} --name  ${TRIVY_REPORT_NAME} --file trivy-report.json --account-name ${STORAGE_ACCOUNT_NAME} --auth-mode login
 az storage blob download --container-name ${SIG_CONTAINER_NAME} --name  ${TRIVY_TABLE_NAME} --file  trivy-images-table.txt --account-name ${STORAGE_ACCOUNT_NAME} --auth-mode login
