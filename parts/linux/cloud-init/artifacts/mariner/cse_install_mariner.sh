d1e4a6e563bfe15489c84fd271c9a4699c109cd1
Aadhar Agarwal <aadagarwal@microsoft.com>
2024-05-29 21:42:36 -0700

    Install nvidia packages

--- parts/linux/cloud-init/artifacts/mariner/cse_install_mariner.sh
+++ parts/linux/cloud-init/artifacts/mariner/cse_install_mariner.sh
@@ -72,6 +72,10 @@ installNvidiaFabricManager() {
 installNvidiaContainerRuntime() {
     MARINER_NVIDIA_CONTAINER_RUNTIME_VERSION="3.13.0"
     MARINER_NVIDIA_CONTAINER_TOOLKIT_VERSION="1.13.5"
+
+    if [[ $OS_VERSION == "3.0" ]]; then
+      MARINER_NVIDIA_CONTAINER_TOOLKIT_VERSION="1.14.4"
+    fi
     
     for nvidia_package in nvidia-container-runtime-${MARINER_NVIDIA_CONTAINER_RUNTIME_VERSION} nvidia-container-toolkit-${MARINER_NVIDIA_CONTAINER_TOOLKIT_VERSION} nvidia-container-toolkit-base-${MARINER_NVIDIA_CONTAINER_TOOLKIT_VERSION} libnvidia-container-tools-${MARINER_NVIDIA_CONTAINER_TOOLKIT_VERSION} libnvidia-container1-${MARINER_NVIDIA_CONTAINER_TOOLKIT_VERSION}; do
       if ! dnf_install 30 1 600 $nvidia_package; then
