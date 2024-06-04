85b29488606ac3253a2fd09d4eecd536a87c5d6d
Cameron Meissner <cameissner@microsoft.com>
2024-06-04 12:42:13 -0700

    chore: enhance retry logs

--- vhdbuilder/scripts/automate_helpers.sh
+++ vhdbuilder/scripts/automate_helpers.sh
@@ -5,10 +5,12 @@ retrycmd_if_failure() {
     retries=$1; wait_sleep=$2; shift && shift
     for i in $(seq 1 $retries); do
         "${@}" && break || \
+        echo "Failed to execute command \"$@\""
         if [ $i -eq $retries ]; then
-            echo Executed \"$@\" $i times;
+            echo "ERROR: Exhausted all retries (${i}/{$retries}), forcing a failure..."
             return 1
         else
+            echo "$(($retries - $i)) retries remaining"
             sleep $wait_sleep
         fi
     done
