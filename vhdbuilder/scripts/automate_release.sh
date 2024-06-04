f10fe671eff76ca4169888e692868c0194e3d840
Cameron Meissner <cameissner@microsoft.com>
2024-06-04 09:31:09 -0700

    chore: address comments

--- vhdbuilder/scripts/automate_release.sh
+++ vhdbuilder/scripts/automate_release.sh
@@ -10,14 +10,9 @@ VHD_BUILD_ID="${VHD_BUILD_ID:-""}"
 trigger_ev2_artifacts() {
     echo "creating EV2 artifacts for VHD build with ID: $VHD_BUILD_ID"
 
-    # Run the pipeline and fetch the run ID to poll for success later
     EV2_BUILD_ID=$(az pipelines run --id $EV2_ARTIFACT_PIPELINE_ID --variables "VHD_PIPELINE_RUN_ID=$VHD_BUILD_ID" | jq -r '.id')
-    if [ $? -ne 0 ]; then
-        echo "failed to trigger EV2 artifact build"
-        return 1
-    fi
-
     STATUS="$(az pipelines runs show --id $EV2_BUILD_ID | jq -r '.status')"
+
     while [ "${STATUS,,}" == "notstarted" ] || [ "${STATUS,,}" == "inprogress" ]; do
         echo "EV2 artifact build $EV2_BUILD_ID is still in-progress..."
         sleep 60
@@ -39,11 +34,8 @@ create_release() {
     RELEASE_ID=$(az pipelines release create --detect true \
         --project CloudNativeCompute \
         --definition-id $SIG_RELEASE_PIPELINE_ID \
-        --artifact-metadata-list "ev2_artifacts=$EV2_BUILD_ID" | jq -r '.releaseId')
-    if [ $? -ne 0 ]; then
-        echo "failed to create release for VHD with build ID: $VHD_BUILD_ID using artifacts from build: $EV2_BUILD_ID"
-        return 1
-    fi
+        --artifact-metadata-list "ev2_artifacts=$EV2_BUILD_ID" | jq -r '.id')
+        
     echo "SIG release successfully created for VHD build with ID: $VHD_BUILD_ID"
     echo "release URL: https://msazure.visualstudio.com/CloudNativeCompute/_releaseProgress?_a=release-pipeline-progress&releaseId=$RELEASE_ID"
 }
