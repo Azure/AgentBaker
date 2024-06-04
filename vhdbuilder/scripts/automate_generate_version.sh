a308b2dad17340722f71c6088ead46995a2f5034
Cameron Meissner <cameissner@microsoft.com>
2024-06-04 14:41:53 -0700

    fix: image version setting

--- vhdbuilder/scripts/automate_generate_version.sh
+++ vhdbuilder/scripts/automate_generate_version.sh
@@ -2,7 +2,6 @@
 set -euxo pipefail
 
 VHD_BUILD_ID="${VHD_BUILD_ID:-""}"
-IMAGE_VERSION_OVERRIDE="${IMAGE_VERSION:-""}"
 
 get_image_version_from_publishing_info() {
     for artifact in $(az pipelines runs artifact list --run-id $VHD_BUILD_ID | jq -r '.[].name'); do # Retrieve what artifacts were published
@@ -10,21 +9,18 @@ get_image_version_from_publishing_info() {
             # just take the image version from the first publishing-info we find (since they should all be the same)
             # TODO(cameissner): add image version validation to separate validation template
             az pipelines runs artifact download --artifact-name $artifact --path $(pwd) --run-id $VHD_BUILD_ID
-            IMAGE_VERSION=$(jq -r .image_version < vhd-publishing-info.json)
+            GENERATED_IMAGE_VERSION=$(jq -r .image_version < vhd-publishing-info.json)
             return 0
         fi
     done
+    echo "unable to find image version from publishing-info artifacts downloaded from VHD build with ID: $VHD_BUILD_ID"
+    return 1
 }
-
-if [ -n "$IMAGE_VERSION_OVERRIDE" ]; then
-    echo "IMAGE_VERSION already has value: $IMAGE_VERSION_OVERRIDE"
-    exit 0
-fi
 
 if [ -z "$VHD_BUILD_ID" ]; then
     echo "VHD_BUILD_ID must be set in order to set the image version"
     exit 1
 fi
 
-get_image_version_from_publishing_info
-export IMAGE_VERSION="$IMAGE_VERSION"
+get_image_version_from_publishing_info || exit $?
+export GENERATED_IMAGE_VERSION="$GENERATED_IMAGE_VERSION"
