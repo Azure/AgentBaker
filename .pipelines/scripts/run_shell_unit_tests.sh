b9e5ea3449dc8b02d0f64b8335c9a404db4330de
Tim Wright <wrightt@microsoft.com>
2024-06-06 17:47:03 +1200

    fix to run tests

--- .pipelines/scripts/run_shell_unit_tests.sh
+++ .pipelines/scripts/run_shell_unit_tests.sh
@@ -3,8 +3,8 @@ set -uo pipefail
 
 sudo sudo apt-get install -y bats
 
-filesToCheck=$(find bash_tests -type f -name "*.test.bats" )
+filesToCheck=$(find bash_tests -type f -name "*.test.bats.sh" )
 
 echo $filesToCheck
 
-bats "${filesToCheck}"
+bats -p ${filesToCheck}
