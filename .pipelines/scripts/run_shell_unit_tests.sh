271c3d87395581fe8d950f3d743132342190746a
Tim Wright <wrightt@microsoft.com>
2024-06-06 18:09:48 +1200

    dir name

--- .pipelines/scripts/run_shell_unit_tests.sh
+++ .pipelines/scripts/run_shell_unit_tests.sh
@@ -7,5 +7,5 @@ filesToCheck=$(find bash_tests -type f -name "*.test.bats.sh" )
 
 echo Running bats...
 
-mkdir -p test_results
-bats -F junit ${filesToCheck} > test_results/bats_shell.xml
+mkdir -p test-results
+bats -F junit ${filesToCheck} > test-results/bats_shell.xml
