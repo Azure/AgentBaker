6719cddaebab8641e2a4c01942044e8a42b62d89
Alison Burgess <alburgess@microsoft.com>
2024-06-13 15:18:37 -0700

    delete the role asignment

--- vhdbuilder/packer/vhd-scanning.sh
+++ vhdbuilder/packer/vhd-scanning.sh
@@ -31,6 +31,7 @@ function cleanup() {
 
     if [ -n "${VM_PRINCIPLE_ID}" ]; then
         az role assignment delete --assignee $VM_PRINCIPLE_ID --role "Storage Blob Data Contributor" --scope "/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${AZURE_RESOURCE_GROUP_NAME}/providers/Microsoft.Storage/storageAccounts/${SCANNING_STORAGE_ACCOUNT}/blobServices/default/containers/vhd-scans"
+        echo "Role assignment deleted"
     fi 
 }
 trap cleanup EXIT
