2ed374a41fb414c7b83432de5b3be69ebebec10b
Devin Wong <devinwon@microsoft.com>
2024-06-18 12:36:44 -0700

    Revert "refactor: move containerd and runc from manifest.json to components.json. Merge DownloadFiles to Packages. (#4463)"
    This reverts commit 2111130701e0a421637ae892c809189054600bf2.

--- pkg/agent/vhd/cache/types.go
+++ pkg/agent/vhd/cache/types.go
@@ -1,14 +1,16 @@
 package cache
 
-// OnVHD represents the cached components on the VHD.
+// CachedOnVHD represents the cached components on the VHD.
 type OnVHD struct {
 	FromManifest                 *Manifest                 `json:"cachedFromManifest"`
 	FromComponentContainerImages map[string]ContainerImage `json:"cachedFromComponentContainerImages"`
-	FromComponentPackages        map[string]Package        `json:"cachedFromComponentPackages"`
+	FromComponentDownloadedFiles map[string]DownloadFile   `json:"cachedFromComponentDownloadedFiles"`
 }
 
 // Manifest represents the manifest.json file.
 type Manifest struct {
+	Containerd             Dependency `json:"containerd"`
+	Runc                   Dependency `json:"runc"`
 	NvidiaContainerRuntime Dependency `json:"nvidia-container-runtime"`
 	NvidiaDrivers          Dependency `json:"nvidia-drivers"`
 	Kubernetes             Dependency `json:"kubernetes"`
@@ -22,10 +24,15 @@ type Dependency struct {
 	Edge      string            `json:"edge"`
 }
 
+// Versions of components on manifest.json.
+type Versions struct {
+	Versions []string `json:"versions"`
+}
+
 // Components represents the components.json file.
 type Components struct {
 	ContainerImages []ContainerImage `json:"containerImages"`
-	Packages        []Package        `json:"Packages"`
+	DownloadFiles   []DownloadFile   `json:"downloadFiles"`
 }
 
 // ContainerImage represents fields that occur on components.json.
@@ -42,43 +49,10 @@ type PrefetchOptimization struct {
 	Binaries []string `json:"binaries"`
 }
 
-// Release represents Release struct that occur on components.json.
-type Release struct {
-	Versions    []string `json:"versions"`
-	DownloadURL string   `json:"downloadURL"`
-}
-
-// Package represents Package metadata struct that occur on components.json.
-type Package struct {
-	Name             string       `json:"name"`
-	DownloadLocation string       `json:"downloadLocation"`
-	DownloadURIs     DownloadURIs `json:"downloadURIs"`
-}
-
-// DownloadURIs represents DownloadURIs struct that occur on components.json.
-type DownloadURIs struct {
-	Ubuntu  *Ubuntu    `json:"ubuntu,omitempty"`
-	Mariner *Mariner   `json:"mariner,omitempty"`
-	Default *DefaultOS `json:"default,omitempty"`
-}
-
-// DefaultOS represents DefaultOS struct that occur on components.json.
-type DefaultOS struct {
-	Current *Release `json:"current"`
-	// unspecified OS (defaultOS) is not expected to have specific release versions.
-	// so we only have `Current`` now.
-}
-
-// Mariner represents Mariner struct that occur on components.json.
-type Mariner struct {
-	Current *Release `json:"current"`
-	// additional release versions can be added here, if any
-}
-
-// Ubuntu represents Ubuntu struct that occur on components.json.
-type Ubuntu struct {
-	R1804   *Release `json:"r1804,omitempty"`
-	R2004   *Release `json:"r2004,omitempty"`
-	R2204   *Release `json:"r2204,omitempty"`
-	Current *Release `json:"current,omitempty"`
+// DownloadFile represents DownloadFile fields that occur on components.json.
+type DownloadFile struct {
+	FileName         string   `json:"fileName"`
+	DownloadLocation string   `json:"downloadLocation"`
+	DownloadURL      string   `json:"downloadURL"`
+	Versions         []string `json:"versions"`
 }
