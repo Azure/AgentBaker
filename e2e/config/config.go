0bb8bdf549a577e10fa327e7f95a4c2a5760e5e8
r2k1 <yokree@gmail.com>
2024-06-23 11:12:22 +1200

    rename envDefault => getEnvWithDefaultIfEmpty

--- e2e/config/config.go
+++ e2e/config/config.go
@@ -8,21 +8,21 @@ import (
 )
 
 var (
-	BuildID            = envDefault(os.Getenv("BUILD_ID"), "local")
-	SubscriptionID     = envDefault("SUBSCRIPTION_ID", "8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8")
-	Location           = envDefault("LOCATION", "eastus")
+	BuildID            = getEnvWithDefaultIfEmpty(os.Getenv("BUILD_ID"), "local")
+	SubscriptionID     = getEnvWithDefaultIfEmpty("SUBSCRIPTION_ID", "8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8")
+	Location           = getEnvWithDefaultIfEmpty("LOCATION", "eastus")
 	ResourceGroupName  = "abe2e-" + Location
 	ScenariosToRun     = envmap("SCENARIOS_TO_RUN")
 	ScenariosToExclude = envmap("SCENARIOS_TO_EXCLUDE")
 	KeepVMSS           = strings.EqualFold(os.Getenv("KEEP_VMSS"), "true")
 	Azure              = MustNewAzureClient(SubscriptionID)
 	// ADO tags every SIG image version with `branch` tag. By specifying `branch=refs/heads/master` we load latest image version from the master branch.
-	SIGVersionTagName             = envDefault("SIG_VERSION_TAG_NAME", "branch")
-	SIGVersionTagValue            = envDefault("SIG_VERSION_TAG_VALUE", "refs/heads/master")
+	SIGVersionTagName             = getEnvWithDefaultIfEmpty("SIG_VERSION_TAG_NAME", "branch")
+	SIGVersionTagValue            = getEnvWithDefaultIfEmpty("SIG_VERSION_TAG_VALUE", "refs/heads/master")
 	IgnoreScenariosWithMissingVHD = strings.EqualFold(os.Getenv("IGNORE_SCENARIOS_WITH_MISSING_VHD"), "true")
 )
 
-func envDefault(env string, defaultValue string) string {
+func getEnvWithDefaultIfEmpty(env string, defaultValue string) string {
 	val, ok := os.LookupEnv(env)
 	if !ok {
 		return defaultValue
