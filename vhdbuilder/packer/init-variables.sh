2b732f4a2955487f43d6fba062679c6b8f545874
Christian Muirhead <christian.muirhead@microsoft.com>
2024-06-25 14:05:28 +1200

    Remove location - it might be confusing things?

--- vhdbuilder/packer/init-variables.sh
+++ vhdbuilder/packer/init-variables.sh
@@ -351,7 +351,6 @@ cat <<EOF > vhdbuilder/packer/settings.json
   "subscription_id":  "${SUBSCRIPTION_ID}",
   "resource_group_name": "${AZURE_RESOURCE_GROUP_NAME}",
   "build_resource_group_name": "${AZURE_RESOURCE_GROUP_NAME}",
-  "location": "${AZURE_LOCATION}",
   "storage_account_name": "${STORAGE_ACCOUNT_NAME}",
   "vm_size": "${AZURE_VM_SIZE}",
   "create_time": "${CREATE_TIME}",
