0d270b0fe2f8c1f8fa1381fcb0a7433688118158
Christian Muirhead <christian.muirhead@microsoft.com>
2024-06-25 14:50:26 +1200

    Setting temp rg name to try to cause an error

--- vhdbuilder/packer/init-variables.sh
+++ vhdbuilder/packer/init-variables.sh
@@ -351,6 +351,8 @@ cat <<EOF > vhdbuilder/packer/settings.json
   "subscription_id":  "${SUBSCRIPTION_ID}",
   "resource_group_name": "${AZURE_RESOURCE_GROUP_NAME}",
   "build_resource_group_name": "${AZURE_RESOURCE_GROUP_NAME}",
+  "temp_resource_group_name": "some-temp-rg",
+  "location": "${AZURE_LOCATION}",
   "storage_account_name": "${STORAGE_ACCOUNT_NAME}",
   "vm_size": "${AZURE_VM_SIZE}",
   "create_time": "${CREATE_TIME}",
