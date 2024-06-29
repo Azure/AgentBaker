f68647fdbd588759c949cb777a75b871cd17dde9
Zachary Bailey <zbailey@microsoft.com>
2024-06-29 11:54:05 -0700

    fix: remove whitespace bug

--- vhdbuilder/packer/init-variables.sh
+++ vhdbuilder/packer/init-variables.sh
@@ -461,8 +461,7 @@ cat <<EOF > vhdbuilder/packer/settings.json
   "vnet_resource_group_name": "${VNET_RG_NAME}",
   "msi_resource_strings": "${msi_resource_strings}",
   "private_packages_url": "${private_packages_url}",
-  "aks_windows_image_version": "${AKS_WINDOWS_IMAGE_VERSION}",
-	"shallow_replication_toggle": "${SHALLOW_REPLICATION_TOGGLE}
+  "aks_windows_image_version": "${AKS_WINDOWS_IMAGE_VERSION}"
 }
 EOF
 
