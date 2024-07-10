45c6723cf2d5f9f960b65c3894a9c9741b13f913
Alison Burgess <alburgess@microsoft.com>
2024-07-10 16:55:54 -0700

    what oras will look like

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -432,10 +432,12 @@ CREDENTIAL_PROVIDER_VERSIONS="
 "
 # TODO (alburgess) oras for credential provider
 for CREDENTIAL_PROVIDER_VERSION in $CREDENTIAL_PROVIDER_VERSIONS; do
-    # CREDENTIAL_PROVIDER_DOWNLOAD_URL="https://acs-mirror.azureedge.net/cloud-provider-azure/v${CREDENTIAL_PROVIDER_VERSION}/binaries/azure-acr-credential-provider-linux-${CPU_ARCH}-v${CREDENTIAL_PROVIDER_VERSION}.tar.gz"
-    # downloadCredentalProvider $CREDENTIAL_PROVIDER_DOWNLOAD_URL
-    CREDENTIAL_PROVIDER_DOWNLOAD_URL="mcr.microsoft.com/oss/binaries/kubernetes/azure-acr-credential-provider:v${CREDENTIAL_PROVIDER_VERSION}-linux-${CPU_ARCH}"
-    downloadCredentalProviderWithOras $CREDENTIAL_PROVIDER_DOWNLOAD_URL
+    # if BLOCK_OUTBOUND_NETWORK 
+    # CREDENTIAL_PROVIDER_DOWNLOAD_URL="mcr.microsoft.com/oss/binaries/kubernetes/azure-acr-credential-provider:v${CREDENTIAL_PROVIDER_VERSION}-linux-${CPU_ARCH}"
+    # downloadCredentalProviderWithOras $CREDENTIAL_PROVIDER_DOWNLOAD_URL
+    # else 
+    CREDENTIAL_PROVIDER_DOWNLOAD_URL="https://acs-mirror.azureedge.net/cloud-provider-azure/v${CREDENTIAL_PROVIDER_VERSION}/binaries/azure-acr-credential-provider-linux-${CPU_ARCH}-v${CREDENTIAL_PROVIDER_VERSION}.tar.gz"
+    downloadCredentalProvider $CREDENTIAL_PROVIDER_DOWNLOAD_URL
     echo "  - Kubelet credential provider version ${CREDENTIAL_PROVIDER_VERSION}" >> ${VHD_LOGS_FILEPATH}
 done
 
