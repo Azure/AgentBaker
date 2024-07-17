6278f22554da9a2df8c23a56f42f9a40f7f08407
Christian Muirhead <christian.muirhead@microsoft.com>
2024-07-17 19:58:02 +1200

    Use existing identity assigned to the scanning VM
    Rather than assigning a role, which requires the more sensitive Role
    Based Access Control Administrator role.

--- vhdbuilder/packer/vhd-scanning.sh
+++ vhdbuilder/packer/vhd-scanning.sh
@@ -20,7 +20,14 @@ SUBNET_NAME="scanning"
 if [ -z "$PACKER_BUILD_LOCATION" ]; then
     echo "PACKER_BUILD_LOCATION must be set to run VHD scanning"
     exit 1
-fi 
+fi
+
+# We assign this identity to the scanning VM so that it has permission
+# to push the trivy output to the storage blob.
+if [ -z "$SCANNING_MSI_RESOURCE_ID" ]; then
+    echo "SCANNING_MSI_RESOURCE_ID must be set to run VHD scanning"
+    exit 1
+fi
 
 # Use the domain name from the classic blob URL to get the storage account name.
 # If the CLASSIC_BLOB var is not set create a new var called BLOB_STORAGE_NAME in the pipeline.
@@ -53,11 +60,6 @@ fi
 function cleanup() {
     echo "Deleting resource group ${RESOURCE_GROUP_NAME}"
     az group delete --name $RESOURCE_GROUP_NAME --yes --no-wait
-
-    if [ -n "${VM_PRINCIPLE_ID}" ]; then
-        az role assignment delete --assignee $VM_PRINCIPLE_ID --role "Storage Blob Data Contributor" --scope "/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${AZURE_RESOURCE_GROUP_NAME}"
-        echo "Role assignment deleted."
-    fi
 }
 trap cleanup EXIT
 
@@ -71,6 +73,8 @@ if [[ "${OS_TYPE}" == "Linux" && "${ENABLE_TRUSTED_LAUNCH}" == "True" ]]; then
     VM_OPTIONS+=" --security-type TrustedLaunch --enable-secure-boot true --enable-vtpm true"
 fi
 
+SCANNING_ID=$(az identity show --ids "${SCANNING_MSI_RESOURCE_ID}" --query id --output tsv)
+
 az vm create --resource-group $RESOURCE_GROUP_NAME \
     --name $VM_NAME \
     --image $VHD_IMAGE \
@@ -80,10 +84,7 @@ az vm create --resource-group $RESOURCE_GROUP_NAME \
     --admin-password $TEST_VM_ADMIN_PASSWORD \
     --os-disk-size-gb 50 \
     ${VM_OPTIONS} \
-    --assign-identity "[system]"
-
-VM_PRINCIPLE_ID=$(az vm identity show --name $VM_NAME --resource-group $RESOURCE_GROUP_NAME --query principalId --output tsv)
-az role assignment create --assignee $VM_PRINCIPLE_ID --role "Storage Blob Data Contributor" --scope "/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${AZURE_RESOURCE_GROUP_NAME}"
+    --assign-identity "${SCANNING_ID}"
 
 FULL_PATH=$(realpath $0)
 CDIR=$(dirname $FULL_PATH)
