e344e64163018da0797bd020af9b94c618c022a7
Zachary Bailey <zbailey@microsoft.com>
2024-07-18 12:23:08 -0700

    test: test azcopy

--- vhdbuilder/packer/vhd-scanning.sh
+++ vhdbuilder/packer/vhd-scanning.sh
@@ -57,7 +57,7 @@ function cleanup() {
 }
 trap cleanup EXIT
 
-VM_OPTIONS="--size Standard_DS1_v2"
+VM_OPTIONS="--size Standard_D4ds_v5"
 if [[ "${ARCHITECTURE,,}" == "arm64" ]]; then
     VM_OPTIONS="--size Standard_D2pds_v5"
 elif [[ "${FEATURE_FLAGS,,}" == "kata" ]]; then
