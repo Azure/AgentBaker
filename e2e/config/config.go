a1b86b34253ef18fa134e3e3ed253f9e0b1e2b96
Lily Pan <lilypan@microsoft.com>
2024-07-22 13:51:45 -0700

    fix keep vmss

--- e2e/config/config.go
+++ e2e/config/config.go
@@ -9,12 +9,12 @@ import (
 var (
 	Location                      = lookupEnvWithDefaultString("LOCATION", "westus3")
 	SubscriptionID                = lookupEnvWithDefaultString("SUBSCRIPTION_ID", "8ecadfc9-d1a3-4ea4-b844-0d9f87e4d7c8")
-	CustomConfig                  = NewCustomConfig(Location, SubscriptionID)
+	CustomConfig                  = NewCustomConfig("", "")
 	AirgapNSGName                 = "abe2e-airgap-securityGroup"
 	BuildID                       = lookupEnvWithDefaultString(os.Getenv("BUILD_ID"), "local")
 	DefaultSubnetName             = "aks-subnet"
 	IgnoreScenariosWithMissingVHD = lookupEnvWithDefaultBool("IGNORE_SCENARIOS_WITH_MISSING_VHD", false)
-	KeepVMSS                      = lookupEnvWithDefaultBool("KEEP_VMSS", false)
+	KeepVMSS                      = lookupEnvWithDefaultBool("KEEP_VMSS", true)
 	SIGVersionTagName             = lookupEnvWithDefaultString("SIG_VERSION_TAG_NAME", "branch") // ADO tags every SIG image version with `branch` tag. By specifying `branch=refs/heads/master` we load latest image version from the master branch.
 	SIGVersionTagValue            = lookupEnvWithDefaultString("SIG_VERSION_TAG_VALUE", "refs/heads/master")
 	SkipTestsWithSKUCapacityIssue = lookupEnvWithDefaultBool("SKIP_TESTS_WITH_SKU_CAPACITY_ISSUE", false)
@@ -33,8 +33,15 @@ type Config struct {
 
 func NewCustomConfig(location string, subscriptionID string) *Config {
 	config := &Config{
-		Location:       location,
-		SubscriptionID: subscriptionID,
+		Location:       Location,
+		SubscriptionID: SubscriptionID,
+	}
+	if location != "" {
+		config.Location = location
+	}
+	if subscriptionID != "" {
+		config.SubscriptionID = subscriptionID
+
 	}
 	config.ResourceGroupName = "abe2e-" + config.Location
 	config.Azure = MustNewAzureClient(config.SubscriptionID)
