9dc051db636b05f6cc1bedfbcc6510938c24c25e
Zachary Bailey <zbailey@microsoft.com>
2024-07-31 13:18:59 -0700

    test: add $ sign

--- retry-function.mk
+++ retry-function.mk
@@ -1,7 +1,7 @@
 define retrycmd_if_failure
     retries=$(1); wait_sleep=$(2); timeout=$(3); cmd=$(4); target=$$(basename $$(echo $(4))); \
     echo "Running $$cmd with $$retries retries, target is $$target"; \
-    for i in $(seq 1 $$retries); do \
+    for i in $$(seq 1 $$retries); do \
         timeout $$timeout $$cmd && break || \
         if [ $$i -eq $$retries ]; then \
             echo "$$target failed $$i times"; \
