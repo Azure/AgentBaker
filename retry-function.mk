a76228935e598f968bb095594d057ef6d8b26199
Zachary Bailey <zbailey@microsoft.com>
2024-07-31 13:34:43 -0700

    perf: add make function to enable parallel execution

---
+++ retry-function.mk
@@ -0,0 +1,14 @@
+define retrycmd_if_failure
+    retries=$(1); wait_sleep=$(2); timeout=$(3); cmd=$(4); target=$$(basename $$(echo $(4))); \
+    echo "Running $$cmd with $$retries retries, target is $$target"; \
+    for i in $$(seq 1 $$retries); do \
+        timeout $$timeout $$cmd && break || \
+        if [ $$i -eq $$retries ]; then \
+            echo "$$target failed $$i times"; \
+            exit 1; \
+        else \
+            sleep $$wait_sleep; \
+        fi \
+    done; \
+    echo "Executed $$target $$i times"
+endef
