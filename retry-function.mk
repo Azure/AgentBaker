a812679ad13785804769f47fcb94c4ce14350e11
Zachary Bailey <zbailey@microsoft.com>
2024-07-31 09:02:04 -0700

    fix: echo statement

--- retry-function.mk
+++ retry-function.mk
@@ -1,8 +1,8 @@
 define retrycmd_if_failure
 	retries=$1; wait_sleep=$2; cmd=$3; \
-	echo "Retries: $(retries)"; \
-	echo "Wait Sleep: $(wait_sleep)"; \
-	echo "Command: $(cmd)"; \
+	echo Retries: $(retries); \
+	echo Wait Sleep: $(wait_sleep); \
+	echo Command: $(cmd); \
   	for i in $$(seq 1 $$retries); do \
     	$(cmd) && break || \
       	if [ $$i -eq $$retries ]; then \
