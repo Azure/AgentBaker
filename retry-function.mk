7d74d6a4680b376228540c82ddd40df8be3e447b
Zachary Bailey <zbailey@microsoft.com>
2024-07-31 08:30:17 -0700

    test: change retry function

--- retry-function.mk
+++ retry-function.mk
@@ -1,13 +1,16 @@
 define retrycmd_if_failure
-	retries=$1; wait_sleep=$2; timeout=$3; shift && shift && shift; \
+	retries=${1}; wait_sleep=${2}; cmd=${3}; \
+	echo "Retries: $$retries"; \
+	echo "Wait Sleep: $$wait_sleep"; \
+	echo "Command: $$cmd"; \
   	for i in $$(seq 1 $$retries); do \
-    	timeout $$timeout "$$@" && break || \
+    	$$cmd && break || \
       	if [ $$i -eq $$retries ]; then \
-        	echo Executed "$$@" $$i times; \
-        	return 1; \
+        	echo Executed $$(basename $$cmd) $$i times; \
+        	exit 1; \
 				else \
 					sleep $$wait_sleep; \
 				fi; \
 		done; \
-	echo Executed "$$@" $$i times;
+	echo Executed $$(basename $$cmd) $$i times;
 endef
