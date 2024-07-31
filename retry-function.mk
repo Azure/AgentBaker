bc943bdd0b714f8310c0d5c6af9d78a345343b58
Zachary Bailey <zbailey@microsoft.com>
2024-07-31 12:48:25 -0700

    fix: add ;

--- retry-function.mk
+++ retry-function.mk
@@ -2,13 +2,13 @@ define retrycmd_if_failure
     retries=$(1); wait_sleep=$(2); timeout=$(3); cmd=$(4); target=$$(basename $$(echo $$cmd)); \
     echo "Running $$cmd with $$retries retries, target is $$target"; \
     for i in $(seq 1 $$retries); do \
-        timeout $$timeout $$cmd && break || \
+        timeout $$timeout $$cmd && break; || \
         if [ $$i -eq $$retries ]; then \
             echo Executed $$target $$i times; \
-            exit 1 \
+            exit 1; \
         else \
-            sleep $$wait_sleep \
+            sleep $$wait_sleep; \
         fi \
-    done \
-    echo Executed $$target $$i times; \
+    done; \
+    echo Executed $$target $$i times
 endef
