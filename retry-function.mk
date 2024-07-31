721357c0887f6d4134db50c4dc13941e28887a1c
Zachary Bailey <zbailey@microsoft.com>
2024-07-31 12:38:46 -0700

    test: end of file error

--- retry-function.mk
+++ retry-function.mk
@@ -1,14 +1,14 @@
 define retrycmd_if_failure
     retries=$(1); wait_sleep=$(2); timeout=$(3); cmd=$(4); target=$$(basename $$(echo $$cmd)); \
     echo "Running $$cmd with $$retries retries, target is $$target"; \
-    for i in $(seq 1 $$retries); do
+    for i in $(seq 1 $$retries); do \
         timeout $$timeout $$cmd && break || \
-        if [ $$i -eq $$retries ]; then
-            echo Executed $$target $$i times;
-            exit 1
-        else
-            sleep $$wait_sleep
-        fi
-    done
-    echo Executed $$target $$i times;
+        if [ $$i -eq $$retries ]; then \
+            echo Executed $$target $$i times; \
+            exit 1 \
+        else \
+            sleep $$wait_sleep \
+        fi \
+    done \
+    echo Executed $$target $$i times; \
 endef
