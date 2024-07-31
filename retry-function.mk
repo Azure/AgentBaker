468ad5e1d022feb9ee66db62148f4e826588af5f
Zachary Bailey <zbailey@microsoft.com>
2024-07-31 10:30:38 -0700

    fix: var syntax

--- retry-function.mk
+++ retry-function.mk
@@ -2,7 +2,7 @@ define retrycmd
     @success=0; \
     cmd=$(1); \
     retries=$(2); \
-		target=$$(basename $$(echo $$cmd)); \
+		target=$$(basename $$(echo $(1))); \
     for i in $$(seq 1 $$retries); do \
         $$cmd && { success=1; break; } || echo "$$target failed. Retrying..."; \
         sleep 3; \
