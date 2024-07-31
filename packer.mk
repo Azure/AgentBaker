8de20fb4bcb19556f2df55b32f02544715bb9d41
Zachary Bailey <zbailey@microsoft.com>
2024-07-31 13:33:51 -0700

    perf: modify make file to enable parallel execution and error handling

--- packer.mk
+++ packer.mk
@@ -1,5 +1,7 @@
 SHELL=/bin/bash -o pipefail
 
+include retry-function.mk
+
 build-packer: build-nbcparser-all
 ifeq (${MODE},linuxVhdMode)
 	@echo "${MODE}: Generating prefetch scripts"
@@ -81,7 +83,7 @@ run-packer-windows: az-login
 	@packer init ./vhdbuilder/packer/packer-plugin.pkr.hcl && packer version && ($(MAKE) -f packer.mk init-packer | tee packer-output) && ($(MAKE) -f packer.mk build-packer-windows | tee -a packer-output)
 
 cleanup: az-login
-	@./vhdbuilder/packer/cleanup.sh
+	$(call retrycmd_if_failure,2,3,900,./vhdbuilder/packer/cleanup.sh)
 
 backfill-cleanup: az-login
 	@chmod +x ./vhdbuilder/packer/backfill-cleanup.sh
@@ -94,10 +96,13 @@ convert-sig-to-classic-storage-account-blob: az-login
 	@./vhdbuilder/packer/convert-sig-to-classic-storage-account-blob.sh
 
 test-building-vhd: az-login
-	@./vhdbuilder/packer/test/run-test.sh
+	$(call retrycmd_if_failure,2,3,1200,./vhdbuilder/packer/test/run-test.sh)
 
 scanning-vhd: az-login
-	@./vhdbuilder/packer/vhd-scanning.sh
+	$(call retrycmd_if_failure,2,3,1200,./vhdbuilder/packer/vhd-scanning.sh)
+
+test-scan-and-cleanup:
+	@$(MAKE) -f packer.mk cleanup test-building-vhd scanning-vhd -j3 --output-sync=target
 
 build-nbcparser-all:
 	@$(MAKE) -f packer.mk build-nbcparser-binary ARCH=amd64
