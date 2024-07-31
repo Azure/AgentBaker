cd1b759a8ff7712b224d7d944003c5c9d27caa5f
r2k1 <yokree@gmail.com>
2024-07-31 19:00:17 +1200

    fix curl validation

--- e2e/validation.go
+++ e2e/validation.go
@@ -36,10 +36,12 @@ func validateWasm(ctx context.Context, t *testing.T, kube *Kubeclient, nodeName 
 
 	execResult, err := pollExecOnPod(ctx, t, kube, defaultNamespace, debugPodName, getWasmCurlCommand(fmt.Sprintf("http://%s/hello", spinPodIP)))
 	if err != nil {
-		if execResult != nil {
-			execResult.dumpAll(t)
-		}
-		return fmt.Errorf("on node %sunable to execute wasm validation command: %w", nodeName, err)
+		return fmt.Errorf("unable to execute wasm validation command, node %s, exit code %s, exec: %w", nodeName, err)
+	}
+
+	if execResult.exitCode != "0" {
+		execResult.dumpAll(t)
+		return fmt.Errorf("wasm validation failed, node %s, exit code %s", nodeName, execResult.exitCode)
 	}
 
 	return nil
