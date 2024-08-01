712101f13718747223ce94edd8a1848db1c7d65e
Zachary Bailey <zbailey@microsoft.com>
2024-07-31 18:10:41 -0700

    perf: run successfull test

--- retry-function.mk
+++ retry-function.mk
@@ -1,7 +1,7 @@
 define retrycmd_if_failure
-    retries=$(1); wait_sleep=$(2); timeout=$(3); cmd=$(4); target=$$(basename $$(echo $(4))); \
-    echo "Running $$cmd with $$retries retries, target is $$target"; \
-    for i in $$(seq 1 $$retries); do \
+    @retries=$(1); wait_sleep=$(2); timeout=$(3); cmd=$(4); target=$$(basename $$(echo $(4))); \
+    @echo "Running $$cmd with $$retries retries, target is $$target"; \
+    @for i in $$(seq 1 $$retries); do \
         timeout $$timeout $$cmd && break || \
         if [ $$i -eq $$retries ]; then \
             echo "$$target failed $$i times"; \
@@ -10,5 +10,5 @@ define retrycmd_if_failure
             sleep $$wait_sleep; \
         fi \
     done; \
-    echo "Executed $$target $$i times"
+    @echo "Executed $$target $$i times"
 endef
