9f79ba4e34de19325d92172ccef2d00558c481f6
Zachary Bailey <zbailey@microsoft.com>
2024-08-05 11:51:41 -0700

    fix: handle failures

--- retry-function.mk
+++ retry-function.mk
@@ -1,14 +1,16 @@
 define retrycmd_if_failure
     retries=$(1); wait_sleep=$(2); cmd=$(3); target=$$(basename $$(echo $(3))); \
-    echo -e "\n==========================================================="; \
+    echo -e "\n======================================================="; \
     echo -e "Running $$cmd with $$retries retries"; \
     for i in $$(seq 1 $$retries); do \
         $$cmd | tee output-$${target%.*}.txt > /dev/null 2>&1 && break || \
         if [ $$i -eq $$retries ]; then \
             echo "$$target failed $$i times"; \
+            cat output-$${target%.*}.txt; \
             exit 1; \
         else \
             sleep $$wait_sleep; \
+            echo -e "\n\n\nNext Attempt:\n\n\n" | tee -a output-$${target%.*}.txt; \
         fi \
     done; \
     cat output-$${target%.*}.txt
