2cfab5cf20145382b4e8545142be50d2f68fac17
Zachary Bailey <zbailey@microsoft.com>
2024-08-05 11:33:28 -0700

    chore: formatting

--- retry-function.mk
+++ retry-function.mk
@@ -1,6 +1,6 @@
 define retrycmd_if_failure
     retries=$(1); wait_sleep=$(2); cmd=$(3); target=$$(basename $$(echo $(3))); \
-    echo -e "\n========================================================="; \
+    echo -e "\n==========================================================="; \
     echo -e "Running $$cmd with $$retries retries"; \
     for i in $$(seq 1 $$retries); do \
         $$cmd | tee output-$${target%.*}.txt > /dev/null 2>&1 && break || \
