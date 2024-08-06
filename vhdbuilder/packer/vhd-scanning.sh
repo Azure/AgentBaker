cc45b29f95e377766f52931aa12b87ddb82e258e
Zachary Bailey <zbailey@microsoft.com>
2024-08-06 08:40:58 -0700

    fix: let test and scan scripts pass

--- vhdbuilder/packer/vhd-scanning.sh
+++ vhdbuilder/packer/vhd-scanning.sh
@@ -21,7 +21,7 @@ VHD_IMAGE="$MANAGED_SIG_ID"
 
 SIG_CONTAINER_NAME="vhd-scans"
 SCAN_VM_ADMIN_USERNAME="azureuser"
-exit 1
+
 # we must create VMs in a vnet which has access to the storage account, otherwise they will not be able to access the VHD blobs
 VNET_NAME="nodesig-pool-vnet-${PACKER_BUILD_LOCATION}"
 SUBNET_NAME="scanning"
