9bb787f789dce923a35371e34518248d6d0c5502
Zachary Bailey <zbailey@microsoft.com>
2024-08-06 08:02:48 -0700

    fix: modify retry function

--- retry-function.mk
+++ retry-function.mk
@@ -1,16 +1,17 @@
 define retrycmd_if_failure
     retries=$(1); wait_sleep=$(2); cmd=$(3); target=$$(basename $$(echo $(3))); \
-    echo -e "\n=============================================="
+    echo -e "\n\n==========================================================="; \
     echo -e "Running $$cmd with $$retries retries"; \
     for i in $$(seq 1 $$retries); do \
-        $$cmd && break || \
+        $$cmd >> output-$${target%.*}.txt 2>&1 && break || \
         if [ $$i -eq $$retries ]; then \
-            echo "$$target failed $$i times"; \
+            echo -e "$$target failed $$i times\n"; \
             cat output-$${target%.*}.txt; \
             exit 1; \
         else \
             sleep $$wait_sleep; \
-            echo -e "\n\n\nNext Attempt:\n\n\n" | tee -a output-$${target%.*}.txt; \
+            echo -e "\n\n\nNext Attempt:\n\n\n" >> output-$${target%.*}.txt; \
         fi \
-    done
+    done; \
+    cat output-$${target%.*}.txt
 endef
