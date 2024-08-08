ee1aa321eb3fc2c08ef518c1a7893cfe70c5e551
Zachary Bailey <zbailey@microsoft.com>
2024-08-08 12:24:03 -0700

    chore: add script

--- vhdbuilder/packer/test/build-performance/evaluate-build-performance.sh
+++ vhdbuilder/packer/test/build-performance/evaluate-build-performance.sh
@@ -7,13 +7,4 @@
 
 
 echo -e "\n${SIG_IMAGE_NAME} build performance data:\n" 
-cat ${VHD_BUILD_PERFORMANCE_DATA}
-
-cd vhdbuilder/packer/test/build-performance
-echo -e "\nCompiling vhd build performance test program..\n"
-
-go build main.go
-
-./main.go
-
-echo -e "\nVHD build performance evaluation complete.\n"
+cat ${VHD_BUILD_PERFORMANCE_DATA_FILE} | jq -C .
