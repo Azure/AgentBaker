c3343bbf330f64a8cae97598e641949af8f06994
Zachary Bailey <zbailey@microsoft.com>
2024-08-08 12:26:36 -0700

    chore: cleanup script

--- vhdbuilder/packer/test/build-performance/evaluate-build-performance.sh
+++ vhdbuilder/packer/test/build-performance/evaluate-build-performance.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# cd into correct directory, cat out VHD performance
-# build go program
-# execute go program
-# apply warnings, if necessary
-
-
 echo -e "\n${SIG_IMAGE_NAME} build performance data:\n" 
 cat ${VHD_BUILD_PERFORMANCE_DATA_FILE} | jq -C .
