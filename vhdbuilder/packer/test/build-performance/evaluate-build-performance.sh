4d6c23277cfcb76aa519a2f1ad6576bd2bad05f4
Zachary Bailey <zbailey@microsoft.com>
2024-08-08 13:28:52 -0700

    fix: syntax

--- vhdbuilder/packer/test/build-performance/evaluate-build-performance.sh
+++ vhdbuilder/packer/test/build-performance/evaluate-build-performance.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-echo -e "\n${SIG_IMAGE_NAME} build performance data:\n" 
-cat ${VHD_BUILD_PERFORMANCE_DATA_FILE} | jq -C .
+echo "##[group]${SIG_IMAGE_NAME} build performance data"
+jq -C '.' vhd-build-performance-data.json
+echo "##[endgroup]"
+echo "VHD Build Performance Data generated."
