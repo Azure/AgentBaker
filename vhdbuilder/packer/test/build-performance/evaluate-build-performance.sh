b8e861ab81affa9bfb8aad8920f38e0c9f088088
Zachary Bailey <zbailey@microsoft.com>
2024-08-08 13:34:04 -0700

    test: check nesting

--- vhdbuilder/packer/test/build-performance/evaluate-build-performance.sh
+++ vhdbuilder/packer/test/build-performance/evaluate-build-performance.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "##[group]${SIG_IMAGE_NAME} build performance data"
+echo "##[group]     Inner Group"
 jq -C '.' vhd-build-performance-data.json
+echo "##[endgroup]"
 echo "##[endgroup]"
 echo "VHD Build Performance Data generated."
