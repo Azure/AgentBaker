7eec12ee20b8764c4e70d35ad29b740131f597a0
Zachary Bailey <zbailey@microsoft.com>
2024-08-08 14:17:46 -0700

    test: build data

--- vhdbuilder/packer/test/build-performance/evaluate-build-performance.sh
+++ vhdbuilder/packer/test/build-performance/evaluate-build-performance.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+scripts=()
+for key in $(jq -r '.[] | keys[]' vhd-build-performance-data.json); do
+  scripts+=("$key")
+done
+
 echo "##[group]${SIG_IMAGE_NAME} build performance data"
-echo "##[group]     Inner Group"
-jq -C '.' vhd-build-performance-data.json
+for script in "${scripts[@]}"; do
+  echo "##[group]   ${script}"
+  jq -C ".[] | select(has(\"$script\"))" vhd-build-performance-data.json
+  echo "##[endgroup]"
+done
 echo "##[endgroup]"
-echo "##[endgroup]"
-echo "VHD Build Performance Data generated."
+
+echo -e "\n${SIG_IMAGE_NAME} build performance data generated"
+
+echo -e "\nCompiling test program..."
+
+sleep 10
+
+echo -e "\nBuild performance evaluation successfully completed"
