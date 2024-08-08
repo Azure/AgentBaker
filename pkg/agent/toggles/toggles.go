df23bed970a13fa45974c70988d116aa98246613
Tim Wright <wrightt@microsoft.com>
2024-08-08 15:45:48 +1200

    write tests for new configmap overrides

--- pkg/agent/toggles/toggles.go
+++ pkg/agent/toggles/toggles.go
@@ -2,5 +2,9 @@ package toggles
 
 // GetLinuxNodeImageVersion gets the value of the 'linux-node-image-version' map toggle.
 func (t *Toggles) GetLinuxNodeImageVersion(entity *Entity) map[string]string {
-	return t.getMap("linux-node-image-version", entity)
+	vhdType := t.getMap("vhd-types", entity)
+	customerEntity := Entity{
+		Fields: vhdType,
+	}
+	return t.getMap("linux-node-image-versions", &customerEntity)
 }
