fb2c6edea4ef1fda4f12ffbdb29b49c837aca6d8
Aadhar Agarwal <aadagarwal@microsoft.com>
2024-08-09 12:23:37 -0700

    Update comments to include AzureLinux

--- vhdbuilder/scripts/linux/mariner/tool_installs_mariner.sh
+++ vhdbuilder/scripts/linux/mariner/tool_installs_mariner.sh
@@ -79,8 +79,7 @@ setMarinerNetworkdConfig() {
     DHCP=yes
     IPv6AcceptRA=no
 EOF
-# TODO: update comment
-# On Mariner 2.0 Marketplace images, the default systemd network config
+# On Mariner/AzureLinux Marketplace images, the default systemd network config
 # has an additional change that prevents Mariner from changing IP addresses
 # every reboot
 if [[ $OS_VERSION == "2.0" || $OS_VERSION == "3.0" ]]; then
