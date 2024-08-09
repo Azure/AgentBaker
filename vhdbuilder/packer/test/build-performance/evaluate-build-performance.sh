89f2ea489cea6de2884efe278ddb0d5df94a3884
Zachary Bailey <zbailey@microsoft.com>
2024-08-08 17:34:06 -0700

    chore: cleanup script

--- vhdbuilder/packer/test/build-performance/evaluate-build-performance.sh
+++ vhdbuilder/packer/test/build-performance/evaluate-build-performance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "Generating ${SIG_IMAGE_NAME} build performance data from ${BUILD_PERF_DATA_FILE}...\n"
+echo -e "\nGenerating ${SIG_IMAGE_NAME} build performance data from ${BUILD_PERF_DATA_FILE}...\n"
 
 scripts=()
 for key in $(jq -r '.[] | keys[]' ${BUILD_PERF_DATA_FILE}); do
@@ -12,11 +12,5 @@ for script in "${scripts[@]}"; do
   jq -C ".[] | select(has(\"$script\"))" ${BUILD_PERF_DATA_FILE}
   echo "##[endgroup]"
 done
-
-echo -e "\n${SIG_IMAGE_NAME} build performance data generated"
-
-echo -e "\nCompiling test program..."
-
-sleep 10
 
 echo -e "\nBuild performance evaluation successfully completed"
