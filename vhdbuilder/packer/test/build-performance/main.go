b45daf10872b1243a1abac64c7b72d51deb8c784
Zachary Bailey <zbailey@microsoft.com>
2024-08-09 13:23:34 -0700

    test: ingestion only

--- vhdbuilder/packer/test/build-performance/main.go
+++ vhdbuilder/packer/test/build-performance/main.go
@@ -2,80 +2,47 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"time"
 
 	"github.com/Azure/azure-kusto-go/azkustodata"
 	"github.com/Azure/azure-kusto-go/azkustoingest"
-	"golang.org/x/tools/go/analysis/passes/nilfunc"
 )
 
 func main() {
 
-	sourceBranch := os.Getenv("SOURCE_BRANCH")
+	kustoTable := os.Getenv("KUSTO_TABLE_NAME")
 	kustoEndpoint := os.Getenv("KUSTO_ENDPOINT")
 	kustoDatabase := os.Getenv("KUSTO_DATABASE_NAME")
-	buildPerformanceDataFile := os.Getenv("VHD_BUILD_PERFORMANCE_DATA_FILE")
-	buildPerformanceTable := os.Getenv("KUSTO_TABLE_NAME")
+	//sourceBranchName := os.Getenv("SOURCE_BRANCH_NAME")
+	sigImageName := os.Getenv("SIG_IMAGE_NAME")
+	buildPerformanceDataFile := sigImageName + "-build-performance"
 
 	// Create Connection String
 	kustoConnectionString := azkustodata.NewConnectionStringBuilder(kustoEndpoint).WithSystemManagedIdentity()
 
-	if sourceBranch == "master" {
+	ingestionClient, err := azkustoingest.New(
+		kustoConnectionString,
+		azkustoingest.WithDefaultTable(kustoTable),
+		azkustoingest.WithDefaultDatabase(kustoDatabase))
 
-		ingestionClient, err := azkustoingest.New(kustoConnectionString, azkustoingest.WithoutEndpointCorrection())
-		if err != nil {
-			panic("Kusto ingestion client could not be created.")
-		}
-		defer ingestionClient.Close()
-
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
-		defer cancel()
-
-		_, err = ingestionClient.FromFile(
-			ctx,
-			buildPerformanceDataFile,
-			azkustoingest.IngestionMappingRef("buildPerfMapping", azkustoingest.JSON))
-	
-		if err != nil {
-		  panic("Failed to ingest build performance data.")
-		}
-
-		fmt.PrintLn("Successfully ingested build performance data.")
-
-		return
-	} else {
-    // Branch is not main, so we will query the Kusto DB for main performance data and then compare this run against that
-    queryClient, err := azkustodata.New(kustoConnectionString)
-
-		if err != nil {
-			panic("Failed to create query client.")
-		}
-
-		defer func (queryClient *azkustodata.Client) {
-			err := queryClient.Close()
-			if err != nil {
-				panic("Failed to close query client.")
-			}
-		}(queryClient)
-
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
-
-		
+	if err != nil {
+		panic("Kusto ingestion client could not be created.")
 	}
-}
-	//dataPlaneClient, err := azkustodata.New(kcsb)
-	//if err != nil {
-	//panic("Could not create Kusto Connection String.")
-	//}
+	defer ingestionClient.Close()
 
-	// TODO: If not master, Read JSON file artifact from staging directory, store in var
+	ctx, cancel := context.WithTimeout(context.Background(), 6*time.Minute)
+	defer cancel()
 
-	// TODO: Query kusto DB for sku specific bell curve for each step
+	_, err = ingestionClient.FromFile(
+		ctx,
+		buildPerformanceDataFile,
+		azkustoingest.IngestionMappingRef("buildPerfMapping", azkustoingest.JSON))
 
-	// TODO: Compare the two, for each step that is not within one standard deviation, log step and difference
+	if err != nil {
+		panic("Failed to ingest build performance data.")
+	}
 
-	// TODO: Output JSON Array with build performance data and logs for out-of-spec steps
-
-	// TODO: Exit 0
+	fmt.Println("Successfully ingested build performance data.")
 }
