d4d0f6c19e125783c235aa1b9ef54cb60605fbab
Zachary Bailey <zbailey@microsoft.com>
2024-08-09 08:06:43 -0700

    add: go program

---
+++ vhdbuilder/packer/test/build-performance/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/Azure/azure-kusto-go/azkustodata"
+	"github.com/Azure/azure-kusto-go/azkustoingest"
+)
+
+func main() {
+
+	sourceBranch := os.Getenv("SOURCE_BRANCH")
+	kustoEndpoint := os.Getenv("KUSTO_ENDPOINT")
+	kustoDatabase := os.Getenv("KUSTO_DATABASE_NAME")
+	buildPerformanceData := os.Getenv("VHD_BUILD_PERFORMANCE_DATA_FILE")
+	buildPerformanceTable := os.Getenv("KUSTO_TABLE_NAME")
+
+	// Create Connection String
+	kustoConnectionString := azkustodata.NewConnectionStringBuilder(kustoEndpoint).WithSystemManagedIdentity()
+
+	if sourceBranch == "master" {
+
+		ingestionClient, err := azkustoingest.New(kustoConnectionString, azkustoingest.WithoutEndpointCorrection())
+		if err != nil {
+			panic("Kusto ingestion client could not be created.")
+		}
+		defer ingestionClient.Close()
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
+		defer cancel()
+
+		if _, err := ingestionClient.FromFile(ctx, buildPerformanceData); err != nil {
+			panic("Failed to ingest build performance data.")
+		}
+
+		return
+	} else {
+		// will add some logic here
+	}
+	//dataPlaneClient, err := azkustodata.New(kcsb)
+	//if err != nil {
+	//panic("Could not create Kusto Connection String.")
+	//}
+
+	// TODO: If not master, Read JSON file artifact from staging directory, store in var
+
+	// TODO: Query kusto DB for sku specific bell curve for each step
+
+	// TODO: Compare the two, for each step that is not within one standard deviation, log step and difference
+
+	// TODO: Output JSON Array with build performance data and logs for out-of-spec steps
+
+	// TODO: Exit 0
+}
