c0967c8b8083002e4f7272bf14c2da60d05b0118
Zachary Bailey <zbailey@microsoft.com>
2024-08-10 16:51:28 -0700

    test: add kusto program

---
+++ vhdbuilder/packer/test/build-performance/evaluate-build-performance.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo -e "\nGenerating ${SIG_IMAGE_NAME} build performance data from ${BUILD_PERF_DATA_FILE}...\n"
+
+scripts=()
+for key in $(jq -r '.[] | keys[]' ${BUILD_PERF_DATA_FILE}); do
+  scripts+=("$key")
+done
+
+for script in "${scripts[@]}"; do
+  echo "##[group]${script}"
+  jq -C ".[] | select(has(\"$script\"))" ${BUILD_PERF_DATA_FILE}
+  echo "##[endgroup]"
+done
+
+jq --arg sig "${SIG_IMAGE_NAME}" \
+--arg date "$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
+--arg commit "${GIT_VERSION}" \
+'. as $orig | [{"sig_image_name":$sig}, {"build_datetime":$date}, {"commit":$commit}, {"scripts": $orig}]' \
+${VHD_BUILD_PERFORMANCE_DATA_FILE} > ${SIG_IMAGE_NAME}-build-performance.json
+
+./kustoProgram
+
+echo -e "\nBuild performance evaluation successfully completed"
---
+++ vhdbuilder/packer/test/build-performance/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/Azure/azure-kusto-go/kusto"
+	"github.com/Azure/azure-kusto-go/kusto/ingest"
+)
+
+func main() {
+
+	kustoTable := os.Getenv("KUSTO_TABLE_NAME")
+	kustoEndpoint := os.Getenv("KUSTO_ENDPOINT")
+	kustoDatabase := os.Getenv("KUSTO_DATABASE_NAME")
+	//sourceBranchName := os.Getenv("SOURCE_BRANCH_NAME")
+	sigImageName := os.Getenv("SIG_IMAGE_NAME")
+	buildPerformanceDataFile := sigImageName + "-build-performance.json"
+	var err error
+
+	// Create Connection String
+	kcsb := kusto.NewConnectionStringBuilder(kustoEndpoint)
+	kustoConnectionString := kcsb.WithSystemManagedIdentity()
+
+	ingestionClient, err := kusto.New(kustoConnectionString)
+	if err != nil {
+		log.Fatalf("Kusto ingestion client could not be created.")
+	}
+	defer ingestionClient.Close()
+
+	ingestor, err := ingest.New(ingestionClient, kustoDatabase, kustoTable)
+	if err != nil {
+		ingestionClient.Close()
+		log.Fatalf("Kusto ingestor could not be created.")
+	}
+	defer ingestor.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 6*time.Minute)
+	defer cancel()
+
+	_, err = ingestor.FromFile(
+		ctx,
+		buildPerformanceDataFile,
+		ingest.IngestionMappingRef("buildPerfMapping", ingest.JSON))
+
+	if err != nil {
+		ingestor.Close()
+		ingestionClient.Close()
+		cancel()
+		log.Fatalf("Failed to ingest build performance data.")
+	}
+	defer ingestor.Close()
+
+	fmt.Println("Successfully ingested build performance data.")
+}
