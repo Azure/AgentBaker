b34786714c11bbec0aa69928155b6f930ce16dce
Zachary Bailey <zbailey@microsoft.com>
2024-08-12 15:09:11 -0700

    fix: get ingestion status

--- vhdbuilder/packer/test/build-performance/main.go
+++ vhdbuilder/packer/test/build-performance/main.go
@@ -27,6 +27,8 @@ func main() {
 	ingestionClient, err := kusto.New(kcsb)
 	if err != nil {
 		log.Fatalf("Kusto ingestion client could not be created.")
+	} else {
+		fmt.Printf("Ingestion client successfully created")
 	}
 	defer ingestionClient.Close()
 
@@ -34,13 +36,15 @@ func main() {
 	if err != nil {
 		ingestionClient.Close()
 		log.Fatalf("Kusto ingestor could not be created.")
+	} else {
+		fmt.Printf("Ingestor created successfully\n")
 	}
 	defer ingestor.Close()
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
 
-	_, err = ingestor.FromFile(
+	status, err := ingestor.FromFile(
 		ctx,
 		buildPerformanceDataFile,
 		ingest.IngestionMappingRef("buildPerfMapping", ingest.JSON))
@@ -50,9 +54,24 @@ func main() {
 		ingestor.Close()
 		ingestionClient.Close()
 		cancel()
-		log.Fatalf("Failed to ingest build performance data.")
+		log.Fatalf("Igestion command failed to be sent")
+	} else {
+		fmt.Printf("Ingestion started successfully\n")
 	}
 	defer ingestor.Close()
+
+	err = <-status.Wait(ctx)
+	if err != nil {
+		fmt.Printf("Ingestion failed: %v\n", err)
+		statusCode, _ := ingest.GetIngestionStatus(err)
+		failureStatus, _ := ingest.GetIngestionFailureStatus(err)
+		fmt.Printf("Ingestion status code: %v\n", statusCode)
+		fmt.Printf("Ingestion failure status: %v\n", failureStatus)
+		ingestor.Close()
+		ingestionClient.Close()
+		cancel()
+		log.Fatalf("Ingestion failed to be completed")
+	}
 
 	fmt.Println("Successfully ingested build performance data.")
 }
