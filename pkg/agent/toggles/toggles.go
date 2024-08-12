d71514c21e05b062973d2fe675178c0183c70e10
Tim Wright <wrightt@microsoft.com>
2024-08-12 16:52:22 +1200

    revert

--- pkg/agent/toggles/toggles.go
+++ pkg/agent/toggles/toggles.go
@@ -2,9 +2,5 @@ package toggles
 
 // GetLinuxNodeImageVersion gets the value of the 'linux-node-image-version' map toggle.
 func (t *Toggles) GetLinuxNodeImageVersion(entity *Entity) map[string]string {
-	vhdType := t.getMap("vhd-types", entity)
-	customerEntity := Entity{
-		Fields: vhdType,
-	}
-	return t.getMap("linux-node-image-versions", &customerEntity)
+	return t.getMap("linux-node-image-versions", entity)
 }
