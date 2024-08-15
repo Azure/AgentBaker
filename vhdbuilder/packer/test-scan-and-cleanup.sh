ad6434cd2b5fdfcc9b56f6f897466acb9307e002
Zachary Bailey <zbailey@microsoft.com>
2024-08-15 08:37:36 -0700

    fix: modify script

--- vhdbuilder/packer/test-scan-and-cleanup.sh
+++ vhdbuilder/packer/test-scan-and-cleanup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 retrycmd_if_failure() {
-    retries=$1; wait_sleep=$2; cmd=$3; target=$(basename $(echo $3))
-    echo -e "\n\n==========================================================="
-    echo -e "Running $cmd with $retries retries"
-    for i in $(seq 1 $retries); do
-      $cmd >> output-${target%.*}.txt 2>&1 && break ||
-      if [ $i -eq $retries ]; then
-        echo -e "$target failed $i times\n"
-        cat output-${target%.*}.txt
-        exit 1
-      else
-        sleep $wait_sleep
-        echo -e "\n\n\nNext Attempt:\n\n\n" >> output-${target%.*}.txt
-      fi
-    done
+  retries=$1; wait_sleep=$2; cmd=$3; target=$(basename $(echo $3))
+  echo -e "\n\n==========================================================="
+  echo -e "Running $cmd with $retries retries"
+  for i in $(seq 1 $retries); do
+    $cmd >> output-${target%.*}.txt 2>&1 && break ||
+    if [ $i -eq $retries ]; then
+      echo -e "$target failed $i times\n"
+      cat output-${target%.*}.txt
+      exit 1
+    else
+      sleep $wait_sleep
+      echo -e "\n\n\nNext Attempt:\n\n\n" >> output-${target%.*}.txt
+    fi
+  done
   cat output-${target%.*}.txt
 }
 
@@ -35,8 +35,10 @@ SIG_VERSION=$(az sig image-version show \
 --query id --output tsv || true)
 
 if [ -z "${SIG_VERSION}" ]; then
-  echo -e "Build step did not produce an image version. Running cleanup...\n\n\n"
+  echo -e "Build step did not produce an image version. Running cleanup adn then exiting.\n\n\n"
   retrycmd_if_failure 2 3 "${MAKE_CMD_PREFIX} ${TARGET_ARRAY[@]}"
+  EXIT_CODE=$?
+  exit ${EXIT_CODE}
 fi
 
 if [ "$IMG_SKU" != "20_04-lts-cvm" ]; then
@@ -57,7 +59,6 @@ for TARGET in "${TARGET_ARRAY[@]}"; do
   retrycmd_if_failure 2 3 "${MAKE_CMD_PREFIX} ${TARGET}" &
   TARGET_PIDS+=($!)
 done
-
 wait ${TARGET_PIDS[@]}
 
-echo -e "\n\n\nTest, Scan, and Cleanup Script completed.\n\n\n"
+echo -e "\n\n\nTest, Scan, and Cleanup script completed.\n\n\n"
