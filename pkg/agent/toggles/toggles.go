5e5eacbd0afed5d55a79c17440dfc2bff46db572
Tim Wright <wrightt@microsoft.com>
2024-08-15 16:57:24 +1200

    move to injecting different toggle structure

--- pkg/agent/toggles/toggles.go
+++
@@ -1,8 +0,0 @@
-package toggles
-
-// GetLinuxNodeImageVersion gets the value of the 'linux-node-image-version' map toggle.
-func (t *Toggles) GetLinuxNodeImageVersion(entity *Entity) map[string]string {
-	vhdType := t.getMap("linux-node-vhd-type", entity)
-	vhdImages := t.getMap("linux-node-image-version", &Entity{Fields: vhdType})
-	return vhdImages
-}
