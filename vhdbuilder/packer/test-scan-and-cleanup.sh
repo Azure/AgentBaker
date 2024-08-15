ac850551e8f612f1495e83cf27902f0b2a8be370
Zachary Bailey <zbailey@microsoft.com>
2024-08-15 12:07:43 -0700

    fix: formatting

--- vhdbuilder/packer/test-scan-and-cleanup.sh
+++ vhdbuilder/packer/test-scan-and-cleanup.sh
@@ -5,25 +5,24 @@ retrycmd_if_failure() {
   echo -e "\n\n==========================================================="
   echo -e "Running $cmd with $retries retries"
   for i in $(seq 1 $retries); do
-    $cmd >> output-${target%.*}.txt 2>&1 && break ||
+    $cmd >> ${target%.*}-output.txt 2>&1 && break ||
     if [ $i -eq $retries ]; then
       echo -e "$target failed $i times\n"
-      cat output-${target%.*}.txt
+      cat ${target%.*}-output.txt
       exit 1
     else
       sleep $wait_sleep
-      echo -e "\n\n\nNext Attempt:\n\n\n" >> output-${target%.*}.txt
+      echo -e "\n\n\nNext Attempt:\n\n\n" >> ${target%.*}-output.txt
     fi
   done
-  cat output-${target%.*}.txt
+  cat ${target%.*}-output.txt
 }
 
 if [[ -z "$SIG_GALLERY_NAME" ]]; then
   SIG_GALLERY_NAME="PackerSigGalleryEastUS"
 fi
 
-TARGET_ARRAY=()
-TARGET_ARRAY+=("./vhdbuilder/packer/cleanup.sh") # Always run cleanup
+SCRIPT_ARRAY+=("./vhdbuilder/packer/cleanup.sh") # Always run cleanup
 
 # Check to ensure the build step succeeded
 SIG_VERSION=$(az sig image-version show \
@@ -35,29 +34,29 @@ SIG_VERSION=$(az sig image-version show \
 
 if [ -z "${SIG_VERSION}" ]; then
   echo -e "Build step did not produce an image version. Running cleanup adn then exiting.\n\n\n"
-  retrycmd_if_failure 2 3 ./vhdbuilder/packer/cleanup.sh
+  retrycmd_if_failure 2 3 "${SCRIPT_ARRAY[@]}"
   EXIT_CODE=$?
   exit ${EXIT_CODE}
 fi
 
 if [ "$IMG_SKU" != "20_04-lts-cvm" ]; then
-  TARGET_ARRAY+=("./vhdbuilder/packer/test/run-test.sh")
+  SCRIPT_ARRAY+=("./vhdbuilder/packer/test/run-test.sh")
 else
   echo -e "Skipping tests for CVM 20.04\n\n\n"
 fi
 
 if [ "$OS_VERSION" != "18.04" ]; then
-  TARGET_ARRAY+=("./vhdbuilder/packer/vhd-scanning.sh")
+  SCRIPT_ARRAY+=("./vhdbuilder/packer/vhd-scanning.sh")
 else
   # 18.04 VMs don't have access to new enough 'az' versions to be able to run the az commands in vhd-scanning-vm-exe.sh
   echo -e "Skipping scanning for 18.04\n\n\n"
 fi
 
-TARGET_PIDS=()
-for TARGET in "${TARGET_ARRAY[@]}"; do
+SCRIPT_PIDS=()
+for TARGET in "${SCRIPT_ARRAY[@]}"; do
   retrycmd_if_failure 2 3 "${TARGET}" &
-  TARGET_PIDS+=($!)
+  SCRIPT_PIDS+=($!)
 done
-wait ${TARGET_PIDS[@]}
+wait ${SCRIPT_PIDS[@]}
 
 echo -e "\n\n\nTest, Scan, and Cleanup script completed.\n\n\n"
