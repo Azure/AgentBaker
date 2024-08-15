8e2a48c0a33a00a1c2703caedcdb667a10cd2b01
Zachary Bailey <zbailey@microsoft.com>
2024-08-15 07:34:52 -0700

    fix: add retrycmd to script

--- vhdbuilder/packer/test-scan-and-cleanup.sh
+++ vhdbuilder/packer/test-scan-and-cleanup.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+retrycmd_if_failure() {
+    retries=$1; wait_sleep=$2; cmd=$3; target=$(basename $(echo $3))
+    echo -e "\n\n==========================================================="
+    echo -e "Running $cmd with $retries retries"
+    for i in $(seq 1 $retries); do
+        $cmd >> output-${target%.*}.txt 2>&1 && break ||
+        if [ $i -eq $retries ]; then
+            echo -e "$target failed $i times\n"
+            cat output-${target%.*}.txt
+            exit 1
+        else
+            sleep $wait_sleep
+            echo -e "\n\n\nNext Attempt:\n\n\n" >> output-${target%.*}.txt
+        fi
+    done
+    cat output-${target%.*}.txt
+}
