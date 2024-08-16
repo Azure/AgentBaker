65a6be7ef1be41c543924fc5f55985b4385735da
Zachary Bailey <zbailey@microsoft.com>
2024-08-16 09:14:50 -0700

    fix: formatting

--- vhdbuilder/packer/test-scan-and-cleanup.sh
+++ vhdbuilder/packer/test-scan-and-cleanup.sh
@@ -75,7 +75,7 @@ for SCRIPT in "${!SCRIPT_PIDS[@]}"; do
   echo -e "${SCRIPT} exited with code ${EXIT_CODE}"
 done
 if [[ "${STEP_FAILED}" == true ]]; then
-  echo -e "\nOne or more scripts failed. Exiting with exit code 1."
+  echo -e "\nOne or more scripts failed. Exiting with exit code 1.\n"
   exit 1
 else
   echo -e "\nTest, Scan, and Cleanup script successfully completed.\n"
