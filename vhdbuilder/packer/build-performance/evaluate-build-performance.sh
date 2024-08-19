e7876214428a990c052892191391413ca9c64392
Zachary Bailey <zbailey@microsoft.com>
2024-08-19 14:08:44 -0700

    modify: pipeline

--- vhdbuilder/packer/build-performance/evaluate-build-performance.sh
+++ vhdbuilder/packer/build-performance/evaluate-build-performance.sh
@@ -2,11 +2,11 @@
 
 echo -e "\nGenerating ${SIG_IMAGE_NAME} build performance data from ${BUILD_PERF_DATA_FILE}...\n"
 
-jq --arg sig "${SIG_IMAGE_NAME}" \
---arg date "$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
---arg commit "${GIT_VERSION}" \
-'. as $orig | {"sig_image_name":$sig, "build_datetime":$date, "commit":$commit, "scripts": ($orig | reduce .[] as $item ({}; . + $item) | map_values(map_values(.total_time_elapsed)))}' \
-${BUILD_PERF_DATA_FILE} > ${SIG_IMAGE_NAME}-build-performance.json
+jq --arg sig_image_name "${SIG_IMAGE_NAME}" \
+   --arg build_datetime "$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
+   --arg commit "$GIT_VERSION" \
+   '{sig_image_name: $sig_image_name, build_datetime: $build_datetime, commit: $commit, scripts: .}' ${BUILD_PERF_DATA_FILE} \
+   >> ${SIG_IMAGE_NAME}-build-performance.json
 
 echo "##[group]Build Information"
 jq -C '. | {sig_image_name, build_datetime, commit}' ${SIG_IMAGE_NAME}-build-performance.json
@@ -22,5 +22,7 @@ for script in "${scripts[@]}"; do
   jq -C ".scripts.\"$script\"" ${SIG_IMAGE_NAME}-build-performance.json
   echo "##[endgroup]"
 done
+
+rm ${SIG_IMAGE_NAME}-build-performance.json
 
 echo -e "\nBuild performance evaluation script completed."
