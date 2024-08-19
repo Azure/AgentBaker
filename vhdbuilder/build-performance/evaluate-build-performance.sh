756b772e89aeedb85c06dd1bfe5b2d6ea7ed70f0
Zachary Bailey <zbailey@microsoft.com>
2024-08-19 09:40:44 -0700

    perf: add build performance script

---
+++ vhdbuilder/build-performance/evaluate-build-performance.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo -e "\nGenerating ${SIG_IMAGE_NAME} build performance data from ${BUILD_PERF_DATA_FILE}...\n"
+
+jq --arg sig "${SIG_IMAGE_NAME}" \
+--arg date "$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
+--arg commit "${GIT_VERSION}" \
+'. as $orig | {"sig_image_name":$sig, "build_datetime":$date, "commit":$commit, "scripts": ($orig | reduce .[] as $item ({}; . + $item) | map_values(map_values(.total_time_elapsed)))}' \
+${BUILD_PERF_DATA_FILE} > ${SIG_IMAGE_NAME}-build-performance.json
+
+echo "##[group]Build Information"
+jq -C '. | {sig_image_name, build_datetime, commit}' ${SIG_IMAGE_NAME}-build-performance.json
+echo "##[endgroup]"
+
+scripts=()
+for entry in $(jq -rc '.scripts | to_entries[]' ${SIG_IMAGE_NAME}-build-performance.json); do
+  scripts+=("$(echo "$entry" | jq -r '.key')")
+done
+
+for script in "${scripts[@]}"; do
+  echo "##[group]${script}"
+  jq -C ".scripts.\"$script\"" ${SIG_IMAGE_NAME}-build-performance.json
+  echo "##[endgroup]"
+done
+
+echo -e "\nBuild performance evaluation script completed."
