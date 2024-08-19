5646015ed107be6528d4038048106136b9755f40
sivakami-projects <126191544+sivakami-projects@users.noreply.github.com>
2024-08-19 15:36:22 -0700

    Chore: Bump CNI/ CNS version from v1.6.0 to v1.6.3. (#4825)
    Co-authored-by: Sivakami Subramaniam <t-sivakamis@microsoft.com>
    Co-authored-by: Sivakami Subramaniam <sivakamis@microsoft.com>

--- vhdbuilder/packer/generate-windows-vhd-configuration.ps1
+++ vhdbuilder/packer/generate-windows-vhd-configuration.ps1
@@ -125,14 +125,14 @@ $global:imagesToPull += @(
     "mcr.microsoft.com/azuremonitor/containerinsights/ciprod:win-3.1.23",
     # CNS (Container Networking Service) Owner: jaer-tsun (Jaeryn)
     "mcr.microsoft.com/containernetworking/azure-cns:v1.4.52",
-    "mcr.microsoft.com/containernetworking/azure-cns:v1.5.26",
     "mcr.microsoft.com/containernetworking/azure-cns:v1.5.32",
     "mcr.microsoft.com/containernetworking/azure-cns:v1.6.0",
+    "mcr.microsoft.com/containernetworking/azure-cns:v1.6.3",
     # CNI installer for azure-vnet. Owner: evanbaker
     "mcr.microsoft.com/containernetworking/cni-dropgz:v0.1.4"
     "mcr.microsoft.com/containernetworking/cni-dropgz:v0.2.0"
-    "mcr.microsoft.com/containernetworking/azure-cni:v1.5.26"
     "mcr.microsoft.com/containernetworking/azure-cni:v1.5.32"
+    "mcr.microsoft.com/containernetworking/azure-cni:v1.6.3"
 )
 
 $global:map = @{
@@ -193,8 +193,8 @@ $global:map = @{
     );
     "c:\akse-cache\win-vnet-cni\" = @(
         # Azure CNI v1 (legacy)
-        "https://acs-mirror.azureedge.net/azure-cni/v1.5.28/binaries/azure-vnet-cni-windows-amd64-v1.5.28.zip",
         "https://acs-mirror.azureedge.net/azure-cni/v1.5.32/binaries/azure-vnet-cni-windows-amd64-v1.5.32.zip",
+        "https://acs-mirror.azureedge.net/azure-cni/v1.6.3/binaries/azure-vnet-cni-windows-amd64-v1.6.3.zip",
         # Azure CNI v2 (pod subnet)
         "https://acs-mirror.azureedge.net/azure-cni/v1.4.54/binaries/azure-vnet-cni-swift-windows-amd64-v1.4.54.zip",
         # Azure CNI for Overlay
