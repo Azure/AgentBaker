683c5aa51d401e9057bcd74f43562504e8d57678
Zachary Bailey <zbailey@microsoft.com>
2024-08-21 12:55:49 -0700

    fix: use boolean

--- vhdbuilder/packer/build-performance/evaluate-build-performance.sh
+++ vhdbuilder/packer/build-performance/evaluate-build-performance.sh
@@ -52,7 +52,7 @@ log_and_exit () {
   local SHOW_FILE=${3:-false}
   echo "##vso[task.logissue type=warning;sourcepath=$(basename $0);]${FILE} ${ERR}. Skipping build performance evaluation."
   echo "##vso[task.complete result=SucceededWithIssues;]"
-  if [[ ${SHOW_FILE} == "true" ]]; then
+  if [[ ${SHOW_FILE} == true ]]; then
     cat ${FILE}
   fi
   exit 0
