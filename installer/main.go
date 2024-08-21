ca76e8933463371f990ab9a366a3b17b1031cf3a
r2k1 <yokree@gmail.com>
2024-08-21 15:25:59 +1200

    add tests

--- installer/main.go
+++ installer/main.go
@@ -6,11 +6,15 @@ import (
 )
 
 func main() {
+	mustConfigureLogging()
+	slog.Info("Starting installer")
+}
+
+func mustConfigureLogging() {
 	file, err := os.OpenFile("installer.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 	slog.SetDefault(slog.New(slog.NewTextHandler(file, &slog.HandlerOptions{AddSource: true})))
-	slog.Info("Starting installer")
 }
