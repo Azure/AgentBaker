d10f9245e20ad05b506fc03f417a61a16e76b04d
Mikolaj Umanski <mumanski@microsoft.com>
2024-08-21 14:28:59 -0700

    chore: remove newline

--- e2e/exec.go
+++ e2e/exec.go
@@ -71,7 +71,6 @@ func extractLogsFromVM(ctx context.Context, t *testing.T, vmssName, privateIP, s
 		t.Logf("executing command on remote VM at %s of VMSS %s: %q", privateIP, vmssName, sourceCmd)
 
 		execResult, err := execOnVM(ctx, opts.clusterConfig.Kube, privateIP, podName, sshPrivateKey, sourceCmd, false)
-
 		if err != nil {
 			t.Logf("error executing command on remote VM at %s of VMSS %s: %s", privateIP, vmssName, err)
 		}
