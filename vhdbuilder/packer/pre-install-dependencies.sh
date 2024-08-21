499eeaf7aa28109800ee34f1d79406e45b8a04ed
Zachary Bailey <zbailey@microsoft.com>
2024-08-21 09:15:56 -0700

    test: make json valid

--- vhdbuilder/packer/pre-install-dependencies.sh
+++ vhdbuilder/packer/pre-install-dependencies.sh
@@ -30,7 +30,7 @@ MANIFEST_FILEPATH=/opt/azure/manifest.json
 cat components.json > ${COMPONENTS_FILEPATH}
 cat manifest.json > ${MANIFEST_FILEPATH}
 echo "Starting build on " $(date) > ${VHD_LOGS_FILEPATH}
-echo '{' > ${VHD_BUILD_PERF_DATA}
+echo '{}' > ${VHD_BUILD_PERF_DATA}
 
 if isMarinerOrAzureLinux "$OS"; then
   chmod 755 /opt
