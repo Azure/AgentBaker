c5ac15a9544a899aada5987f5c3477a3fbb4c61a
Zachary Bailey <zbailey@microsoft.com>
2024-08-21 10:39:45 -0700

    test: no build perf file

--- vhdbuilder/packer/pre-install-dependencies.sh
+++ vhdbuilder/packer/pre-install-dependencies.sh
@@ -24,13 +24,11 @@ source /home/packer/packer_source.sh
 CPU_ARCH=$(getCPUArch)  #amd64 or arm64
 VHD_LOGS_FILEPATH=/opt/azure/vhd-install.complete
 COMPONENTS_FILEPATH=/opt/azure/components.json
-VHD_BUILD_PERF_DATA=/opt/azure/vhd-build-performance-data.json
 MANIFEST_FILEPATH=/opt/azure/manifest.json
 #this is used by post build test to check whether the compoenents do indeed exist
 cat components.json > ${COMPONENTS_FILEPATH}
 cat manifest.json > ${MANIFEST_FILEPATH}
 echo "Starting build on " $(date) > ${VHD_LOGS_FILEPATH}
-echo '{' > ${VHD_BUILD_PERF_DATA}
 
 if [[ $OS == $MARINER_OS_NAME ]]; then
   chmod 755 /opt
