628b2c9c11d09b77f87de3dec2a7acfdbbe29186
Aadhar Agarwal <aadagarwal@microsoft.com>
2024-08-21 15:53:16 -0700

    Update installNvidiaContainerRuntime func name to installNvidiaContainerToolkit

--- parts/linux/cloud-init/artifacts/cse_config.sh
+++ parts/linux/cloud-init/artifacts/cse_config.sh
@@ -663,7 +663,7 @@ configGPUDrivers() {
         fi
     elif isMarinerOrAzureLinux "$OS"; then
         downloadGPUDrivers
-        installNvidiaContainerRuntime
+        installNvidiaContainerToolkit
         enableNvidiaPersistenceMode
     else 
         echo "os $OS not supported at this time. skipping configGPUDrivers"
