4bc939ab6e6268db25339e66d29118a689bedb72
Alison Burgess <alburgess@microsoft.com>
2024-08-26 15:58:39 -0700

    fix local var

--- parts/linux/cloud-init/artifacts/cse_install.sh
+++ parts/linux/cloud-init/artifacts/cse_install.sh
@@ -175,6 +175,8 @@ downloadSecureTLSBootstrapKubeletExecPlugin() {
 
 downloadContainerdWasmShims() {
     declare -a wasmShimPids=()
+    local containerd_wasm_filepath="/usr/local/bin" 
+    
     for shim_version in $CONTAINERD_WASM_VERSIONS; do
         binary_version="$(echo "${shim_version}" | tr . -)"
 
@@ -194,7 +196,6 @@ downloadContainerdWasmShims() {
             rm -r $WASM_TMP
         }
         installWithCurl() {
-            local containerd_wasm_filepath="/usr/local/bin" 
             local containerd_wasm_url="https://acs-mirror.azureedge.net/containerd-wasm-shims/${shim_version}/linux/${CPU_ARCH}"
             retrycmd_if_failure 30 5 60 curl -fSLv -o "$containerd_wasm_filepath/containerd-shim-spin-${binary_version}-v1" "$containerd_wasm_url/containerd-shim-spin-v1" 2>&1 | tee $CURL_OUTPUT >/dev/null | grep -E "^(curl:.*)|([eE]rr.*)$" && (cat $CURL_OUTPUT && exit $ERR_KRUSTLET_DOWNLOAD_TIMEOUT) &
             wasmShimPids+=($!)
