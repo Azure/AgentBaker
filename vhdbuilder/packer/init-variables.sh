08273e332544bec517cf3158733d8fd94ab3ccf7
Zachary Bailey <zbailey@microsoft.com>
2024-08-27 15:44:59 -0700

    fix: replicate to westus

--- vhdbuilder/packer/init-variables.sh
+++ vhdbuilder/packer/init-variables.sh
@@ -159,7 +159,7 @@ if [[ "${MODE}" == "linuxVhdMode" ]]; then
 			SIG_IMAGE_NAME="AzureLinux${SIG_IMAGE_NAME}"
 		fi
 		if [[ ${IMG_SKU} == "20_04-lts-cvm" ]]; then
-			SIG_IMAGE_NAME+="New"
+			SIG_IMAGE_NAME+="TESTDEF"
 	  fi
 		echo "No input for SIG_IMAGE_NAME was provided, defaulting to: ${SIG_IMAGE_NAME}"
 	else
