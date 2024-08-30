f0c0ffcd6869da5b464059f818d227fce0897bfc
Zachary Bailey <zbailey@microsoft.com>
2024-08-30 08:37:51 -0700

    fix: change image def

--- vhdbuilder/packer/init-variables.sh
+++ vhdbuilder/packer/init-variables.sh
@@ -159,7 +159,7 @@ if [[ "${MODE}" == "linuxVhdMode" ]]; then
 			SIG_IMAGE_NAME="AzureLinux${SIG_IMAGE_NAME}"
 		fi
 		if [[ ${IMG_SKU} == "20_04-lts-cvm" ]]; then
-			SIG_IMAGE_NAME+="SpecializedImageDef"
+			SIG_IMAGE_NAME+="Specialized"
 	  fi
 		echo "No input for SIG_IMAGE_NAME was provided, defaulting to: ${SIG_IMAGE_NAME}"
 	else
