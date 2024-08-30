d7966d21d93b428539f72f72bae43565784a8eb6
Tim Wright <wrightt@microsoft.com>
2024-08-30 12:47:18 +1200

    simplify

--- cmd/cli/makeCse.sh
+++ cmd/cli/makeCse.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-export fqdn=aks-timmy-wrightt-resource-82acd5-zr3wop33.hcp.australiaeast.azmk8s.io
 export subID=subID
 export tenantId=tenantId
-export orchestratorVersion=1.30.3
+
+export fqdn=https://aks-timmy-wrightt-resource-82acd5-zr3wop33.hcp.australiaeast.azmk8s.io
 export WINDOWS_E2E_VMSIZE=Standard_DS1_v2
 export KUBERNETES_VERSION=1.30.3
-export csePackageURL=https://acs-mirror.azureedge.net/aks/windows/cse/csescripts-v0.0.1.zip
+export csePackageURL=https://acs-mirror.azureedge.net/aks/windows/cse/
 
 export WINDOWS_E2E_IMAGE=2019-containerd
 export WINDOWS_DISTRO=aks-windows-2019-containerd
@@ -15,9 +15,6 @@ export WINDOWS_DISTRO=aks-windows-2019-containerd
 export WINDOWS_GPU_DRIVER_SUFFIX=
 export WINDOWS_GPU_DRIVER_URL=""
 export CONFIG_GPU_DRIVER_IF_NEEDED=false
-
-export WINDOWS_PACKAGE_VERSION=$KUBERNETES_VERSION
-export K8S_VERSION=${WINDOWS_PACKAGE_VERSION//./}
 
 envsubst < percluster_template.json > _percluster_config.json
 
