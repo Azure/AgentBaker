d5b499f625c589f4a055ee967cf01d8804216a53
r2k1 <yokree@gmail.com>
2024-08-31 21:10:55 +1200

    fix path to installer

--- installer/build.sh
+++ installer/build.sh
@@ -1,4 +1,3 @@
-rm -r dist
 GOOS=linux GOARCH=amd64 go build -o ./dist/installer-linux-amd64
 GOOS=linux GOARCH=arm64 go build -o ./dist/installer-linux-arm64
 GOOS=windows GOARCH=amd64 go build -o ./dist/installer-windows-amd64.exe
