c9b7636efed988a491636cc2a555907a2b8247c7
r2k1 <yokree@gmail.com>
2024-08-31 20:23:44 +1200

    build installer

--- installer/build.sh
+++ installer/build.sh
@@ -1,1 +1,5 @@
-GOOS=linux GOARCH=amd64 go build -o ../dist/installer-linux-amd64
+rm -r dist
+GOOS=linux GOARCH=amd64 go build -o ./dist/installer-linux-amd64
+GOOS=linux GOARCH=arm64 go build -o ./dist/installer-linux-arm64
+GOOS=windows GOARCH=amd64 go build -o ./dist/installer-windows-amd64.exe
+GOOS=windows GOARCH=arm64 go build -o ./dist/installer-windows-arm64.exe
