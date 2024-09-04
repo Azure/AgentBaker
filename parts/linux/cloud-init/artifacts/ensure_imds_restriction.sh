42b1ad92744cc617d36a9250920a1d7f21ac016e
Tongyao Si <tosi@microsoft.com>
2024-09-04 06:51:41 +0000

    code review

--- parts/linux/cloud-init/artifacts/ensure_imds_restriction.sh
+++ parts/linux/cloud-init/artifacts/ensure_imds_restriction.sh
@@ -6,6 +6,10 @@ primaryNicIP="${PRIMARY_NIC_IP}"
 insertRuleToMangleTable="${INSERT_IMDS_RESTRICTION_RULE_TO_MANGLE_TABLE}"
 enableIMDSRestriction="${ENABLE_IMDS_RESTRICTION}"
 
+echo "Primary NIC IP: $primaryNicIP"
+echo "Insert IMDS restriction rule to mangle table: $insertRuleToMangleTable"
+echo "Enable IMDS restriction: $enableIMDSRestriction"
+
 ensureIMDSRestrictionRule() {
     if [[ "{$insertRuleToMangleTable,,}" == "true" ]]; then
         echo "Before inserting IMDS restriction rule to mangle table, checking whether the rule already exists..."
@@ -15,7 +19,7 @@ ensureIMDSRestrictionRule() {
             return 0
         fi
         echo "Inserting IMDS restriction rule to mangle table..."
-        iptables -t mangle -I FORWARD 1 ! -s "$primaryNicIP" -d 169.254.169.254/32 -p tcp -m tcp --dport 80 -m comment --comment "AKS managed: added by AgentBaker ensureIMDSRestriction for IMDS restriction feature" -j DROP || exit ${ERR_INSERT_IMDS_RESTRICTION_RULE_INTO_MANGLE_TABLE}
+        iptables -t mangle -I FORWARD 1 ! -s "$primaryNicIP" -d 169.254.169.254/32 -p tcp -m tcp --dport 80 -m comment --comment "AKS managed: added by AgentBaker ensureIMDSRestriction for IMDS restriction feature" -j DROP || exit 1
     else
         echo "Before inserting IMDS restriction rule to filter table, checking whether the rule already exists..."
         iptables -t filter -S | grep -- '-d 169.254.169.254/32 -p tcp -m tcp --dport 80 -m comment --comment "AKS managed: added by AgentBaker ensureIMDSRestriction for IMDS restriction feature" -j DROP'
@@ -24,7 +28,7 @@ ensureIMDSRestrictionRule() {
             return 0
         fi
         echo "Inserting IMDS restriction rule to filter table..."
-        iptables -t filter -I FORWARD 1 ! -s "$primaryNicIP" -d 169.254.169.254/32 -p tcp -m tcp --dport 80 -m comment --comment "AKS managed: added by AgentBaker ensureIMDSRestriction for IMDS restriction feature" -j DROP || exit ${ERR_INSERT_IMDS_RESTRICTION_RULE_INTO_FILTER_TABLE}
+        iptables -t filter -I FORWARD 1 ! -s "$primaryNicIP" -d 169.254.169.254/32 -p tcp -m tcp --dport 80 -m comment --comment "AKS managed: added by AgentBaker ensureIMDSRestriction for IMDS restriction feature" -j DROP || exit 1
     fi
 }
 
@@ -35,7 +39,7 @@ disableIMDSRestriction() {
         echo "IMDS restriction rule does not exist in mangle table, no need to delete"
     else
         echo "Deleting IMDS restriction rule from mangle table..."
-        iptables -t mangle -D FORWARD ! -s $primaryNicIP -d 169.254.169.254/32 -p tcp -m tcp --dport 80 -m comment --comment "AKS managed: added by AgentBaker ensureIMDSRestriction for IMDS restriction feature" -j DROP || exit ${ERR_DELETE_IMDS_RESTRICTION_RULE_FROM_MANGLE_TABLE}
+        iptables -t mangle -D FORWARD ! -s $primaryNicIP -d 169.254.169.254/32 -p tcp -m tcp --dport 80 -m comment --comment "AKS managed: added by AgentBaker ensureIMDSRestriction for IMDS restriction feature" -j DROP || exit 1
     fi
 
     echo "Checking whether IMDS restriction rule exists in filter table..."
@@ -44,12 +48,12 @@ disableIMDSRestriction() {
          echo "IMDS restriction rule does not exist in filter table, no need to delete"
     else
         echo "Deleting IMDS restriction rule from filter table..."
-        iptables -t filter -D FORWARD ! -s $primaryNicIP -d 169.254.169.254/32 -p tcp -m tcp --dport 80 -m comment --comment "AKS managed: added by AgentBaker ensureIMDSRestriction for IMDS restriction feature" -j DROP || exit ${ERR_DELETE_IMDS_RESTRICTION_RULE_FROM_FILTER_TABLE}
+        iptables -t filter -D FORWARD ! -s $primaryNicIP -d 169.254.169.254/32 -p tcp -m tcp --dport 80 -m comment --comment "AKS managed: added by AgentBaker ensureIMDSRestriction for IMDS restriction feature" -j DROP || exit 1
     fi
 }
 
 if [[ "${enableIMDSRestriction,,}" == "true" ]]; then
-    ensureIMDSRestrictionRule
+    logs_to_events "AKS.CSE.ensureIMDSRestriction" ensureIMDSRestrictionRule || exit 1
 else
-    disableIMDSRestriction
+    logs_to_events "AKS.CSE.ensureIMDSRestriction" disableIMDSRestriction || exit 1
 fi
