345908e61be89bb844811daf9580189e50904dd9
andyzhangx <xiazhang@microsoft.com>
2024-09-04 11:05:05 +0000

    chore: update testdata change

--- pkg/agent/testdata/AKSUbuntu1604+CustomKubeletConfig+DynamicKubeletConfig/line62.sh
+++ pkg/agent/testdata/AKSUbuntu1604+CustomKubeletConfig+DynamicKubeletConfig/line62.sh
@@ -33,7 +33,7 @@ installDeps() {
     local OSVERSION
     OSVERSION=$(grep DISTRIB_RELEASE /etc/*-release| cut -f 2 -d "=")
     BLOBFUSE_VERSION="1.4.5"
-    BLOBFUSE2_VERSION="2.3.0"
+    BLOBFUSE2_VERSION="2.3.2"
 
     if [ "${OSVERSION}" == "18.04" ]; then
         BLOBFUSE2_VERSION="2.2.0"
