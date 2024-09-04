4a847bcb70fd2a079195f259cbd74901fca47e53
Zachary Bailey <zbailey@microsoft.com>
2024-09-04 12:47:45 -0700

    feat: update azure-arm builder for packer

--- packer.mk
+++ packer.mk
@@ -81,6 +81,7 @@ init-packer:
 	@./vhdbuilder/packer/init-variables.sh
 
 run-packer: az-login
+	@packer init ./vhdbuilder/packer/linux-packer-plugin.pkr.hcl
 	@packer version && ($(MAKE) -f packer.mk init-packer | tee packer-output) && ($(MAKE) -f packer.mk build-packer | tee -a packer-output)
 
 run-packer-windows: az-login
