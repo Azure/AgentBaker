2ed9ab601ba1ab83a8e85b94e522ea50ad4b7e64
Zachary Bailey <zbailey@microsoft.com>
2024-09-11 09:55:36 -0700

    save progress

--- vhdbuilder/packer/build-performance/main.go
+++ vhdbuilder/packer/build-performance/main.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/Azure/azure-kusto-go/kusto"
 	"github.com/Azure/azure-kusto-go/kusto/ingest"
+	"github.com/Azure/azure-kusto-go/kusto/kql"
 )
 
 func main() {
@@ -21,9 +22,12 @@ func main() {
 	// build data variables
 	sigImageName := os.Getenv("SIG_IMAGE_NAME")
 	buildPerformanceDataFile := sigImageName + "-build-performance.json"
+	sourceBranch := os.Getenv("GIT_BRANCH")
+
 	var err error
 
 	fmt.Printf("\nRunning build performance program for %s...\n\n", sigImageName)
+
 	// Create Connection String
 	kcsb := kusto.NewConnectionStringBuilder(kustoEndpoint).WithUserManagedIdentity(kustoClientID)
 
@@ -36,32 +40,48 @@ func main() {
 	}
 	defer client.Close()
 
-	// Create Ingestor
-	ingestor, err := ingest.New(client, kustoDatabase, kustoTable)
-	if err != nil {
-		client.Close()
-		log.Fatalf("Kusto ingestor could not be created.")
-	} else {
-		fmt.Printf("Created ingestor...\n\n")
-	}
-	defer ingestor.Close()
-
 	// Create Context
 	ctx, cancel := context.WithTimeout(context.Background(), 6*time.Minute)
 	defer cancel()
 
-	// Ingest Data
-	_, err = ingestor.FromFile(ctx, buildPerformanceDataFile, ingest.IngestionMappingRef("buildPerfMap", ingest.MultiJSON))
-	if err != nil {
-		fmt.Printf("Ingestion failed: %v\n\n", err)
-		ingestor.Close()
-		client.Close()
-		cancel()
-		log.Fatalf("Igestion command failed to be sent.\n")
-	} else {
-		fmt.Printf("Ingestion started successfully.\n\n")
-	}
-	defer ingestor.Close()
+	if sourceBranch == "refs/heads/zb/ingestBuildPerfData" {
 
-	fmt.Printf("Successfully ingested build performance data.\n")
+		fmt.Printf("Branch is %s, ingesting data.\n", sourceBranch)
+
+		// Create Ingestor
+		ingestor, err := ingest.New(client, kustoDatabase, kustoTable)
+		if err != nil {
+			client.Close()
+			log.Fatalf("Kusto ingestor could not be created.")
+		} else {
+			fmt.Printf("Created ingestor...\n\n")
+		}
+		defer ingestor.Close()
+
+		// Ingest Data
+		_, err = ingestor.FromFile(ctx, buildPerformanceDataFile, ingest.IngestionMappingRef("buildPerfMap", ingest.MultiJSON))
+		if err != nil {
+			fmt.Printf("Ingestion failed: %v\n\n", err)
+			ingestor.Close()
+			client.Close()
+			cancel()
+			log.Fatalf("Igestion command failed to be sent.\n")
+		} else {
+			fmt.Printf("Ingestion started successfully.\n\n")
+		}
+		defer ingestor.Close()
+
+		fmt.Printf("Successfully ingested build performance data.\n")
+	}
+
+	// Create query regardless of the branch
+	query := kql.New("systemNodes | project CollectionTime, NodeId | where CollectionTime > startTime and NodeId == nodeIdValue")
+
+	params := kql.NewParameters().AddString("SIG_IMAGE_NAME", sigImageName).AddString("DATA", "BUILD_PERFORMANCE")
+
+	result, err := client.Query(ctx, kustoDatabase, query, QueryParameters(params))
+	if err != nil {
+		fmt.Printf("Failed to query build performance data for %s.\n\n", sigImageName)
+	}
+
 }
