96a78660c6a25d34172a0faad96da1c23e5f6ae2
Devin Wong <devinwon@microsoft.com>
2024-09-10 19:08:48 -0700

    testing

--- parts/linux/cloud-init/artifacts/cse_helpers.sh
+++ parts/linux/cloud-init/artifacts/cse_helpers.sh
@@ -544,14 +544,18 @@ updatePackageVersions() {
 
     # if .downloadURIs.${osLowerCase} doesn't exist, it will get the versions from .downloadURIs.default.
     # Otherwise get the versions from .downloadURIs.${osLowerCase}
+    echo "executing jq .downloadURIs.${osLowerCase} on ${package}"
     if [[ $(echo "${package}" | jq ".downloadURIs.${osLowerCase}") == "null" ]]; then
         osLowerCase="default"
     fi
 
     # jq the versions from the package. If downloadURIs.$osLowerCase.$release.versionsV2 is not null, then get the versions from there.
     # Otherwise get the versions from .downloadURIs.$osLowerCase.$release.versions
+    echo "executing jq .downloadURIs.${osLowerCase}.${RELEASE}.versionsV2 on ${package}"
     if [[ $(echo "${package}" | jq ".downloadURIs.${osLowerCase}.${RELEASE}.versionsV2") != "null" ]]; then
+        echo "executing jq -r .downloadURIs.${osLowerCase}.${RELEASE}.versionsV2[] | select(.latestVersion != null) | .latestVersion on ${package}"
         local latestVersions=($(echo "${package}" | jq -r ".downloadURIs.${osLowerCase}.${RELEASE}.versionsV2[] | select(.latestVersion != null) | .latestVersion"))
+        echo "executing jq -r .downloadURIs.${osLowerCase}.${RELEASE}.versionsV2[] | select(.previousLatestVersion != null) | .previousLatestVersion on ${package}"
         local previousLatestVersions=($(echo "${package}" | jq -r ".downloadURIs.${osLowerCase}.${RELEASE}.versionsV2[] | select(.previousLatestVersion != null) | .previousLatestVersion"))
         for version in "${latestVersions[@]}"; do
             PACKAGE_VERSIONS+=("${version}")
