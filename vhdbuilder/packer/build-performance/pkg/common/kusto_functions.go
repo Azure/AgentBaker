36948d08d2fadbbf583278e33307ff15d0b06408
Zachary Bailey <zbailey@microsoft.com>
2024-09-12 09:37:39 -0700

    save progress

--- vhdbuilder/packer/build-performance/pkg/common/kusto_functions.go
+++ vhdbuilder/packer/build-performance/pkg/common/kusto_functions.go
@@ -5,8 +5,12 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/Azure/AgentBaker/vhdbuilder/packer/build-performance/pkg/common"
 	"github.com/Azure/azure-kusto-go/kusto"
+	kustoErrors "github.com/Azure/azure-kusto-go/kusto/data/errors"
+	"github.com/Azure/azure-kusto-go/kusto/data/table"
 	"github.com/Azure/azure-kusto-go/kusto/ingest"
+	"github.com/Azure/azure-kusto-go/kusto/kql"
 )
 
 func IngestData(client *kusto.Client, kustoDatabase string, kustoTable string, ctx context.Context, buildPerformanceDataFile string) {
@@ -32,4 +36,35 @@ func IngestData(client *kusto.Client, kustoDatabase string, kustoTable string, c
 		fmt.Printf("Successfully ingested build performance data.\n\n")
 	}
 	defer ingestor.Close()
+}
+
+func QueryData(sigImageName string, client *kusto.Client, kustoDatabase string, kustoTable string, ctx context.Context, buildPerformanceDataFile string) *common.SKU {
+
+	query := kql.New("Get_Performance_Data | where SIG_IMAGE_NAME == SKU")
+	params := kql.NewParameters().AddString("SKU", sigImageName)
+
+	iter, err := client.Query(ctx, kustoDatabase, query, kusto.QueryParameters(params))
+	if err != nil {
+		fmt.Printf("Failed to query build performance data for %s.\n\n", sigImageName)
+	}
+	defer iter.Stop()
+
+	data := common.SKU{}
+	err = iter.DoOnRowOrError(
+		func(row *table.Row, e *kustoErrors.Error) error {
+			if e != nil {
+				return e
+			}
+
+			if err := row.ToStruct(&data); err != nil {
+				return err
+			}
+
+			return nil
+		},
+	)
+	if err != nil {
+		fmt.Printf("Failed to load %s performance data into Go struct.\n\n", sigImageName)
+	}
+	return &data
 }
