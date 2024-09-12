a4f89b59bd92bad789cbf662356333680449d86e
Zachary Bailey <zbailey@microsoft.com>
2024-09-12 11:45:15 -0700

    save progress

--- vhdbuilder/packer/build-performance/main.go
+++ vhdbuilder/packer/build-performance/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 	"time"
 
 	"github.com/Azure/AgentBaker/vhdbuilder/packer/build-performance/pkg/common"
@@ -14,25 +13,14 @@ import (
 
 func main() {
 
-	kustoTable := os.Getenv("BUILD_PERFORMANCE_TABLE_NAME")
-	kustoEndpoint := os.Getenv("BUILD_PERFORMANCE_KUSTO_ENDPOINT")
-	kustoDatabase := os.Getenv("BUILD_PERFORMANCE_DATABASE_NAME")
-	kustoClientID := os.Getenv("BUILD_PERFORMANCE_CLIENT_ID")
+	config, err := common.SetupConfig()
+	if err != nil {
+		log.Fatalf("Error setting up build-performance program configuration. Exiting...")
+	}
 
-	sigImageName := os.Getenv("SIG_IMAGE_NAME")
-	buildPerformanceDataFile := sigImageName + "-build-performance.json"
-	sourceBranch := os.Getenv("GIT_BRANCH")
+	dataMaps, err := common.CreateDataMaps()
 
-	queriedPerformanceData := map[string]map[string][]float64{}
-	currentBuildPerformanceData := map[string]map[string]float64{}
-	holdingMap := map[string]map[string]string{}
-	regressions := map[string]map[string]float64{}
-
-	var err error
-
-	fmt.Printf("\nRunning build performance program for %s...\n\n", sigImageName)
-
-	kcsb := kusto.NewConnectionStringBuilder(kustoEndpoint).WithUserManagedIdentity(kustoClientID)
+	kcsb := kusto.NewConnectionStringBuilder(config.KustoEndpoint).WithUserManagedIdentity(config.KustoClientID)
 
 	client, err := kusto.New(kcsb)
 	if err != nil {
@@ -45,17 +33,21 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
 	defer cancel()
 
-	if sourceBranch == "refs/heads/zb/ingestBuildPerfData" {
+	if config.SourceBranch == "refs/heads/zb/ingestBuildPerfData" {
 
-		fmt.Printf("Branch is %s, ingesting data.\n", sourceBranch)
-		common.IngestData(client, kustoDatabase, kustoTable, ctx, buildPerformanceDataFile)
-
+		fmt.Printf("Branch is %s, ingesting data.\n", config.SourceBranch)
+		common.IngestData(client, config.KustoDatabase, config.KustoTable, ctx, config.LocalBuildPerformanceFile)
 	}
 
-	data := common.QueryData(sigImageName, client, kustoDatabase, kustoTable, ctx, buildPerformanceDataFile)
-	common.DecodeVHDPerformanceData("/home/zbailey/go/go_proj/go_test/json.json", holdingMap)
-	common.ConvertTimestampsToSeconds(holdingMap, currentBuildPerformanceData)
-	common.ParseKustoData(data, queriedPerformanceData)
-	common.EvaluatePerformance(currentBuildPerformanceData, queriedPerformanceData, regressions)
-	common.PrintRegressions(regressions)
+	aggregateSKUData := common.QueryData(config.SigImageName, client, config.KustoDatabase, config.KustoTable, ctx, config.LocalBuildPerformanceFile)
+
+	common.DecodeVHDPerformanceData(config.LocalBuildPerformanceFile, dataMaps.HoldingMap)
+
+	common.ConvertTimestampsToSeconds(dataMaps.HoldingMap, dataMaps.LocalPerformanceDataMap)
+
+	common.ParseKustoData(aggregateSKUData, dataMaps.QueriedPerformanceData)
+
+	common.EvaluatePerformance(dataMaps.LocalPerformanceDataMap, dataMaps.QueriedPerformanceData, dataMaps.RegressionMap)
+
+	common.PrintRegressions(dataMaps.RegressionMap)
 }
--- vhdbuilder/packer/build-performance/pkg/common/types.go
+++ vhdbuilder/packer/build-performance/pkg/common/types.go
@@ -10,16 +10,16 @@ type Config struct {
 	SourceBranch              string
 }
 
-type SKU struct {
-	Name               string `kusto:"SIG_IMAGE_NAME"`
-	SKUPerformanceData string `kusto:"BUILD_PERFORMANCE"`
-}
-
 type DataMaps struct {
 	LocalPerformanceDataMap   map[string]map[string]float64
 	QueriedPerformanceDataMap map[string]map[string][]float64
 	HoldingMap                map[string]map[string]string
 	RegressionMap             map[string]map[string]float64
+}
+
+type SKU struct {
+	Name               string `kusto:"SIG_IMAGE_NAME"`
+	SKUPerformanceData string `kusto:"BUILD_PERFORMANCE"`
 }
 
 //var err error
