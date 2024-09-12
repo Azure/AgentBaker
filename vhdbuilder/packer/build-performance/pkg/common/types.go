1165dcf4f50a31553291e9af06b9a99e96102c0a
Zachary Bailey <zbailey@microsoft.com>
2024-09-12 09:59:33 -0700

    feat: create structs

--- vhdbuilder/packer/build-performance/pkg/common/types.go
+++ vhdbuilder/packer/build-performance/pkg/common/types.go
@@ -1,6 +1,25 @@
 package common
 
+type Config struct {
+	KustoTable                string
+	KustoEndpoint             string
+	KustoDatabase             string
+	KustoClientID             string
+	SigImageName              string
+	LocalBuildPerformanceFile string
+	SourceBranch              string
+}
+
 type SKU struct {
 	Name               string `kusto:"SIG_IMAGE_NAME"`
 	SKUPerformanceData string `kusto:"BUILD_PERFORMANCE"`
 }
+
+type DataMaps struct {
+	LocalPerformanceDataMap   map[string]map[string]float64
+	QueriedPerformanceDataMap map[string]map[string][]float64
+	HoldingMap                map[string]map[string]string
+	RegressionMap             map[string]map[string]float64
+}
+
+//var err error
