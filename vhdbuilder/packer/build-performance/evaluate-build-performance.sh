57ef7b57ed63681b78477b740e1c5618ecb36f7a
Zachary Bailey <zbailey@microsoft.com>
2024-09-14 13:49:20 -0700

    comment out binary

--- vhdbuilder/packer/build-performance/evaluate-build-performance.sh
+++ vhdbuilder/packer/build-performance/evaluate-build-performance.sh
@@ -58,11 +58,11 @@ for script in "${scripts[@]}"; do
   echo "##[endgroup]"
 done
 
-echo -e "\nRunning build performance evaluation program...\n"
-mv ${SIG_IMAGE_NAME}-build-performance.json vhdbuilder/packer/build-performance
-pushd vhdbuilder/packer/build-performance
-  ./buildPerformance
-  rm ${SIG_IMAGE_NAME}-build-performance.json
-popd
+#echo -e "\nRunning build performance evaluation program...\n"
+#mv ${SIG_IMAGE_NAME}-build-performance.json vhdbuilder/packer/build-performance
+#pushd vhdbuilder/packer/build-performance
+  #./buildPerformance
+  #rm ${SIG_IMAGE_NAME}-build-performance.json
+#popd
 
-echo -e "\nBuild performance evaluation script completed."
+#echo -e "\nBuild performance evaluation script completed."
