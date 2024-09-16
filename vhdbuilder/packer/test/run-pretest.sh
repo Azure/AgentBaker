263f1caccf05de4bb99388de01c66b7bf44995e8
Devin Wong <devinwon@microsoft.com>
2024-09-16 11:42:13 -0700

    update run-pretest.sh

--- vhdbuilder/packer/test/run-pretest.sh
+++ vhdbuilder/packer/test/run-pretest.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
 set -euo pipefail
+set -x
 
 components=$(jq .ContainerImages[] --monochrome-output --compact-output < parts/linux/cloud-init/artifacts/components.json)
-for component in ${components[*]}; do
-	downloadURL=$(echo ${component} | jq .downloadURL)
+for component in "${components[*]}"; do
+	downloadURL=$(echo "${component}" | jq .downloadURL)
 	downloadURL=$(echo ${downloadURL//\*/} | jq 'sub(".com/" ; ".com/v2/") | sub(":" ; "/tags/list")' -r)
 	amd64OnlyVersionsStr=$(echo "${component}" | jq .amd64OnlyVersions -r)
-	multiArchVersionsStr=$(echo "${component}" | jq .multiArchVersions -r)
 	amd64OnlyVersions=""
 	if [[ ${amd64OnlyVersionsStr} != null ]]; then
 		amd64OnlyVersions=$(echo "${amd64OnlyVersionsStr}" | jq -r ".[]")
 	fi
-	multiArchVersions=""
-	if [[ ${multiArchVersionsStr} != null ]]; then
-		multiArchVersions=$(echo "${multiArchVersionsStr}" | jq -r ".[]")
-	fi
+	latestVersions=($(echo "${component}" | jq -r ".multiArchVersionsV2[] | select(.latestVersion != null) | .latestVersion"))
+    previousLatestVersions=($(echo "${component}" | jq -r ".multiArchVersionsV2[] | select(.previousLatestVersion != null) | .previousLatestVersion"))
+    for version in "${latestVersions[@]}"; do
+      multiArchVersionsV2+=("${version}")
+    done
+    for version in "${previousLatestVersions[@]}"; do
+      multiArchVersionsV2+=("${version}")
+    done
+
 	arch=$(uname -m)
 	if [[ ${arch,,} == "aarch64" || ${arch,,} == "arm64"  ]]; then
-		versionsToBeDownloaded="${multiArchVersions}"
+		versionsToBeDownloaded="${multiArchVersionsV2}"
 	else
-		versionsToBeDownloaded="${amd64OnlyVersions} ${multiArchVersions}"
+		versionsToBeDownloaded="${amd64OnlyVersions} ${multiArchVersionsV2}"
 	fi
 
 	validVersions=$(curl -sL https://$downloadURL | jq .tags[])
