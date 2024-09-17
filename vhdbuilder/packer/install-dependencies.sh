3a0136698ef2f7078fd2865a241e3ff5f0f4d2e0
Alison Burgess <alburgess@microsoft.com>
2024-09-17 15:53:16 -0700

    add quotes

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -295,7 +295,7 @@ while IFS= read -r p; do
       #  downloadContainerdWasmShims "${downloadDir}" "${evaluatedURL}" "${version}"
       #  echo "  - containerd-wasm-shims version ${version}" >> ${VHD_LOGS_FILEPATH}
       #done
-      installingContainerdWasmShims "${downloadDir}" "${evaluatedURL}" ${PACKAGE_VERSIONS[@]}
+      installingContainerdWasmShims "${downloadDir}" "${evaluatedURL}" "${PACKAGE_VERSIONS[@]}"
       echo "  - containerd-wasm-shims version ${PACKAGE_VERSIONS[@]}" >> ${VHD_LOGS_FILEPATH}
       ;;
     "kubernetes-binaries")
