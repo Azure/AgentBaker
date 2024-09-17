6ee8428d0abf98e45140756c566f33d7458675d0
Tim Wright <wrightt@microsoft.com>
2024-09-18 10:10:16 +1200

    add way to skip hns setup

--- pkg/agent/variables.go
+++ pkg/agent/variables.go
@@ -95,6 +95,7 @@ func getWindowsCustomDataVariables(config *datamodel.NodeBootstrappingConfigurat
 		"primaryAvailabilitySetName":           cs.Properties.GetPrimaryAvailabilitySetName(),
 		"primaryScaleSetName":                  config.PrimaryScaleSetName,
 		"useManagedIdentityExtension":          useManagedIdentity(cs),
+		"skipExternalHnsNetwork":               skipExternalHnsNetwork(config),
 		"useInstanceMetadata":                  useInstanceMetadata(cs),
 		"loadBalancerSku":                      cs.Properties.OrchestratorProfile.KubernetesConfig.LoadBalancerSku,
 		"excludeMasterFromStandardLB":          true,
@@ -152,6 +153,17 @@ func useManagedIdentity(cs *datamodel.ContainerService) string {
 	useManagedIdentity := cs.Properties.OrchestratorProfile.KubernetesConfig != nil &&
 		cs.Properties.OrchestratorProfile.KubernetesConfig.UseManagedIdentity
 	return strconv.FormatBool(useManagedIdentity)
+}
+
+func skipExternalHnsNetwork(config *datamodel.NodeBootstrappingConfiguration) string {
+	skipExternalHnsNetwork := config.ContainerService.Properties.OrchestratorProfile.KubernetesConfig != nil &&
+		config.ContainerService.Properties.OrchestratorProfile.KubernetesConfig.SkipExternalHnsNetwork
+
+	if skipExternalHnsNetwork {
+		return "$true"
+	} else {
+		return "$false"
+	}
 }
 
 func useInstanceMetadata(cs *datamodel.ContainerService) string {
