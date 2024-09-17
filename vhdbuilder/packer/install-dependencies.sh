5b15b4e6f75f0989152038f6778a6d153d69f415
Alison Burgess <alburgess@microsoft.com>
2024-09-17 16:35:22 -0700

    more debugging

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -295,7 +295,10 @@ while IFS= read -r p; do
       #  downloadContainerdWasmShims "${downloadDir}" "${evaluatedURL}" "${version}"
       #  echo "  - containerd-wasm-shims version ${version}" >> ${VHD_LOGS_FILEPATH}
       #done
+      echo "Debug: Original URL before evaluation: ${PACKAGE_DOWNLOAD_URL}"
       evaluatedURL=$(evalPackageDownloadURL ${PACKAGE_DOWNLOAD_URL})
+      echo "Debug: Evaluated URL: ${evaluatedURL}"
+      echo "inside wasm-shims evaluatedURL: ${evaluatedURL} PACKAGE_VERSIONS: ${PACKAGE_VERSIONS[@]}"
       installingContainerdWasmShims "${downloadDir}" "${evaluatedURL}" "${PACKAGE_VERSIONS[@]}"
       echo "  - containerd-wasm-shims version ${PACKAGE_VERSIONS[@]}" >> ${VHD_LOGS_FILEPATH}
       ;;
