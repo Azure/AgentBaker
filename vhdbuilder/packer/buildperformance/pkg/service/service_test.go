941ec54c7b5138a3ab8971b4be7362c50cab9a6b
Zachary Bailey <zbailey@microsoft.com>
2024-09-20 13:24:42 -0700

    fix: align to typed maps in testing to stay consistent with program code

--- vhdbuilder/packer/buildperformance/pkg/service/service_test.go
+++ vhdbuilder/packer/buildperformance/pkg/service/service_test.go
@@ -67,9 +67,9 @@ func TestCreateDataMaps(t *testing.T) {
 		{
 			name: "should correctly return a struct of type DataMaps",
 			expected: &DataMaps{
-				LocalPerformanceDataMap:   map[string]map[string]float64{},
-				QueriedPerformanceDataMap: map[string]map[string][]float64{},
-				RegressionMap:             map[string]map[string]float64{},
+				LocalPerformanceDataMap:   EvaluationMap{},
+				QueriedPerformanceDataMap: QueryMap{},
+				RegressionMap:             EvaluationMap{},
 			},
 		},
 	}
@@ -86,14 +86,14 @@ func TestDecodeLocalPerformanceData(t *testing.T) {
 	cases := []struct {
 		name       string
 		mapsStruct *DataMaps
-		expected   map[string]map[string]float64
+		expected   EvaluationMap
 	}{
 		{
 			name: "should correctly unmarshal local JSON file into appropriate field in DataMaps struct",
 			mapsStruct: &DataMaps{
-				LocalPerformanceDataMap: map[string]map[string]float64{},
+				LocalPerformanceDataMap: EvaluationMap{},
 			},
-			expected: map[string]map[string]float64{
+			expected: EvaluationMap{
 				"pre_install_dependencies": {
 					"copy_packer_files":                                                 65,
 					"enable_modified_log_rotate_service":                                65,
@@ -156,7 +156,7 @@ func TestParseKustoData(t *testing.T) {
 		name       string
 		queriedSKU *SKU
 		mapsStruct *DataMaps
-		expected   map[string]map[string][]float64
+		expected   QueryMap
 		err        error
 	}{
 		{
@@ -166,9 +166,9 @@ func TestParseKustoData(t *testing.T) {
 				SKUPerformanceData: `{"post_install_dependencies": {"resolve_conf": [15.0, 10.0], "post_install_dependencies_overall": [30.0, 20], "set_variables_and_source_packer_files": [30.0, 20], "log_and_detach_ua": [30.0, 20], "determine_disk_usage": [30.0, 20], "install_asc_baseline": [30.0, 20], "write_logs": [30.0, 20], "list_installed_packages": [30.0, 20]}, "pre_install_dependencies": {"copy_packer_files": [30.0, 20], "sync_container_logs": [30.0, 20], "handle_mariner_and_fips_configurations": [30.0, 20], "handle_azureLinux_and_cgroupV2": [30.0, 20], "enable_modified_log_rotate_service": [15.0, 10.0], "make_directory_and_update_certs": [30.0, NaN], "start_system_logs_and_aks_log_collector": [30.0, 20], "pre_install_dependencies_overall": [30.0, 20], "source_packer_files_declare_variables_and_set_mariner_permissions": [30.0, 20]}, "install_dependencies": {"download_runc": [30.0, 20], "download_azure_acr_credential_provider": [30.0, 20], "create_containerd_service_directory_download_shims_configure_runtime_and_network": [30.0, 20], "configure_telemetry_create_logging_directory": [30.0, 20], "pull_nvidia_driver_image_and_run_installBcc_in_subshell": [30.0, 20], "download_azure_cni": [15.0, 10.0], "download_gpu_device_plugin": [30.0, 20], "download_kubernetes_binaries": [30.0, 20], "declare_variables_and_source_packer_files": [30.0, 20], "download_containerd": [30.0, 20], "artifact_streaming_and_download_teleportd": [30.0, 20], "configure_networking_and_interface": [15.0, 10.0], "finish_installing_bcc_tools": [30.0, 20], "download_cri_tools": [30.0, 20], "purge_and_reinstall_ubuntu": [30.0, 20], "pull_and_retag_container_images": [30.0, 20], "install_dependencies_overall": [30.0, 20], "check_container_runtime_and_network_configurations": [30.0, 20], "download_oras": [30.0, 20], "install_dependencies": [30.0, 20], "download_cni_plugins": [30.0, 20]}}`,
 			},
 			mapsStruct: &DataMaps{
-				QueriedPerformanceDataMap: map[string]map[string][]float64{},
+				QueriedPerformanceDataMap: QueryMap{},
 			},
-			expected: map[string]map[string][]float64{
+			expected: QueryMap{
 				"pre_install_dependencies": {
 					"copy_packer_files":                                                 []float64{30.0, 20},
 					"sync_container_logs":                                               []float64{30.0, 20},
@@ -275,7 +275,7 @@ func TestEvaluatePerformance(t *testing.T) {
 		{
 			name: "should correctly identify regressions",
 			initialMaps: &DataMaps{
-				QueriedPerformanceDataMap: map[string]map[string][]float64{
+				QueriedPerformanceDataMap: QueryMap{
 					"pre_install_dependencies": {
 						"copy_packer_files":                                                 []float64{30.0, 20},
 						"sync_container_logs":                                               []float64{30.0, 20},
@@ -321,7 +321,7 @@ func TestEvaluatePerformance(t *testing.T) {
 						"list_installed_packages":               []float64{30.0, 20},
 					},
 				},
-				LocalPerformanceDataMap: map[string]map[string]float64{
+				LocalPerformanceDataMap: EvaluationMap{
 					"pre_install_dependencies": {
 						"copy_packer_files":                                                 65,
 						"enable_modified_log_rotate_service":                                65,
@@ -367,10 +367,10 @@ func TestEvaluatePerformance(t *testing.T) {
 						"write_logs":                            65,
 					},
 				},
-				RegressionMap: map[string]map[string]float64{},
+				RegressionMap: EvaluationMap{},
 			},
 			expected: &DataMaps{
-				RegressionMap: map[string]map[string]float64{
+				RegressionMap: EvaluationMap{
 					"pre_install_dependencies": {
 						"enable_modified_log_rotate_service": 30,
 					},
@@ -405,7 +405,7 @@ func TestDisplayRegressions(t *testing.T) {
 		{
 			name: "should correctly unmarshal regression map into JSON",
 			regressionmap: &DataMaps{
-				RegressionMap: map[string]map[string]float64{
+				RegressionMap: EvaluationMap{
 					"pre_install_dependencies": {
 						"enable_modified_log_rotate_service": 30,
 					},
