fae7f709e88dccd7ff8b4e38926dbe5df140dd11
Lily Pan <lilypan@microsoft.com>
2024-09-27 14:47:57 -0700

    exit 0 if nbc.json not present

--- parts/linux/cloud-init/artifacts/nbcparser.sh
+++ parts/linux/cloud-init/artifacts/nbcparser.sh
@@ -7,7 +7,7 @@ if [ $? -ne 0 ] ; then
 fi
 echo "cloud-init succeeded";
 if [ ! -f /opt/azure/containers/nbc.json ]; then 
-    exit 1
+    exit 0
 fi
 /opt/azure/containers/nbcparser /opt/azure/containers/nbc.json > cse_cmd.sh
 chmod +x cse_cmd.sh
