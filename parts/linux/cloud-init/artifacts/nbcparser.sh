d78d76e4347ae9b57d742e5e6a8da5c8d1d7e260
Lily Pan <lilypan@microsoft.com>
2024-09-27 14:07:26 -0700

    add cloud init wait to nbcparser.sh

--- parts/linux/cloud-init/artifacts/nbcparser.sh
+++ parts/linux/cloud-init/artifacts/nbcparser.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -x
 
+cloud-init status --wait > /dev/null 2>&1
+if [ $? -ne 0 ] ; then
+    exit 1
+fi
+echo "cloud-init succeeded";
+if [ ! -f /opt/azure/containers/nbc.json ]; then 
+    exit 1
+fi
 /opt/azure/containers/nbcparser /opt/azure/containers/nbc.json > cse_cmd.sh
 chmod +x cse_cmd.sh
 ./cse_cmd.sh
