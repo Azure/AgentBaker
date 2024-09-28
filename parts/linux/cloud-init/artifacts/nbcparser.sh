f3be2c3d17c3d8b2071e003f0b9a637a21e1e8f7
Lily Pan <lilypan@microsoft.com>
2024-09-27 17:01:56 -0700

    add back systemd wait conditions

--- parts/linux/cloud-init/artifacts/nbcparser.sh
+++ parts/linux/cloud-init/artifacts/nbcparser.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -x
-
-cloud-init status --wait > /dev/null 2>&1
-if [ $? -ne 0 ] ; then
-    exit 1
-fi
-echo "cloud-init succeeded";
 if [ ! -f /opt/azure/containers/nbc.json ]; then 
     exit 0
 fi
