b1a9e025a64c974a68ba195acd3d65cd898f878b
Junjie Zhang <junjiezhang@microsoft.com>
2024-09-29 03:50:25 +0000

    fix: upload new linux file when it's outdated after autoupgrade

--- e2e/windows/e2e-starter.sh
+++ e2e/windows/e2e-starter.sh
@@ -116,10 +116,11 @@ if [ "$create_cluster" == "true" ]; then
 else
     if [[ "$(check_linux_file_exists_in_storage_account)" == *"Linux file does not exist in storage account."* ]]; then 
         upload_linux_file_to_storage_account
-    elif [[  ]]
-        
+    else
+        check_linux_file_outdated
     fi
 fi
+
 download_linux_file_from_storage_account
 log "Download of linux file from storage account completed"
 
