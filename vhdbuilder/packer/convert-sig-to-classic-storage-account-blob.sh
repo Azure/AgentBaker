417d9ba90a16906130fb2f3929495c0921829f04
Zachary Bailey <zbailey@microsoft.com>
2024-10-02 14:46:50 -0700

    fix: msi resource id

--- vhdbuilder/packer/convert-sig-to-classic-storage-account-blob.sh
+++ vhdbuilder/packer/convert-sig-to-classic-storage-account-blob.sh
@@ -85,14 +85,14 @@ sas=$(az disk grant-access --ids $disk_resource_id --duration-in-seconds 3600 --
 
 echo "Uploading $disk_resource_id to ${CLASSIC_BLOB}/${CAPTURED_SIG_VERSION}.vhd"
 
+echo "Setting azcopy environment variables with pool identity: $AZURE_MSI_RESOURCE_STRING"
+# TBD: Need to investigate why `azcopy-preview login --login-type=MSI` does not work
+export AZCOPY_AUTO_LOGIN_TYPE="MSI"
+export AZCOPY_MSI_RESOURCE_STRING="$AZURE_MSI_RESOURCE_STRING"
+
 if [[ "${OS_TYPE}" == "Linux" ]]; then
-  azcopy login --login-type=MSI
   azcopy copy "${sas}" "${CLASSIC_BLOB}/${CAPTURED_SIG_VERSION}.vhd" --recursive=true || exit $?
 else
-  echo "Setting azcopy environment variables with pool identity: $AZURE_MSI_RESOURCE_STRING"
-  # TBD: Need to investigate why `azcopy-preview login --login-type=MSI` does not work
-  export AZCOPY_AUTO_LOGIN_TYPE="MSI"
-  export AZCOPY_MSI_RESOURCE_STRING="$AZURE_MSI_RESOURCE_STRING"
   azcopy-preview copy "${sas}" "${CLASSIC_BLOB}/${CAPTURED_SIG_VERSION}.vhd" --recursive=true || exit $?
 fi
 
