57f990321a873801f72e34c4d4190b34ee5fb774
Zachary Bailey <zbailey@microsoft.com>
2024-10-03 14:24:11 -0700

    chore: make generate

--- pkg/agent/testdata/AKSUbuntu2204+Containerd+MIG+ArtifactStreaming/line9.sh
+++ pkg/agent/testdata/AKSUbuntu2204+Containerd+MIG+ArtifactStreaming/line9.sh
@@ -145,6 +145,8 @@ CURL_OUTPUT=/tmp/curl_verbose.out
 ORAS_OUTPUT=/tmp/oras_verbose.out
 ORAS_REGISTRY_CONFIG_FILE=/etc/oras/config.yaml 
 
+source vhdbuilder/packer/build-performance/build-performance-funcs.sh
+
 retrycmd_if_failure() {
     retries=$1; wait_sleep=$2; timeout=$3; shift && shift && shift
     for i in $(seq 1 $retries); do
@@ -467,54 +469,6 @@ installJq() {
       apt_get_install 5 1 60 jq && echo "jq was installed: $(jq --version)"
     fi
   fi
-}
-
-check_array_size() {
-  declare -n array_name=$1
-  local array_size=${#array_name[@]}
-  if [[ ${array_size} -gt 0 ]]; then
-    last_index=$(( ${#array_name[@]} - 1 ))
-  else
-    return 1
-  fi
-}
-
-capture_benchmark() {
-  set +x
-  local title="$1"
-  title="${title//[[:space:]]/_}"
-  title="${title//-/_}"
-  local is_final_section=${2:-false}
-
-  local current_time=$(date +%s)
-  if [[ "$is_final_section" == true ]]; then
-    local start_time=$script_start_stopwatch
-  else
-    local start_time=$section_start_stopwatch
-  fi
-  
-  total_time_elapsed=$(date -d@$((current_time - start_time)) -u +%H:%M:%S)
-  benchmarks[$title]=${total_time_elapsed}
-  benchmarks_order+=($title) 
-
-  section_start_stopwatch=$(date +%s)
-}
-
-process_benchmarks() {
-  set +x
-  check_array_size benchmarks || { echo "Benchmarks array is empty"; return; }
-  script_object=$(jq -n --arg script_name "${SCRIPT_NAME}" '{($script_name): {}}')
-
-  for ((i=0; i<${#benchmarks_order[@]}; i+=1)); do
-    section_name=${benchmarks_order[i]}
-    section_object=$(jq -n --arg section_name "${section_name}" --arg total_time_elapsed "${benchmarks[${section_name}]}" \
-    '{($section_name): $total_time_elapsed'})
-    script_object=$(jq -n --argjson script_object "$script_object" --argjson section_object "$section_object" --arg script_name "${SCRIPT_NAME}" \
-    '$script_object | .[$script_name] += $section_object')
-  done
- 
-  jq ". += $script_object" ${VHD_BUILD_PERF_DATA} > temp-build-perf-file.json && mv temp-build-perf-file.json ${VHD_BUILD_PERF_DATA}
-  chmod 755 ${VHD_BUILD_PERF_DATA}
 }
 
 evalPackageDownloadURL() {
