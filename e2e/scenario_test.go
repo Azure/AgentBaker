67608382abe4e0d0675f24397db577ccea3cceb3
Mikolaj Umanski <mumanski@microsoft.com>
2024-10-03 15:01:01 -0700

    fix: correcly set version for 1804 containerd check

--- e2e/scenario_test.go
+++ e2e/scenario_test.go
@@ -2,6 +2,7 @@ package e2e
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/Azure/agentbaker/pkg/agent/datamodel"
@@ -514,13 +515,15 @@ func Test_marinerv2Wasm(t *testing.T) {
 
 // Returns config for the 'base' E2E scenario
 func Test_ubuntu1804(t *testing.T) {
+	// for ubuntu1804 containerd version is frozen and its using outdated versioning style, hence this modification
+	expected1804ContainredVersion := strings.Replace(getExpectedPackageVersions("containerd", "ubuntu", "r1804")[0], "-", "+azure-ubuntu18.04u", 1)
 	RunScenario(t, &Scenario{
 		Description: "Tests that a node using an Ubuntu 1804 VHD can be properly bootstrapped",
 		Config: Config{
 			Cluster: ClusterKubenet,
 			VHD:     config.VHDUbuntu1804Gen2Containerd,
 			LiveVMValidators: []*LiveVMValidator{
-				mobyComponentVersionValidator("containerd", getExpectedPackageVersions("containerd", "ubuntu", "r1804")[0], "apt"),
+				mobyComponentVersionValidator("containerd", expected1804ContainredVersion, "apt"),
 				mobyComponentVersionValidator("runc", getExpectedPackageVersions("runc", "ubuntu", "r1804")[0], "apt"),
 			},
 		},
