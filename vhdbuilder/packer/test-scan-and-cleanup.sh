f95c0cf41828143a4946d1cdeedf3acf433de932
Zachary Bailey <zbailey@microsoft.com>
2024-10-04 10:13:35 -0700

    fix: source performance funcs

--- vhdbuilder/packer/test-scan-and-cleanup.sh
+++ vhdbuilder/packer/test-scan-and-cleanup.sh
@@ -21,7 +21,7 @@ for v in "${required_env_vars[@]}"; do
   fi
 done
 
-source ../../parts/linux/cloud-init/artifacts/cse_benchmark_functions.sh
+source /parts/linux/cloud-init/artifacts/cse_benchmark_functions.sh
 
 retrycmd_if_failure() {
   RETRIES=${1}; WAIT_SLEEP=${2}; CMD=${3}; TARGET=$(basename ${3} .sh)
