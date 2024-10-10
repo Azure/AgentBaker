87388e08a3d79352305e6ac90b129c8e5e4a2c7d
Zachary Bailey <zbailey@microsoft.com>
2024-10-10 09:58:24 -0700

    fix: dont use jq

--- parts/linux/cloud-init/artifacts/cse_main.sh
+++ parts/linux/cloud-init/artifacts/cse_main.sh
@@ -474,7 +474,7 @@ mkdir -p /opt/azure/containers && touch /opt/azure/containers/provision.complete
 
 capture_benchmark "${SCRIPT_NAME}_overall" true
 process_benchmarks
-cat ${PERFORMANCE_DATA_FILE} | jq .
+cat ${PERFORMANCE_DATA_FILE}
 SIZE=$(stat -c %s ${PERFORMANCE_DATA_FILE})
 echo "Size of perf data file: ${SIZE}"
 
