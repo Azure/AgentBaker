c762f6f9a149fe6698a2a543b3b9a5e46efdab5b
Zachary Bailey <zbailey@microsoft.com>
2024-10-10 14:11:06 -0700

    test: test ingestion

--- vhdbuilder/packer/buildperformance/main.go
+++ vhdbuilder/packer/buildperformance/main.go
@@ -24,7 +24,7 @@ func main() {
 	}
 	log.Println("Program config set")
 
-	maps := service.CreateDataMaps()
+	//maps := service.CreateDataMaps()
 
 	client, err := service.CreateKustoClient(config.KustoEndpoint, config.KustoClientId)
 	if err != nil {
@@ -43,17 +43,19 @@ func main() {
 		log.Printf("Data ingested for %s\n", config.SigImageName)
 	}
 
-	queryData, err := service.QueryData(ctx, client, config.SigImageName, config.KustoDatabase)
-	if err != nil {
-		panic(err)
-	}
-	log.Printf("Queried aggregated performance data for %s\n", config.SigImageName)
+	/*
+		queryData, err := service.QueryData(ctx, client, config.SigImageName, config.KustoDatabase)
+		if err != nil {
+			panic(err)
+		}
+		log.Printf("Queried aggregated performance data for %s\n", config.SigImageName)
 
-	if err = maps.PreparePerformanceDataForEvaluation(config.LocalBuildPerformanceFile, queryData); err != nil {
-		panic(err)
-	}
+		if err = maps.PreparePerformanceDataForEvaluation(config.LocalBuildPerformanceFile, queryData); err != nil {
+			panic(err)
+		}
 
-	if err = maps.EvaluatePerformance(); err != nil {
-		panic(err)
-	}
+		if err = maps.EvaluatePerformance(); err != nil {
+			panic(err)
+		}
+	*/
 }
