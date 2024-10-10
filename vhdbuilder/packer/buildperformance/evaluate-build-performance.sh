5e2b7f38e80d09ca55e5308cbb2ce1792bf3d47a
Zachary Bailey <zbailey@microsoft.com>
2024-10-10 13:45:37 -0700

    chore: add captured_sig_version

--- vhdbuilder/packer/buildperformance/evaluate-build-performance.sh
+++ vhdbuilder/packer/buildperformance/evaluate-build-performance.sh
@@ -29,19 +29,20 @@ echo -e "\nGenerating build performance data for ${SIG_IMAGE_NAME}...\n"
 
 jq --arg sig "${SIG_IMAGE_NAME}" \
   --arg arch "${ARCHITECTURE}" \
+  --arg captured_sig_version "${CAPTURED_SIG_VERSION}" \
   --arg build_id "${BUILD_ID}" \
   --arg date "$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
   --arg status "${JOB_STATUS}" \
   --arg branch "${GIT_BRANCH}" \
   --arg commit "${GIT_VERSION}" \
-  '{sig_image_name: $sig, architecture: $arch, build_id: $build_id, build_datetime: $date,
+  '{sig_image_name: $sig, architecture: $arch, captured_sig_version: $captured_sig_version, build_id: $build_id, build_datetime: $date,
   build_status: $status, branch: $branch, commit: $commit, scripts: .}' \
   ${PERFORMANCE_DATA_FILE} > ${SIG_IMAGE_NAME}-build-performance.json
 
 rm ${PERFORMANCE_DATA_FILE}
 
 echo "##[group]Build Information"
-jq -C '. | {sig_image_name, architecture, build_id, build_datetime, build_status, branch, commit}' ${SIG_IMAGE_NAME}-build-performance.json
+jq -C '. | {sig_image_name, architecture, captured_sig_version, build_id, build_datetime, build_status, branch, commit}' ${SIG_IMAGE_NAME}-build-performance.json
 echo "##[endgroup]"
 
 scripts=()
