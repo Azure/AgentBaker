b87dc28510b9a1126c76b95e9eac8053938f534a
Zachary Bailey <zbailey@microsoft.com>
2024-10-10 07:53:43 -0700

    fix: change dir

--- parts/linux/cloud-init/artifacts/cse_helpers.sh
+++ parts/linux/cloud-init/artifacts/cse_helpers.sh
@@ -159,7 +159,7 @@ EVENTS_LOGGING_DIR=/var/log/azure/Microsoft.Azure.Extensions.CustomScript/events
 CURL_OUTPUT=/tmp/curl_verbose.out
 ORAS_OUTPUT=/tmp/oras_verbose.out
 ORAS_REGISTRY_CONFIG_FILE=/etc/oras/config.yaml # oras registry auth config file, not used, but have to define to avoid error "Error: failed to get user home directory: $HOME is not defined"
-PERFORMANCE_DATA_FILE="/opt/azure/containers/cse-performance.json"
+PERFORMANCE_DATA_FILE="/var/log/azure/cse-performance.json"
 echo '{}' > ${PERFORMANCE_DATA_FILE} 
 
 retrycmd_if_failure() {
