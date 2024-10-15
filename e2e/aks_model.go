1effb4f8db586882f6b710b70a580e2062a4329e
Alison Burgess <alburgess@microsoft.com>
2024-10-15 15:56:44 -0700

    turn anon pull true

--- e2e/aks_model.go
+++ e2e/aks_model.go
@@ -212,7 +212,7 @@ func createPrivateAzureContainerRegistry(ctx context.Context, t *testing.T, node
 	params := &armcontainerregistry.RegistryUpdateParameters{
 		Properties: &armcontainerregistry.RegistryPropertiesUpdateParameters{
 			AdminUserEnabled:     to.Ptr(false),
-			AnonymousPullEnabled: to.Ptr(false),
+			AnonymousPullEnabled: to.Ptr(true), // required to pull images from the private ACR
 		},
 	}
 	pollerResp, err := config.Azure.RegistriesClient.BeginUpdate(
