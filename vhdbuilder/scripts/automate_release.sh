6b65b511524ec6e41fc18ab1c672c963eb89c2b2
Cameron Meissner <cameissner@microsoft.com>
2024-10-15 15:45:17 -0700

    chore: debug

--- vhdbuilder/scripts/automate_release.sh
+++ vhdbuilder/scripts/automate_release.sh
@@ -51,12 +51,12 @@ create_release() {
     echo "creating SIG release for VHD with build ID: $VHD_BUILD_ID"
     EV2_BUILD_ID=105825377
     EV2_BUILD_NUMBER=20241015.8
-    ADO_PAT=asdfasdfasdfasdf
+    # ADO_PAT=asdfasdfasdfasdf
 
     # we do it this way for now since the devops CLI extension doesn't support what we need: https://github.com/Azure/azure-devops-cli-extension/issues/1257
     echo "sending POST request to $RELEASE_API_ENDPOINT"
     REQUEST_BODY="{'artifacts': [{'alias': '$RELEASE_EV2_ARTIFACTS_ALIAS_NAME', 'instanceReference': {'id': '$EV2_BUILD_ID', 'name': '$EV2_BUILD_NUMBER'}}], 'definitionId': $SIG_RELEASE_PIPELINE_ID}"
-    RESPONSE=$(curl -v --http1.1 -X POST -H "Authorization: Basic $(echo -n ":$ADO_PAT" | base64)" -H "Content-Type: application/json" -d "$REQUEST_BODY" "$RELEASE_API_ENDPOINT")
+    RESPONSE=$(curl --http1.1 -X POST -H "Authorization: Basic $(echo -n ":$ADO_PAT" | base64)" -H "Content-Type: application/json" -d "$REQUEST_BODY" "$RELEASE_API_ENDPOINT" | grep -v "Authorization: Basic")
     
     if [ $? -ne 0 ]; then
         echo "failed to create release, received response:"
