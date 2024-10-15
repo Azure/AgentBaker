3e0a6362317513a08fdb1194cf8116da611a46b7
Zachary Bailey <zbailey@microsoft.com>
2024-10-15 09:02:22 -0700

    fix: add types

---
+++ vhdbuilder/packer/buildperformance/pkg/service/types.go
@@ -0,0 +1,34 @@
+package service
+
+type Config struct {
+	KustoTable                string
+	KustoEndpoint             string
+	KustoDatabase             string
+	KustoClientId             string
+	KustoIngestionMapping     string
+	SigImageName              string
+	LocalBuildPerformanceFile string
+	SourceBranch              string
+}
+
+type SKU struct {
+	Name               string `kusto:"SIG_IMAGE_NAME"`
+	SKUPerformanceData string `kusto:"BUILD_PERFORMANCE"`
+}
+
+type QueryCompletionInfo struct {
+	Payload []map[string]int `json:"dataset_statistics"`
+}
+
+type DataMaps struct {
+	LocalPerformanceDataMap   EvaluationMap
+	QueriedPerformanceDataMap QueryMap
+	RegressionMap             EvaluationMap
+	StagingMap                StagingMap `json:"scripts"`
+}
+
+type StagingMap map[string]map[string]string
+
+type QueryMap map[string]map[string][]float64
+
+type EvaluationMap map[string]map[string]float64
