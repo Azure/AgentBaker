0c3b9b3e88206a1c187e1f739ba824cc777a63ad
Zachary Bailey <zbailey@microsoft.com>
2024-10-15 09:01:37 -0700

    fix: new branch

---
+++ vhdbuilder/packer/buildperformance/pkg/service/kusto_service.go
@@ -0,0 +1,108 @@
+package service
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/Azure/azure-kusto-go/azkustodata"
+	"github.com/Azure/azure-kusto-go/azkustodata/kql"
+	"github.com/Azure/azure-kusto-go/azkustoingest"
+)
+
+func CreateKustoClient(kustoEndpoint string, kustoClientId string) (*azkustodata.Client, error) {
+	kustoConnectionString := azkustodata.NewConnectionStringBuilder(kustoEndpoint).WithUserManagedIdentity(kustoClientId)
+	client, err := azkustodata.New(kustoConnectionString)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create kusto client: %w", err)
+	}
+	return client, nil
+}
+
+func IngestData(ctx context.Context, config *Config) error {
+	if config.SourceBranch == "refs/heads/zb/buildPerfRegression5" {
+		kustoConnectionString := azkustodata.NewConnectionStringBuilder(config.KustoEndpoint).WithUserManagedIdentity(config.KustoClientId)
+
+		ingestor, err := azkustoingest.New(kustoConnectionString, config.KustoDatabase, config.KustoTable)
+		if err != nil {
+			return fmt.Errorf("failed to create ingestor: %w", err)
+		}
+		defer ingestor.Close()
+
+		if _, err = ingestor.FromFile(ctx, config.LocalBuildPerformanceFile, azkustoingest.IngestionMappingRef(config.KustoIngestionMapping, azkustoingest.MultiJSON)); err != nil {
+			return fmt.Errorf("failed to ingest build performance data: %w", err)
+		}
+		log.Printf("Data ingested for %s\n", config.SigImageName)
+		return nil
+	}
+	log.Println("Data not ingested as source branch is not master")
+	return nil
+}
+
+func QueryData(ctx context.Context, config *Config) (*SKU, error) {
+	client, err := CreateKustoClient(config.KustoEndpoint, config.KustoClientId)
+	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
+	log.Println("Kusto query client created")
+
+	query := kql.New("Get_Performance_Data | where SIG_IMAGE_NAME == SKU")
+	params := kql.NewParameters().AddString("SKU", config.SigImageName)
+
+	dataset, err := client.Query(ctx, config.KustoDatabase, query, azkustodata.QueryParameters(params))
+	if err != nil {
+		return nil, fmt.Errorf("failed to query kusto database: %w", err)
+	}
+
+	data, err := query.ToStructs[SKU](dataset)
+	if err != nil {
+		return nil, fmt.Errorf("failed to persist query data: %w", err)
+	}
+
+	numRows := data[0].Rows()
+	fmt.Println("Number of rows returned from query: %d", numRows)
+
+	//if err := CheckNumberOfRowsReturned(iter); err != nil {
+	//return nil, err
+	//}
+
+	log.Printf("Queried aggregated performance data for %s\n", config.SigImageName)
+	log.Println("Query returned 1 row of aggregated data as expected")
+
+	return &data[0], nil
+}
+
+func CheckNumberOfRowsReturned(iter *azkustodata.RowIterator) error {
+	// GetQueryCompletionInformation returns a datatable with information about the query
+	infoTable, err := iter.GetQueryCompletionInformation()
+	if err != nil {
+		return fmt.Errorf("unable to get query completion information: %v", err)
+	}
+
+	if len(infoTable.KustoRows) == 0 {
+		return fmt.Errorf("query completion information is empty")
+	}
+
+	// Custom struct to unmarshal the query completion information
+	QueryInformation := QueryCompletionInfo{}
+
+	// The number of rows returned by the query is stored in the last element of a slice in the last row in the datatable returned by GetQueryCompletionInformation
+	row := infoTable.KustoRows[len(infoTable.KustoRows)-1]
+	payload := row[len(row)-1].String()
+
+	if err = json.Unmarshal([]byte(payload), &QueryInformation); err != nil {
+		return fmt.Errorf("could not unmarshal query completion information: %v", err)
+	}
+
+	numRows := QueryInformation.Payload[0]["table_row_count"]
+
+	if numRows != 1 {
+		return fmt.Errorf("unexpected number of rows returned from query: %v", numRows)
+	}
+
+	log.Println("Query returned 1 row of aggregated data as expected")
+
+	return nil
+}
---
+++ vhdbuilder/packer/buildperformance/pkg/service/service.go
@@ -0,0 +1,183 @@
+package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+// Set conditions for program to run successfully wtih SetupConfig and CreateDataMaps functions
+func SetupConfig() (*Config, error) {
+	envVars := map[string]string{
+		"kustoTable":            os.Getenv("BUILD_PERFORMANCE_TABLE_NAME"),
+		"kustoEndpoint":         os.Getenv("BUILD_PERFORMANCE_KUSTO_ENDPOINT"),
+		"kustoDatabase":         os.Getenv("BUILD_PERFORMANCE_DATABASE_NAME"),
+		"kustoClientId":         os.Getenv("BUILD_PERFORMANCE_CLIENT_ID"),
+		"kustoIngestionMapping": os.Getenv("BUILD_PERFORMANCE_INGESTION_MAPPING"),
+		"sourceBranch":          os.Getenv("GIT_BRANCH"),
+		"sigImageName":          os.Getenv("SIG_IMAGE_NAME"),
+	}
+	missingVar := false
+	for name, value := range envVars {
+		if value == "" {
+			log.Printf("missing environment variable %q.", name)
+			missingVar = true
+		}
+	}
+	if missingVar {
+		return nil, fmt.Errorf("required environment variables were not set")
+	}
+
+	log.Println("Program config set")
+
+	return &Config{
+		KustoTable:                envVars["kustoTable"],
+		KustoEndpoint:             envVars["kustoEndpoint"],
+		KustoDatabase:             envVars["kustoDatabase"],
+		KustoClientId:             envVars["kustoClientId"],
+		KustoIngestionMapping:     envVars["kustoIngestionMapping"],
+		SigImageName:              envVars["sigImageName"],
+		LocalBuildPerformanceFile: envVars["sigImageName"] + "-build-performance.json",
+		SourceBranch:              envVars["sourceBranch"],
+	}, nil
+}
+
+func CreateDataMaps() *DataMaps {
+	return &DataMaps{
+		// LocalPerformanceDataMap will hold the performance data from the local JSON file
+		LocalPerformanceDataMap: EvaluationMap{},
+		// QueriedPerformanceDataMap will hold the aggregated performance data queried from Kusto
+		QueriedPerformanceDataMap: QueryMap{},
+		// RegressionMap will hold all identified regressions in the current build
+		RegressionMap: EvaluationMap{},
+		// StagingMap is a temporary holding map that will hold the data from the local JSON file before it is converted to floats
+		StagingMap: StagingMap{},
+	}
+}
+
+// Prepare performance data for evaluation with PreparePerformanceDataForEvaluation and associated helper functions
+func (maps *DataMaps) PreparePerformanceDataForEvaluation(localBuildPerformanceFile string, queriedData *SKU) error {
+	if err := maps.DecodeLocalPerformanceData(localBuildPerformanceFile); err != nil {
+		return fmt.Errorf("error decoding local performance data: %w", err)
+	}
+	if err := maps.ParseKustoData(queriedData); err != nil {
+		return fmt.Errorf("error parsing kusto data: %w", err)
+	}
+	return nil
+}
+
+func (maps *DataMaps) DecodeLocalPerformanceData(filePath string) error {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return fmt.Errorf("error reading file: %v", err)
+	}
+
+	if err = json.Unmarshal(data, &maps); err != nil {
+		return fmt.Errorf("error unmarshaling JSON to temoporary map: %v", err)
+	}
+
+	if err = maps.ConvertTimestampsToSeconds(maps.StagingMap); err != nil {
+		return fmt.Errorf("failed to convert timestamps to floats for evaluation: %w", err)
+	}
+
+	return nil
+}
+
+func (maps *DataMaps) ConvertTimestampsToSeconds(holdingMap StagingMap) error {
+	for key, value := range holdingMap {
+		script := map[string]float64{}
+		for section, timeElapsed := range value {
+			t, err := time.Parse("15:04:05", timeElapsed)
+			if err != nil {
+				return fmt.Errorf("error parsing timestamp in local build JSON data: %w", err)
+			}
+			totalSeconds := float64(t.Hour()*3600 + t.Minute()*60 + t.Second())
+			script[section] = totalSeconds
+		}
+		maps.LocalPerformanceDataMap[key] = script
+	}
+	return nil
+}
+
+func (maps *DataMaps) ParseKustoData(data *SKU) error {
+	data.SKUPerformanceData = data.CleanData()
+	kustoData := []byte(data.SKUPerformanceData)
+	if err := json.Unmarshal(kustoData, &maps.QueriedPerformanceDataMap); err != nil {
+		return fmt.Errorf("error unmarshalling kusto data: %w", err)
+	}
+	return nil
+}
+
+func (sku *SKU) CleanData() string {
+	return strings.ReplaceAll(sku.SKUPerformanceData, "NaN", "-1")
+}
+
+// After preparing performance data, evaluate it with EvaluatePerformance and associated helper functions
+func (maps *DataMaps) EvaluatePerformance() error {
+	// Iterate over LocalPerformanceDataMap and compare it against identical sections in QueriedPerformanceDataMap
+	for scriptName, scriptData := range maps.LocalPerformanceDataMap {
+		for section, timeElapsed := range scriptData {
+			// The value of QueriedPerformanceDataMap[scriptName][section] is an array with two elements: [avg, 2*stdev]
+			// First we check that the queried data contains this section
+			sectionDataSlice, ok := maps.QueriedPerformanceDataMap[scriptName][section]
+			if !ok {
+				log.Printf("no data available for %s in %s\n", section, scriptName)
+				continue
+			}
+			// Adding the slice values together gives us the maximum time allowed for the section
+			maxTimeAllowed, err := SumSlice(sectionDataSlice)
+			if err != nil {
+				log.Printf("error calculating max time allowed for %s in %s: %v\n", section, scriptName, err)
+				continue
+			}
+			if maxTimeAllowed == -1 {
+				log.Printf("not enough data available for %s in %s\n", section, scriptName)
+				continue
+			}
+			if timeElapsed > maxTimeAllowed {
+				if maps.RegressionMap[scriptName] == nil {
+					maps.RegressionMap[scriptName] = map[string]float64{}
+				}
+				// Record the amount of time the section exceeded the maximum allowed time by
+				maps.RegressionMap[scriptName][section] = timeElapsed - maxTimeAllowed
+			}
+		}
+	}
+	if len(maps.RegressionMap) > 0 {
+		log.Println("##vso[task.logissue type=warning;sourcepath=buildperformance;]Regressions listed below. Values are the excess time over 3 stdev above the mean")
+		if err := maps.DisplayRegressions(); err != nil {
+			return fmt.Errorf("error printing regressions: %w", err)
+		}
+		return nil
+	}
+	log.Println("\nNo regressions found for this pipeline run")
+	return nil
+}
+
+func SumSlice(slice []float64) (float64, error) {
+	var sum float64
+	if len(slice) != 2 {
+		return sum, fmt.Errorf("expected 2 elements in slice, got %d: %v", len(slice), slice)
+	}
+	if slice[0] == -1 || slice[1] == -1 {
+		sum = -1
+		return sum, nil
+	}
+	sum = slice[0] + slice[1]*3
+	return sum, nil
+}
+
+func (maps DataMaps) DisplayRegressions() error {
+	// Marshall JSON data and indent for better readability
+	data, err := json.MarshalIndent(maps.RegressionMap, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshalling regression data: %w", err)
+	}
+
+	// Print JSON to the console for review by the user
+	log.Println(string(data))
+	return nil
+}
