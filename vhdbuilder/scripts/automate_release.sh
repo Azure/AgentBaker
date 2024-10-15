5d83329c2c0a65d98541db012efd66619c1d5951
Cameron Meissner <cameissner@microsoft.com>
2024-10-15 16:01:11 -0700

    chore: user agent

--- vhdbuilder/scripts/automate_release.sh
+++ vhdbuilder/scripts/automate_release.sh
@@ -56,7 +56,7 @@ create_release() {
     # we do it this way for now since the devops CLI extension doesn't support what we need: https://github.com/Azure/azure-devops-cli-extension/issues/1257
     echo "sending POST request to $RELEASE_API_ENDPOINT"
     REQUEST_BODY="{'artifacts': [{'alias': '$RELEASE_EV2_ARTIFACTS_ALIAS_NAME', 'instanceReference': {'id': '$EV2_BUILD_ID', 'name': '$EV2_BUILD_NUMBER'}}], 'definitionId': $SIG_RELEASE_PIPELINE_ID}"
-    RESPONSE=$(curl --http1.1 -X POST -H "Authorization: Basic $(echo -n ":$ADO_PAT" | base64)" -H "Content-Type: application/json" -d "$REQUEST_BODY" "$RELEASE_API_ENDPOINT" | grep -v "Authorization: Basic")
+    RESPONSE=$(curl --http1.1 -X POST -H "User-Agent: vhdbuilder/1.0" -H "Authorization: Basic $(echo -n ":$ADO_PAT" | base64)" -H "Content-Type: application/json" -d "$REQUEST_BODY" "$RELEASE_API_ENDPOINT")
     
     if [ $? -ne 0 ]; then
         echo "failed to create release, received response:"
