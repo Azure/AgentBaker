d8add7068132470489c33755613f756ae11bea5a
Cameron Meissner <cameissner@microsoft.com>
2024-10-16 10:48:11 -0700

    chore: add unit tests

---
+++ spec/parts/linux/cloud-init/artifacts/cse_config_spec.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+Describe 'cse_config.sh'
+    Describe 'getPrimaryNicIP()'
+        It 'should return the correct IP when a single network interface is attached to the VM'
+            curl() {
+                cat spec/parts/linux/cloud-init/artifacts/imds_mocks/network/single_nic.json
+            }
+            When call getPrimaryNicIP
+            The output should equal "0.0.0.0"
+        End
+
+        It 'should return the correct IP when multiple network interfaces are attached to the VM'
+            curl() {
+                cat spec/parts/linux/cloud-init/artifacts/imds_mocks/network/multi_nic.json
+            }
+            When call getPrimaryNicIP
+            The output should equal "0.0.0.0"
+        End
+    End
+End
