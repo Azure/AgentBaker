2093f0be2677686efadee874987e042299d25593
Lily Pan <lilypan@microsoft.com>
2024-10-17 13:32:21 -0700

    use cloud-init status instead of target

--- parts/linux/cloud-init/artifacts/bootstrap.sh
+++ parts/linux/cloud-init/artifacts/bootstrap.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -x
+# Check cloud-init status until it is done
+while true; do
+    status=$(cloud-init status)
+
+    if [[ "$status" == *"done"* ]]; then
+        echo "Cloud-init has completed."
+        break
+    fi
+    sleep 1
+done
 # TODO: propagate exit status to bootstrapper
 if [ ! -f /opt/azure/containers/nbc.json ]; then 
     exit 1
