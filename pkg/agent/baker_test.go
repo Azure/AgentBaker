5acac7d25b7f6ca3fa0c221a0a24082697f610a5
Tim Wright <wrightt@microsoft.com>
2024-10-21 19:18:15 +1300

    bug: don't compress custom data for windows

--- pkg/agent/baker_test.go
+++ pkg/agent/baker_test.go
@@ -349,14 +349,20 @@ var _ = Describe("Assert generated customData and cseCmd", func() {
 			configCustomDataInput.(*datamodel.NodeBootstrappingConfiguration),
 		)
 		Expect(err).To(BeNil())
-		customDataBytes, err := base64.StdEncoding.DecodeString(nodeBootstrapping.CustomData)
-		Expect(err).To(BeNil())
 
-		// try to unzip the bytes. If this fails then the custom data was not zipped. And it should be due to customdata size limitations.
-		unzippedDataBytes, err := getGzipDecodedValue(customDataBytes)
-		Expect(err).To(BeNil())
+		var customDataBytes []byte
+		if config.AgentPoolProfile.IsWindows() {
+			customDataBytes, err = base64.StdEncoding.DecodeString(nodeBootstrapping.CustomData)
+			Expect(err).To(BeNil())
+		} else {
+			// try to unzip the bytes. If this fails then the custom data was not zipped. And it should be due to customdata size limitations.
+			zippedDataBytes, err := base64.StdEncoding.DecodeString(nodeBootstrapping.CustomData)
+			Expect(err).To(BeNil())
+			customDataBytes, err = getGzipDecodedValue(zippedDataBytes)
+			Expect(err).To(BeNil())
+		}
 
-		customData := string(unzippedDataBytes)
+		customData := string(customDataBytes)
 		Expect(err).To(BeNil())
 
 		if generateTestData() {
