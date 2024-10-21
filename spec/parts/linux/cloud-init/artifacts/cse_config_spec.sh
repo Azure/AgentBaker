d5a75e83befa4ec822773d00e110b45c8e7aa54b
Cameron Meissner <cameissner@microsoft.com>
2024-10-21 10:29:21 -0700

    chore: refactor tests

--- spec/parts/linux/cloud-init/artifacts/cse_config_spec.sh
+++ spec/parts/linux/cloud-init/artifacts/cse_config_spec.sh
@@ -21,54 +21,6 @@ Describe 'cse_config.sh'
         End
     End
 
-    Describe 'addKubeletNodeLabel'
-        It 'should perform a no-op when the specified label already exists within the label string'
-            KUBELET_NODE_LABELS="kubernetes.azure.com/nodepool-type=VirtualMachineScaleSets,kubernetes.azure.com/kubelet-serving-ca=cluster,kubernetes.azure.com/agentpool=wp0"
-            When call addKubeletNodeLabel kubernetes.azure.com/kubelet-serving-ca=cluster
-            The stdout should include 'kubelet node label kubernetes.azure.com/kubelet-serving-ca=cluster is already present, nothing to add'
-            The variable KUBELET_NODE_LABELS should equal 'kubernetes.azure.com/nodepool-type=VirtualMachineScaleSets,kubernetes.azure.com/kubelet-serving-ca=cluster,kubernetes.azure.com/agentpool=wp0'
-        End
-
-        It 'should append the label when it does not already exist within the label string'
-            KUBELET_NODE_LABELS="kubernetes.azure.com/nodepool-type=VirtualMachineScaleSets,kubernetes.azure.com/agentpool=wp0"
-            When call addKubeletNodeLabel "kubernetes.azure.com/kubelet-serving-ca=cluster"
-            The stdout should not include 'kubelet node label kubernetes.azure.com/kubelet-serving-ca=cluster is already present, nothing to add'
-            The variable KUBELET_NODE_LABELS should equal 'kubernetes.azure.com/nodepool-type=VirtualMachineScaleSets,kubernetes.azure.com/agentpool=wp0,kubernetes.azure.com/kubelet-serving-ca=cluster'
-        End
-    End
-
-    Describe 'removeKubeletNodeLabel'
-        It 'should remove the specified label when it exists within kubelet node labels'
-            KUBELET_NODE_LABELS="kubernetes.azure.com/nodepool-type=VirtualMachineScaleSets,kubernetes.azure.com/kubelet-serving-ca=cluster,kubernetes.azure.com/agentpool=wp0"
-            When call removeKubeletNodeLabel kubernetes.azure.com/kubelet-serving-ca=cluster
-            The variable KUBELET_NODE_LABELS should equal 'kubernetes.azure.com/nodepool-type=VirtualMachineScaleSets,kubernetes.azure.com/agentpool=wp0'
-        End
-
-        It 'should remove the specified label when it is the first label within kubelet node labels'
-            KUBELET_NODE_LABELS="kubernetes.azure.com/kubelet-serving-ca=cluster,kubernetes.azure.com/nodepool-type=VirtualMachineScaleSets,kubernetes.azure.com/agentpool=wp0"
-            When call removeKubeletNodeLabel kubernetes.azure.com/kubelet-serving-ca=cluster
-            The variable KUBELET_NODE_LABELS should equal 'kubernetes.azure.com/nodepool-type=VirtualMachineScaleSets,kubernetes.azure.com/agentpool=wp0'
-        End
-
-        It 'should remove the specified label when it is the last label within kubelet node labels'
-            KUBELET_NODE_LABELS="kubernetes.azure.com/nodepool-type=VirtualMachineScaleSets,kubernetes.azure.com/agentpool=wp0,kubernetes.azure.com/kubelet-serving-ca=cluster"
-            When call removeKubeletNodeLabel kubernetes.azure.com/kubelet-serving-ca=cluster
-            The variable KUBELET_NODE_LABELS should equal 'kubernetes.azure.com/nodepool-type=VirtualMachineScaleSets,kubernetes.azure.com/agentpool=wp0'
-        End
-
-        It 'should not alter kubelet node labels if the target label does not exist'
-            KUBELET_NODE_LABELS="kubernetes.azure.com/nodepool-type=VirtualMachineScaleSets,kubernetes.azure.com/agentpool=wp0"
-            When call removeKubeletNodeLabel kubernetes.azure.com/kubelet-serving-ca=cluster
-            The variable KUBELET_NODE_LABELS should equal 'kubernetes.azure.com/nodepool-type=VirtualMachineScaleSets,kubernetes.azure.com/agentpool=wp0'
-        End
-
-        It 'should return an empty string if the only label within kubelet node labels is the target'
-            KUBELET_NODE_LABELS="kubernetes.azure.com/kubelet-serving-ca=cluster"
-            When call removeKubeletNodeLabel kubernetes.azure.com/kubelet-serving-ca=cluster
-            The variable KUBELET_NODE_LABELS should equal ''
-        End
-    End
-
     Describe 'configureKubeletServingCertificateRotation'
         It 'should no-op when EnableKubeletServingCertificateRotation is false'
             retrycmd_if_failure_no_stats() { # for mocking IMDS calls
