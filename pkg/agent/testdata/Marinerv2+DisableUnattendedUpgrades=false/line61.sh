7be18d7820ef1c952c8ad331d8dad584949dc33d
Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>
2024-10-21 14:23:23 -0700

    remove 2.0 specific config for nvidia container runtime

--- pkg/agent/testdata/Marinerv2+DisableUnattendedUpgrades=false/line61.sh
+++ pkg/agent/testdata/Marinerv2+DisableUnattendedUpgrades=false/line61.sh
@@ -67,12 +67,7 @@ installNvidiaFabricManager() {
 }
 
 installNvidiaContainerToolkit() {
-    MARINER_NVIDIA_CONTAINER_RUNTIME_VERSION="3.13.0"
     MARINER_NVIDIA_CONTAINER_TOOLKIT_VERSION="1.16.2"
-
-    if [[ $OS_VERSION == "2.0" ]]; then
-      MARINER_NVIDIA_CONTAINER_TOOLKIT_VERSION="1.13.5"
-    fi
     
     for nvidia_package in nvidia-container-toolkit-${MARINER_NVIDIA_CONTAINER_TOOLKIT_VERSION} nvidia-container-toolkit-base-${MARINER_NVIDIA_CONTAINER_TOOLKIT_VERSION} libnvidia-container-tools-${MARINER_NVIDIA_CONTAINER_TOOLKIT_VERSION} libnvidia-container1-${MARINER_NVIDIA_CONTAINER_TOOLKIT_VERSION}; do
       if ! dnf_install 30 1 600 $nvidia_package; then
@@ -80,11 +75,6 @@ installNvidiaContainerToolkit() {
       fi
     done
 
-    if [[ $OS_VERSION == "2.0" ]]; then
-      if ! dnf_install 30 1 600 nvidia-container-runtime-${MARINER_NVIDIA_CONTAINER_RUNTIME_VERSION}; then
-        exit $ERR_APT_INSTALL_TIMEOUT
-      fi
-    fi
 }
 
 enableNvidiaPersistenceMode() {
