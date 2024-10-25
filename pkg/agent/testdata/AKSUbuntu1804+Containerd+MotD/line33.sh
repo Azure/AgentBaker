e94b054e80023beb03dfa717e1beef6237903724
anujmaheshwari1 <anujmaheshwari600@gmail.com>
2024-10-25 11:52:25 -0700

    fix: use netcat for api server check for vnet integration scenario (#5158)
    Co-authored-by: anujmaheshwari1 <amaheshwari@microsoft.com>

--- pkg/agent/testdata/AKSUbuntu1804+Containerd+MotD/line33.sh
+++ pkg/agent/testdata/AKSUbuntu1804+Containerd+MotD/line33.sh
@@ -323,12 +323,7 @@ if ! [[ ${API_SERVER_NAME} =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     fi
 else
     API_SERVER_CONN_RETRIES=300
-    if [ "${UBUNTU_RELEASE}" == "18.04" ]; then
-        #TODO (djsly): remove this once 18.04 isn't supported anymore
-        logs_to_events "AKS.CSE.apiserverNC" "retrycmd_if_failure ${API_SERVER_CONN_RETRIES} 1 10 nc -vz ${API_SERVER_NAME} 443" || time nc -vz ${API_SERVER_NAME} 443 || VALIDATION_ERR=$ERR_K8S_API_SERVER_CONN_FAIL
-    else
-        logs_to_events "AKS.CSE.apiserverCurl" "retrycmd_if_failure ${API_SERVER_CONN_RETRIES} 1 10 curl -v --cacert /etc/kubernetes/certs/ca.crt https://${API_SERVER_NAME}:443" || time curl -v --cacert /etc/kubernetes/certs/ca.crt "https://${API_SERVER_NAME}:443" || VALIDATION_ERR=$ERR_K8S_API_SERVER_CONN_FAIL
-    fi
+    logs_to_events "AKS.CSE.apiserverNC" "retrycmd_if_failure ${API_SERVER_CONN_RETRIES} 1 10 nc -vz ${API_SERVER_NAME} 443" || time nc -vz ${API_SERVER_NAME} 443 || VALIDATION_ERR=$ERR_K8S_API_SERVER_CONN_FAIL
 fi
 
 if [[ ${ID} != "mariner" ]] && [[ ${ID} != "azurelinux" ]]; then
