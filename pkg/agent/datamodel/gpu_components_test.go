0a756a05f77bfd03bca77e61abf4ab5a2d7b5f24
Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>
2024-10-28 11:05:34 -0700

    Add regex tests for components.json format

--- pkg/agent/datamodel/gpu_components_test.go
+++ pkg/agent/datamodel/gpu_components_test.go
@@ -2,6 +2,7 @@
 package datamodel
 
 import (
+	"regexp"
 	"testing"
 )
 
@@ -21,4 +22,30 @@ func TestLoadConfig(t *testing.T) {
 	if AKSGPUGridVersionSuffix == "" {
 		t.Error(("AKSGPUGridVersionSuffix is empty"))
 	}
+
+	// Define regular expressions for expected formats
+	versionRegex := `^\d+\.\d+\.\d+$` // match version strings in a format like "X.Y.Z", where each of X, Y, and Z are numbers. e.g., "550.90.12"
+	suffixRegex := `^\d{14}$`         //  match a string of exactly 14 digits, which can represent a timestamp e.g., "20241021235610"
+
+	// Compile the regular expressions
+	versionPattern := regexp.MustCompile(versionRegex)
+	suffixPattern := regexp.MustCompile(suffixRegex)
+
+	// Test NvidiaCudaDriverVersion and other variables' format
+	if !versionPattern.MatchString(NvidiaCudaDriverVersion) {
+		t.Errorf("NvidiaCudaDriverVersion '%s' does not match expected format", NvidiaCudaDriverVersion)
+	}
+
+	if !versionPattern.MatchString(NvidiaGridDriverVersion) {
+		t.Errorf("NvidiaGridDriverVersion '%s' does not match expected format", NvidiaGridDriverVersion)
+	}
+
+	if !suffixPattern.MatchString(AKSGPUCudaVersionSuffix) {
+		t.Errorf("AKSGPUCudaVersionSuffix '%s' does not match expected format", AKSGPUCudaVersionSuffix)
+	}
+
+	if !suffixPattern.MatchString(AKSGPUGridVersionSuffix) {
+		t.Errorf("AKSGPUGridVersionSuffix '%s' does not match expected format", AKSGPUGridVersionSuffix)
+	}
+
 }
