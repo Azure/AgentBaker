1a6f09a89cf375aa125bae4088fd77c7abb644a2
Cameron Meissner <cameronmeissner@gmail.com>
2024-11-04 16:42:30 -0800

    fix: add retry for initial trivy scan to handle 429's from ghcr on DB update (#5215)
    Co-authored-by: Cameron Meissner <cameissner@microsoft.com>

--- vhdbuilder/packer/trivy-scan.sh
+++ vhdbuilder/packer/trivy-scan.sh
@@ -39,6 +39,20 @@ export SYSTEM_COLLECTIONURI=${25}
 export SYSTEM_TEAMPROJECT=${26}
 export BUILD_BUILDID=${27}
 
+retrycmd_if_failure() {
+    retries=$1; wait_sleep=$2; timeout=$3; shift && shift && shift
+    for i in $(seq 1 $retries); do
+        timeout $timeout "${@}" && break || \
+        if [ $i -eq $retries ]; then
+            echo Executed \"$@\" $i times;
+            return 1
+        else
+            sleep $wait_sleep
+        fi
+    done
+    echo Executed \"$@\" $i times;
+}
+
 install_azure_cli() {
     OS_SKU=${1}
     OS_VERSION=${2}
@@ -163,7 +177,7 @@ for CONTAINER_IMAGE in $IMAGE_LIST; do
     fi
 done
 
-rm ./trivy 
+rm ./trivy
 
 chmod a+r "${TRIVY_REPORT_ROOTFS_JSON_PATH}"
 chmod a+r "${TRIVY_REPORT_IMAGE_TABLE_PATH}"
