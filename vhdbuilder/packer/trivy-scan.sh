b8cc6436e45b814570b103ea3d0fdd23dfa83c04
Sylvain Boily <4981802+djsly@users.noreply.github.com>
2024-11-12 16:19:49 -0500

    cherry picking the crictl version bump and trivy-db logic to avoid throttling (#5233)
    Co-authored-by: Cameron Meissner <cameronmeissner@gmail.com>
    Co-authored-by: Cameron Meissner <cameissner@microsoft.com>

--- vhdbuilder/packer/trivy-scan.sh
+++ vhdbuilder/packer/trivy-scan.sh
@@ -54,6 +54,20 @@ retrycmd_if_failure() {
     echo Executed \"$@\" $i times;
 }
 
+retrycmd_if_failure() {
+    retries=$1; wait_sleep=$2; timeout=$3; shift && shift && shift
+    for i in $(seq 1 $retries); do
+        timeout $timeout "${@}" && break || \
+        if [ $i -eq $retries ]; then
+            echo Executed \"$@\" $i times;
+            return 1
+        else
+            sleep $wait_sleep
+        fi
+    done
+    echo Executed \"$@\" $i times;
+}
+
 install_azure_cli() {
     OS_SKU=${1}
     OS_VERSION=${2}
