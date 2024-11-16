a46c5d5959bf1cd8732b40d15f99f6b90f3b6344
Devinwong <wongsiosun@outlook.com>
2024-11-15 16:30:48 -0800

    chore: rename leading aKS to aks in tests (#5279)

--- pkg/proto/aksnodeconfig/v1/helper_test.go
+++ pkg/proto/aksnodeconfig/v1/helper_test.go
@@ -285,11 +285,11 @@ func TestAKSNodeConfigBuilder_validateRequiredFields(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			aKSNodeConfigBuilder := NewAKSNodeConfigBuilder()
-			aKSNodeConfigBuilder.ApplyConfiguration(tt.fields.nodeBootstrapConfig)
-			err := aKSNodeConfigBuilder.validateRequiredFields()
+			aksNodeConfigBuilder := NewAKSNodeConfigBuilder()
+			aksNodeConfigBuilder.ApplyConfiguration(tt.fields.nodeBootstrapConfig)
+			err := aksNodeConfigBuilder.validateRequiredFields()
 			if (err != nil) != tt.wantErr {
-				t.Errorf("aKSNodeConfigBuilder.validateRequiredFields() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("aksNodeConfigBuilder.validateRequiredFields() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
