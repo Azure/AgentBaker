3a530124ac6af4935714961de77ca2254e1d69c3
Zachary Bailey <zbailey@microsoft.com>
2024-11-20 14:29:45 -0800

    fix: test and scan vm sizes

--- vhdbuilder/packer/test/run-test.sh
+++ vhdbuilder/packer/test/run-test.sh
@@ -103,6 +103,11 @@ else
     TARGET_COMMAND_STRING+="--security-type TrustedLaunch --enable-secure-boot true --enable-vtpm true"
   fi
 
+  if [[ "${OS_TYPE}" == "Linux" && "${IMG_SKU}" == "20_04-lts-cvm" ]]; then
+    TARGET_COMMAND_STRING="--size Standard_EC16ads_v5"
+    TARGET_COMMAND_STRING+=" --security-type ConfidentialVM --enable-secure-boot true --enable-vtpm true --os-disk-security-encryption-type VMGuestStateOnly --specialized"
+  fi
+
   az vm create \
       --resource-group $TEST_VM_RESOURCE_GROUP_NAME \
       --name $VM_NAME \
