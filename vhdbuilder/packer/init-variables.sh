4ad5a9bebc7de744ec6ab57cae9e67b95d2bb124
Zachary Bailey <zbailey@microsoft.com>
2024-11-22 09:30:30 -0800

    fix: point to correct image def

--- vhdbuilder/packer/init-variables.sh
+++ vhdbuilder/packer/init-variables.sh
@@ -164,7 +164,7 @@ if [[ "${MODE}" == "linuxVhdMode" ]]; then
 			SIG_IMAGE_NAME="AzureLinux${SIG_IMAGE_NAME}"
 		fi
 		if [[ "${IMG_SKU,,}" == "20_04-lts-cvm" ]]; then
-			SIG_IMAGE_NAME+="tl"
+			SIG_IMAGE_NAME+="TL"
 		fi
 		echo "No input for SIG_IMAGE_NAME was provided, defaulting to: ${SIG_IMAGE_NAME}"
 	else
