3f1e18f098bffb961baf1873ee8d6de0b1528dec
Mikolaj Umanski <mumanski@microsoft.com>
2024-12-03 14:21:06 -0800

    test: add check to azure linux, remove incorrect check for ubuntu

--- e2e/scenario_test.go
+++ e2e/scenario_test.go
@@ -1111,7 +1111,6 @@ func Test_Ubuntu2204_MessageOfTheDay(t *testing.T) {
 			},
 			Validator: func(ctx context.Context, s *Scenario) {
 				ValidateFileHasContent(ctx, s, "/etc/motd", "foobar")
-				ValidateFileHasContent(ctx, s, "/etc/dnf/automatic.conf", "emit_via = stdio")
 			},
 		},
 	})
@@ -1128,6 +1127,7 @@ func Test_AzureLinuxV2_MessageOfTheDay(t *testing.T) {
 			},
 			Validator: func(ctx context.Context, s *Scenario) {
 				ValidateFileHasContent(ctx, s, "/etc/motd", "foobar")
+				ValidateFileHasContent(ctx, s, "/etc/dnf/automatic.conf", "emit_via = stdio")
 			},
 		},
 	})
