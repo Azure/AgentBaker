2ccc779aed214fdff61875880771d62155d0f06a
jasonjung <jasonjung@microsoft.com>
2024-12-08 17:14:06 -0800

    test vm creation-7: script execute

--- vhdbuilder/packer/query-cve.sh
+++ vhdbuilder/packer/query-cve.sh
@@ -3,11 +3,13 @@ set -eux
 
 QUERY_RESOURCE_PREFIX="cve-query"
 QUERY_VM_NAME="$QUERY_RESOURCE_PREFIX-vm-$(date +%s)-$RANDOM"
-VHD_IMAGE="/subscriptions/c4c3550e-a965-4993-a50c-628fd38cd3e1/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/2204containerd"
+VHD_IMAGE="/subscriptions/c4c3550e-a965-4993-a50c-628fd38cd3e1/resourceGroups/aksvhdtestbuildrg/providers/Microsoft.Compute/galleries/PackerSigGalleryEastUS/images/2204gen2arm64containerd" # to variable
 RESOURCE_GROUP_NAME="$QUERY_RESOURCE_PREFIX-$(date +%s)-$RANDOM"
 
 QUERY_VM_USERNAME="azureuser"
-VM_LOCATION="westus2"
+VM_LOCATION="westus2" # to variable
+
+QUERY_SCRIPT_PATH="query-cve-vm.sh"
 
 set +x
 QUERY_VM_PASSWORD="QueryVM@$(date +%s)"
@@ -27,3 +29,15 @@ az vm create --resource-group $RESOURCE_GROUP_NAME \
   --admin-username $QUERY_VM_USERNAME \
   --admin-password $QUERY_VM_PASSWORD \
   --assign-identity "${UMSI_RESOURCE_ID}"
+
+FULL_PATH=$(realpath $0)
+CDIR=$(dirname $FULL_PATH)
+QUERY_SCRIPT_PATH="$CDIR/$QUERY_SCRIPT_PATH"
+
+az vm run-command invoke \
+  --command-id RunShellScript \
+  --name $QUERY_VM_NAME \
+  --resource-group $RESOURCE_GROUP_NAME \
+  --scripts @$QUERY_SCRIPT_PATH\
+  --parameters "UMSI_PRINCIPAL_ID"=${UMSI_PRINCIPAL_ID} \
+      "ACCOUNT_NAME"=${ACCOUNT_NAME}
