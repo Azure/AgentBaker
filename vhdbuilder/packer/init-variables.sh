4598c06361c78d77b50e10bd45861aa32329f450
Zachary Bailey <zbailey@microsoft.com>
2024-12-11 07:39:41 -0800

    feat: modify init-variables.sh for region selection depending on sku

--- vhdbuilder/packer/init-variables.sh
+++ vhdbuilder/packer/init-variables.sh
@@ -85,7 +85,11 @@ if [ -z "${VNET_RG_NAME}" ]; then
 			# TODO(cameissner): build out updated pool resources in prod so we don't have to pivot like this
 			VNET_RG_NAME="nodesig-${ENVIRONMENT}-${PACKER_BUILD_LOCATION}-agent-pool"
 		else
-			VNET_RG_NAME="nodesig-${ENVIRONMENT}-${PACKER_BUILD_LOCATION}-packer-vnet-rg"
+			if [[ ${IMG_SKU} == "20_04-lts-cvm" ]]; then
+				VNET_RG_NAME="nodesig-${ENVIRONMENT}-${CVM_PACKER_BUILD_LOCATION}-packer-vnet-rg"
+			else
+				VNET_RG_NAME="nodesig-${ENVIRONMENT}-${PACKER_BUILD_LOCATION}-packer-vnet-rg"
+			fi
 		fi
 	fi
 	if [ "$MODE" == "windowsVhdMode" ]; then
@@ -103,7 +107,11 @@ if [ -z "${VNET_NAME}" ]; then
 			# TODO(cameissner): build out updated pool resources in prod so we don't have to pivot like this
 			VNET_NAME="nodesig-pool-vnet-${PACKER_BUILD_LOCATION}"
 		else
-			VNET_NAME="nodesig-packer-vnet-${PACKER_BUILD_LOCATION}"
+			if [[ ${IMG_SKU} == "20_04-lts-cvm" ]]; then
+				VNET_NAME="nodesig-packer-vnet-${CVM_PACKER_BUILD_LOCATION}"
+			else
+				VNET_NAME="nodesig-packer-vnet-${PACKER_BUILD_LOCATION}"
+			fi
 		fi
 	fi
 	if [ "$MODE" == "windowsVhdMode" ]; then
@@ -259,7 +267,7 @@ if [[ "$MODE" == "linuxVhdMode" || "$MODE" == "windowsVhdMode" ]]; then
 		  if [[ ${ARCHITECTURE,,} == "arm64" ]]; then
         TARGET_COMMAND_STRING+="--architecture Arm64"
       elif [[ ${IMG_SKU} == "20_04-lts-cvm" ]]; then
-        TARGET_COMMAND_STRING+="--features SecurityType=ConfidentialVMSupported"
+        TARGET_COMMAND_STRING+="--os-state Specialized --features SecurityType=ConfidentialVM"
       fi
 
       az sig image-definition create \
@@ -525,6 +533,7 @@ fi
 # windows_image_version refers to the version from azure gallery
 cat <<EOF > vhdbuilder/packer/settings.json
 { 
+  "cvm_location": "${CVM_PACKER_BUILD_LOCATION}",
   "subscription_id":  "${SUBSCRIPTION_ID}",
   "resource_group_name": "${AZURE_RESOURCE_GROUP_NAME}",
   "location": "${PACKER_BUILD_LOCATION}",
