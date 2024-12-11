adba8bf62e2675a7c68a2646c01fbc84557f03af
Cameron Meissner <cameissner@microsoft.com>
2024-12-10 16:32:59 -0800

    fix: always specify agent identity ID when logging into azure

--- packer.mk
+++ packer.mk
@@ -61,14 +61,8 @@ endif
 endif
 
 az-login:
-	@echo "Logging into Azure with agent VM MSI..."
-ifeq ($(origin MANAGED_IDENTITY_ID), undefined)
-	@echo "Logging in with Hosted Pool's Default Managed Identity"
-	@az login --identity
-else
-	@echo "Logging in with Hosted Pool's Managed Identity: ${MANAGED_IDENTITY_ID}"
-	@az login --identity --username ${MANAGED_IDENTITY_ID}
-endif
+	@echo "Logging into Azure with identity: ${AZURE_MSI_RESOURCE_STRING}..."
+	@az login --identity --username ${AZURE_MSI_RESOURCE_STRING}
 	@echo "Using the subscription ${SUBSCRIPTION_ID}"
 	@az account set -s ${SUBSCRIPTION_ID}
 
