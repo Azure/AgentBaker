596278f59dcf00b21ac547ff231652fac4aa3393
Cameron Meissner <cameissner@microsoft.com>
2024-12-13 10:37:18 -0800

    fmt

--- vhdbuilder/packer/init-variables.sh
+++ vhdbuilder/packer/init-variables.sh
@@ -510,7 +510,8 @@ if [ "$MODE" == "linuxVhdMode" ] && [ "${OS_SKU,,}" == "ubuntu" ]; then
 	if [ "${UBUNTU_RELEASE}" == "18.04" ] || [ "${UBUNTU_RELEASE}" == "20.04" ] || [ "${ENABLE_FIPS,,}" == "true" ]; then
 		set +x
 		if [ -z "${UA_TOKEN}" ]; then
-			echo "UA_TOKEN must be provided when building SKUs which require ESM" && exit 1
+			echo "UA_TOKEN must be provided when building SKUs which require ESM"
+			exit 1
 		fi
 		cat <<EOF > vhdbuilder/packer/ua-settings.json
 { 
