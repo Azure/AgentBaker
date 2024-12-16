405e097173cbe8a782468ca88c9cf20590d5b736
Alison Burgess <alburgess@microsoft.com>
2024-12-16 11:20:04 -0800

    remove in just single test

--- e2e/scenario_test.go
+++ e2e/scenario_test.go
@@ -632,7 +632,11 @@ func Test_Ubuntu2204Gen2_ContainerdAirgappedK8sNotCached(t *testing.T) {
 		Config: Config{
 			Cluster: ClusterKubenetAirgap,
 			VHD:     config.VHDUbuntu2204Gen2ContainerdAirgappedK8sNotCached,
+
 			BootstrapConfigMutator: func(nbc *datamodel.NodeBootstrappingConfiguration) {
+				if nbc.KubeletConfig != nil {
+					delete(nbc.KubeletConfig, "--azure-container-registry-config")
+				}
 
 				nbc.OutboundType = datamodel.OutboundTypeBlock
 				nbc.ContainerService.Properties.SecurityProfile = &datamodel.SecurityProfile{
