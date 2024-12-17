ae4b0a75a2c174209dc8949a390a23f7d0a969eb
Tim Wright <wrightt@microsoft.com>
2024-12-17 23:02:19 +0000

    quote variables, fix windows download version

--- vhdbuilder/packer/test/run-test.sh
+++ vhdbuilder/packer/test/run-test.sh
@@ -40,7 +40,7 @@ else
 fi
 
 # Create the testing resource group
-az group create --name $TEST_VM_RESOURCE_GROUP_NAME --location ${AZURE_LOCATION} --tags "source=AgentBaker" "now=$(date +%s)" "branch=${GIT_BRANCH}"
+az group create --name "$TEST_VM_RESOURCE_GROUP_NAME" --location "${AZURE_LOCATION}" --tags "source=AgentBaker" "now=$(date +%s)" "branch=${GIT_BRANCH}"
 
 # defer function to cleanup resource group when VHD debug is not enabled
 function cleanup() {
@@ -48,7 +48,7 @@ function cleanup() {
     echo "VHD debug mode is enabled, please manually delete test vm resource group $RESOURCE_GROUP_NAME after debugging"
   else
     echo "Deleting resource group ${TEST_VM_RESOURCE_GROUP_NAME}"
-    az group delete --name $TEST_VM_RESOURCE_GROUP_NAME --yes --no-wait
+    az group delete --name "$TEST_VM_RESOURCE_GROUP_NAME" --yes --no-wait
   fi
 }
 trap cleanup EXIT
@@ -78,42 +78,42 @@ fi
 if [ "${OS_TYPE,,}" == "linux" ]; then
   # in linux mode, explicitly create the NIC referencing the existing packer subnet to be attached to the testing VM so we avoid creating ephemeral vnets
   PACKER_SUBNET_ID="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${PACKER_VNET_RESOURCE_GROUP_NAME}/providers/Microsoft.Network/virtualNetworks/${PACKER_VNET_NAME}/subnets/packer"
-  if [ -z "$(az network vnet subnet show --ids $PACKER_SUBNET_ID | jq -r '.id')" ]; then
+  if [ -z "$(az network vnet subnet show --ids "$PACKER_SUBNET_ID" | jq -r '.id')" ]; then
       echo "packer subnet $PACKER_SUBNET_ID seems to be missing, unable to create test VM"
       exit 1
   fi
-  TESTING_NIC_ID=$(az network nic create --resource-group $TEST_VM_RESOURCE_GROUP_NAME --name "testing$(date +%s)${RANDOM}" --subnet $PACKER_SUBNET_ID | jq -r '.NewNIC.id')
+  TESTING_NIC_ID=$(az network nic create --resource-group "$TEST_VM_RESOURCE_GROUP_NAME" --name "testing$(date +%s)${RANDOM}" --subnet "$PACKER_SUBNET_ID" | jq -r '.NewNIC.id')
   if [ -z "$TESTING_NIC_ID" ]; then
       echo "unable to create new NIC for test VM"
       exit 1
   fi
   az vm create \
-      --resource-group $TEST_VM_RESOURCE_GROUP_NAME \
-      --name $VM_NAME \
-      --image $MANAGED_SIG_ID \
-      --admin-username $TEST_VM_ADMIN_USERNAME \
-      --admin-password $TEST_VM_ADMIN_PASSWORD \
-      --nics $TESTING_NIC_ID \
+      --resource-group "$TEST_VM_RESOURCE_GROUP_NAME" \
+      --name "$VM_NAME" \
+      --image "$MANAGED_SIG_ID" \
+      --admin-username "$TEST_VM_ADMIN_USERNAME" \
+      --admin-password "$TEST_VM_ADMIN_PASSWORD" \
+      --nics "$TESTING_NIC_ID" \
       ${TARGET_COMMAND_STRING}
 else
   az vm create \
-      --resource-group $TEST_VM_RESOURCE_GROUP_NAME \
-      --name $VM_NAME \
-      --image $MANAGED_SIG_ID \
-      --admin-username $TEST_VM_ADMIN_USERNAME \
-      --admin-password $TEST_VM_ADMIN_PASSWORD \
+      --resource-group "$TEST_VM_RESOURCE_GROUP_NAME" \
+      --name "$VM_NAME" \
+      --image "$MANAGED_SIG_ID" \
+      --admin-username "$TEST_VM_ADMIN_USERNAME" \
+      --admin-password "$TEST_VM_ADMIN_PASSWORD" \
       --public-ip-address "" \
       ${TARGET_COMMAND_STRING}
 fi
 
 echo "VHD test VM username: $TEST_VM_ADMIN_USERNAME, password: $TEST_VM_ADMIN_PASSWORD"
 
-time az vm wait -g $TEST_VM_RESOURCE_GROUP_NAME -n $VM_NAME --created
+time az vm wait -g "$TEST_VM_RESOURCE_GROUP_NAME" -n "$VM_NAME" --created
 capture_benchmark "${SCRIPT_NAME}_create_test_vm"
 set -x
 
 FULL_PATH=$(realpath $0)
-CDIR=$(dirname $FULL_PATH)
+CDIR=$(dirname "$FULL_PATH")
 
 if [ "$OS_TYPE" == "Linux" ]; then
   if [ -z "${ENABLE_FIPS// }" ]; then
@@ -125,10 +125,10 @@ if [ "$OS_TYPE" == "Linux" ]; then
   SCRIPT_PATH="$CDIR/$LINUX_SCRIPT_PATH"
   for i in $(seq 1 3); do
     ret=$(az vm run-command invoke --command-id RunShellScript \
-      --name $VM_NAME \
-      --resource-group $TEST_VM_RESOURCE_GROUP_NAME \
-      --scripts @$SCRIPT_PATH \
-      --parameters ${CONTAINER_RUNTIME} ${OS_VERSION} ${ENABLE_FIPS} ${OS_SKU} ${GIT_BRANCH} ${IMG_SKU}) && break
+      --name "$VM_NAME" \
+      --resource-group "$TEST_VM_RESOURCE_GROUP_NAME" \
+      --scripts "@$SCRIPT_PATH" \
+      --parameters "${CONTAINER_RUNTIME}" "${OS_VERSION}" "${ENABLE_FIPS}" "${OS_SKU}" "${GIT_BRANCH}" "${IMG_SKU}") && break
     echo "${i}: retrying az vm run-command"
   done
   # The error message for a Linux VM run-command is as follows:
@@ -143,9 +143,11 @@ if [ "$OS_TYPE" == "Linux" ]; then
   #    }
   #  ]
   #  We have extract the message field from the json, and get the errors outputted to stderr + remove \n
-  errMsg=$(echo -e $(echo $ret | jq ".value[] | .message" | grep -oP '(?<=stderr]).*(?=\\n")'))
-  echo $errMsg
-  if [ $errMsg != '' ]; then
+  errMsg=$(echo -e "$(echo "$ret" | jq ".value[] | .message" | grep -oP '(?<=stderr]).*(?=\\n")')")
+  echo "$errMsg"
+  if [ "$errMsg" != '' ]; then
+    echo "Tests failed. Test output is: "
+    echo "$ret"
     exit 1
   fi
 else
@@ -153,16 +155,16 @@ else
   echo "Run $SCRIPT_PATH"
   az vm run-command invoke --command-id RunPowerShellScript \
     --name $VM_NAME \
-    --resource-group $TEST_VM_RESOURCE_GROUP_NAME \
-    --scripts @$SCRIPT_PATH \
+    --resource-group "$TEST_VM_RESOURCE_GROUP_NAME" \
+    --scripts "@$SCRIPT_PATH" \
     --output json
 
   SCRIPT_PATH="$CDIR/$WIN_SCRIPT_PATH"
   echo "Run $SCRIPT_PATH"
   ret=$(az vm run-command invoke --command-id RunPowerShellScript \
-    --name $VM_NAME \
-    --resource-group $TEST_VM_RESOURCE_GROUP_NAME \
-    --scripts @$SCRIPT_PATH \
+    --name "$VM_NAME" \
+    --resource-group "$TEST_VM_RESOURCE_GROUP_NAME" \
+    --scripts "@$SCRIPT_PATH" \
     --output json \
     --parameters "windowsSKU=${WINDOWS_SKU}" "skipValidateReofferUpdate=${SKIPVALIDATEREOFFERUPDATE}")
   # An example of failed run-command output:
@@ -196,7 +198,7 @@ else
   # we have to use `-E` to disable interpretation of backslash escape sequences, for jq cannot process string
   # with a range of control characters not escaped as shown in the error below:
   #   Invalid string: control characters from U+0000 through U+001F must be escaped
-  errMsg=$(echo -E $ret | jq '.value[]  | select(.code == "ComponentStatus/StdErr/succeeded") | .message')
+  errMsg=$(echo -E "$ret" | jq '.value[]  | select(.code == "ComponentStatus/StdErr/succeeded") | .message')
   # a successful errMsg should be '""' after parsed by `jq`
   if [ "$errMsg" != \"\" ]; then
         echo "Tests failed. errMsg is $errMsg"
