746282278715f7c02ede313c50d695fb62fe2803
Beilei Huang <beileihuang@microsoft.com>
2024-12-31 17:31:07 +1300

    remove 2404 related test for now so that we can build VHD

--- e2e/config/vhd.go
+++ e2e/config/vhd.go
@@ -115,6 +115,14 @@ var (
 		Gallery: linuxGallery,
 	}
 
+	VHDUbuntu2404Gen1Containerd = &Image{
+		Name:    "2404gen1containerd",
+		OS:      OSUbuntu,
+		Arch:    "amd64",
+		Distro:  datamodel.AKSUbuntuContainerd2404,
+		Gallery: linuxGallery,
+	}
+
 	VHDUbuntu2404Gen2Containerd = &Image{
 		Name:    "2404gen2containerd",
 		OS:      OSUbuntu,
