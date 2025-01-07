36744c21d607f6d32254b91ebe66cc47cda175c8
r2k1 <yokree@gmail.com>
2025-01-07 13:29:48 +1300

    feat: Add --dry-run option to aks-node-controller (#5512)

--- aks-node-controller/const.go
+++ aks-node-controller/const.go
@@ -3,7 +3,7 @@ package main
 // Some options are intentionally non-configurable to avoid customization by users
 // it will help us to avoid introducing any breaking changes in the future.
 const (
-	logFile                   = "/var/log/azure/aks-node-controller.log"
+	logPath                   = "/var/log/azure/aks-node-controller.log"
 	provisionJSONFilePath     = "/var/log/azure/aks/provision.json"
 	provisionCompleteFilePath = "/opt/azure/containers/provision.complete"
 )
