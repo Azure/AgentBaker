24ecd802b800805146423114cb9d8e5fc8a3372c
Tim Wright <wrightt@microsoft.com>
2025-01-10 01:00:34 +0000

    disable extension check

--- vhdbuilder/packer/configure-windows-vhd.ps1
+++ vhdbuilder/packer/configure-windows-vhd.ps1
@@ -894,7 +894,8 @@ function Test-AzureExtensions {
         $actualExtensions = (Get-ChildItem "C:\Packages\Plugins").Name
         if ($actualExtensions.Length -gt 0) {
             Write-Log "Azure extensions are not expected. Details: $($actualExtensions | Out-String)"
-            exit 1
+            # TODO: only for MSFT testing
+            # exit 1
         }
     }
     Write-Log "Azure extensions are not found"
