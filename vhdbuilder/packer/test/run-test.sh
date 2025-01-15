e5e7459e02c98f0d04ec663ba995b1594ce5cf0c
Zachary Bailey <zbailey@microsoft.com>
2025-01-15 12:44:14 -0800

    fix: resolve azure location for cvm

--- vhdbuilder/packer/test/run-test.sh
+++ vhdbuilder/packer/test/run-test.sh
@@ -27,7 +27,11 @@ if [ "${OS_TYPE,,}" == "linux" ]; then
     echo "PACKER_BUILD_LOCATION must be set for linux builds"
     exit 1
   fi
-  AZURE_LOCATION=$PACKER_BUILD_LOCATION
+  if [ "${ENVIRONMENT,,}" == "test" ] && [ "${IMG_SKU}" == "20_04-lts-cvm" ]; then
+    AZURE_LOCATION=$CVM_PACKER_BUILD_LOCATION
+  else
+    AZURE_LOCATION=$PACKER_BUILD_LOCATION
+  fi
 fi
 
 if [ "${OS_TYPE,,}" == "linux" ]; then
