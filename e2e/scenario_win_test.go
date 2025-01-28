3ff8da7e0f180efe2aa5a932c3fdd843d4ed57a9
Tim Wright <wrightt@microsoft.com>
2025-01-28 01:46:19 +0000

    fix windows test with wrong cli args

--- e2e/scenario_win_test.go
+++ e2e/scenario_win_test.go
@@ -87,7 +87,7 @@ func Test_Windows23H2Gen2(t *testing.T) {
 			},
 			Validator: func(ctx context.Context, s *Scenario) {
 				ValidateFileHasContent(ctx, s, "/k/kubeletstart.ps1", "--container-runtime=remote")
-				ValidateWindowsProcessHasCliArguments(ctx, s, "kubelet.exe", []string{"--address=0.0.0.0", "--anonymous-auth=false"})
+				ValidateWindowsProcessHasCliArguments(ctx, s, "kubelet.exe", []string{"--rotate-certificates=true", "--client-ca-file=c:\\k\\ca.crt"})
 			},
 		},
 	})
