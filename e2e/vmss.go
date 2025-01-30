336babdd991f24626754769d3cf44318a574cc28
Tim Wright <wrightt@microsoft.com>
2025-01-30 04:42:30 +0000

    don't log cse

--- e2e/vmss.go
+++ e2e/vmss.go
@@ -77,8 +77,6 @@ func createVMSS(ctx context.Context, s *Scenario) *armcompute.VirtualMachineScal
 		cleanupVMSS(ctx, s)
 	})
 
-	s.T.Logf("CSE: \n---START-CSE---\n%s\n---END-CSE", cse)
-
 	vmssResp, err := operation.PollUntilDone(ctx, config.DefaultPollUntilDoneOptions)
 
 	// fail test, but continue to extract debug information
