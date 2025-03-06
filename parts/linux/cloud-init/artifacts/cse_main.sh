e200c71cd04602bfaf6b23a0f0f6e253b31a9216
Sylvain Boily <4981802+djsly@users.noreply.github.com>
2025-03-06 14:22:26 -0500

    disable verify_DNS_health

--- parts/linux/cloud-init/artifacts/cse_main.sh
+++ parts/linux/cloud-init/artifacts/cse_main.sh
@@ -76,7 +76,9 @@ registry_domain_name="mcr.microsoft.com"
 if [[ -n ${BOOTSTRAP_PROFILE_CONTAINER_REGISTRY_SERVER} ]]; then
     registry_domain_name="${BOOTSTRAP_PROFILE_CONTAINER_REGISTRY_SERVER%/}"
 fi
-verify_DNS_health $registry_domain_name || exit $ERR_DNS_HEALTH_FAIL
+
+# Disabling DNS health check until better E2E are created.
+# verify_DNS_health $registry_domain_name || exit $ERR_DNS_HEALTH_FAIL
 
 if [[ -n "${OUTBOUND_COMMAND}" ]]; then
     if [[ -n "${PROXY_VARS}" ]]; then
