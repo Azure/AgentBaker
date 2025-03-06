4bb22ebe7a9a56783f1e8964dea937280bb577fe
Henry Li <lihl@microsoft.com>
2025-03-05 19:07:34 -0800

    save vhd

--- e2e/config/vhd.go
+++ e2e/config/vhd.go
@@ -100,7 +100,7 @@ var (
 		Name:    "AzureLinuxV3gen2",
 		OS:      OSAzureLinux,
 		Arch:    "amd64",
-		Version: "1.1740610599.27113",
+		Version: "1.1741027729.8264",
 		Distro:  datamodel.AKSAzureLinuxV3Gen2,
 		Gallery: linuxGallery,
 	}
