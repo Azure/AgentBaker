e7a1f5f602b04ef391b62f6b77eaf74acc078a80
Tim Wright <wrightt@microsoft.com>
2025-03-06 00:54:49 +0000

    add github pipeline

--- vhdbuilder/scripts/windows/generate_cached_stuff_list.ps1
+++ vhdbuilder/scripts/windows/generate_cached_stuff_list.ps1
@@ -1,13 +1,57 @@
 
+param(
+    [string]
+    $outputDirParam,
+    [string]
+    $helpersFileParam,
+    [string]
+    $windowsSettingsFileParam,
+    [string]
+    $componentsJsonFileParam
+)
+
 $HelpersFile = "vhdbuilder/packer/windows/components_json_helpers.ps1"
 $WindowsSettingsFile = "vhdbuilder/packer/windows/windows_settings.json"
 $ComponentsJsonFile = "parts/common/components.json"
+$outputDir = "temp"
+
+
+if (![string]::IsNullOrEmpty($outputDirParam))
+{
+    Write-Output "Setting output dir to to to $outputDirParam"
+    $outputDir = $outputDirParam
+} else {
+    Write-Output "using default output dir: $outputDir"
+}
+
+if (![string]::IsNullOrEmpty($helpersFileParam))
+{
+    Write-Output "Setting helpers file to $helpersFileParam"
+    $HelpersFile = $helpersFileParam
+} else {
+    Write-Output "using default helpers file: $HelpersFile"
+}
+
+if (![string]::IsNullOrEmpty($windowsSettingsFileParam))
+{
+    Write-Output "Setting windows settings file to $windowsSettingsFileParam"
+    $WindowsSettingsFile = $windowsSettingsFileParam
+} else {
+    Write-Output "using default windows settings: $WindowsSettingsFile"
+}
+
+if (![string]::IsNullOrEmpty($componentsJsonFileParam))
+{
+    Write-Output "Setting components json file to to $componentsJsonFileParam"
+    $ComponentsJsonFile = $componentsJsonFileParam
+} else {
+    Write-Output "using default components json: $ComponentsJsonFile"
+}
 
 . "$HelpersFile"
 
 $componentsJson = Get-Content $ComponentsJsonFile | Out-String | ConvertFrom-Json
 $windowsSettingsJson = Get-Content $WindowsSettingsFile | Out-String | ConvertFrom-Json
-
 $BaseVersions = GetWindowsBaseVersions $windowsSettingsJson
 
 foreach ($WindowsSku in $BaseVersions)
@@ -22,7 +66,8 @@ foreach ($WindowsSku in $BaseVersions)
     $map = GetPackagesFromComponentsJson $componentsJson
     $releaseNotesToSet = GetKeyMapForReleaseNotes $windowsSettingsJson
 
-    $fileName = "parts/common/components_per_vhd/${WindowsSku}.txt"
+    $fileName = "${outputDir}/${WindowsSku}.txt"
+    Write-Output "Creating file $fileName"
     Write-Output $WindowsSku > $fileName
 
     Write-Output "---- Patch Data ----" >> $fileName
