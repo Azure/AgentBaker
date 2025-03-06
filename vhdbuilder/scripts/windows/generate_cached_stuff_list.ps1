359afafb615e2b5c5ce6569364493b0124b86e1e
Tim Wright <wrightt@microsoft.com>
2025-03-06 13:00:13 +1300

    add lists

---
+++ vhdbuilder/scripts/windows/generate_cached_stuff_list.ps1
@@ -0,0 +1,42 @@
+
+$HelpersFile = "vhdbuilder/packer/windows/components_json_helpers.ps1"
+$WindowsSettingsFile = "vhdbuilder/packer/windows/windows_settings.json"
+$ComponentsJsonFile = "parts/common/components.json"
+
+. "$HelpersFile"
+
+
+
+
+
+$BaseVersions = GetWindowsBaseVersions $windowsSettingsJson
+
+foreach ($WindowsSku in $BaseVersions)
+{
+
+    $componentsJson = Get-Content $ComponentsJsonFile | Out-String | ConvertFrom-Json
+    $windowsSettingsJson = Get-Content $WindowsSettingsFile | Out-String | ConvertFrom-Json
+    $patch_data = GetPatchInfo $windowsSKU $windowsSettingsJson
+    $patchUrls = $patch_data | % { $_.url }
+    $patchIDs = $patch_data | % { $_.id }
+
+    $imagesToPull = GetComponentsFromComponentsJson $componentsJson
+    $keysToSet = GetRegKeysToApply $windowsSettingsJson
+    $map = GetPackagesFromComponentsJson $componentsJson
+    $releaseNotesToSet = GetKeyMapForReleaseNotes $windowsSettingsJson
+
+    $fileName = "parts/common/components_per_vhd/${WindowsSku}.txt"
+    Write-Output $WindowsSku > $fileName
+
+    Write-Output "---- Patch Data ----" >> $fileName
+    echo $patchData | ConvertTo-Json | Write-Output >> $fileName
+    Write-Output "">> $fileName
+    Write-Output  "---- Container Images to Pull ----" >> $fileName
+    echo $imagesToPull | ConvertTo-Json | Write-Output >> $fileName
+    Write-Output "" >> $fileName
+    Write-Output  "---- Packages to Download ----" >> $fileName
+    echo $map | ConvertTo-Json | Write-Output >> $fileName
+    Write-Output  "---- Win Reg Keys ----" >> $fileName
+    echo $keysToSet | ConvertTo-Json | Write-Output >> $fileName
+    Write-Output "" >> $fileName
+}
