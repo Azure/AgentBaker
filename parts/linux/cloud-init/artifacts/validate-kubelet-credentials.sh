fc44cf7597c7383c63183c307561f7885c6c496d
Cameron Meissner <cameissner@microsoft.com>
2025-03-06 09:31:56 -0800

    chore: don't silence

--- parts/linux/cloud-init/artifacts/validate-kubelet-credentials.sh
+++ parts/linux/cloud-init/artifacts/validate-kubelet-credentials.sh
@@ -17,7 +17,7 @@ function validateKubeconfig {
     if ! retrycmd_if_failure $VALIDATE_KUBELET_CREDENTIALS_MAX_RETRIES \
         $VALIDATE_KUBELET_CREDENTIALS_RETRY_DELAY_SECONDS \
         $VALIDATE_KUBELET_CREDENTIALS_RETRY_TIMEOUT_SECONDS \
-        kubectl version --kubeconfig "$kubeconfig_path" >/dev/null; then
+        kubectl version --kubeconfig "$kubeconfig_path"; then
         
         # for now we simply exit 0 here to prevent provisioning failures in cases where the credential
         # doesn't become valid until after we've exhausted our retries - kubelet should still eventually be able to register
