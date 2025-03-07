80fcfe4a513cf8f1cbc18402a8ce1a30a9ba395f
Fan Shang Xiang <fanshangxiang@gmail.com>
2025-03-07 10:43:28 +0800

    Add 10 more retry on outbound command to ensure vmagent fetched the latest mapping (#6001)

--- parts/linux/cloud-init/artifacts/cse_main.sh
+++ parts/linux/cloud-init/artifacts/cse_main.sh
@@ -84,7 +84,7 @@ if [[ -n "${OUTBOUND_COMMAND}" ]]; then
     if [[ -n "${PROXY_VARS}" ]]; then
         eval $PROXY_VARS
     fi
-    retrycmd_if_failure 50 1 5 $OUTBOUND_COMMAND >> /var/log/azure/cluster-provision-cse-output.log 2>&1 || exit $ERR_OUTBOUND_CONN_FAIL;
+    retrycmd_if_failure 60 1 5 $OUTBOUND_COMMAND >> /var/log/azure/cluster-provision-cse-output.log 2>&1 || exit $ERR_OUTBOUND_CONN_FAIL;
 else
     # This file indicates the cluster doesn't have outbound connectivity and should be excluded in future external outbound checks
     touch /var/run/outbound-check-skipped
