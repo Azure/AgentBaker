56320c6fb01c56ba38b98c415198a8f8f2bd4f03
Henry Li <69694695+henryli001@users.noreply.github.com>
2025-03-07 16:39:51 -0800

    fix: fix mig-partition timeout to ensure the service completes (#5971)
    Co-authored-by: Henry Li <lihl@microsoft.com>

--- parts/linux/cloud-init/artifacts/cse_config.sh
+++ parts/linux/cloud-init/artifacts/cse_config.sh
@@ -14,7 +14,7 @@ configPrivateClusterHosts() {
 [Service]
 Environment="KUBE_API_SERVER_NAME=${API_SERVER_NAME}"
 EOF
-  systemctlEnableAndStart reconcile-private-hosts || exit $ERR_SYSTEMCTL_START_FAIL
+  systemctlEnableAndStart reconcile-private-hosts 30 || exit $ERR_SYSTEMCTL_START_FAIL
 }
 configureTransparentHugePage() {
     ETC_SYSFS_CONF="/etc/sysfs.conf"
@@ -337,7 +337,7 @@ net.bridge.bridge-nf-call-iptables = 1
 EOF
   retrycmd_if_failure 120 5 25 sysctl --system || exit $ERR_SYSCTL_RELOAD
   systemctl is-active --quiet docker && (systemctl_disable 20 30 120 docker || exit $ERR_SYSTEMD_DOCKER_STOP_FAIL)
-  systemctlEnableAndStart containerd || exit $ERR_SYSTEMCTL_START_FAIL
+  systemctlEnableAndStart containerd 30 || exit $ERR_SYSTEMCTL_START_FAIL
 }
 
 configureContainerdRegistryHost() {
@@ -356,11 +356,11 @@ EOF
 }
 
 ensureNoDupOnPromiscuBridge() {
-    systemctlEnableAndStart ensure-no-dup || exit $ERR_SYSTEMCTL_START_FAIL
+    systemctlEnableAndStart ensure-no-dup 30 || exit $ERR_SYSTEMCTL_START_FAIL
 }
 
 ensureTeleportd() {
-    systemctlEnableAndStart teleportd || exit $ERR_SYSTEMCTL_START_FAIL
+    systemctlEnableAndStart teleportd 30 || exit $ERR_SYSTEMCTL_START_FAIL
 }
 
 ensureArtifactStreaming() {
@@ -398,12 +398,12 @@ ensureDocker() {
         fi
     done
     systemctl is-active --quiet containerd && (systemctl_disable 20 30 120 containerd || exit $ERR_SYSTEMD_CONTAINERD_STOP_FAIL)
-    systemctlEnableAndStart docker || exit $ERR_DOCKER_START_FAIL
+    systemctlEnableAndStart docker 30 || exit $ERR_DOCKER_START_FAIL
 
 }
 
 ensureDHCPv6() {
-    systemctlEnableAndStart dhcpv6 || exit $ERR_SYSTEMCTL_START_FAIL
+    systemctlEnableAndStart dhcpv6 30 || exit $ERR_SYSTEMCTL_START_FAIL
     retrycmd_if_failure 120 5 25 modprobe ip6_tables || exit $ERR_MODPROBE_FAIL
 }
 
@@ -658,11 +658,11 @@ EOF
         logs_to_events "AKS.CSE.ensureKubelet.installCredentialProvider" installCredentialProvider
     fi
 
-    systemctlEnableAndStart kubelet || exit $ERR_KUBELET_START_FAIL
+    systemctlEnableAndStart kubelet 30 || exit $ERR_KUBELET_START_FAIL
 }
 
 ensureSnapshotUpdate() {
-    systemctlEnableAndStart snapshot-update.timer || exit $ERR_SNAPSHOT_UPDATE_START_FAIL
+    systemctlEnableAndStart snapshot-update.timer 30 || exit $ERR_SNAPSHOT_UPDATE_START_FAIL
 }
 
 ensureMigPartition(){
@@ -676,7 +676,7 @@ EOF
     # it MAY succeed, only due to unreliability of systemd
     # service type=Simple, which does not exit non-zero
     # on failure if ExecStart failed to invoke.
-    systemctlEnableAndStart mig-partition
+    systemctlEnableAndStart mig-partition 300
 }
 
 ensureSysctl() {
@@ -843,7 +843,7 @@ ensureGPUDrivers() {
         logs_to_events "AKS.CSE.ensureGPUDrivers.validateGPUDrivers" validateGPUDrivers
     fi
     if [[ $OS == $UBUNTU_OS_NAME ]]; then
-        logs_to_events "AKS.CSE.ensureGPUDrivers.nvidia-modprobe" "systemctlEnableAndStart nvidia-modprobe" || exit $ERR_GPU_DRIVERS_START_FAIL
+        logs_to_events "AKS.CSE.ensureGPUDrivers.nvidia-modprobe" "systemctlEnableAndStart nvidia-modprobe 30" || exit $ERR_GPU_DRIVERS_START_FAIL
     fi
 }
 
