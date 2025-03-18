c8c15b2b1840b41a271ef887efe01947d43d5db8
Tim Wright <wrightt@microsoft.com>
2025-03-18 21:54:54 +0000

    logging

--- vhdbuilder/release-notes/autonotes/main.go
+++ vhdbuilder/release-notes/autonotes/main.go
@@ -185,17 +185,19 @@ func getReleaseNotes(sku, path string, fl *flags) error {
 }
 
 func getReleaseNotesWindows(sku, path string, fl *flags) error {
-
 	releaseNotesName := fmt.Sprintf("vhd-release-notes-%s", sku)
 	imageListName := fmt.Sprintf("vhd-image-list-%s", sku)
 
 	artifactsDirOut := filepath.Join(fl.path, path)
-	if err := os.MkdirAll(filepath.Dir(artifactsDirOut), 0644); err != nil {
+	parentDirectory := filepath.Dir(artifactsDirOut)
+	fmt.Printf("Creating parent directory for sku'%s': '%s'\n", parentDirectory)
+	if err := os.MkdirAll(parentDirectory, 0644); err != nil {
 		return fmt.Errorf("failed to create parent directory %s with error: %s", artifactsDirOut, err)
 	}
 
+	fmt.Printf("Creating directory for sku '%s': '%s'\n", artifactsDirOut)
 	if err := os.MkdirAll(artifactsDirOut, 0644); err != nil {
-		return fmt.Errorf("failed to create parent directory %s with error: %s", artifactsDirOut, err)
+		return fmt.Errorf("failed to create directory %s with error: %s", artifactsDirOut, err)
 	}
 
 	fmt.Printf("downloading releaseNotes '%s' from windows build '%s'\n", releaseNotesName, fl.build)
