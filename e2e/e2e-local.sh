8af835fd40fceac534e5bc596264d5732f16adcb
Alison Burgess <alburgess@microsoft.com>
2025-03-25 12:32:16 -0700

    go mod tidy

--- e2e/e2e-local.sh
+++ e2e/e2e-local.sh
@@ -9,4 +9,5 @@ go version
 # Note, if you run "go test ./..." you won't see the output of the tests until they finish.
 # -count 1 disables caching of test results
 # default go test timeout is 10 minutes, it's not enough
+go mod tidy
 go test -parallel $PARALLEL -timeout $TIMEOUT -v -count 1
