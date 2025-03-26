39fb87b5b30f9afbac42fbc29b26d72f9053e0c1
Zachary Bailey <zbailey@microsoft.com>
2025-03-26 08:05:09 -0700

    chore: make generate

--- pkg/agent/testdata/AKSUbuntu2204+SecureTLSBoostrapping/line9.sh
+++ pkg/agent/testdata/AKSUbuntu2204+SecureTLSBoostrapping/line9.sh
@@ -649,16 +649,29 @@ updatePackageDownloadURL() {
     updateRelease "${package}" "${os}" "${osVersion}"
     local osLowerCase=$(echo "${os}" | tr '[:upper:]' '[:lower:]')
     
-    #if .downloadURIs.${osLowerCase} exist, then get the downloadURL from there.
+    #if .downloadURIs.${osLowerCase} exist, then get the downloadURL/packagesURL from there.
     #otherwise get the downloadURL from .downloadURIs.default 
     if [[ $(echo "${package}" | jq ".downloadURIs.${osLowerCase}") != "null" ]]; then
-        downloadURL=$(echo "${package}" | jq ".downloadURIs.${osLowerCase}.${RELEASE}.packagesURL" -r)
+        if [[ $(echo "${package}" | jq ".downloadURIs.${osLowerCase}.${RELEASE}.packagesURL") != "null" ]]; then
+            downloadURL=$(echo "${package}" | jq ".downloadURIs.${osLowerCase}.${RELEASE}.packagesURL" -r)
+            [ "${downloadURL}" = "null" ] && PACKAGE_DOWNLOAD_URL="" || PACKAGE_DOWNLOAD_URL="${downloadURL}"
+            return
+        else
+            downloadURL=$(echo "${package}" | jq ".downloadURIs.${osLowerCase}.${RELEASE}.downloadURL" -r)
+            [ "${downloadURL}" = "null" ] && PACKAGE_DOWNLOAD_URL="" || PACKAGE_DOWNLOAD_URL="${downloadURL}"
+            return
+        fi
+    fi
+
+    if [[ $(echo "${package}" | jq ".downloadURIs.default.${RELEASE}.packagesURL") != "null" ]]; then
+        downloadURL=$(echo "${package}" | jq ".downloadURIs.default.${RELEASE}.packagesURL" -r)
         [ "${downloadURL}" = "null" ] && PACKAGE_DOWNLOAD_URL="" || PACKAGE_DOWNLOAD_URL="${downloadURL}"
         return
     fi
-    downloadURL=$(echo "${package}" | jq ".downloadURIs.default.${RELEASE}.packagesURL" -r)
+
+    downloadURL=$(echo "${package}" | jq ".downloadURIs.default.${RELEASE}.downloadURL" -r)
     [ "${downloadURL}" = "null" ] && PACKAGE_DOWNLOAD_URL="" || PACKAGE_DOWNLOAD_URL="${downloadURL}"
-    return    
+    return
 }
 
 addKubeletNodeLabel() {
