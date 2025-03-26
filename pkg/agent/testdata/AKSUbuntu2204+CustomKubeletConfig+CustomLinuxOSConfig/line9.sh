a6091210cfa88930f9411733b10584288e4f3516
Xinhe Li <xinhl@microsoft.com>
2025-03-26 17:11:45 +0000

    ut2

--- pkg/agent/testdata/AKSUbuntu2204+CustomKubeletConfig+CustomLinuxOSConfig/line9.sh
+++ pkg/agent/testdata/AKSUbuntu2204+CustomKubeletConfig+CustomLinuxOSConfig/line9.sh
@@ -769,7 +769,7 @@ oras_login_with_kubelet_identity() {
 
     set +x 
     access_url="http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/&client_id=$client_id"
-    raw_access_token=$(retrycmd_get_access_token_for_oras 10 5 $access_url)
+    raw_access_token=$(retrycmd_get_access_token_for_oras 30 10 $access_url)
     ret_code=$? 
     if [ $ret_code -ne 0 ] || [[ "$raw_access_token" == *"error"* ]]; then
         echo $raw_access_token
