4b31f7fbbfcc6ac413f1d90bdb4995909a4e0346
Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>
2025-03-26 13:22:21 -0700

    Revert "Add VHD as parameter"
    This reverts commit 8ad9b44c9a36cd9f256b731b4767496e82418515.

--- e2e/scenario_test.go
+++ e2e/scenario_test.go
@@ -1007,6 +1007,9 @@ func Test_Ubuntu2204_GPUA100(t *testing.T) {
 	runScenarioUbuntu2204GPU(t, "Standard_NC24ads_A100_v4")
 }
 
+func Test_Ubuntu2204_GPUA10(t *testing.T) {
+	runScenarioUbuntuGRID(t, "Standard_NV6ads_A10_v5")
+}
 
 // Returns config for the 'gpu' E2E scenario
 func runScenarioUbuntu2204GPU(t *testing.T, vmSize string) {
@@ -1037,16 +1040,15 @@ func runScenarioUbuntu2204GPU(t *testing.T, vmSize string) {
 	})
 }
 
-
-func runScenarioUbuntuGRID(t *testing.T, vmSize string, vhdType *Image) {
+func runScenarioUbuntuGRID(t *testing.T, vmSize string) {
 	RunScenario(t, &Scenario{
-		Description: fmt.Sprintf("Tests that a GPU-enabled node with VM size %s using an Ubuntu % VHD can be properly bootstrapped, and that the GRID license is valid", vmSize, vhdType.Name),
+		Description: fmt.Sprintf("Tests that a GPU-enabled node with VM size %s using an Ubuntu 2204 VHD can be properly bootstrapped, and that the GRID license is valid", vmSize),
 		Tags: Tags{
 			GPU: true,
 		},
 		Config: Config{
 			Cluster: ClusterKubenet,
-			VHD:     vhdType,
+			VHD:     config.VHDUbuntu2204Gen2Containerd,
 			BootstrapConfigMutator: func(nbc *datamodel.NodeBootstrappingConfiguration) {
 				nbc.AgentPoolProfile.VMSize = vmSize
 				nbc.ConfigGPUDriverIfNeeded = true
@@ -1068,12 +1070,34 @@ func runScenarioUbuntuGRID(t *testing.T, vmSize string, vhdType *Image) {
 	})
 }
 
-func Test_Ubuntu2204_GPUA10(t *testing.T) {
-	runScenarioUbuntuGRID(t, "Standard_NV6ads_A10_v5")
-}
-
 func Test_Ubuntu2404_GPUA10(t *testing.T) {
-	runScenarioUbuntuGRID(t, "Standard_NV6ads_A10_v5", config.VHDUbuntu2404Gen2Containerd)
+	RunScenario(t, &Scenario{
+		Description: fmt.Sprintf("Tests that a GPU-enabled node with VM size %s using an Ubuntu 2204 VHD can be properly bootstrapped, and that the GRID license is valid", vmSize),
+		Tags: Tags{
+			GPU: true,
+		},
+		Config: Config{
+			Cluster: ClusterKubenet,
+			VHD:     config.VHDUbuntu2404Gen2Containerd,
+			BootstrapConfigMutator: func(nbc *datamodel.NodeBootstrappingConfiguration) {
+				nbc.AgentPoolProfile.VMSize = vmSize
+				nbc.ConfigGPUDriverIfNeeded = true
+				nbc.EnableGPUDevicePluginIfNeeded = false
+				nbc.EnableNvidia = true
+			},
+			VMConfigMutator: func(vmss *armcompute.VirtualMachineScaleSet) {
+				vmss.SKU.Name = to.Ptr(vmSize)
+			},
+			Validator: func(ctx context.Context, s *Scenario) {
+				// Ensure nvidia-modprobe install does not restart kubelet and temporarily cause node to be unschedulable
+				ValidateNvidiaModProbeInstalled(ctx, s)
+				ValidateNvidiaGRIDLicenseValid(ctx, s)
+				ValidateKubeletHasNotStopped(ctx, s)
+				ValidateServicesDoNotRestartKubelet(ctx, s)
+				ValidateNvidiaPersistencedRunning(ctx, s)
+			},
+		},
+	})
 }
 
 func Test_Ubuntu2204_GPUA10_Scriptless(t *testing.T) {
