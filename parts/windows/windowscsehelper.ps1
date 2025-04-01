398d8a3f1bc8cf70a258abf6f0fe630a0edf9efc
Zachary Bailey <zbailey@microsoft.com>
2025-04-01 14:16:52 -0700

    fix: move functions to appropriate file

--- parts/windows/windowscsehelper.ps1
+++ parts/windows/windowscsehelper.ps1
@@ -473,3 +473,53 @@ function Logs-To-Event {
 "@
     echo $jsonString | Set-Content ${global:EventsLoggingDir}${eventsFileName}.json
 }
+
+function ResolvePackagesSourceUrl {
+  param (
+    [string]$url
+    [int]$maxAttempts = 5
+  )
+
+  $attempt = 0
+  $PACKAGES_DOWNLOAD_BASE_URL=""
+
+  while ($attempt -lt $maxAttempts) 
+  {
+    try 
+    {
+      $response = Invoke-WebRequest -Uri $url -Method Head -ErrorAction Stop
+
+      if ($response.StatusCode -eq 200) 
+      {
+        $PACKAGES_DOWNLOAD_BASE_URL="packages.aks.azure.com"
+        break
+      }
+    }
+    catch 
+    {
+      $attempt++
+      Start-Sleep -Seconds 1
+    }
+  }
+
+  if (-not $PACKAGES_DOWNLOAD_BASE_URL) 
+  {
+    $PACKAGES_DOWNLOAD_BASE_URL="acs-mirror.azureedge.net"
+  }
+
+  Write-Host "Packages source URL is $PACKAGES_DOWNLOAD_BASE_URL"
+
+  return $PACKAGES_DOWNLOAD_BASE_URL
+}
+
+function ReplaceBaseUrl {
+  param (
+    [string]$url
+    [string]$packagesUrl
+  )
+
+  if ($url.Contains("acs-mirror.azureedge.net") -and $packagesUrl -eq "packages.aks.azure.com")
+  {
+    $url = $url.Replace("acs-mirror.azureedge.net", $packagesUrl)
+  }
+}
