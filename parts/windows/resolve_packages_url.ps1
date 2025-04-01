9ff2e84463b9c4146b265e53c614190078786cb5
Zachary Bailey <zbailey@microsoft.com>
2025-04-01 11:28:46 -0700

    fix: styling

--- parts/windows/resolve_packages_url.ps1
+++ parts/windows/resolve_packages_url.ps1
@@ -7,22 +7,27 @@ function ResolvePackagesSourceURL {
   $attempt = 0
   $PACKAGES_DOWNLOAD_BASE_URL=""
 
-  while ($attempt -lt $maxAttempts) {
-    try {
+  while ($attempt -lt $maxAttempts) 
+  {
+    try 
+    {
       $response = Invoke-WebRequest -Uri $url -Method Head -ErrorAction Stop
 
-      if ($response.StatusCode -eq 200) {
+      if ($response.StatusCode -eq 200) 
+      {
         $PACKAGES_DOWNLOAD_BASE_URL="packages.aks.azure.com"
         break
       }
     }
-    catch {
+    catch 
+    {
       $attempt++
       Start-Sleep -Seconds 1
     }
   }
 
-  if (-not $PACKAGES_DOWNLOAD_BASE_URL) {
+  if (-not $PACKAGES_DOWNLOAD_BASE_URL) 
+  {
     $PACKAGES_DOWNLOAD_BASE_URL="acs-mirror.azureedge.net"
   }
 
