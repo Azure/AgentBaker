0e8d6bd8ccb621e1fb301f8e93290f6900fe7eac
Tim Wright <wrightt@microsoft.com>
2025-04-02 01:00:14 +0000

    extract login

--- packer.mk
+++ packer.mk
@@ -105,9 +105,6 @@ generate-publishing-info: az-login
 convert-sig-to-classic-storage-account-blob: az-login
 	@./vhdbuilder/packer/convert-sig-to-classic-storage-account-blob.sh
 
-test-building-vhd: az-login
-	@./vhdbuilder/packer/test/run-test.sh
-
 scanning-vhd: az-login
 	@./vhdbuilder/packer/vhd-scanning.sh
 
