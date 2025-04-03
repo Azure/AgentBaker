170e742271a61bba5d31f00b65499f462de3efcc
Zachary Bailey <zbailey@microsoft.com>
2025-04-03 09:45:08 -0700

    fix: styling

--- parts/linux/cloud-init/artifacts/cse_install.sh
+++ parts/linux/cloud-init/artifacts/cse_install.sh
@@ -383,6 +383,7 @@ downloadAzureCNI() {
     if [ $PACKAGE_DOWNLOAD_BASE_URL == "packages.aks.azure.com" ]; then
         VNET_CNI_PLUGINS_URL="${VNET_CNI_PLUGINS_URL//"acs-mirror.azureedge.net"/$PACKAGE_DOWNLOAD_BASE_URL}"
     fi
+    
     CNI_TGZ_TMP=${VNET_CNI_PLUGINS_URL##*/} # Use bash builtin ## to remove all chars ("*") up to the final "/"
     retrycmd_get_tarball 120 5 "$CNI_DOWNLOADS_DIR/${CNI_TGZ_TMP}" ${VNET_CNI_PLUGINS_URL} || exit $ERR_CNI_DOWNLOAD_TIMEOUT
 }
