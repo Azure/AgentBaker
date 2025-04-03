12a55f9984e799dabad7830c5e7523e02e7ba57e
Zachary Bailey <zbailey@microsoft.com>
2025-04-03 08:57:30 -0700

    chore: make generate

--- pkg/agent/testdata/AKSUbuntu1804+Containerd+runcshimv2/line9.sh
+++ pkg/agent/testdata/AKSUbuntu1804+Containerd+runcshimv2/line9.sh
@@ -767,7 +767,7 @@ resolve_packages_source_url() {
     local wait_sleep=1
 
     for i in $(seq 1 $retries); do
-      response_code=$(curl -s -o /dev/null -w "%{http_code}" --max-time 5 https://packages.aks.azure.com/acs-mirror/healthz)
+      response_code=$(curl -s -o --noproxy /dev/null -w "%{http_code}" --max-time 5 https://packages.aks.azure.com/acs-mirror/healthz)
       if [ ${response_code} -eq 200 ]; then
         break
       else
