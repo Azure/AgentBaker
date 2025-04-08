c5969a06311870f9b54d9e8456291414d1dfca40
Zachary Bailey <zbailey@microsoft.com>
2025-04-08 08:36:58 -0700

    chore: make generate

--- pkg/agent/testdata/AKSUbuntu2204+IMDSRestrictionOff/line40.sh
+++ pkg/agent/testdata/AKSUbuntu2204+IMDSRestrictionOff/line40.sh
@@ -141,7 +141,7 @@ downloadCredentialProvider() {
     local cred_provider_url=$2
     if [[ -n $cred_provider_url ]]; then
         CREDENTIAL_PROVIDER_DOWNLOAD_URL=$cred_provider_url
-        logs_to_events "AKS.CSE.logDownloadURL" update_base_url $CREDENTIAL_PROVIDER_DOWNLOAD_URL
+        logs_to_events "AKS.CSE.logDownloadURL" "update_base_url $CREDENTIAL_PROVIDER_DOWNLOAD_URL"
     fi
 
     mkdir -p $CREDENTIAL_PROVIDER_DOWNLOAD_DIR
@@ -218,7 +218,7 @@ installContainerdWasmShims(){
             shims_to_download+=("wws")
         fi
         containerd_wasm_url=$(evalPackageDownloadURL ${PACKAGE_DOWNLOAD_URL})
-        logs_to_events "AKS.CSE.logDownloadURL" update_base_url $containerd_wasm_url
+        logs_to_events "AKS.CSE.logDownloadURL" "update_base_url $containerd_wasm_url"
         downloadContainerdWasmShims $download_location $containerd_wasm_url "$version" "${shims_to_download[@]}"
     done
     wait ${WASMSHIMPIDS[@]}
@@ -281,7 +281,7 @@ installSpinKube(){
 
     for version in "${package_versions[@]}"; do
         containerd_spinkube_url=$(evalPackageDownloadURL ${PACKAGE_DOWNLOAD_URL})
-        logs_to_events "AKS.CSE.logDownloadURL" update_base_url $containerd_spinkube_url
+        logs_to_events "AKS.CSE.logDownloadURL" "update_base_url $containerd_spinkube_url"
         downloadSpinKube $download_location $containerd_spinkube_url "$version"
     done
     wait ${SPINKUBEPIDS[@]}
@@ -355,7 +355,7 @@ downloadAzureCNI() {
         return
     fi
 
-    logs_to_events "AKS.CSE.logDownloadURL" update_base_url $VNET_CNI_PLUGINS_URL
+    logs_to_events "AKS.CSE.logDownloadURL" "update_base_url $VNET_CNI_PLUGINS_URL"
 
     CNI_TGZ_TMP=${VNET_CNI_PLUGINS_URL##*/} # Use bash builtin #
     retrycmd_get_tarball 120 5 "$CNI_DOWNLOADS_DIR/${CNI_TGZ_TMP}" ${VNET_CNI_PLUGINS_URL} || exit $ERR_CNI_DOWNLOAD_TIMEOUT
@@ -366,7 +366,7 @@ downloadCrictl() {
     downloadDir=${1:-${CRICTL_DOWNLOAD_DIR}}
     mkdir -p $downloadDir
     url=${2}
-    logs_to_events "AKS.CSE.logDownloadURL" update_base_url $url
+    logs_to_events "AKS.CSE.logDownloadURL" "update_base_url $url"
     crictlTgzTmp=${url##*/}
     retrycmd_curl_file 10 5 60 "$downloadDir/${crictlTgzTmp}" ${url} || exit $ERR_CRICTL_DOWNLOAD_TIMEOUT
 }
@@ -419,7 +419,7 @@ installTeleportdPlugin() {
     if semverCompare ${CURRENT_VERSION:-"0.0.0"} ${TARGET_VERSION}; then
         echo "currently installed teleportd version ${CURRENT_VERSION} is greater than (or equal to) target base version ${TARGET_VERSION}. skipping installTeleportdPlugin."
     else
-        logs_to_events "AKS.CSE.logDownloadURL" update_base_url $TELEPORTD_PLUGIN_DOWNLOAD_URL
+        logs_to_events "AKS.CSE.logDownloadURL" "update_base_url $TELEPORTD_PLUGIN_DOWNLOAD_URL"
         downloadTeleportdPlugin ${TELEPORTD_PLUGIN_DOWNLOAD_URL} ${TARGET_VERSION}
         mv "${TELEPORTD_PLUGIN_DOWNLOAD_DIR}/teleportd-v${TELEPORTD_VERSION}" "${TELEPORTD_PLUGIN_BIN_DIR}/teleportd" || exit ${ERR_TELEPORTD_INSTALL_ERR}
         chmod 755 "${TELEPORTD_PLUGIN_BIN_DIR}/teleportd" || exit ${ERR_TELEPORTD_INSTALL_ERR}
@@ -525,7 +525,7 @@ extractKubeBinaries() {
     local is_private_url="$3"
     local k8s_downloads_dir=${4:-"/opt/kubernetes/downloads"}
 
-    logs_to_events "AKS.CSE.logDownloadURL" update_base_url $kube_binary_url
+    logs_to_events "AKS.CSE.logDownloadURL" "update_base_url $kube_binary_url"
     local k8s_tgz_tmp_filename=${kube_binary_url##*/}
 
     if [[ $is_private_url == true ]]; then
