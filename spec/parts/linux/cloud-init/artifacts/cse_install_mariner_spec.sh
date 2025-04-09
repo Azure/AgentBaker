0812e090be76fdcdbf8c731d98015e8c9db8aaed
Devin Wong <wongsiosun@outlook.com>
2025-04-08 17:54:45 -0700

    fix unit test

--- spec/parts/linux/cloud-init/artifacts/cse_install_mariner_spec.sh
+++ spec/parts/linux/cloud-init/artifacts/cse_install_mariner_spec.sh
@@ -21,17 +21,11 @@ Describe 'cse_install_mariner.sh'
             OS_VERSION="2.0"
             When run installDeps
             The stdout should include "dnf install 30 1 600 mariner-repos-cloud-native"
-            for dnf_package in ca-certificates check-restart cifs-utils cloud-init-azure-kvp conntrack-tools cracklib dnf-automatic ebtables ethtool fuse git inotify-tools iotop iproute ipset iptables jq kernel-devel logrotate lsof nmap-ncat nfs-utils pam pigz psmisc rsyslog socat sysstat traceroute util-linux xz zip blobfuse2 nftables iscsi-initiator-utils; do
-                The stdout should include "dnf install 30 1 600 $dnf_package"
-            done
         End
         It 'installs the required packages with installDeps for AzureLinux 3.0'
             OS_VERSION="3.0"
             When run installDeps
             The stdout should include "dnf install 30 1 600 azurelinux-repos-cloud-native"
-            for dnf_package in ca-certificates check-restart cifs-utils cloud-init-azure-kvp conntrack-tools cracklib dnf-automatic ebtables ethtool fuse git inotify-tools iotop iproute ipset iptables jq kernel-devel logrotate lsof nmap-ncat nfs-utils pam pigz psmisc rsyslog socat sysstat traceroute util-linux xz zip blobfuse2 nftables iscsi-initiator-utils; do
-                The stdout should include "dnf install 30 1 600 $dnf_package"
-            done
         End
     End
 End
