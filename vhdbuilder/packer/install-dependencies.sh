7133897f67aa02f577d15a8cf2089d576ff1c7ff
Devin Wong <wongsiosun@outlook.com>
2025-04-11 12:37:10 -0700

    fix

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -58,6 +58,7 @@ APT::Periodic::Unattended-Upgrade "0";
 EOF
 fi
 
+updateAptWithMicrosoftPkg
 # If the IMG_SKU does not contain "minimal", installDeps normally
 if [[ "$IMG_SKU" != *"minimal"* ]]; then
   installDeps
