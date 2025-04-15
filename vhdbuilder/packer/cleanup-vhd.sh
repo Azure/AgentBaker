07e945b96f744a65e6b80742e20d02cc35be29a1
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2025-04-15 17:10:23 +0200

    vhdbuilder: Upload and cleanup cis scanner
    Signed-off-by: Jeremi Piotrowski <jpiotrowski@microsoft.com>

--- vhdbuilder/packer/cleanup-vhd.sh
+++ vhdbuilder/packer/cleanup-vhd.sh
@@ -3,6 +3,7 @@
 # Cleanup packer SSH key and machine ID generated for this boot
 rm -f /root/.ssh/authorized_keys
 rm -f /home/packer/.ssh/authorized_keys
+rm -rf /home/packer/cisassessor*
 rm -f /var/log/cloud-init.log /var/log/cloud-init-output.log 
 rm -f /etc/machine-id
 touch /etc/machine-id
