0a3a21055fba1f49bc5bde4a7a753c1557399ffe
Devin Wong <wongsiosun@outlook.com>
2025-04-23 14:02:12 -0700

    fix concurrency

--- pkg/agent/testdata/AKSUbuntu1604+K8S115/line40.sh
+++ pkg/agent/testdata/AKSUbuntu1604+K8S115/line40.sh
@@ -244,13 +244,7 @@ downloadContainerdWasmShims() {
     fi
 
     for shim in "${shims_to_download[@]}"; do
-        output_file="$containerd_wasm_filepath/containerd-shim-${shim}-${binary_version}-v1"
-        download_url="$containerd_wasm_url/containerd-shim-${shim}-v1"
-        retrycmd_if_failure 30 5 60 curl -fSLv -o "$output_file" "$download_url" 2>&1 | tee $CURL_OUTPUT &
-        if grep -E "^(curl:.*)|([eE]rr.*)$" $CURL_OUTPUT; then
-            cat $CURL_OUTPUT
-            exit $ERR_KRUSTLET_DOWNLOAD_TIMEOUT
-        fi
+        retrycmd_if_failure 30 5 60 curl -fSLv -o "$containerd_wasm_filepath/containerd-shim-${shim}-${binary_version}-v1" "$containerd_wasm_url/containerd-shim-${shim}-v1" 2>&1 | tee $CURL_OUTPUT | grep -E "^(curl:.*)|([eE]rr.*)$" && (cat $CURL_OUTPUT && exit $ERR_KRUSTLET_DOWNLOAD_TIMEOUT) &
         WASMSHIMPIDS+=($!)
     done
 }
@@ -304,13 +298,7 @@ downloadSpinKube(){
         return 
     fi
     
-    output_file="$containerd_spinkube_filepath/containerd-shim-spin-v2"
-    download_url="$containerd_spinkube_url/containerd-shim-spin-v2"
-    retrycmd_if_failure 30 5 60 curl -fSLv -o "$output_file" "$download_url" 2>&1 | tee $CURL_OUTPUT &
-    if grep -E "^(curl:.*)|([eE]rr.*)$" $CURL_OUTPUT; then
-        cat $CURL_OUTPUT
-        exit $ERR_KRUSTLET_DOWNLOAD_TIMEOUT
-    fi
+    retrycmd_if_failure 30 5 60 curl -fSLv -o "$containerd_spinkube_filepath/containerd-shim-spin-v2" "$containerd_spinkube_url/containerd-shim-spin-v2" 2>&1 | tee $CURL_OUTPUT | grep -E "^(curl:.*)|([eE]rr.*)$" && (cat $CURL_OUTPUT && exit $ERR_KRUSTLET_DOWNLOAD_TIMEOUT) &
     SPINKUBEPIDS+=($!)
 }
 
