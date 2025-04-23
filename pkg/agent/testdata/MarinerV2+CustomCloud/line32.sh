1dddc8e8571c4c3f1ccafacd709853a672f54f1e
Mikolaj Umanski <mik.umanski@gmail.com>
2025-04-23 22:07:46 +0000

    fix: exit CSE if enableLocalDNS fails

--- pkg/agent/testdata/MarinerV2+CustomCloud/line32.sh
+++ pkg/agent/testdata/MarinerV2+CustomCloud/line32.sh
@@ -352,7 +352,7 @@ if [ $VALIDATION_ERR -ne 0 ]; then
     exit $VALIDATION_ERR
 fi
 
-logs_to_events "AKS.CSE.enableLocalDNS" enableLocalDNS
+logs_to_events "AKS.CSE.enableLocalDNS" enableLocalDNS || exit $?
 
 logs_to_events "AKS.CSE.ensureKubelet" ensureKubelet
 
