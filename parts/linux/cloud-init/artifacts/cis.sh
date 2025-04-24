ab86eee0a6a4c60a68f0764e806ea766bc5c3339
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2025-04-24 13:33:40 +0200

    feat(cis): Set root password parameters

--- parts/linux/cloud-init/artifacts/cis.sh
+++ parts/linux/cloud-init/artifacts/cis.sh
@@ -18,6 +18,8 @@ assignRootPW() {
         echo 'root:'$HASH | /usr/sbin/chpasswd -e || exit $ERR_CIS_ASSIGN_ROOT_PW
     fi
     set -x
+    chage --maxdays 90 root
+    chage --inactive 30 root
 }
 
 assignFilePermissions() {
