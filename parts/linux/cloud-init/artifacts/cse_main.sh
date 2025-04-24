e85ee19980a27796d10917bac65ac7312146f174
Devin Wong <wongsiosun@outlook.com>
2025-04-24 16:44:26 -0700

    fix minor

--- parts/linux/cloud-init/artifacts/cse_main.sh
+++ parts/linux/cloud-init/artifacts/cse_main.sh
@@ -133,7 +133,7 @@ export -f should_skip_binary_cleanup
 SKIP_BINARY_CLEANUP=$(retrycmd_if_failure_no_stats 10 1 10 bash -cx should_skip_binary_cleanup)
 # this needs better fix to separate logs and return value;
 FULL_INSTALL_REQUIRED=$(getInstallModeAndCleanupContainerImages "$SKIP_BINARY_CLEANUP" "$IS_VHD" | tail -1)
-if [ "$?" != 0 ]; then
+if [ "$?" -ne 0 ]; then
     echo "Failed to get the install mode and cleanup container images"
     exit "$ERR_CLEANUP_CONTAINER_IMAGES"
 fi
