c69b79c686d73b6f29624e557e68544ce188c2dc
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2025-04-29 09:50:16 +0000

    parts: cse_install_flatcar: Keep containerd and cleanup GPU driver

--- parts/linux/cloud-init/artifacts/flatcar/cse_install_flatcar.sh
+++ parts/linux/cloud-init/artifacts/flatcar/cse_install_flatcar.sh
@@ -2,4 +2,14 @@
 
 echo "Sourcing cse_install_distro.sh for Flatcar"
 
+cleanUpGPUDrivers() {
+    rm -Rf $GPU_DEST /opt/gpu
+}
+
+# Use Flatcar provided containerd
+installStandaloneContainerd() {
+    local desiredVersion="${1:-}"
+    CURRENT_VERSION=$(containerd -version | cut -d " " -f 3)
+    echo "currently installed containerd version: ${CURRENT_VERSION}. Desired version ${desiredVersion}. Skipping installStandaloneContainerd on Flatcar."
+}
 #EOF
