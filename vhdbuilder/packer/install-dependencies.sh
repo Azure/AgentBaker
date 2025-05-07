1003522c1b6336ec756ab120be7b67472187912e
Cameron Meissner <cameronmeissner@gmail.com>
2025-05-07 16:22:32 -0700

    feat(stls): install aks-secure-tls-bootstrap-client binary on linux VHDs (#6332)

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -348,6 +348,14 @@ while IFS= read -r p; do
         echo "  - oras version ${version}" >> ${VHD_LOGS_FILEPATH}
       done
       ;;
+    "aks-secure-tls-bootstrap-client")
+      for version in ${PACKAGE_VERSIONS[@]}; do
+        # removed at provisioning time if secure TLS bootstrapping is disabled
+        evaluatedURL=$(evalPackageDownloadURL ${PACKAGE_DOWNLOAD_URL})
+        downloadSecureTLSBootstrapClient "${downloadDir}" "${evaluatedURL}" "${version}"
+        echo "  - aks-secure-tls-bootstrap-client version ${version}" >> ${VHD_LOGS_FILEPATH}
+      done
+      ;;
     "azure-acr-credential-provider")
       for version in ${PACKAGE_VERSIONS[@]}; do
         evaluatedURL=$(evalPackageDownloadURL ${PACKAGE_DOWNLOAD_URL})
