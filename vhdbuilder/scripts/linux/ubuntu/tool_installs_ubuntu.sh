859243f3e0ed794a1af89f68076d31b681460202
Zachary Bailey <zbailey@microsoft.com>
2025-05-08 10:59:39 -0700

    fix: ua attach bug

--- vhdbuilder/scripts/linux/ubuntu/tool_installs_ubuntu.sh
+++ vhdbuilder/scripts/linux/ubuntu/tool_installs_ubuntu.sh
@@ -240,7 +240,12 @@ listInstalledPackages() {
 
 attachUA() {
     echo "attaching ua..."
-    retrycmd_silent 5 10 120 ua attach $UA_TOKEN || exit $ERR_UA_ATTACH
+    retrycmd_silent 5 10 120 ua attach $UA_TOKEN
+    if [ $? -ne 0 ]; then
+        echo "UA attach failed, attempting detach and then reattempting..."
+        detachAndCleanUpUA
+        retrycmd_silent 5 10 120 ua attach $UA_TOKEN || exit $ERR_UA_ATTACH
+    fi
 
     echo "disabling ua livepatch..."
     retrycmd_if_failure 5 10 300 echo y | ua disable livepatch
