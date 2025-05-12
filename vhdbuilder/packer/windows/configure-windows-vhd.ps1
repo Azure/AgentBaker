5341f93f6ef045afd550ef35f86505ed24394435
Beilei Huang <beileihuang@microsoft.com>
2025-05-12 15:19:39 +1200

    fix: update crictl command to specify configuration file for image pulling

--- vhdbuilder/packer/windows/configure-windows-vhd.ps1
+++ vhdbuilder/packer/windows/configure-windows-vhd.ps1
@@ -337,6 +337,7 @@ function Get-ContainerImages
         {   
             Write-Log "Pulling image $image"
             Retry-Command -ScriptBlock {
+                # TODO: remove when crictl.exe can find the default config file
                 $crictlPath = (Get-Command crictl.exe -ErrorAction SilentlyContinue).Path
                 $configPath = Join-Path (Split-Path -Parent $crictlPath) "crictl.yaml"
                 & crictl.exe -c $configPath pull $image
