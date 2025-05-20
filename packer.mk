acd65ec2d48e374ffff759886aa80ce73294d320
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2025-05-20 16:25:47 +0200

    fix: Stop passing managed identity ID with --username (#6397)

--- packer.mk
+++ packer.mk
@@ -77,11 +77,11 @@ ifeq ($(origin MANAGED_IDENTITY_ID), undefined)
 	@az login --identity
 else
 	@echo "Logging in with Hosted Pool's Managed Identity: ${MANAGED_IDENTITY_ID}"
-	@az login --identity --username ${MANAGED_IDENTITY_ID}
+	@az login --identity --client-id ${MANAGED_IDENTITY_ID}
 endif
 else
 	@echo "Logging into Azure with identity: ${AZURE_MSI_RESOURCE_STRING}..."
-	@az login --identity --username ${AZURE_MSI_RESOURCE_STRING}
+	@az login --identity --resource-id ${AZURE_MSI_RESOURCE_STRING}
 endif
 	@echo "Using the subscription ${SUBSCRIPTION_ID}"
 	@az account set -s ${SUBSCRIPTION_ID}
