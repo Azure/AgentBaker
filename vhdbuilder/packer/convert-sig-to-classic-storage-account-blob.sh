ce23b64e30d94eda1d58af41e50db346b961d783
Cameron Meissner <cameronmeissner@gmail.com>
2025-05-21 09:53:29 -0700

    chore: cleanup

--- vhdbuilder/packer/convert-sig-to-classic-storage-account-blob.sh
+++ vhdbuilder/packer/convert-sig-to-classic-storage-account-blob.sh
@@ -55,7 +55,7 @@ capture_benchmark "${SCRIPT_NAME}_set_variables_for_converting_to_disk"
 
 echo "Converting $sig_resource_id to $disk_resource_id"
 if [ "${OS_TYPE}" = "Linux" ] && [ "${ENABLE_TRUSTED_LAUNCH}" = "True" ]; then
-  az resource create --id $disk_resource_id  --is-full-object --api-version 2024-03-02 --location $LOCATION --properties "{\"location\": \"$LOCATION\", \
+  az resource create --id $disk_resource_id  --api-version 2024-03-02 --is-full-object --location $LOCATION --properties "{\"location\": \"$LOCATION\", \
     \"properties\": { \
       \"osType\": \"$OS_TYPE\", \
       \"securityProfile\": { \
