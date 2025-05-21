54d96a71e90d2e0f3c3503c60a445af4bd9a5de8
Zachary Bailey <zachary.r.bailey@outlook.com>
2025-05-21 13:59:28 -0700

    test: create checksum file

--- vhdbuilder/packer/sign-vhd.sh
+++ vhdbuilder/packer/sign-vhd.sh
@@ -9,7 +9,7 @@ export AZCOPY_MSI_RESOURCE_STRING="$AZURE_MSI_RESOURCE_STRING"
 export AZCOPY_CONCURRENCY_VALUE="AUTO"
 
 # Dynamically determine url for checksum
-azcopy copy "${checksum_url}${sas_token}" "$VHD_RELEASE_CANDIDATE" --recursive=true
+azcopy copy "${CLASSIC_BLOB}/${CAPTURED_SIG_VERSION}.vhd${sas_token}" "$VHD_RELEASE_CANDIDATE" --recursive=true
 
 echo "Calculating checksum..."
 
@@ -19,6 +19,4 @@ echo "Digitally signing sha256 file..."
 
 gpg --armor --detach-sign --output "${VHD_RELEASE_CANDIDATE}.sha256.asc" "${VHD_RELEASE_CANDIDATE}.sha256"
 
-
-echo "Printing checksum..."
-cat mydisk.sha256   
+echo "VHD file checksum created and signed"
