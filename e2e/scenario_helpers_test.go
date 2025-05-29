907a29bac481d8e96903f43b2eb2bf03d475c773
Tim Wright <wrightt@microsoft.com>
2025-05-29 02:21:46 +0000

    don't overwrite windows cse

--- e2e/scenario_helpers_test.go
+++ e2e/scenario_helpers_test.go
@@ -116,9 +116,9 @@ func prepareAKSNode(ctx context.Context, s *Scenario) {
 	}
 
 	nbc := getBaseNBC(s.T, s.Runtime.Cluster, s.VHD)
-	if s.VHD.OS == config.OSWindows {
-		nbc.ContainerService.Properties.WindowsProfile.CseScriptsPackageURL = windowsCSE(ctx, s.T)
-	}
+	//if s.VHD.OS == config.OSWindows {
+	//	nbc.ContainerService.Properties.WindowsProfile.CseScriptsPackageURL = windowsCSE(ctx, s.T)
+	//}
 
 	if s.BootstrapConfigMutator != nil {
 		// deep copy the nbc so that we can mutate it without affecting the original
