079979810ad88b08b593c4800ef220358a49ab05
Tim Wright <wrightt@microsoft.com>
2025-05-30 03:10:49 +0000

    set cse scripts package url

--- e2e/scenario_helpers_test.go
+++ e2e/scenario_helpers_test.go
@@ -117,6 +117,10 @@ func prepareAKSNode(ctx context.Context, s *Scenario) {
 
 	nbc := getBaseNBC(s.T, s.Runtime.Cluster, s.VHD)
 
+	if s.VHD.OS == config.OSWindows {
+		nbc.ContainerService.Properties.WindowsProfile.CseScriptsPackageURL = "https://packages.aks.azure.com/aks/windows/cse"
+	}
+
 	if s.BootstrapConfigMutator != nil {
 		// deep copy the nbc so that we can mutate it without affecting the original
 		clonedNbc, err := deepcopy.Anything(nbc)
