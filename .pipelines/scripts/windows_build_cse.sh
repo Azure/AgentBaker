57677b450e967844306f67b8e07e1cc14e586cb6
Beilei Huang <beileihuang@microsoft.com>
2025-06-02 18:21:03 +1000

    fix: correct minor issues in windows_build_cse.sh script

--- .pipelines/scripts/windows_build_cse.sh
+++ .pipelines/scripts/windows_build_cse.sh
@@ -4,39 +4,31 @@ set -uo pipefail
 # This script builds CSE package in zip format. It has the following steps:
 # 1. copy the cse pacakges to a temp directory
 # 2. remove unnecessary files from the cse packages
-# 3. zip it with name "aks-windows-cse-scripts-current.zip" 
+# 3. zip it with name "aks-windows-cse-scripts-current.zip"
 # 4. publish as an artifact
 
 
 echo "Building CSE package for Windows..."
-# Create a temporary directory for the CSE package
 releaseTempDir=$(mktemp -d)
 stagingDir=$(mkdir "$releaseTempDir/windows")
 
-
-echo "Temporary directory created: $releaseTempDir" 
-# Copy the CSE package files to the temporary directory
 cp -r staging/cse/windows/* "$stagingDir"
-# Remove unnecessary files from the CSE package
-echo "Removing unnecessary files from the CSE package..."
-rm $stagingDir/*.tests.ps1
-rm $stagingDir/*.tests.suites -r
-rm $stagingDir/README
-rm $stagingDir/provisioningscripts/*.md
-rm $stagingDir/debug/update-scripts.ps1
+rm "$stagingDir"/*.tests.ps1
+rm "$stagingDir"/*.tests.suites -r
+rm "$stagingDir"/README
+rm "$stagingDir"/provisioningscripts/*.md
+rm "$stagingDir"/debug/update-scripts.ps1
 
 # Create a zip file of the CSE package
 csePackage="aks-windows-cse-scripts-current.zip"
-echo "Creating zip file: $csePackage"
-echo "Creating release pacakge $releaseTempDir/$csePackage"
-pushd $stagingDir
-  zip -r $releaseTempDir/$csePackage ./*
-popd
+echo "Creating CSE elease pacakge $releaseTempDir/$csePackage"
+pushd "$stagingDir" || exit 1
+  zip -r "$releaseTempDir"/$csePackage ./*
+popd || exit 1
 
 # Publish the zip file as an artifact of the pipeline 
 echo "Publishing CSE package as an artifact..."
 echo "##vso[artifact.upload artifactname=CSEPackage;]$ZIP_FILE"
 echo "CSE package built and published successfully."
-# delete temp directory 
-echo "Cleaning up temporary folder $releaseTempDir."
+
 rm -rf "$releaseTempDir"
