ea95635a37b153f9772e0f33ff054d138c979132
Ganeshkumar Ashokavardhanan <35557827+ganeshkumarashok@users.noreply.github.com>
2025-06-02 14:43:48 -0700

    cherry-pick FF 535

--- parts/linux/cloud-init/artifacts/cse_config.sh
+++ parts/linux/cloud-init/artifacts/cse_config.sh
@@ -768,6 +768,14 @@ configGPUDrivers() {
     if [ "$OS" = "$UBUNTU_OS_NAME" ]; then
         mkdir -p /opt/{actions,gpu}
         if [ "${CONTAINER_RUNTIME}" = "containerd" ]; then
+            # if target cloud is AzureUSGovernmentCloud, and NVIDIA_DRIVER_IMAGE contains GRID then use the specific 535 NVIDIA driver image
+            # this is being added because of the host driver in Fairfax is not compatible with the 550 guest drivers now
+            # It can be removed and synced with 550/other clouds, once it is moved to a compatible host driver (check with the HPC team)
+            if [ "$TARGET_CLOUD" = "AzureUSGovernmentCloud" ] && \
+            [ "${NVIDIA_DRIVER_IMAGE#*GRID}" != "$NVIDIA_DRIVER_IMAGE" ]; then
+                NVIDIA_DRIVER_IMAGE_TAG="535.161.08-20250325114356"
+            fi
+
             ctr -n k8s.io image pull $NVIDIA_DRIVER_IMAGE:$NVIDIA_DRIVER_IMAGE_TAG
             retrycmd_if_failure 5 10 600 bash -c "$CTR_GPU_INSTALL_CMD $NVIDIA_DRIVER_IMAGE:$NVIDIA_DRIVER_IMAGE_TAG gpuinstall /entrypoint.sh install"
             ret=$?
