fa0e59feccf313c4f54d39d04627f37ecdccad68
Beilei Huang <beileihuang@microsoft.com>
2025-06-04 12:38:52 +1000

    extract scripts to a separate file

---
+++ .pipelines/scripts/windows_package_cse.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euox pipefail
+
+# This script packages the CSE for Windows into a zip file.
+
+# check env variables CSE_PUBLISH_DIR, CSE_FILE_NAME are set
+if [ -z "$CSE_RELEASE_DIR" ] || [ -z "$CSE_PUBLISH_DIR" ] || [ -z "$CSE_FILE_NAME" ]; then
+  echo "required environment variables are not set. CSE_RELEASE_DIR=$CSE_RELEASE_DIR, CSE_PUBLISH_DIR=$CSE_PUBLISH_DIR, CSE_FILE_NAME=$CSE_FILE_NAME."
+  exit 1
+fi
+
+mkdir -p "$CSE_RELEASE_DIR"
+cp -r ./staging/cse/windows/* "$CSE_RELEASE_DIR"
+rm "$CSE_RELEASE_DIR"/*.tests.ps1
+rm "$CSE_RELEASE_DIR"/*.tests.suites -r
+rm "$CSE_RELEASE_DIR"/README
+rm "$CSE_RELEASE_DIR"/debug/update-scripts.ps1
+
+mkdir -p "$CSE_PUBLISH_DIR"
+echo "Creating CSE release pacakge $CSE_PUBLISH_DIR/$CSE_FILE_NAME"
+pushd "$CSE_RELEASE_DIR" || exit 1
+  zip -r "$CSE_PUBLISH_DIR/$CSE_FILE_NAME" ./*
+popd || exit 1
