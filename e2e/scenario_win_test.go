861fd231e54435a57ff303142ebb076ed7d8cc4e
Beilei Huang <beileihuang@microsoft.com>
2025-06-09 23:14:22 +1000

    test: add regression test for previous cse packages

--- e2e/scenario_win_test.go
+++ e2e/scenario_win_test.go
@@ -169,21 +169,51 @@ func Test_Windows2019CachingRegression(t *testing.T) {
 	})
 }
 
-func Test_WindowsCachingRegression(t *testing.T) {
+func Test_Windows23H2Gen2CachingRegression(t *testing.T) {
 	RunScenario(t, &Scenario{
-		Description: "Windows VHD built before local cache enabled should still work",
+		Description: "Windows 23H2 VHD built before local cache enabled should still work - overwrite the CSE scripts package URL",
 		Config: Config{
-			Cluster:                ClusterAzureNetwork,
-			VHD:                    config.VHDWindows23H2Gen2,
-			VMConfigMutator:        EmptyVMConfigMutator,
-			BootstrapConfigMutator: EmptyBootstrapConfigMutator,
+			Cluster:         ClusterAzureNetwork,
+			VHD:             config.VHDWindows23H2Gen2,
+			VMConfigMutator: EmptyVMConfigMutator,
+			BootstrapConfigMutator: func(nbc *datamodel.NodeBootstrappingConfiguration) {
+				nbc.CloudSpecConfig.KubernetesSpecConfig.CseScriptsPackageURL = "https://packages.aks.azure.com/aks/windows/cse/aks-windows-cse-scripts-v0.0.52.zip"
+			},
 			Validator: func(ctx context.Context, s *Scenario) {
-				ValidateWindowsVersionFromWindowsSettings(ctx, s, "23H2-gen2")
-				ValidateWindowsProductName(ctx, s, "Windows Server 2022 Datacenter")
-				ValidateWindowsDisplayVersion(ctx, s, "23H2")
-				ValidateFileHasContent(ctx, s, "/k/kubeletstart.ps1", "--container-runtime=remote")
-				ValidateWindowsProcessHasCliArguments(ctx, s, "kubelet.exe", []string{"--rotate-certificates=true", "--client-ca-file=c:\\k\\ca.crt"})
-				ValidateCiliumIsNotRunningWindows(ctx, s)
+				ValidateFileHasContent(ctx, s, "/k/test.txt", "this is a test file")
+			},
+		},
+	})
+}
+
+func Test_Windows2022CachingRegression(t *testing.T) {
+	RunScenario(t, &Scenario{
+		Description: "Windows 2022 VHD built before local cache enabled should still work - overwrite the CSE scripts package URL",
+		Config: Config{
+			Cluster:         ClusterAzureNetwork,
+			VHD:             config.VHDWindows2022ContainerdGen2,
+			VMConfigMutator: EmptyVMConfigMutator,
+			BootstrapConfigMutator: func(nbc *datamodel.NodeBootstrappingConfiguration) {
+				nbc.CloudSpecConfig.KubernetesSpecConfig.CseScriptsPackageURL = "https://packages.aks.azure.com/aks/windows/cse/aks-windows-cse-scripts-v0.0.52.zip"
+			},
+			Validator: func(ctx context.Context, s *Scenario) {
+				ValidateFileHasContent(ctx, s, "/k/test.txt", "this is a test file")
+			},
+		},
+	})
+}
+
+func Test_Windows2019CachingRegression(t *testing.T) {
+	RunScenario(t, &Scenario{
+		Description: "Windows 2019 VHD built before local cache enabled should still work - overwrite the CSE scripts package URL",
+		Config: Config{
+			Cluster:         ClusterAzureNetwork,
+			VHD:             config.VHDWindows2019Containerd,
+			VMConfigMutator: EmptyVMConfigMutator,
+			BootstrapConfigMutator: func(nbc *datamodel.NodeBootstrappingConfiguration) {
+				nbc.CloudSpecConfig.KubernetesSpecConfig.CseScriptsPackageURL = "https://packages.aks.azure.com/aks/windows/cse/aks-windows-cse-scripts-v0.0.52.zip"
+			},
+			Validator: func(ctx context.Context, s *Scenario) {
 				ValidateFileHasContent(ctx, s, "/k/test.txt", "this is a test file")
 			},
 		},
