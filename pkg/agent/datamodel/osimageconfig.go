83b4370b0fa5e131df66373fe46f953dbac352fe
Sylvain Boily <4981802+djsly@users.noreply.github.com>
2025-06-17 18:07:12 -0400

    adding file

---
+++ pkg/agent/datamodel/osimageconfig.go
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+package datamodel
+
+//nolint:gochecknoglobals
+var (
+	AKSUbuntuContainerd2204Gen2OSImageConfig = AzureOSImageConfig{
+		ImageOffer:     "aks",
+		ImageSku:       "aks-ubuntu-containerd-22.04-gen2",
+		ImagePublisher: "microsoft-aks",
+		ImageVersion:   "2025.06.02",
+	}
+
+	AzureCloudToOSImageMap = map[string]map[Distro]AzureOSImageConfig{
+		AzureChinaCloud: {
+			AKSUbuntuContainerd2204Gen2: AKSUbuntuContainerd2204Gen2OSImageConfig,
+		},
+		AzureGermanCloud: {
+			AKSUbuntuContainerd2204Gen2: AKSUbuntuContainerd2204Gen2OSImageConfig,
+		},
+		AzureUSGovernmentCloud: {
+			AKSUbuntuContainerd2204Gen2: AKSUbuntuContainerd2204Gen2OSImageConfig,
+		},
+		AzurePublicCloud: {
+			AKSUbuntuContainerd2204Gen2: AKSUbuntuContainerd2204Gen2OSImageConfig,
+		},
+		USNatCloud: {
+			AKSUbuntuContainerd2204Gen2: AKSUbuntuContainerd2204Gen2OSImageConfig,
+		},
+		USSecCloud: {
+			AKSUbuntuContainerd2204Gen2: AKSUbuntuContainerd2204Gen2OSImageConfig,
+		},
+	}
+)
