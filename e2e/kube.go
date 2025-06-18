e6d084d6b1928d3d56f283f1c172a3d40096c718
Devin Wong <wongsiosun@outlook.com>
2025-06-18 15:26:00 -0700

    remove unused k8s.io/apimachinery/pkg/util/intstr

--- e2e/kube.go
+++ e2e/kube.go
@@ -20,7 +20,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
-	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
