fa13228d13a5aaabaf11364f5e1e8367eac93252
Tim Wright <wrightt@microsoft.com>
2025-06-19 09:28:49 +1200

    flake: startup nanoserver pod not iis for windows node testing (#6495)

--- e2e/kube.go
+++ e2e/kube.go
@@ -529,31 +529,17 @@ func podHTTPServerLinux(s *Scenario) *corev1.Pod {
 	}
 }
 
-func podHTTPServerWindows(s *Scenario) *corev1.Pod {
+func podWindows(s *Scenario, podName string, imageName string) *corev1.Pod {
 	return &corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      fmt.Sprintf("%s-test-pod", s.Runtime.KubeNodeName),
+			Name:      fmt.Sprintf("%s-test-%s-pod", s.Runtime.KubeNodeName, podName),
 			Namespace: "default",
 		},
 		Spec: corev1.PodSpec{
 			Containers: []corev1.Container{
 				{
-					Name:  "iis-container",
-					Image: "mcr.microsoft.com/windows/servercore/iis",
-					Ports: []corev1.ContainerPort{
-						{
-							ContainerPort: 80,
-						},
-					},
-					ReadinessProbe: &corev1.Probe{
-						PeriodSeconds: 1,
-						ProbeHandler: corev1.ProbeHandler{
-							HTTPGet: &corev1.HTTPGetAction{
-								Path: "/",
-								Port: intstr.FromInt32(80),
-							},
-						},
-					},
+					Name:  podName,
+					Image: imageName,
 				},
 			},
 			NodeSelector: map[string]string{
