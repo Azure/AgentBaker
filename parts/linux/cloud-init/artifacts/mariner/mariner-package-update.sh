f773447d7c142de00d7c73a4238c1cff596167f7
YaoC <chengyao09@hotmail.com>
2025-06-19 00:59:38 +0000

    chore: make repo list as a variable

--- parts/linux/cloud-init/artifacts/mariner/mariner-package-update.sh
+++ parts/linux/cloud-init/artifacts/mariner/mariner-package-update.sh
@@ -7,30 +7,19 @@ dnf_update() {
     retries=10
     dnf_update_output=/tmp/dnf-update.out
     versionID=$(grep '^VERSION_ID=' /etc/os-release  | cut -d'=' -f2 | tr -d '"')
+    if [ "${versionID}" = "3.0" ]; then
+        repo_list=(--repo azurelinux-official-base --repo azurelinux-official-ms-non-oss --repo azurelinux-official-ms-oss --repo azurelinux-official-nvidia) 
+    else
+        repo_list=(--repo mariner-official-base --repo mariner-official-microsoft --repo mariner-official-extras --repo mariner-official-nvidia) 
+    fi
     for i in $(seq 1 $retries); do
-        if [ "${versionID}" = "3.0" ]; then
-            ! (dnf update \
-                --exclude mshv-linuxloader \
-                --exclude kernel-mshv \
-                --repo azurelinux-official-base \
-                --repo azurelinux-official-ms-non-oss \
-                --repo azurelinux-official-ms-oss \
-                --repo azurelinux-official-nvidia \
-                -y --refresh 2>&1 | tee $dnf_update_output | grep -E "^([WE]:.*)|([eE]rr.*)$") && \
-            cat $dnf_update_output && break || \
-            cat $dnf_update_output
-        else
-            ! (dnf update \
-                --exclude mshv-linuxloader \
-                --exclude kernel-mshv \
-                --repo mariner-official-base \
-                --repo mariner-official-microsoft \
-                --repo mariner-official-extras \
-                --repo mariner-official-nvidia \
-                -y --refresh 2>&1 | tee $dnf_update_output | grep -E "^([WE]:.*)|([eE]rr.*)$") && \
-            cat $dnf_update_output && break || \
-            cat $dnf_update_output
-        fi
+        ! (dnf update \
+            --exclude mshv-linuxloader \
+            --exclude kernel-mshv \
+            "${repo_list[@]}" \
+            -y --refresh 2>&1 | tee $dnf_update_output | grep -E "^([WE]:.*)|([eE]rr.*)$") && \
+        cat $dnf_update_output && break || \
+        cat $dnf_update_output
 
         if [ $i -eq $retries ]; then
         return 1
