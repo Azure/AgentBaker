ddba7991fb9cfa795ba71a44b9113b4f28a51c4d
sabina <sabinamiezah@microsoft.com>
2025-06-24 13:37:20 -0700

    Check that valid image url value is carrying along to produce packer settings script

--- vhdbuilder/packer/produce-packer-settings.sh
+++ vhdbuilder/packer/produce-packer-settings.sh
@@ -403,6 +403,7 @@ if [ "$OS_TYPE" = "Windows" ]; then
  	export AZCOPY_AUTO_LOGIN_TYPE="MSI" # use Managed Identity for AzCopy authentication
 	export AZCOPY_MSI_RESOURCE_STRING="${AZURE_MSI_RESOURCE_STRING}"
 
+	echo "VALID IMAGE URL: ${WINDOWS_CONTAINERIMAGE_JSON_URL}"
 	if [ -n "${WINDOWS_CONTAINERIMAGE_JSON_URL}" ]; then
 		# Download the json artifact from the url
 		filename=$(basename "$WINDOWS_CONTAINERIMAGE_JSON_URL")
