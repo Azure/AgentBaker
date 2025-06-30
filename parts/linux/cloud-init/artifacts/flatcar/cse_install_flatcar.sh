dbbf543962b926761639e5a37232288d46f2ec08
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2025-06-30 15:26:08 +0000

    build: Add distro helpers for flatcar

---
+++ parts/linux/cloud-init/artifacts/flatcar/cse_install_flatcar.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+stub() {
+    echo "${FUNCNAME[1]} stub"
+}
+
+installDeps() {
+    stub
+}
+
+installCriCtlPackage() {
+    stub
+}
+
+# CSE+VHD can dictate the containerd version, users don't care as long as it works
+installStandaloneContainerd() {
+    stub
+}
+
+ensureRunc() {
+  stub
+}
+
+cleanUpGPUDrivers() {
+    stub
+}
+
+#EOF
