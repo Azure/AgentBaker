00c47126bfd709e208dd0d0f6dc0e5fb94db3235
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2025-06-30 15:26:08 +0000

    build: Add distro helpers for flatcar

---
+++ parts/linux/cloud-init/artifacts/flatcar/cse_helpers_flatcar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Sourcing cse_helpers_distro.sh for Flatcar"
+
+stub() {
+    echo "${FUNCNAME[1]} stub"
+}
+
+dnfversionlockWALinuxAgent() {
+    stub
+}
+
+aptmarkWALinuxAgent() {
+    stub
+}
+
+#EOF
