5c7ccdf2adb4c19a594a853647339f413382639d
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2025-06-30 15:26:08 +0000

    build: Add distro helpers for flatcar

---
+++ vhdbuilder/scripts/linux/flatcar/tool_installs_flatcar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Sourcing tool_installs_flatcar.sh"
+
+stub() {
+    echo "${FUNCNAME[1]} stub"
+}
+installBcc() {
+    stub
+}
+
+installBpftrace() {
+    stub
+}
+
+listInstalledPackages() {
+    stub
+}
