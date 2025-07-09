7420dcbce3045761cba980427516554fc9850c52
Christian Smith <1001494+smith1511@users.noreply.github.com>
2025-07-09 11:36:16 +1000

    Portal link

--- e2e/vmss.go
+++ e2e/vmss.go
@@ -269,10 +269,15 @@ func extractLogsFromVMWindows(ctx context.Context, s *Scenario) {
 	s.T.Logf("run command executed successfully: %v", runCommandResp)
 
 	s.T.Logf("uploaded logs to %s", blobUrl)
-	s.T.Logf("view storage account in Azure portal: https://portal.azure.com/?feature.customportal=false#@microsoft.onmicrosoft.com/resource/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Storage/storageAccounts/%s/overview",
+
+	// Create a reusable URL for the Azure portal link to the storage account
+	azurePortalURL := fmt.Sprintf("https://portal.azure.com/?feature.customportal=false#@microsoft.onmicrosoft.com/resource/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Storage/storageAccounts/%s/overview",
 		config.Config.SubscriptionID,
 		config.ResourceGroupName,
 		config.Config.BlobStorageAccount())
+
+	s.T.Logf("Storage account in Azure portal: %s", azurePortalURL)
+	s.T.Logf("##vso[task.logissue type=warning;]%s", azurePortalURL)
 
 	downloadBlob := func(blobSuffix string) {
 		fileName := filepath.Join(testDir(s.T), blobSuffix)
