b7e894b64d36f63a8cedcf0c4527b88fa73384a2
r2k1 <yokree@gmail.com>
2025-07-23 18:12:33 +1200

    prevent T from extracting from context

--- e2e/toolkit/log.go
+++ e2e/toolkit/log.go
@@ -6,16 +6,19 @@ import (
 	"testing"
 )
 
+// testLoggerKey is a private key to prevent *testing.T from being easily accessed
+type testLoggerKey struct{}
+
 func ContextWithLog(ctx context.Context, t *testing.T) context.Context {
 	if t == nil {
 		log.Println("WARNING: No *testing.T provided, this function should only be called from a test")
 		return ctx
 	}
-	return context.WithValue(ctx, "T", t)
+	return context.WithValue(ctx, testLoggerKey{}, t)
 }
 
 func Logf(ctx context.Context, format string, args ...any) {
-	t, ok := ctx.Value("T").(*testing.T)
+	t, ok := ctx.Value(testLoggerKey{}).(*testing.T)
 	if !ok || t == nil {
 		log.Printf(format+"WARNING: No *testing.T in Context, this function should only be called from ", args...)
 	}
@@ -24,7 +27,7 @@ func Logf(ctx context.Context, format string, args ...any) {
 }
 
 func Log(ctx context.Context, args ...any) {
-	t, ok := ctx.Value("T").(*testing.T)
+	t, ok := ctx.Value(testLoggerKey{}).(*testing.T)
 	if !ok || t == nil {
 		log.Println("WARNING: No *testing.T in Context, this function should only be called from a test")
 		return
