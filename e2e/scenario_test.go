19a62b2bd230057a249dafdc5cc81f2e590749ae
Suraj Deshmukh <suraj.deshmukh@microsoft.com>
2025-07-25 16:12:46 -0700

    e2e: Add GPU test for A100 instance type
    Add test case for Ubuntu 24.04 with A100 GPU using Standard_ND96asr_v4 instance
    type in South Central US location.
    
    Signed-off-by: Suraj Deshmukh <suraj.deshmukh@microsoft.com>

--- e2e/scenario_test.go
+++ e2e/scenario_test.go
@@ -1836,6 +1836,10 @@ func Test_Ubuntu2404_GPU_H100(t *testing.T) {
 	RunScenario(t, runScenarioGPUNPD(t, "Standard_ND96isr_H100_v5", "uaenorth"))
 }
 
+func Test_Ubuntu2404_GPU_A100(t *testing.T) {
+	RunScenario(t, runScenarioGPUNPD(t, "Standard_ND96asr_v4", "southcentralus"))
+}
+
 func Test_AzureLinux3_Kube_Package_Install(t *testing.T) {
 	RunScenario(t, &Scenario{
 		Description: "tests that an AzureLinux node will install kube pkgs from PMC and can be properly bootstrapped",
