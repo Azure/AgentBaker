ffb32b57ce96765b6db3dc745b975519654d47f6
Martin <martin.heberling.dalies@gmail.com>
2025-08-21 18:37:06 -0700

    Fixed go templating conditionals.

--- pkg/agent/baker.go
+++ pkg/agent/baker.go
@@ -1600,6 +1600,7 @@ root = "{{GetDataDir}}"{{- end}}
   privileged_without_host_devices = true
   [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.kata-cc.options]
     ConfigPath = "/opt/confidential-containers/share/defaults/kata-containers/configuration-clh-snp.toml"
+{{- end}}
 `
 	containerdV2NoGPUConfigTemplate ContainerdConfigTemplate = `version = 2
 oom_score = -999{{if HasDataDir }}
@@ -1667,6 +1668,7 @@ root = "{{GetDataDir}}"{{- end}}
   [proxy_plugins.tardev]
     type = "snapshot"
     address = "/run/containerd/tardev-snapshotter.sock"
+{{- end}}
 `
 	containerdV1NoGPUConfigTemplate ContainerdConfigTemplate = `version = 2
 oom_score = -999{{if HasDataDir }}
@@ -1740,6 +1742,7 @@ root = "{{GetDataDir}}"{{- end}}
   privileged_without_host_devices = true
   [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.kata-cc.options]
     ConfigPath = "/opt/confidential-containers/share/defaults/kata-containers/configuration-clh-snp.toml"
+{{- end}}
 `
 )
 
