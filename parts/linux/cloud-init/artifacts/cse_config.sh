5741d22bcb29d1bd7634d20251c9384dd9560213
Alex Benn <alex.benn.is@gmail.com>
2025-08-25 17:13:47 -0400

    bump aks-gpu image version to use. Actually install GPU drivers on ARM64 when on GPU node.

--- parts/linux/cloud-init/artifacts/cse_config.sh
+++ parts/linux/cloud-init/artifacts/cse_config.sh
@@ -899,10 +899,6 @@ configGPUDrivers() {
 }
 
 validateGPUDrivers() {
-    if [ "$(isARM64)" -eq 1 ]; then
-        return
-    fi
-
     retrycmd_if_failure 24 5 25 nvidia-modprobe -u -c0 && echo "gpu driver loaded" || configGPUDrivers || exit $ERR_GPU_DRIVERS_START_FAIL
     
     if which nvidia-smi; then
@@ -924,10 +920,6 @@ validateGPUDrivers() {
 }
 
 ensureGPUDrivers() {
-    if [ "$(isARM64)" -eq 1 ]; then
-        return
-    fi
-
     if [ "${CONFIG_GPU_DRIVER_IF_NEEDED}" = true ]; then
         logs_to_events "AKS.CSE.ensureGPUDrivers.configGPUDrivers" configGPUDrivers
     else
