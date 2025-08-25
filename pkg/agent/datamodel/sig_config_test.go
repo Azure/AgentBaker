fc916ac66d3988740672b70d13996a8a939f3efa
Martin <martin.heberling.dalies@gmail.com>
2025-08-25 13:26:17 -0700

    Freezing kata mariner vhd for kata-cc.

--- pkg/agent/datamodel/sig_config_test.go
+++ pkg/agent/datamodel/sig_config_test.go
@@ -32,8 +32,6 @@ var _ = Describe("GetMaintainedLinuxSIGImageConfigMap", func() {
 			AKSCBLMarinerV2Gen2FIPS:               SIGCBLMarinerV2Gen2FIPSImageConfigTemplate.WithOptions(),
 			AKSAzureLinuxV2Gen2FIPS:               SIGAzureLinuxV2Gen2FIPSImageConfigTemplate.WithOptions(),
 			AKSAzureLinuxV3Gen2FIPS:               SIGAzureLinuxV3Gen2FIPSImageConfigTemplate.WithOptions(),
-			AKSCBLMarinerV2Gen2Kata:               SIGCBLMarinerV2KataImageConfigTemplate.WithOptions(),
-			AKSAzureLinuxV2Gen2Kata:               SIGAzureLinuxV2KataImageConfigTemplate.WithOptions(),
 			AKSAzureLinuxV3Gen2Kata:               SIGAzureLinuxV3KataImageConfigTemplate.WithOptions(),
 			AKSCBLMarinerV2Arm64Gen2:              SIGCBLMarinerV2Arm64ImageConfigTemplate.WithOptions(),
 			AKSAzureLinuxV2Arm64Gen2:              SIGAzureLinuxV2Arm64ImageConfigTemplate.WithOptions(),
@@ -338,7 +336,7 @@ var _ = Describe("GetSIGAzureCloudSpecConfig", func() {
 		Expect(azurelinuxV2Gen2Kata.ResourceGroup).To(Equal("resourcegroup"))
 		Expect(azurelinuxV2Gen2Kata.Gallery).To(Equal("aksazurelinux"))
 		Expect(azurelinuxV2Gen2Kata.Definition).To(Equal("V2katagen2"))
-		Expect(azurelinuxV2Gen2Kata.Version).To(Equal(LinuxSIGImageVersion))
+		Expect(azurelinuxV2Gen2Kata.Version).To(Equal(FrozenAzureLinuxV2KataGen2SIGImageVersion))
 
 		azurelinuxV3Gen2Kata := sigConfig.SigAzureLinuxImageConfig[AKSAzureLinuxV3Gen2Kata]
 		Expect(azurelinuxV3Gen2Kata.ResourceGroup).To(Equal("resourcegroup"))
