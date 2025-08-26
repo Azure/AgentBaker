6d242b9be8906e4c26b25b5073459e2a2d26fec0
Anson Qian <anson627@gmail.com>
2025-08-26 11:20:15 -0400

    update e2e test

--- e2e/vmss.go
+++ e2e/vmss.go
@@ -558,7 +558,7 @@ func getBaseVMSSModel(s *Scenario, customData, cseCmd, location string) armcompu
 				StorageProfile: &armcompute.VirtualMachineScaleSetStorageProfile{
 					OSDisk: &armcompute.VirtualMachineScaleSetOSDisk{
 						CreateOption: to.Ptr(armcompute.DiskCreateOptionTypesFromImage),
-						DiskSizeGB:   to.Ptr(int32(50)),
+						DiskSizeGB:   to.Ptr(int32(60)),
 						OSType:       to.Ptr(armcompute.OperatingSystemTypesLinux),
 						Caching:      to.Ptr(armcompute.CachingTypesReadOnly),
 						DiffDiskSettings: &armcompute.DiffDiskSettings{
