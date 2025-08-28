3f5922f9f64bdd696fcbcee2e0bd16a33b12ef54
Kanan Mehta <kananmehta@microsoft.com>
2025-08-28 12:08:21 -0700

    feat: update getmaintainedlinuximageconfigs

--- pkg/agent/datamodel/sig_config.go
+++ pkg/agent/datamodel/sig_config.go
@@ -949,6 +949,7 @@ func GetMaintainedLinuxSIGImageConfigMap() map[Distro]SigImageConfig {
 		getSigUbuntuImageConfigMapWithOpts(),
 		getSigCBLMarinerImageConfigMapWithOpts(),
 		getSigAzureLinuxImageConfigMapWithOpts(),
+		getSigFlatcarImageConfigMapWithOpts(),
 	}
 
 	maintained := map[Distro]SigImageConfig{}
