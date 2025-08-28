90fd75a766ec8df26079d631e78de3703c58eb1d
James Le Cuirot <jlecuirot@microsoft.com>
2025-08-28 17:29:06 +0100

    Install kubelet and kubectl to Flatcar from sysexts published on MAR
    Flatcar is immutable so cannot use traditional packages. The sysexts are
    downloaded using ORAS and the desired version is later "merged" by
    simply creating symlinks and calling systemd-sysext refresh.
    
    Sysexts are a systemd concept, so they have been published using
    systemd's architecture names rather than Docker's even though they are
    hosted in an OCI registry.
    
    The sysexts are built using Dalec from a distribution other than
    Flatcar, which is currently Azure Linux 3.
    
    Renovate has been configured to consider `azlinux3` part of the version
    so that different Kubernetes releases could potentially be built from
    different distributions. The use of `matchCurrentVersion` ensures that
    tags containing `azlinux3` (e.g. not `noble`) will only be applied
    against artifacts already using `azlinux3`. Switching distribution
    should be a manual change.

--- parts/linux/cloud-init/artifacts/flatcar/cse_install_flatcar.sh
+++ parts/linux/cloud-init/artifacts/flatcar/cse_install_flatcar.sh
@@ -4,12 +4,43 @@ stub() {
     echo "${FUNCNAME[1]} stub"
 }
 
+downloadSysextFromVersion() {
+    local sysextName=$1
+    local sysextURL=$2
+    local downloadDir=${3:-"/opt/${sysextName}/downloads"}
+
+    local tries
+    while ! timeout 60 oras pull --output "${downloadDir}" "${sysextURL}"; do
+        [[ $(( ++tries )) -eq 120 ]] && exit "${ERR_ORAS_PULL_SYSEXT_FAIL}"
+        sleep 5
+    done
+
+    echo "Succeeded to download ${sysextName} from ${sysextURL}"
+}
+
+mergeSysexts() {
+    local sysextArch
+    sysextArch=$(getSystemdArch)
+
+    while [[ ${1-} ]]; do
+        local sysextName=$1 sysextVersion=$2
+        ln -snf "/opt/${sysextName}/downloads/${sysextName}-${sysextVersion}-${sysextArch}.raw" "/etc/extensions/${sysextName}.raw"
+        shift 2
+    done
+
+    systemd-sysext refresh
+}
+
 installDeps() {
     stub
 }
 
 installCriCtlPackage() {
     stub
+}
+
+installKubeletKubectlFromPkg() {
+    mergeSysexts kubelet "$1" kubectl "$1"
 }
 
 # CSE+VHD can dictate the containerd version, users don't care as long as it works
