014f31e5feac12888b3fbac939c750af88c3bbcd
Keith Pimm <keithpimm@microsoft.com>
2025-09-03 15:41:54 -0500

    Add check for existing containerd configuration file. Skip generation if it already exists

--- parts/linux/cloud-init/artifacts/cse_config.sh
+++ parts/linux/cloud-init/artifacts/cse_config.sh
@@ -339,8 +339,11 @@ ExecStartPost=/sbin/iptables -P FORWARD ACCEPT
 EOF
 
   mkdir -p /etc/containerd
-  # RP doesn't currently recognize the GB200 as a GPU node. Check manually here.
-  if [ "${GPU_NODE}" = "true" ] || [ "${VM_SIZE}" = "Standard_ND128isr_NDR_GB200_v6" ]; then
+
+  # Skip containerd config generation if config already exists
+  if [ -f "/etc/containerd/config.toml" ]; then
+    echo "containerd config already exists at /etc/containerd/config.toml, skipping generation"
+  elif [ "${GPU_NODE}" = "true"  ]; then
     # Check VM tag directly to determine if GPU drivers should be skipped
     export -f should_skip_nvidia_drivers
     should_skip=$(retrycmd_silent 10 1 10 bash -cx should_skip_nvidia_drivers)
