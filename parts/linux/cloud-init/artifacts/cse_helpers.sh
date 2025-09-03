6037b2cceef32d61b24a5b9b283104311bf92a87
James Le Cuirot <jlecuirot@microsoft.com>
2025-09-03 12:39:37 +0100

    feat: install Kubernetes binaries and symlinks to /opt/bin
    /usr/local/bin is generally read-only on immutable distributions, so
    these binaries cannot be installed there.
    
    The packages published to PMC and the system extensions built from those
    install their binaries to /usr/bin. These were previously moved to
    /usr/local/bin, but since this is no longer possible, symlinks are now
    created in /opt/bin instead.
    
    Symlinks are not used when installing from a tarball because the
    versioned binaries (e.g. kubelet-*) are removed, leading to dangling
    links.
    
    The `install` command has been used to write these binaries because it
    removes the existing file first (rather than following a symlink) and
    takes care of making the new file executable.

--- parts/linux/cloud-init/artifacts/cse_helpers.sh
+++ parts/linux/cloud-init/artifacts/cse_helpers.sh
@@ -175,7 +175,7 @@ AZURELINUX_KATA_OS_NAME="AZURELINUXKATA"
 AZURELINUX_OS_NAME="AZURELINUX"
 FLATCAR_OS_NAME="FLATCAR"
 AZURELINUX_OSGUARD_OS_VARIANT="OSGUARD"
-KUBECTL=/usr/local/bin/kubectl
+KUBECTL=/opt/bin/kubectl
 DOCKER=/usr/bin/docker
 # this will be empty during VHD build
 # but vhd build runs with `set -o nounset`
@@ -1238,6 +1238,7 @@ extract_tarball() {
     local tarball="$1"
     local dest="$2"
     shift 2
+    mkdir -p "$dest"
     # Use tar options if provided, otherwise default to -xzf
     case "$tarball" in
         *.tar.gz|*.tgz)
