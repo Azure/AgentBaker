444ece4bfac90997701b95d2677ab158155211b2
Keith Pimm <keithpimm@microsoft.com>
2025-09-03 14:05:35 -0500

    Attempt to write containerd configuration directly after making modifications to RP

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -622,7 +622,7 @@ EOF
     systemctl enable nvidia-dcgm
     systemctl enable nvidia-dcgm-exporter
     systemctl enable nvidia-device-plugin
-    systemctl enable containerd-nvidia-config
+    #systemctl enable containerd-nvidia-config
   fi
 fi
 
