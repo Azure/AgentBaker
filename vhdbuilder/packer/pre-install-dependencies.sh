c3cbfcc146043d9dd7aaca4d08edcc7cdd4843b6
Zachary Bailey <zachary.r.bailey@outlook.com>
2025-09-04 15:00:39 -0700

    fix: fi

--- vhdbuilder/packer/pre-install-dependencies.sh
+++ vhdbuilder/packer/pre-install-dependencies.sh
@@ -107,10 +107,7 @@ else
   fi
 
   if [ -n "${VHD_BUILD_TIMESTAMP}" ] && [ ${UBUNTU_RELEASE//./} -eq 2204 ]; then
-    echo "Using packages from desired snapshot..."
-    if [ "${CPU_ARCH}" = "arm64" ]; then
-      sed -i "s#http://ports.ubuntu.com/ubuntu-ports/#https://snapshot.ubuntu.com/ubuntu/${VHD_BUILD_TIMESTAMP}#g" /etc/apt/sources.list
-    else
+    if [ "${CPU_ARCH}" = "amd64" ]; then
       sed -i "s#http://azure.archive.ubuntu.com/ubuntu/#https://snapshot.ubuntu.com/ubuntu/${VHD_BUILD_TIMESTAMP}#g" /etc/apt/sources.list
     fi
   fi
