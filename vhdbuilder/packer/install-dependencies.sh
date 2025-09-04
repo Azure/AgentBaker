9288e5cda39642970f2276d4d4fa96d924302a9a
Keith Pimm <keithpimm@microsoft.com>
2025-09-04 11:17:52 -0500

    Remove OFED driver installation to troubleshoot VHD build failure around the NVIDIA driver

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -582,16 +582,12 @@ EOF
     # will appear directing the reader away from the proprietary driver. The GPUs
     # are also not visible in nvidia-smi output with the proprietary drivers
     apt install -y \
-      mlnx-ofed-kernel-dkms \
-      mlnx-ofed-kernel-utils \
-      mlnx-ofed-basic \
-      rdma-core \
-      ibverbs-utils \
-      ibverbs-providers
-
-    systemctl restart openibd
-    ofed_info -s
-    apt install -y \
+      #mlnx-ofed-kernel-dkms \
+      #mlnx-ofed-kernel-utils \
+      #mlnx-ofed-basic \
+      #rdma-core \
+      #ibverbs-utils \
+      #ibverbs-providers \
       nvidia-driver-580-open \
       cuda-toolkit-12 \
       nvidia-container-toolkit \
@@ -621,6 +617,8 @@ EOF
     systemctl enable nvidia-dcgm
     systemctl enable nvidia-dcgm-exporter
     systemctl enable nvidia-device-plugin
+    #systemctl enable openibd
+    #ofed_info -s
   fi
 fi
 
