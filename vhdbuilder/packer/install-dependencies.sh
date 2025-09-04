b7613e20589facc07d6bb3b95224ac0872ad7951
Keith Pimm <keithpimm@microsoft.com>
2025-09-04 12:05:47 -0500

    Fix formatting for package installation

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -598,12 +598,6 @@ EOF
     # will appear directing the reader away from the proprietary driver. The GPUs
     # are also not visible in nvidia-smi output with the proprietary drivers
     apt install -y \
-      #mlnx-ofed-kernel-dkms \
-      #mlnx-ofed-kernel-utils \
-      #mlnx-ofed-basic \
-      #rdma-core \
-      #ibverbs-utils \
-      #ibverbs-providers \
       nvidia-driver-580-open \
       cuda-toolkit-12 \
       nvidia-container-toolkit \
@@ -613,6 +607,12 @@ EOF
       libcap2-bin \
       k8s-device-plugin
 
+    #mlnx-ofed-kernel-dkms \
+    #mlnx-ofed-kernel-utils \
+    #mlnx-ofed-basic \
+    #rdma-core \
+    #ibverbs-utils \
+    #ibverbs-providers \
     # 3. Add char device symlinks for NVIDIA devices
     mkdir -p "$(dirname /lib/udev/rules.d/71-nvidia-dev-char.rules)"
     cat << EOF >> /lib/udev/rules.d/71-nvidia-dev-char.rules
