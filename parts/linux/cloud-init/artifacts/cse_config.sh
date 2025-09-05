e156ba801695a322db0bd77b9194d4d92b803615
Sri Harsha <SriHarshaBS009@gmail.com>
2025-09-05 18:01:02 +0000

    Add commented lines

--- parts/linux/cloud-init/artifacts/cse_config.sh
+++ parts/linux/cloud-init/artifacts/cse_config.sh
@@ -1053,7 +1053,8 @@ LOCALDNS_COREFILE="/opt/azure/containers/localdns/localdns.corefile"
 LOCALDNS_SLICEFILE="/etc/systemd/system/localdns.slice"
 # This function is called from cse_main.sh.
 # It creates the localdns corefile and slicefile, then enables and starts localdns.
-# In this function, generated base64 encoded localdns corefile is decoded and written to the corefile path. 
+# In this function, generated base64 encoded localdns corefile is decoded and written to the corefile path.
+# This function also creates the localdns slice file with memory and cpu limits, that will be used by localdns systemd unit. 
 shouldEnableLocalDNS() {
     mkdir -p "$(dirname "${LOCALDNS_COREFILE}")"
     touch "${LOCALDNS_COREFILE}"
