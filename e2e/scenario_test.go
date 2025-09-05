0bf7806805ae014510ba1a2e54f36ec26b1bb97e
Johanan Liebermann <jliebermann@microsoft.com>
2025-09-05 18:11:26 +0300

    Fix airgap test description
    Change "is airgap" to "is airgapped" (the former is grammatically
    inaccurate).
    
    Signed-off-by: Johanan Liebermann <jliebermann@microsoft.com>

--- e2e/scenario_test.go
+++ e2e/scenario_test.go
@@ -1397,7 +1397,7 @@ func Test_Flatcar_DisableSSH(t *testing.T) {
 
 func Test_Ubuntu2204_AirGap(t *testing.T) {
 	RunScenario(t, &Scenario{
-		Description: "Tests that a node using the Ubuntu 2204 VHD and is airgap can be properly bootstrapped",
+		Description: "Tests that a node using the Ubuntu 2204 VHD and is airgapped can be properly bootstrapped",
 		Tags: Tags{
 			Airgap: true,
 		},
@@ -1430,7 +1430,7 @@ func Test_Ubuntu2204_AirGap_NonAnonymousACR(t *testing.T) {
 	require.NoError(t, err)
 
 	RunScenario(t, &Scenario{
-		Description: "Tests that a node using the Ubuntu 2204 VHD and is airgap can be properly bootstrapped",
+		Description: "Tests that a node using the Ubuntu 2204 VHD and is airgapped can be properly bootstrapped",
 		Tags: Tags{
 			Airgap:          true,
 			NonAnonymousACR: true,
@@ -1468,7 +1468,7 @@ func Test_Ubuntu2204_AirGap_NonAnonymousACR(t *testing.T) {
 
 func Test_Ubuntu2204Gen2_ContainerdAirgappedK8sNotCached(t *testing.T) {
 	RunScenario(t, &Scenario{
-		Description: "Tests that a node using the Ubuntu 2204 VHD without k8s binary and is airgap can be properly bootstrapped",
+		Description: "Tests that a node using the Ubuntu 2204 VHD without k8s binary and is airgapped can be properly bootstrapped",
 		Tags: Tags{
 			Airgap: true,
 		},
@@ -1500,7 +1500,7 @@ func Test_Ubuntu2204Gen2_ContainerdAirgappedNonAnonymousK8sNotCached(t *testing.
 	identity, err := config.Azure.UserAssignedIdentities.Get(ctx, config.ResourceGroupName(location), config.VMIdentityName, nil)
 	require.NoError(t, err)
 	RunScenario(t, &Scenario{
-		Description: "Tests that a node using the Ubuntu 2204 VHD without k8s binary and is airgap can be properly bootstrapped",
+		Description: "Tests that a node using the Ubuntu 2204 VHD without k8s binary and is airgapped can be properly bootstrapped",
 		Tags: Tags{
 			Airgap:          true,
 			NonAnonymousACR: true,
