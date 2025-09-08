a9b1e6eb060e7f66e89af4cd28fb9ebcc2da51d1
Lily Pan <lilylpan0426@gmail.com>
2025-09-08 13:45:17 -0700

    fix: fix osguard cache update

--- parts/linux/cloud-init/artifacts/azlosguard/cse_helpers_osguard.sh
+++ parts/linux/cloud-init/artifacts/azlosguard/cse_helpers_osguard.sh
@@ -27,6 +27,21 @@ dnf_update() {
     stub
 }
 
+tdnf_makecache() {
+    retries=10
+    tdnf_makecache_output=/tmp/tdnf-makecache.out
+    for i in $(seq 1 $retries); do
+        ! (tdnf makecache -y 2>&1 | tee $tdnf_makecache_output | grep -E "^([WE]:.*)|([eE]rr.*)$") && \
+        cat $tdnf_makecache_output && break || \
+        cat $tdnf_makecache_output
+        if [ $i -eq $retries ]; then
+            return 1
+        else sleep 5
+        fi
+    done
+    echo Executed tdnf makecache -y $i times
+}
+
 tdnf_download() {
     retries=$1; wait_sleep=$2; timeout=$3; downloadDir=$4; shift && shift && shift && shift
     mkdir -p ${downloadDir}
@@ -36,6 +51,7 @@ tdnf_download() {
             return 1
         else
             sleep $wait_sleep
+            tdnf_makecache
         fi
     done
     echo Executed tdnf install -y \"$@\" $i times;
