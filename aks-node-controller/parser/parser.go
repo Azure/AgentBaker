a7687ff95453f4769ff8e9abf1c8c7a2fcdc4de9
Sri Harsha <SriHarshaBS009@gmail.com>
2025-09-08 14:39:09 -0700

    parser, helper and template changes for localdns scriptless (#6967)

--- aks-node-controller/parser/parser.go
+++ aks-node-controller/parser/parser.go
@@ -167,6 +167,10 @@ func getCSEEnv(config *aksnodeconfigv1.Configuration) map[string]string {
 		"ENABLE_IMDS_RESTRICTION":                        fmt.Sprintf("%v", config.GetImdsRestrictionConfig().GetEnableImdsRestriction()),
 		"INSERT_IMDS_RESTRICTION_RULE_TO_MANGLE_TABLE":   fmt.Sprintf("%v", config.GetImdsRestrictionConfig().GetInsertImdsRestrictionRuleToMangleTable()),
 		"PRE_PROVISION_ONLY":                             fmt.Sprintf("%v", config.GetPreProvisionOnly()),
+		"SHOULD_ENABLE_LOCALDNS":                         shouldEnableLocalDns(config),
+		"LOCALDNS_CPU_LIMIT":                             getLocalDnsCpuLimitInPercentage(config),
+		"LOCALDNS_MEMORY_LIMIT":                          getLocalDnsMemoryLimitInMb(config),
+		"LOCALDNS_GENERATED_COREFILE":                    getLocalDnsCorefileBase64(config),
 	}
 
 	for i, cert := range config.CustomCaCerts {
