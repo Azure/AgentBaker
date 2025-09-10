2cb38451f0c9f958c8bb59a83c3b12aadf561f09
Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>
2025-09-10 13:36:53 -0700

    Fix Azure Linux 3.0 artifact streaming package name
    Use acr-mirror-azurelinux3.rpm for Azure Linux 3.0 instead of
    acr-mirror-mariner.rpm to match the correct package naming.

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -453,8 +453,10 @@ if [ "$OS" = "$UBUNTU_OS_NAME" ] && [ "$(isARM64)" -ne 1 ] && [ "$UBUNTU_MAJOR_V
 fi
 
 # Artifact Streaming enabled for Azure Linux 2.0 and 3.0
-if [ "$OS" = "$MARINER_OS_NAME" ] && { [ "$OS_VERSION" = "2.0" ] || [ "$OS_VERSION" = "3.0" ]; } && [ "$(isARM64)" -ne 1 ]; then
+if [ "$OS" = "$MARINER_OS_NAME" ] && [ "$OS_VERSION" = "2.0" ] && [ "$(isARM64)" -ne 1 ]; then
   installAndConfigureArtifactStreaming acr-mirror-mariner rpm
+elif [ "$OS" = "$MARINER_OS_NAME" ] && [ "$OS_VERSION" = "3.0" ] && [ "$(isARM64)" -ne 1 ]; then
+  installAndConfigureArtifactStreaming acr-mirror-azurelinux3 rpm
 fi
 
 # k8s will use images in the k8s.io namespaces - create it
