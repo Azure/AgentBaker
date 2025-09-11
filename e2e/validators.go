d25b7f7f129b7b17699cefc79ba44de83828b49d
r2k1 <yokree@gmail.com>
2025-09-12 07:21:07 +1200

    fix log

--- e2e/validators.go
+++ e2e/validators.go
@@ -367,18 +367,13 @@ func ValidateInstalledPackageVersion(ctx context.Context, s *Scenario, component
 		}
 	}()
 	execResult := execScriptOnVMForScenarioValidateExitCode(ctx, s, installedCommand, 0, "could not get package list")
-	containsComponent := func() bool {
-		for _, line := range strings.Split(execResult.stdout.String(), "\n") {
-			if strings.Contains(line, component) && strings.Contains(line, version) {
-				return true
-			}
+	for _, line := range strings.Split(execResult.stdout.String(), "\n") {
+		if strings.Contains(line, component) && strings.Contains(line, version) {
+			s.T.Logf("found %s %s in the installed packages", component, version)
+			return
 		}
-		return false
-	}()
-	if !containsComponent {
-		s.T.Errorf("expected to find %s %s in the installed packages, but did not", component, version)
 	}
-	s.T.Logf("found %s %s in the installed packages", component, version)
+	s.T.Errorf("expected to find %s %s in the installed packages, but did not", component, version)
 }
 
 func ValidateKubeletNodeIP(ctx context.Context, s *Scenario) {
