ded5bf5a5313a39f4270a8980e36dc826a53825e
r2k1 <yokree@gmail.com>
2025-09-11 21:20:51 +1200

    improve logging

--- e2e/validators.go
+++ e2e/validators.go
@@ -355,7 +355,6 @@ func execScriptOnVMForScenarioValidateExitCode(ctx context.Context, s *Scenario,
 
 func ValidateInstalledPackageVersion(ctx context.Context, s *Scenario, component, version string) {
 	s.T.Helper()
-	s.T.Logf("assert %s %s is installed on the VM", component, version)
 	installedCommand := func() string {
 		switch s.VHD.OS {
 		case config.OSUbuntu:
@@ -377,9 +376,9 @@ func ValidateInstalledPackageVersion(ctx context.Context, s *Scenario, component
 		return false
 	}()
 	if !containsComponent {
-		s.T.Logf("expected to find %s %s in the installed packages, but did not", component, version)
-		s.T.Fail()
+		s.T.Errorf("expected to find %s %s in the installed packages, but did not", component, version)
 	}
+	s.T.Logf("found %s %s in the installed packages", component, version)
 }
 
 func ValidateKubeletNodeIP(ctx context.Context, s *Scenario) {
