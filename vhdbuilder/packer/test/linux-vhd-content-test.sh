afe5c4361e8dd9b952684665ce08af177ed295d8
James Le Cuirot <jlecuirot@microsoft.com>
2025-09-23 22:49:40 +0100

    Install credential provider to Flatcar from sysext published on MAR
    See previous commit for details.

--- vhdbuilder/packer/test/linux-vhd-content-test.sh
+++ vhdbuilder/packer/test/linux-vhd-content-test.sh
@@ -227,26 +227,27 @@ testPackagesInstalled() {
         testAcrCredentialProviderInstalled "$PACKAGE_DOWNLOAD_URL" "${PACKAGE_VERSIONS[@]}"
         continue
         ;;
+      "azure-acr-credential-provider-pmc"|\
       "kubelet"|\
       "kubectl"|\
       "nvidia-device-plugin"|\
       "datacenter-gpu-manager-4-core"|\
       "datacenter-gpu-manager-4-proprietary")
-        testPkgDownloaded "${name}" "${PACKAGE_VERSIONS[@]}"
+        testPkgDownloaded "${name%-pmc}" "${downloadLocation}" "${PACKAGE_VERSIONS[@]}"
         continue
         ;;
       "datacenter-gpu-manager-exporter")
         # On Ubuntu 22.04 and 24.04, the package is called datacenter-gpu-manager-exporter
         [ "$OS_SKU" = "Ubuntu" ] && \
           { [ "$OS_VERSION" = "22.04" ] || [ "$OS_VERSION" = "24.04" ]; } && \
-          testPkgDownloaded "${name}" "${PACKAGE_VERSIONS[@]}"
+          testPkgDownloaded "${name}" "${downloadLocation}" "${PACKAGE_VERSIONS[@]}"
         continue
         ;;
       "dcgm-exporter")
         # The package is called dcgm-exporter in AzureLinux 3.0
         [ "$OS_SKU" = "AzureLinux" ] && \
           [ "$OS_VERSION" = "3.0" ] && \
-          testPkgDownloaded "${name}" "${PACKAGE_VERSIONS[@]}"
+          testPkgDownloaded "${name}" "${downloadLocation}" "${PACKAGE_VERSIONS[@]}"
         continue
         ;;
     esac
@@ -778,11 +779,10 @@ testKubeBinariesPresent() {
 testPkgDownloaded() {
   local test="testPkgDownloaded"
   echo "$test:Start"
-  local packageName=$1; shift
+  local packageName=$1 downloadLocation=$2; shift 2
   local packageVersions=("$@")
   local sysextArch sysextFile
   sysextArch=$(getSystemdArch)
-  downloadLocation="/opt/${packageName}/downloads"
   for packageVersion in "${packageVersions[@]}"; do
     echo "checking package version: $packageVersion ..."
     # Strip epoch (e.g., 1:4.4.1-1 -> 4.4.1-1)
