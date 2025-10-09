d9c2a986144563e34c91ec44023ed4fd3ddcbf39
r2k1 <yokree@gmail.com>
2025-10-10 11:25:59 +1300

    add VHD Caching tag

--- e2e/test_helpers.go
+++ e2e/test_helpers.go
@@ -278,6 +278,7 @@ func maybeSkipScenario(ctx context.Context, t testing.TB, s *Scenario) {
 	s.Tags.OS = string(s.VHD.OS)
 	s.Tags.Arch = s.VHD.Arch
 	s.Tags.ImageName = s.VHD.Name
+	s.Tags.VHDCaching = true
 	if s.AKSNodeConfigMutator != nil {
 		s.Tags.Scriptless = true
 	}
