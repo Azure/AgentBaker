17c8429194cb40fd0402bcf2bc1507ffbb46b493
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2025-10-10 12:34:06 +0200

    fixup! test: Check for CIS regression on Ubuntu

--- vhdbuilder/packer/vhd-scanning.sh
+++ vhdbuilder/packer/vhd-scanning.sh
@@ -268,7 +268,7 @@ fi
 # Compare current cis-report.txt against stored baseline for Ubuntu 22.04 / 24.04.
 # A regression is when a rule that previously "pass" now has any other result.
 compare_cis_with_baseline() {
-    local baseline_file="vhdbuilder/packer/cis-${OS_SKU}-${OS_VERSION}-baseline.txt"
+    local baseline_file="vhdbuilder/packer/cis/baselines/${OS_SKU}/${OS_VERSION}.txt"
     local current_file="cis-report.txt"
     local regressions_file="cis-regressions.txt"
 
