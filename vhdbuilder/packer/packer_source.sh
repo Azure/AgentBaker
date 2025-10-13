a724c27542baa21ddf57192645a0d1f385c7e3c8
Keith Pimm <keithpimm@microsoft.com>
2025-10-13 18:54:21 -0500

    Remove file conflict with local doca repo

--- vhdbuilder/packer/packer_source.sh
+++ vhdbuilder/packer/packer_source.sh
@@ -409,11 +409,12 @@ copyPackerFiles() {
       cpAndMode $CONTAINERD_NVIDIA_TOML_SRC $CONTAINERD_NVIDIA_TOML_DEST 644
 
       DOCA_LIST_SRC=/home/packer/doca.list
-      DOCA_LIST_DEST=/etc/apt/sources.list.d/doca.list
+      # If the local repo is used, these file names will conflict. Postfix with -net.
+      DOCA_LIST_DEST=/etc/apt/sources.list.d/doca-net.list
       cpAndMode $DOCA_LIST_SRC $DOCA_LIST_DEST 644
 
       DOCA_PUB_SRC=/home/packer/doca.pub
-      DOCA_PUB_DEST=/etc/apt/keyrings/doca.pub
+      DOCA_PUB_DEST=/etc/apt/keyrings/doca-net.pub
       cpAndMode $DOCA_PUB_SRC $DOCA_PUB_DEST 644
 
       NVIDIA_MODPROBE_PARAMETERS_SRC=/home/packer/modprobe-nvidia-parameters.conf
