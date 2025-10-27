6019b5612bc4549ec08837bf846adb4c9563fcad
Alex Benn <alex.benn.is@gmail.com>
2025-10-27 14:24:29 -0400

    start fleshing out test. Add exec commands to run stuff on the test vm.

--- e2e/scenario_grace_blackwell_test.go
+++ e2e/scenario_grace_blackwell_test.go
@@ -1,4 +1,17 @@
+package e2e
 
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/Azure/agentbaker/e2e/components"
+	"github.com/Azure/agentbaker/e2e/config"
+	"github.com/Azure/agentbaker/pkg/agent/datamodel"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v6"
+	"github.com/stretchr/testify/require"
+)
 
 func Test_Ubuntu2404_GB200(t *testing.T) {
 	RunScenario(t, &Scenario{
@@ -23,27 +36,22 @@ func Test_Ubuntu2404_GB200(t *testing.T) {
 				os := "ubuntu"
 				osVersion := "r2404"
 
-				// Validate that the NVIDIA device plugin binary was installed correctly
-				versions := components.GetExpectedPackageVersions("nvidia-device-plugin", os, osVersion)
-				require.Lenf(s.T, versions, 1, "Expected exactly one nvidia-device-plugin version for %s %s but got %d", os, osVersion, len(versions))
-				ValidateInstalledPackageVersion(ctx, s, "nvidia-device-plugin", versions[0])
+				// Check that:
+				// 1. All packages installed match or exceed the CRD from HPC team/NVIDIA (either checked-in or provided via environment variable)
+				// 2. azure-nvidia kernel is installed and in use
+				// 3. DCGM and DCGM exporter are running
+				// 4. GPU driver and plugin are running and advertise GPU resources
+				// 5. The IB interface is up
+				// 6. DOCA/OFED drivers are running
+				// 7. NVIDIA IMEX service is running
 
-				// Validate that the NVIDIA device plugin systemd service is running
-				ValidateNvidiaDevicePluginServiceRunning(ctx, s)
+				// First, CRD check.
+				ValidateGB200CRDSatisfied(ctx, s)
 
-				// Validate that MIG mode is enabled via nvidia-smi
-				ValidateMIGModeEnabled(ctx, s)
+				// Next, validate that the azure-nvidia kernel is in use.
+				ValidateAzureNvidiaKernelRunning(ctx, s)
 
-				// Validate that MIG instances are created
-				ValidateMIGInstancesCreated(ctx, s, "MIG 2g.20gb")
-
-				// Validate that GPU resources are advertised by the device plugin
-				ValidateNodeAdvertisesGPUResources(ctx, s, 3)
-
-				// Validate that MIG workloads can be scheduled
-				ValidateGPUWorkloadSchedulable(ctx, s, 3)
-
-				// Validate that the NVIDIA DCGM packages were installed correctly
+				// Validate that the NVIDIA DCGM packages are installed and running.
 				for _, packageName := range getDCGMPackageNames(os) {
 					versions := components.GetExpectedPackageVersions(packageName, os, osVersion)
 					require.Lenf(s.T, versions, 1, "Expected exactly one %s version for %s %s but got %d", packageName, os, osVersion, len(versions))
@@ -53,6 +61,19 @@ func Test_Ubuntu2404_GB200(t *testing.T) {
 				ValidateNvidiaDCGMExporterSystemDServiceRunning(ctx, s)
 				ValidateNvidiaDCGMExporterIsScrapable(ctx, s)
 				ValidateNvidiaDCGMExporterScrapeCommonMetric(ctx, s, "DCGM_FI_DEV_GPU_TEMP")
+
+				// Validate that the NVIDIA device plugin binary was installed correctly
+				versions := components.GetExpectedPackageVersions("nvidia-device-plugin", os, osVersion)
+				require.Lenf(s.T, versions, 1, "Expected exactly one nvidia-device-plugin version for %s %s but got %d", os, osVersion, len(versions))
+				ValidateInstalledPackageVersion(ctx, s, "nvidia-device-plugin", versions[0])
+
+				// Validate that the NVIDIA device plugin systemd service is running
+				ValidateNvidiaDevicePluginServiceRunning(ctx, s)
+
+				// Validate that GPU resources are advertised by the device plugin
+				ValidateNodeAdvertisesGPUResources(ctx, s, 4)
+
+				// TODO: IB interface, DOCA/OFED, IMEX.
 			},
 		},
 	})
