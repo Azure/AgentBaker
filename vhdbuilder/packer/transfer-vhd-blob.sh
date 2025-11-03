efb3a036a25e223edf6c3ac87bc5c75eafb2c56c
Zachary Bailey <zachary.r.bailey@outlook.com>
2025-11-03 10:30:39 -0800

    fix

--- vhdbuilder/packer/transfer-vhd-blob.sh
+++ vhdbuilder/packer/transfer-vhd-blob.sh
@@ -2,3 +2,10 @@
 
 echo "Checking azcopy version..."
 azcopy --version
+
+
+export AZCOPY_AUTO_LOGIN_TYPE="MSI"
+export AZCOPY_MSI_RESOURCE_STRING="$AZURE_MSI_RESOURCE_STRING"
+export AZCOPY_CONCURRENCY_VALUE="AUTO"
+
+azcopy copy "${CLASSIC_BLOB}/${CAPTURED_SIG_VERSION}.vhd" "./${CAPTURED_SIG_VERSION}.vhd"
