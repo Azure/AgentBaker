5d56ba4970032958f1c2f213b98319b7dd59bed2
Zachary Bailey <zachary.r.bailey@outlook.com>
2025-11-03 09:29:08 -0800

    fix

--- vhdbuilder/packer/hash-and-sign-vhd.sh
+++ vhdbuilder/packer/hash-and-sign-vhd.sh
@@ -1,4 +1,25 @@
 #!/bin/bash -e
 
-echo "Checking azcopy version..."
-azcopy --version
+required_env_vars=(
+  "VHD_FILE"
+)
+
+for v in "${required_env_vars[@]}"
+do
+    if [ -z "${!v}" ]; then
+        echo "$v was not set!"
+        exit 1
+    fi
+done
+
+if [ ! -f "$VHD_FILE" ]; then
+  echo "Error: file ${VHD_FILE} not found"
+  exit 1
+fi
+
+HASH_FILE=""
+SIG_FILE=""
+
+
+
+
