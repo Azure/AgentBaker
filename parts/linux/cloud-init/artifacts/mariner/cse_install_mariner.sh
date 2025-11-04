8afa4c04e0975dfc128c319d26d18d0dbd4a47a1
andyzhangx <xiazhang@microsoft.com>
2025-11-04 06:49:40 +0000

    chore: install with rpm -i

--- parts/linux/cloud-init/artifacts/mariner/cse_install_mariner.sh
+++ parts/linux/cloud-init/artifacts/mariner/cse_install_mariner.sh
@@ -187,9 +187,7 @@ installAznfsPkgFromPMC() {
   readonly aznfs_rpm_file="/tmp/packages-microsoft-prod.rpm"
   local aznfs_pkg_url="https://packages.microsoft.com/config/rhel/9/packages-microsoft-prod.rpm"
   retrycmd_curl_file 120 5 25 ${aznfs_rpm_file} ${aznfs_pkg_url} || exit $ERR_MS_PROD_DEB_DOWNLOAD_TIMEOUT
-  if ! dnf_install 30 1 600 ${aznfs_rpm_file}; then
-    exit $ERR_APT_INSTALL_TIMEOUT
-  fi
+  rpm -i ${aznfs_rpm_file}
   dnf check-update --refresh -y
   export AZNFS_NONINTERACTIVE_INSTALL=1
   if ! dnf_install 30 1 600 aznfs; then
