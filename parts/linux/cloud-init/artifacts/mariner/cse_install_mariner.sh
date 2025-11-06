056785b70811a9bcc87ec966bc354db2f8dcc065
lilypan26 <lilylpan0426@gmail.com>
2025-11-05 16:02:46 -0800

    feat: add fallback to kube binary install if cached deb/rpm not found (#7312)

--- parts/linux/cloud-init/artifacts/mariner/cse_install_mariner.sh
+++ parts/linux/cloud-init/artifacts/mariner/cse_install_mariner.sh
@@ -371,11 +371,16 @@ installRPMPackageFromFile() {
 
     rpmFile=$(find "${downloadDir}" -maxdepth 1 -name "${packagePrefix}" -print -quit 2>/dev/null) || rpmFile=""
     if [ -z "${rpmFile}" ]; then
+        if fallbackToKubeBinaryInstall "${packageName}" "${desiredVersion}"; then
+            echo "Successfully installed ${packageName} version ${desiredVersion} from binary fallback"
+            rm -rf ${downloadDir}
+            return 0
+        fi
         # query all package versions and get the latest version for matching k8s version
         fullPackageVersion=$(dnf list ${packageName} --showduplicates | grep ${desiredVersion}- | awk '{print $2}' | sort -V | tail -n 1)
         if [ -z "${fullPackageVersion}" ]; then
             echo "Failed to find valid ${packageName} version for ${desiredVersion}"
-            exit 1
+            return 1
         fi
         echo "Did not find cached rpm file, downloading ${packageName} version ${fullPackageVersion}"
         downloadPkgFromVersion "${packageName}" ${fullPackageVersion} "${downloadDir}"
@@ -383,7 +388,7 @@ installRPMPackageFromFile() {
     fi
 	  if [ -z "${rpmFile}" ]; then
         echo "Failed to locate ${packageName} rpm"
-        exit 1
+        return 1
     fi
 
     if ! dnf_install 30 1 600 ${rpmFile}; then
