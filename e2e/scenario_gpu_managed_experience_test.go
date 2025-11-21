8cca0d7946f1cd71523b22f0332c24bd50160043
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2025-11-21 12:36:33 +0100

    test: switch AL3 GPU tests back to NC6s_v3
    AzureLinux3 does not support the vGPU driver needed for A10.

--- e2e/scenario_gpu_managed_experience_test.go
+++ e2e/scenario_gpu_managed_experience_test.go
@@ -262,14 +262,14 @@ func Test_AzureLinux3_NvidiaDevicePluginRunning(t *testing.T) {
 			Cluster: ClusterKubenet,
 			VHD:     config.VHDAzureLinuxV3Gen2,
 			BootstrapConfigMutator: func(nbc *datamodel.NodeBootstrappingConfiguration) {
-				nbc.AgentPoolProfile.VMSize = "Standard_NV6ads_A10_v5"
+				nbc.AgentPoolProfile.VMSize = "Standard_NC6s_v3"
 				nbc.ConfigGPUDriverIfNeeded = true
 				nbc.EnableGPUDevicePluginIfNeeded = true
 				nbc.EnableNvidia = true
 				nbc.ManagedGPUExperienceAFECEnabled = true
 			},
 			VMConfigMutator: func(vmss *armcompute.VirtualMachineScaleSet) {
-				vmss.SKU.Name = to.Ptr("Standard_NV6ads_A10_v5")
+				vmss.SKU.Name = to.Ptr("Standard_NC6s_v3")
 				if vmss.Tags == nil {
 					vmss.Tags = map[string]*string{}
 				}
