cc111d3733d442daa12cfe75f6f363e4d576b976
Alex Benn <alex.benn.is@gmail.com>
2025-12-03 16:21:10 -0500

    feat: add GB300 VHD SKU to AgentBaker. Disable building it by default until we need it.

--- vhdbuilder/packer/convert-sig-to-classic-storage-account-blob.sh
+++ vhdbuilder/packer/convert-sig-to-classic-storage-account-blob.sh
@@ -84,9 +84,9 @@ elif [ "${OS_TYPE}" = "Linux" ] && grep -q "cvm" <<< "$FEATURE_FLAGS"; then
       } \
     } \
   }"
-elif [ "${OS_TYPE}" = "Linux" ] && grep -q "GB200" <<< "$FEATURE_FLAGS"; then
-  echo "GB200: Creating standard disk from SIG image"
-  # GB200 uses standard disk creation for now, but can be customized in the future if needed
+elif [ "${OS_TYPE}" = "Linux" ] && grep -q "GB[23]00" <<< "$FEATURE_FLAGS"; then
+  echo "GB200/GB300: Creating standard disk from SIG image"
+  # GB200/GB300 uses standard disk creation for now, but can be customized in the future if needed
   az resource create --id $disk_resource_id  --api-version 2024-03-02 --is-full-object --location $LOCATION --properties "{\"location\": \"$LOCATION\", \
     \"properties\": { \
       \"osType\": \"$OS_TYPE\", \
