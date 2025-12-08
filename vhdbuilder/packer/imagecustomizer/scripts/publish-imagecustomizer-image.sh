97ae84f05fc7874077ae1c6c2459946224a07152
Zachary Bailey <zachary.r.bailey@outlook.com>
2025-12-08 07:35:11 -0800

    fix

--- vhdbuilder/packer/imagecustomizer/scripts/publish-imagecustomizer-image.sh
+++ vhdbuilder/packer/imagecustomizer/scripts/publish-imagecustomizer-image.sh
@@ -37,7 +37,7 @@ fi
 capture_benchmark "${SCRIPT_NAME}_prepare_upload_vhd_to_blob"
 
 echo "Testing az storage blob copy start...."
-az storage blob copy start --account-name "$STORAGE_ACCOUNT_NAME" --destination-blob "immutabletestblob.vhd" --destination-container "vhd" --source-uri  --auth-mode login
+az storage blob copy start --account-name "$STORAGE_ACCOUNT_NAME" --destination-blob "immutabletestblob.vhd" --destination-container "$VHD_CONTAINER_NAME" --source-uri  --auth-mode login
 echo "az storage blob copy start test finished."
 
 echo "Testing az storage blob copy start....2"
