c05915d956a407f231c5b95bd2bb5e75ae211051
Zachary Bailey <zachary.r.bailey@outlook.com>
2025-12-10 12:58:00 -0800

    fix

--- vhdbuilder/packer/imagecustomizer/scripts/publish-imagecustomizer-image.sh
+++ vhdbuilder/packer/imagecustomizer/scripts/publish-imagecustomizer-image.sh
@@ -145,7 +145,12 @@ az sig image-version create \
     --target-regions ${TARGET_REGIONS}
 capture_benchmark "${SCRIPT_NAME}_create_sig_image_version"
 
-azcopy remove "${CLASSIC_BLOB_STAGING}/${CAPTURED_SIG_VERSION}.vhd" --recursive=true
-
+if [ "${GENERATE_PUBLISHING_INFO,,}" != "true" ]
+    if [ "${ENVIRONMENT,,}" = "test" ]; then
+        azcopy remove "${CLASSIC_BLOB}/${CAPTURED_SIG_VERSION}.vhd" --recursive=true
+    else
+        azcopy remove "${CLASSIC_BLOB_STAGING}/${CAPTURED_SIG_VERSION}.vhd" --recursive=true
+    fi
+fi
 # Set SIG ID in pipeline for use during testing
 echo "##vso[task.setvariable variable=MANAGED_SIG_ID]$SIG_IMAGE_RESOURCE_ID"
