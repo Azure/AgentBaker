04a97d716af5983de16755ffb8503d20e37e9445
Cameron Meissner <cameronmeissner@gmail.com>
2025-12-11 10:21:10 -0800

    fix: dynamically resolve root device within disk_queue.service

---
+++ parts/linux/cloud-init/artifacts/disk_queue.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# note that "readlink -f /dev/disk/azure/os" also works to resolve the root device,
+# though it's unclear whether this link will be present on all VM SKUs
+ROOT_DEV=$(findmnt -n -o SOURCE / | sed 's/[0-9]*$//')
+DEV_NAME=$(basename "$ROOT_DEV")
+
+echo "resolved root device: $DEV_NAME, will apply settings to /sys/block/$DEV_NAME/queue/{nr_requests, depth}"
+
+echo 128 > "/sys/block/$DEV_NAME/queue/nr_requests"
+echo 64 > "/sys/block/$DEV_NAME/queue/depth"
