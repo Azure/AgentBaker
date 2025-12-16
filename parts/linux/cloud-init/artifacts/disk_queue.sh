46f315d2db142ef5f68eadb2426e7994fc67aaff
Cameron Meissner <cameronmeissner@gmail.com>
2025-12-16 09:12:45 -0800

    chore: use /dev/disk/azure/os

--- parts/linux/cloud-init/artifacts/disk_queue.sh
+++ parts/linux/cloud-init/artifacts/disk_queue.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# note that "readlink -f /dev/disk/azure/os" also works to resolve the root device,
-# though it's unclear whether this link will be present on all VM SKUs
-ROOT_DEV=$(findmnt -n -o SOURCE / | sed 's/[0-9]*$//')
+# the "/dev/disk/azure/os" link is added via the azure-vm-utils package for NVMe disks
+# and is backwards-compatible with SCSI disks: https://learn.microsoft.com/en-us/azure/virtual-machines/linux/azure-virtual-machine-utilities
+ROOT_DEV=$(readlink -f /dev/disk/azure/os)
 DEV_NAME=$(basename "$ROOT_DEV")
 
-echo "resolved root device: $DEV_NAME, will apply settings to /sys/block/$DEV_NAME/queue/{nr_requests, depth}"
+echo "resolved root device: $DEV_NAME, will apply settings to /sys/block/$DEV_NAME/queue/nr_requests"
 
 if [ ! -d "/sys/block/$DEV_NAME/queue" ]; then
     echo "queue settings directory for device: $DEV_NAME does not exist, unable to apply desired settings"
