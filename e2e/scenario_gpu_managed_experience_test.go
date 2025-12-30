3596e1516bd18ace1428adba33d2510cd5aab4ca
Mark Ibrahim <markibrahim@microsoft.com>
2025-12-30 22:20:27 +0000

    Update E2E test

--- e2e/scenario_gpu_managed_experience_test.go
+++ e2e/scenario_gpu_managed_experience_test.go
@@ -75,8 +75,8 @@ func Test_Version_Consistency_GPU_Managed_Components(t *testing.T) {
 			{"datacenter-gpu-manager-4-proprietary", "azurelinux", "v3.0"},
 		},
 		{
-			{"datacenter-gpu-manager-exporter", "ubuntu", "r2404"},
-			{"datacenter-gpu-manager-exporter", "ubuntu", "r2204"},
+			{"dcgm-exporter", "ubuntu", "r2404"},
+			{"dcgm-exporter", "ubuntu", "r2204"},
 			{"dcgm-exporter", "azurelinux", "v3.0"},
 		},
 	}
