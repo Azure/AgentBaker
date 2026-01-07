13250102fdd736840a615c2f54d09f9bda6b1cf7
Lily Pan <lilylpan0426@gmail.com>
2026-01-06 16:57:32 -0800

    add ncv6 grid gpus for testing

--- pkg/agent/datamodel/gpu_components.go
+++ pkg/agent/datamodel/gpu_components.go
@@ -82,15 +82,17 @@ see https://github.com/Azure/azhpc-extensions/blob/daaefd78df6f27012caf30f3b54c3
 */
 //nolint:gochecknoglobals
 var ConvergedGPUDriverSizes = map[string]bool{
-	"standard_nv6ads_a10_v5":   true,
-	"standard_nv12ads_a10_v5":  true,
-	"standard_nv18ads_a10_v5":  true,
-	"standard_nv36ads_a10_v5":  true,
-	"standard_nv72ads_a10_v5":  true,
-	"standard_nv36adms_a10_v5": true,
-	"standard_nc8ads_a10_v4":   true,
-	"standard_nc16ads_a10_v4":  true,
-	"standard_nc32ads_a10_v4":  true,
+	"standard_nv6ads_a10_v5":                true,
+	"standard_nv12ads_a10_v5":               true,
+	"standard_nv18ads_a10_v5":               true,
+	"standard_nv36ads_a10_v5":               true,
+	"standard_nv72ads_a10_v5":               true,
+	"standard_nv36adms_a10_v5":              true,
+	"standard_nc8ads_a10_v4":                true,
+	"standard_nc16ads_a10_v4":               true,
+	"standard_nc32ads_a10_v4":               true,
+	"standard_nc128ds_xl_rtxpro6000bse_v6":  true,
+	"standard_nc128lds_xl_rtxpro6000bse_v6": true,
 }
 
 //nolint:gochecknoglobals
