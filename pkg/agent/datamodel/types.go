42452d6bd706a5f361131cd9c78633ae707d317a
Ganeshkumar Ashokavardhanan <aganeshkumar@microsoft.com>
2026-01-16 09:10:22 -0800

    NBC changes for managed GPU

--- pkg/agent/datamodel/types.go
+++ pkg/agent/datamodel/types.go
@@ -1726,6 +1726,7 @@ type NodeBootstrappingConfiguration struct {
 	EnableNvidia                    bool
 	EnableAMDGPU                    bool
 	ManagedGPUExperienceAFECEnabled bool
+	EnableManagedGPU                bool
 	EnableACRTeleportPlugin         bool
 	TeleportdPluginURL              string
 	EnableArtifactStreaming         bool
