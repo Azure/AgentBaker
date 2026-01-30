642cede62e00402a9c2cb84de1f4c91b0aea6be3
Paul Miller <pmiller@microsoft.com>
2026-01-30 23:07:40 +0000

    different package seperator in mariner than ubuntu
    Signed-off-by: Devin Wong <wongsiosun@outlook.com>

--- parts/linux/cloud-init/artifacts/mariner/cse_install_mariner.sh
+++ parts/linux/cloud-init/artifacts/mariner/cse_install_mariner.sh
@@ -100,7 +100,8 @@ installCNI() {
     fi
     packageVersion=${PACKAGE_VERSIONS[0]}
 
-    packageName="containernetworking-plugins=${packageVersion}"
+    # - between name and vesion unlike apt which uses =
+    packageName="containernetworking-plugins-${packageVersion}"
     echo "Installing ${packageName} with dnf"
     dnf_install 30 1 600 ${packageName} || exit $ERR_CNI_VERSION_INVALID
 
