02385cb2062ef0015a4a9526ab86d695bbc49523
Sri Harsha <SriHarshaBS009@gmail.com>
2026-02-04 02:22:22 +0000

    repro the issue

--- parts/linux/cloud-init/artifacts/ubuntu/cse_install_ubuntu.sh
+++ parts/linux/cloud-init/artifacts/ubuntu/cse_install_ubuntu.sh
@@ -282,6 +282,7 @@ installPkgWithAptGet() {
     # if no deb file with desired version found then try fetching from packages.microsoft repo
     debFile=$(find "${downloadDir}" -maxdepth 1 -name "${packagePrefix}" -print -quit 2>/dev/null) || debFile=""
     if [ -z "${debFile}" ]; then
+
         # if fallbackToKubeBinaryInstall "${packageName}" "${packageVersion}"; then
         #     echo "Successfully installed ${packageName} version ${packageVersion} from binary fallback"
         #     rm -rf ${downloadDir}
