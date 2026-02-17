ed458c6a63b72f15dad9d903779bcb051ed6f1d4
Saewon Kwak <23280628+saewoni@users.noreply.github.com>
2026-02-17 05:11:08 +0000

    revert the packer_source.sh file

--- vhdbuilder/packer/packer_source.sh
+++ vhdbuilder/packer/packer_source.sh
@@ -321,6 +321,22 @@ copyPackerFiles() {
   cpAndMode $AZURE_NETWORK_UDEV_RULE_SRC $AZURE_NETWORK_UDEV_RULE_DEST 0644
 # ---------------------------------------------------------------------------------------
 
+# ------------------------- Files related to inspektor-gadget ---------------------------
+  IG_IMPORT_SCRIPT_SRC=/home/packer/ig-import-gadgets.sh
+  IG_IMPORT_SCRIPT_DEST=/usr/share/inspektor-gadget/import_gadgets.sh
+  IG_REMOVE_SCRIPT_SRC=/home/packer/ig-remove-gadgets.sh
+  IG_REMOVE_SCRIPT_DEST=/usr/share/inspektor-gadget/remove_gadgets.sh
+  IG_SERVICE_SRC=/home/packer/ig-import-gadgets.service
+  IG_SERVICE_DEST=/usr/lib/systemd/system/ig-import-gadgets.service
+
+  # Skip for Mariner, OSGuard, Flatcar, and Kata
+  if ! { isMariner "$OS" || isAzureLinuxOSGuard "$OS" "$OS_VARIANT" || isFlatcar "$OS" || grep -q "kata" <<< "$FEATURE_FLAGS"; }; then
+    cpAndMode $IG_IMPORT_SCRIPT_SRC $IG_IMPORT_SCRIPT_DEST 755
+    cpAndMode $IG_REMOVE_SCRIPT_SRC $IG_REMOVE_SCRIPT_DEST 755
+    cpAndMode $IG_SERVICE_SRC $IG_SERVICE_DEST 644
+  fi
+# ---------------------------------------------------------------------------------------
+
   # Install AKS diagnostic
   cpAndMode $AKS_DIAGNOSTIC_SCRIPT_SRC $AKS_DIAGNOSTIC_SCRIPT_DEST 755
 
