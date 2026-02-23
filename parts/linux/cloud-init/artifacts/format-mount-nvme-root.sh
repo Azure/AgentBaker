b532d278caef0558a6be2e2aec947d9b952d8c92
Alex Benn <alex.benn.is@gmail.com>
2026-02-23 13:02:09 -0500

    Use mdadm to bond the four NVMe drives in software RAID0. Put kubelet root there instead.

--- parts/linux/cloud-init/artifacts/format-mount-nvme-root.sh
+++ parts/linux/cloud-init/artifacts/format-mount-nvme-root.sh
@@ -14,18 +14,18 @@ KUBELET_DIR="/var/lib/kubelet"
 mkdir -p "${MOUNT_POINT}"
 
 SENTINEL_FILE="/opt/azure/containers/bind-sentinel"
-if [ ! -e "$SENTINEL_FILE" ]; then
-    # Partition and format the NVMe disk if it's not already done.
-    if [ -e /dev/disk/azure/local/by-index/1 ] && [ ! -e /dev/disk/azure/local/by-index/1-part1 ]; then
-        parted /dev/disk/azure/local/by-index/1 --script mklabel gpt mkpart primary ext4 0% 100%
-        mkfs.ext4 -F /dev/disk/azure/local/by-index/1-part1
+if [ ! -e "${SENTINEL_FILE}" ]; then
+    # Bond (via software RAID) and format the NVMe disks if that's not already done.
+    if [ -e /dev/disk/azure/local/by-index/1 ] && [ ! -e /dev/md0 ]; then
+        mdadm --create --verbose /dev/md0 --level=0 --raid-devices=4 /dev/disk/azure/local/by-index/1 /dev/disk/azure/local/by-index/2 /dev/disk/azure/local/by-index/3 /dev/disk/azure/local/by-index/4
+        mkfs.ext4 -F /dev/md0
     fi
-    mount /dev/disk/azure/local/by-index/1-part1 "${MOUNT_POINT}"
-    mv "$KUBELET_DIR" "${KUBELET_MOUNT_POINT}"
-    touch "$SENTINEL_FILE"
+    mount /dev/md0 "${MOUNT_POINT}"
+    mv "${KUBELET_DIR}" "${KUBELET_MOUNT_POINT}"
+    touch "${SENTINEL_FILE}"
 else
     # On subsequent boots, the disk should already be partitioned and formatted, so just mount it.
-    mount /dev/disk/azure/local/by-index/1-part1 "${MOUNT_POINT}"
+    mount /dev/md0 "${MOUNT_POINT}"
 fi
 
 # on every boot, bind mount the kubelet directory back to the expected
