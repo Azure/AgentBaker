1b6fdc7ab9dc58c8f664dfc7b7307b6eb41683b9
Cameron Meissner <cameronmeissner@gmail.com>
2026-02-25 16:33:59 -0800

    chore: fix capture filter

--- parts/linux/cloud-init/artifacts/aks-pcap.sh
+++ parts/linux/cloud-init/artifacts/aks-pcap.sh
@@ -3,4 +3,4 @@ set -uxo pipefail
 
 mkdir -p /var/log/azure/aks
 
-timeout 300 tcpdump -i eth0 -s 0 -w /var/log/azure/aks/aks-node.pcap 'dst port 443' || /opt/azure/containers/aks-log-collector.sh >/var/log/azure/aks/cse-aks-log-collector.log 2>&1
+timeout 300 tcpdump -i eth0 -s 0 -w /var/log/azure/aks/aks-node.pcap 'port 443' || /opt/azure/containers/aks-log-collector.sh >/var/log/azure/aks/cse-aks-log-collector.log 2>&1
