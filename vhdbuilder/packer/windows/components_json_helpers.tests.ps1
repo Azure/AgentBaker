6547dd3afcbe0e33a13a011453a2b45e9345452e
Jane Jung <janejung@microsoft.com>
2026-03-04 14:02:52 +1300

    adding more tests

--- vhdbuilder/packer/windows/components_json_helpers.tests.ps1
+++ vhdbuilder/packer/windows/components_json_helpers.tests.ps1
@@ -38,12 +38,16 @@ Describe 'Tests of GetAllCachedThings ' {
         $windowsSettingsTestString = '{
 "WindowsBaseVersions": {
 "2019": {
+  "os_disk_size": "35",
+  "base_image_publisher": "MicrosoftWindowsServer",
+  "base_image_offer": "WindowsServer",
   "base_image_sku": "2019-Datacenter-Core-smalldisk",
   "windows_image_name": "windows-2019",
   "base_image_version": "17763.6893.250210",
   "patches_to_apply": [{"id": "patchid", "url": "patch_url"}]
 },
  "23H2-gen2": {
+  "os_disk_size": "40",
   "base_image_sku": "2019-Datacenter-Core-smalldisk",
   "windows_image_name": "windows-2019",
   "base_image_version": "17763.6893.250210",
@@ -160,6 +164,54 @@ Describe 'Tests of GetAllCachedThings ' {
         $allpackages = GetAllCachedThings $componentsJson $windowsSettings
 
         $allpackages | Should -Be ( $allpackages | Sort-Object )
+    }
+
+    it 'has the base image sku in it' {
+        $windowsSku = "2019"
+
+        $allpackages = GetAllCachedThings $componentsJson $windowsSettings
+
+        $allpackages | Should -Contain "Windows 2019 base image sku: 2019-Datacenter-Core-smalldisk"
+    }
+
+    it 'has the os disk size in it' {
+        $windowsSku = "2019"
+
+        $allpackages = GetAllCachedThings $componentsJson $windowsSettings
+
+        $allpackages | Should -Contain "Windows 2019 os disk size: 35"
+    }
+
+    it 'has the base image publisher when set' {
+        $windowsSku = "2019"
+
+        $allpackages = GetAllCachedThings $componentsJson $windowsSettings
+
+        $allpackages | Should -Contain "Windows 2019 base image publisher: MicrosoftWindowsServer"
+    }
+
+    it 'has the base image offer when set' {
+        $windowsSku = "2019"
+
+        $allpackages = GetAllCachedThings $componentsJson $windowsSettings
+
+        $allpackages | Should -Contain "Windows 2019 base image offer: WindowsServer"
+    }
+
+    it 'does not have base image publisher when not set' {
+        $windowsSku = "23H2-gen2"
+
+        $allpackages = GetAllCachedThings $componentsJson $windowsSettings
+
+        ($allpackages | Where-Object { $_ -like "Windows 23H2-gen2 base image publisher:*" }) | Should -BeNullOrEmpty
+    }
+
+    it 'does not have base image offer when not set' {
+        $windowsSku = "23H2-gen2"
+
+        $allpackages = GetAllCachedThings $componentsJson $windowsSettings
+
+        ($allpackages | Where-Object { $_ -like "Windows 23H2-gen2 base image offer:*" }) | Should -BeNullOrEmpty
     }
 }
 
@@ -1275,7 +1327,7 @@ Describe 'Tests of components-test.json ' {
 
         $artifacts["c:\akse-cache\oci-test-arch\"] | Should -Contain "mcr.microsoft.com/aks/oci-test-registry-arch-amd64:2.0.0"
     }
-    
+
     it 'can get OCI artifacts with CPU_ARCH and version variable replacement' {
         $CPU_ARCH = "amd64"
         $artifacts = GetOCIArtifactsFromComponentsJson $componentsJson
@@ -1312,7 +1364,7 @@ Describe 'Tests of components-test.json ' {
 
     it 'skips OCI artifacts with missing windowsDownloadLocation' {
         $artifacts = GetOCIArtifactsFromComponentsJson $componentsJson
-        
+
         # No entry should exist for the artifact without a download location
         $artifacts.Keys | ForEach-Object { $_ } | Should -Not -Contain "mcr.microsoft.com/aks/oci-test-registry-no-location:3.0.0"
     }
@@ -1320,7 +1372,7 @@ Describe 'Tests of components-test.json ' {
     it 'can get OCI artifacts from the default when there is no windows override set' {
         # Modify a copy of the components.json to test default behavior
         $componentsJsonCopy = $componentsJson | ConvertTo-Json -Depth 10 | ConvertFrom-Json
-        
+
         # Find the oci-test-registry artifact and set windowsVersions to null to test default behavior
         foreach ($artifact in $componentsJsonCopy.OCIArtifacts) {
             if ($artifact.registry -like "*oci-test-registry*" -and $artifact.repository -like "*oci-test-registry*") {
@@ -1328,9 +1380,9 @@ Describe 'Tests of components-test.json ' {
                 break
             }
         }
-        
+
         $artifacts = GetOCIArtifactsFromComponentsJson $componentsJsonCopy
-        
+
         # Should still get the artifact using the default version
         $artifacts["c:\akse-cache\oci-test\"] | Should -Contain "mcr.microsoft.com/aks/oci-test-registry:1.0.0"
     }
