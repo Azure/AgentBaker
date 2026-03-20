0130ba45bf4cc3445e08dedf814ab4c152596c41
Tim Wright <wrightt@microsoft.com>
2026-03-20 05:49:52 +0000

    reformat

--- staging/cse/windows/aa_commonfuncs.ps1
+++ staging/cse/windows/aa_commonfuncs.ps1
@@ -1,20 +1,23 @@
 # Internal wrapper around nssm.exe; isolated so tests can mock it without needing a real binary.
-function Invoke-NssmExe {
+function Invoke-NssmExe
+{
     param(
-        [Parameter(Mandatory=$true)][string]$KubeDir,
-        [Parameter(Mandatory=$true, ValueFromRemainingArguments=$true)][string[]]$NssmArguments
+        [Parameter(Mandatory = $true)][string]$KubeDir,
+        [Parameter(Mandatory = $true, ValueFromRemainingArguments = $true)][string[]]$NssmArguments
     )
     & "$KubeDir\nssm.exe" @NssmArguments | RemoveNulls
 }
 
 # Invokes nssm.exe with the given arguments and throws if the exit code is non-zero.
-function Invoke-Nssm {
+function Invoke-Nssm
+{
     param(
-        [Parameter(Mandatory=$true)][string]$KubeDir,
-        [Parameter(Mandatory=$true, ValueFromRemainingArguments=$true)][string[]]$NssmArguments
+        [Parameter(Mandatory = $true)][string]$KubeDir,
+        [Parameter(Mandatory = $true, ValueFromRemainingArguments = $true)][string[]]$NssmArguments
     )
     Invoke-NssmExe -KubeDir $KubeDir @NssmArguments
-    if ($LASTEXITCODE -ne 0) {
-        throw "nssm.exe $($NssmArguments -join ' ') failed (exit code $LASTEXITCODE)"
+    if ($LASTEXITCODE -ne 0)
+    {
+        throw "nssm.exe $( $NssmArguments -join ' ' ) failed (exit code $LASTEXITCODE)"
     }
 }
