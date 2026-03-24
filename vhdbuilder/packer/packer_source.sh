426868cadf8499ea75612b81fcc5a8bdadd56117
Saewon Kwak <sakwa@microsoft.com>
2026-03-24 22:26:30 +0000

    feat(vhd): wire aks-hosts-setup files into all packer VHD builds
    Add file provisioners for aks-hosts-setup.sh, aks-hosts-setup.service,
    and aks-hosts-setup.timer to all 10 packer JSON templates, and add
    cpAndMode entries to packer_source.sh to place them at:
    - /opt/azure/containers/aks-hosts-setup.sh (0755)
    - /etc/systemd/system/aks-hosts-setup.service (0644)
    - /etc/systemd/system/aks-hosts-setup.timer (0644)
    
    Without this, enableAKSHostsSetup() in CSE silently skips because
    the VHD-presence guard finds the files missing.

--- vhdbuilder/packer/packer_source.sh
+++ vhdbuilder/packer/packer_source.sh
@@ -316,6 +316,18 @@ copyPackerFiles() {
     LOCALDNS_EXPORTER_SERVICE_DEST=/etc/systemd/system/localdns-exporter@.service
     cpAndMode $LOCALDNS_EXPORTER_SERVICE_SRC $LOCALDNS_EXPORTER_SERVICE_DEST 0644
   fi
+
+  AKS_HOSTS_SETUP_SH_SRC=/home/packer/aks-hosts-setup.sh
+  AKS_HOSTS_SETUP_SH_DEST=/opt/azure/containers/aks-hosts-setup.sh
+  cpAndMode $AKS_HOSTS_SETUP_SH_SRC $AKS_HOSTS_SETUP_SH_DEST 0755
+
+  AKS_HOSTS_SETUP_SVC_SRC=/home/packer/aks-hosts-setup.service
+  AKS_HOSTS_SETUP_SVC_DEST=/etc/systemd/system/aks-hosts-setup.service
+  cpAndMode $AKS_HOSTS_SETUP_SVC_SRC $AKS_HOSTS_SETUP_SVC_DEST 0644
+
+  AKS_HOSTS_SETUP_TIMER_SRC=/home/packer/aks-hosts-setup.timer
+  AKS_HOSTS_SETUP_TIMER_DEST=/etc/systemd/system/aks-hosts-setup.timer
+  cpAndMode $AKS_HOSTS_SETUP_TIMER_SRC $AKS_HOSTS_SETUP_TIMER_DEST 0644
 # ---------------------------------------------------------------------------------------
 
 # ------------------------- Files related to azure-network ------------------------------
