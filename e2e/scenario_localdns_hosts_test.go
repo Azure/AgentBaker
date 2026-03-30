d56cbf9583704bccac5b17d867d875e6b68485b5
Saewon Kwak <sakwa@microsoft.com>
2026-03-30 06:10:28 +0000

    fix hosts plugin e2e: use configured FQDN and add TrustedLaunch for ACL
    ValidateLocalDNSHostsPluginBypass was hardcoding packages.microsoft.com
    as the test FQDN, but that FQDN is not in the NBC's CriticalFQDNs list
    so it never appears in /etc/localdns/hosts. Use the first entry from
    GetDefaultFQDNsForValidation() (mcr.microsoft.com) instead.
    
    ACL subtests were missing the VMConfigMutator to enable TrustedLaunch,
    causing ARM to reject the VMSS creation with a 400 BadRequest.
    
    Also simplify GetDefaultFQDNsForValidation to only return public cloud
    FQDNs since AgentBaker e2e only runs in public cloud.

--- e2e/scenario_localdns_hosts_test.go
+++ e2e/scenario_localdns_hosts_test.go
@@ -6,6 +6,7 @@ import (
 	aksnodeconfigv1 "github.com/Azure/agentbaker/aks-node-controller/pkg/gen/aksnodeconfig/v1"
 	"github.com/Azure/agentbaker/e2e/config"
 	"github.com/Azure/agentbaker/pkg/agent/datamodel"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v7"
 )
 
 // Test_LocalDNSHostsPlugin tests the localdns hosts plugin across all supported distros
@@ -16,13 +17,16 @@ import (
 // Run a single distro with: go test -run "Test_LocalDNSHostsPlugin/AzureLinuxV3" -v
 func Test_LocalDNSHostsPlugin(t *testing.T) {
 	tests := []struct {
-		name string
-		vhd  *config.Image
+		name            string
+		vhd             *config.Image
+		vmConfigMutator func(*armcompute.VirtualMachineScaleSet)
 	}{
 		{name: "Ubuntu2204", vhd: config.VHDUbuntu2204Gen2Containerd},
 		{name: "Ubuntu2404", vhd: config.VHDUbuntu2404Gen2Containerd},
 		{name: "AzureLinuxV3", vhd: config.VHDAzureLinuxV3Gen2},
-		{name: "ACL", vhd: config.VHDACLGen2TL},
+		{name: "ACL", vhd: config.VHDACLGen2TL, vmConfigMutator: func(vmss *armcompute.VirtualMachineScaleSet) {
+			vmss.Properties = addTrustedLaunchToVMSS(vmss.Properties)
+		}},
 	}
 
 	for _, tt := range tests {
@@ -35,6 +39,7 @@ func Test_LocalDNSHostsPlugin(t *testing.T) {
 					BootstrapConfigMutator: func(nbc *datamodel.NodeBootstrappingConfiguration) {
 						nbc.AgentPoolProfile.LocalDNSProfile.EnableHostsPlugin = true
 					},
+					VMConfigMutator: tt.vmConfigMutator,
 				},
 			})
 		})
@@ -49,13 +54,16 @@ func Test_LocalDNSHostsPlugin(t *testing.T) {
 // Run a single distro with: go test -run "Test_LocalDNSHostsPlugin_Scriptless/Ubuntu2204" -v
 func Test_LocalDNSHostsPlugin_Scriptless(t *testing.T) {
 	tests := []struct {
-		name string
-		vhd  *config.Image
+		name            string
+		vhd             *config.Image
+		vmConfigMutator func(*armcompute.VirtualMachineScaleSet)
 	}{
 		{name: "Ubuntu2204", vhd: config.VHDUbuntu2204Gen2Containerd},
 		{name: "Ubuntu2404", vhd: config.VHDUbuntu2404Gen2Containerd},
 		{name: "AzureLinuxV3", vhd: config.VHDAzureLinuxV3Gen2},
-		{name: "ACL", vhd: config.VHDACLGen2TL},
+		{name: "ACL", vhd: config.VHDACLGen2TL, vmConfigMutator: func(vmss *armcompute.VirtualMachineScaleSet) {
+			vmss.Properties = addTrustedLaunchToVMSS(vmss.Properties)
+		}},
 	}
 
 	for _, tt := range tests {
@@ -63,8 +71,9 @@ func Test_LocalDNSHostsPlugin_Scriptless(t *testing.T) {
 			RunScenario(t, &Scenario{
 				Description: "Tests that localdns hosts plugin works correctly on " + tt.name + " (scriptless)",
 				Config: Config{
-					Cluster: ClusterKubenet,
-					VHD:     tt.vhd,
+					Cluster:         ClusterKubenet,
+					VHD:             tt.vhd,
+					VMConfigMutator: tt.vmConfigMutator,
 					AKSNodeConfigMutator: func(config *aksnodeconfigv1.Configuration) {
 						config.LocalDnsProfile.EnableHostsPlugin = true
 					},
