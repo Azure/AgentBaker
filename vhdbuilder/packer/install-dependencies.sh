1e7bc473d8c152a5193c0531ab2018e56efa5c9a
Keith Pimm <keithpimm@microsoft.com>
2026-04-03 12:26:15 -0500

    Fix POSIX sh compatibility issue

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -31,8 +31,8 @@ source /home/packer/provision_source_benchmarks.sh
 source /home/packer/provision_source_distro.sh
 source /home/packer/tool_installs.sh
 source /home/packer/tool_installs_distro.sh
-[[ -f /home/packer/install-ig.sh ]] && source /home/packer/install-ig.sh
-[[ -f /home/packer/install-node-exporter.sh ]] && source /home/packer/install-node-exporter.sh
+[ -f /home/packer/install-ig.sh ] && source /home/packer/install-ig.sh
+[ -f /home/packer/install-node-exporter.sh ] && source /home/packer/install-node-exporter.sh
 
 CPU_ARCH=$(getCPUArch)  #amd64 or arm64
 SYSTEMD_ARCH=$(getSystemdArch)  # x86-64 or arm64
