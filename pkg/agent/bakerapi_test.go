5d7fc8dbab1285d0ebdb5d1e9bc4e005f7ab0474
aadhar-agarwal <108542189+aadhar-agarwal@users.noreply.github.com>
2026-04-14 07:56:10 -0700

    feat: add OSSKU-based IsCgroupV2 detection and CustomizedImageTrustedLaunch distro (#8252)
    Signed-off-by: Aadhar Agarwal <aadagarwal@microsoft.com>

--- pkg/agent/bakerapi_test.go
+++ pkg/agent/bakerapi_test.go
@@ -254,6 +254,15 @@ var _ = Describe("AgentBaker API implementation tests", func() {
 			Expect(err).NotTo(HaveOccurred())
 		})
 
+		It("should not return an error for customized trusted launch image", func() {
+			config.AgentPoolProfile.Distro = datamodel.CustomizedImageTrustedLaunch
+			agentBaker, err := NewAgentBaker()
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = agentBaker.GetNodeBootstrapping(context.Background(), config)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
 		It("should not return an error for customized windows image", func() {
 			config.AgentPoolProfile.Distro = datamodel.CustomizedWindowsOSImage
 			agentBaker, err := NewAgentBaker()
