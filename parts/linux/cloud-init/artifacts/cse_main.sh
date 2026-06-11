c3ef7a56d8a52e1785cdbc564f25747822fdb192
Hossam Rabbouh <horabbouh@microsoft.com>
2026-06-11 19:19:23 -0400

    Add opt-out tag

--- parts/linux/cloud-init/artifacts/cse_main.sh
+++ parts/linux/cloud-init/artifacts/cse_main.sh
@@ -405,8 +405,11 @@ function nodePrep {
     # Configure Azure network settings (udev rules for NIC configuration)
     logs_to_events "AKS.CSE.ensureAzureNetworkConfig" ensureAzureNetworkConfig
 
-    # Bring up secondary Standard-type NICs (if any) via IMDS metadata
-    logs_to_events "AKS.CSE.configureSecondaryNICs" configureSecondaryNICs || exit $ERR_SECONDARY_NIC_CONFIG_FAIL
+    # Bring up secondary Standard-type NICs (if any) via IMDS metadata.
+    # Can be disabled via VMSS tag "SkipSecondaryNICConfig=true".
+    if [ "$(get_imds_vm_tag_value 'SkipSecondaryNICConfig')" != "true" ]; then
+        logs_to_events "AKS.CSE.configureSecondaryNICs" configureSecondaryNICs || exit $ERR_SECONDARY_NIC_CONFIG_FAIL
+    fi
 
     # Determine if GPU driver installation should be skipped
     export -f should_skip_nvidia_drivers
