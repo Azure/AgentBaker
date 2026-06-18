ad3147e4f18506c3558c56d7be06b0d5ee0c6551
cameronmeissner <cameronmeissner@gmail.com>
2026-06-18 12:25:51 -0700

    test(e2e): add Test_Ubuntu2004Gen2FIPS E2E scenario

--- e2e/config/vhd.go
+++ e2e/config/vhd.go
@@ -69,11 +69,23 @@ var (
 		Distro:  datamodel.AKSUbuntuContainerd2204TLGen2,
 		Gallery: imageGalleryLinux,
 	}
+
 	VHDUbuntu2004FIPSContainerd = &Image{
 		Name:                  "2004fipscontainerd",
 		OS:                    OSUbuntu,
 		Arch:                  "amd64",
 		Distro:                datamodel.AKSUbuntuFipsContainerd2004,
+		Gallery:               imageGalleryLinux,
+		UnsupportedLocalDns:   true,
+		UnsupportedGen2:       true,
+		SkipOldVHDValidations: true,
+	}
+
+	VHDUbuntu2004FIPSGen2Containerd = &Image{
+		Name:                  "2004fipsgen2containerd",
+		OS:                    OSUbuntu,
+		Arch:                  "amd64",
+		Distro:                datamodel.AKSUbuntuFipsContainerd2004Gen2,
 		Gallery:               imageGalleryLinux,
 		UnsupportedLocalDns:   true,
 		UnsupportedGen2:       true,
