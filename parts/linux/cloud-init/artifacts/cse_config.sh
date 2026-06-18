5d426b8c96f4f58e81f90c49a5f8acc3a243814a
mipres <mipres@microsoft.com>
2026-06-17 20:19:43 -0700

    Fixed if string compare.

--- parts/linux/cloud-init/artifacts/cse_config.sh
+++ parts/linux/cloud-init/artifacts/cse_config.sh
@@ -1108,7 +1108,7 @@ ensureGPUDrivers() {
 dnf_install_amd_ama_core_package() {
     ver=$1; retries=$2; wait_sleep=$3; timeout=$4; shift && shift && shift && shift
     # Currently version 1.5.0 is supported.  Add more versions as they become available.
-    if [ "$ver" == "1.5.0" ]; then
+    if [ "$ver" = "1.5.0" ]; then
         AMD_AMA_CORE_PACKAGE="https://download.microsoft.com/download/f030c57a-a582-4bcc-9c7c-593c9a486814/amd-ama-core_1.5.0-20260424092403.x86_64.rpm"
     else
         return 1
@@ -1152,7 +1152,7 @@ setupAmdAma() {
 
         # Install FW package
         AMD_AMA_FIRMWARE_PACKAGE="${AMD_AMA_DRIVER_PACKAGE/driver/firmware}"
-        if [ -z "$AMD_AMA_FIRMWARE_PACKAGE" ] || [ "$AMD_AMA_FIRMWARE_PACKAGE" == "$AMD_AMA_DRIVER_PACKAGE" ]; then
+        if [ -z "$AMD_AMA_FIRMWARE_PACKAGE" ] || [ "$AMD_AMA_FIRMWARE_PACKAGE" = "$AMD_AMA_DRIVER_PACKAGE" ]; then
             echo "Unable to find AMD AMA firmware package for current kernel version, exiting..."
             exit $ERR_AMDAMA_DRIVER_NOT_FOUND
         fi
