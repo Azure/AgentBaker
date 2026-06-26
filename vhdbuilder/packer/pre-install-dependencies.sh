29e805f711323ac7247bc6381ee34ad313d47f95
cameronmeissner <cameronmeissner@gmail.com>
2026-06-26 15:24:33 -0700

    chore: typo

--- vhdbuilder/packer/pre-install-dependencies.sh
+++ vhdbuilder/packer/pre-install-dependencies.sh
@@ -37,7 +37,7 @@ capture_benchmark "${SCRIPT_NAME}_source_packer_files_and_declare_variables"
 copyPackerFiles
 
 # Install minimal build dependencies as needed (currently only needed on Ubuntu 26.04)
-if [ "${OS_RELEASE}" = "26.04" ]; then
+if [ "${UBUNTU_RELEASE}" = "26.04" ]; then
   installMinimalBuildDeps
 fi
 
