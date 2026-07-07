23f68b4e2b4415bc0996beb99ee4b7018c1666e2
cameronmeissner <cameronmeissner@gmail.com>
2026-07-07 12:46:37 -0700

    chore: purge after bcc install

--- vhdbuilder/scripts/linux/ubuntu/tool_installs_ubuntu.sh
+++ vhdbuilder/scripts/linux/ubuntu/tool_installs_ubuntu.sh
@@ -66,6 +66,8 @@ installBcc() {
     # only ensuring they are installed above.
     if [ "${VERSION}" = "22.04" ] || [ "${VERSION}" = "24.04" ]; then
         apt_get_purge 120 5 300 bison cmake flex libedit-dev libllvm14 llvm-14-dev libclang-14-dev zlib1g-dev libelf-dev libfl-dev || exit $ERR_BCC_INSTALL_TIMEOUT
+    elif [ "${VERSION}" = "26.04" ]; then
+        apt_get_purge 120 5 300 git bison cmake flex libedit-dev libllvm22 llvm-22-dev libclang-22-dev zlib1g-dev libelf-dev libfl-dev || exit $ERR_BCC_INSTALL_TIMEOUT
     else
         apt_get_purge 120 5 300 git bison cmake flex libedit-dev libllvm6.0 llvm-6.0-dev libclang-6.0-dev zlib1g-dev libelf-dev libfl-dev || exit $ERR_BCC_INSTALL_TIMEOUT
     fi
