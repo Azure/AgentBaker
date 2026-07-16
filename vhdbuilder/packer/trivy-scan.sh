84374b6bae51ab0cf0689127d633c069f16da1b3
cameronmeissner <cameronmeissner@gmail.com>
2026-07-16 14:34:26 -0700

    chore: correctly install azcli on 26.04 scanning VM

--- vhdbuilder/packer/trivy-scan.sh
+++ vhdbuilder/packer/trivy-scan.sh
@@ -107,6 +107,19 @@ install_azure_cli() {
         return 0
     fi
 
+    if [ "$OS_VERSION" = "26.04" ]; then
+        apt_get_update
+        apt_get_install 5 1 60 python3-pip
+        pip install azure-cli
+        export PATH="/home/$TEST_VM_ADMIN_USERNAME/.local/bin:$PATH"
+        CHECKAZ=$(pip freeze | grep "azure-cli==")
+        if [ -z "$CHECKAZ" ]; then
+            echo "Azure CLI is not installed properly."
+            exit 1
+        fi
+        return 0
+    fi
+
     if [ "${ARCHITECTURE,,}" = "arm64" ]; then
         if [ "$OS_VERSION" = "22.04" ]; then
             apt_get_update
@@ -120,7 +133,7 @@ install_azure_cli() {
             fi
             return 0
         fi
-        if [ "$OS_VERSION" = "24.04" ] || [ "$OS_VERSION" = "26.04" ]; then
+        if [ "$OS_VERSION" = "24.04" ]; then
             apt_get_install 5 1 60 ca-certificates curl apt-transport-https lsb-release gnupg
             curl -sL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
             echo "deb [arch=arm64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
