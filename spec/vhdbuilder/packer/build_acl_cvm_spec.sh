0709dda4dc89ff866bcca3962b2c3189304fc064
Mayan Singh <mayankfz@gmail.com>
2026-08-17 07:19:17 +0000

    add acl cvm support
    Signed-off-by: Mayan Singh <mayankfz@gmail.com>

---
+++ spec/vhdbuilder/packer/build_acl_cvm_spec.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env shellspec
+
+Describe 'build-acl-cvm.sh'
+  setup() {
+    TEST_ROOT="${SHELLSPEC_TMPBASE}/build-acl-cvm"
+    MOCK_BIN="${TEST_ROOT}/bin"
+    BASE_TEMPLATE="${TEST_ROOT}/acl.json"
+    CAPTURED_TEMPLATE="${TEST_ROOT}/acl-cvm.json"
+    rm -rf "$TEST_ROOT"
+    mkdir -p "$MOCK_BIN"
+    cat > "$BASE_TEMPLATE" <<'EOF'
+{"builders":[{"type":"azure-arm","shared_image_gallery_destination":{"image_name":"acl"}}],"provisioners":[{"type":"shell","inline":["true"]}]}
+EOF
+    cat > "${MOCK_BIN}/packer" <<'EOF'
+#!/bin/sh
+for argument in "$@"; do
+  template=$argument
+done
+cp "$template" "$CAPTURED_TEMPLATE"
+EOF
+    chmod +x "${MOCK_BIN}/packer"
+    export CAPTURED_TEMPLATE
+    PATH="${MOCK_BIN}:${PATH}"
+    export PATH
+  }
+
+  BeforeEach 'setup'
+
+  build_and_validate_cvm_template() {
+    ./vhdbuilder/packer/build-acl-cvm.sh &&
+      jq -e '
+        .builders[0].secure_boot_enabled == true and
+        .builders[0].vtpm_enabled == true and
+        .builders[0].security_type == "ConfidentialVM" and
+        .builders[0].security_encryption_type == "VMGuestStateOnly" and
+        .builders[0].shared_image_gallery_destination.specialized == true and
+        .builders[0].shared_image_gallery_destination.confidential_vm_image_encryption_type == "EncryptedVMGuestStateOnlyWithPmk" and
+        .provisioners == [{"type":"shell","inline":["true"]}]
+      ' "$CAPTURED_TEMPLATE" >/dev/null
+  }
+
+  It 'applies the Azure Linux CVM settings without changing provisioners'
+    ACL_PACKER_TEMPLATE="$BASE_TEMPLATE"
+    export ACL_PACKER_TEMPLATE
+    When call build_and_validate_cvm_template
+    The status should be success
+    The output should include "Using ACL CVM settings derived from $BASE_TEMPLATE"
+  End
+End
