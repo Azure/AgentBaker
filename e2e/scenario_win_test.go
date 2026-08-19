ecb5cca9ff0f01be16cc8fb19fd3f1ce8932d837
Tim Wright <wrightt@microsoft.com>
2026-08-19 13:00:02 +1200

    fix: remove TLS settings from WS2022 VHDs. (#9248)

--- e2e/scenario_win_test.go
+++ e2e/scenario_win_test.go
@@ -71,7 +71,6 @@ func Test_Windows2022_AzureNetwork(t *testing.T) {
 				ValidateDotnetNotInstalledWindows(ctx, s)
 				ValidateWindowsSystemServicesRestartConfiguration(ctx, s)
 				ValidateCollectWindowsLogsScript(ctx, s)
-				ValidateWindowsSecureTLSEnabled(ctx, s)
 			},
 		},
 	})
@@ -121,7 +120,6 @@ func Test_Windows2022Gen2AzureNetwork(t *testing.T) {
 				ValidateFileHasContent(ctx, s, "/AzureData/CustomDataSetupScript.log", "CSEScriptsPackageUrl used for provision is https://packages.aks.azure.com/aks/windows/cse/aks-windows-cse-scripts-current.zip")
 				ValidateWindowsSystemServicesRestartConfiguration(ctx, s)
 				ValidateCollectWindowsLogsScript(ctx, s)
-				ValidateWindowsSecureTLSEnabled(ctx, s)
 			},
 		},
 	})
