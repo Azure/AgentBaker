d905939a09a113cf7b0aa4ddcf1db56e6c649435
Mayan Singh <mayankfz@gmail.com>
2026-09-03 07:37:39 +0000

    fix: address ACL CVM review comments

--- vhdbuilder/packer/build-acl-cvm.sh
+++ vhdbuilder/packer/build-acl-cvm.sh
@@ -2,6 +2,17 @@
 set -euo pipefail
 
 base_template=${ACL_PACKER_TEMPLATE:-vhdbuilder/packer/vhd-image-builder-acl.json}
+cvm_template=$(mktemp "${TMPDIR:-/tmp}/acl-cvm.XXXXXX.json")
+trap 'rm -f "$cvm_template"' EXIT
 
 echo "Using pre-CPS ACL image settings derived from $base_template"
-packer build -timestamp-ui -var-file=vhdbuilder/packer/settings.json "$base_template"
+jq '
+	.builders[0].secure_boot_enabled = true |
+	.builders[0].vtpm_enabled = true |
+	.builders[0].security_type = "ConfidentialVM" |
+	.builders[0].security_encryption_type = "VMGuestStateOnly" |
+	.builders[0].shared_image_gallery_destination.specialized = true |
+	.builders[0].shared_image_gallery_destination.confidential_vm_image_encryption_type = "EncryptedVMGuestStateOnlyWithPmk"
+' "$base_template" > "$cvm_template"
+
+packer build -timestamp-ui -var-file=vhdbuilder/packer/settings.json "$cvm_template"
