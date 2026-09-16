7a46e4c9ba029ec7eee641e7458bd30896369e52
Zachary Bailey <zachary.r.bailey@outlook.com>
2026-09-16 15:30:04 -0400

    fix

--- vhdbuilder/packer/ubuntu-2604-cvm/trim-2604-cvm-packages.sh
+++ vhdbuilder/packer/ubuntu-2604-cvm/trim-2604-cvm-packages.sh
@@ -46,7 +46,7 @@ main() {
         echo "No installed server-cvm packages marked for removal were found"
     fi
 
-    apt-mark manual jq rsyslog sudo
+    apt-mark manual curl jq rsyslog sudo
     verifyRequiredPackagesInstalled
 }
 
