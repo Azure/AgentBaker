ce32970c8ec54b00a056b0b1904aab3c720f408c
Cameron Meissner <cameronmeissner@gmail.com>
2026-09-18 14:06:58 -0700

    feat(build): introduce TRUSTED_LAUNCH_SUPPORTED to later no longer build dedicated TrustedLaunch images (#9487)

--- vhdbuilder/packer/produce-packer-settings-functions.sh
+++ vhdbuilder/packer/produce-packer-settings-functions.sh
@@ -40,6 +40,20 @@ function produce_ua_token() {
 	fi
 }
 
+function resolve_security_type_feature() {
+	if [ "${ENABLE_TRUSTED_LAUNCH,,}" = "true" ]; then
+		# TODO: remove once all relevant images have been updated to TrustedLaunchSupported
+		# Note that ordering matters here - ENABLE_TRUSTED_LAUNCH -> TrustedLaunch takes precedence over TRUSTED_LAUNCH_SUPPORTED -> TrustedLaunchSupported
+		SECURITY_TYPE_FEATURE="TrustedLaunch"
+	elif [ "${TRUSTED_LAUNCH_SUPPORTED,,}" = "true" ]; then
+		SECURITY_TYPE_FEATURE="TrustedLaunchSupported"
+	elif grep -q "cvm" <<<"$FEATURE_FLAGS"; then
+		SECURITY_TYPE_FEATURE="ConfidentialVMSupported"
+	else
+		SECURITY_TYPE_FEATURE="Standard"
+	fi
+}
+
 function ensure_sig_image_name_linux() {
 	# Ensure the SIG name
 	if [ -z "${SIG_GALLERY_NAME}" ]; then
@@ -514,7 +528,10 @@ function ensure_sig_vhd_exists() {
 		# shellcheck disable=SC3010
 		if [[ ${ARCHITECTURE,,} == "arm64" ]] || grep -q "cvm" <<<"$FEATURE_FLAGS" || [[ ${HYPERV_GENERATION} == "V1" ]]; then
 			if [ "${ARCHITECTURE,,}" = "arm64" ]; then
-				if [ "${ENABLE_TRUSTED_LAUNCH}" = "True" ]; then
+				# This path must be used for images that are built on VMs with TrustedLaunch enabled (e.g. images that ONLY are designed to run on VMs with TrustedLaunch enabled).
+				# At the time of writing, all "TL" images are built using the "Standard" security type, and thus can be snapshotted into image definitions with the "TrustedLaunchSupported" security type.
+				# TODO: revisit whether we can remove this image definition creation path if we plan on continuing to always build trusted launch capable images on standard VMs.
+				if [ "${ENABLE_TRUSTED_LAUNCH,,}" = "true" ]; then
 					az sig image-definition create \
 						--resource-group ${AZURE_RESOURCE_GROUP_NAME} \
 						--gallery-name ${SIG_GALLERY_NAME} \
@@ -527,6 +544,19 @@ function ensure_sig_vhd_exists() {
 						--location ${AZURE_LOCATION} \
 						--architecture Arm64 \
 						--features "DiskControllerTypes=SCSI,NVMe SecurityType=TrustedLaunch"
+				elif [ "${TRUSTED_LAUNCH_SUPPORTED,,}" = "true" ]; then
+					az sig image-definition create \
+						--resource-group ${AZURE_RESOURCE_GROUP_NAME} \
+						--gallery-name ${SIG_GALLERY_NAME} \
+						--gallery-image-definition ${SIG_IMAGE_NAME} \
+						--publisher microsoft-aks \
+						--offer ${SIG_GALLERY_NAME} \
+						--sku ${SIG_IMAGE_NAME} \
+						--os-type ${OS_TYPE} \
+						--hyper-v-generation ${HYPERV_GENERATION} \
+						--location ${AZURE_LOCATION} \
+						--architecture Arm64 \
+						--features "DiskControllerTypes=SCSI,NVMe SecurityType=TrustedLaunchSupported"
 				else
 					az sig image-definition create \
 						--resource-group ${AZURE_RESOURCE_GROUP_NAME} \
@@ -568,7 +598,10 @@ function ensure_sig_vhd_exists() {
 			fi
 		else
 			# TL can only be enabled on Gen2 VMs, therefore if TL enabled = true, mark features for both TL and NVMe
-			if [ "${ENABLE_TRUSTED_LAUNCH}" = "True" ]; then
+			# This path must be used for images that are built on VMs with TrustedLaunch enabled (e.g. images that ONLY are designed to run on VMs with TrustedLaunch enabled).
+			# At the time of writing, all "TL" images are built using the "Standard" security type, and thus can be snapshotted into image definitions with the "TrustedLaunchSupported" security type.
+			# TODO: revisit whether we can remove this image definition creation path if we plan on continuing to always build trusted launch capable images on standard VMs.
+			if [ "${ENABLE_TRUSTED_LAUNCH,,}" = "true" ]; then
 				az sig image-definition create \
 					--resource-group ${AZURE_RESOURCE_GROUP_NAME} \
 					--gallery-name ${SIG_GALLERY_NAME} \
@@ -580,6 +613,18 @@ function ensure_sig_vhd_exists() {
 					--hyper-v-generation ${HYPERV_GENERATION} \
 					--location ${AZURE_LOCATION} \
 					--features "DiskControllerTypes=SCSI,NVMe SecurityType=TrustedLaunch"
+			elif [ "${TRUSTED_LAUNCH_SUPPORTED,,}" = "true" ]; then
+				az sig image-definition create \
+					--resource-group ${AZURE_RESOURCE_GROUP_NAME} \
+					--gallery-name ${SIG_GALLERY_NAME} \
+					--gallery-image-definition ${SIG_IMAGE_NAME} \
+					--publisher microsoft-aks \
+					--offer ${SIG_GALLERY_NAME} \
+					--sku ${SIG_IMAGE_NAME} \
+					--os-type ${OS_TYPE} \
+					--hyper-v-generation ${HYPERV_GENERATION} \
+					--location ${AZURE_LOCATION} \
+					--features "DiskControllerTypes=SCSI,NVMe SecurityType=TrustedLaunchSupported"
 			else
 				# For vanilla Gen2, mark only NVMe
 				az sig image-definition create \
--- vhdbuilder/packer/test/run-test.sh
+++ vhdbuilder/packer/test/run-test.sh
@@ -71,6 +71,7 @@ VM_SIZE="Standard_D2ds_v5"
 TARGET_COMMAND_STRING=""
 if [ "${ARCHITECTURE,,}" = "arm64" ]; then
   # Ampere Altra (v5) doesn't support TrustedLaunch; Cobalt 100 (v6) does
+  # TODO: remove once all relevant images have been updated to TrustedLaunchSupported and have corresponding TL-based AgentBaker E2E tests
   if [ "${ENABLE_TRUSTED_LAUNCH,,}" = "true" ]; then
     VM_SIZE="Standard_D2pds_v6"
   else
@@ -79,6 +80,7 @@ if [ "${ARCHITECTURE,,}" = "arm64" ]; then
 fi
 TARGET_COMMAND_STRING="--size $VM_SIZE"
 
+# TODO: remove once all relevant images have been updated to TrustedLaunchSupported and have corresponding TL-based AgentBaker E2E tests
 if [ "${ENABLE_TRUSTED_LAUNCH,,}" = "true" ]; then
   if [ -n "$TARGET_COMMAND_STRING" ]; then
     # To take care of Mariner Kata TL images
