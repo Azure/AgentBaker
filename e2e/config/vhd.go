884ebd02dbb6c69bf5d09c9059f4526c7aba265d
Peter Damianov <ptrdamianov@gmail.com>
2026-09-23 21:35:58 +0000

    update os gb size request

--- e2e/config/vhd.go
+++ e2e/config/vhd.go
@@ -171,11 +171,12 @@ var (
 	}
 
 	VHDUbuntu2404ArmGBContainerd = &Image{
-		Name:    "2404gen2arm64gbcontainerd",
-		OS:      OSUbuntu,
-		Arch:    "arm64",
-		Distro:  datamodel.AKSUbuntuArm64GB200Containerd2404Gen2,
-		Gallery: &Config.GalleryLinux,
+		Name:         "2404gen2arm64gbcontainerd",
+		OS:           OSUbuntu,
+		Arch:         "arm64",
+		Distro:       datamodel.AKSUbuntuArm64GB200Containerd2404Gen2,
+		Gallery:      &Config.GalleryLinux,
+		OSDiskSizeGB: 70,
 	}
 
 	VHDUbuntu2604MinimalGen2Containerd = &Image{
